62bd263bb8e0849f4429047e6ba99e97eb804100
patacongo <patacongo@42af7a65-404d-4744-a932-0658087f49c3>
2007-03-19 16:52:38 +0000

    Cosmetic typo
    git-svn-id: svn://svn.code.sf.net/p/nuttx/code/trunk@100 42af7a65-404d-4744-a932-0658087f49c3

--- examples/ostest/roundrobin.c
+++ examples/ostest/roundrobin.c
@@ -221,7 +221,7 @@ void rr_test(void)
 
   printf("rr_test: Waiting for sieves to complete -- this should take awhile\n");
   printf("rr_test: If RR scheduling is working, they should start and complete at\n");
-  printf("rr_test: about then same time\n");
+  printf("rr_test: about the same time\n");
   fflush(stdout);
 
   pthread_join(sieve2_thread, &result);
