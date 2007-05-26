fb9df8dfaeaecfa93ffbfd70f9d2e7a7c549e187
patacongo <patacongo@42af7a65-404d-4744-a932-0658087f49c3>
2007-05-26 19:22:34 +0000

    Finish FAT directory operations; add option to disable mountpoints; fix ARM compile errors
    git-svn-id: svn://svn.code.sf.net/p/nuttx/code/trunk@252 42af7a65-404d-4744-a932-0658087f49c3

--- arch/arm/src/common/up_unblocktask.c
+++ arch/arm/src/common/up_unblocktask.c
@@ -43,6 +43,7 @@
 #include <debug.h>
 #include <nuttx/arch.h>
 #include "os_internal.h"
+#include "clock_internal.h"
 #include "up_internal.h"
 
 /************************************************************
