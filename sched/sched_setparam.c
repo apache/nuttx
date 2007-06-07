637618e8bb89272d28c06958b18bfa1cb19e9411
patacongo <patacongo@42af7a65-404d-4744-a932-0658087f49c3>
2007-06-07 00:08:40 +0000

    Removed redundant interrupt disable
    git-svn-id: svn://svn.code.sf.net/p/nuttx/code/trunk@268 42af7a65-404d-4744-a932-0658087f49c3

--- sched/sched_setparam.c
+++ sched/sched_setparam.c
@@ -176,9 +176,7 @@ int sched_setparam(pid_t pid, const struct sched_param *param)
            {
              /* A context switch will occur. */
 
-             irqstate_t flags = irqsave();
              up_reprioritize_rtr(tcb, (ubyte)sched_priority);
-             irqrestore(flags);
            }
 
          /* Otherwise, we can just change priority since it has no effect */
@@ -205,9 +203,7 @@ int sched_setparam(pid_t pid, const struct sched_param *param)
            {
              /* A context switch will occur. */
 
-             irqstate_t flags = irqsave();
              up_reprioritize_rtr(tcb, (ubyte)sched_priority);
-             irqrestore(flags);
            }
 
          /* Otherwise, we can just change priority and re-schedule (since it
