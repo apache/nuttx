4a7a565ef397b283545631b92dcd378ed63e8b3c
patacongo <patacongo@42af7a65-404d-4744-a932-0658087f49c3>
2008-01-30 13:08:36 +0000

    Typo
    git-svn-id: svn://svn.code.sf.net/p/nuttx/code/trunk@587 42af7a65-404d-4744-a932-0658087f49c3

--- examples/ostest/sem.c
+++ examples/ostest/sem.c
@@ -1,7 +1,7 @@
 /***********************************************************************
  * sem.c
  *
- *   Copyright (C) 2007 Gregory Nutt. All rights reserved.
+ *   Copyright (C) 2007, 2008 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <spudmonkey@racsa.co.cr>
  *
  * Redistribution and use in source and binary forms, with or without
@@ -14,7 +14,7 @@
  *    notice, this list of conditions and the following disclaimer in
  *    the documentation and/or other materials provided with the
  *    distribution.
- * 3. Neither the name Gregory Nutt nor the names of its contributors may be
+ * 3. Neither the name NuttX nor the names of its contributors may be
  *    used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
@@ -66,7 +66,7 @@ static void *waiter_func(void *parameter)
       printf("waiter_func: Thread %d initial semaphore value = %d\n",  id, value);
     }
 
-  printf("waiter_func: Thread %d aiting on semaphore\n",  id);
+  printf("waiter_func: Thread %d waiting on semaphore\n",  id);
   status = sem_wait(&sem);
   if (status != 0)
     {
