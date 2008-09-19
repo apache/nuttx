7595ec8761eb0e3fc446eea2f952ba0cf1fdf22c
patacongo <patacongo@42af7a65-404d-4744-a932-0658087f49c3>
2008-09-19 00:17:30 +0000

    cosmetic
    git-svn-id: svn://svn.code.sf.net/p/nuttx/code/trunk@936 42af7a65-404d-4744-a932-0658087f49c3

--- lib/lib_stdstream.c
+++ lib/lib_stdstream.c
@@ -1,7 +1,7 @@
-/************************************************************
- * lib_stdstream.c
+/****************************************************************************
+ * lib/lib_stdstream.c
  *
- *   Copyright (C) 2007 Gregory Nutt. All rights reserved.
+ *   Copyright (C) 2007, 2008 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <spudmonkey@racsa.co.cr>
  *
  * Redistribution and use in source and binary forms, with or without
@@ -14,7 +14,7 @@
  *    notice, this list of conditions and the following disclaimer in
  *    the documentation and/or other materials provided with the
  *    distribution.
- * 3. Neither the name Gregory Nutt nor the names of its contributors may be
+ * 3. Neither the name NuttX nor the names of its contributors may be
  *    used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
@@ -31,17 +31,17 @@
  * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  *
- ************************************************************/
+ ****************************************************************************/
 
-/************************************************************
+/****************************************************************************
  * Included Files
- ************************************************************/
+ ****************************************************************************/
 
 #include "lib_internal.h"
 
-/************************************************************
+/****************************************************************************
  * Private Functions
- ************************************************************/
+ ****************************************************************************/
 
 static void stdstream_putc(struct lib_stream_s *this, int ch)
 {
@@ -55,9 +55,9 @@ static void stdstream_putc(struct lib_stream_s *this, int ch)
     }
 }
 
-/************************************************************
+/****************************************************************************
  * Public Functions
- ************************************************************/
+ ****************************************************************************/
 
 void lib_stdstream(struct lib_stdstream_s *stdstream,
                    FILE *stream)
