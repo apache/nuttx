ebefecd9ac604ddff486dc46e25b865d5c08dd1e
patacongo <patacongo@42af7a65-404d-4744-a932-0658087f49c3>
2008-10-09 20:25:33 +0000

    Extend USB serial test
    git-svn-id: svn://svn.code.sf.net/p/nuttx/code/trunk@1017 42af7a65-404d-4744-a932-0658087f49c3

---
+++ examples/usbserial/host.c
@@ -0,0 +1,202 @@
+/****************************************************************************
+ * examples/usbserial/host.c
+ *
+ *   Copyright (C) 2008 Gregory Nutt. All rights reserved.
+ *   Author: Gregory Nutt <spudmonkey@racsa.co.cr>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name NuttX nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <stdio.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+
+/****************************************************************************
+ * Definitions
+ ****************************************************************************/
+
+#define DEFAULT_TTYDEV "/dev/ttyUSB0"
+#define BUFFER_SIZE    1024
+
+/****************************************************************************
+ * Private Data
+ ****************************************************************************/
+
+static const char *g_ttydev = DEFAULT_TTYDEV;
+static char g_buffer[BUFFER_SIZE];
+static const char g_outmsg[] =
+  "I am proud to come to this city as the guest of your distinguished Mayor, "
+  "who has symbolized throughout the world the fighting spirit of West Berlin. "
+  "And I am proud to visit the Federal Republic with your distinguished Chancellor "
+  "who for so many years has committed Germany to democracy and freedom and "
+  "progress, and to come here in the company of my fellow American, General Clay, "
+  "who has been in this city during its great moments of crisis and will come "
+  "again if ever needed.\n"
+  "Two thousand years ago the proudest boast was \"civis Romanus sum.\" Today, "
+  "in the world of freedom, the proudest boast is \"Ich bin ein Berliner.\"\r\""
+  "I appreciate my interpreter translating my German!\n"
+  "There are many people in the world who really don't understand, or say they "
+  "don't, what is the great issue between the free world and the Communist world. "
+  "Let them come to Berlin. There are some who say that communism is the wave of "
+  "the future. Let them come to Berlin. And there are some who say in Europe and "
+  "elsewhere we can work with the Communists. Let them come to Berlin. And there "
+  "are even a few who say that it is true that communism is an evil system, but it "
+  "permits us to make economic progress. Lass' sie nach Berlin kommen. Let them "
+  "come to Berlin.\n"
+  "Freedom has many difficulties and democracy is not perfect, but we have never "
+  "had to put a wall up to keep our people in, to prevent them from leaving us. I "
+  "want to say, on behalf of my countrymen, who live many miles away on the other "
+  "side of the Atlantic, who are far distant from you, that they take the greatest "
+  "pride that they have been able to share with you, even from a distance, the "
+  "story of the last 18 years. I know of no town, no city, that has been besieged "
+  "for 18 years that still lives with the vitality and the force, and the hope and "
+  "the determination of the city of West Berlin. While the wall is the most obvious "
+  "and vivid demonstration of the failures of the Communist system, for all the "
+  "world to see, we take no satisfaction in it, for it is, as your Mayor has said, "
+  "an offense not only against history but an offense against humanity, separating "
+  "families, dividing husbands and wives and brothers and sisters, and dividing a "
+  "people who wish to be joined together.\n"
+  "What is true of this city is true of Germany--real, lasting peace in Europe can "
+  "never be assured as long as one German out of four is denied the elementary "
+  "right of free men, and that is to make a free choice. In 18 years of peace and "
+  "good faith, this generation of Germans has earned the right to be free, "
+  "including the right to unite their families and their nation in lasting peace, "
+  "with good will to all people. You live in a defended island of freedom, but "
+  "your life is part of the main. So let me ask you as I close, to lift your eyes "
+  "beyond the dangers of today, to the hopes of tomorrow, beyond the freedom merely "
+  "of this city of Berlin, or your country of Germany, to the advance of freedom "
+  "everywhere, beyond the wall to the day of peace with justice, beyond yourselves "
+  "and ourselves to all mankind.\n"
+  "Freedom is indivisible, and when one man is enslaved, all are not free. When all "
+  "are free, then we can look forward to that day when this city will be joined as "
+  "one and this country and this great Continent of Europe in a peaceful and hopeful "
+  "globe. When that day finally comes, as it will, the people of West Berlin can take "
+  "sober satisfaction in the fact that they were in the front lines for almost two "
+  "decades.\n"
+  "All free men, wherever they may live, are citizens of Berlin, and, therefore, "
+  "as a free man, I take pride in the words \"Ich bin ein Berliner.\"\n"
+  "President John F. Kennedy - June 26, 1963\n";
+
+/****************************************************************************
+ * show_usage
+ ****************************************************************************/
+
+static void show_usage(const char *progname, int exitcode)
+{
+  fprintf(stderr, "USAGE: %s [<ttydev>]\n", progname);
+  exit(exitcode);
+}
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * main
+ ****************************************************************************/
+
+int main(int argc, char **argv, char **envp)
+{
+  ssize_t nbytes;
+  int fd;
+
+  /* Handle input parameters */
+
+  if (argc > 1)
+    {
+      if (argc > 2)
+        {
+          fprintf(stderr, "Too many arguments on command line\n");
+          show_usage(argv[0], 1);
+        }
+      g_ttydev = argv[1];
+    }
+
+  /* Open the USB serial device for blocking read/write */
+
+  do
+    {
+      printf("main: Opening USB serial driver\n");
+      fd = open(g_ttydev, O_RDWR);
+      if (fd < 0)
+        {
+          printf("main: ERROR: Failed to open %s: %s\n", g_ttydev, strerror(errno));
+          printf("main:        Assume not connected. Wait and try again.\n");
+          printf("main:        (Control-C to terminate).\n");
+          sleep(5);
+        }
+    }
+  while (fd < 0);
+  printf("main: Successfully opened the serial driver\n");
+
+  /* Wait for hello... */
+
+  for (;;)
+    {
+      printf("main: Reading from the serial driver\n");
+      printf("main: ... (Control-C to terminate) ...\n");
+      nbytes = read(fd, g_buffer, BUFFER_SIZE-1);
+      if (nbytes < 0)
+        {
+          printf("main: ERROR: Failed to read from %s: %s\n", g_ttydev, strerror(errno));
+          close(fd);
+          return 2;
+        }
+
+      g_buffer[nbytes] = '\0';
+      printf("main: Received %d bytes:\n", nbytes);
+      printf("      \"%s\"\n", g_buffer);
+      printf("main: Sending %d bytes..\n", sizeof(g_outmsg));
+      nbytes = write(fd, g_outmsg, sizeof(g_outmsg));
+      if (nbytes < 0)
+        {
+          printf("main: ERROR: Failed to write to %s: %s\n", g_ttydev, strerror(errno));
+          close(fd);
+          return 2;
+        }
+      printf("main: %d bytes sent\n", nbytes);
+    }
+
+  /* Won't get here, but if we did this what we would have to do */
+
+  close(fd);
+  return 0;
+}
+
--- examples/usbserial/main.c
+++ examples/usbserial/main.c
@@ -39,8 +39,11 @@
 
 #include <nuttx/config.h>
 
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <errno.h>
 #include <debug.h>
 
@@ -64,9 +67,40 @@
 #  endif
 #endif
 
+#define IOBUFFER_SIZE 256
+
 /****************************************************************************
  * Private Data
  ****************************************************************************/
+
+static const char g_shortmsg[] = "Hello, World!!\n";
+static const char g_longmsg[] =
+  "The Spanish Armada a Speech by Queen Elizabeth I of England\n"
+  "Addressed to the English army at Tilbury Fort - 1588\n"
+  "My loving people, we have been persuaded by some, that are careful of our "
+  "safety, to take heed how we commit ourselves to armed multitudes, for fear "
+  "of treachery; but I assure you, I do not desire to live to distrust my "
+  "faithful and loving people.\n"
+  "Let tyrants fear; I have always so behaved myself that, under God, I have "
+  "placed my chiefest strength and safeguard in the loyal hearts and good will "
+  "of my subjects. And therefore I am come amongst you at this time, not as for "
+  "my recreation or sport, but being resolved, in the midst and heat of the "
+  "battle, to live or die amongst you all; to lay down, for my God, and for "
+  "my kingdom, and for my people, my honour and my blood, even the dust.\n"
+  "I know I have but the body of a weak and feeble woman; but I have the heart "
+  "of a king, and of a king of England, too; and think foul scorn that Parma "
+  "or Spain, or any prince of Europe, should dare to invade the borders of my "
+  "realms: to which, rather than any dishonour should grow by me, I myself will "
+  "take up arms; I myself will be your general, judge, and rewarder of every "
+  "one of your virtues in the field.\n"
+  "I know already, by your forwardness, that you have deserved rewards and "
+  "crowns; and we do assure you, on the word of a prince, they shall be duly "
+  "paid you. In the mean my lieutenant general shall be in my stead, than whom "
+  "never prince commanded a more noble and worthy subject; not doubting by "
+  "your obedience to my general, by your concord in the camp, and by your "
+  "valour in the field, we shall shortly have a famous victory over the enemies "
+  "of my God, of my kingdom, and of my people.\n";
+static char g_iobuffer[IOBUFFER_SIZE];
 
 /****************************************************************************
  * Public Functions
@@ -91,8 +125,12 @@ void user_initialize(void)
 
 int user_start(int argc, char *argv[])
 {
-  FILE *stream;
+  int infd;
+  int outfd;
+  int count;
+  ssize_t nbytes;
   int ret;
+  int i;
 
   /* Initialize the USB serial driver */
 
@@ -105,27 +143,24 @@ int user_start(int argc, char *argv[])
     }
   message("user_start: Successfully registered the serial driver\n");
 
-  /* Open the USB serial device for output */
+  /* Open the USB serial device for writing (blocking) */
 
   do
     {
       message("user_start: Opening USB serial driver\n");
-      stream = fopen("/dev/ttyUSB0", "w");
-      if (!stream)
+      outfd = open("/dev/ttyUSB0", O_WRONLY);
+      if (outfd < 0)
         {
           int errcode = errno;
-          message("user_start: ERROR: Failed to open /dev/ttyUSB0: %d\n", errcode);
+          message("user_start: ERROR: Failed to open /dev/ttyUSB0 for writing: %d\n", errcode);
 
           /* ENOTCONN means that the USB device is not yet connected */
 
-#if 0 /* BUG: May report the wrong error */
           if (errcode == ENOTCONN)
-#endif
             {
               message("user_start:        Not connected. Wait and try again.\n");
               sleep(5);
             }
-#if 0 /* BUG: May report the wrong error */
           else
             {
               /* Give up on other errors */
@@ -133,40 +168,82 @@ int user_start(int argc, char *argv[])
               message("user_start:        Aborting\n");
               return 2;
             }
-#endif
         }
     }
-  while (!stream);
+  while (outfd < 0);
+
+  /* Open the USB serial device for reading (non-blocking) */
+
+  infd = open("/dev/ttyUSB0", O_RDONLY|O_NONBLOCK);
+  if (infd < 0)
+    {
+      message("user_start: ERROR: Failed to open /dev/ttyUSB0 for reading: %d\n", errno);
+      close(outfd);
+      return 3;
+    }
+
   message("user_start: Successfully opened the serial driver\n");
 
   /* Then say hello -- forever */
 
+  count = 0;
   for (;;)
     {
-      message("user_start: Saying hello\n");
-      ret = fprintf(stream, "Hello, World!!\n");
-      if (ret < 0)
+      if (count < 8)
         {
-          message("user_start: ERROR: fprintf failed: %d\n", errno);
-          fclose(stream);
-          return 3;
+          message("user_start: Saying hello\n");
+          nbytes = write(outfd, g_shortmsg, sizeof(g_shortmsg));
+          count++;
+        }
+      else
+        {
+          message("user_start: Reciting QEI's speech of 1588\n");
+          nbytes = write(outfd, g_longmsg, sizeof(g_longmsg));
+          count = 0;
         }
 
-      ret = fflush(stream);
-      if (ret < 0)
+      if (nbytes < 0)
         {
-          message("user_start: ERROR: fflush failed: %d\n", errno);
-          fclose(stream);
+          message("user_start: ERROR: write failed: %d\n", errno);
+          close(infd);
+          close(outfd);
           return 4;
         }
 
+      /* Poll for incoming messages */
+
       message("user_start: Waiting\n");
-      sleep(5);
+      for (i = 0; i < 5; i++)
+        {
+          nbytes = read(infd, g_iobuffer, IOBUFFER_SIZE);
+          if (nbytes < 0)
+            {
+              int errorcode = errno;
+              if (errorcode != EAGAIN)
+                {
+                  message("user_start: ERROR: read failed: %d\n", errno);
+                  close(infd);
+                  close(outfd);
+                  return 6;
+                }
+            }
+          else
+            {
+              message("user_start: Received %d bytes:\n", nbytes);
+              if (nbytes > 0)
+                {
+                  message("user_start: ");
+                  (void)fwrite(g_iobuffer, 1, nbytes, stdout);
+                }
+            }
+          sleep(1);
+        }
     }
 
   /* Won't get here, but if we did this what we would have to do */
 
-  fclose(stream);
+  close(infd);
+  close(outfd);
   return 0;
 }
 
