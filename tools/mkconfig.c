9247dc69ac7beb944313816e5126f82e502eb314
patacongo <patacongo@42af7a65-404d-4744-a932-0658087f49c3>
2008-10-23 20:53:39 +0000

    Need write-able block drivers even if no write-able FS
    git-svn-id: svn://svn.code.sf.net/p/nuttx/code/trunk@1070 42af7a65-404d-4744-a932-0658087f49c3

--- tools/mkconfig.c
+++ tools/mkconfig.c
@@ -263,13 +263,13 @@ int main(int argc, char **argv, char **envp)
   printf("# undef CONFIG_FS_FAT\n");
   printf("# undef CONFIG_FS_ROMFS\n");
   printf("#endif\n\n");
-  printf("/* Check if any readable and writable filesystem is supported */\n\n");
+  printf("/* Check if any readable and writable filesystem (OR USB storage) is supported */\n\n");
   printf("#undef CONFIG_FS_READABLE\n");
   printf("#undef CONFIG_FS_WRITABLE\n");
-  printf("#if defined(CONFIG_FS_FAT) || defined(CONFIG_FS_ROMFS)\n");
+  printf("#if defined(CONFIG_FS_FAT) || defined(CONFIG_FS_ROMFS) || defined(CONFIG_USBSTRG)\n");
   printf("# define CONFIG_FS_READABLE 1\n");
   printf("#endif\n\n");
-  printf("#if defined(CONFIG_FS_FAT)\n");
+  printf("#if defined(CONFIG_FS_FAT) || defined(CONFIG_USBSTRG)\n");
   printf("# define CONFIG_FS_WRITABLE 1\n");
   printf("#endif\n\n");
   printf("/* There can be no network support with no socket descriptors */\n\n");
