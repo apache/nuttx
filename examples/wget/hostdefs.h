e3f089a210124e1d02bcc208db51e1edcc1fda8e
patacongo <patacongo@42af7a65-404d-4744-a932-0658087f49c3>
2009-03-26 02:34:25 +0000

    wget test
    git-svn-id: svn://svn.code.sf.net/p/nuttx/code/trunk@1643 42af7a65-404d-4744-a932-0658087f49c3

---
+++ examples/wget/hostdefs.h
@@ -0,0 +1,24 @@
+#ifndef __HOSTDEFS_H
+#define __HOSTDEFS_H
+
+# include <stdio.h>
+
+typedef unsigned char ubyte;
+typedef unsigned short uint16;
+typedef unsigned int  uint32;
+typedef unsigned char boolean;
+
+# define HTONS(a) htons(a)
+# define HTONL(a) htonl(a)
+# define CONFIG_CPP_HAVE_WARNING 1
+# define FAR
+
+# define ndbg(...) printf(__VA_ARGS__)
+# define nvdbg(...) printf(__VA_ARGS__)
+
+# define TRUE  (1)
+# define FALSE (0)
+# define ERROR (-1)
+# define OK    (0)
+
+#endif /* __HOSTDEFS_H */
