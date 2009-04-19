90864d83831ca7a0af1e57469571fe81304e1cef
patacongo <patacongo@42af7a65-404d-4744-a932-0658087f49c3>
2009-04-19 15:23:14 +0000

    Use cstdio vs stdio.h
    git-svn-id: svn://svn.code.sf.net/p/nuttx/code/trunk@1710 42af7a65-404d-4744-a932-0658087f49c3

--- examples/helloxx/main.cxx
+++ examples/helloxx/main.cxx
@@ -39,7 +39,7 @@
 
 #include <nuttx/config.h>
 #include <nuttx/init.h>
-#include <stdio.h>
+#include <cstdio>
 #include <debug.h>
 
 //***************************************************************************
