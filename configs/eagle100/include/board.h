65f28e48e8e3131c4e3b5692220c5add590e323e
patacongo <patacongo@42af7a65-404d-4744-a932-0658087f49c3>
2009-05-06 23:09:15 +0000

    Add LM3S6918 support
    git-svn-id: svn://svn.code.sf.net/p/nuttx/code/trunk@1756 42af7a65-404d-4744-a932-0658087f49c3

--- configs/eagle100/include/board.h
+++ configs/eagle100/include/board.h
@@ -1,5 +1,5 @@
 /************************************************************************************
- * configs/mx1ads/include/board.h
+ * configs/eagle100/include/board.h
  * include/arch/board/board.h
  *
  *   Copyright (C) 2009 Gregory Nutt. All rights reserved.
@@ -49,104 +49,25 @@
  * Definitions
  ************************************************************************************/
 
-/* Clock settings -- All clock values are precalculated */
+/* Clocking *************************************************************************/
 
-#define IMX_SYS_CLK_FREQ    16780000  /* Crystal frequency */
-
-/* MPCTL0 -- Controls the MCU clock:
- *
- *                                                MFI + MFN / (MFD+1)
- *  IMX_MCUPLL_CLK_FREQ = 2 * IMX_SYS_CLK_FREQ * --------------------
- *                                                    PD + 1
- */
-
-#if 0 /* 150 MHz */
-#  define IMX_MPCTL0_MFN   16
-#  define IMX_MPCTL0_MFI   9
-#  define IMX_MPCTL0_MFD   99
-#  define IMX_MPCTL0_PD    1
-#else /* 180 MHz */
-#  define IMX_MPCTL0_MFN   441
-#  define IMX_MPCTL0_MFI   4
-#  define IMX_MPCTL0_MFD   938
-#  define IMX_MPCTL0_PD    0
-#endif
-
-#define IMX_MPCTL0_VALUE \
-  ((IMX_MPCTL0_MFN << PLL_MPCTL0_MFN_SHIFT) |\
-   (IMX_MPCTL0_MFI << PLL_MPCTL0_MFI_SHIFT) |\
-   (IMX_MPCTL0_MFD << PLL_MPCTL0_MFD_SHIFT) |\
-   (IMX_MPCTL0_PD << PLL_MPCTL0_PD_SHIFT))
-
-/* This yields: */
-
-#if 0 /* 150 MHz */
-#  define IMX_MCUPLL_CLK_FREQ 153704800
-#else /* 180 MHz */
-#  define IMX_MCUPLL_CLK_FREQ 183561405
-#endif
-
-/* SPCTL0 -- Controls the system PLL:
- *
- *                                                MFI + MFN / (MFD+1)
- *  IMX_SYSPLL_CLK_FREQ = 2 * IMX_SYS_CLK_FREQ * --------------------
- *                                                    PD + 1
- */
-
-#define IMX_SPCTL0_MFN   678
-#define IMX_SPCTL0_MFI   5
-#define IMX_SPCTL0_MFD   938
-#define IMX_SPCTL0_PD    1
-
-#define IMX_SPCTL0_VALUE \
-  ((IMX_SPCTL0_MFN << PLL_SPCTL0_MFN_SHIFT) |\
-   (IMX_SPCTL0_MFI << PLL_SPCTL0_MFI_SHIFT) |\
-   (IMX_SPCTL0_MFD << PLL_SPCTL0_MFD_SHIFT) |\
-   (IMX_SPCTL0_PD << PLL_SPCTL0_PD_SHIFT))
-
-/* This yields: */
-
-#define IMX_SYSPLL_CLK_FREQ 96015910
-
-/* PDCR -- Controls peripheral clocks */
-
-#define IMX_PCLKDIV1 0
-#define IMX_PCLKDIV2 0
-#define IMX_PCLKDIV3 0
-
-#define IMX_PCDR_VALUE \
-  ((IMX_PCLKDIV1 << PLL_PCDR_PCLKDIV1_SHIFT) |\
-   (IMX_PCLKDIV2 << PLL_PCDR_PCLKDIV2_SHIFT) |\
-   (IMX_PCLKDIV3 << PLL_PCDR_PCLKDIV3_SHIFT))
-
-/* PERCLK1: UART, Timers, PWM */
-
-#define IMX_PERCLK1_FREQ  (IMX_SYSPLL_CLK_FREQ/(IMX_PCLKDIV1+1))
-
-/* PERCLK2: CSPI, LCD, SD */
-
-#define IMX_PERCLK2_FREQ  (IMX_SYSPLL_CLK_FREQ/(IMX_PCLKDIV2+1))
-
-/* PERCLK3: SSI */
-
-#define IMX_PERCLK3_FREQ  (IMX_SYSPLL_CLK_FREQ/(IMX_PCLKDIV3+1))
-
-/* CSCR settings -- Controls HCLK and BCLK and USB clock.
- * HCLK: SDRAM, CSI, Memory Stick, I2C, DMA
- */
-
-#define IMX_CSCR_BCLKDIV 1
-#define IMX_CSCR_USBDIV  6
+#define LM3X_ARM_CLK_FREQ    50000000  /* 50MHz */
 
 /* LED definitions ******************************************************************/
 
-/* The MX1ADS has only one usable LED: Port A, bit 2 */
-
+/* The Eagle-100 has only one user LED: Port A, Bit 6.  Below are the mapping of this
+ * single LED.  From this single LED, we can get the following information:
+ *
+ *   OFF Steady:    The system has failed to boot to the point of enabling interrupts
+ *   ON Steady:     The systems has enabled interrupts, but none have been received
+ *   Dull glow:     The system is taking interrupts
+ *   Slow blinking: The system has panicked
+ */
                                 /* ON   OFF */
 #define LED_STARTED       0     /* OFF  OFF */
 #define LED_HEAPALLOCATE  1     /* OFF  OFF */
-#define LED_IRQSENABLED   2     /* OFF  OFF */
-#define LED_STACKCREATED  3     /* OFF  OFF */
+#define LED_IRQSENABLED   2     /* ON   ON */
+#define LED_STACKCREATED  3     /* ON   ON */
 #define LED_INIRQ         4     /* ON   OFF */
 #define LED_SIGNAL        5     /* ON   OFF */
 #define LED_ASSERTION     6     /* ON   OFF */
@@ -158,12 +79,12 @@
 
 #ifndef __ASSEMBLY__
 
-/* All i.MX architectures must provide the following entry point.  This entry point
+/* All LM3S architectures must provide the following entry point.  This entry point
  * is called early in the intitialization -- after all memory has been configured
  * and mapped but before any devices have been initialized.
  */
 
-extern void imx_boardinitialize(void);
+extern void lm3s_boardinitialize(void);
 
 #endif
 
