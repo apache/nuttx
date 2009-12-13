2880e1d5c2d6e5b8bbdda595f1a93f66aa0a38f8
patacongo <patacongo@42af7a65-404d-4744-a932-0658087f49c3>
2009-12-13 19:58:21 +0000

    Add stdbool.h
    git-svn-id: svn://svn.code.sf.net/p/nuttx/code/trunk@2331 42af7a65-404d-4744-a932-0658087f49c3

---
+++ include/stdbool.h
@@ -0,0 +1,71 @@
+/****************************************************************************
+ * include/stdbool.h
+ *
+ *   Copyright (C) 2009 Gregory Nutt. All rights reserved.
+ *   Author: Gregory Nutt <spudmonkey@racsa.co.cr>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name NuttX nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+#ifndef __INCLUDE_STDBOOL_H
+#define __INCLUDE_STDBOOL_H
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+#include <nuttx/compiler.h>
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+/* bool, true, and false must be provided as macros so that they can be
+ * redefined by the application if necessary.
+ */
+
+#define bool  _Bool
+#define true  1
+#define false 0
+
+#define __bool_true_false_are_defined 1
+
+/****************************************************************************
+ * Public Types
+ ****************************************************************************/
+
+/* A byte is the smallest address memory element (at least in architectures
+ * that do not support bit banding.  We select ubyte versus the more standard
+ * int as the underlying type to minimize the RAM footprint of the executable.
+ */
+
+typedef ubyte _Bool;
+
+#endif /* __INCLUDE_STDBOOL_H */
