fdbb185fd505902670471753ac1346d4fcff29ff
patacongo <patacongo@42af7a65-404d-4744-a932-0658087f49c3>
2010-06-15 14:46:53 +0000

    Add GPDMA skeleton file
    git-svn-id: svn://svn.code.sf.net/p/nuttx/code/trunk@2750 42af7a65-404d-4744-a932-0658087f49c3

--- arch/arm/src/lpc17xx/lpc17_internal.h
+++ arch/arm/src/lpc17xx/lpc17_internal.h
@@ -346,6 +346,53 @@
  * Public Types
  ************************************************************************************/
 
+typedef FAR void *DMA_HANDLE;
+typedef void (*dma_callback_t)(DMA_HANDLE handle, void *arg, int result);
+
+/* The following is used for sampling DMA registers when CONFIG DEBUG_DMA is selected */
+
+#ifdef CONFIG_DEBUG_DMA
+struct lpc17_dmaglobalregs_s
+{
+  /* Global Registers */
+
+  uint32_t intst;       /* DMA Interrupt Status Register */
+  uint32_t inttcst;     /* DMA Interrupt Terminal Count Request Status Register */
+  uint32_t interrst;    /* DMA Interrupt Error Status Register */
+  uint32_t rawinttcst;  /* DMA Raw Interrupt Terminal Count Status Register */
+  uint32_t rawinterrst; /* DMA Raw Error Interrupt Status Register */
+  uint32_t enbldchns;   /* DMA Enabled Channel Register */
+  uint32_t softbreq;    /* DMA Software Burst Request Register */
+  uint32_t softsreq;    /* DMA Software Single Request Register */
+  uint32_t softlbreq;   /* DMA Software Last Burst Request Register */
+  uint32_t softlsreq;   /* DMA Software Last Single Request Register */
+  uint32_t config;     /* DMA Configuration Register */
+  uint32_t sync;      /* DMA Synchronization Register */
+};
+
+struct lpc17_dmachanregs_s
+{
+  /* Channel Registers */
+
+  uint32_t srcaddr;  /* DMA Channel Source Address Register */
+  uint32_t destaddr; /* DMA Channel Destination Address Register */
+  uint32_t lli;      /* DMA Channel Linked List Item Register */
+  uint32_t control;  /* DMA Channel Control Register */
+  uint32_t config;   /* DMA Channel Configuration Register */
+};
+
+struct lpc17_dmaregs_s
+{
+  /* Global Registers */
+
+  struct lpc17_dmaglobalregs_s gbl;
+
+  /* Channel Registers */
+
+  struct lpc17_dmachanregs_s   ch;
+};
+#endif
+
 /************************************************************************************
  * Inline Functions
  ************************************************************************************/
@@ -558,6 +605,130 @@ EXTERN void spi_flush(FAR struct spi_dev_s *dev);
 #endif
 #if defined(CONFIG_LPC17_SSP0) || defined(CONFIG_LPC17_SSP1)
 EXTERN void ssp_flush(FAR struct spi_dev_s *dev);
+#endif
+
+/****************************************************************************
+ * Name: lpc17_dmainitialize
+ *
+ * Description:
+ *   Initialize the GPDMA subsystem.
+ *
+ * Returned Value:
+ *   None
+ *
+ ****************************************************************************/
+
+#ifdef CONFIG_LPC17_GPDMA
+EXTERN void lpc17_dmainitilaize(void);
+#endif
+
+/****************************************************************************
+ * Name: lpc17_dmachannel
+ *
+ * Description:
+ *   Allocate a DMA channel.  This function sets aside a DMA channel and
+ *   gives the caller exclusive access to the DMA channel.
+ *
+ * Returned Value:
+ *   One success, this function returns a non-NULL, void* DMA channel
+ *   handle.  NULL is returned on any failure.  This function can fail only
+ *   if no DMA channel is available.
+ *
+ ****************************************************************************/
+
+#ifdef CONFIG_LPC17_GPDMA
+EXTERN DMA_HANDLE lpc17_dmachannel(void);
+#endif
+
+/****************************************************************************
+ * Name: lpc17_dmafree
+ *
+ * Description:
+ *   Release a DMA channel.  NOTE:  The 'handle' used in this argument must
+ *   NEVER be used again until lpc17_dmachannel() is called again to re-gain
+ *   a valid handle.
+ *
+ * Returned Value:
+ *   None
+ *
+ ****************************************************************************/
+
+#ifdef CONFIG_LPC17_GPDMA
+EXTERN void lpc17_dmafree(DMA_HANDLE handle);
+#endif
+
+/****************************************************************************
+ * Name: lpc17_dmasetup
+ *
+ * Description:
+ *   Configure DMA for one transfer.
+ *
+ ****************************************************************************/
+
+#ifdef CONFIG_LPC17_GPDMA
+EXTERN int lpc17_dmarxsetup(DMA_HANDLE handle,
+                            uint32_t control, uint32_t config,
+                            uint32_t srcaddr, uint32_t destaddr,
+                            size_t nbytes);
+#endif
+
+/****************************************************************************
+ * Name: lpc17_dmastart
+ *
+ * Description:
+ *   Start the DMA transfer
+ *
+ ****************************************************************************/
+
+#ifdef CONFIG_LPC17_GPDMA
+EXTERN int lpc17_dmastart(DMA_HANDLE handle, dma_callback_t callback, void *arg);
+#endif
+
+/****************************************************************************
+ * Name: lpc17_dmastop
+ *
+ * Description:
+ *   Cancel the DMA.  After lpc17_dmastop() is called, the DMA channel is
+ *   reset and lpc17_dmasetup() must be called before lpc17_dmastart() can be
+ *   called again
+ *
+ ****************************************************************************/
+
+#ifdef CONFIG_LPC17_GPDMA
+EXTERN void lpc17_dmastop(DMA_HANDLE handle);
+#endif
+
+/****************************************************************************
+ * Name: lpc17_dmasample
+ *
+ * Description:
+ *   Sample DMA register contents
+ *
+ ****************************************************************************/
+
+#ifdef CONFIG_LPC17_GPDMA
+#ifdef CONFIG_DEBUG_DMA
+EXTERN void lpc17_dmasample(DMA_HANDLE handle, struct lpc17_dmaregs_s *regs);
+#else
+#  define lpc17_dmasample(handle,regs)
+#endif
+#endif
+
+/****************************************************************************
+ * Name: lpc17_dmadump
+ *
+ * Description:
+ *   Dump previously sampled DMA register contents
+ *
+ ****************************************************************************/
+
+#ifdef CONFIG_LPC17_GPDMA
+#ifdef CONFIG_DEBUG_DMA
+EXTERN void lpc17_dmadump(DMA_HANDLE handle, const struct lpc17_dmaregs_s *regs,
+                          const char *msg);
+#else
+#  define lpc17_dmadump(handle,regs,msg)
+#endif
 #endif
 
 #undef EXTERN
