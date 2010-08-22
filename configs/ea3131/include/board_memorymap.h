33e0bdda8a986f24662e10cba9bf3cd640fa515f
patacongo <patacongo@42af7a65-404d-4744-a932-0658087f49c3>
2010-08-22 03:44:18 +0000

    Fix demand-paging compilation errors
    git-svn-id: svn://svn.code.sf.net/p/nuttx/code/trunk@2879 42af7a65-404d-4744-a932-0658087f49c3

--- configs/ea3131/include/board_memorymap.h
+++ configs/ea3131/include/board_memorymap.h
@@ -2,7 +2,7 @@
  * configs/ea3131/include/board_memorymap.h
  * include/arch/board/board_memorymap.h
  *
- *   Copyright (C) 2009 Gregory Nutt. All rights reserved.
+ *   Copyright (C) 2009-2010 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <spudmonkey@racsa.co.cr>
  *
  * Redistribution and use in source and binary forms, with or without
