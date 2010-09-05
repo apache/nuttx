c53d9511836d1cfb9bc42c7a2d64b3c1c07dcef0
patacongo <patacongo@42af7a65-404d-4744-a932-0658087f49c3>
2010-09-05 19:13:48 +0000

    paging debug fixes
    git-svn-id: svn://svn.code.sf.net/p/nuttx/code/trunk@2922 42af7a65-404d-4744-a932-0658087f49c3

--- sched/task_start.c
+++ sched/task_start.c
@@ -1,7 +1,7 @@
 /****************************************************************************
  * sched/task_start.c
  *
- *   Copyright (C) 2007-2009 Gregory Nutt. All rights reserved.
+ *   Copyright (C) 2007-2010 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <spudmonkey@racsa.co.cr>
  *
  * Redistribution and use in source and binary forms, with or without
@@ -108,7 +108,8 @@ void task_start(void)
     }
 
   /* Call the 'main' entry point passing argc and argv.  If/when
-   * the task returns,  */
+   * the task returns.
+   */
 
   exit(tcb->entry.main(argc, tcb->argv));
 }
