11404da9afe85a9d9d3d87712635506742676529
patacongo <patacongo@42af7a65-404d-4744-a932-0658087f49c3>
2011-01-20 18:28:59 +0000

    A/D definitions
    git-svn-id: svn://svn.code.sf.net/p/nuttx/code/trunk@3268 42af7a65-404d-4744-a932-0658087f49c3

--- arch/hc/src/mc9s12ne64/chip.h
+++ arch/hc/src/mc9s12ne64/chip.h
@@ -1,7 +1,7 @@
 /************************************************************************************
  * arch/hc/src/mc9s12ne64/chip.h
  *
- *   Copyright (C) 2009-2010 Gregory Nutt. All rights reserved.
+ *   Copyright (C) 2009-2011 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <spudmonkey@racsa.co.cr>
  *
  * Redistribution and use in source and binary forms, with or without
