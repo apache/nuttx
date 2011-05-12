22ccf9967f405d2da42768b6500af22f4a0d9172
patacongo <patacongo@42af7a65-404d-4744-a932-0658087f49c3>
2011-05-12 17:42:01 +0000

    First set of changes to incorporate the RGMP port
    git-svn-id: svn://svn.code.sf.net/p/nuttx/code/trunk@3595 42af7a65-404d-4744-a932-0658087f49c3

---
+++ arch/rgmp/include/arch.h
@@ -0,0 +1,72 @@
+/****************************************************************************
+ * arch/rgmp/include/arch.h
+ *
+ *   Copyright (C) 2011 Yu Qiang. All rights reserved.
+ *   Author: Yu Qiang <yuq825@gmail.com>
+ *
+ * This file is a part of NuttX:
+ *
+ *   Copyright (C) 2011 Gregory Nutt. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name NuttX nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+#ifndef __RGMP_ARCH_ARCH_H
+#define __RGMP_ARCH_ARCH_H
+
+#ifndef __ASSEMBLY__
+
+#include <nuttx/sched.h>
+#include <rgmp/hpet.h>
+
+struct up_wait {
+    struct up_wait *next;
+    _TCB *task;
+};
+
+extern _TCB *current_task;
+
+void up_sigentry(void);
+
+int up_register_bridge(char *name, int size);
+int up_unregister_bridge(char *name);
+
+static inline void up_mdelay(uint32_t msec)
+{
+    hpet_ndelay(msec*1000000);
+}
+
+static inline void up_udelay(uint32_t usec)
+{
+    hpet_udelay(usec*1000);
+}
+
+#endif
+
+#endif
