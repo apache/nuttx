ccaadba6645dcb2fbb6abd30b8b0ae2e466e519b
patacongo <patacongo@42af7a65-404d-4744-a932-0658087f49c3>
2011-06-16 17:52:25 +0000

    Reduce memory usage in same AVR configurations
    git-svn-id: svn://svn.code.sf.net/p/nuttx/code/trunk@3714 42af7a65-404d-4744-a932-0658087f49c3

--- tools/mkconfig.c
+++ tools/mkconfig.c
@@ -189,7 +189,6 @@ int main(int argc, char **argv, char **envp)
   printf(" */\n\n");
   printf("#if CONFIG_STDIO_BUFFER_SIZE == 0\n");
   printf("# undef CONFIG_STDIO_LINEBUFFER\n");
-  printf("# define CONFIG_STDIO_LINEBUFFER 0\n");
   printf("#endif\n\n");
   printf("/* If the maximum message size is zero, then we assume that message queues\n");
   printf(" * support should be disabled\n");
