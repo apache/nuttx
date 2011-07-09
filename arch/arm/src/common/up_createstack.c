efa3bf787d78eb7de8e88180fcf6ca1591aa0cd5
patacongo <patacongo@42af7a65-404d-4744-a932-0658087f49c3>
2011-07-09 12:53:12 +0000

    ARM stack check logic; ARM no-console build fixes; Nucleus-2G updates
    git-svn-id: svn://svn.code.sf.net/p/nuttx/code/trunk@3760 42af7a65-404d-4744-a932-0658087f49c3

--- arch/arm/src/common/up_createstack.c
+++ arch/arm/src/common/up_createstack.c
@@ -55,6 +55,16 @@
  * Private Types
  ****************************************************************************/
 
+/* On most larger then 8 bit archs this will need to be word aligned so
+ * so maybe some checks should be put in place?
+ */
+
+static void *memset32(void *s, uint32_t  c, size_t n)
+{
+  uint32_t *p = (uint32_t *)s;
+  while (n-- > 0) *p++ = c;
+  return s;
+}
 /****************************************************************************
  * Private Function Prototypes
  ****************************************************************************/
@@ -129,9 +139,19 @@ int up_create_stack(_TCB *tcb, size_t stack_size)
        tcb->adj_stack_ptr  = (uint32_t*)top_of_stack;
        tcb->adj_stack_size = size_of_stack;
 
+       /* If stack debug is enabled, then fill the stack with a
+        * recognizable value that we can use later to test for high
+        * water marks.
+        */
+
+#if defined(CONFIG_DEBUG) && defined(CONFIG_DEBUG_STACK)
+       memset32(tcb->stack_alloc_ptr, 0xDEADBEEF, tcb->adj_stack_size/4);
+#endif
+
        up_ledon(LED_STACKCREATED);
        return OK;
      }
 
    return ERROR;
 }
+
