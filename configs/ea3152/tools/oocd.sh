41079bcc3c05be52584a4854b2f39dbaa049d8f9
patacongo <patacongo@42af7a65-404d-4744-a932-0658087f49c3>
2011-10-13 18:14:11 +0000

    Try older OpenOCD
    git-svn-id: svn://svn.code.sf.net/p/nuttx/code/trunk@4044 42af7a65-404d-4744-a932-0658087f49c3

--- configs/ea3152/tools/oocd.sh
+++ configs/ea3152/tools/oocd.sh
@@ -8,6 +8,9 @@ if [ -z "${TOPDIR}" ]; then
 	exit 1
 fi
 
+###############################################################################
+# OpenOCD 0.4.0 ###############################################################
+###############################################################################
 # This script *probably* only works with the following version of OpenOCD:
 
 OPENOCD_PATH="/cygdrive/c/OpenOCD/openocd-0.4.0/src"
@@ -19,15 +22,9 @@ OPENOCD_SEARCHDIR="${TOPDIR}/configs/ea3152/tools"
 OPENOCD_WSEARCHDIR="`cygpath -w ${OPENOCD_SEARCHDIR}`"
 OPENOCD_INTERFACE="olimex-arm-usb-ocd.cfg"
 OPENOCD_TARGET="lpc3152.cfg"
-
 OPENOCD_ARGS="-s ${OPENOCD_WSEARCHDIR} -f ${OPENOCD_INTERFACE} -f ${OPENOCD_TARGET}"
 
 # Verify that everything is what it claims it is and is located where it claims it is.
-
-if [ "X$2" = "X-d" ]; then
-	OPENOCD_ARGS=$OPENOCD_ARGS" -d3"
-	set -x
-fi
 
 if [ ! -d "${OPENOCD_PATH}" ]; then
 	echo "OpenOCD path does not exist: ${OPENOCD_PATH}"
@@ -45,6 +42,48 @@ if [ ! -f "${OPENOCD_SEARCHDIR}/${OPENOCD_INTERFACE}" ]; then
 	echo "OpenOCD interface config file does not exist: ${OPENOCD_SEARCHDIR}/${OPENOCD_INTERFACE}"
 	exit 1
 fi
+###############################################################################
+
+###############################################################################
+# Older OpenOCD that Shipped with the ARM-OCD JTAG ############################
+###############################################################################
+# This script *probably* only works with the following version of OpenOCD:
+#
+#OPENOCD_PATH="/cygdrive/c/gccfd/openocd/bin"
+#OPENOCD_EXE=openocd-ftd2xx.exe
+#
+# Local search directory and configurations
+#
+#OPENOCD_CFG="${TOPDIR}/configs/ea3152/tools/armusbocd.cfg"
+#OPENOCD_ARGS="-f `cygpath -w ${OPENOCD_CFG}`"
+#
+#if [ ! -f ${OPENOCD_CFG} ]; then
+#	echo "OpenOCD config file does not exist: ${OPENOCD_CFG}"
+#	exit 1
+#fi
+#
+# Verify that everything is what it claims it is and is located where it claims it is.
+#
+#if [ ! -d "${OPENOCD_PATH}" ]; then
+#	echo "OpenOCD path does not exist: ${OPENOCD_PATH}"
+#	exit 1
+#fi
+#if [ ! -x "${OPENOCD_PATH}/${OPENOCD_EXE}" ]; then
+#	echo "OpenOCD does not exist: ${OPENOCD_PATH}/${OPENOCD_EXE}"
+#	exit 1
+#fi
+#if [ ! -f ${OPENOCD_CFG} ]; then
+#	echo "OpenOCD config file does not exist: ${OPENOCD_CFG}"
+#	exit 1
+#fi
+###############################################################################
+
+# Enable debug if so requested
+
+if [ "X$2" = "X-d" ]; then
+	OPENOCD_ARGS=$OPENOCD_ARGS" -d3"
+	set -x
+fi
 
 # Okay... do it!
 
@@ -53,7 +92,3 @@ ${OPENOCD_PATH}/${OPENOCD_EXE} ${OPENOCD_ARGS} &
 echo "OpenOCD daemon started"
 ps -ef | grep openocd
 echo "In GDB: target remote localhost:3333"
-
-
-
-
