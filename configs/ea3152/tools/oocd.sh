7067a35f4fced9c4e9cd663697190697a4500cfe
patacongo <patacongo@42af7a65-404d-4744-a932-0658087f49c3>
2011-10-13 23:38:08 +0000

    Try J-link OpenOCD support
    git-svn-id: svn://svn.code.sf.net/p/nuttx/code/trunk@4046 42af7a65-404d-4744-a932-0658087f49c3

--- configs/ea3152/tools/oocd.sh
+++ configs/ea3152/tools/oocd.sh
@@ -1,10 +1,64 @@
 #!/bin/sh
 
+# Get command line parameters
+
+USAGE="USAGE: $0 [-dhjo14] <TOPDIR>"
+ADVICE="Try '$0 -h' for more information"
+
+INTERFACE=Olimex
+OPENOCD=0.1.0
+
+while [ ! -z "$1" ]; do
+	case $1 in
+	-d )
+		set -x
+		;;
+	-j )
+		INTERFACE=Jlink
+		;;
+	-o )
+		INTERFACE=Olimex
+		;;
+	-1 )
+		OPENOCD=0.1.0
+		;;
+	-4 )
+		OPENOCD=0.4.0
+		;;
+	-h )
+		echo "$0 is a tool for generation of proper version files for the NuttX build"
+		echo ""
+		echo $USAGE
+		echo ""
+		echo "Where:"
+		echo "  -d"
+		echo "	  Enable script debug"
+		echo "  -h"
+		echo "	  show this help message and exit"
+		echo "  -j"
+		echo "	  Use the Segger J-link interface"
+		echo "  -o"
+		echo "	  Use the Olimex ARM USB OCD interface (Default)"
+		echo "  -1"
+		echo "	  Use the Olimex GCCFD OpenOCD 0.1.0 (Default)"
+		echo "  -4"
+		echo "	  Use the OpenOCD 0.4.0"
+		echo "  <TOPDIR>"
+		echo "	  The full path to the top-level NuttX directory"
+		exit 0
+		;;
+	* )
+		break;
+		;;
+	esac
+	shift
+done
+
 TOPDIR=$1
-USAGE="$0 <TOPDIR> [-d]"
 if [ -z "${TOPDIR}" ]; then
 	echo "Missing argument"
 	echo $USAGE
+	echo $ADVICE
 	exit 1
 fi
 
@@ -13,31 +67,42 @@ fi
 ###############################################################################
 # This script *probably* only works with the following versions of OpenOCD:
 
-OPENOCD_PATH="/cygdrive/c/OpenOCD/openocd-0.4.0/src"
-OPENOCD_EXE=openocd.exe
+if [ "X${OPENOCD}" = "X0.4.0" ]; then
 
-echo "Trying OpenOCD 0.4.0 path: ${OPENOCD_PATH}/${OPENOCD_EXE}"
+	# Local search directory and configurations
 
-if [ -x "${OPENOCD_PATH}/${OPENOCD_EXE}" ]; then
+	OPENOCD_SEARCHDIR="${TOPDIR}/configs/ea3152/tools"
+	OPENOCD_WSEARCHDIR="`cygpath -w ${OPENOCD_SEARCHDIR}`"
 
-  # Local search directory and configurations
+	if [ "X${INTERFACE}" = "XJlink" ]; then
+		OPENOCD_PATH="/cygdrive/c/Program Files (x86)/OpenOCD/0.4.0/bin"
+		OPENOCD_EXE=openocd.exe
+		OPENOCD_INTERFACE="jlink.cfg"
+	else
+		OPENOCD_PATH="/cygdrive/c/OpenOCD/openocd-0.4.0/src"
+		OPENOCD_EXE=openocd.exe
+		OPENOCD_INTERFACE="olimex-arm-usb-ocd.cfg"
+	fi
 
-  OPENOCD_SEARCHDIR="${TOPDIR}/configs/ea3152/tools"
-  OPENOCD_WSEARCHDIR="`cygpath -w ${OPENOCD_SEARCHDIR}`"
-  OPENOCD_INTERFACE="olimex-arm-usb-ocd.cfg"
-  OPENOCD_TARGET="lpc3152.cfg"
-  OPENOCD_ARGS="-s ${OPENOCD_WSEARCHDIR} -f ${OPENOCD_INTERFACE} -f ${OPENOCD_TARGET}"
+	OPENOCD_TARGET="lpc3152.cfg"
+	OPENOCD_ARGS="-s ${OPENOCD_WSEARCHDIR} -f ${OPENOCD_INTERFACE} -f ${OPENOCD_TARGET}"
 
-  # Verify that everything is what it claims it is and is located where it claims it is.
+	echo "Trying OpenOCD 0.4.0 path: ${OPENOCD_PATH}/${OPENOCD_EXE}"
 
-  if [ ! -f "${OPENOCD_SEARCHDIR}/${OPENOCD_TARGET}" ]; then
-	echo "OpenOCD target config file does not exist: ${OPENOCD_SEARCHDIR}/${OPENOCD_TARGET}"
-	exit 1
-  fi
-  if [ ! -f "${OPENOCD_SEARCHDIR}/${OPENOCD_INTERFACE}" ]; then
-	echo "OpenOCD interface config file does not exist: ${OPENOCD_SEARCHDIR}/${OPENOCD_INTERFACE}"
-	exit 1
-  fi
+	# Verify that everything is what it claims it is and is located where it claims it is.
+
+	if [ ! -x "${OPENOCD_PATH}/${OPENOCD_EXE}" ]; then
+		echo "OpenOCD executable does not exist: ${OPENOCD_PATH}/${OPENOCD_EXE}"
+		exit 1
+	fi
+	if [ ! -f "${OPENOCD_SEARCHDIR}/${OPENOCD_TARGET}" ]; then
+		echo "OpenOCD target config file does not exist: ${OPENOCD_SEARCHDIR}/${OPENOCD_TARGET}"
+		exit 1
+	fi
+	if [ ! -f "${OPENOCD_SEARCHDIR}/${OPENOCD_INTERFACE}" ]; then
+		echo "OpenOCD interface config file does not exist: ${OPENOCD_SEARCHDIR}/${OPENOCD_INTERFACE}"
+		exit 1
+	fi
 
 ###############################################################################
 
@@ -46,39 +111,47 @@ if [ -x "${OPENOCD_PATH}/${OPENOCD_EXE}" ]; then
 ###############################################################################
 
 else
-  echo "OpenOCD 0.4.0 path does not exist: ${OPENOCD_PATH}/${OPENOCD_EXE}"
+	if [ "X${OPENOCD}" = "X0.1.0" ]; then
 
-  OPENOCD_PATH="/cygdrive/c/gccfd/openocd/bin"
-  OPENOCD_EXE=openocd-ftd2xx.exe
+		OPENOCD_PATH="/cygdrive/c/gccfd/openocd/bin"
+		OPENOCD_EXE=openocd-ftd2xx.exe
 
-  echo "Trying GCCFD OpenOCD 0.1.0 path: ${OPENOCD_PATH}/${OPENOCD_EXE}"
+		echo "Trying GCCFD OpenOCD 0.1.0 path: ${OPENOCD_PATH}/${OPENOCD_EXE}"
 
-  # Local search directory and configurations
+		# Local search directory and configurations
 
-  OPENOCD_CFG="${TOPDIR}/configs/ea3152/tools/armusbocd.cfg"
-  OPENOCD_ARGS="-f `cygpath -w ${OPENOCD_CFG}`"
+		if [ "X${INTERFACE}" = "XJlink" ]; then
+			echo "The Olimex OpenOCD doesn't support J-Link"
+			exit 1
+		fi
 
-  if [ ! -f ${OPENOCD_CFG} ]; then
-	echo "OpenOCD config file does not exist: ${OPENOCD_CFG}"
-	exit 1
-  fi
+		OPENOCD_CFG="${TOPDIR}/configs/ea3152/tools/armusbocd.cfg"
+		OPENOCD_ARGS="-f `cygpath -w ${OPENOCD_CFG}`"
 
-  # Verify that everything is what it claims it is and is located where it claims it is.
+		# Verify that everything is what it claims it is and is located where it claims it is.
 
-  if [ ! -d "${OPENOCD_PATH}" ]; then
-	echo "OpenOCD path does not exist: ${OPENOCD_PATH}"
-	exit 1
-  fi
-  if [ ! -x "${OPENOCD_PATH}/${OPENOCD_EXE}" ]; then
-	echo "OpenOCD does not exist: ${OPENOCD_PATH}/${OPENOCD_EXE}"
-	exit 1
-  fi
-  if [ ! -f ${OPENOCD_CFG} ]; then
-	echo "OpenOCD config file does not exist: ${OPENOCD_CFG}"
-	exit 1
-  fi
+		if [ ! -f ${OPENOCD_CFG} ]; then
+			echo "OpenOCD config file does not exist: ${OPENOCD_CFG}"
+			exit 1
+		fi
+		if [ ! -x "${OPENOCD_PATH}/${OPENOCD_EXE}" ]; then
+			echo "OpenOCD executable does not exist: ${OPENOCD_PATH}/${OPENOCD_EXE}"
+			exit 1
+		fi
+		if [ ! -d "${OPENOCD_PATH}" ]; then
+			echo "OpenOCD path does not exist: ${OPENOCD_PATH}"
+			exit 1
+		fi
+		if [ ! -f ${OPENOCD_CFG} ]; then
+			echo "OpenOCD config file does not exist: ${OPENOCD_CFG}"
+			exit 1
+		fi
+	else
+		echo "Unsupported OpenOCD version"
+		echo $ADVICE
+		exit 1
+	fi
 fi
-###############################################################################
 
 # Enable debug if so requested
 
@@ -90,7 +163,7 @@ fi
 # Okay... do it!
 
 echo "Starting OpenOCD"
-${OPENOCD_PATH}/${OPENOCD_EXE} ${OPENOCD_ARGS} &
+"${OPENOCD_PATH}/${OPENOCD_EXE}" ${OPENOCD_ARGS} &
 echo "OpenOCD daemon started"
 ps -ef | grep openocd
 echo "In GDB: target remote localhost:3333"
