07a2d59fbc4b6fd310d9eb8f7084354fbefc7f40
patacongo <patacongo@42af7a65-404d-4744-a932-0658087f49c3>
2011-12-22 15:59:50 +0000

    Add loopback support to STM32 CAN driver; Add apps/examples/can loopback test
    git-svn-id: svn://svn.code.sf.net/p/nuttx/code/trunk@4213 42af7a65-404d-4744-a932-0658087f49c3

--- arch/arm/src/stm32/stm32_can.h
+++ arch/arm/src/stm32/stm32_can.h
@@ -50,17 +50,73 @@
 /************************************************************************************
  * Pre-processor Definitions
  ************************************************************************************/
+/* Configuration ********************************************************************/
+/* Up to 2 CAN interfaces are supported */
+
+#if STM32_NCAN < 2
+#  undef CONFIG_STM32_CAN2
+#endif
+
+#if STM32_NCAN < 1
+#  undef CONFIG_STM32_CAN1
+#endif
+
+#if defined(CONFIG_CAN) && (defined(CONFIG_STM32_CAN1) || defined(CONFIG_STM32_CAN2))
+
+/* CAN BAUD */
+
+#if defined(CONFIG_STM32_CAN1) && !defined(CONFIG_CAN1_BAUD)
+#  error "CONFIG_CAN1_BAUD is not defined"
+#endif
+
+#if defined(CONFIG_STM32_CAN2) && !defined(CONFIG_CAN2_BAUD)
+#  error "CONFIG_CAN2_BAUD is not defined"
+#endif
 
 /************************************************************************************
  * Public Types
  ************************************************************************************/
 
+#ifndef __ASSEMBLY__
+
 /************************************************************************************
  * Public Data
  ************************************************************************************/
+
+#undef EXTERN
+#if defined(__cplusplus)
+#define EXTERN extern "C"
+extern "C" {
+#else
+#define EXTERN extern
+#endif
 
 /************************************************************************************
  * Public Functions
  ************************************************************************************/
 
+/****************************************************************************
+ * Name: stm32_caninitialize
+ *
+ * Description:
+ *   Initialize the selected CAN port
+ *
+ * Input Parameter:
+ *   Port number (for hardware that has mutiple CAN interfaces)
+ *
+ * Returned Value:
+ *   Valid CAN device structure reference on succcess; a NULL on failure
+ *
+ ****************************************************************************/
+
+struct can_dev_s;
+EXTERN FAR struct can_dev_s *stm32_caninitialize(int port);
+
+#undef EXTERN
+#if defined(__cplusplus)
+}
+#endif
+
+#endif /* __ASSEMBLY__ */
+#endif /* CONFIG_CAN && (CONFIG_STM32_CAN1 || CONFIG_STM32_CAN2) */
 #endif /* __ARCH_ARM_SRC_STM32_STM32_CAN_H */
