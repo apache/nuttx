9c9d742528797049af3ba5504d25e497d3a3c752
patacongo <patacongo@42af7a65-404d-4744-a932-0658087f49c3>
2013-02-13 00:32:00 +0000

    LPC1788 header file updates from Rommel Marcelo
    git-svn-id: svn://svn.code.sf.net/p/nuttx/code/trunk@5647 42af7a65-404d-4744-a932-0658087f49c3

--- arch/arm/src/lpc17xx/chip/lpc17_wdt.h
+++ arch/arm/src/lpc17xx/chip/lpc17_wdt.h
@@ -51,47 +51,95 @@
 
 /* Register offsets *****************************************************************/
 
-#define LPC17_WDT_WDMOD_OFFSET     0x0000  /* Watchdog mode register */
-#define LPC17_WDT_WDTC_OFFSET      0x0004  /* Watchdog timer constant register */
-#define LPC17_WDT_WDFEED_OFFSET    0x0008  /* Watchdog feed sequence register */
-#define LPC17_WDT_WDTV_OFFSET      0x000c  /* Watchdog timer value register */
-#define LPC17_WDT_WDCLKSEL_OFFSET  0x0010  /* Watchdog clock source selection register */
+#define LPC17_WDT_MOD_OFFSET       0x0000  /* Watchdog mode register */
+#define LPC17_WDT_TC_OFFSET        0x0004  /* Watchdog timer constant register */
+#define LPC17_WDT_FEED_OFFSET      0x0008  /* Watchdog feed sequence register */
+#define LPC17_WDT_TV_OFFSET        0x000c  /* Watchdog timer value register */
+
+#ifdef LPC176x
+#  define LPC17_WDT_CLKSEL_OFFSET  0x0010  /* Watchdog clock source selection register */
+#endif
+
+#ifdef LPC178x
+#  define LPC17_WDT_WARNINT_OFFSET 0x0014  /* Watchdog warning interrupt */
+#  define LPC17_WDT_WINDOW_OFFSET  0x0018  /* Watchdog window compare value */
+#endif
 
 /* Register addresses ***************************************************************/
 
-#define LPC17_WDT_WDMOD            (LPC17_WDT_BASE+LPC17_WDT_WDMOD_OFFSET)
-#define LPC17_WDT_WDTC             (LPC17_WDT_BASE+LPC17_WDT_WDTC_OFFSET)
-#define LPC17_WDT_WDFEED           (LPC17_WDT_BASE+LPC17_WDT_WDFEED_OFFSET)
-#define LPC17_WDT_WDTV             (LPC17_WDT_BASE+LPC17_WDT_WDTV_OFFSET)
-#define LPC17_WDT_WDCLKSEL         (LPC17_WDT_BASE+LPC17_WDT_WDCLKSEL_OFFSET)
+#define LPC17_WDT_MOD              (LPC17_WDT_BASE+LPC17_WDT_MOD_OFFSET)
+#define LPC17_WDT_TC               (LPC17_WDT_BASE+LPC17_WDT_TC_OFFSET)
+#define LPC17_WDT_FEED             (LPC17_WDT_BASE+LPC17_WDT_FEED_OFFSET)
+#define LPC17_WDT_TV               (LPC17_WDT_BASE+LPC17_WDT_TV_OFFSET)
+#define LPC17_WDT_CLKSEL           (LPC17_WDT_BASE+LPC17_WDT_CLKSEL_OFFSET)
+
+#ifdef LPC178x
+#  define LPC17_WDT_WARNINT        (LPC17_WDT_BASE+LPC17_WDT_WARNINT_OFFSET)
+#  define LPC17_WDT_WINDOW         (LPC17_WDT_BASE+LPC17_WDT_WINDOW_OFFSET)
+#endif
 
 /* Register bit definitions *********************************************************/
 
 /* Watchdog mode register */
 
-#define WDT_WDMOD_WDEN             (1 << 0)  /* Bit 0: Watchdog enable */
-#define WDT_WDMOD_WDRESET          (1 << 1)  /* Bit 1: Watchdog reset enable */
-#define WDT_WDMOD_WDTOF            (1 << 2)  /* Bit 2: Watchdog time-out */
-#define WDT_WDMOD_WDINT            (1 << 3)  /* Bit 3: Watchdog interrupt */
-                                             /* Bits 14-31: Reserved */
+#define WDT_MOD_WDEN               (1 << 0)  /* Bit 0: Watchdog enable */
+#define WDT_MOD_WDRESET            (1 << 1)  /* Bit 1: Watchdog reset enable */
+#define WDT_MOD_WDTOF              (1 << 2)  /* Bit 2: Watchdog time-out */
+#define WDT_MOD_WDINT              (1 << 3)  /* Bit 3: Watchdog interrupt */
+#ifdef LPC178x
+#  define WDT_MOD_WDPROTECT        (1 << 4)  /* Bit 4: Watchdog interrupt */
+#endif
+                                             /* Bits 5-31: Reserved */
+/* Watchdog timer constant register */
 
-/* Watchdog timer constant register (Bits 0-31: Watchdog time-out interval) */
+#ifdef LPC176x
+#  define WDT_TC                   (0xffffffff) /* Bits 0-31: Watchdog time-out interval */
+#endif
+#ifdef LPC178x
+#  define WDT_TC                   (0x00ffffff) /* Bits 0-23: Watchdog time-out interval */
+                                             /* Bits 24-31: Reserved */
+#endif
 
 /* Watchdog feed sequence register  */
 
-#define WDT_WDFEED_MASK            (0xff)    /* Bits 0-7: Feed value should be 0xaa followed by 0x55 */
+#define WDT_FEED_MASK              (0xff)    /* Bits 0-7: Feed value should be 0xaa
+                                              *           followed by 0x55 */
                                              /* Bits 14-31: Reserved */
-/* Watchdog timer value register (Bits 0-31: Counter timer value) */
+/* Watchdog timer value register */
+
+#ifdef LPC176x
+#  define WDT_TVT                  (0xffffffff) /* Bits 0-31: Watchdog timer value */
+#endif
+#ifdef LPC178x
+#  define WDT_TVT                  (0xffffff) /* Bits 0-23: Watchdog timer value */
+                                              /* Bits 24-31: Reserved */
+#endif
 
 /* Watchdog clock source selection register */
 
-#define WDT_WDCLKSEL_WDSEL_SHIFT   (0)       /* Bits 0-1: Clock source for the Watchdog timer */
-#define WDT_WDCLKSEL_WDSEL_MASK    (3 << WDT_WDCLKSEL_WDSEL_SHIFT)
-#  define WDT_WDCLKSEL_WDSEL_INTRC (0 << WDT_WDCLKSEL_WDSEL_SHIFT) /* Internal RC osc */
-#  define WDT_WDCLKSEL_WDSEL_APB   (1 << WDT_WDCLKSEL_WDSEL_SHIFT) /* APB peripheral clock (watchdog pclk) */
-#  define WDT_WDCLKSEL_WDSEL_RTC   (2 << WDT_WDCLKSEL_WDSEL_SHIFT) /* RTC oscillator (rtc_clk) */
+#ifdef LPC176x
+#  define WDT_CLKSEL_WDSEL_SHIFT   (0)       /* Bits 0-1: Clock source for the Watchdog timer */
+#  define WDT_CLKSEL_WDSEL_MASK    (3 << WDT_CLKSEL_WDSEL_SHIFT)
+#    define WDT_CLKSEL_WDSEL_INTRC (0 << WDT_CLKSEL_WDSEL_SHIFT) /* Internal RC osc */
+#    define WDT_CLKSEL_WDSEL_APB   (1 << WDT_CLKSEL_WDSEL_SHIFT) /* APB peripheral clock (watchdog pclk) */
+#    define WDT_CLKSEL_WDSEL_RTC   (2 << WDT_CLKSEL_WDSEL_SHIFT) /* RTC oscillator (rtc_clk) */
                                              /* Bits 2-30: Reserved */
-#define WDT_WDCLKSEL_WDLOCK        (1 << 31) /* Bit 31: Lock WDT register bits if set */
+#  define WDT_CLKSEL_WDLOCK        (1 << 31) /* Bit 31: Lock WDT register bits if set */
+#endif
+
+/* Watchdog timer warning interrupt register */
+
+#ifdef LPC178x
+# define WDT_WARNINT               (0x3ff)   /* Bits 0-9: Warning Interrupt compare value */
+                                             /* Bits 10-31: Reserved */
+#endif
+
+/* Watchdog timer value register */
+
+#ifdef LPC178x
+# define WDT_WINDOW                (0xffffff) /* Bits 0-23: Watchdog window value */
+                                              /* Bits 24-31: Reserved */
+#endif
 
 /************************************************************************************
  * Public Types
