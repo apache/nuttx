c78e96bebdbd3bcc093973290212c6e4b33e0c02
patacongo <patacongo@42af7a65-404d-4744-a932-0658087f49c3>
2013-03-22 14:49:21 +0000

    Add kernel build support to the STM32 family and to the STM32F4Discovery board
    git-svn-id: svn://svn.code.sf.net/p/nuttx/code/trunk@5774 42af7a65-404d-4744-a932-0658087f49c3

---
+++ configs/zkit-arm-1769/src/up_lcd.c
@@ -0,0 +1,195 @@
+/****************************************************************************
+ * configs/zkit-arm-1769/src/up_lcd.c
+ * arch/arm/src/board/up_lcd.c
+ *
+ *   Copyright (C) 2013 Zilogic Systems. All rights reserved.
+ *   Author: Manikandan <code@zilogic.com>
+ *
+ * Based on configs/lm3s6965-ek/src/up_oled.c
+ *
+ *   Copyright (C) 2012 Gregory Nutt. All rights reserved.
+ *   Author: Gregory Nutt <gnutt@nuttx.org>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name NuttX nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <debug.h>
+#include <errno.h>
+
+#include <nuttx/spi.h>
+#include <nuttx/lcd/lcd.h>
+#include <nuttx/lcd/st7567.h>
+
+#include "up_arch.h"
+#include "up_internal.h"
+
+#include "lpc17_gpio.h"
+#include "zkitarm_internal.h"
+
+#ifdef CONFIG_NX_LCDDRIVER
+
+/****************************************************************************
+ * Definitions
+ ****************************************************************************/
+
+/* Enables debug output from this file (needs CONFIG_DEBUG with
+ * CONFIG_DEBUG_VERBOSE too)
+ */
+
+#undef LCD_DEBUG   /* Define to enable debug */
+#undef LCD_VERBOSE /* Define to enable verbose debug */
+
+#ifdef LCD_DEBUG
+#  define leddbg  lldbg
+#  ifdef LCD_VERBOSE
+#    define ledvdbg lldbg
+#  else
+#    define ledvdbg(x...)
+#  endif
+#else
+#  undef LCD_VERBOSE
+#  define leddbg(x...)
+#  define ledvdbg(x...)
+#endif
+/****************************************************************************
+ * Private Data
+ ****************************************************************************/
+
+FAR struct spi_dev_s *spi;
+FAR struct lcd_dev_s *dev;
+
+/****************************************************************************
+ * Private Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: up_lcdinitialize
+ ****************************************************************************/
+
+int up_lcdinitialize(void)
+{
+  lpc17_configgpio(ZKITARM_OLED_RST);
+  lpc17_configgpio(ZKITARM_OLED_CS);
+  lpc17_configgpio(ZKITARM_OLED_RS);
+  lpc17_gpiowrite(ZKITARM_OLED_RST, 1);
+  lpc17_gpiowrite(ZKITARM_OLED_CS, 1);
+  lpc17_gpiowrite(ZKITARM_OLED_RS, 1);
+
+  spi = up_spiinitialize(0);
+  if (!spi)
+    {
+      glldbg("Failed to initialize SSI port 0\n");
+      return 0;
+    }
+  lpc17_gpiowrite(ZKITARM_OLED_RST, 0);
+  up_mdelay(1);
+  lpc17_gpiowrite(ZKITARM_OLED_RST, 1);
+  return 1;
+}
+
+/****************************************************************************
+ * Name: up_lcdgetdev
+ ****************************************************************************/
+
+FAR struct lcd_dev_s *up_lcdgetdev(int lcddev)
+{
+  dev = st7567_initialize(spi, lcddev);
+  if (!dev)
+    {
+      glldbg("Failed to bind SSI port 0 to OLCD %d: %d\n", lcddev);
+    }
+  else
+    {
+      gllvdbg("Bound SSI port 0 to OLCD %d\n", lcddev);
+
+      /* And turn the OLCD on (CONFIG_LCD_MAXPOWER should be 1) */
+      (void)dev->setpower(dev, CONFIG_LCD_MAXPOWER);
+      return dev;
+    }
+  return NULL;
+}
+
+/****************************************************************************
+ * Name: up_lcduninitialize
+ ****************************************************************************/
+
+void up_lcduninitialize(void)
+{
+  /* TO-FIX */
+}
+
+/******************************************************************************
+ * Name:  lpc17_spicmddata
+ *
+ * Description:
+ *   Set or clear the SD1329 D/Cn bit to select data (true) or command
+ *   (false).  This function must be provided by platform-specific logic.
+ *   This is an implementation of the cmddata method of the SPI
+ *   interface defined by struct spi_ops_s (see include/nuttx/spi.h).
+ *
+ * Input Parameters:
+ *
+ *   spi - SPI device that controls the bus the device that requires the CMD/
+ *         DATA selection.
+ *   devid - If there are multiple devices on the bus, this selects which one
+ *         to select cmd or data.  NOTE:  This design restricts, for example,
+ *         one one SPI display per SPI bus.
+ *   cmd - true: select command; false: select data
+ *
+ * Returned Value:
+ *   None
+ *
+ ******************************************************************************/
+
+int lpc17_ssp0cmddata(FAR struct spi_dev_s *dev, enum spi_dev_e devid, bool cmd)
+{
+  if (devid == SPIDEV_DISPLAY)
+    {
+      /* Set GPIO to 1 for data, 0 for command */
+
+      lpc17_gpiowrite(ZKITARM_OLED_RS, !cmd);
+      return OK;
+    }
+
+  return -ENODEV;
+}
+
+#endif /* CONFIG_ARCH_LCDS */
