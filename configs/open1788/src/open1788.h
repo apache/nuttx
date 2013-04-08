56cb8ce142934ba0ca3128c963e230025e37af11
Gregory Nutt <gnutt@nuttx.org>
2013-04-08 09:55:12 -0600

    Minor improvements to comments and debug output

--- configs/open1788/src/open1788.h
+++ configs/open1788/src/open1788.h
@@ -129,19 +129,20 @@
 #define GPIO_LCD_BL      (GPIO_OUTPUT | GPIO_VALUE_ZERO | GPIO_PORT2 | GPIO_PIN1)
 
 /* XPT2046 Touchscreen **************************************************************/
-/* -------------- -------------------- ------------ --------------
+/* -------------- -------------------- ------------ --------------------------------
  * XTPT2046       Module               Module       Open1788 LED
  *                Signal               Connector    Connector
- * -------------- -------------------- ------------ --------------
+ * -------------- -------------------- ------------ ---------------------------------
  * Pin 11 PENIRQ\ PENIRQ (pulled high) PORT3 Pin 1  P2.15 PENIRQ
- * Pin 12 DOUT    MISO                 PORT3 Pin 4  P1.18 MISO1
+ * Pin 12 DOUT    MISO                 PORT3 Pin 4  P1.18 MISO1  (Also USB HOST UP LED)
  * Pin 13 BUSY    BUSY (pulled high)   PORT3 Pin 9  P2.14 BUSY
- * Pin 14 DIN     MOSI                 PORT3 Pin 3  P0.13 MOSI1
- * Pin 15 CS\     SSEL (pulled high)   PORT3 Pin 6  P1.8  GPIO
+ * Pin 14 DIN     MOSI                 PORT3 Pin 3  P0.13 MOSI1  (Also USB Device up LED and SD CD pin)
+ * Pin 15 CS\     SSEL (pulled high)   PORT3 Pin 6  P1.8  GPIO   (Also RMII_CRS_DV)
  * Pin 16 DCLK    SCK                  PORT3 Pin 5  P1.19 SCK1
- * -------------- -------------------- ------------ --------------
+ * -------------- -------------------- ------------ ---------------------------------
  *
- * Pins are configured as floating because there are pullups on the module.
+ * Pins should not need to be configured as pull-ups because, according to the LCD
+ * schematic, the are pulled-up on board the LCD module.
  */
 
 #define GPIO_TC_PENIRQ   (GPIO_INTBOTH | GPIO_FLOAT | GPIO_PORT2 | GPIO_PIN15)
