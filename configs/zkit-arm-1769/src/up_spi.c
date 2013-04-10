18ab40bb9925c744c9c546f86503aba110ef7147
Gregory Nutt <gnutt@nuttx.org>
2013-04-10 07:31:22 -0600

    Zkit-arm-1769 board uses SSP0 to LCD and SPI to SD-Card

--- configs/zkit-arm-1769/src/up_spi.c
+++ configs/zkit-arm-1769/src/up_spi.c
@@ -1,9 +1,9 @@
 /************************************************************************************
- * configs/zkit-arm-1769/src/up_ssp.c
- * arch/arm/src/board/up_ssp.c
+ * configs/zkit-arm-1769/src/up_spi.c
+ * arch/arm/src/board/up_spi.c
  *
  *   Copyright (C) 2013 Zilogic Systems. All rights reserved.
- *   Author: BabuSubashChandar <code@zilogic.com>
+ *   Author: Kannan <code@zilogic.com>
  *
  * Based on configs/lpcxpresso-lpc1768/src/up_ssp.c
  *
@@ -54,11 +54,11 @@
 
 #include "up_arch.h"
 #include "chip.h"
-#include "lpc17_ssp.h"
+#include "lpc17_spi.h"
 #include "lpc17_gpio.h"
 #include "zkitarm_internal.h"
 
-#if defined(CONFIG_LPC17_SSP0) || defined(CONFIG_LPC17_SSP1)
+#if defined(CONFIG_LPC17_SPI)
 
 /************************************************************************************
  * Definitions
@@ -67,23 +67,23 @@
 /* CONFIG_DEBUG_SPI enables debug output from this file (needs CONFIG_DEBUG too) */
 
 #ifdef CONFIG_DEBUG_SPI
-#  define sspdbg  lldbg
+#  define spidbg  lldbg
 #  ifdef CONFIG_DEBUG_VERBOSE
-#    define sspvdbg lldbg
+#    define spivdbg lldbg
 #  else
-#    define sspvdbg(x...)
+#    define spivdbg(x...)
 #  endif
 #else
-#  define sspdbg(x...)
-#  define sspvdbg(x...)
+#  define spidbg(x...)
+#  define spivdbg(x...)
 #endif
 
 /* Dump GPIO registers */
 
 #if defined(CONFIG_DEBUG_SPI) && defined(CONFIG_DEBUG_VERBOSE)
-#  define ssp_dumpgpio(m) lpc17_dumpgpio(SDCCS_GPIO, m)
+#  define spi_dumpgpio(m) lpc17_dumpgpio(SDCCS_GPIO, m)
 #else
-#  define ssp_dumpgpio(m)
+#  define spi_dumpgpio(m)
 #endif
 
 /************************************************************************************
@@ -95,102 +95,96 @@
  ************************************************************************************/
 
 /************************************************************************************
- * Name: zkit_sspinitialize
+ * Name: zkit_spiinitialize
  *
  * Description:
- *   Called to configure SPI chip select GPIO pins for the ZKIT-ARM-1769 Kit.
+ *   Called to configure SPI chip select and card detect GPIO pins for the
+ *   ZKIT-ARM-1769 Kit.
  *
  ************************************************************************************/
 
-void weak_function zkit_sspinitialize(void)
+void weak_function zkit_spiinitialize(void)
 {
-  /* Configure the SPI-based LCD CS GPIO */
+  /* Configure the SPI-based microSD CS  and Card Detect (CD) GPIO */
 
-  ssp_dumpgpio("zkit_sspinitialize() Entry)");
+  spi_dumpgpio("zkit_spiinitialize() Entry)");
 
-  /* Configure chip select for the LCD. */
+  /* Configure card detect and chip select for the SD slot. */
 
-#ifdef CONFIG_LPC17_SSP0
-#ifdef CONFIG_NX_LCDDRIVER
-  (void)lpc17_configgpio(ZKITARM_OLED_CS);
-#endif
-#endif /* CONFIG_LPC17_SSP0 */
+  (void)lpc17_configgpio(ZKITARM_SD_CS);
+  (void)lpc17_configgpio(ZKITARM_SD_CD);
 
-  ssp_dumpgpio("zkit_sspinitialize() Exit");
+  spi_dumpgpio("zkit_spiinitialize() Exit");
 }
 
 /************************************************************************************
- * Name:  lpc17_ssp0/ssp1select and lpc17_ssp0/ssp1status
+ * Name:  lpc17_spiselect and lpc17_spistatus
  *
  * Description:
- *   The external functions, lpc17_ssp0/ssp1select and lpc17_ssp0/ssp1status
- *   must be provided by board-specific logic.  They are implementations of the select
+ *   The external functions, lpc17_spiselect and  lpc17_spistatus
+ *   must be provided by board-specific logic. They are implementations of the select
  *   and status methods of the SPI interface defined by struct spi_ops_s (see
- *   include/nuttx/spi.h). All other methods (including lpc17_sspinitialize())
+ *   include/nuttx/spi.h). All other methods (including up_spiinitialize())
  *   are provided by common LPC17xx logic.  To use this common SPI logic on your
  *   board:
  *
  *   1. Provide logic in lpc17_boardinitialize() to configure SPI/SSP chip select
  *      pins.
- *   2. Provide lpc17_ssp0/ssp1select() and lpc17_ssp0/ssp1status() functions
+ *   2. Provide lpc17_spiselect and  lpc17_spistatus functions
  *      in your board-specific logic.  These functions will perform chip selection
  *      and status operations using GPIOs in the way your board is configured.
- *   3. Add a calls to lpc17_sspinitialize() in your low level application
+ *   3. Add a calls to up_spiinitialize() in your low level application
  *      initialization logic
- *   4. The handle returned by lpc17_sspinitialize() may then be used to bind the
+ *   4. The handle returned by up_spiinitialize() may then be used to bind the
  *      SPI driver to higher level logic (e.g., calling
  *      mmcsd_spislotinitialize(), for example, will bind the SPI driver to
  *      the SPI MMC/SD driver).
  *
  ************************************************************************************/
 
-#ifdef CONFIG_LPC17_SSP1
-void  lpc17_ssp1select(FAR struct spi_dev_s *dev, enum spi_dev_e devid, bool selected)
+void  lpc17_spiselect(FAR struct spi_dev_s *dev, enum spi_dev_e devid, bool selected)
 {
-  sspdbg("devid: %d CS: %s\n", (int)devid, selected ? "assert" : "de-assert");
-  ssp_dumpgpio("lpc17_spi1select() Entry");
+  spidbg("devid: %d CS: %s\n", (int)devid, selected ? "assert" : "de-assert");
+  spi_dumpgpio("lpc17_spiselect() Entry");
 
-#warning "Assert CS here (false)"
-
-  ssp_dumpgpio("lpc17_spi1select() Exit");
-}
-
-uint8_t lpc17_ssp1status(FAR struct spi_dev_s *dev, enum spi_dev_e devid)
-{
-  sspdbg("Returning SPI_STATUS_PRESENT\n");
-  return SPI_STATUS_PRESENT;
-}
-#endif /* CONFIG_LPC17_SSP1 */
-
-#ifdef CONFIG_LPC17_SSP0
-void  lpc17_ssp0select(FAR struct spi_dev_s *dev, enum spi_dev_e devid, bool selected)
-{
-  sspdbg("devid: %d CS: %s\n", (int)devid, selected ? "assert" : "de-assert");
-  ssp_dumpgpio("lpc17_spi0select() Entry");
-
-#ifdef CONFIG_NX_LCDDRIVER
-  if (devid == SPIDEV_DISPLAY)
+  if (devid == SPIDEV_MMCSD)
     {
-      /* Assert the CS pin to the OLED display */
+      /* Assert/de-assert the CS pin to the card */
 
-      (void)lpc17_gpiowrite(ZKITARM_OLED_CS, !selected);
-    }
-#endif
-
-  ssp_dumpgpio("lpc17_spi0select() Exit");
-}
-
-uint8_t lpc17_ssp0status(FAR struct spi_dev_s *dev, enum spi_dev_e devid)
-{
-  if (devid == SPIDEV_DISPLAY)
-    {
-      sspdbg("Returning SPI_STATUS_PRESENT\n");
-      return SPI_STATUS_PRESENT;
+      (void)lpc17_gpiowrite(ZKITARM_SD_CS, !selected);
     }
 
-  sspdbg("Returning zero\n");
+  spi_dumpgpio("lpc17_spiselect() Exit");
+}
+
+uint8_t lpc17_spistatus(FAR struct spi_dev_s *dev, enum spi_dev_e devid)
+{
+  if (devid == SPIDEV_MMCSD)
+    {
+      /* Read the state of the card-detect bit */
+
+      if (lpc17_gpioread(ZKITARM_SD_CD) == 0)
+        {
+          spidbg("Returning SPI_STATUS_PRESENT\n");
+          return SPI_STATUS_PRESENT;
+        }
+    }
+
+  spidbg("Returning zero\n");
   return 0;
 }
 
-#endif /* CONFIG_LPC17_SSP0 */
-#endif /* CONFIG_LPC17_SSP0 || CONFIG_LPC17_SSP1 */
+#ifdef CONFIG_SPI_CMDDATA
+/******************************************************************************
+ * Name:  lpc17_spicmddata
+ *
+ * Description: Dummy Function
+ *
+ ******************************************************************************/
+
+int lpc17_spicmddata(FAR struct spi_dev_s *dev, enum spi_dev_e devid, bool cmd)
+{
+  return OK;
+}
+
+#endif /* CONFIG_SPI_CMDDATA */
