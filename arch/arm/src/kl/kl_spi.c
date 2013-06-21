add0a6799538cbfcfb8add9d7df5e32a6662c797
Gregory Nutt <gnutt@nuttx.org>
2013-06-21 09:32:57 -0600

    Straighten out issues about who calls C++ initializers with CXXTEST or HELLOXX are built as NSH applications; Add an ofstream test to CXXTEST suggested by Michael; Update many defconfig fiels to that they set configurations to handle C++ constructors just as before these configuration changes

--- arch/arm/src/kl/kl_spi.c
+++ arch/arm/src/kl/kl_spi.c
@@ -1,5 +1,5 @@
 /****************************************************************************
- * arch/arm/src/kl/kl_start.c
+ * arch/arm/src/kl/kl_spi.c
  *
  *   Copyright (C) 2013 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>
