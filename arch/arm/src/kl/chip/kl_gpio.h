b14f0cf9f7d3a212dcb0968cfdc76110c52df7cc
Gregory Nutt <gnutt@nuttx.org>
2013-06-20 18:00:56 -0600

    KL25Z GPIO register dump function now compiles

--- arch/arm/src/kl/chip/kl_gpio.h
+++ arch/arm/src/kl/chip/kl_gpio.h
@@ -46,58 +46,60 @@
  * Pre-processorDefinitions
  ************************************************************************************/
 
+#define KL_GPIO_NPORTS          5
+
 /* Register Offsets *****************************************************************/
 
-#define KL_GPIO_PDOR_OFFSET 0x0000 /* Port Data Output Register */
-#define KL_GPIO_PSOR_OFFSET 0x0004 /* Port Set Output Register */
-#define KL_GPIO_PCOR_OFFSET 0x0008 /* Port Clear Output Register */
-#define KL_GPIO_PTOR_OFFSET 0x000c /* Port Toggle Output Register */
-#define KL_GPIO_PDIR_OFFSET 0x0010 /* Port Data Input Register */
-#define KL_GPIO_PDDR_OFFSET 0x0014 /* Port Data Direction Register */
+#define KL_GPIO_PDOR_OFFSET     0x0000 /* Port Data Output Register */
+#define KL_GPIO_PSOR_OFFSET     0x0004 /* Port Set Output Register */
+#define KL_GPIO_PCOR_OFFSET     0x0008 /* Port Clear Output Register */
+#define KL_GPIO_PTOR_OFFSET     0x000c /* Port Toggle Output Register */
+#define KL_GPIO_PDIR_OFFSET     0x0010 /* Port Data Input Register */
+#define KL_GPIO_PDDR_OFFSET     0x0014 /* Port Data Direction Register */
 
 /* Register Addresses ***************************************************************/
 
-#define KL_GPIO_PDOR(n)    (KL_GPIO_BASE(n)+KL_GPIO_PDOR_OFFSET)
-#define KL_GPIO_PSOR(n)    (KL_GPIO_BASE(n)+KL_GPIO_PSOR_OFFSET)
-#define KL_GPIO_PCOR(n)    (KL_GPIO_BASE(n)+KL_GPIO_PCOR_OFFSET)
-#define KL_GPIO_PTOR(n)    (KL_GPIO_BASE(n)+KL_GPIO_PTOR_OFFSET)
-#define KL_GPIO_PDIR(n)    (KL_GPIO_BASE(n)+KL_GPIO_PDIR_OFFSET)
-#define KL_GPIO_PDDR(n)    (KL_GPIO_BASE(n)+KL_GPIO_PDDR_OFFSET)
+#define KL_GPIO_PDOR(n)         (KL_GPIO_BASE(n)+KL_GPIO_PDOR_OFFSET)
+#define KL_GPIO_PSOR(n)         (KL_GPIO_BASE(n)+KL_GPIO_PSOR_OFFSET)
+#define KL_GPIO_PCOR(n)         (KL_GPIO_BASE(n)+KL_GPIO_PCOR_OFFSET)
+#define KL_GPIO_PTOR(n)         (KL_GPIO_BASE(n)+KL_GPIO_PTOR_OFFSET)
+#define KL_GPIO_PDIR(n)         (KL_GPIO_BASE(n)+KL_GPIO_PDIR_OFFSET)
+#define KL_GPIO_PDDR(n)         (KL_GPIO_BASE(n)+KL_GPIO_PDDR_OFFSET)
 
-#define KL_GPIOA_PDOR      (KL_GPIOA_BASE+KL_GPIO_PDOR_OFFSET)
-#define KL_GPIOA_PSOR      (KL_GPIOA_BASE+KL_GPIO_PSOR_OFFSET)
-#define KL_GPIOA_PCOR      (KL_GPIOA_BASE+KL_GPIO_PCOR_OFFSET)
-#define KL_GPIOA_PTOR      (KL_GPIOA_BASE+KL_GPIO_PTOR_OFFSET)
-#define KL_GPIOA_PDIR      (KL_GPIOA_BASE+KL_GPIO_PDIR_OFFSET)
-#define KL_GPIOA_PDDR      (KL_GPIOA_BASE+KL_GPIO_PDDR_OFFSET)
+#define KL_GPIOA_PDOR           (KL_GPIOA_BASE+KL_GPIO_PDOR_OFFSET)
+#define KL_GPIOA_PSOR           (KL_GPIOA_BASE+KL_GPIO_PSOR_OFFSET)
+#define KL_GPIOA_PCOR           (KL_GPIOA_BASE+KL_GPIO_PCOR_OFFSET)
+#define KL_GPIOA_PTOR           (KL_GPIOA_BASE+KL_GPIO_PTOR_OFFSET)
+#define KL_GPIOA_PDIR           (KL_GPIOA_BASE+KL_GPIO_PDIR_OFFSET)
+#define KL_GPIOA_PDDR           (KL_GPIOA_BASE+KL_GPIO_PDDR_OFFSET)
 
-#define KL_GPIOB_PDOR      (KL_GPIOB_BASE+KL_GPIO_PDOR_OFFSET)
-#define KL_GPIOB_PSOR      (KL_GPIOB_BASE+KL_GPIO_PSOR_OFFSET)
-#define KL_GPIOB_PCOR      (KL_GPIOB_BASE+KL_GPIO_PCOR_OFFSET)
-#define KL_GPIOB_PTOR      (KL_GPIOB_BASE+KL_GPIO_PTOR_OFFSET)
-#define KL_GPIOB_PDIR      (KL_GPIOB_BASE+KL_GPIO_PDIR_OFFSET)
-#define KL_GPIOB_PDDR      (KL_GPIOB_BASE+KL_GPIO_PDDR_OFFSET)
+#define KL_GPIOB_PDOR           (KL_GPIOB_BASE+KL_GPIO_PDOR_OFFSET)
+#define KL_GPIOB_PSOR           (KL_GPIOB_BASE+KL_GPIO_PSOR_OFFSET)
+#define KL_GPIOB_PCOR           (KL_GPIOB_BASE+KL_GPIO_PCOR_OFFSET)
+#define KL_GPIOB_PTOR           (KL_GPIOB_BASE+KL_GPIO_PTOR_OFFSET)
+#define KL_GPIOB_PDIR           (KL_GPIOB_BASE+KL_GPIO_PDIR_OFFSET)
+#define KL_GPIOB_PDDR           (KL_GPIOB_BASE+KL_GPIO_PDDR_OFFSET)
 
-#define KL_GPIOC_PDOR      (KL_GPIOC_BASE+KL_GPIO_PDOR_OFFSET)
-#define KL_GPIOC_PSOR      (KL_GPIOC_BASE+KL_GPIO_PSOR_OFFSET)
-#define KL_GPIOC_PCOR      (KL_GPIOC_BASE+KL_GPIO_PCOR_OFFSET)
-#define KL_GPIOC_PTOR      (KL_GPIOC_BASE+KL_GPIO_PTOR_OFFSET)
-#define KL_GPIOC_PDIR      (KL_GPIOC_BASE+KL_GPIO_PDIR_OFFSET)
-#define KL_GPIOC_PDDR      (KL_GPIOC_BASE+KL_GPIO_PDDR_OFFSET)
+#define KL_GPIOC_PDOR           (KL_GPIOC_BASE+KL_GPIO_PDOR_OFFSET)
+#define KL_GPIOC_PSOR           (KL_GPIOC_BASE+KL_GPIO_PSOR_OFFSET)
+#define KL_GPIOC_PCOR           (KL_GPIOC_BASE+KL_GPIO_PCOR_OFFSET)
+#define KL_GPIOC_PTOR           (KL_GPIOC_BASE+KL_GPIO_PTOR_OFFSET)
+#define KL_GPIOC_PDIR           (KL_GPIOC_BASE+KL_GPIO_PDIR_OFFSET)
+#define KL_GPIOC_PDDR           (KL_GPIOC_BASE+KL_GPIO_PDDR_OFFSET)
 
-#define KL_GPIOD_PDOR      (KL_GPIOD_BASE+KL_GPIO_PDOR_OFFSET)
-#define KL_GPIOD_PSOR      (KL_GPIOD_BASE+KL_GPIO_PSOR_OFFSET)
-#define KL_GPIOD_PCOR      (KL_GPIOD_BASE+KL_GPIO_PCOR_OFFSET)
-#define KL_GPIOD_PTOR      (KL_GPIOD_BASE+KL_GPIO_PTOR_OFFSET)
-#define KL_GPIOD_PDIR      (KL_GPIOD_BASE+KL_GPIO_PDIR_OFFSET)
-#define KL_GPIOD_PDDR      (KL_GPIOD_BASE+KL_GPIO_PDDR_OFFSET)
+#define KL_GPIOD_PDOR           (KL_GPIOD_BASE+KL_GPIO_PDOR_OFFSET)
+#define KL_GPIOD_PSOR           (KL_GPIOD_BASE+KL_GPIO_PSOR_OFFSET)
+#define KL_GPIOD_PCOR           (KL_GPIOD_BASE+KL_GPIO_PCOR_OFFSET)
+#define KL_GPIOD_PTOR           (KL_GPIOD_BASE+KL_GPIO_PTOR_OFFSET)
+#define KL_GPIOD_PDIR           (KL_GPIOD_BASE+KL_GPIO_PDIR_OFFSET)
+#define KL_GPIOD_PDDR           (KL_GPIOD_BASE+KL_GPIO_PDDR_OFFSET)
 
-#define KL_GPIOE_PDOR      (KL_GPIOE_BASE+KL_GPIO_PDOR_OFFSET)
-#define KL_GPIOE_PSOR      (KL_GPIOE_BASE+KL_GPIO_PSOR_OFFSET)
-#define KL_GPIOE_PCOR      (KL_GPIOE_BASE+KL_GPIO_PCOR_OFFSET)
-#define KL_GPIOE_PTOR      (KL_GPIOE_BASE+KL_GPIO_PTOR_OFFSET)
-#define KL_GPIOE_PDIR      (KL_GPIOE_BASE+KL_GPIO_PDIR_OFFSET)
-#define KL_GPIOE_PDDR      (KL_GPIOE_BASE+KL_GPIO_PDDR_OFFSET)
+#define KL_GPIOE_PDOR           (KL_GPIOE_BASE+KL_GPIO_PDOR_OFFSET)
+#define KL_GPIOE_PSOR           (KL_GPIOE_BASE+KL_GPIO_PSOR_OFFSET)
+#define KL_GPIOE_PCOR           (KL_GPIOE_BASE+KL_GPIO_PCOR_OFFSET)
+#define KL_GPIOE_PTOR           (KL_GPIOE_BASE+KL_GPIO_PTOR_OFFSET)
+#define KL_GPIOE_PDIR           (KL_GPIOE_BASE+KL_GPIO_PDIR_OFFSET)
+#define KL_GPIOE_PDDR           (KL_GPIOE_BASE+KL_GPIO_PDDR_OFFSET)
 
 /* Register Bit Definitions *********************************************************/
 
