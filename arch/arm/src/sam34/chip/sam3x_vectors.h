b089263e1c7d2e37e93389d652f80b79ad3fee67
Gregory Nutt <gnutt@nuttx.org>
2013-06-26 12:59:56 -0600

    Add SAM3X/3A interrupt vectors

--- arch/arm/src/sam34/chip/sam3x_vectors.h
+++ arch/arm/src/sam34/chip/sam3x_vectors.h
@@ -1,7 +1,7 @@
 /************************************************************************************************
- * arch/arm/src/sam34/chip/sam3u_vectors.h
+ * arch/arm/src/sam34/chip/sam3x_vectors.h
  *
- *   Copyright (C) 2009-2010, 2013 Gregory Nutt. All rights reserved.
+ *   Copyright (C) 2013 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>
  *
  * Redistribution and use in source and binary forms, with or without
@@ -49,9 +49,9 @@
 
 #ifdef CONFIG_ARMV7M_CMNVECTOR
 
-/* Reserve 30 interrupt table entries for I/O interrupts. */
+/* Reserve 45 interrupt table entries for I/O interrupts. */
 
-# define ARMV7M_PERIPHERAL_INTERRUPTS 30
+#  define ARMV7M_PERIPHERAL_INTERRUPTS 45
 
 #else
   VECTOR(sam_supc, SAM_IRQ_SUPC)      /* Vector 16+0:  Supply Controller */
@@ -64,24 +64,39 @@
   VECTOR(sam_eefc1, SAM_IRQ_EEFC1)    /* Vector 16+7:  Enhanced Embedded Flash Controller 1 */
   VECTOR(sam_uart0, SAM_IRQ_UART0)    /* Vector 16+8:  Universal Asynchronous Receiver Transmitter */
   VECTOR(sam_smc, SAM_IRQ_SMC)        /* Vector 16+9:  Static Memory Controller */
-  VECTOR(sam_pioa, SAM_IRQ_PIOA)      /* Vector 16+10: Parallel I/O Controller A */
-  VECTOR(sam_piob, SAM_IRQ_PIOB)      /* Vector 16+11: Parallel I/O Controller B */
-  VECTOR(sam_pioc, SAM_IRQ_PIOC)      /* Vector 16+12: Parallel I/O Controller C */
-  VECTOR(sam_usart0, SAM_IRQ_USART0)  /* Vector 16+13: USART 0 */
-  VECTOR(sam_usart1, SAM_IRQ_USART1)  /* Vector 16+14: USART 1 */
-  VECTOR(sam_usart2, SAM_IRQ_USART2)  /* Vector 16+15: USART 2 */
-  VECTOR(sam_usart3, SAM_IRQ_USART3)  /* Vector 16+16: USART 3 */
-  VECTOR(sam_hsmci, SAM_IRQ_HSMCI)    /* Vector 16+17: High Speed Multimedia Card Interface */
-  VECTOR(sam_twi0, SAM_IRQ_TWI0)      /* Vector 16+18: Two-Wire Interface 0 */
-  VECTOR(sam_twi1, SAM_IRQ_TWI1)      /* Vector 16+19: Two-Wire Interface 1 */
-  VECTOR(sam_spi, SAM_IRQ_SPI)        /* Vector 16+20: Serial Peripheral Interface */
-  VECTOR(sam_ssc, SAM_IRQ_SSC)        /* Vector 16+21: Synchronous Serial Controller */
-  VECTOR(sam_tc0, SAM_IRQ_TC0)        /* Vector 16+22: Timer Counter 0 */
-  VECTOR(sam_tc1, SAM_IRQ_TC1)        /* Vector 16+23: Timer Counter 1 */
-  VECTOR(sam_tc2, SAM_IRQ_TC2)        /* Vector 16+24: Timer Counter 2 */
-  VECTOR(sam_pwm, SAM_IRQ_PWM)        /* Vector 16+25: Pulse Width Modulation Controller */
-  VECTOR(sam_adc12b, SAM_IRQ_ADC12B)  /* Vector 16+26: 12-bit ADC Controller */
-  VECTOR(sam_adc, SAM_IRQ_ADC)        /* Vector 16+27: 10-bit ADC Controller */
-  VECTOR(sam_dmac, SAM_IRQ_DMAC)      /* Vector 16+28: DMA Controller */
-  VECTOR(sam_udphs, SAM_IRQ_UDPHS)    /* Vector 16+29: USB Device High Speed */
+  VECTOR(sam_sdramc, SAM_IRQ_SDRAMC)  /* Vector 16+10: Synchronous Dynamic RAM Controller */
+  VECTOR(sam_pioa, SAM_IRQ_PIOA)      /* Vector 16+11: Parallel I/O Controller A */
+  VECTOR(sam_piob, SAM_IRQ_PIOB)      /* Vector 16+12: Parallel I/O Controller B */
+  VECTOR(sam_pioc, SAM_IRQ_PIOC)      /* Vector 16+13: Parallel I/O Controller C */
+  VECTOR(sam_piod, SAM_IRQ_PIOD)      /* Vector 16+14: Parallel I/O Controller D */
+  VECTOR(sam_pioe, SAM_IRQ_PIOE)      /* Vector 16+15: Parallel I/O Controller E */
+  VECTOR(sam_piof, SAM_IRQ_PIOF)      /* Vector 16+16: Parallel I/O Controller F */
+  VECTOR(sam_usart0, SAM_IRQ_USART0)  /* Vector 16+17: USART 0 */
+  VECTOR(sam_usart1, SAM_IRQ_USART1)  /* Vector 16+18: USART 1 */
+  VECTOR(sam_usart2, SAM_IRQ_USART2)  /* Vector 16+19: USART 2 */
+  VECTOR(sam_usart3, SAM_IRQ_USART3)  /* Vector 16+20: USART 3 */
+  VECTOR(sam_hsmci, SAM_IRQ_HSMCI)    /* Vector 16+21: High Speed Multimedia Card Interface */
+  VECTOR(sam_twi0, SAM_IRQ_TWI0)      /* Vector 16+22: Two-Wire Interface 0 */
+  VECTOR(sam_twi1, SAM_IRQ_TWI1)      /* Vector 16+23: Two-Wire Interface 1 */
+  VECTOR(sam_spi0, SAM_IRQ_SPI0)      /* Vector 16+24: Serial Peripheral Interface 0 */
+  VECTOR(sam_spi1, SAM_IRQ_SPI1)      /* Vector 16+25: Serial Peripheral Interface 1 */
+  VECTOR(sam_ssc, SAM_IRQ_SSC)        /* Vector 16+26: Synchronous Serial Controller */
+  VECTOR(sam_tc0, SAM_IRQ_TC0)        /* Vector 16+27: Timer Counter 0 */
+  VECTOR(sam_tc1, SAM_IRQ_TC1)        /* Vector 16+28: Timer Counter 1 */
+  VECTOR(sam_tc2, SAM_IRQ_TC2)        /* Vector 16+29: Timer Counter 2 */
+  VECTOR(sam_tc3, SAM_IRQ_TC3)        /* Vector 16+30: Timer Counter 3 */
+  VECTOR(sam_tc4, SAM_IRQ_TC4)        /* Vector 16+31: Timer Counter 4 */
+  VECTOR(sam_tc5, SAM_IRQ_TC5)        /* Vector 16+32: Timer Counter 5 */
+  VECTOR(sam_tc6, SAM_IRQ_TC6)        /* Vector 16+33: Timer Counter 6 */
+  VECTOR(sam_tc7, SAM_IRQ_TC7)        /* Vector 16+34: Timer Counter 7 */
+  VECTOR(sam_tc8, SAM_IRQ_TC8)        /* Vector 16+35: Timer Counter 8 */
+  VECTOR(sam_pwm, SAM_IRQ_PWM)        /* Vector 16+36: Pulse Width Modulation Controller */
+  VECTOR(sam_adc, SAM_IRQ_ADC)        /* Vector 16+37: ADC Controller */
+  VECTOR(sam_dacc, SAM_IRQ_DACC)      /* Vector 16+38: DAC Controller */
+  VECTOR(sam_dmac, SAM_IRQ_DMAC)      /* Vector 16+39: DMA Controller */
+  VECTOR(sam_uotghs, SAM_IRQ_UOTGHS)  /* Vector 16+40: USB OTG High Speed */
+  VECTOR(sam_trng, SAM_IRQ_TRNG)      /* Vector 16+41: True Random Number Generator */
+  VECTOR(sam_emac, SAM_IRQ_EMAC)      /* Vector 16+42: Ethernet MAC */
+  VECTOR(sam_can0, SAM_IRQ_CAN0)      /* Vector 16+43: CAN Controller 0 */
+  VECTOR(sam_can1, SAM_IRQ_CAN1)      /* Vector 16+44: CAN Controller 1 */
 #endif
