d361bce93aace6c3b118fe24edc752d8e50e8b01
Gregory Nutt <gnutt@nuttx.org>
2013-09-06 11:43:59 -0600

    Networking:  Important bug fix.  The backlog buffer pointer was not being incrmented so that only the first backlog buffer was getting added to the freelist.  From Max Holtzberg

--- net/uip/uip_tcpbacklog.c
+++ net/uip/uip_tcpbacklog.c
@@ -127,6 +127,7 @@ int uip_backlogcreate(FAR struct uip_conn *conn, int nblg)
       for (i = 0; i < nblg; i++)
         {
           sq_addfirst(&blc->bc_node, &bls->bl_free);
+          blc++;
         }
     }
 
