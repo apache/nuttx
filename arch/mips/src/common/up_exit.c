4aaee6fb3c1720b1fce84cb2220fdb0b97a127b9
Gregory Nutt <gnutt@nuttx.org>
2013-09-28 16:50:07 -0600

    Clean up some naming: fd vs. fildes vs. filedes and filep vs filp

--- arch/mips/src/common/up_exit.c
+++ arch/mips/src/common/up_exit.c
@@ -106,14 +106,14 @@ static void _up_dumponexit(FAR struct tcb_s *tcb, FAR void *arg)
   for (i = 0; i < CONFIG_NFILE_STREAMS; i++)
     {
       struct file_struct *filep = &streamlist->sl_streams[i];
-      if (filep->fs_filedes >= 0)
+      if (filep->fs_fd >= 0)
         {
 #if CONFIG_STDIO_BUFFER_SIZE > 0
           sdbg("      fd=%d nbytes=%d\n",
-               filep->fs_filedes,
+               filep->fs_fd,
                filep->fs_bufpos - filep->fs_bufstart);
 #else
-          sdbg("      fd=%d\n", filep->fs_filedes);
+          sdbg("      fd=%d\n", filep->fs_fd);
 #endif
         }
     }
