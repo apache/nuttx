491ad55b5c7967fa00fa95473a6e077a1da98758
Gregory Nutt <gnutt@nuttx.org>
2013-10-11 17:12:35 -0600

    SAMA5 LCDC:  Fixed backlight PWM divider. Backlight no longer flashes

--- configs/sama5d3x-ek/include/board.h
+++ configs/sama5d3x-ek/include/board.h
@@ -71,18 +71,26 @@
 
 #endif
 
-/* LCD Geometry and Timing */
+/* LCD Interface, Geometry and Timing */
 
-#define BOARD_LCD_WIDTH       800      /* Display width (pixels) */
-#define BOARD_LCD_HEIGHT      480      /* Display height (pixels) */
-#define BOARD_LCD_PIXELCLOCK  33260000 /* Pixel clock */
-#define BOARD_LCDC_GUARDTIME  9        /* Guard time */
-#define BOARD_LCD_VSPW        2        /* Vertical pulse width (lines) */
-#define BOARD_LCD_HSPW        128      /* Horizontal pulse width (LCDDOTCLK) */
-#define BOARD_LCD_VFPW        37       /* Vertical front porch (lines) */
-#define BOARD_LCD_VBPW        8        /* Vertical back porch (lines) */
-#define BOARD_LCD_HFPW        168      /* Horizontal front porch (LCDDOTCLK cycles) */
-#define BOARD_LCD_HBPW        88       /* Horizontal back porch (LCDDOTCLK cycles) */
+#define BOARD_LCDC_OUTPUT_BPP 24       /* Output format to H/W is 24BPP RGB */
+#define BOARD_LCDC_WIDTH      800      /* Display width (pixels) */
+#define BOARD_LCDC_HEIGHT     480      /* Display height (rows) */
+#undef  BOARD_LCDC_MCK_MUL2            /* Source clock is Mck (vs 2*Mck) */
+#define BOARD_LCDC_PIXCLK_INV 1        /* Invert pixel clock, use falling edge */
+#define BOARD_LCDC_PIXELCLOCK 33260000 /* Pixel clock frequency */
+#define BOARD_LCDC_GUARDTIME  9        /* Guard time (frames) */
+#define BOARD_LCDC_VSPW       2        /* Vertical pulse width (lines) */
+#define BOARD_LCDC_HSPW       128      /* Horizontal pulse width (LCDDOTCLK) */
+#define BOARD_LCDC_VFPW       37       /* Vertical front porch (lines) */
+#define BOARD_LCDC_VBPW       8        /* Vertical back porch (lines) */
+#define BOARD_LCDC_HFPW       168      /* Horizontal front porch (LCDDOTCLK) */
+#define BOARD_LCDC_HBPW       88       /* Horizontal back porch (LCDDOTCLK) */
+
+/* Backlight prescaler value and PWM output polarity */
+
+#define BOARD_LCDC_PWMPS      LCDC_LCDCFG6_PWMPS_DIV1
+#define BOARD_LCDC_PWMPOL     LCDC_LCDCFG6_PWMPOL
 
 /* LED definitions ******************************************************************/
 /* There are two LEDs on the SAMA5D3 series-CM board that can be controlled
