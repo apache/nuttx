f07134e95919717b10d07b8bc9a95b35af5c8cb1
Gregory Nutt <gnutt@nuttx.org>
2013-11-14 15:33:43 -0600

    LPC31: Configuration and build support for a forthcoming USB host driver; configs/ea3131/nsh converted to use kconfig-frontend tools

--- configs/ea3131/nsh/setenv.sh
+++ configs/ea3131/nsh/setenv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # configs/ea3131/nsh/setenv.sh
 #
-#   Copyright (C) 2010 Gregory Nutt. All rights reserved.
+#   Copyright (C) 2010, 2013 Gregory Nutt. All rights reserved.
 #   Author: Gregory Nutt <gnutt@nuttx.org>
 #
 # Redistribution and use in source and binary forms, with or without
