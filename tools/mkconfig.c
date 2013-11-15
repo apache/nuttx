517546bb47c395e18c617370a29e0c4091a84858
Gregory Nutt <gnutt@nuttx.org>
2013-11-15 07:33:39 -0600

    tools/mkconfig.c:  CONFIG_FS_SMARTFS must be included in the list of writable file systems

--- tools/mkconfig.c
+++ tools/mkconfig.c
@@ -223,14 +223,14 @@ int main(int argc, char **argv, char **envp)
   printf("#endif\n\n");
   printf("/* Check if any readable and writable filesystem (OR USB storage) is supported */\n\n");
   printf("#undef CONFIG_FS_READABLE\n");
-  printf("#undef CONFIG_FS_WRITABLE\n");
+  printf("#undef CONFIG_FS_WRITABLE\n\n");
   printf("#if defined(CONFIG_FS_FAT) || defined(CONFIG_FS_ROMFS) || defined(CONFIG_USBMSC) || \\\n");
   printf("    defined(CONFIG_FS_NXFFS) || defined(CONFIG_FS_SMARTFS) || defined(CONFIG_FS_BINFS) || \\\n");
   printf("    defined(CONFIG_NFS)\n");
   printf("# define CONFIG_FS_READABLE 1\n");
   printf("#endif\n\n");
   printf("#if defined(CONFIG_FS_FAT) || defined(CONFIG_USBMSC) || defined(CONFIG_FS_NXFFS) || \\\n");
-  printf("    defined(CONFIG_NFS)\n");
+  printf("    defined(CONFIG_FS_SMARTFS) || defined(CONFIG_NFS)\n");
   printf("# define CONFIG_FS_WRITABLE 1\n");
   printf("#endif\n\n");
   printf("/* There can be no network support with no socket descriptors */\n\n");
