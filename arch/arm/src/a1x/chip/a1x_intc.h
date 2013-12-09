8f11a1453580b50cf06b15f27c3260550f402d58
Gregory Nutt <gnutt@nuttx.org>
2013-12-09 07:47:15 -0600

    A10: Re-write of interrupt management logic

--- arch/arm/src/a1x/chip/a1x_intc.h
+++ arch/arm/src/a1x/chip/a1x_intc.h
@@ -53,27 +53,43 @@
 #define A1X_INTC_BASEADDR_OFFSET     0x0004 /* Interrupt Base Address */
 #define A1X_INTC_PROTECT_OFFSET      0x0008 /* Interrupt Protection Register */
 #define A1X_INTC_NMICTRL_OFFSET      0x000c /* Interrupt Control */
+
+#define A1X_INTC_IRQ_PEND_OFFSET(n)  (0x0010 + (((n) >> 3) & ~3))
 #define A1X_INTC_IRQ_PEND0_OFFSET    0x0010 /* Interrupt IRQ Pending 0 Status */
 #define A1X_INTC_IRQ_PEND1_OFFSET    0x0014 /* Interrupt IRQ Pending 1 Status */
 #define A1X_INTC_IRQ_PEND2_OFFSET    0x0018 /* Interrupt IRQ Pending 2 Status */
+
+#define A1X_INTC_FIQ_PEND_OFFSET(n)  (0x0020 + (((n) >> 3) & ~3))
 #define A1X_INTC_FIQ_PEND0_OFFSET    0x0020 /* Interrupt FIQ Pending 0 Status */
 #define A1X_INTC_FIQ_PEND1_OFFSET    0x0024 /* Interrupt FIQ Pending 1 Status */
 #define A1X_INTC_FIQ_PEND2_OFFSET    0x0028 /* Interrupt FIQ Pending 2 Status */
+
+#define A1X_INTC_FIRQ_SEL_OFFSET(n)  (0x0030 + (((n) >> 3) & ~3))
 #define A1X_INTC_IRQ_SEL0_OFFSET     0x0030 /* Interrupt Select 0 */
 #define A1X_INTC_IRQ_SEL1_OFFSET     0x0034 /* Interrupt Select 1 */
 #define A1X_INTC_IRQ_SEL2_OFFSET     0x0038 /* Interrupt Select 2 */
+
+#define A1X_INTC_EN_OFFSET(n)        (0x0040 + (((n) >> 3) & ~3))
 #define A1X_INTC_EN0_OFFSET          0x0040 /* Interrupt Enable 0 */
 #define A1X_INTC_EN1_OFFSET          0x0044 /* Interrupt Enable 1 */
 #define A1X_INTC_EN2_OFFSET          0x0048 /* Interrupt Enable 2 */
+
+#define A1X_INTC_MASK_OFFSET(n)      (0x0050 + (((n) >> 3) & ~3))
 #define A1X_INTC_MASK0_OFFSET        0x0050 /* Interrupt Mask 0 */
 #define A1X_INTC_MASK1_OFFSET        0x0054 /* Interrupt Mask 1 */
 #define A1X_INTC_MASK2_OFFSET        0x0058 /* Interrupt Mask 2 */
+
+#define A1X_INTC_RESP_OFFSET(n)      (0x0060 + (((n) >> 3) & ~3))
 #define A1X_INTC_RESP0_OFFSET        0x0060 /* Interrupt Response 0 */
 #define A1X_INTC_RESP1_OFFSET        0x0064 /* Interrupt Response 1 */
 #define A1X_INTC_RESP2_OFFSET        0x0068 /* Interrupt Response 2 */
+
+#define A1X_INTC_FF_OFFSET(n)        (0x0070 + (((n) >> 3) & ~3))
 #define A1X_INTC_FF0_OFFSET          0x0070 /* Interrupt Fast Forcing 0 */
 #define A1X_INTC_FF1_OFFSET          0x0074 /* Interrupt Fast Forcing 1 */
 #define A1X_INTC_FF2_OFFSET          0x0078 /* Interrupt Fast Forcing 2 */
+
+#define A1X_INTC_PRIO_OFFSET(n)      (0x0080 + (((n) >> 2) & ~3))
 #define A1X_INTC_PRIO0_OFFSET        0x0080 /* Interrupt Source Priority 0 */
 #define A1X_INTC_PRIO1_OFFSET        0x0084 /* Interrupt Source Priority 1 */
 #define A1X_INTC_PRIO2_OFFSET        0x0088 /* Interrupt Source Priority 2 */
@@ -86,27 +102,43 @@
 #define A1X_INTC_BASEADDR            (A1X_INTC_VADDR+A1X_INTC_BASEADDR_OFFSET)
 #define A1X_INTC_PROTECT             (A1X_INTC_VADDR+A1X_INTC_PROTECT_OFFSET)
 #define A1X_INTC_NMICTRL             (A1X_INTC_VADDR+A1X_INTC_NMICTRL_OFFSET)
+
+#define A1X_INTC_IRQ_PEND(n)         (A1X_INTC_VADDR+A1X_INTC_IRQ_PEND_OFFSET(n))
 #define A1X_INTC_IRQ_PEND0           (A1X_INTC_VADDR+A1X_INTC_IRQ_PEND0_OFFSET)
 #define A1X_INTC_IRQ_PEND1           (A1X_INTC_VADDR+A1X_INTC_IRQ_PEND1_OFFSET)
 #define A1X_INTC_IRQ_PEND2           (A1X_INTC_VADDR+A1X_INTC_IRQ_PEND2_OFFSET)
+
+#define A1X_INTC_FIQ_PEND(n)         (A1X_INTC_VADDR+A1X_INTC_FIQ_PEND_OFFSET(n))
 #define A1X_INTC_FIQ_PEND0           (A1X_INTC_VADDR+A1X_INTC_FIQ_PEND0_OFFSET)
 #define A1X_INTC_FIQ_PEND1           (A1X_INTC_VADDR+A1X_INTC_FIQ_PEND1_OFFSET)
 #define A1X_INTC_FIQ_PEND2           (A1X_INTC_VADDR+A1X_INTC_FIQ_PEND2_OFFSET)
+
+#define A1X_INTC_IRQ_SEL(n)          (A1X_INTC_VADDR+A1X_INTC_IRQ_SEL_OFFSET(n))
 #define A1X_INTC_IRQ_SEL0            (A1X_INTC_VADDR+A1X_INTC_IRQ_SEL0_OFFSET)
 #define A1X_INTC_IRQ_SEL1            (A1X_INTC_VADDR+A1X_INTC_IRQ_SEL1_OFFSET)
 #define A1X_INTC_IRQ_SEL2            (A1X_INTC_VADDR+A1X_INTC_IRQ_SEL2_OFFSET)
+
+#define A1X_INTC_EN(n)               (A1X_INTC_VADDR+A1X_INTC_EN_OFFSET(n))
 #define A1X_INTC_EN0                 (A1X_INTC_VADDR+A1X_INTC_EN0_OFFSET)
 #define A1X_INTC_EN1                 (A1X_INTC_VADDR+A1X_INTC_EN1_OFFSET)
 #define A1X_INTC_EN2                 (A1X_INTC_VADDR+A1X_INTC_EN2_OFFSET)
+
+#define A1X_INTC_MASK(n)             (A1X_INTC_VADDR+A1X_INTC_MASK_OFFSET(n))
 #define A1X_INTC_MASK0               (A1X_INTC_VADDR+A1X_INTC_MASK0_OFFSET)
 #define A1X_INTC_MASK1               (A1X_INTC_VADDR+A1X_INTC_MASK1_OFFSET)
 #define A1X_INTC_MASK2               (A1X_INTC_VADDR+A1X_INTC_MASK2_OFFSET)
+
+#define A1X_INTC_RESP(n)             (A1X_INTC_VADDR+A1X_INTC_RESP_OFFSET(n))
 #define A1X_INTC_RESP0               (A1X_INTC_VADDR+A1X_INTC_RESP0_OFFSET)
 #define A1X_INTC_RESP1               (A1X_INTC_VADDR+A1X_INTC_RESP1_OFFSET)
 #define A1X_INTC_RESP2               (A1X_INTC_VADDR+A1X_INTC_RESP2_OFFSET)
+
+#define A1X_INTC_FF(n)               (A1X_INTC_VADDR+A1X_INTC_FF_OFFSET(n))
 #define A1X_INTC_FF0                 (A1X_INTC_VADDR+A1X_INTC_FF0_OFFSET)
 #define A1X_INTC_FF1                 (A1X_INTC_VADDR+A1X_INTC_FF1_OFFSET)
 #define A1X_INTC_FF2                 (A1X_INTC_VADDR+A1X_INTC_FF2_OFFSET)
+
+#define A1X_INTC_PRIO(n)             (A1X_INTC_VADDR+A1X_INTC_PRIO_OFFSET(n))
 #define A1X_INTC_PRIO0               (A1X_INTC_VADDR+A1X_INTC_PRIO0_OFFSET)
 #define A1X_INTC_PRIO1               (A1X_INTC_VADDR+A1X_INTC_PRIO1_OFFSET)
 #define A1X_INTC_PRIO2               (A1X_INTC_VADDR+A1X_INTC_PRIO2_OFFSET)
@@ -136,80 +168,39 @@
 
 /* Interrupt IRQ Pending 0-2 Status */
 
-#define INTC_IRQ_PEND(n)             (1 << (n))         /* n=0-31:  Interrupt pending */
-#  define INTC_IRQ_PEND0(n)          (1 << (n))         /* n=0-31:  Interrupt pending */
-#  define INTC_IRQ_PEND1(n)          (1 << ((n) - 32))  /* n=32-63: Interrupt pending */
-#  define INTC_IRQ_PEND2(n)          (1 << ((n) - 64))  /* n=64-95: Interrupt pending */
+#define INTC_IRQ_PEND(n)             (1 << ((n) & 0x1f)) /* n=0-95:  Interrupt pending */
 
 /* Interrupt FIQ Pending 0-2 Status */
 
-#define INTC_FIQ_PEND(n)             (1 << (n))         /* n=0-31:  Interrupt pending */
-#  define INTC_FIQ_PEND0(n)          (1 << (n))         /* n=0-31:  Interrupt pending */
-#  define INTC_FIQ_PEND1(n)          (1 << ((n) - 32))  /* n=32-63: Interrupt pending */
-#  define INTC_FIQ_PEND2(n)          (1 << ((n) - 64))  /* n=64-95: Interrupt pending */
+#define INTC_FIQ_PEND(n)             (1 << ((n) & 0x1f)) /* n=0-95:  Interrupt pending */
 
 /* Interrupt Select 0-2 */
 
-#define INTC_IRQ_SEL(n)              (1 << (n))         /* n=0-31:  FIQ (vs IRQ) */
-#  define INTC_IRQ_SEL0(n)           (1 << (n))         /* n=0-31:  FIQ (vs IRQ) */
-#  define INTC_IRQ_SEL1(n)           (1 << ((n) - 32))  /* n=32-63: FIQ (vs IRQ) */
-#  define INTC_IRQ_SEL2(n)           (1 << ((n) - 64))  /* n=64-95: FIQ (vs IRQ) */
+#define INTC_IRQ_SEL(n)              (1 << ((n) & 0x1f)) /* n=0-95:  FIQ (vs IRQ) */
 
 /* Interrupt Enable 0-2 */
 
-#define INTC_EN(n)                   (1 << (n))         /* n=0-31:  Interrupt enable */
-#  define INTC_EN0(n)                (1 << (n))         /* n=0-31:  Interrupt enable */
-#  define INTC_EN1(n)                (1 << ((n) - 32))  /* n=32-63: Interrupt enable */
-#  define INTC_EN2(n)                (1 << ((n) - 64))  /* n=64-95: Interrupt enable */
+#define INTC_EN(n)                   (1 << ((n) & 0x1f)) /* n=0-95:  Interrupt enable */
 
 /* Interrupt Mask 0-2 */
 
-#define INTC_MASK(n)                 (1 << (n))         /* n=0-31:  Interrupt mask */
-#  define INTC_MASK0(n)              (1 << (n))         /* n=0-31:  Interrupt mask */
-#  define INTC_MASK1(n)              (1 << ((n) - 32))  /* n=32-63: Interrupt mask */
-#  define INTC_MASK2(n)              (1 << ((n) - 64))  /* n=64-95: Interrupt mask */
+#define INTC_MASK(n)                 (1 << ((n) & 0x1f)) /* n=0-95:  Interrupt mask */
 
 /* Interrupt Response 0-2 */
 
-#define INTC_RESP(n)                 (1 << (n))         /* n=0-31:  Interrupt level mask */
-#  define INTC_RESP0(n)              (1 << (n))         /* n=0-31:  Interrupt level mask */
-#  define INTC_RESP1(n)              (1 << ((n) - 32))  /* n=32-63: Interrupt level mask */
-#  define INTC_RESP2(n)              (1 << ((n) - 64))  /* n=64-95: Interrupt level mask */
+#define INTC_RESP(n)                 (1 << ((n) & 0x1f)) /* n=0-95:  Interrupt level mask */
 
 /* Interrupt Fast Forcing 0-2 */
 
-#define INTC_FF(n)                   (1 << (n))         /* n=0-31:  Enable fast forcing feature */
-#  define INTC_FF0(n)                (1 << (n))         /* n=0-31:  Enable fast forcing feature */
-#  define INTC_FF1(n)                (1 << ((n) - 32))  /* n=32-63: Enable fast forcing feature */
-#  define INTC_FF2(n)                (1 << ((n) - 64))  /* n=64-95: Enable fast forcing feature */
+#define INTC_FF(n)                   (1 << ((n) & 0x1f)) /* n=0-95:  Enable fast forcing feature */
 
 /* Interrupt Source Priority 0-4 */
 
 #define INTC_PRIO_MIN                0
 #define INTC_PRIO_MAX                3
 
-#define INTC_PRIO_SHIFT(n)           ((n) << 1)         /* n=0-15: Priority level */
+#define INTC_PRIO_SHIFT(n)           (((n) & 15) << 1)   /* n=0-95: Priority level */
 #define INTC_PRIO_MASK(n)            (3 << INTC_PRIO_SHIFT(n))
 #  define INTC_PRIO(n,p)             ((uint32_t)(p) << INTC_PRIO_SHIFT(n))
-
-#define INTC_PRIO0_SHIFT(n)          ((n) << 1)         /* n=0-15: Priority level */
-#define INTC_PRIO0_MASK(n)           (3 << INTC_PRIO0_SHIFT(n))
-#  define INTC_PRIO0(n,p)            ((uint32_t)(p) << INTC_PRIO0_SHIFT(n))
-
-#define INTC_PRIO1_SHIFT(n)          (((n) - 16) << 1)  /* n=16-31: Priority level */
-#define INTC_PRIO1_MASK(n)           (3 << INTC_PRIO1_SHIFT(n))
-#  define INTC_PRIO1(n,p)            ((uint32_t)(p) << INTC_PRIO1_SHIFT(n))
-
-#define INTC_PRIO2_SHIFT(n)          (((n) - 32) << 1)  /* n=32-47: Priority level */
-#define INTC_PRIO2_MASK(n)           (3 << INTC_PRIO2_SHIFT(n))
-#  define INTC_PRIO2(n,p)            ((uint32_t)(p) << INTC_PRIO2_SHIFT(n))
-
-#define INTC_PRIO3_SHIFT(n)          (((n) - 48) << 1)  /* n=48-63: Priority level */
-#define INTC_PRIO3_MASK(n)           (3 << INTC_PRIO3_SHIFT(n))
-#  define INTC_PRIO3(n,p)            ((uint32_t)(p) << INTC_PRIO3_SHIFT(n))
-
-#define INTC_PRIO4_SHIFT(n)          (((n) - 64) << 1)  /* n=64-79: Priority level */
-#define INTC_PRIO4_MASK(n)           (3 << INTC_PRIO4_SHIFT(n))
-#  define INTC_PRIO4(n,p)            ((uint32_t)(p) << INTC_PRIO4_SHIFT(n))
 
 #endif /* __ARCH_ARM_SRC_A1X_CHIP_A1X_INTC_H */
