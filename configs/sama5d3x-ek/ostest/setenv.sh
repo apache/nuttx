58854838311e6edae95fcaa36fad1c8b44d714be
Gregory Nutt <gnutt@nuttx.org>
2013-12-26 10:01:14 -0600

    default buildroot path is build_arm, not build_arm_nofpu

--- configs/sama5d3x-ek/ostest/setenv.sh
+++ configs/sama5d3x-ek/ostest/setenv.sh
@@ -68,7 +68,7 @@ export TOOLCHAIN_BIN="/cygdrive/c/Program Files (x86)/CodeSourcery/Sourcery_Code
 
 # This is the Cygwin path to the location where I build the buildroot
 # toolchain.
-#export TOOLCHAIN_BIN="${WD}/../misc/buildroot/build_arm_nofpu/staging_dir/bin"
+#export TOOLCHAIN_BIN="${WD}/../misc/buildroot/build_arm/staging_dir/bin"
 
 # Add the path to the toolchain to the PATH varialble
 export PATH="${TOOLCHAIN_BIN}:/sbin:/usr/sbin:${PATH_ORIG}"
