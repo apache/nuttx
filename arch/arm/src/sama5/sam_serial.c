c6685c7f7d5d1827b1d996a9b91703682cf34afc
Gregory Nutt <gnutt@nuttx.org>
2014-04-02 09:02:15 -0600

    Fix typo introduced to the serial driver when the DBGU driver was added

--- arch/arm/src/sama5/sam_serial.c
+++ arch/arm/src/sama5/sam_serial.c
@@ -1,7 +1,7 @@
 /****************************************************************************
  * arch/arm/src/sama5/sam_serial.c
  *
- *   Copyright (C) 2013 Gregory Nutt. All rights reserved.
+ *   Copyright (C) 2013-2014 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>
  *
  * Redistribution and use in source and binary forms, with or without
@@ -174,7 +174,6 @@
 
 #ifdef USE_SERIALDRIVER
 
-#undef HAVE_UART_CONSOLE
 #undef TTYS1_DEV
 #undef TTYS2_DEV
 #undef TTYS3_DEV
