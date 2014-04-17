6d0863b657c77c58e378f403a1878fc9ccbe388a
Gregory Nutt <gnutt@nuttx.org>
2014-04-17 11:22:46 -0600

    Add logic to register HID keybaord and mouse classes; fix some conditional compilation.  From Leo

--- include/nuttx/usb/usbhost.h
+++ include/nuttx/usb/usbhost.h
@@ -798,6 +798,7 @@ int usbhost_registerclass(struct usbhost_registry_s *class);
 
 const struct usbhost_registry_s *usbhost_findclass(const struct usbhost_id_s *id);
 
+#ifdef CONFIG_USBHOST_MSC
 /****************************************************************************
  * Name: usbhost_storageinit
  *
@@ -816,7 +817,9 @@ const struct usbhost_registry_s *usbhost_findclass(const struct usbhost_id_s *id
  ****************************************************************************/
 
 int usbhost_storageinit(void);
+#endif
 
+#ifdef CONFIG_USBHOST_HIDKBD
 /****************************************************************************
  * Name: usbhost_kbdinit
  *
@@ -835,7 +838,9 @@ int usbhost_storageinit(void);
  ****************************************************************************/
 
 int usbhost_kbdinit(void);
+#endif
 
+#ifdef CONFIG_USBHOST_HIDMOUSE
 /****************************************************************************
  * Name: usbhost_mouse_init
  *
@@ -854,6 +859,7 @@ int usbhost_kbdinit(void);
  ****************************************************************************/
 
 int usbhost_mouse_init(void);
+#endif
 
 /****************************************************************************
  * Name: usbhost_wlaninit
