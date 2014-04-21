c4d5a30824235ad36601ec0c25ecd1000d4e64d3
Gregory Nutt <gnutt@nuttx.org>
2014-04-21 17:49:40 -0600

    SAM4S Xplained Pro:  Added card detect, automout procfs.  From Bob Doison

--- configs/sam4s-xplained-pro/src/sam_boot.c
+++ configs/sam4s-xplained-pro/src/sam_boot.c
@@ -60,7 +60,7 @@
  *
  * Description:
  *   All SAM3U architectures must provide the following entry point.  This entry point
- *   is called early in the intitialization -- after all memory has been configured
+ *   is called early in the initialization -- after all memory has been configured
  *   and mapped but before any devices have been initialized.
  *
  ************************************************************************************/
