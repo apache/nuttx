9d7827917346a7d7b53c0e3a5719b2b472d5f3d3
Gregory Nutt <gnutt@nuttx.org>
2014-06-15 12:55:48 -0600

    Several other interrupt button implementations had the same problem as did the sam4s-xplained

--- configs/sam4l-xplained/src/sam_buttons.c
+++ configs/sam4l-xplained/src/sam_buttons.c
@@ -142,11 +142,24 @@ xcpt_t board_button_irq(int id, xcpt_t irqhandler)
       oldhandler = *g_irqsw0;
       *g_irqsw0 = irqhandler;
 
-      /* Configure the interrupt */
+      /* Are we attaching or detaching? */
 
-      sam_gpioirq(IRQ_SW0);
-      (void)irq_attach(IRQ_SW0, irqhandler);
-      sam_gpioirqenable(IRQ_SW0);
+      if (irqhandler != NULL)
+        {
+          /* Configure the interrupt */
+
+          sam_gpioirq(IRQ_SW0);
+          (void)irq_attach(IRQ_SW0, irqhandler);
+          sam_gpioirqenable(IRQ_SW0);
+        }
+      else
+        {
+          /* Detach and disable the interrupt */
+
+          (void)irq_detach(IRQ_SW0)
+          sam_gpioirqdisable(IRQ_SW0);
+        }
+
       irqrestore(flags);
     }
 
