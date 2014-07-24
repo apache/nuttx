8718dad9c867046e37ee8847f0e06e80e748b060
Gregory Nutt <gnutt@nuttx.org>
2014-07-24 16:42:15 -0600

    Oops, should have been NVIC_SYSH_PRIORITY_DEFAULT

--- arch/arm/src/armv7-m/up_initialstate.c
+++ arch/arm/src/armv7-m/up_initialstate.c
@@ -157,7 +157,7 @@ void up_initial_state(struct tcb_s *tcb)
 #else /* CONFIG_SUPPRESS_INTERRUPTS */
 
 #ifdef CONFIG_ARMV7M_USEBASEPRI
-  xcp->regs[REG_BASEPRI] = NVIC_SYSH_MAXNORMAL_PRIORITY;
+  xcp->regs[REG_BASEPRI] = NVIC_SYSH_PRIORITY_DEFAULT;
 #endif
 
 #endif /* CONFIG_SUPPRESS_INTERRUPTS */
