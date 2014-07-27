8e20aae28fb80005fd5481a6066e5eb2c08459b9
Gregory Nutt <gnutt@nuttx.org>
2014-07-26 18:47:33 -0600

    Change naming from cp_XYZ_cache() to arch_XYP_cache() so that all cache operations will pick up L2 support if it is enabled

--- configs/sama5d3x-ek/src/nor_main.c
+++ configs/sama5d3x-ek/src/nor_main.c
@@ -187,8 +187,8 @@ int nor_main(int argc, char *argv)
 
   /* Invalidate caches and TLBs */
 
-  cp15_invalidate_icache();
-  cp15_invalidate_dcache_all();
+  arch_invalidate_icache();
+  arch_invalidate_dcache_all();
   cp15_invalidate_tlbs();
 
   /* Then jump into NOR flash */
