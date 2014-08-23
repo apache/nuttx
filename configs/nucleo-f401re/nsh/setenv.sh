31a24897296290a76e7852d54a9fd0691b547637
Gregory Nutt <gnutt@nuttx.org>
2014-08-23 12:43:21 -0600

    gran_reserve():  Add a new function to reserve unallocatable regions in the granule heap

--- configs/nucleo-f401re/nsh/setenv.sh
+++ configs/nucleo-f401re/nsh/setenv.sh
@@ -52,12 +52,13 @@ fi
 # the CodeSourcery toolchain in any other location
 #export TOOLCHAIN_BIN="/cygdrive/c/Program Files (x86)/CodeSourcery/Sourcery G++ Lite/bin"
 export TOOLCHAIN_BIN="/cygdrive/c/Program Files (x86)/CodeSourcery/Sourcery_CodeBench_Lite_for_ARM_EABI/bin"
+#export TOOLCHAIN_BIN="/cygdrive/c/Users/MyName/MentorGraphics/Sourcery_CodeBench_Lite_for_ARM_EABI/bin"
 
 # This the Cygwin path to the location where I build the buildroot
 # toolchain.
 #export TOOLCHAIN_BIN="${WD}/../misc/buildroot/build_arm_nofpu/staging_dir/bin"
 
 # Add the path to the toolchain to the PATH varialble
-#export PATH="${TOOLCHAIN_BIN}:/sbin:/usr/sbin:${PATH_ORIG}"
+export PATH="${TOOLCHAIN_BIN}:/sbin:/usr/sbin:${PATH_ORIG}"
 
 echo "PATH : ${PATH}"
