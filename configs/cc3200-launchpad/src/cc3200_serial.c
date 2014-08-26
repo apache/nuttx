f62a65befc6b6939b72e22b76289ef89b86748f7
Gregory Nutt <gnutt@nuttx.org>
2014-08-26 15:36:02 -0600

    Add basic support for the CC3200 Launchpad.  From Jim Ewing

---
+++ configs/cc3200-launchpad/src/cc3200_serial.c
@@ -0,0 +1,124 @@
+/************************************************************************************
+ * configs/cc3200/src/cc3200_serial.c
+ *
+ *   Copyright (C) 2013 Droidifi LLC. All rights reserved.
+ *   Author: Jim Ewing <jim@droidifi.com>
+ *
+ *   Adapted for the cc3200 from code:
+ *
+ *   Copyright (C) Gregory Nutt.
+ *   Gregory Nutt <gnutt@nuttx.org>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ************************************************************************************/
+
+/************************************************************************************
+ * Included Files
+ ************************************************************************************/
+
+#include <nuttx/config.h>
+#include <nuttx/arch.h>
+#include <cc3200_utils.h>
+#include "chip/cc3200_memorymap.h"
+#include "up_internal.h"
+
+#if !defined(HAVE_SERIALCONSOLE)
+
+/************************************************************************************
+ * Private Data
+ ************************************************************************************/
+
+/************************************************************************************
+ * Private Functions
+ ************************************************************************************/
+void cc3200_uart0_init(void)
+{
+  HWREG(0x44025048) |= 0x01;
+
+  cc3200_pin_type_uart(PIN_55, PIN_MODE_3);
+  cc3200_pin_type_uart(PIN_57, PIN_MODE_3);
+
+  while(HWREG(0x4000C018) & 0x08)
+    {
+    }
+
+  HWREG(0x4000C02C) &= ~(0x00000010);
+  HWREG(0x4000C030) &= ~(0x01 | 0x100 | 0x200);
+  HWREG(0x4000C030) &= ~(0x20);
+
+  HWREG(0x4000C024)  = ((((80000000 * 8) / 115200) + 1) / 2) / 64;
+  HWREG(0x4000C028)  = ((((80000000 * 8) / 115200) + 1) / 2) % 64;
+
+  HWREG(0x4000C02C)  = (0x60 | 0x82 | 0x10);
+  HWREG(0x4000C030) |= (0x01 | 0x100 | 0x200);
+}
+
+/************************************************************************************
+ * Public Functions
+ ************************************************************************************/
+
+/************************************************************************************
+ * Name: up_earlyconsoleinit
+ *
+ * Description:
+ *   Performs the low level UART initialization early in  debug so that the serial
+ *   console will be available during bootup.  This must be called before
+ *   up_consoleinit.
+ *
+ ************************************************************************************/
+
+#ifdef USE_EARLYSERIALINIT
+void up_earlyconsoleinit(void)
+{
+  cc3200_init();
+  cc3200_uart0_init();
+}
+#endif
+
+/************************************************************************************
+ * Name: up_consoleinit
+ *
+ * Description:
+ *   Register serial console and serial ports.  This assumes that
+ *   up_earlyconsoleinit was called previously.
+ *
+ ************************************************************************************/
+
+#if USE_SERIALDRIVER
+void up_consoleinit(void)
+{
+  /* There is probably a problem if we are here */
+
+  lowconsole_init();
+}
+#endif
+
+void cc3200_uart_init(void)
+{
+  cc3200_uart0_init();
+}
+
+#endif /* !HAVE_SERIALCONSOLE && CONFIG_ARCH_LCD */
---
+++ configs/cc3200-launchpad/src/cc3200_utils.c
@@ -0,0 +1,136 @@
+/************************************************************************************
+ * configs/cc3200/src/cc3200_util.c
+ *
+ *   Copyright (C) 2014 Droidifi LLC. All rights reserved.
+ *   Author: Jim Ewing <jim@droidifi.com>
+ *
+ *   Adapted from code Copyright (C) 2014 Texas Instruments Incorporated
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ************************************************************************************/
+
+#include <sys/types.h>
+#include <cc3200_utils.h>
+
+void cc3200_putc(char c)
+{
+  while(HWREG(0x4000C000 + 0x00000018) & 0x00000020)
+    ;
+
+  HWREG(0x4000C000) = c;
+}
+
+char cc3200_getc(void)
+{
+  if (!(HWREG(0x4000C000 + 0x00000018) & 0x00000010))
+    {
+      return HWREG(0x4000C000);
+    }
+  else
+    {
+      return -1;
+    }
+}
+
+void cc3200_print(char* str)
+{
+  while (str && *str != '\0')
+    {
+      cc3200_putc(*str++);
+    }
+}
+
+void cc3200_pin_config_set(uint32_t pin, uint32_t pin_strength, uint32_t pin_type)
+{
+  uint32_t pad;
+
+  pad = g_cc3200_pinmap[pin & 0x3F];
+
+  switch (pin_type)
+    {
+    case PIN_TYPE_ANALOG:
+      HWREG(0x4402E144) |= ((0x80 << pad) & (0x1E << 8));
+      pad = ((pad << 2) + PAD_CONFIG_BASE);
+      HWREG(pad) |= 0xC00;
+      break;
+
+    default:
+      HWREG(0x4402E144) &= ~((0x80 << pad) & (0x1E << 8));
+      pad = ((pad << 2) + PAD_CONFIG_BASE);
+      HWREG(pad) = ((HWREG(pad) & ~(PAD_STRENGTH_MASK | PAD_TYPE_MASK)) | (pin_strength | pin_type ));
+      break;
+    }
+}
+
+void cc3200_pin_mode_set(uint32_t pin, uint32_t pin_mode)
+{
+  uint32_t pad;
+
+  pad = g_cc3200_pinmap[pin & 0x3F];
+  pad = ((pad << 2) + PAD_CONFIG_BASE);
+  HWREG(pad) = (((HWREG(pad) & ~PAD_MODE_MASK) |  pin_mode) & ~(3<<10));
+}
+
+void cc3200_pin_type_uart(uint32_t pin, uint32_t pin_mode)
+{
+  cc3200_pin_mode_set(pin, pin_mode);
+  cc3200_pin_config_set(pin, PIN_STRENGTH_2MA, PIN_TYPE_STD);
+}
+
+void cc3200_init(void)
+{
+  HWREG(0x4402F064) |= 0x800000;
+  HWREG(0x4402F800  + 0x00000418) |= (1<<4);
+  HWREG(0x4402E16C) |= 0x3C;
+  HWREG(0x4402F804) = 0x0;
+  HWREG(0x4402F804) = 0x0;
+
+  if (((HWREG(0x4402F0C8) & 0xFF) == 0x2))
+    {
+      HWREG(0x4402E110) = ((HWREG(0x4402E110) & ~0xC0F) | 0x2);
+      HWREG(0x4402E114) = ((HWREG(0x4402E110) & ~0xC0F) | 0x2);
+      HWREG(0x4402E184) |= 0x2;
+    }
+
+  if ((HWREG(0x4402E0A4) & 0xF) == 0x1)
+    {
+      HWREG(0x4402E0A4) = ((HWREG(0x4402E0A4) & ~0xF));
+    }
+
+  if ((HWREG(0x4402E0A8) & 0xF) == 0x1)
+    {
+      HWREG(0x4402E0A8) = ((HWREG(0x4402E0A8) & ~0xF));
+    }
+
+  if (((HWREG(0x4402DC78) >> 22) & 0xF) == 0xE)
+    {
+      HWREG(0x4402F0B0) = ((HWREG(0x4402F0B0) & ~(0x00FC0000))|(0x32 << 18));
+    }
+  else
+    {
+      HWREG(0x4402F0B0) = ((HWREG(0x4402F0B0) & ~(0x00FC0000))|(0x29 << 18));
+    }
+}
