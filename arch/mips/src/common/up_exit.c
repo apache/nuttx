9bc9d2d86d9f454f5e3ccb064559d00f8c89d627
Gregory Nutt <gnutt@nuttx.org>
2014-08-27 16:31:02 -0600

    Add ADDRENV support to ALL implmentations of _exit()

--- arch/mips/src/common/up_exit.c
+++ arch/mips/src/common/up_exit.c
@@ -1,7 +1,7 @@
 /****************************************************************************
  * arch/mips/src/common/up_exit.c
  *
- *   Copyright (C) 2011, 2013 Gregory Nutt. All rights reserved.
+ *   Copyright (C) 2011, 2013-2014 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>
  *
  * Redistribution and use in source and binary forms, with or without
@@ -51,6 +51,7 @@
 
 #include "task/task.h"
 #include "sched/sched.h"
+#include "group/group.h"
 #include "up_internal.h"
 
 /****************************************************************************
@@ -163,6 +164,16 @@ void _exit(int status)
    */
 
   tcb = (struct tcb_s*)g_readytorun.head;
+
+#ifdef CONFIG_ARCH_ADDRENV
+  /* Make sure that the address environment for the previously running
+   * task is closed down gracefully (data caches dump, MMU flushed) and
+   * set up the address environment for the new thread at the head of
+   * the ready-to-run list.
+   */
+
+  (void)group_addrenv(tcb);
+#endif
 
   /* Then switch contexts */
 
