70e5350942b870e9dad54007b87c0375c6448c66
Gregory Nutt <gnutt@nuttx.org>
2014-09-04 10:28:38 -0600

    Mostly cosmetic changes

--- arch/arm/src/armv7-a/arm_schedulesigaction.c
+++ arch/arm/src/armv7-a/arm_schedulesigaction.c
@@ -75,7 +75,7 @@
  *   This function is called by the OS when one or more
  *   signal handling actions have been queued for execution.
  *   The architecture specific code must configure things so
- *   that the 'igdeliver' callback is executed on the thread
+ *   that the 'sigdeliver' callback is executed on the thread
  *   specified by 'tcb' as soon as possible.
  *
  *   This function may be called from interrupt handling logic.
