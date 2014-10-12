541105fe5f3280ef75d31ac15455abc0ef3c57bb
Gregory Nutt <gnutt@nuttx.org>
2014-10-12 09:41:08 -0600

    Update STM3240G-EVAL nxwm and knxwm configurations

--- configs/stm3240g-eval/nsh2/setenv.sh
+++ configs/stm3240g-eval/nsh2/setenv.sh
@@ -56,6 +56,7 @@ fi
 # toolchain under windows.  You will also have to edit this if you install
 # the CodeSourcery toolchain in any other location
 export TOOLCHAIN_BIN="/cygdrive/c/Program Files (x86)/CodeSourcery/Sourcery G++ Lite/bin"
+#export TOOLCHAIN_BIN="/cygdrive/c/Users/MyName/MentorGraphics/Sourcery_CodeBench_Lite_for_ARM_EABI/bin"
 
 # These are the Cygwin paths to the locations where I installed the Atollic
 # toolchain under windows.  You will also have to edit this if you install
