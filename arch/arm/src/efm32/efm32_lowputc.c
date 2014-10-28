28ac9e5841dd3597d426f42d29ac8160cc10faa7
Gregory Nutt <gnutt@nuttx.org>
2014-10-28 12:42:19 -0600

    Oops... fix an error in an assertion of last commit

--- arch/arm/src/efm32/efm32_lowputc.c
+++ arch/arm/src/efm32/efm32_lowputc.c
@@ -212,7 +212,7 @@ static void efm32_uart_setbaud(uintptr_t base,  uint32_t baud)
   putreg32(regval, base + EFM32_USART_CTRL_OFFSET);
 
   regval = (uint32_t)clkdiv << _USART_CLKDIV_DIV_SHIFT;
-  DEBUGASSERT((regval & _USART_CLKDIV_MASK) == clkdiv);
+  DEBUGASSERT((regval & _USART_CLKDIV_MASK) == regval);
   putreg32(regval, base + EFM32_USART_CLKDIV_OFFSET);
 }
 #endif
