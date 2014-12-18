dc0231c2b6abdf0a4e00cd87063c85527df74275
Gregory Nutt <gnutt@nuttx.org>
2014-12-18 13:56:15 -0600

    Fix a couple of spelling errors in comments

--- configs/freedom-kl25z/src/kl_boardinitialize.c
+++ configs/freedom-kl25z/src/kl_boardinitialize.c
@@ -64,7 +64,7 @@
  *
  * Description:
  *   All K25Z architectures must provide the following entry point.  This entry point
- *   is called early in the intitialization -- after all memory has been configured
+ *   is called early in the initialization -- after all memory has been configured
  *   and mapped but before any devices have been initialized.
  *
  ************************************************************************************/
@@ -84,7 +84,7 @@ void kl_boardinitialize(void)
 
   /* Initialize USB if the 1) USB device controller is in the configuration
    * and 2) disabled, and 3) the weak function kl_usbinitialize() has been
-   * brought into the build. Presumeably either CONFIG_USBHOST or
+   * brought into the build. Presumably either CONFIG_USBHOST or
    * CONFIG_USBDEV is also selected.
    */
 
