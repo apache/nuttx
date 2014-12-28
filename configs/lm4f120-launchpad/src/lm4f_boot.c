febd316186bb285d85373305060784aa0ebc5e96
Gregory Nutt <gnutt@nuttx.org>
2014-12-28 16:55:47 -0600

    Tiva SSI: Fix some recent breakage to the Tiva SSI driver for the case where only one SSI modules is enabled

--- configs/lm4f120-launchpad/src/lm4f_boot.c
+++ configs/lm4f120-launchpad/src/lm4f_boot.c
@@ -49,7 +49,7 @@
 #include "lmf4120-launchpad.h"
 
 /************************************************************************************
- * Definitions
+ * Pre-processor Definitions
  ************************************************************************************/
 
 /************************************************************************************
@@ -65,7 +65,7 @@
  *
  * Description:
  *   All Stellaris architectures must provide the following entry point.  This entry
- *   point is called early in the intitialization -- after all memory has been
+ *   point is called early in the initialization -- after all memory has been
  *   configured and mapped but before any devices have been initialized.
  *
  ************************************************************************************/
