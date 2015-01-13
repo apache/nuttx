9c3dce06e1f9a49fd031d53d17085c7e36e561ea
Gregory Nutt <gnutt@nuttx.org>
2015-01-13 11:10:35 -0600

    DK-TM3C129X Timer:  Add timer initialization logic to the board bring-up

--- configs/dk-tm4c129x/src/tm4c_bringup.c
+++ configs/dk-tm4c129x/src/tm4c_bringup.c
@@ -49,6 +49,14 @@
  * Pre-Processor Definitions
  ****************************************************************************/
 
+#if defined(CONFIG_I2C) && defined(CONFIG_I2C_LM75) && defined(CONFIG_TIVA_I2C6)
+#  define HAVE_TMP100
+#endif
+
+#ifdef CONFIG_DK_TM4C129X_TIMER
+#  define HAVE_TIMER
+#endif
+
 #ifdef CONFIG_SYSTEM_LM75_DEVNAME
 #  define TMP100_DEVNAME CONFIG_SYSTEM_LM75_DEVNAME
 #else
@@ -69,15 +77,27 @@
 
 int tm4c_bringup(void)
 {
-#if defined(CONFIG_I2C) && defined(CONFIG_I2C_LM75) && defined(CONFIG_TIVA_I2C6)
+#if defined(HAVE_TMP100) || defined(HAVE_TIMER)
   int ret;
+#endif
 
+#ifdef HAVE_TMP100
   /* Initialize and register the TMP-100 Temperature Sensor driver. */
 
   ret = tiva_tmp100_initialize(TMP100_DEVNAME);
   if (ret < 0)
     {
       dbg("ERROR: Failed to initialize TMP100 driver: %d\n", ret);
+    }
+#endif
+
+#ifdef HAVE_TIMER
+  /* Initialize the timer driver */
+
+  ret = tiva_timer_initialize();
+  if (ret < 0)
+    {
+      dbg("ERROR: Failed to initialize timer driver: %d\n", ret);
     }
 #endif
 
