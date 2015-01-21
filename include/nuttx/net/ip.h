38e0dcf0979bb521c5d512c2c31d3f7a04232c3d
Gregory Nutt <gnutt@nuttx.org>
2015-01-21 14:11:20 -0600

    Networking:  Fix another error in an IPv6 macro. Again ampersand where there should not be one

--- include/nuttx/net/ip.h
+++ include/nuttx/net/ip.h
@@ -302,7 +302,7 @@ struct ipv6_hdr_s
 
 #ifdef CONFIG_NET_IPv6
 #  define net_ipv6addr_cmp(addr1, addr2) \
-   (memcmp(&addr1, &addr2, sizeof(net_ipv6addr_t)) == 0)
+   (memcmp(addr1, addr2, sizeof(net_ipv6addr_t)) == 0)
 #  define net_ipv6addr_hdrcmp(addr1, addr2) \
    net_ipv6addr_cmp(addr1, addr2)
 #endif
