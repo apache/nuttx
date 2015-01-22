0d5c5f6dcaa03c1f35b0d9a3613b6e82da9d2e55
Gregory Nutt <gnutt@nuttx.org>
2015-01-22 10:22:47 -0600

    Armv7-M: Remove Px4-only setting of stack to 0xff.  This is incompatible with standard NuttX stack montitoring logic

--- arch/arm/src/armv7-m/up_initialstate.c
+++ arch/arm/src/armv7-m/up_initialstate.c
@@ -97,10 +97,6 @@ void up_initial_state(struct tcb_s *tcb)
   /* Set the stack limit value */
 
   xcp->regs[REG_R10]     = (uint32_t)tcb->stack_alloc_ptr + 64;
-
-  /* Fill the stack with a watermark value */
-
-  memset(tcb->stack_alloc_ptr, 0xff, tcb->adj_stack_size);
 #endif
 
   /* Save the task entry point (stripping off the thumb bit) */
