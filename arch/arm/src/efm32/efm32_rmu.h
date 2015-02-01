515856c3d9a4bbdafb35edc1a58ad2972f796155
Gregory Nutt <gnutt@nuttx.org>
2015-02-01 06:19:53 -0600

    EFM32 Add support of BURTC and add possibility of debug message of RMU: Pierre-noel Bouteville

--- arch/arm/src/efm32/efm32_rmu.h
+++ arch/arm/src/efm32/efm32_rmu.h
@@ -46,6 +46,28 @@
 #ifdef CONFIG_EFM32_RMU
 
 /****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+/* Configuration ************************************************************/
+
+#ifndef CONFIG_DEBUG
+#  undef CONFIG_DEBUG_VERBOSE
+#  undef CONFIG_EFM32_RMU_DEBUG
+#endif
+
+#ifdef CONFIG_EFM32_RMU_DEBUG
+#  define rmudbg lldbg
+#  ifdef CONFIG_DEBUG_VERBOSE
+#    define rmuvdbg lldbg
+#  else
+#    define rmuvdbg(x...)
+#  endif
+#else
+#  define rmudbg(x...)
+#  define rmuvdbg(x...)
+#endif
+
+/****************************************************************************
  * Public Data
  ****************************************************************************/
 
@@ -63,6 +85,23 @@ extern uint32_t g_efm32_rstcause;
  ************************************************************************************/
 
 void efm32_rmu_initialize(void);
+
+/************************************************************************************
+ * Name: efm32_reset_cause_list_str
+ *
+ * Description:
+ *    Return next reset cause string, NULL if no more reset cause.
+ *
+ * Input Parmeters:
+ *   reg: reset cause register to decode (like g_efm32_rstcause)
+ *   idx: Use to keep in maind reset cause decoding position.
+ *        set *idx to zero before first call.
+ *
+ ************************************************************************************/
+
+#ifdef CONFIG_EFM32_RMU_DEBUG
+const char *efm32_reset_cause_list_str(uint32_t reg, unsigned int *idx);
+#endif
 
 #endif /* CONFIG_EFM32_RMU */
 #endif /* __ARCH_ARM_SRC_EFM32_EFM32_RMU_H */
