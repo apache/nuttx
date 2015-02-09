25f187d7545ec928c9436a5f97edf316e6821513
Gregory Nutt <gnutt@nuttx.org>
2015-02-09 15:24:31 -0600

    ARMv7-A interrupt handler:  Should not automatically re-enable interrupts on interrupt return.  That interferes with the driver's ability to manage interrupts.

--- arch/arm/src/armv7-a/arm_doirq.c
+++ arch/arm/src/armv7-a/arm_doirq.c
@@ -87,10 +87,6 @@ uint32_t *arm_doirq(int irq, uint32_t *regs)
 
   current_regs = regs;
 
-  /* Mask and acknowledge the interrupt */
-
-  up_maskack_irq(irq);
-
   /* Deliver the IRQ */
 
   irq_dispatch(irq, regs);
@@ -129,10 +125,6 @@ uint32_t *arm_doirq(int irq, uint32_t *regs)
 
   regs         = (uint32_t *)current_regs;
   current_regs = NULL;
-
-  /* Unmask the last interrupt (global interrupts are still disabled) */
-
-  up_enable_irq(irq);
 #endif
 
   board_led_off(LED_INIRQ);
