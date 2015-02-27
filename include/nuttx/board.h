07a81485387d4650d6ada7edd1197cab9ee0cd37
Gregory Nutt <gnutt@nuttx.org>
2015-02-27 17:17:42 -0600

    included/nuttx/board.h:  Move all board common prototypes to this header file

---
+++ include/nuttx/board.h
@@ -0,0 +1,147 @@
+/****************************************************************************
+ * include/nuttx/board.h
+ *
+ *   Copyright (C) 2015 Gregory Nutt. All rights reserved.
+ *   Author: Gregory Nutt <gnutt@nuttx.org>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name NuttX nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+#ifndef __INCLUDE_NUTTX_BOARD_H
+#define __INCLUDE_NUTTX_BOARD_H
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+/****************************************************************************
+ * Public Function Prototypes
+ *
+ * These are all standard board interfaces that are exported from board-
+ * specific logic to OS internal logic.  These should never be accessed
+ * directly from application code but may be freely used within the OS
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: board_led_initialize
+ *
+ * Description:
+ *   This functions is called very early in initialization to perform board-
+ *   specific initialization of LED-related resources.  This includes such
+ *   things as, for example, configure GPIO pins to drive the LEDs and also
+ *   putting the LEDs in their correct initial state.
+ *
+ *   In most architectures, board_led_initialize() is called fromo board-
+ *   specific initialization logic.  But there are a few architectures where
+ *   this initialization function is called from shared chip logic.
+ *
+ * Input Parameters:
+ *   None
+ *
+ * Returned Value:
+ *   None
+ *
+ ****************************************************************************/
+
+#ifdef CONFIG_ARCH_LEDS
+void board_led_initialize(void);
+#else
+# define board_led_initialize()
+#endif
+
+/****************************************************************************
+ * Name: board_led_on
+ *
+ * Description:
+ *   Set the LED configuration into the ON condition for the state provided
+ *   by the led parameter.  This may be one of:
+ *
+ *     LED_STARTED       NuttX has been started
+ *     LED_HEAPALLOCATE  Heap has been allocated
+ *     LED_IRQSENABLED   Interrupts enabled
+ *     LED_STACKCREATED  Idle stack created
+ *     LED_INIRQ         In an interrupt
+ *     LED_SIGNAL        In a signal handler
+ *     LED_ASSERTION     An assertion failed
+ *     LED_PANIC         The system has crashed
+ *     LED_IDLE          MCU is in sleep mode
+ *
+ *   Where these values are defined in a board-specific way in the standard
+ *   board.h header file exported by every architecture.
+ *
+ * Input Parameters:
+ *   led - Identifies the LED state to put in the ON state (which may or may
+ *         not equate to turning an LED on)
+ *
+ * Returned Value:
+ *   None
+ *
+ ****************************************************************************/
+
+#ifdef CONFIG_ARCH_LEDS
+void board_led_on(int led);
+#else
+# define board_led_on(led)
+#endif
+
+/****************************************************************************
+ * Name: board_led_off
+ *
+ * Description:
+ *   Set the LED configuration into the OFF condition for the state provided
+ *   by the led parameter.  This may be one of:
+ *
+ *     LED_INIRQ         Leaving an interrupt
+ *     LED_SIGNAL        Leaving a signal handler
+ *     LED_ASSERTION     Recovering from an assertion failure
+ *     LED_PANIC         The system has crashed (blinking).
+ *     LED_IDLE          MCU is not in sleep mode
+ *
+ *   Where these values are defined in a board-specific way in the standard
+ *   board.h header file exported by every architecture.
+ *
+ * Input Parameters:
+ *   led - Identifies the LED state to put in the OFF state (which may or may
+ *         not equate to turning an LED off)
+ *
+ * Returned Value:
+ *   None
+ *
+ ****************************************************************************/
+
+#ifdef CONFIG_ARCH_LEDS
+void board_led_off(int led);
+#else
+# define board_led_off(led)
+#endif
+
+#endif /* __INCLUDE_NUTTX_BOARD_H */
