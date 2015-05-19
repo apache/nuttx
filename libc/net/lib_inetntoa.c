87f4f0ccdd226c21aa3ef68152c2742accea82ef
Gregory Nutt <gnutt@nuttx.org>
2015-05-19 12:07:07 -0600

    Minor fixes to apps/netutils/Kconfig and nuttx/libc/net/lib_inetntoa.c from Max Neklyudov

--- libc/net/lib_inetntoa.c
+++ libc/net/lib_inetntoa.c
@@ -65,16 +65,16 @@
 FAR char *inet_ntoa(struct in_addr in)
 {
   static char buffer[INET_ADDRSTRLEN+2];
-  FAR char *ptr = (FAR char*)&in.s_addr;
-  sprintf(buffer, "%d.%d.%d.%d", ptr[0], ptr[1], ptr[2], ptr[3]);
+  FAR unsigned char *ptr = (FAR unsigned char*)&in.s_addr;
+  sprintf(buffer, "%u.%u.%u.%u", ptr[0], ptr[1], ptr[2], ptr[3]);
   return buffer;
 }
 #else
 FAR char *_inet_ntoa(in_addr_t in)
 {
   static char buffer[INET_ADDRSTRLEN+2];
-  FAR char *ptr = (FAR char*)&in;
-  sprintf(buffer, "%d.%d.%d.%d", ptr[0], ptr[1], ptr[2], ptr[3]);
+  FAR unsigned char *ptr = (FAR unsigned char*)&in;
+  sprintf(buffer, "%u.%u.%u.%u", ptr[0], ptr[1], ptr[2], ptr[3]);
   return buffer;
 }
 #endif
