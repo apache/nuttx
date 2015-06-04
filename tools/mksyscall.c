b26dbe3ec6956816fe917a54a19d38417b449764
Gregory Nutt <gnutt@nuttx.org>
2015-06-04 09:49:52 -0600

    Trivial spacing change

--- tools/mksyscall.c
+++ tools/mksyscall.c
@@ -514,7 +514,7 @@ static void generate_stub(int nparms)
             {
               /* Always pass six arguments */
 
-              for (j = i+1; j <=6; j++)
+              for (j = i+1; j <= 6; j++)
                 {
                   fprintf(stream, ", parm%d", j);
                 }
