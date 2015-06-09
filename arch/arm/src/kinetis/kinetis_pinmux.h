13e8c0d5b171e523ad809b7e48bd94be7f7e3b04
Gregory Nutt <gnutt@nuttx.org>
2015-06-09 07:03:25 -0600

    Update/add README.txt files

--- arch/arm/src/kinetis/kinetis_pinmux.h
+++ arch/arm/src/kinetis/kinetis_pinmux.h
@@ -1,7 +1,7 @@
 /********************************************************************************************
  * arch/arm/src/kinetis/kinetis_pinmux.h
  *
- *   Copyright (C) 2011 Gregory Nutt. All rights reserved.
+ *   Copyright (C) 2011, 2015 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>
  *
  * Redistribution and use in source and binary forms, with or without
