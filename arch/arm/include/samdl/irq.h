ab277096fdd9090ae1ad7126425c7098c8bfd6a6
Gregory Nutt <gnutt@nuttx.org>
2015-06-20 14:40:47 -0600

    SAMD21: Add interrupt vectors definitions

--- arch/arm/include/samdl/irq.h
+++ arch/arm/include/samdl/irq.h
@@ -77,6 +77,8 @@
 
 #if defined(CONFIG_ARCH_FAMILY_SAMD20)
 #  include <arch/samdl/samd20_irq.h>
+#elif defined(CONFIG_ARCH_FAMILY_SAMD20)
+#  include <arch/samdl/samd21_irq.h>
 #elif defined(CONFIG_ARCH_FAMILY_SAML21)
 #  include <arch/samdl/saml21_irq.h>
 #else
