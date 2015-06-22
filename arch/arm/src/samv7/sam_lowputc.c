3751f9f7ac358cf2f9616bb7990042e20b83e912
Gregory Nutt <gnutt@nuttx.org>
2015-06-22 07:48:29 -0600

    Back out another error introduced with commit 02c33f66c5a8be774034cd40e4125e9323c7b4d8. Causes an infinite loop in up_lowputc().

--- arch/arm/src/samv7/sam_lowputc.c
+++ arch/arm/src/samv7/sam_lowputc.c
@@ -226,6 +226,8 @@ void up_lowputc(char ch)
           /* Send the character */
 
           putreg32((uint32_t)ch, SAM_CONSOLE_BASE + SAM_UART_THR_OFFSET);
+          irqrestore(flags);
+          return;
         }
 
       irqrestore(flags);
