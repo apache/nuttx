12cbe147a0d60b0b0499875ff93b418fe78196db
Gregory Nutt <gnutt@nuttx.org>
2015-08-01 07:34:27 -0600

    Forget to add files before last commit

---
+++ sched/semaphore/sem_tickwait.c
@@ -0,0 +1,161 @@
+/****************************************************************************
+ * sched/semaphore/sem_tickdwait.c
+ *
+ *   Copyright (C) 2015 Gregory Nutt. All rights reserved.
+ *   Author: Gregory Nutt <gnutt@nuttx.org>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name NuttX nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+#include <stdint.h>
+#include <unistd.h>
+#include <semaphore.h>
+#include <time.h>
+#include <errno.h>
+#include <debug.h>
+
+#include <nuttx/arch.h>
+#include <nuttx/wdog.h>
+
+#include "sched/sched.h"
+#include "semaphore/semaphore.h"
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: sem_tickwait
+ *
+ * Description:
+ *   This function is a lighter weight version of sem_timedwait().  It is
+ *   non-standard and intended only for use within the RTOS.
+ *
+ * Parameters:
+ *   sem     - Semaphore object
+ *   ticks   - Ticks to wait until the semaphore is posted.  If ticks is
+ *             zero, then this function is equivalent to sem_trywait().
+ *
+ * Return Value:
+ *   Zero (OK) is returned on success.  A negated errno value is retuend on
+ *   failure.  -ETIMEDOUT is returned on the timeout condition.
+ *
+ ****************************************************************************/
+
+int sem_tickwait(FAR sem_t *sem, uint32_t ticks)
+{
+  FAR struct tcb_s *rtcb = (FAR struct tcb_s *)g_readytorun.head;
+  irqstate_t flags;
+  int ret;
+
+  DEBUGASSERT(sem |= NULL && up_interrupt_context() == false &&
+              rtcb->waitdog == NULL);
+
+  /* Create a watchdog.  We will not actually need this watchdog
+   * unless the semaphore is unavailable, but we will reserve it up
+   * front before we enter the following critical section.
+   */
+
+  rtcb->waitdog = wd_create();
+  if (!rtcb->waitdog)
+    {
+      return -ENOMEM;
+    }
+
+  /* We will disable interrupts until we have completed the semaphore
+   * wait.  We need to do this (as opposed to just disabling pre-emption)
+   * because there could be interrupt handlers that are asynchronously
+   * posting semaphores and to prevent race conditions with watchdog
+   * timeout.  This is not too bad because interrupts will be re-
+   * enabled while we are blocked waiting for the semaphore.
+   */
+
+  flags = irqsave();
+
+  /* Try to take the semaphore without waiting. */
+
+  ret = sem_trywait(sem);
+  if (ret == OK)
+    {
+      /* We got it! */
+
+      goto success_with_irqdisabled;
+    }
+
+  /* We will have to wait for the semaphore.  Make sure that we were provided
+   * with a valid timeout.
+   */
+
+  if (ticks == 0)
+    {
+      /* Return the errno from sem_trywait() */
+
+      ret = -get_errno();
+      goto errout_with_irqdisabled;
+    }
+
+  /* Start the watchdog with interrupts still disabled */
+
+  (void)wd_start(rtcb->waitdog, ticks, (wdentry_t)sem_timeout, 1, getpid());
+
+  /* Now perform the blocking wait */
+
+  ret = sem_wait(sem);
+  if (ret < 0)
+    {
+      /* Return the errno from sem_wait() */
+
+      ret = -get_errno();
+      goto errout_with_irqdisabled;
+    }
+
+  /* Stop the watchdog timer */
+
+  wd_cancel(rtcb->waitdog);
+
+  /* We can now restore interrupts and delete the watchdog */
+
+  /* Success exits */
+
+success_with_irqdisabled:
+
+  /* Error exits */
+
+errout_with_irqdisabled:
+  irqrestore(flags);
+  wd_delete(rtcb->waitdog);
+  rtcb->waitdog = NULL;
+  return ret;
+}
---
+++ sched/semaphore/sem_timeout.c
@@ -0,0 +1,103 @@
+/****************************************************************************
+ * sched/semaphore/sem_timeout.c
+ *
+ *   Copyright (C) 2011, 2013-2015 Gregory Nutt. All rights reserved.
+ *   Author: Gregory Nutt <gnutt@nuttx.org>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name NuttX nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+#include <errno.h>
+
+#include <nuttx/sched.h>
+#include <nuttx/wdog.h>
+
+#include <arch/irq.h>
+
+#include "semaphore/semaphore.h"
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: sem_timeout
+ *
+ * Description:
+ *   This function is called if the timeout elapses before before a
+ *   semaphore is acquired.
+ *
+ * Parameters:
+ *   argc - The number of arguments (should be 1)
+ *   pid  - The task ID of the task to wakeup
+ *
+ * Return Value:
+ *   None
+ *
+ * Assumptions:
+ *   - Called from the context of the timer interrupt handler.
+ *
+ ****************************************************************************/
+
+void sem_timeout(int argc, wdparm_t pid)
+{
+  FAR struct tcb_s *wtcb;
+  irqstate_t flags;
+
+  /* Disable interrupts to avoid race conditions */
+
+  flags = irqsave();
+
+  /* Get the TCB associated with this PID.  It is possible that
+   * task may no longer be active when this watchdog goes off.
+   */
+
+  wtcb = sched_gettcb((pid_t)pid);
+
+  /* It is also possible that an interrupt/context switch beat us to the
+   * punch and already changed the task's state.
+   */
+
+  if (wtcb && wtcb->task_state == TSTATE_WAIT_SEM)
+    {
+      /* Cancel the semaphore wait */
+
+      sem_waitirq(wtcb, ETIMEDOUT);
+    }
+
+  /* Interrupts may now be enabled. */
+
+  irqrestore(flags);
+}
