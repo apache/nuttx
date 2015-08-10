c74dc5f83f3199b55088a9022aa3ed4b08480265
Gregory Nutt <gnutt@nuttx.org>
2015-08-10 10:38:41 -0600

    Changes from review epoll() implementation for consistency with NuttX naming and coding style

--- include/sys/epoll.h
+++ include/sys/epoll.h
@@ -1,7 +1,58 @@
-#ifndef _EPOLL__H
-#define _EPOLL__H
+/****************************************************************************
+ * fs/vfs/fs_epoll.c
+ *
+ *   Copyright (C) 2015 Anton D. Kachalov. All rights reserved.
+ *   Author: Anton D. Kachalov <mouse@mayc.ru>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name NuttX nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+#ifndef __INCLUDE_NUTTX_SYS_EPOLL_H
+#define __INCLUDE_NUTTX_SYS_EPOLL_H
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
 
 #include <poll.h>
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+#define EPOLL_CTL_ADD 1 /* Add a file descriptor to the interface.  */
+#define EPOLL_CTL_DEL 2 /* Remove a file descriptor from the interface.  */
+#define EPOLL_CTL_MOD 3 /* Change file descriptor epoll_event structure.  */
+
+/****************************************************************************
+ * Public Types
+ ****************************************************************************/
 
 enum EPOLL_EVENTS
   {
@@ -25,19 +76,15 @@ enum EPOLL_EVENTS
 #define EPOLLHUP EPOLLHUP
   };
 
-#define EPOLL_CTL_ADD 1 /* Add a file descriptor to the interface.  */
-#define EPOLL_CTL_DEL 2 /* Remove a file descriptor from the interface.  */
-#define EPOLL_CTL_MOD 3 /* Change file descriptor epoll_event structure.  */
-
 typedef union poll_data
 {
-  int         fd;       /* The descriptor being polled */
+  int          fd;       /* The descriptor being polled */
 } epoll_data_t;
 
 struct epoll_event
 {
   epoll_data_t data;
-  sem_t       *sem;      /* Pointer to semaphore used to post output event */
+  FAR sem_t   *sem;      /* Pointer to semaphore used to post output event */
   pollevent_t  events;   /* The input event flags */
   pollevent_t  revents;  /* The output event flags */
   FAR void    *priv;     /* For use by drivers */
@@ -47,8 +94,12 @@ struct epoll_head
 {
   int size;
   int occupied;
-  struct epoll_event *evs;
+  FAR struct epoll_event *evs;
 };
+
+/****************************************************************************
+ * Public Function Prototypes
+ ****************************************************************************/
 
 int epoll_create(int size);
 int epoll_ctl(int epfd, int op, int fd, struct epoll_event *ev);
@@ -56,4 +107,4 @@ int epoll_wait(int epfd, struct epoll_event *evs, int maxevents, int timeout);
 
 void epoll_close(int epfd);
 
-#endif
+#endif /* __INCLUDE_NUTTX_SYS_EPOLL_H */
