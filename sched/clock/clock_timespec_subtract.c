da8950fe5bd7857b9aec8ceb32866b10244b9727
Lok <loketep@yahoo.com>
2015-11-03 07:28:46 -0600

    Fix an error in clock_timespec_subtract

--- sched/clock/clock_timespec_subtract.c
+++ sched/clock/clock_timespec_subtract.c
@@ -83,7 +83,7 @@ void clock_timespec_subtract(FAR const struct timespec *ts1,
     }
   else
     {
-      sec = ts1->tv_sec + ts2->tv_sec;
+      sec = ts1->tv_sec - ts2->tv_sec;
       if (ts1->tv_nsec < ts2->tv_nsec)
         {
           nsec = (ts1->tv_nsec + NSEC_PER_SEC) - ts2->tv_nsec;
