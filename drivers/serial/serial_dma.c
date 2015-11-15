7f1ac3619462a442683922b9301817c11322414d
Gregory Nutt <gnutt@nuttx.org>
2015-11-15 07:58:08 -0600

    Serial driver: Move serial DMA logic to a separate file

--- drivers/serial/serial_dma.c
+++ drivers/serial/serial_dma.c
@@ -1,7 +1,7 @@
 /************************************************************************************
- * drivers/serial/serialirq.c
+ * drivers/serial/serial_dma.c
  *
- *   Copyright (C) 2007-2009, 2011, 2015 Gregory Nutt. All rights reserved.
+ *   Copyright (C) 2015 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>
  *
  * Redistribution and use in source and binary forms, with or without
@@ -43,23 +43,10 @@
 #include <stdint.h>
 #include <semaphore.h>
 #include <debug.h>
+
 #include <nuttx/serial/serial.h>
 
-/************************************************************************************
- * Pre-processor Definitions
- ************************************************************************************/
-
-/************************************************************************************
- * Private Types
- ************************************************************************************/
-
-/************************************************************************************
- * Private Function Prototypes
- ************************************************************************************/
-
-/************************************************************************************
- * Private Variables
- ************************************************************************************/
+#ifdef CONFIG_SERIAL_DMA
 
 /************************************************************************************
  * Private Functions
@@ -146,7 +133,6 @@ static inline bool uart_dorxflowcontrol(FAR uart_dev_t *dev,
  *
  ************************************************************************************/
 
-#ifdef CONFIG_SERIAL_DMA
 void uart_xmitchars_dma(FAR uart_dev_t *dev)
 {
   FAR struct uart_dmaxfer_s *xfer = &dev->dmatx;
@@ -175,7 +161,6 @@ void uart_xmitchars_dma(FAR uart_dev_t *dev)
 
   uart_dmasend(dev);
 }
-#endif /* CONFIG_SERIAL_DMA */
 
 /************************************************************************************
  * Name: uart_xmitchars_done
@@ -187,7 +172,6 @@ void uart_xmitchars_dma(FAR uart_dev_t *dev)
  *
  ************************************************************************************/
 
-#ifdef CONFIG_SERIAL_DMA
 void uart_xmitchars_done(FAR uart_dev_t *dev)
 {
   FAR struct uart_dmaxfer_s *xfer = &dev->dmatx;
@@ -209,7 +193,6 @@ void uart_xmitchars_done(FAR uart_dev_t *dev)
       uart_datasent(dev);
     }
 }
-#endif /* CONFIG_SERIAL_DMA */
 
 /************************************************************************************
  * Name: uart_recvchars_dma
@@ -219,7 +202,6 @@ void uart_xmitchars_done(FAR uart_dev_t *dev)
  *
  ************************************************************************************/
 
-#ifdef CONFIG_SERIAL_DMA
 void uart_recvchars_dma(FAR uart_dev_t *dev)
 {
   FAR struct uart_dmaxfer_s *xfer = &dev->dmarx;
@@ -292,7 +274,6 @@ void uart_recvchars_dma(FAR uart_dev_t *dev)
 
   uart_dmareceive(dev);
 }
-#endif /* CONFIG_SERIAL_DMA */
 
 /************************************************************************************
  * Name: uart_recvchars_done
@@ -304,7 +285,6 @@ void uart_recvchars_dma(FAR uart_dev_t *dev)
  *
  ************************************************************************************/
 
-#ifdef CONFIG_SERIAL_DMA
 void uart_recvchars_done(FAR uart_dev_t *dev)
 {
   FAR struct uart_dmaxfer_s *xfer = &dev->dmarx;
@@ -326,153 +306,5 @@ void uart_recvchars_done(FAR uart_dev_t *dev)
       uart_datareceived(dev);
     }
 }
+
 #endif /* CONFIG_SERIAL_DMA */
-
-/************************************************************************************
- * Name: uart_xmitchars
- *
- * Description:
- *   This function is called from the UART interrupt handler when an interrupt
- *   is received indicating that there is more space in the transmit FIFO.  This
- *   function will send characters from the tail of the xmit buffer while the driver
- *   write() logic adds data to the head of the xmit buffer.
- *
- ************************************************************************************/
-
-void uart_xmitchars(FAR uart_dev_t *dev)
-{
-  uint16_t nbytes = 0;
-
-  /* Send while we still have data in the TX buffer & room in the fifo */
-
-  while (dev->xmit.head != dev->xmit.tail && uart_txready(dev))
-    {
-      /* Send the next byte */
-
-      uart_send(dev, dev->xmit.buffer[dev->xmit.tail]);
-      nbytes++;
-
-      /* Increment the tail index */
-
-      if (++(dev->xmit.tail) >= dev->xmit.size)
-        {
-          dev->xmit.tail = 0;
-        }
-    }
-
-  /* When all of the characters have been sent from the buffer disable the TX
-   * interrupt.
-   *
-   * Potential bug?  If nbytes == 0 && (dev->xmit.head == dev->xmit.tail) &&
-   * dev->xmitwaiting == true, then disabling the TX interrupt will leave
-   * the uart_write() logic waiting to TX to complete with no TX interrupts.
-   * Can that happen?
-   */
-
-  if (dev->xmit.head == dev->xmit.tail)
-    {
-      uart_disabletxint(dev);
-    }
-
-  /* If any bytes were removed from the buffer, inform any waiters there there is
-   * space available.
-   */
-
-  if (nbytes)
-    {
-      uart_datasent(dev);
-    }
-}
-
-/************************************************************************************
- * Name: uart_receivechars
- *
- * Description:
- *   This function is called from the UART interrupt handler when an interrupt
- *   is received indicating that are bytes available in the receive fifo.  This
- *   function will add chars to head of receive buffer.  Driver read() logic will
- *   take characters from the tail of the buffer.
- *
- ************************************************************************************/
-
-void uart_recvchars(FAR uart_dev_t *dev)
-{
-  FAR struct uart_buffer_s *rxbuf = &dev->recv;
-#ifdef CONFIG_SERIAL_IFLOWCONTROL_WATERMARKS
-  unsigned int watermark;
-#endif
-  unsigned int status;
-  int nexthead = rxbuf->head + 1;
-  uint16_t nbytes = 0;
-  bool is_full;
-
-  if (nexthead >= rxbuf->size)
-    {
-      nexthead = 0;
-    }
-
-#ifdef CONFIG_SERIAL_IFLOWCONTROL_WATERMARKS
-  /* Pre-calcuate the watermark level that we will need to test against. */
-
-  watermark = (CONFIG_SERIAL_IFLOWCONTROL_UPPER_WATERMARK * rxbuf->size) / 100;
-#endif
-
-  /* Loop putting characters into the receive buffer until there are no further
-   * characters to available.
-   */
-
-  while (uart_rxavailable(dev))
-    {
-      is_full = (nexthead == rxbuf->tail);
-      char ch;
-
-#ifdef CONFIG_SERIAL_IFLOWCONTROL
-#ifdef CONFIG_SERIAL_IFLOWCONTROL_WATERMARKS
-      if (uart_dorxflowcontrol(dev, rxbuf, watermark))
-        {
-          break;
-        }
-#else
-      if (uart_dorxflowcontrol(dev, rxbuf, is_full))
-        {
-          break;
-        }
-#endif
-#endif
-
-      ch = uart_receive(dev, &status);
-
-      /* If the RX buffer becomes full, then the serial data is discarded.  This is
-       * necessary because on most serial hardware, you must read the data in order
-       * to clear the RX interrupt. An option on some hardware might be to simply
-       * disable RX interrupts until the RX buffer becomes non-FULL.  However, that
-       * would probably just cause the overrun to occur in hardware (unless it has
-       * some large internal buffering).
-       */
-
-      if (!is_full)
-        {
-          /* Add the character to the buffer */
-
-          rxbuf->buffer[rxbuf->head] = ch;
-          nbytes++;
-
-          /* Increment the head index */
-
-          rxbuf->head = nexthead;
-          if (++nexthead >= rxbuf->size)
-            {
-               nexthead = 0;
-            }
-        }
-    }
-
-  /* If any bytes were added to the buffer, inform any waiters there is new
-   * incoming data available.
-   */
-
-  if (nbytes)
-    {
-      uart_datareceived(dev);
-    }
-}
