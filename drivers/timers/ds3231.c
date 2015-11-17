25c4ff828592f7f3c8e2f0a534d7fa1424abce0e
Gregory Nutt <gnutt@nuttx.org>
2015-11-17 16:24:09 -0600

    Update some comments

--- drivers/timers/ds3231.c
+++ drivers/timers/ds3231.c
@@ -220,7 +220,14 @@ static int rtc_bcd2bin(uint8_t value)
  *
  * Description:
  *   Initialize the hardware RTC per the selected configuration.  This function is
- *   called once during the OS initialization sequence
+ *   called once during the OS initialization sequence by board-specific logic.
+ *
+ *   After ds3231_rtc_initialize() is called, the OS function clock_synchronize()
+ *   should also be called to synchronize the system timer to a hardware RTC.  That
+ *   operation is normally performed automatically by the system during clock
+ *   initialization.  However, when an external RTC is used, the board logic will
+ *   need to explicitly re-synchronize the system timer to the RTC when the RTC
+ *   becomes available.
  *
  * Input Parameters:
  *   None
--- include/nuttx/timers/ds3231.h
+++ include/nuttx/timers/ds3231.h
@@ -61,7 +61,14 @@ extern "C"
  *
  * Description:
  *   Initialize the hardware RTC per the selected configuration.  This function is
- *   called once during the OS initialization sequence
+ *   called once during the OS initialization sequence by board-specific logic.
+ *
+ *   After ds3231_rtc_initialize() is called, the OS function clock_synchronize()
+ *   should also be called to synchronize the system timer to a hardware RTC.  That
+ *   operation is normally performed automatically by the system during clock
+ *   initialization.  However, when an external RTC is used, the board logic will
+ *   need to explicitly re-synchronize the system timer to the RTC when the RTC
+ *   becomes available.
  *
  * Input Parameters:
  *   None
