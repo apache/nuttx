25e54726a333d66520ad1129a6faae8abaa57365
Gregory Nutt <gnutt@nuttx.org>
2015-11-18 14:33:27 -0600

    STM32: Fix a build error detected by tools/testbuil.sh:  The STM32L does not define UID address

--- arch/arm/src/stm32/stm32_uid.c
+++ arch/arm/src/stm32/stm32_uid.c
@@ -37,8 +37,13 @@
  * Included Files
  ************************************************************************************/
 
+#include <nuttx/config.h>
+
 #include <chip/stm32_memorymap.h>
+
 #include "stm32_uid.h"
+
+#ifdef STM32_SYSMEM_UID /* Not defined for the STM32L */
 
 /************************************************************************************
  * Public Functions
@@ -53,3 +58,6 @@ void stm32_get_uniqueid(uint8_t uniqueid[12])
       uniqueid[i] = *((uint8_t*)(STM32_SYSMEM_UID)+i);
     }
 }
+
+#endif /* STM32_SYSMEM_UID */
+
