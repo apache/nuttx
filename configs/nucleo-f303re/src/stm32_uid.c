f4213873f38d7c09c9134cea88d9d950ab3f0427
Gregory Nutt <gnutt@nuttx.org>
2015-11-19 10:23:01 -0600

    board_uniqueid should return a negated errno value on failure

--- configs/nucleo-f303re/src/stm32_uid.c
+++ configs/nucleo-f303re/src/stm32_uid.c
@@ -36,23 +36,34 @@
 /************************************************************************************
  * Included Files
  ************************************************************************************/
+
 #include <nuttx/config.h>
-#include <nuttx/board.h>
+
+#include <errno.h>
 #include "stm32_uid.h"
+
+#include <nuttx/board.h>
+
+/************************************************************************************
+ * Pre-processor Definitions
+ ************************************************************************************/
 
 #ifndef OK
 #  define OK 0
 #endif
-#define ERR_NULL 1
 
 /************************************************************************************
  * Public Functions
  ************************************************************************************/
+
 #if defined(CONFIG_BOARDCTL_UNIQUEID)
 int board_uniqueid(uint8_t *uniqueid)
 {
-  if(uniqueid == 0)
-    return -ERR_NULL;
+  if (uniqueid == 0)
+    {
+      return -EINVAL;
+    }
+
   stm32_get_uniqueid(uniqueid);
   return OK;
 }
