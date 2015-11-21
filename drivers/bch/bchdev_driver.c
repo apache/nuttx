dcb85af387ff27de15de436d49e8593bfbe6e8dc
Gregory Nutt <gnutt@nuttx.org>
2015-11-21 10:14:01 -0600

    Add logic to automatically wrap a block device as a BCH device with no visible character device

--- drivers/bch/bchdev_driver.c
+++ drivers/bch/bchdev_driver.c
@@ -1,7 +1,7 @@
 /****************************************************************************
  * drivers/bch/bchdev_driver.c
  *
- *   Copyright (C) 2008-2009, 2014 Gregory Nutt. All rights reserved.
+ *   Copyright (C) 2008-2009, 2014-2015 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>
  *
  * Redistribution and use in source and binary forms, with or without
