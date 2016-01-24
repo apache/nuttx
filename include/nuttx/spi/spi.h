a4a496ad6f8e875c1b646b0ad38ab8f38e024efb
Gregory Nutt <gnutt@nuttx.org>
2016-01-23 19:13:31 -0600

    spi.h needs to include errno.h now

--- include/nuttx/spi/spi.h
+++ include/nuttx/spi/spi.h
@@ -46,6 +46,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <assert.h>
+#include <errno.h>
 
 /****************************************************************************
  * Pre-processor Definitions
