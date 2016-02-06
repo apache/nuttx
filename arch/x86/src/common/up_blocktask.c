b0bcd82ba7372be652e31cfd1c0103ec3980d8bd
Gregory Nutt <gnutt@nuttx.org>
2016-02-06 17:10:44 -0600

    x86: Replace explict references to g_readytorun with indirect references via the this_task() macro

--- arch/x86/src/common/up_blocktask.c
+++ arch/x86/src/common/up_blocktask.c
@@ -75,7 +75,7 @@
 
 void up_block_task(struct tcb_s *tcb, tstate_t task_state)
 {
-  struct tcb_s *rtcb = (struct tcb_s*)g_readytorun.head;
+  struct tcb_s *rtcb = this_task();
   bool switch_needed;
 
   /* Verify that the context switch can be performed */
@@ -96,7 +96,7 @@ void up_block_task(struct tcb_s *tcb, tstate_t task_state)
 
   sched_addblocked(tcb, (tstate_t)task_state);
 
-  /* If there are any pending tasks, then add them to the g_readytorun
+  /* If there are any pending tasks, then add them to the ready-to-run
    * task list now
    */
 
@@ -124,10 +124,10 @@ void up_block_task(struct tcb_s *tcb, tstate_t task_state)
           up_savestate(rtcb->xcp.regs);
 
           /* Restore the exception context of the rtcb at the (new) head
-           * of the g_readytorun task list.
+           * of the ready-to-run task list.
            */
 
-          rtcb = (struct tcb_s*)g_readytorun.head;
+          rtcb = this_task();
 
           /* Reset scheduler parameters */
 
@@ -141,17 +141,17 @@ void up_block_task(struct tcb_s *tcb, tstate_t task_state)
         }
 
       /* Copy the user C context into the TCB at the (old) head of the
-       * g_readytorun Task list. if up_saveusercontext returns a non-zero
+       * ready-to-run Task list. if up_saveusercontext returns a non-zero
        * value, then this is really the previously running task restarting!
        */
 
       else if (!up_saveusercontext(rtcb->xcp.regs))
         {
           /* Restore the exception context of the rtcb at the (new) head
-           * of the g_readytorun task list.
+           * of the ready-to-run task list.
            */
 
-          rtcb = (struct tcb_s*)g_readytorun.head;
+          rtcb = this_task();
 
 #ifdef CONFIG_ARCH_ADDRENV
          /* Make sure that the address environment for the previously
