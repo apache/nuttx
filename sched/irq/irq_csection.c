fccef49305b38bbbfe0e8c221e4dc933daf2a021
Gregory Nutt <gnutt@nuttx.org>
2016-02-15 13:48:08 -0600

    Cosmetic changes to comments

--- sched/irq/irq_csection.c
+++ sched/irq/irq_csection.c
@@ -1,7 +1,7 @@
 /****************************************************************************
  * sched/irq/irq_csection.c
  *
- *   Copyright (C) 2007-2008, 2010 Gregory Nutt. All rights reserved.
+ *   Copyright (C) 2016 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>
  *
  * Redistribution and use in source and binary forms, with or without
