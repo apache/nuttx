a1ee5ae6e5c5bd2b179d2c24930264c3c7472ce2
Gregory Nutt <gnutt@nuttx.org>
2016-03-12 08:53:41 -0600

    EFM32 Serial:  Fix typo in initializer.  Noted by Pierre-noel Bouteville

--- arch/arm/src/efm32/efm32_serial.c
+++ arch/arm/src/efm32/efm32_serial.c
@@ -1,7 +1,7 @@
 /****************************************************************************
  * arch/arm/src/efm32/efm32_serial.c
  *
- *   Copyright (C) 2024 Gregory Nutt. All rights reserved.
+ *   Copyright (C) 2014 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>
  *
  * Redistribution and use in source and binary forms, with or without
@@ -389,7 +389,7 @@ static struct efm32_config_s g_usart1config =
 {
   .uartbase  = EFM32_USART1_BASE,
   .rxhandler = efm32_usart1_rxinterrupt,
-  .rxhandler = efm32_usart1_txinterrupt,
+  .txhandler = efm32_usart1_txinterrupt,
   .baud      = CONFIG_USART1_BAUD,
   .rxirq     = EFM32_IRQ_USART1_RX,
   .txirq     = EFM32_IRQ_USART1_TX,
