2b2f15756989fe0142a48b366988324ddff0082c
Gregory Nutt <gnutt@nuttx.org>
2016-03-12 15:28:58 -0600

    Forgot to add a file before last commit

---
+++ arch/sim/src/up_smphook.c
@@ -0,0 +1,85 @@
+/****************************************************************************
+ * arch/sim/src/up_simhook.c
+ *
+ *   Copyright (C) 2016 Gregory Nutt. All rights reserved.
+ *   Author: Gregory Nutt <gnutt@nuttx.org>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name NuttX nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+#include <assert.h>
+
+#include <nuttx/sched.h>
+
+#include "sched/sched.h"
+
+#ifdef CONFIG_SMP
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: sim_smp_hook
+ *
+ * Description:
+ *   This is a mindless little hook between sim_idle_trampoline() and the
+ *   real IDLE task logic.  This is only needed because we need to access
+ *   elements of the TCB in the simulation domain but sim_idle_trampoline()
+ *   lies in the host domain.
+ *
+ * Input Parameters:
+ *   None
+ *
+ * Returned Value:
+ *   None (should not return)
+ *
+ ****************************************************************************/
+
+void sim_smp_hook(void)
+{
+  struct tcb_s *tcb = this_task();
+  DEBUGASSERT(tcb != NULL && tcb->start != NULL);
+
+  /* Transfer control to the "real" thread start-up routine */
+
+  tcb->start();
+
+  /* That function should not return. */
+
+  PANIC();
+}
+
+#endif /* CONFIG_SMP */
