39a37d6aa8a493ba217dd90453863cc32dcf3e6f
Gregory Nutt <gnutt@nuttx.org>
2016-04-14 08:39:48 -0600

    Separate out psock_listen() and psock_accept() for internal use by the OS

--- include/nuttx/net/net.h
+++ include/nuttx/net/net.h
@@ -504,6 +504,106 @@ int psock_bind(FAR struct socket *psock, FAR const struct sockaddr *addr,
                socklen_t addrlen);
 
 /****************************************************************************
+ * Function: psock_listen
+ *
+ * Description:
+ *   To accept connections, a socket is first created with psock_socket(), a
+ *   willingness to accept incoming connections and a queue limit for
+ *   incoming connections are specified with psock_listen(), and then the
+ *   connections are accepted with psock_accept(). The psock_listen() call
+ *   applies only to sockets of type SOCK_STREAM or SOCK_SEQPACKET.
+ *
+ * Parameters:
+ *   psock    Reference to an internal, boound socket structure.
+ *   backlog  The maximum length the queue of pending connections may grow.
+ *            If a connection request arrives with the queue full, the client
+ *            may receive an error with an indication of ECONNREFUSED or,
+ *            if the underlying protocol supports retransmission, the request
+ *            may be ignored so that retries succeed.
+ *
+ * Returned Value:
+ *   On success, zero is returned. On error, -1 is returned, and errno is set
+ *   appropriately.
+ *
+ *   EADDRINUSE
+ *     Another socket is already listening on the same port.
+ *   EOPNOTSUPP
+ *     The socket is not of a type that supports the listen operation.
+ *
+ ****************************************************************************/
+
+int psock_listen(FAR struct socket *psock, int backlog);
+
+/****************************************************************************
+ * Function: psock_accept
+ *
+ * Description:
+ *   The psock_accept function is used with connection-based socket types
+ *   (SOCK_STREAM, SOCK_SEQPACKET and SOCK_RDM). It extracts the first
+ *   connection request on the queue of pending connections, creates a new
+ *   connected socket with mostly the same properties as 'sockfd', and
+ *   allocates a new socket descriptor for the socket, which is returned. The
+ *   newly created socket is no longer in the listening state. The original
+ *   socket 'sockfd' is unaffected by this call.  Per file descriptor flags
+ *   are not inherited across an psock_accept.
+ *
+ *   The 'sockfd' argument is a socket descriptor that has been created with
+ *   socket(), bound to a local address with bind(), and is listening for
+ *   connections after a call to listen().
+ *
+ *   On return, the 'addr' structure is filled in with the address of the
+ *   connecting entity. The 'addrlen' argument initially contains the size
+ *   of the structure pointed to by 'addr'; on return it will contain the
+ *   actual length of the address returned.
+ *
+ *   If no pending connections are present on the queue, and the socket is
+ *   not marked as non-blocking, psock_accept blocks the caller until a
+ *   connection is present. If the socket is marked non-blocking and no
+ *   pending connections are present on the queue, psock_accept returns
+ *   EAGAIN.
+ *
+ * Parameters:
+ *   psock    Reference to the listening socket structure
+ *   addr     Receives the address of the connecting client
+ *   addrlen  Input: allocated size of 'addr', Return: returned size of 'addr'
+ *   newsock  Location to return the accepted socket information.
+ *
+ * Returned Value:
+ *  Returns 0 (OK) on success.  On failure, it returns -1 (ERROR) with the
+ *  errno variable set to indicate the nature of the error.
+ *
+ * EAGAIN or EWOULDBLOCK
+ *   The socket is marked non-blocking and no connections are present to
+ *   be accepted.
+ * EOPNOTSUPP
+ *   The referenced socket is not of type SOCK_STREAM.
+ * EINTR
+ *   The system call was interrupted by a signal that was caught before
+ *   a valid connection arrived.
+ * ECONNABORTED
+ *   A connection has been aborted.
+ * EINVAL
+ *   Socket is not listening for connections.
+ * EMFILE
+ *   The per-process limit of open file descriptors has been reached.
+ * ENFILE
+ *   The system maximum for file descriptors has been reached.
+ * EFAULT
+ *   The addr parameter is not in a writable part of the user address
+ *   space.
+ * ENOBUFS or ENOMEM
+ *   Not enough free memory.
+ * EPROTO
+ *   Protocol error.
+ * EPERM
+ *   Firewall rules forbid connection.
+ *
+ ****************************************************************************/
+
+int psock_accept(FAR struct socket *psock, FAR struct sockaddr *addr,
+                 FAR socklen_t *addrlen, FAR struct socket *newsock);
+
+/****************************************************************************
  * Name: psock_connect
  *
  * Description:
