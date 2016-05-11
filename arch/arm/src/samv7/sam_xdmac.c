8517a303a5134db236dd63883a85baf15839e84d
David Sidrane <david_s5@nscdg.com>
2016-05-11 23:13:24 +0000

    sam_xdmac.c edited online with Bitbucket

--- arch/arm/src/samv7/sam_xdmac.c
+++ arch/arm/src/samv7/sam_xdmac.c
@@ -1809,7 +1809,7 @@ int sam_dmatxsetup(DMA_HANDLE handle, uint32_t paddr, uint32_t maddr,
       /* Set up the maximum size transfer */
 
       ret = sam_txbuffer(xdmach, paddr, maddr, maxtransfer);
-      if (ret == OK);
+      if (ret == OK)
         {
           /* Decrement the number of bytes left to transfer */
 
