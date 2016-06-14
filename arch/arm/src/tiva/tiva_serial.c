078e9b7082ac02b2a90580e1ff9c798378b19ae3
Shirshak Sengupta <sgshirshak@gmail.com>
2016-06-14 10:47:18 -0600

    Bug Fix in tiva_serial.c - UART5, UART6 and UART7 were not being configured as TTYS0 for printing over serial console

--- arch/arm/src/tiva/tiva_serial.c
+++ arch/arm/src/tiva/tiva_serial.c
@@ -109,13 +109,16 @@
 #    define UART4_ASSIGNED      1
 #elif defined(CONFIG_UART5_SERIAL_CONSOLE)
 #    define CONSOLE_DEV         g_uart5port /* UART5 is console */
-#    define TTYS5_DEV           g_uart5port /* UART5 is ttyS0 */
+#    define TTYS0_DEV           g_uart5port /* UART5 is ttyS0 */
+#    define UART5_ASSIGNED      1
 #elif defined(CONFIG_UART6_SERIAL_CONSOLE)
 #    define CONSOLE_DEV         g_uart6port /* UART6 is console */
-#    define TTYS5_DEV           g_uart6port /* UART6 is ttyS0 */
+#    define TTYS0_DEV           g_uart6port /* UART6 is ttyS0 */
+#    define UART6_ASSIGNED      1
 #elif defined(CONFIG_UART7_SERIAL_CONSOLE)
 #    define CONSOLE_DEV         g_uart7port /* UART7 is console */
-#    define TTYS5_DEV           g_uart7port /* UART7 is ttyS0 */
+#    define TTYS0_DEV           g_uart7port /* UART7 is ttyS0 */
+#    define UART7_ASSIGNED      1
 #else
 #  undef CONSOLE_DEV                        /* No console */
 #  if defined(CONFIG_TIVA_UART0)
