4ad7129ac932bf1f1b6193eba884e457d56304e9
Gregory Nutt <gnutt@nuttx.org>
2016-07-02 11:55:31 -0600

    Update README and some comments

--- configs/freedom-k64f/README.txt
+++ configs/freedom-k64f/README.txt
@@ -57,6 +57,12 @@ Serial Console
   The primary serial port interface signals are PTB16 UART1_RX and PTB17
   UART1_TX. These signals are connected to the OpenSDAv2 circuit.
 
+  REVISIT: The above quote was taken from the "FRDM-K64F Freedom Module User’s
+  Guide". It appears from the "K64 Sub-Family Reference Manual" that the only
+  options for UART1 are {PTC3, PTE1} for UART1_RX and {PTC4, PTE0} for UART1_TX.
+
+  PTB16 and PTB17 are valid options for UART0, however.
+
 LEDs and Buttons
 ================
 
--- configs/freedom-k64f/include/board.h
+++ configs/freedom-k64f/include/board.h
@@ -201,9 +201,11 @@
 /* The primary serial port interface signals are PTB16 UART1_RX and PTB17 UART1_TX.
  * These signals are connected to the OpenSDAv2 circuit.
  *
- * REVISIT: The above quoate was taken from the "FRDM-K64F Freedom Module User’s
+ * REVISIT: The above quote was taken from the "FRDM-K64F Freedom Module User’s
  * Guide". It appears from the "K64 Sub-Family Reference Manual" that the only
  * options for UART1 are {PTC3, PTE1} for UART1_RX and {PTC4, PTE0} for UART1_TX.
+ *
+ * PTB16 and PTB17 are valid options for UART0, however.
  */
 
 /************************************************************************************
