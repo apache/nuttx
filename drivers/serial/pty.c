d9fbf4c90febd501f055cda4e3fd19c1dab8a862
Gregory Nutt <gnutt@nuttx.org>
2016-07-18 11:27:06 -0600

    Eliminate some warnings

--- drivers/serial/pty.c
+++ drivers/serial/pty.c
@@ -715,6 +715,7 @@ static int pty_ioctl(FAR struct file *filep, int cmd, unsigned long arg)
           termiosp->c_iflag = dev->pd_iflag;
           termiosp->c_oflag = dev->pd_oflag;
           termiosp->c_lflag = 0;
+          ret = OK;
         }
         break;
 
@@ -732,6 +733,7 @@ static int pty_ioctl(FAR struct file *filep, int cmd, unsigned long arg)
 
           dev->pd_iflag = termiosp->c_iflag;
           dev->pd_oflag = termiosp->c_oflag;
+          ret = OK;
         }
         break;
 #endif
