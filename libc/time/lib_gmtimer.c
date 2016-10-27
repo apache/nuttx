123c520db42113c8553c2741b8fdcd8cb95400c2
Gregory Nutt <gnutt@nuttx.org>
2016-10-27 08:36:24 -0600

    Correct time initialization for the case where time is initialized from settings in the .config file.  The correct range for month (tm_mon) internally is 0-11, not 1-12 as it is in the .config file.

--- libc/time/lib_gmtimer.c
+++ libc/time/lib_gmtimer.c
@@ -55,10 +55,6 @@
 #define SEC_PER_DAY  ((time_t)24 * SEC_PER_HOUR)
 
 /****************************************************************************
- * Private Type Declarations
- ****************************************************************************/
-
-/****************************************************************************
  * Private Function Prototypes
  ****************************************************************************/
 
@@ -90,8 +86,7 @@ static void   clock_utc2julian(time_t jdn, int *year, int *month, int *day);
  ****************************************************************************/
 
 /****************************************************************************
- * Function:  clock_calendar2utc, clock_gregorian2utc,
- *            and clock_julian2utc
+ * Function:  clock_utc2calendar, clock_utc2gregorian, and clock_utc2julian
  *
  * Description:
  *    Calendar to UTC conversion routines.  These conversions
