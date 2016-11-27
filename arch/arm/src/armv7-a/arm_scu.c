278d8330d6f48590088ac7ea498a97d72b45459b
Gregory Nutt <gnutt@nuttx.org>
2016-11-27 02:59:42 +0000

    arm_scu.c edited online with Bitbucket.  Fux some typos.

--- arch/arm/src/armv7-a/arm_scu.c
+++ arch/arm/src/armv7-a/arm_scu.c
@@ -1,7 +1,7 @@
 /****************************************************************************
- * arch/arm/src/armv7-a/arm_undefinedinsn.c
+ * arch/arm/src/armv7-a/arm_scu.c
  *
- *   Copyright (C) 2013, 2016 Gregory Nutt. All rights reserved.
+ *   Copyright (C) 2016 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>
  *
  * Redistribution and use in source and binary forms, with or without
