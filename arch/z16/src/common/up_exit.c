3ed091376c113b8c800806aca72c3b696790840e
Gregory Nutt <gnutt@nuttx.org>
2017-01-13 11:08:24 -0600

    In all implementations of _exit(), use enter_critical_section() vs. disabling local interrupts.

--- arch/z16/src/common/up_exit.c
+++ arch/z16/src/common/up_exit.c
@@ -1,7 +1,7 @@
 /****************************************************************************
  * arch/z16/src/common/up_exit.c
  *
- *   Copyright (C) 2008-2009, 2013 Gregory Nutt. All rights reserved.
+ *   Copyright (C) 2008-2009, 2013, 2017 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>
  *
  * Redistribution and use in source and binary forms, with or without
@@ -43,9 +43,9 @@
 #include <debug.h>
 
 #include <nuttx/arch.h>
-
+#include <nuttx/irq.h>
 #ifdef CONFIG_DUMP_ON_EXIT
-#include <nuttx/fs/fs.h>
+#  include <nuttx/fs/fs.h>
 #endif
 
 #include "chip/chip.h"
@@ -141,11 +141,11 @@ void _exit(int status)
 {
   FAR struct tcb_s* tcb;
 
-  /* Disable interrupts.  Interrupts will remain disabled until
-   * the new task is resumed below.
+  /* Make sure that we are in a critical section with local interrupts.
+   * The IRQ state will be restored when the next task is started.
    */
 
-  (void)up_irq_save();
+  (void)enter_critical_section();
 
   sinfo("TCB=%p exiting\n", tcb);
 
