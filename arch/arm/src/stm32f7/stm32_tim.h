2ee99c9d4ef381744b804b1ff68d058d47d8e042
David Sidrane <david_s5@nscdg.com>
2017-02-28 14:29:06 -1000

    STM32F7:stm32_tim  irq fixes

--- arch/arm/src/stm32f7/stm32_tim.h
+++ arch/arm/src/stm32f7/stm32_tim.h
@@ -167,7 +167,8 @@ struct stm32_tim_ops_s
 
   /* Timer interrupts */
 
-  int  (*setisr)(FAR struct stm32_tim_dev_s *dev, int (*handler)(int irq, void *context), int source);
+  int  (*setisr)(FAR struct stm32_tim_dev_s *dev, xcpt_t handler,
+                 int source);
   void (*enableint)(FAR struct stm32_tim_dev_s *dev, int source);
   void (*disableint)(FAR struct stm32_tim_dev_s *dev, int source);
   void (*ackint)(FAR struct stm32_tim_dev_s *dev, int source);
