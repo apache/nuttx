apache/nuttx

commit ff73be870e38959b0aaee5961dc47b4b58dc2d86

Move renaming files in fs/. to fs/vfs/. (Don't all belong there)

fs/vfs/fs_sendfile.c
@@ -1,5 +1,5 @@
 /************************************************************************
- * fs/fs_sendfile.c
+ * fs/vfs/fs_sendfile.c
  *
  *   Copyright (C) 2007, 2009, 2011, 2013 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>

fs/fs_filedup2.c
@@ -1,121 +1,0 @@
-/****************************************************************************
- * fs/fs_filedup2.c
- *
- *   Copyright (C) 2007-2009, 2011-2013 Gregory Nutt. All rights reserved.
- *   Author: Gregory Nutt <gnutt@nuttx.org>
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- * 3. Neither the name NuttX nor the names of its contributors may be
- *    used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
- * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
- * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- *
- ****************************************************************************/
-
-/****************************************************************************
- * Included Files
- ****************************************************************************/
-
-#include <nuttx/config.h>
-
-#include <unistd.h>
-#include <sched.h>
-#include <errno.h>
-
-#include "fs.h"
-
-#if CONFIG_NFILE_DESCRIPTORS > 0
-
-/****************************************************************************
- * Pre-processor Definitions
- ****************************************************************************/
-
-#define DUP_ISOPEN(fd, list) \
-  ((unsigned int)fd < CONFIG_NFILE_DESCRIPTORS && \
-   list->fl_files[fd].f_inode != NULL)
-
-/****************************************************************************
- * Private Functions
- ****************************************************************************/
-
-/****************************************************************************
- * Global Functions
- ****************************************************************************/
-
-/****************************************************************************
- * Name: file_dup2 OR dup2
- *
- * Description:
- *   Clone a file descriptor to a specific descriptor number. If socket
- *   descriptors are implemented, then this is called by dup2() for the
- *   case of file descriptors.  If socket descriptors are not implemented,
- *   then this function IS dup2().
- *
- ****************************************************************************/
-
-#if defined(CONFIG_NET) && CONFIG_NSOCKET_DESCRIPTORS > 0
-int file_dup2(int fd1, int fd2)
-#else
-int dup2(int fd1, int fd2)
-#endif
-{
-  FAR struct filelist *list;
-
-  /* Get the thread-specific file list */
-
-  list = sched_getfiles();
-  if (!list)
-    {
-      set_errno(EMFILE);
-      return ERROR;
-    }
-
-  /* Verify that fd is a valid, open file descriptor */
-
-  if (!DUP_ISOPEN(fd1, list))
-    {
-      set_errno(EBADF);
-      return ERROR;
-    }
-
-  /* Handle a special case */
-
-  if (fd1 == fd2)
-    {
-      return fd1;
-    }
-
-  /* Verify fd2 */
-
-  if ((unsigned int)fd2 >= CONFIG_NFILE_DESCRIPTORS)
-    {
-      set_errno(EBADF);
-      return ERROR;
-    }
-
-  return files_dup(&list->fl_files[fd1], &list->fl_files[fd2]);
-}
-
-#endif /* CONFIG_NFILE_DESCRIPTORS > 0 */
-

fs/vfs/Make.defs
@@ -1,7 +1,7 @@
 ############################################################################
-# fs/Makefile
+# fs/vfs/Make.defs
 #
-#   Copyright (C) 2007, 2008, 2011-2014 Gregory Nutt. All rights reserved.
+#   Copyright (C) 2014 Gregory Nutt. All rights reserved.
 #   Author: Gregory Nutt <gnutt@nuttx.org>
 #
 # Redistribution and use in source and binary forms, with or without
@@ -33,71 +33,59 @@
 #
 ############################################################################
 
--include $(TOPDIR)/Make.defs
+# If there are no file descriptors configured, then a small part of the
+# logic in this directory may still apply to socket descriptors
 
-ASRCS =
-AOBJS = $(ASRCS:.S=$(OBJEXT))
+ifeq ($(CONFIG_NFILE_DESCRIPTORS),0)
+ifneq ($(CONFIG_NSOCKET_DESCRIPTORS),0)
 
-CSRCS =
+# Socket descriptor support
 
-DEPPATH = --dep-path .
-VPATH = .
+CSRCS += fs_close.c fs_read.c fs_write.c fs_ioctl.c fs_poll.c fs_select.c
 
-include inode/Make.defs
-include vfs/Make.defs
-include driver/Make.defs
-include dirent/Make.defs
-include mmap/Make.defs
+# Support for network access using streams
 
-# Additional files required is mount-able file systems are supported
-
-ifneq ($(CONFIG_NFILE_DESCRIPTORS),0)
-ifneq ($(CONFIG_DISABLE_MOUNTPOINT),y)
-
-include mount/Make.defs
-include fat/Make.defs
-include romfs/Make.defs
-include nxffs/Make.defs
-include nfs/Make.defs
-include smartfs/Make.defs
-include binfs/Make.defs
-include procfs/Make.defs
-
-endif
+ifneq ($(CONFIG_NFILE_STREAMS),0)
+CSRCS += fs_fdopen.c
 endif
 
-COBJS = $(CSRCS:.c=$(OBJEXT))
+# Support for sendfile()
 
-SRCS = $(ASRCS) $(CSRCS)
-OBJS = $(AOBJS) $(COBJS)
+ifeq ($(CONFIG_NET_SENDFILE),y)
+CSRCS += fs_sendfile.c
+endif
 
-BIN = libfs$(LIBEXT)
+# Include vfs build support
 
-SUBDIRS = mmap fat romfs nxffs nfs binfs procfs
+DEPPATH += --dep-path vfs
+VPATH += :vfs
+CFLAGS += ${shell $(INCDIR) $(INCDIROPT) "$(CC)" $(TOPDIR)$(DELIM)fs$(DELIM)vfs}
+endif
 
-all: $(BIN)
+else
 
-$(AOBJS): %$(OBJEXT): %.S
-	$(call ASSEMBLE, $<, $@)
+# Common file/socket descriptor support
 
-$(COBJS): %$(OBJEXT): %.c
-	$(call COMPILE, $<, $@)
+CSRCS += fs_close.c fs_dup.c fs_dup2.c fs_fcntl.c fs_filedup.c fs_filedup2.c
+CSRCS += fs_ioctl.c fs_lseek.c fs_mkdir.c fs_open.c fs_poll.c fs_read.c
+CSRCS += fs_rename.c fs_rmdir.c fs_stat.c fs_statfs.c fs_select.c
+CSRCS += fs_unlink.c fs_write.c
 
-$(BIN):	$(OBJS)
-	$(call ARCHIVE, $@, $(OBJS))
+# Stream support
 
-.depend: Makefile $(SRCS)
-	$(Q) $(MKDEP) $(DEPPATH) "$(CC)" -- $(CFLAGS) -- $(SRCS) >Make.dep
-	$(Q) touch $@
+ifneq ($(CONFIG_NFILE_STREAMS),0)
+CSRCS += fs_fdopen.c
+endif
 
-depend: .depend
+# Support for sendfile()
 
-clean:
-	$(call DELFILE, $(BIN))
-	$(call CLEAN)
+ifeq ($(CONFIG_NET_SENDFILE),y)
+CSRCS += fs_sendfile.c
+endif
 
-distclean: clean
-	$(call DELFILE, Make.dep)
-	$(call DELFILE, .depend)
+# Include vfs build support
 
--include Make.dep
+DEPPATH += --dep-path vfs
+VPATH += :vfs
+CFLAGS += ${shell $(INCDIR) $(INCDIROPT) "$(CC)" $(TOPDIR)$(DELIM)fs$(DELIM)vfs}
+endif

fs/vfs/fs_close.c
@@ -1,7 +1,7 @@
 /****************************************************************************
- * fs/fs_fsync.c
+ * fs/vfs/fs_close.c
  *
- *   Copyright (C) 2007-2009, 2013 Gregory Nutt. All rights reserved.
+ *   Copyright (C) 2007-2009, 2012 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>
  *
  * Redistribution and use in source and binary forms, with or without
@@ -40,97 +40,94 @@
 #include <nuttx/config.h>
 
 #include <unistd.h>
-#include <fcntl.h>
+#include <sched.h>
 #include <errno.h>
-#include <assert.h>
-
 #include <nuttx/fs/fs.h>
-#include <nuttx/sched.h>
+
+#if defined(CONFIG_NET) && CONFIG_NSOCKET_DESCRIPTORS > 0
+# include <nuttx/net/net.h>
+#endif
 
 #include "fs.h"
 
 /****************************************************************************
- * Pre-processor Definitions
+ * Global Functions
  ****************************************************************************/
 
 /****************************************************************************
- * Private Variables
- ****************************************************************************/
-
-/****************************************************************************
- * Public Variables
- ****************************************************************************/
-
-/****************************************************************************
- * Private Functions
- ****************************************************************************/
-
-/****************************************************************************
- * Public Functions
- ****************************************************************************/
-
-/****************************************************************************
- * Name: fsync
+ * Name: close
  *
  * Description:
- *   This func simply binds inode sync methods to the sync system call.
+ *   close() closes a file descriptor, so that it no longer refers to any
+ *   file and may be reused. Any record locks (see fcntl(2)) held on the file
+ *   it was associated with, and owned by the process, are removed (regardless
+ *   of the file descriptor that was used to obtain the lock).
+ *
+ *   If fd is the last copy of a particular file descriptor the resources
+ *   associated with it are freed; if the descriptor was the last reference
+ *   to a file which has been removed using unlink(2) the file is deleted.
+ *
+ * Parameters:
+ *   fd   file descriptor to close
+ *
+ * Returned Value:
+ *   0 on success; -1 on error with errno set appropriately.
+ *
+ * Assumptions:
  *
  ****************************************************************************/
 
-int fsync(int fd)
+int close(int fd)
 {
-  FAR struct filelist *list;
-  FAR struct file     *filep;
-  struct inode        *inode;
-  int                  ret;
-
-  /* Get the thread-specific file list */
-
-  list = sched_getfiles();
-  DEBUGASSERT(list);
+  int err;
+#if CONFIG_NFILE_DESCRIPTORS > 0
+  int ret;
 
   /* Did we get a valid file descriptor? */
 
   if ((unsigned int)fd >= CONFIG_NFILE_DESCRIPTORS)
+#endif
     {
-      ret = EBADF;
-      goto errout;
+      /* Close a socket descriptor */
+
+#if defined(CONFIG_NET) && CONFIG_NSOCKET_DESCRIPTORS > 0
+      if ((unsigned int)fd < (CONFIG_NFILE_DESCRIPTORS+CONFIG_NSOCKET_DESCRIPTORS))
+        {
+          return net_close(fd);
+        }
+      else
+#endif
+        {
+          err = EBADF;
+          goto errout;
+        }
     }
 
-  /* Was this file opened for write access? */
-
-  filep = &list->fl_files[fd];
-  if ((filep->f_oflags & O_WROK) == 0)
-    {
-      ret = EBADF;
-      goto errout;
-    }
-
-  /* Is this inode a registered mountpoint? Does it support the
-   * sync operations may be relevant to device drivers but only
-   * the mountpoint operations vtable contains a sync method.
+#if CONFIG_NFILE_DESCRIPTORS > 0
+  /* Close the driver or mountpoint.  NOTES: (1) there is no
+   * exclusion mechanism here , the driver or mountpoint must be
+   * able to handle concurrent operations internally, (2) The driver
+   * may have been opened numerous times (for different file
+   * descriptors) and must also handle being closed numerous times.
+   * (3) for the case of the mountpoint, we depend on the close
+   * methods bing identical in signature and position in the operations
+   * vtable.
    */
 
-  inode = filep->f_inode;
-  if (!inode || !INODE_IS_MOUNTPT(inode) ||
-      !inode->u.i_mops || !inode->u.i_mops->sync)
+  ret = files_close(fd);
+  if (ret < 0)
     {
-      ret = EINVAL;
+      /* An error occurred while closing the driver */
+
+      err = -ret;
       goto errout;
     }
+  return OK;
 
-  /* Yes, then tell the mountpoint to sync this file */
-
-  ret = inode->u.i_mops->sync(filep);
-  if (ret >= 0)
-    {
-      return OK;
-    }
-
-  ret = -ret;
+#endif
 
 errout:
-  set_errno(ret);
+  set_errno(err);
   return ERROR;
 }
 

fs/vfs/fs_dup.c
@@ -1,7 +1,7 @@
 /****************************************************************************
- * fs/fs_dup2.c
+ * fs/vfs/fs_dup.c
  *
- *   Copyright (C) 2007-2009, 2011, 2013 Gregory Nutt. All rights reserved.
+ *   Copyright (C) 2007-2009 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>
  *
  * Redistribution and use in source and binary forms, with or without
@@ -43,14 +43,8 @@
 #include <sched.h>
 #include <errno.h>
 
+#include <nuttx/fs/fs.h>
 #include "fs.h"
-
-/* This logic in this applies only when both socket and file descriptors are
- * in that case, this function descriminates which type of dup2 is being
- * performed.
- */
-
-#if CONFIG_NFILE_DESCRIPTORS > 0 && defined(CONFIG_NET) && CONFIG_NSOCKET_DESCRIPTORS > 0
 
 /****************************************************************************
  * Pre-processor Definitions
@@ -65,45 +59,49 @@
  ****************************************************************************/
 
 /****************************************************************************
- * Name: dup2
+ * Name: dup
  *
  * Description:
- *   Clone a file descriptor or socket descriptor to a specific descriptor
- *   number
+ *   Clone a file or socket descriptor to an arbitray descriptor number
  *
  ****************************************************************************/
 
-int dup2(int fd1, int fd2)
+int dup(int fd)
 {
+ int ret = OK;
+
   /* Check the range of the descriptor to see if we got a file or a socket
-   * descriptor.
-   */
+   * descriptor. */
 
-  if ((unsigned int)fd1 >= CONFIG_NFILE_DESCRIPTORS)
+#if CONFIG_NFILE_DESCRIPTORS > 0
+  if ((unsigned int)fd < CONFIG_NFILE_DESCRIPTORS)
     {
-      /* Not a valid file descriptor.  Did we get a valid socket descriptor? */
+      /* Its a valid file descriptor.. dup the file descriptor using any
+       * other file descriptor*/
 
-      if ((unsigned int)fd1 < (CONFIG_NFILE_DESCRIPTORS+CONFIG_NSOCKET_DESCRIPTORS))
+      ret = file_dup(fd, 0);
+    }
+  else
+#endif
+    {
+      /* Not a vailid file descriptor.  Did we get a valid socket descriptor? */
+
+#if defined(CONFIG_NET) && CONFIG_NSOCKET_DESCRIPTORS > 0
+      if ((unsigned int)fd < (CONFIG_NFILE_DESCRIPTORS+CONFIG_NSOCKET_DESCRIPTORS))
         {
           /* Yes.. dup the socket descriptor */
 
-          return net_dup2(fd1, fd2);
+          ret = net_dup(fd, CONFIG_NFILE_DESCRIPTORS);
         }
       else
+#endif
         {
           /* No.. then it is a bad descriptor number */
 
           set_errno(EBADF);
-          return ERROR;
+          ret = ERROR;
         }
     }
-  else
-    {
-      /* Its a valid file descriptor.. dup the file descriptor */
 
-      return file_dup2(fd1, fd2);
-    }
+  return ret;
 }
-
-#endif /* CONFIG_NFILE_DESCRIPTORS > 0 ... */
-

fs/vfs/fs_dup2.c
@@ -1,7 +1,7 @@
 /****************************************************************************
- * fs/fs_close.c
+ * fs/vfs/fs_dup2.c
  *
- *   Copyright (C) 2007-2009, 2012 Gregory Nutt. All rights reserved.
+ *   Copyright (C) 2007-2009, 2011, 2013 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>
  *
  * Redistribution and use in source and binary forms, with or without
@@ -42,92 +42,68 @@
 #include <unistd.h>
 #include <sched.h>
 #include <errno.h>
-#include <nuttx/fs/fs.h>
-
-#if defined(CONFIG_NET) && CONFIG_NSOCKET_DESCRIPTORS > 0
-# include <nuttx/net/net.h>
-#endif
 
 #include "fs.h"
+
+/* This logic in this applies only when both socket and file descriptors are
+ * in that case, this function descriminates which type of dup2 is being
+ * performed.
+ */
+
+#if CONFIG_NFILE_DESCRIPTORS > 0 && defined(CONFIG_NET) && CONFIG_NSOCKET_DESCRIPTORS > 0
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Private Functions
+ ****************************************************************************/
 
 /****************************************************************************
  * Global Functions
  ****************************************************************************/
 
 /****************************************************************************
- * Name: close
+ * Name: dup2
  *
  * Description:
- *   close() closes a file descriptor, so that it no longer refers to any
- *   file and may be reused. Any record locks (see fcntl(2)) held on the file
- *   it was associated with, and owned by the process, are removed (regardless
- *   of the file descriptor that was used to obtain the lock).
- *
- *   If fd is the last copy of a particular file descriptor the resources
- *   associated with it are freed; if the descriptor was the last reference
- *   to a file which has been removed using unlink(2) the file is deleted.
- *
- * Parameters:
- *   fd   file descriptor to close
- *
- * Returned Value:
- *   0 on success; -1 on error with errno set appropriately.
- *
- * Assumptions:
+ *   Clone a file descriptor or socket descriptor to a specific descriptor
+ *   number
  *
  ****************************************************************************/
 
-int close(int fd)
+int dup2(int fd1, int fd2)
 {
-  int err;
-#if CONFIG_NFILE_DESCRIPTORS > 0
-  int ret;
-
-  /* Did we get a valid file descriptor? */
-
-  if ((unsigned int)fd >= CONFIG_NFILE_DESCRIPTORS)
-#endif
-    {
-      /* Close a socket descriptor */
-
-#if defined(CONFIG_NET) && CONFIG_NSOCKET_DESCRIPTORS > 0
-      if ((unsigned int)fd < (CONFIG_NFILE_DESCRIPTORS+CONFIG_NSOCKET_DESCRIPTORS))
-        {
-          return net_close(fd);
-        }
-      else
-#endif
-        {
-          err = EBADF;
-          goto errout;
-        }
-    }
-
-#if CONFIG_NFILE_DESCRIPTORS > 0
-  /* Close the driver or mountpoint.  NOTES: (1) there is no
-   * exclusion mechanism here , the driver or mountpoint must be
-   * able to handle concurrent operations internally, (2) The driver
-   * may have been opened numerous times (for different file
-   * descriptors) and must also handle being closed numerous times.
-   * (3) for the case of the mountpoint, we depend on the close
-   * methods bing identical in signature and position in the operations
-   * vtable.
+  /* Check the range of the descriptor to see if we got a file or a socket
+   * descriptor.
    */
 
-  ret = files_close(fd);
-  if (ret < 0)
+  if ((unsigned int)fd1 >= CONFIG_NFILE_DESCRIPTORS)
     {
-      /* An error occurred while closing the driver */
+      /* Not a valid file descriptor.  Did we get a valid socket descriptor? */
 
-      err = -ret;
-      goto errout;
+      if ((unsigned int)fd1 < (CONFIG_NFILE_DESCRIPTORS+CONFIG_NSOCKET_DESCRIPTORS))
+        {
+          /* Yes.. dup the socket descriptor */
+
+          return net_dup2(fd1, fd2);
+        }
+      else
+        {
+          /* No.. then it is a bad descriptor number */
+
+          set_errno(EBADF);
+          return ERROR;
+        }
     }
-  return OK;
+  else
+    {
+      /* Its a valid file descriptor.. dup the file descriptor */
 
-#endif
-
-errout:
-  set_errno(err);
-  return ERROR;
+      return file_dup2(fd1, fd2);
+    }
 }
+
+#endif /* CONFIG_NFILE_DESCRIPTORS > 0 ... */
 

fs/vfs/fs_fcntl.c
@@ -1,5 +1,5 @@
 /****************************************************************************
- * fs/fs_fcntl.c
+ * fs/vfs/fs_fcntl.c
  *
  *   Copyright (C) 2009, 2012-2013 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>

fs/vfs/fs_fdopen.c
@@ -1,5 +1,5 @@
 /****************************************************************************
- * fs/fs_fdopen.c
+ * fs/vfs/fs_fdopen.c
  *
  *   Copyright (C) 2007-2014 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>

commit d3408809e4c57e6d4e5f49cc35286bcd7761ccd6

sendfile():  Fix error introduced with commit ff73be870e38959b0aaee5961dc47b4b58dc2d86.  Noted by Maciej Wójcik

arch/arm/src/stm32/chip/stm32f33xxx_hrtim.h
@@ -2060,4 +2060,4 @@
 #define HRTIM_BDMADR_SHIFT             0         /* Bits 0-31: Burst DMA Data register */
 #define HRTIM_BDMADR_MASK              (0xffffffff << HRTIM_BDMADR_SHIFT)
 
-#endif                          /* __ARCH_ARM_SRC_STM32_CHIP_STM32_HRTIM_H */
+#endif /* __ARCH_ARM_SRC_STM32_CHIP_STM32_HRTIM_H */

fs/vfs/fs_sendfile.c
@@ -1,7 +1,7 @@
 /****************************************************************************
  * fs/vfs/fs_sendfile.c
  *
- *   Copyright (C) 2007, 2009, 2011, 2013 Gregory Nutt. All rights reserved.
+ *   Copyright (C) 2007, 2009, 2011, 2013, 2017 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>
  *
  * Redistribution and use in source and binary forms, with or without
@@ -103,7 +103,6 @@
 ssize_t sendfile(int outfd, int infd, off_t *offset, size_t count)
 {
 #if defined(CONFIG_NET_TCP) && CONFIG_NSOCKET_DESCRIPTORS > 0
-
   /* Check the destination file descriptor:  Is it a (probable) file
    * descriptor?  Check the source file:  Is it a normal file?
    */
@@ -117,7 +116,7 @@ ssize_t sendfile(int outfd, int infd, off_t *offset, size_t count)
        * structure.
        */
 
-      filep = fs_getfilep(fd);
+      filep = fs_getfilep(infd);
       if (!filep)
         {
           /* The errno value has already been set */