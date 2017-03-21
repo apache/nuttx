3872055daa9e9d8410a3e356a0a61248239ed43f
Gregory Nutt <gnutt@nuttx.org>
2017-03-21 07:20:45 -0600

    drivers/audio/wm8904: WM8904 has same problem as that fixed by Juha Niskanen in the MaxTouch driver.

--- drivers/input/mxt.c
+++ drivers/input/mxt.c
@@ -1,7 +1,7 @@
 /****************************************************************************
  * drivers/input/mxt.c
  *
- *   Copyright (C) 2014, 2016 Gregory Nutt. All rights reserved.
+ *   Copyright (C) 2014, 2016-2017 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>
  *
  * Redistribution and use in source and binary forms, with or without
