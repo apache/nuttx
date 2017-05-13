7fd08a60a326dc13b1554b246dc4d29c0b440f58
Gregory Nutt <gnutt@nuttx.org>
2017-05-13 16:53:55 -0600

    Move prototype from nuttx/include/nuttx/arch.h to apps/include/platform/cxxinitialize.h

--- include/nuttx/arch.h
+++ include/nuttx/arch.h
@@ -1941,26 +1941,6 @@ void up_mdelay(unsigned int milliseconds);
 void up_udelay(useconds_t microseconds);
 
 /****************************************************************************
- * Name: up_cxxinitialize
- *
- * Description:
- *   If C++ and C++ static constructors are supported, then this function
- *   must be provided by board-specific logic in order to perform
- *   initialization of the static C++ class instances.
- *
- *   This function should then be called in the application-specific
- *   logic in order to perform the C++ initialization.  NOTE  that no
- *   component of the core NuttX RTOS logic is involved; This function
- *   definition only provides the 'contract' between application
- *   specific C++ code and platform-specific toolchain support
- *
- ****************************************************************************/
-
-#if defined(CONFIG_HAVE_CXX) && defined(CONFIG_HAVE_CXXINITIALIZE)
-void up_cxxinitialize(void);
-#endif
-
-/****************************************************************************
  * These are standard interfaces that are exported by the OS for use by the
  * architecture specific logic
  ****************************************************************************/
