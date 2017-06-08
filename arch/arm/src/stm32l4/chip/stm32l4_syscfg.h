1a405d2881b28b42a8899d428341606ba5ab1998
Gregory Nutt <gnutt@nuttx.org>
2017-06-08 09:07:04 -0600

    STM32L4:  Add L475 syscfg register definitions.

--- arch/arm/src/stm32l4/chip/stm32l4_syscfg.h
+++ arch/arm/src/stm32l4/chip/stm32l4_syscfg.h
@@ -43,10 +43,12 @@
 #include <nuttx/config.h>
 #include "chip.h"
 
-#if defined(CONFIG_STM32L4_STM32L4X6)
-#  include "chip/stm32l4x6xx_syscfg.h"
-#elif defined(CONFIG_STM32L4_STM32L4X3)
+#if defined(CONFIG_STM32L4_STM32L4X3)
 #  include "chip/stm32l4x3xx_syscfg.h"
+#elif defined(CONFIG_STM32L4_STM32L4X5)
+#  include "chip/stm32l4x5xx_syscfg.h"
+#elif defined(CONFIG_STM32L4_STM32L4X6)
+#  include "chip/stm32l4x6xx_syscfg.h"
 #else
 #  error "Unsupported STM32 L4 chip"
 #endif
