c1b6edec13b70d4e401ac766c5e22aa1093d86ce
Gregory Nutt <gnutt@nuttx.org>
2017-08-13 06:43:34 -0600

    Costmetic fix to comment

--- libxx/libxx_delete_sized.cxx
+++ libxx/libxx_delete_sized.cxx
@@ -1,7 +1,7 @@
 //***************************************************************************
 // libxx/libxx_delete_sized.cxx
 //
-//   Copyright (C) 2007 Gregory Nutt. All rights reserved.
+//   Copyright (C) 2017 Gregory Nutt. All rights reserved.
 //   Author: Gregory Nutt <gnutt@nuttx.org>
 //
 // Redistribution and use in source and binary forms, with or without
