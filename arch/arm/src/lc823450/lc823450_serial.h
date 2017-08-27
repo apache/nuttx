56bf5b2a98b076c10c33e4327c0c7dcab53f8357
Masayuki Ishikawa <Masayuki.Ishikawa@jp.sony.com>
2017-08-27 19:00:31 +0900

    arch/arm/src/lc823450: Conform to the NuttX coding style
    Signed-off-by: Masayuki Ishikawa <Masayuki.Ishikawa@jp.sony.com>

--- arch/arm/src/lc823450/lc823450_serial.h
+++ arch/arm/src/lc823450/lc823450_serial.h
@@ -41,6 +41,7 @@
  * Pre-processor Definitions
  ************************************************************************************/
 
+
 #define LC823450_UART0_REGBASE  0x4008b000
 #define LC823450_UART1_REGBASE  0x4008c000
 #define LC823450_UART2_REGBASE  0x4008d000
