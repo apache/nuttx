8e6c78f42595fedb1da6b391caf8a01615843101
Gregory Nutt <gnutt@nuttx.org>
2017-08-30 13:44:13 -0600

    Fix last commit.  It violates the NuttX coding standard because it is not C89 compliant.

--- drivers/rwbuffer.c
+++ drivers/rwbuffer.c
@@ -752,6 +752,9 @@ void rwb_uninitialize(FAR struct rwbuffer_s *rwb)
 ssize_t rwb_read(FAR struct rwbuffer_s *rwb, off_t startblock,
                  size_t nblocks, FAR uint8_t *rdbuffer)
 {
+#ifdef CONFIG_DRVR_READAHEAD
+  size_t remaining;
+#endif
   int ret = OK;
 
   finfo("startblock=%ld nblocks=%ld rdbuffer=%p\n",
@@ -781,8 +784,6 @@ ssize_t rwb_read(FAR struct rwbuffer_s *rwb, off_t startblock,
 #endif
 
 #ifdef CONFIG_DRVR_READAHEAD
-  size_t remaining;
-
   if (rwb->rhmaxblocks > 0)
     {
       /* Loop until we have read all of the requested blocks */
