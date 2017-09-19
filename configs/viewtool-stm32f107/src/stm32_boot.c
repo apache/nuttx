7d2b9fc5cd414287a10d365ca368742758601a81
Gregory Nutt <gnutt@nuttx.org>
2017-09-19 11:44:17 -0600

    viewtool-stm32f107:  Replace old stm32_appinit.c (now stm32_bringup.c) with 'standard' stm32_appinit.c from the STM32F4-Discovery.  Add CONFIG_BOARD_INITIALIZE logic to stm32_boot.c

--- configs/viewtool-stm32f107/src/stm32_boot.c
+++ configs/viewtool-stm32f107/src/stm32_boot.c
@@ -87,3 +87,25 @@ void stm32_boardinitialize(void)
 
   stm32_led_initialize();
 }
+
+/****************************************************************************
+ * Name: board_initialize
+ *
+ * Description:
+ *   If CONFIG_BOARD_INITIALIZE is selected, then an additional
+ *   initialization call will be performed in the boot-up sequence to a
+ *   function called board_initialize().  board_initialize() will be
+ *   called immediately after up_initialize() is called and just before the
+ *   initial application is started.  This additional initialization phase
+ *   may be used, for example, to initialize board-specific device drivers.
+ *
+ ****************************************************************************/
+
+#ifdef CONFIG_BOARD_INITIALIZE
+void board_initialize(void)
+{
+  /* Perform board-specific initialization */
+
+  (void)stm32_bringup();
+}
+#endif
