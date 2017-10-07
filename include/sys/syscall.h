7154fc09ffc9a986970d8537e74a09a02f5dace4
Gregory Nutt <gnutt@nuttx.org>
2017-10-07 07:46:42 -0600

    syscall/: Fix some backward conditional compilation

--- include/sys/syscall.h
+++ include/sys/syscall.h
@@ -302,7 +302,7 @@
 #  else
 #    define __SYS_termios               __SYS_poll
 #  endif
-#  ifndef CONFIG_SERIAL_TERMIOS
+#  ifdef CONFIG_SERIAL_TERMIOS
 #    define SYS_tcdrain                __SYS_termios
 #    define __SYS_boardctl             (__SYS_termios+1)
 #  else
