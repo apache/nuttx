ce42b0e030da3c9370bee1c33f36d8e8fd18423b
Gregory Nutt <gnutt@nuttx.org>
2017-11-18 19:26:38 -0600

    include/syslog.h:  Trivial change in name of formal parameter for consistency.

--- include/syslog.h
+++ include/syslog.h
@@ -212,8 +212,8 @@ void closelog(void);
  *
  ****************************************************************************/
 
-int syslog(int priority, FAR const IPTR char *format, ...);
-int vsyslog(int priority, FAR const IPTR char *src, va_list ap);
+int syslog(int priority, FAR const IPTR char *fmt, ...);
+int vsyslog(int priority, FAR const IPTR char *fmt, va_list ap);
 
 /****************************************************************************
  * Name: setlogmask
