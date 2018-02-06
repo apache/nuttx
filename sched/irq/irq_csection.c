apache/nuttx

commit 37c9b3d54aa82272d9d85a8f7294b53fd8842d43

sched/sched:  Implements a global scheduler lock capability as part of SMP support.  This allows the scheduler to be locked with no knowledge or access to the TCB of the currently running task.  This is necessary because accessing the TCB of the currenlty running task is, itself, a non-atomic operation.  This global scheduler lock cpability was add just to support that atomic access to the TCB.

sched/irq/irq_csection.c
@@ -1,7 +1,7 @@
 /****************************************************************************
  * sched/irq/irq_csection.c
  *
- *   Copyright (C) 2016-2017 Gregory Nutt. All rights reserved.
+ *   Copyright (C) 2016-2018 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>
  *
  * Redistribution and use in source and binary forms, with or without
@@ -506,7 +506,7 @@ void leave_critical_section(irqstate_t flags)
                */
 
               cpu = this_cpu();
-              DEBUGASSERT(spin_islocked(&g_cpu_irqlock) &&
+              DEBUGASSERT(sched_islocked_global() &&
                           (g_cpu_irqset & (1 << cpu)) != 0);
 
               /* Check if releasing the lock held by this CPU will unlock the
@@ -521,8 +521,7 @@ void leave_critical_section(irqstate_t flags)
                    * because we were within a critical section then.
                    */
 
-                  if (g_pendingtasks.head != NULL &&
-                      !spin_islocked(&g_cpu_schedlock))
+                  if (g_pendingtasks.head != NULL && !sched_islocked_global())
                     {
                       /* Release any ready-to-run tasks that have collected
                        * in g_pendingtasks.  NOTE: This operation has a very
@@ -615,6 +614,15 @@ bool irq_cpu_locked(int cpu)
 
       return false;
     }
+
+#ifdef CONFIG_ARCH_HAVE_FETCHADD
+  /* If the global lockcount has been incremented then simply return true */
+
+  if (g_global_lockcount > 0)
+    {
+      return true;
+    }
+#endif
 
   /* Test if g_cpu_irqlock is locked.  We don't really need to use check
    * g_cpu_irqlock to do this, we can use the g_cpu_set.

include/nuttx/arch.h
@@ -1705,9 +1705,9 @@ int up_timer_start(FAR const struct timespec *ts);
  ****************************************************************************/
 
 #ifdef CONFIG_ARCH_HAVE_FETCHADD
-int32_t up_fetchadd32(FAR int32_t *addr, int32_t value);
-int16_t up_fetchadd16(FAR int16_t *addr, int16_t value);
-int8_t up_fetchadd8(FAR int8_t *addr, int8_t value);
+int32_t up_fetchadd32(FAR volatile int32_t *addr, int32_t value);
+int16_t up_fetchadd16(FAR volatile int16_t *addr, int16_t value);
+int8_t up_fetchadd8(FAR volatile int8_t *addr, int8_t value);
 #endif
 
 /****************************************************************************
@@ -1729,9 +1729,9 @@ int8_t up_fetchadd8(FAR int8_t *addr, int8_t value);
  ****************************************************************************/
 
 #ifdef CONFIG_ARCH_HAVE_FETCHADD
-int32_t up_fetchsub32(FAR int32_t *addr, int32_t value);
-int16_t up_fetchsub16(FAR int16_t *addr, int16_t value);
-int8_t up_fetchsub8(FAR int8_t *addr, int8_t value);
+int32_t up_fetchsub32(FAR volatile int32_t *addr, int32_t value);
+int16_t up_fetchsub16(FAR volatile int16_t *addr, int16_t value);
+int8_t up_fetchsub8(FAR volatile int8_t *addr, int8_t value);
 #endif
 
 /****************************************************************************

sched/sched/Make.defs
@@ -1,7 +1,7 @@
 ############################################################################
 # sched/sched/Make.defs
 #
-#   Copyright (C) 2014 Gregory Nutt. All rights reserved.
+#   Copyright (C) 2014, 2018 Gregory Nutt. All rights reserved.
 #   Author: Gregory Nutt <gnutt@nuttx.org>
 #
 # Redistribution and use in source and binary forms, with or without
@@ -101,6 +101,8 @@ endif
 ifeq ($(CONFIG_SMP),y)
 CSRCS += sched_tasklistlock.c
 ifeq ($(CONFIG_ARCH_GLOBAL_IRQDISABLE),y)
+CSRCS += sched_thistask.c
+else ifeq ($(CONFIG_ARCH_HAVE_FETCHADD),y)
 CSRCS += sched_thistask.c
 endif
 endif

sched/sched/sched.h
@@ -73,16 +73,14 @@
 /* These are macros to access the current CPU and the current task on a CPU.
  * These macros are intended to support a future SMP implementation.
  * NOTE: this_task() for SMP is implemented in sched_thistask.c if the CPU
- * supports disabling of inter-processor interrupts.
- *
- * REVISIT: A mechanism to provide an atomic this_task() is still required
- * for the case where where inter-processor interrupts cannot be disabled!
+ * supports disabling of inter-processor interrupts or if it supports the
+ * atomic fetch add operation.
  */
 
 #ifdef CONFIG_SMP
 #  define current_task(cpu)      ((FAR struct tcb_s *)g_assignedtasks[cpu].head)
 #  define this_cpu()             up_cpu_index()
-#  ifndef CONFIG_ARCH_GLOBAL_IRQDISABLE
+#  if !defined(CONFIG_ARCH_GLOBAL_IRQDISABLE) && !defined(CONFIG_ARCH_HAVE_FETCHADD)
 #    define this_task()          (current_task(this_cpu()))
 #  endif
 #else
@@ -376,6 +374,14 @@ extern volatile cpu_set_t g_cpu_lockset SP_SECTION;
 
 extern volatile spinlock_t g_cpu_tasklistlock SP_SECTION;
 
+#ifdef CONFIG_ARCH_HAVE_FETCHADD
+/* This is part of the sched_lock() logic to handle atomic operations when
+ * locking the scheduler.
+ */
+
+extern volatile int16_t g_global_lockcount;
+#endif
+
 #endif /* CONFIG_SMP */
 
 /****************************************************************************
@@ -435,7 +441,7 @@ void sched_sporadic_lowpriority(FAR struct tcb_s *tcb);
 #endif
 
 #ifdef CONFIG_SMP
-#ifdef CONFIG_ARCH_GLOBAL_IRQDISABLE
+#if defined(CONFIG_ARCH_GLOBAL_IRQDISABLE) || defined(CONFIG_ARCH_HAVE_FETCHADD)
 FAR struct tcb_s *this_task(void);
 #endif
 
@@ -445,11 +451,20 @@ int  sched_cpu_pause(FAR struct tcb_s *tcb);
 irqstate_t sched_tasklist_lock(void);
 void sched_tasklist_unlock(irqstate_t lock);
 
-#  define sched_islocked(tcb) spin_islocked(&g_cpu_schedlock)
+#ifdef CONFIG_ARCH_HAVE_FETCHADD
+#  define sched_islocked_global() \
+     (spin_islocked(&g_cpu_schedlock) || g_global_lockcount > 0)
 #else
-#  define sched_cpu_select(a) (0)
-#  define sched_cpu_pause(t)  (-38)  /* -ENOSYS */
-#  define sched_islocked(tcb) ((tcb)->lockcount > 0)
+#  define sched_islocked_global() \
+     spin_islocked(&g_cpu_schedlock)
+#endif
+
+#  define sched_islocked_tcb(tcb) sched_islocked_global()
+
+#else
+#  define sched_cpu_select(a)     (0)
+#  define sched_cpu_pause(t)      (-38)  /* -ENOSYS */
+#  define sched_islocked_tcb(tcb) ((tcb)->lockcount > 0)
 #endif
 
 /* CPU load measurement support */

sched/sched/sched_addreadytorun.c
@@ -1,7 +1,8 @@
 /****************************************************************************
  * sched/sched/sched_addreadytorun.c
  *
- *   Copyright (C) 2007-2009, 2014, 2016-2017 Gregory Nutt. All rights reserved.
+ *   Copyright (C) 2007-2009, 2014, 2016-2018 Gregory Nutt. All rights
+ *     reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>
  *
  * Redistribution and use in source and binary forms, with or without
@@ -194,7 +195,7 @@ bool sched_addreadytorun(FAR struct tcb_s *btcb)
       cpu = sched_cpu_select(btcb->affinity);
     }
 
-  /* Get the task currently running on the CPU (maybe the IDLE task) */
+  /* Get the task currently running on the CPU (may be the IDLE task) */
 
   rtcb = (FAR struct tcb_s *)g_assignedtasks[cpu].head;
 
@@ -240,7 +241,7 @@ bool sched_addreadytorun(FAR struct tcb_s *btcb)
    */
 
   me = this_cpu();
-  if ((spin_islocked(&g_cpu_schedlock) || irq_cpu_locked(me)) &&
+  if ((sched_islocked_global() || irq_cpu_locked(me)) &&
       task_state != TSTATE_TASK_ASSIGNED)
     {
       /* Add the new ready-to-run task to the g_pendingtasks task list for
@@ -387,7 +388,7 @@ bool sched_addreadytorun(FAR struct tcb_s *btcb)
                * different CPU the next time that it runs.
                */
 
-              if (spin_islocked(&g_cpu_schedlock))
+              if (sched_islocked_global())
                 {
                   next->task_state = TSTATE_TASK_PENDING;
                   tasklist         = (FAR dq_queue_t *)&g_pendingtasks;

sched/sched/sched_lock.c
@@ -1,7 +1,7 @@
 /****************************************************************************
  * sched/sched/sched_lock.c
  *
- *   Copyright (C) 2007, 2009, 2016 Gregory Nutt. All rights reserved.
+ *   Copyright (C) 2007, 2009, 2016, 2018 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>
  *
  * Redistribution and use in source and binary forms, with or without
@@ -116,6 +116,13 @@ volatile spinlock_t g_cpu_schedlock SP_SECTION = SP_UNLOCKED;
 volatile spinlock_t g_cpu_locksetlock SP_SECTION;
 volatile cpu_set_t g_cpu_lockset SP_SECTION;
 
+#ifdef CONFIG_ARCH_HAVE_FETCHADD
+/* This is part of the sched_lock() logic to handle atomic operations when
+ * locking the scheduler.
+ */
+
+volatile int16_t g_global_lockcount;
+#endif
 #endif /* CONFIG_SMP */
 
 /****************************************************************************
@@ -140,16 +147,32 @@ volatile cpu_set_t g_cpu_lockset SP_SECTION;
  *
  ****************************************************************************/
 
+#ifdef CONFIG_SMP
+
 int sched_lock(void)
 {
-  FAR struct tcb_s *rtcb = this_task();
+  FAR struct tcb_s *rtcb;
+  int cpu;
 
-  /* Check for some special cases:  (1) rtcb may be NULL only during early
-   * boot-up phases, and (2) sched_lock() should have no effect if called
-   * from the interrupt level.
+  /* The following operation is non-atomic unless CONFIG_ARCH_HAVE_FETCHADD
+   * defined.
    */
 
-  if (rtcb && !up_interrupt_context())
+#ifdef CONFIG_ARCH_HAVE_FETCHADD
+   DEBUGASSERT((uint16_t)g_global_lockcount < INT16_MAX); /* Not atomic! */
+   (void)up_fetchadd16(&g_global_lockcount, 1);
+#endif
+
+  /* This operation is save if CONFIG_ARCH_HAVE_FETCHADD is defined.  NOTE
+   * we cannot use this_task() because it calls sched_lock().
+   */
+
+  cpu  = this_cpu();
+  rtcb = current_task(cpu);
+
+  /* rtcb may be NULL only during early boot-up phases. */
+
+  if (rtcb != NULL)
     {
       /* Catch attempts to increment the lockcount beyond the range of the
        * integer type.
@@ -157,7 +180,6 @@ int sched_lock(void)
 
       DEBUGASSERT(rtcb->lockcount < MAX_LOCK_COUNT);
 
-#ifdef CONFIG_SMP
       /* We must hold the lock on this CPU before we increment the lockcount
        * for the first time. Holding the lock is sufficient to lockout context
        * switching.
@@ -184,14 +206,12 @@ int sched_lock(void)
           DEBUGASSERT(g_cpu_schedlock == SP_LOCKED &&
                       (g_cpu_lockset & (1 << this_cpu())) != 0);
         }
-#endif
 
       /* A counter is used to support locking.  This allows nested lock
        * operations on this thread (on any CPU)
        */
 
       rtcb->lockcount++;
-
 
 #ifdef CONFIG_SCHED_INSTRUMENTATION_PREEMPTION
       /* Check if we just acquired the lock */
@@ -204,7 +224,6 @@ int sched_lock(void)
         }
 #endif
 
-#ifdef CONFIG_SMP
       /* Move any tasks in the ready-to-run list to the pending task list
        * where they will not be available to run until the scheduler is
        * unlocked and sched_mergepending() is called.
@@ -213,8 +232,54 @@ int sched_lock(void)
       sched_mergeprioritized((FAR dq_queue_t *)&g_readytorun,
                              (FAR dq_queue_t *)&g_pendingtasks,
                              TSTATE_TASK_PENDING);
+    }
+
+#ifdef CONFIG_ARCH_HAVE_FETCHADD
+   DEBUGASSERT(g_global_lockcount > 0);
+   (void)up_fetchsub16(&g_global_lockcount, 1);
+#endif
+
+  return OK;
+}
+
+#else /* CONFIG_SMP */
+
+int sched_lock(void)
+{
+  FAR struct tcb_s *rtcb = this_task();
+
+  /* Check for some special cases:  (1) rtcb may be NULL only during early
+   * boot-up phases, and (2) sched_lock() should have no effect if called
+   * from the interrupt level.
+   */
+
+  if (rtcb != NULL && !up_interrupt_context())
+    {
+      /* Catch attempts to increment the lockcount beyond the range of the
+       * integer type.
+       */
+
+      DEBUGASSERT(rtcb->lockcount < MAX_LOCK_COUNT);
+
+      /* A counter is used to support locking.  This allows nested lock
+       * operations on this thread (on any CPU)
+       */
+
+      rtcb->lockcount++;
+
+#ifdef CONFIG_SCHED_INSTRUMENTATION_PREEMPTION
+      /* Check if we just acquired the lock */
+
+      if (rtcb->lockcount == 1)
+        {
+          /* Note that we have pre-emption locked */
+
+          sched_note_premption(rtcb, true);
+        }
 #endif
     }
 
   return OK;
 }
+
+#endif /* CONFIG_SMP */

sched/sched/sched_mergepending.c
@@ -210,7 +210,7 @@ bool sched_mergepending(void)
    */
 
   me = this_cpu();
-  if (!spin_islocked(&g_cpu_schedlock) && !irq_cpu_locked(me))
+  if (!sched_islocked_global() && !irq_cpu_locked(me))
     {
       /* Find the CPU that is executing the lowest priority task */
 
@@ -248,7 +248,7 @@ bool sched_mergepending(void)
            * Check if that happened.
            */
 
-          if (spin_islocked(&g_cpu_schedlock) || irq_cpu_locked(me))
+          if (sched_islocked_global() || irq_cpu_locked(me))
             {
               /* Yes.. then we may have incorrectly placed some TCBs in the
                * g_readytorun list (unlikely, but possible).  We will have to

sched/sched/sched_removereadytorun.c
@@ -208,7 +208,7 @@ bool sched_removereadytorun(FAR struct tcb_s *rtcb)
        * REVISIT: What if it is not the IDLE thread?
        */
 
-      if (!spin_islocked(&g_cpu_schedlock) && !irq_cpu_locked(me))
+      if (!sched_islocked_global() && !irq_cpu_locked(me))
         {
           /* Search for the highest priority task that can run on this
            * CPU.

commit 56bb76caca2204fd405dcc77a53ed24c3ab202cd

sched/irq:  Fix an error in a assertion introduced in commit 37c9b3d54aa82272d9d85a8f7294b53fd8842d43.  Noted by Masayuki Ishikawa.

sched/irq/irq_csection.c
@@ -506,7 +506,7 @@ void leave_critical_section(irqstate_t flags)
                */
 
               cpu = this_cpu();
-              DEBUGASSERT(sched_islocked_global() &&
+              DEBUGASSERT(spin_islocked(&g_cpu_irqlock) &&
                           (g_cpu_irqset & (1 << cpu)) != 0);
 
               /* Check if releasing the lock held by this CPU will unlock the