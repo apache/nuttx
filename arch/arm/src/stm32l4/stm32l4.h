apache/nuttx

commit 9223123cd2cb698ec7ca540460d7505a6f33d65b

arch/arm/src/stm32l4: Added custom pin mapping for STM32L4X2XX processors.

arch/arm/src/stm32l4/Kconfig
@@ -400,7 +400,14 @@ config STM32L4_STM32L4X1
 config STM32L4_STM32L4X2
 	bool
 	default n
-	select STM32L4_STM32L4X3
+	select ARCH_HAVE_FPU
+	select STM32L4_HAVE_USART1
+	select STM32L4_HAVE_USART2
+	select STM32L4_HAVE_UART3
+	select STM32L4_HAVE_LPTIM1
+	select STM32L4_HAVE_LPTIM2
+	select STM32L4_HAVE_COMP
+	select STM32L4_HAVE_SAI1
 
 config STM32L4_STM32L4X3
 	bool

arch/arm/include/stm32l4/irq.h
@@ -76,7 +76,7 @@
 
 #define STM32L4_IRQ_FIRST         (16) /* Vector number of the first external interrupt */
 
-#if defined(CONFIG_STM32L4_STM32L4X3)
+#if defined(CONFIG_STM32L4_STM32L4X2) || defined(CONFIG_STM32L4_STM32L4X3)
 #  include <arch/stm32l4/stm32l4x3xx_irq.h>
 #elif defined(CONFIG_STM32L4_STM32L4X5)
 #  include <arch/stm32l4/stm32l4x5xx_irq.h>

arch/arm/include/stm32l4/stm32l4x3xx_irq.h
@@ -54,9 +54,13 @@
  * NVIC.  This does, however, waste several words of memory in the IRQ to handle mapping tables.
  *
  * Processor Exceptions (vectors 0-15).  These common definitions can be found in the file
- * nuttx/arch/arm/include/stm32f7/irq.h which includes this file
+ * nuttx/arch/arm/include/stm32l4/irq.h which includes this file
  *
  * External interrupts (vectors >= 16)
+ *
+ * These interrupts vectors was implemented based on RM0394 Table 45 and should work for
+ * STM32L431xx, STM32L451xx, STM32L4X2 and STM32L4X3.
+ *
  */
 
 #define STM32L4_IRQ_WWDG        (STM32L4_IRQ_FIRST + 0)  /* 0:  Window Watchdog interrupt */
@@ -71,12 +75,12 @@
 #define STM32L4_IRQ_EXTI2       (STM32L4_IRQ_FIRST + 8)  /* 8:  EXTI Line 2 interrupt */
 #define STM32L4_IRQ_EXTI3       (STM32L4_IRQ_FIRST + 9)  /* 9:  EXTI Line 3 interrupt */
 #define STM32L4_IRQ_EXTI4       (STM32L4_IRQ_FIRST + 10) /* 10: EXTI Line 4 interrupt */
-#define STM32L4_IRQ_DMA1CH1     (STM32L4_IRQ_FIRST + 11) /* 12: DMA1 Channel 1 global interrupt */
-#define STM32L4_IRQ_DMA1CH2     (STM32L4_IRQ_FIRST + 12) /* 13: DMA1 Channel 2 global interrupt */
-#define STM32L4_IRQ_DMA1CH3     (STM32L4_IRQ_FIRST + 13) /* 14: DMA1 Channel 3 global interrupt */
-#define STM32L4_IRQ_DMA1CH4     (STM32L4_IRQ_FIRST + 14) /* 15: DMA1 Channel 4 global interrupt */
-#define STM32L4_IRQ_DMA1CH5     (STM32L4_IRQ_FIRST + 15) /* 16: DMA1 Channel 5 global interrupt */
-#define STM32L4_IRQ_DMA1CH6     (STM32L4_IRQ_FIRST + 16) /* 17: DMA1 Channel 6 global interrupt */
+#define STM32L4_IRQ_DMA1CH1     (STM32L4_IRQ_FIRST + 11) /* 11: DMA1 Channel 1 global interrupt */
+#define STM32L4_IRQ_DMA1CH2     (STM32L4_IRQ_FIRST + 12) /* 12: DMA1 Channel 2 global interrupt */
+#define STM32L4_IRQ_DMA1CH3     (STM32L4_IRQ_FIRST + 13) /* 13: DMA1 Channel 3 global interrupt */
+#define STM32L4_IRQ_DMA1CH4     (STM32L4_IRQ_FIRST + 14) /* 14: DMA1 Channel 4 global interrupt */
+#define STM32L4_IRQ_DMA1CH5     (STM32L4_IRQ_FIRST + 15) /* 15: DMA1 Channel 5 global interrupt */
+#define STM32L4_IRQ_DMA1CH6     (STM32L4_IRQ_FIRST + 16) /* 16: DMA1 Channel 6 global interrupt */
 #define STM32L4_IRQ_DMA1CH7     (STM32L4_IRQ_FIRST + 17) /* 17: DMA1 Channel 7 global interrupt */
 #define STM32L4_IRQ_ADC1        (STM32L4_IRQ_FIRST + 18) /* 18: ADC1 global interrupt */
 #define STM32L4_IRQ_CAN1TX      (STM32L4_IRQ_FIRST + 19) /* 19: CAN1 TX interrupts */
@@ -143,7 +147,7 @@
 #define STM32L4_IRQ_I2C4EV      (STM32L4_IRQ_FIRST + 83) /* 83: I2C4 event interrupt */
 #define STM32L4_IRQ_I2C4ER      (STM32L4_IRQ_FIRST + 84) /* 84: I2C4 error interrupt */
 
-#if defined(CONFIG_STM32L4_STM32L4X3)
+#if defined(CONFIG_STM32L4_STM32L4X2) || defined(CONFIG_STM32L4_STM32L4X3)
 #  define STM32L4_IRQ_NEXTINTS  85
 #else
 #  error "Unsupported STM32L4 chip"

arch/arm/src/stm32l4/chip/stm32l4_pinmap.h
@@ -44,7 +44,9 @@
 #include <nuttx/config.h>
 #include "chip.h"
 
-#if defined(CONFIG_STM32L4_STM32L4X3)
+#if defined(CONFIG_STM32L4_STM32L4X2)
+#  include "chip/stm32l4x2xx_pinmap.h"
+#elif defined(CONFIG_STM32L4_STM32L4X3)
 #  include "chip/stm32l4x3xx_pinmap.h"
 #elif defined(CONFIG_STM32L4_STM32L4X5)
 #  include "chip/stm32l4x5xx_pinmap.h"

arch/arm/src/stm32l4/chip/stm32l4_syscfg.h
@@ -43,7 +43,7 @@
 #include <nuttx/config.h>
 #include "chip.h"
 
-#if defined(CONFIG_STM32L4_STM32L4X3)
+#if defined(CONFIG_STM32L4_STM32L4X2) || defined(CONFIG_STM32L4_STM32L4X3)
 #  include "chip/stm32l4x3xx_syscfg.h"
 #elif defined(CONFIG_STM32L4_STM32L4X5)
 #  include "chip/stm32l4x5xx_syscfg.h"

arch/arm/src/stm32l4/chip/stm32l4x2xx_pinmap.h
@@ -1,5 +1,5 @@
 /************************************************************************************
- * arch/arm/src/stm32l4/chip/stm32l4x3xx_pinmap.h
+ * arch/arm/src/stm32l4/chip/stm32l4x2xx_pinmap.h
  *
  *   Copyright (C) 2015-2016 Sebastien Lorquet. All rights reserved.
  *   Authors: Sebastien Lorquet <sebastien@lorquet.fr>
@@ -34,8 +34,8 @@
  *
  ************************************************************************************/
 
-#ifndef __ARCH_ARM_SRC_STM32L4_CHIP_STM32L4X3XX_PINMAP_H
-#define __ARCH_ARM_SRC_STM32L4_CHIP_STM32L4X3XX_PINMAP_H
+#ifndef __ARCH_ARM_SRC_STM32L4_CHIP_STM32L4X2XX_PINMAP_H
+#define __ARCH_ARM_SRC_STM32L4_CHIP_STM32L4X2XX_PINMAP_H
 
 /************************************************************************************
  * Included Files
@@ -138,6 +138,34 @@
 
 #define GPIO_DAC1_OUT_1        (GPIO_ANALOG|GPIO_PORTA|GPIO_PIN4)
 #define GPIO_DAC1_OUT_2        (GPIO_ANALOG|GPIO_PORTA|GPIO_PIN5)
+
+/* Digital Filter for Sigma-Delta Modulators (DFSDM) */
+
+#define GPIO_DFSDM_DATIN0_1    (GPIO_ALT|GPIO_AF6 |GPIO_PORTB|GPIO_PIN1)
+#define GPIO_DFSDM_DATIN0_2    (GPIO_ALT|GPIO_AF6 |GPIO_PORTD|GPIO_PIN3)
+#define GPIO_DFSDM_DATIN0_3    (GPIO_ALT|GPIO_AF6 |GPIO_PORTA|GPIO_PIN7)
+#define GPIO_DFSDM_DATIN1_1    (GPIO_ALT|GPIO_AF6 |GPIO_PORTB|GPIO_PIN12)
+#define GPIO_DFSDM_DATIN1_2    (GPIO_ALT|GPIO_AF6 |GPIO_PORTD|GPIO_PIN6)
+#define GPIO_DFSDM_DATIN1_3    (GPIO_ALT|GPIO_AF6 |GPIO_PORTA|GPIO_PIN9)
+#define GPIO_DFSDM_DATIN2_1    (GPIO_ALT|GPIO_AF6 |GPIO_PORTB|GPIO_PIN14)
+#define GPIO_DFSDM_DATIN2_2    (GPIO_ALT|GPIO_AF6 |GPIO_PORTE|GPIO_PIN7)
+#define GPIO_DFSDM_DATIN3_1    (GPIO_ALT|GPIO_AF6 |GPIO_PORTC|GPIO_PIN7)
+#define GPIO_DFSDM_DATIN3_2    (GPIO_ALT|GPIO_AF6 |GPIO_PORTE|GPIO_PIN4)
+
+#define GPIO_DFSDM_CKIN0_1     (GPIO_ALT|GPIO_AF6 |GPIO_PORTB|GPIO_PIN2)
+#define GPIO_DFSDM_CKIN0_2     (GPIO_ALT|GPIO_AF6 |GPIO_PORTD|GPIO_PIN4)
+#define GPIO_DFSDM_CKIN0_3     (GPIO_ALT|GPIO_AF6 |GPIO_PORTB|GPIO_PIN0)
+#define GPIO_DFSDM_CKIN1_1     (GPIO_ALT|GPIO_AF6 |GPIO_PORTB|GPIO_PIN13)
+#define GPIO_DFSDM_CKIN1_2     (GPIO_ALT|GPIO_AF6 |GPIO_PORTD|GPIO_PIN7)
+#define GPIO_DFSDM_CKIN1_3     (GPIO_ALT|GPIO_AF6 |GPIO_PORTA|GPIO_PIN8)
+#define GPIO_DFSDM_CKIN2_1     (GPIO_ALT|GPIO_AF6 |GPIO_PORTB|GPIO_PIN15)
+#define GPIO_DFSDM_CKIN2_2     (GPIO_ALT|GPIO_AF6 |GPIO_PORTE|GPIO_PIN8)
+#define GPIO_DFSDM_CKIN3_1     (GPIO_ALT|GPIO_AF6 |GPIO_PORTC|GPIO_PIN6)
+#define GPIO_DFSDM_CKIN3_2     (GPIO_ALT|GPIO_AF6 |GPIO_PORTE|GPIO_PIN5)
+
+#define GPIO_DFSDM_CKOUT_1     (GPIO_ALT|GPIO_AF6 |GPIO_PORTA|GPIO_PIN5)
+#define GPIO_DFSDM_CKOUT_2     (GPIO_ALT|GPIO_AF6 |GPIO_PORTC|GPIO_PIN2)
+#define GPIO_DFSDM_CKOUT_3     (GPIO_ALT|GPIO_AF6 |GPIO_PORTE|GPIO_PIN9)
 
 /* I2C */
 
@@ -262,13 +290,9 @@
 /* Single Wire Protocol Interface */
 
 #define GPIO_SWPMI1_IO_1       (GPIO_ALT|GPIO_AF12|GPIO_PORTA|GPIO_PIN8)
-#define GPIO_SWPMI1_IO_2       (GPIO_ALT|GPIO_AF12|GPIO_PORTB|GPIO_PIN12)
 #define GPIO_SWPMI1_TX_1       (GPIO_ALT|GPIO_AF12|GPIO_PORTA|GPIO_PIN13)
-#define GPIO_SWPMI1_TX_2       (GPIO_ALT|GPIO_AF12|GPIO_PORTB|GPIO_PIN13)
 #define GPIO_SWPMI1_RX_1       (GPIO_ALT|GPIO_AF12|GPIO_PORTA|GPIO_PIN14)
-#define GPIO_SWPMI1_RX_2       (GPIO_ALT|GPIO_AF12|GPIO_PORTB|GPIO_PIN14)
 #define GPIO_SWPMI1_SUSPEND_1  (GPIO_ALT|GPIO_AF12|GPIO_PORTA|GPIO_PIN15)
-#define GPIO_SWPMI1_SUSPEND_2  (GPIO_ALT|GPIO_AF12|GPIO_PORTB|GPIO_PIN15)
 
 /* SPI */
 
@@ -494,10 +518,14 @@
 /* Trace */
 
 #define GPIO_TRACECK           (GPIO_ALT|GPIO_AF0 |GPIO_PORTE|GPIO_PIN2)
-#define GPIO_TRACED0           (GPIO_ALT|GPIO_AF0 |GPIO_PORTE|GPIO_PIN3)
-#define GPIO_TRACED1           (GPIO_ALT|GPIO_AF0 |GPIO_PORTE|GPIO_PIN4)
-#define GPIO_TRACED2           (GPIO_ALT|GPIO_AF0 |GPIO_PORTE|GPIO_PIN5)
-#define GPIO_TRACED3           (GPIO_ALT|GPIO_AF0 |GPIO_PORTE|GPIO_PIN6)
+#define GPIO_TRACED0_1         (GPIO_ALT|GPIO_AF0 |GPIO_PORTE|GPIO_PIN3)
+#define GPIO_TRACED1_1         (GPIO_ALT|GPIO_AF0 |GPIO_PORTE|GPIO_PIN4)
+#define GPIO_TRACED2_1         (GPIO_ALT|GPIO_AF0 |GPIO_PORTE|GPIO_PIN5)
+#define GPIO_TRACED3_1         (GPIO_ALT|GPIO_AF0 |GPIO_PORTE|GPIO_PIN6)
+#define GPIO_TRACED0_2         (GPIO_ALT|GPIO_AF0 |GPIO_PORTC|GPIO_PIN1)
+#define GPIO_TRACED1_2         (GPIO_ALT|GPIO_AF0 |GPIO_PORTC|GPIO_PIN10)
+#define GPIO_TRACED2_2         (GPIO_ALT|GPIO_AF0 |GPIO_PORTD|GPIO_PIN2)
+#define GPIO_TRACED3_2         (GPIO_ALT|GPIO_AF0 |GPIO_PORTC|GPIO_PIN12)
 
 /* UARTs/USARTs */
 
@@ -545,6 +573,13 @@
 #define GPIO_USART3_RTS_DE_4   (GPIO_ALT|GPIO_AF7 |GPIO_PORTD|GPIO_PIN2)
 #define GPIO_USART3_RTS_DE_5   (GPIO_ALT|GPIO_AF7 |GPIO_PORTD|GPIO_PIN12)
 
+#define GPIO_UART4_TX_1        (GPIO_ALT|GPIO_AF8 |GPIO_PORTA|GPIO_PIN0)
+#define GPIO_UART4_TX_2        (GPIO_ALT|GPIO_AF8 |GPIO_PORTC|GPIO_PIN10)
+#define GPIO_UART4_RX_1        (GPIO_ALT|GPIO_AF8 |GPIO_PORTA|GPIO_PIN1)
+#define GPIO_UART4_RX_2        (GPIO_ALT|GPIO_AF8 |GPIO_PORTC|GPIO_PIN11)
+#define GPIO_UART4_CTS         (GPIO_ALT|GPIO_AF8 |GPIO_PORTB|GPIO_PIN7)
+#define GPIO_UART4_RTS_DE      (GPIO_ALT|GPIO_AF8 |GPIO_PORTA|GPIO_PIN15)
+
 #define GPIO_LPUART1_TX_1      (GPIO_ALT|GPIO_AF8 |GPIO_PORTA|GPIO_PIN2)
 #define GPIO_LPUART1_TX_2      (GPIO_ALT|GPIO_AF8 |GPIO_PORTB|GPIO_PIN11)
 #define GPIO_LPUART1_TX_3      (GPIO_ALT|GPIO_AF8 |GPIO_PORTC|GPIO_PIN1)
@@ -556,4 +591,4 @@
 #define GPIO_LPUART1_RTS_DE_1  (GPIO_ALT|GPIO_AF8 |GPIO_PORTB|GPIO_PIN1)
 #define GPIO_LPUART1_RTS_DE_2  (GPIO_ALT|GPIO_AF8 |GPIO_PORTB|GPIO_PIN12)
 
-#endif /* __ARCH_ARM_SRC_STM32L4_CHIP_STM32L4X3XX_PINMAP_H */
+#endif /* __ARCH_ARM_SRC_STM32L4_CHIP_STM32L4X2XX_PINMAP_H */

arch/arm/src/stm32l4/chip/stm32l4x3xx_rcc.h
@@ -45,7 +45,7 @@
 
 #include <nuttx/config.h>
 
-#if defined(CONFIG_STM32L4_STM32L4X3)
+#if defined(CONFIG_STM32L4_STM32L4X2) || defined(CONFIG_STM32L4_STM32L4X3)
 
 /****************************************************************************************************
  * Pre-processor Definitions

arch/arm/src/stm32l4/chip/stm32l4x3xx_syscfg.h
@@ -43,7 +43,7 @@
 #include <nuttx/config.h>
 #include "chip.h"
 
-#if defined(CONFIG_STM32L4_STM32L4X3)
+#if defined(CONFIG_STM32L4_STM32L4X2) || defined(CONFIG_STM32L4_STM32L4X3)
 
 /****************************************************************************************************
  * Pre-processor Definitions

commit 84f8e01c17ebb2f376e0c5f281f93d9554960ba3

arch/arm/src/stm32l4:  Fix ADC and COMP breakage caused by CONFIG_STM32L4_STM32L4X2 separation in 9223123c

arch/arm/src/stm32l4/Kconfig
@@ -403,7 +403,7 @@ config STM32L4_STM32L4X2
 	select ARCH_HAVE_FPU
 	select STM32L4_HAVE_USART1
 	select STM32L4_HAVE_USART2
-	select STM32L4_HAVE_UART3
+	select STM32L4_HAVE_USART3 if !(STM32L4_L432XX || STM32L4_L442XX)
 	select STM32L4_HAVE_LPTIM1
 	select STM32L4_HAVE_LPTIM2
 	select STM32L4_HAVE_COMP
@@ -415,12 +415,12 @@ config STM32L4_STM32L4X3
 	select ARCH_HAVE_FPU
 	select STM32L4_HAVE_USART1
 	select STM32L4_HAVE_USART2
-	select STM32L4_HAVE_USART3 if !(STM32L4_L432XX || STM32L4_L442XX)
+	select STM32L4_HAVE_USART3
 	select STM32L4_HAVE_LPTIM1
 	select STM32L4_HAVE_LPTIM2
 	select STM32L4_HAVE_COMP
 	select STM32L4_HAVE_SAI1
-	select STM32L4_HAVE_LCD if !(STM32L4_STM32L4X1 || STM32L4_STM32L4X2)
+	select STM32L4_HAVE_LCD if !STM32L4_STM32L4X1
 
 config STM32L4_STM32L4X5
 	bool

arch/arm/src/stm32l4/stm32l4.h
@@ -59,6 +59,7 @@
 #include "stm32l4_adc.h"
 #include "stm32l4_can.h"
 #include "stm32l4_comp.h"
+#include "stm32l4_dac.h"
 #include "stm32l4_dbgmcu.h"
 #include "stm32l4_dma.h"
 #include "stm32l4_exti.h"

arch/arm/src/stm32l4/stm32l4_adc.c
@@ -76,13 +76,14 @@
 #if defined(CONFIG_STM32L4_ADC1) || defined(CONFIG_STM32L4_ADC2) || \
     defined(CONFIG_STM32L4_ADC3)
 
-/* This implementation is for the STM32L4X3 and STM32L4X6 only */
+#if !(defined(CONFIG_STM32L4_STM32L4X2) || defined(CONFIG_STM32L4_STM32L4X3) || \
+      defined(CONFIG_STM32L4_STM32L4X6))
+#  error "Unrecognized STM32 chip"
+#endif
 
-#if defined(CONFIG_STM32L4_STM32L4X3) || defined(CONFIG_STM32L4_STM32L4X6)
-
-#if defined(CONFIG_STM32L4_STM32L4X3)
+#if defined(CONFIG_STM32L4_STM32L4X2) || defined(CONFIG_STM32L4_STM32L4X3)
 #  if defined(CONFIG_STM32L4_ADC2) || defined(CONFIG_STM32L4_ADC3)
-#    error "Using non-existent ADC on STM32L4X3"
+#    error "Using non-existent ADC"
 #  endif
 #endif
 
@@ -104,7 +105,7 @@
 
 /* ADC interrupts ***********************************************************/
 
-#if defined(CONFIG_STM32L4_STM32L4X3)
+#if defined(CONFIG_STM32L4_STM32L4X2) || defined(CONFIG_STM32L4_STM32L4X3)
 #  define STM32L4_IRQ_ADC12 STM32L4_IRQ_ADC1
 #endif
 
@@ -2034,6 +2035,5 @@ struct adc_dev_s *stm32l4_adc_initialize(int intf, FAR const uint8_t *chanlist,
   return dev;
 }
 
-#endif /* CONFIG_STM32L4_STM32L4X3 || CONFIG_STM32L4_STM32L4X6 */
 #endif /* CONFIG_STM32L4_ADC1 || CONFIG_STM32L4_ADC2 || CONFIG_STM32L4_ADC3 */
 #endif /* CONFIG_ADC */

arch/arm/src/stm32l4/stm32l4_comp.c
@@ -1,5 +1,4 @@
 /****************************************************************************
-
  * arch/arm/src/stm32l4/stm32l4_comp.c
  *
  *   Copyright (c) 2017 Gregory Nutt. All rights reserved.
@@ -38,7 +37,6 @@
  ****************************************************************************/
 
 /****************************************************************************
-
  * Included Files
  ****************************************************************************/
 
@@ -56,8 +54,8 @@
 
 #include <errno.h>
 
-#if !(defined(CONFIG_STM32L4_STM32L4X3) || defined(CONFIG_STM32L4_STM32L4X5) || \
-      defined(CONFIG_STM32L4_STM32L4X6))
+#if !(defined(CONFIG_STM32L4_STM32L4X2) || defined(CONFIG_STM32L4_STM32L4X3) || \
+      defined(CONFIG_STM32L4_STM32L4X5) || defined(CONFIG_STM32L4_STM32L4X6))
 #  error "Unrecognized STM32 chip"
 #endif
 
@@ -375,7 +373,7 @@ static int stm32l4_compconfig(FAR const struct comp_dev_s *dev)
       regval |= COMP_CSR_INPSEL_PIN2;
       break;
 
-#if defined(CONFIG_STM32L4_STM32L4X3)
+#if defined(CONFIG_STM32L4_STM32L4X2) || defined(CONFIG_STM32L4_STM32L4X3)
     case STM32L4_COMP_INP_PIN_3:
       stm32l4_configgpio(cmp == STM32L4_COMP1 ? GPIO_COMP1_INP_3 : GPIO_COMP2_INP_3);
       regval |= COMP_CSR_INPSEL_PIN3;
@@ -439,7 +437,7 @@ static int stm32l4_compconfig(FAR const struct comp_dev_s *dev)
 #endif
       break;
 
-#if defined(CONFIG_STM32L4_STM32L4X3)
+#if defined(CONFIG_STM32L4_STM32L4X2) || defined(CONFIG_STM32L4_STM32L4X3)
     case STM32L4_COMP_INM_PIN_3:
       stm32l4_configgpio(cmp == STM32L4_COMP1 ? GPIO_COMP1_INM_3 : GPIO_COMP2_INM_3);
       regval |= COMP_CSR_INMSEL_INMESEL;

arch/arm/src/stm32l4/stm32l4_comp.h
@@ -50,7 +50,7 @@
  * Public Types
  ************************************************************************************/
 
-#if defined(CONFIG_STM32L4_STM32L4X3)
+#if defined(CONFIG_STM32L4_STM32L4X2) || defined(CONFIG_STM32L4_STM32L4X3)
 
 /* Comparators */
 

arch/arm/src/stm32l4/stm32l4_exti_comp.c
@@ -69,8 +69,8 @@ static struct comp_callback_s g_comp_handlers[STM32L4_COMP_NUM];
 
 static const uint32_t g_comp_lines[STM32L4_COMP_NUM] =
 {
-#if defined(CONFIG_STM32L4_STM32L4X3) || defined (CONFIG_STM32L4_STM32L4X5) || \
-    defined(CONFIG_STM32L4_STM32L4X6)
+#if defined(CONFIG_STM32L4_STM32L4X2) || defined(CONFIG_STM32L4_STM32L4X3) || \
+    defined(CONFIG_STM32L4_STM32L4X5) || defined(CONFIG_STM32L4_STM32L4X6)
   EXTI1_COMP1,
   EXTI1_COMP2
 #else