da4ab2af98dacdb1b36d179412dee7ed6c4beb08
Xiang Xiao <xiaoxiang@xiaomi.com>
2019-01-26 11:56:49 -0600

    include/nuttx/elf.h:  I forgot to add this header file that was introduced in a preceding commiti 095e28d45e3362613ab54697e19c4764347b8466.

---
+++ include/nuttx/elf.h
@@ -0,0 +1,118 @@
+/****************************************************************************
+ * include/nuttx/elf.h
+ *
+ *   Copyright (C) 2019 Pinecone Inc. All rights reserved.
+ *   Author: Xiang Xiao <xiaoxiang@pinecone.net>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name NuttX nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+#ifndef __INCLUDE_NUTTX_ELF_H
+#define __INCLUDE_NUTTX_ELF_H
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <elf32.h>
+
+/****************************************************************************
+ * Public Function Prototypes
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: up_checkarch
+ *
+ * Description:
+ *   Given the ELF header in 'hdr', verify that the module is appropriate
+ *   for the current, configured architecture.  Every architecture that uses
+ *   the module loader must provide this function.
+ *
+ * Input Parameters:
+ *   hdr - The ELF header read from the module file.
+ *
+ * Returned Value:
+ *   True if the architecture supports this module file.
+ *
+ ****************************************************************************/
+
+#ifdef CONFIG_LIBC_ARCH_ELF
+bool up_checkarch(FAR const Elf32_Ehdr *hdr);
+#endif
+
+/****************************************************************************
+ * Name: up_relocate and up_relocateadd
+ *
+ * Description:
+ *   Perform on architecture-specific ELF relocation.  Every architecture
+ *   that uses the module loader must provide this function.
+ *
+ * Input Parameters:
+ *   rel - The relocation type
+ *   sym - The ELF symbol structure containing the fully resolved value.
+ *         There are a few relocation types for a few architectures that do
+ *         not require symbol information.  For those, this value will be
+ *         NULL.  Implementations of these functions must be able to handle
+ *         that case.
+ *   addr - The address that requires the relocation.
+ *
+ * Returned Value:
+ *   Zero (OK) if the relocation was successful.  Otherwise, a negated errno
+ *   value indicating the cause of the relocation failure.
+ *
+ ****************************************************************************/
+
+#ifdef CONFIG_LIBC_ARCH_ELF
+int up_relocate(FAR const Elf32_Rel *rel, FAR const Elf32_Sym *sym,
+                uintptr_t addr);
+int up_relocateadd(FAR const Elf32_Rela *rel,
+                   FAR const Elf32_Sym *sym, uintptr_t addr);
+#endif
+
+/****************************************************************************
+ * Name: up_init_exidx
+ *
+ * Description:
+ *   Initialize the exception index section.
+ *
+ * Input Parameters:
+ *   address - The exception index section address.
+ *   size    - The exception index section size.
+ *
+ * Returned Value:
+ *   Zero (OK) if the initialization was successful. Otherwise, a negated errno
+ *   value indicating the cause of the failure.
+ *
+ ****************************************************************************/
+
+#ifdef CONFIG_CXX_EXCEPTION
+int up_init_exidx(Elf32_Addr address, Elf32_Word size);
+#endif
+
+#endif /* __INCLUDE_NUTTX_ELF_H */
