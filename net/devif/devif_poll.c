apache/nuttx

commit c9b73f51391bd20853c513b232f75c64549d6d1c

net/icmp and icmpv6:  Bind icmp callback from device to connection.  Resolves the issue that bind() could not be called before send()

net/devif/devif_poll.c
@@ -1,7 +1,7 @@
 /****************************************************************************
  * net/devif/devif_poll.c
  *
- *   Copyright (C) 2007-2010, 2012, 2014, 2016-2018 Gregory Nutt. All rights
+ *   Copyright (C) 2007-2010, 2012, 2014, 2016-2019 Gregory Nutt. All rights
  *     reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>
  *
@@ -324,17 +324,27 @@ static int devif_poll_ieee802154_connections(FAR struct net_driver_s *dev,
 static inline int devif_poll_icmp(FAR struct net_driver_s *dev,
                                   devif_poll_callback_t callback)
 {
-  /* Perform the ICMP poll */
+  FAR struct icmp_conn_s *conn = NULL;
+  int bstop = 0;
 
-  icmp_poll(dev);
+  /* Traverse all of the allocated ICMP connections and perform the poll action */
 
-  /* Perform any necessary conversions on outgoing packets */
+  while (!bstop && (conn = icmp_nextconn(conn)) != NULL)
+    {
+      /* Perform the ICMP poll */
 
-  devif_packet_conversion(dev, DEVIF_ICMP);
+      icmp_poll(dev, conn);
 
-  /* Call back into the driver */
+      /* Perform any necessary conversions on outgoing packets */
 
-  return callback(dev);
+      devif_packet_conversion(dev, DEVIF_ICMP);
+
+      /* Call back into the driver */
+
+      bstop = callback(dev);
+    }
+
+  return bstop;
 }
 #endif /* CONFIG_NET_ICMP && CONFIG_NET_ICMP_SOCKET */
 
@@ -350,17 +360,27 @@ static inline int devif_poll_icmp(FAR struct net_driver_s *dev,
 static inline int devif_poll_icmpv6(FAR struct net_driver_s *dev,
                                     devif_poll_callback_t callback)
 {
-  /* Perform the ICMPv6 poll */
+  FAR struct icmpv6_conn_s *conn = NULL;
+  int bstop = 0;
 
-  icmpv6_poll(dev);
+  /* Traverse all of the allocated ICMPV6 connections and perform the poll action */
 
-  /* Perform any necessary conversions on outgoing packets */
+  while (!bstop && (conn = icmpv6_nextconn(conn)) != NULL)
+    {
+      /* Perform the ICMPV6 poll */
 
-  devif_packet_conversion(dev, DEVIF_ICMP6);
+      icmpv6_poll(dev, conn);
 
-  /* Call back into the driver */
+      /* Perform any necessary conversions on outgoing packets */
 
-  return callback(dev);
+      devif_packet_conversion(dev, DEVIF_ICMPV6);
+
+      /* Call back into the driver */
+
+      bstop = callback(dev);
+    }
+
+  return bstop;
 }
 #endif /* CONFIG_NET_ICMPv6_SOCKET || CONFIG_NET_ICMPv6_NEIGHBOR*/
 

net/icmp/icmp.h
@@ -61,8 +61,10 @@
 
 /* Allocate/free an ICMP data callback */
 
-#define icmp_callback_alloc(dev)   devif_callback_alloc(dev, &(dev)->d_conncb)
-#define icmp_callback_free(dev,cb) devif_dev_callback_free(dev, cb)
+#define icmp_callback_alloc(dev, conn) \
+  devif_callback_alloc((dev), &(conn)->list)
+#define icmp_callback_free(dev, conn, cb) \
+  devif_conn_callback_free((dev), (cb), &(conn)->list)
 
 /****************************************************************************
  * Public types
@@ -71,14 +73,14 @@
 #ifdef CONFIG_NET_ICMP_SOCKET
 /* Representation of a IPPROTO_ICMP socket connection */
 
-struct devif_callback_s; /* Forward reference */
+struct devif_callback_s;         /* Forward reference */
 
 struct icmp_conn_s
 {
-  dq_entry_t node;     /* Supports a double linked list */
-  uint16_t   id;       /* ICMP ECHO request ID */
-  uint8_t    nreqs;    /* Number of requests with no response received */
-  uint8_t    crefs;    /* Reference counts on this instance */
+  dq_entry_t node;               /* Supports a double linked list */
+  uint16_t   id;                 /* ICMP ECHO request ID */
+  uint8_t    nreqs;              /* Number of requests with no response received */
+  uint8_t    crefs;              /* Reference counts on this instance */
 
   /* The device that the ICMP request was sent on */
 
@@ -92,6 +94,10 @@ struct icmp_conn_s
 
   struct iob_queue_s readahead;  /* Read-ahead buffering */
 #endif
+
+  /* Defines the list of ICMP callbacks */
+
+  FAR struct devif_callback_s *list;
 };
 #endif
 
@@ -236,7 +242,8 @@ FAR struct icmp_conn_s *icmp_findconn(FAR struct net_driver_s *dev,
  *   Poll a device "connection" structure for availability of ICMP TX data
  *
  * Input Parameters:
- *   dev - The device driver structure to use in the send operation
+ *   dev  - The device driver structure to use in the send operation
+ *   conn - A pointer to the ICMP connection structure
  *
  * Returned Value:
  *   None
@@ -247,7 +254,7 @@ FAR struct icmp_conn_s *icmp_findconn(FAR struct net_driver_s *dev,
  ****************************************************************************/
 
 #ifdef CONFIG_NET_ICMP_SOCKET
-void icmp_poll(FAR struct net_driver_s *dev);
+void icmp_poll(FAR struct net_driver_s *dev, FAR struct icmp_conn_s *conn);
 #endif
 
 /****************************************************************************

net/icmp/icmp_input.c
@@ -2,8 +2,8 @@
  * net/icmp/icmp_input.c
  * Handling incoming ICMP input
  *
- *   Copyright (C) 2007-2009, 2012, 2014-2015, 2017 Gregory Nutt. All rights
- *     reserved.
+ *   Copyright (C) 2007-2009, 2012, 2014-2015, 2017, 2019 Gregory Nutt. All
+ *     rights reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>
  *
  * Adapted for NuttX from logic in uIP which also has a BSD-like license:
@@ -289,28 +289,28 @@ void icmp_input(FAR struct net_driver_s *dev)
 
   else if (ipicmp->type == ICMP_ECHO_REPLY)
     {
+      FAR struct icmp_conn_s *conn;
       uint16_t flags;
 
-      flags = devif_conn_event(dev, NULL, ICMP_ECHOREPLY, dev->d_conncb);
+      /* Nothing consumed the ICMP reply.  That might because this is
+       * an old, invalid reply or simply because the ping application
+       * has not yet put its poll or recv in place.
+       */
+
+      /* Is there any connection that might expect this reply? */
+
+      conn = icmp_findconn(dev, ipicmp->id);
+      if (conn == NULL)
+        {
+          /* No.. drop the packet */
+
+          goto drop;
+        }
+
+      flags = devif_conn_event(dev, conn, ICMP_ECHOREPLY, conn->list);
       if ((flags & ICMP_ECHOREPLY) != 0)
         {
-          FAR struct icmp_conn_s *conn;
           uint16_t nbuffered;
-
-          /* Nothing consumed the ICMP reply.  That might because this is
-           * an old, invalid reply or simply because the ping application
-           * has not yet put its poll or recv in place.
-           */
-
-          /* Is there any connection that might expect this reply? */
-
-          conn = icmp_findconn(dev, ipicmp->id);
-          if (conn == NULL)
-            {
-              /* No.. drop the packet */
-
-              goto drop;
-            }
 
           /* Add the ICMP echo reply to the IPPROTO_ICMP socket read-ahead
            * buffer.

net/icmp/icmp_netpoll.c
@@ -1,7 +1,7 @@
 /****************************************************************************
  * net/icmp/icmp_netpoll.c
  *
- *   Copyright (C) 2017-2018 Gregory Nutt. All rights reserved.
+ *   Copyright (C) 2017-2019 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>
  *
  * Redistribution and use in source and binary forms, with or without
@@ -197,19 +197,9 @@ int icmp_pollsetup(FAR struct socket *psock, FAR struct pollfd *fds)
 
   net_lock();
 
-  /* Get the device that will provide the provide the NETDEV_DOWN event.
-   * NOTE: in the event that the local socket is bound to INADDR_ANY, the
-   * dev value will be zero and there will be no NETDEV_DOWN notifications.
-   */
-
-  if (conn->dev == NULL)
-    {
-      conn->dev = netdev_default();
-    }
-
   /* Allocate a ICMP callback structure */
 
-  cb = icmp_callback_alloc(conn->dev);
+  cb = icmp_callback_alloc(conn->dev, conn);
   if (cb == NULL)
     {
       ret = -EBUSY;
@@ -315,7 +305,7 @@ int icmp_pollteardown(FAR struct socket *psock, FAR struct pollfd *fds)
       /* Release the callback */
 
       net_lock();
-      icmp_callback_free(conn->dev, info->cb);
+      icmp_callback_free(conn->dev, conn, info->cb);
       net_unlock();
 
       /* Release the poll/select data slot */

net/icmp/icmp_poll.c
@@ -1,7 +1,7 @@
 /****************************************************************************
  * net/icmp/icmp_poll.c
  *
- *   Copyright (C) 2008-2009, 2014 Gregory Nutt. All rights reserved.
+ *   Copyright (C) 2008-2009, 2014, 2019 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>
  *
  * Redistribution and use in source and binary forms, with or without
@@ -38,7 +38,8 @@
  ****************************************************************************/
 
 #include <nuttx/config.h>
-#if defined(CONFIG_NET) && defined(CONFIG_NET_ICMP) && defined(CONFIG_NET_ICMP_SOCKET)
+#if defined(CONFIG_NET) && defined(CONFIG_NET_ICMP) && \
+    defined(CONFIG_NET_ICMP_SOCKET)
 
 #include <debug.h>
 
@@ -60,7 +61,8 @@
  *   Poll a device "connection" structure for availability of ICMP TX data
  *
  * Input Parameters:
- *   dev - The device driver structure to use in the send operation
+ *   dev  - The device driver structure to use in the send operation
+ *   conn - A pointer to the ICMP connection structure
  *
  * Returned Value:
  *   None
@@ -70,7 +72,7 @@
  *
  ****************************************************************************/
 
-void icmp_poll(FAR struct net_driver_s *dev)
+void icmp_poll(FAR struct net_driver_s *dev, FAR struct icmp_conn_s *conn)
 {
   /* Setup for the application callback */
 
@@ -80,7 +82,7 @@ void icmp_poll(FAR struct net_driver_s *dev)
 
   /* Perform the application callback */
 
-  (void)devif_conn_event(dev, NULL, ICMP_POLL, dev->d_conncb);
+  (void)devif_conn_event(dev, conn, ICMP_POLL, conn->list);
 }
 
 #endif /* CONFIG_NET && CONFIG_NET_ICMP && CONFIG_NET_ICMP_SOCKET */

net/icmp/icmp_recvfrom.c
@@ -1,7 +1,7 @@
 /****************************************************************************
  * net/icmp/icmp_recvfrom.c
  *
- *   Copyright (C) 2017 Gregory Nutt. All rights reserved.
+ *   Copyright (C) 2017, 2019 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>
  *
  * Redistribution and use in source and binary forms, with or without
@@ -488,8 +488,8 @@ ssize_t icmp_recvfrom(FAR struct socket *psock, FAR void *buf, size_t len,
 
   /* Set up the callback */
 
-  state.recv_cb = icmp_callback_alloc(dev);
-  if (state.recv_cb)
+  state.recv_cb = icmp_callback_alloc(dev, conn);
+  if (state.recv_cb != NULL)
     {
       state.recv_cb->flags = (ICMP_ECHOREPLY | NETDEV_DOWN);
       state.recv_cb->priv  = (FAR void *)&state;
@@ -503,7 +503,7 @@ ssize_t icmp_recvfrom(FAR struct socket *psock, FAR void *buf, size_t len,
       ninfo("Start time: 0x%08x\n", state.recv_time);
       net_lockedwait(&state.recv_sem);
 
-      icmp_callback_free(dev, state.recv_cb);
+      icmp_callback_free(dev, conn, state.recv_cb);
     }
 
   net_unlock();

net/icmp/icmp_sendto.c
@@ -1,7 +1,7 @@
 /****************************************************************************
  * net/icmp/icmp_sendto.c
  *
- *   Copyright (C) 2017 Gregory Nutt. All rights reserved.
+ *   Copyright (C) 2017, 2019 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>
  *
  * Redistribution and use in source and binary forms, with or without
@@ -455,8 +455,8 @@ ssize_t icmp_sendto(FAR struct socket *psock, FAR const void *buf, size_t len,
 
   /* Set up the callback */
 
-  state.snd_cb = icmp_callback_alloc(dev);
-  if (state.snd_cb)
+  state.snd_cb = icmp_callback_alloc(dev, conn);
+  if (state.snd_cb != NULL)
     {
       state.snd_cb->flags   = (ICMP_POLL | NETDEV_DOWN);
       state.snd_cb->priv    = (FAR void *)&state;
@@ -484,7 +484,7 @@ ssize_t icmp_sendto(FAR struct socket *psock, FAR const void *buf, size_t len,
       ninfo("Start time: 0x%08x\n", state.snd_time);
       net_lockedwait(&state.snd_sem);
 
-      icmp_callback_free(dev, state.snd_cb);
+      icmp_callback_free(dev, conn, state.snd_cb);
     }
 
   net_unlock();

net/icmpv6/icmpv6.h
@@ -1,7 +1,7 @@
 /****************************************************************************
  * net/icmpv6/icmpv6.h
  *
- *   Copyright (C) 2015, 2017-2018 Gregory Nutt. All rights reserved.
+ *   Copyright (C) 2015, 2017-2019 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>
  *
  * Redistribution and use in source and binary forms, with or without
@@ -62,10 +62,10 @@
 
 /* Allocate a new ICMPv6 data callback */
 
-#define icmpv6_callback_alloc(dev) \
-  devif_callback_alloc((dev), &(dev)->d_conncb)
-#define icmpv6_callback_free(dev,cb) \
-  devif_dev_callback_free((dev), (cb))
+#define icmpv6_callback_alloc(dev, conn) \
+  devif_callback_alloc((dev), &(conn)->list)
+#define icmpv6_callback_free(dev, conn, cb) \
+  devif_conn_callback_free((dev), (cb), &(conn)->list)
 
 /****************************************************************************
  * Public Type Definitions
@@ -95,6 +95,10 @@ struct icmpv6_conn_s
 
   struct iob_queue_s readahead;  /* Read-ahead buffering */
 #endif
+
+  /* Defines the list of ICMPV6 callbacks */
+
+  FAR struct devif_callback_s *list;
 };
 #endif
 
@@ -228,7 +232,8 @@ int icmpv6_neighbor(const net_ipv6addr_t ipaddr);
  ****************************************************************************/
 
 #if defined(CONFIG_NET_ICMPv6_SOCKET) || defined(CONFIG_NET_ICMPv6_NEIGHBOR)
-void icmpv6_poll(FAR struct net_driver_s *dev);
+void icmpv6_poll(FAR struct net_driver_s *dev,
+                 FAR struct icmpv6_conn_s *conn);
 #endif
 
 /****************************************************************************
@@ -238,7 +243,7 @@ void icmpv6_poll(FAR struct net_driver_s *dev);
  *   Set up to send an ICMPv6 Neighbor Solicitation message
  *
  * Input Parameters:
- *   dev - Reference to a device driver structure
+ *   dev    - Reference to a device driver structure
  *   ipaddr - IP address of Neighbor to be solicited
  *
  * Returned Value:
@@ -408,7 +413,8 @@ void icmpv6_notify(net_ipv6addr_t ipaddr);
  *   device.
  *
  * Input Parameters:
- *   dev - The device driver structure to assign the address to
+ *   dev   - The device driver structure to assign the address to
+ *   psock - A pointer to a NuttX-specific, internal socket structure
  *
  * Returned Value:
  *   Zero (OK) is returned on success; A negated errno value is returned on
@@ -417,7 +423,8 @@ void icmpv6_notify(net_ipv6addr_t ipaddr);
  ****************************************************************************/
 
 #ifdef CONFIG_NET_ICMPv6_AUTOCONF
-int icmpv6_autoconfig(FAR struct net_driver_s *dev);
+int icmpv6_autoconfig(FAR struct net_driver_s *dev,
+                      FAR struct socket *psock);
 #endif
 
 /****************************************************************************

commit d701dc6a4aae6b61c26e5055fa0bcd08186b483e

net/dev/devif_poll.c:  Fix compile error introduced by c9b73f51391bd20853c513b232f75c64549d6d1c.  Typo:  DEVIF_ICMP6, not DEVIF_ICMPV6.

net/devif/devif_poll.c
@@ -373,7 +373,7 @@ static inline int devif_poll_icmpv6(FAR struct net_driver_s *dev,
 
       /* Perform any necessary conversions on outgoing packets */
 
-      devif_packet_conversion(dev, DEVIF_ICMPV6);
+      devif_packet_conversion(dev, DEVIF_ICMP6);
 
       /* Call back into the driver */
 