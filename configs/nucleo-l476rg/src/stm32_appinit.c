53a94594d695fb0852118470b7b95c29c345652a
Fabian Justi <fabianjusti@gmx.de>
2019-04-17 13:48:37 +0000

    Merged in Justifa/nuttx (pull request #855)
    Master
    
    * arch/arm/src/stm32l4/stm32l4_pwm.c Fix register addr, en_bit and resetbit for pwm timers
    
    * configs/nucleo-l476rg/src/nucleo-l476rg.h Fixed naming stm32l4_pwm_setup
        configs/nucleo-l476rg/src/stm32_appinit.c Fixed naming stm32l4_pwm_setup
    
    Approved-by: David Sidrane <david_s5@nscdg.com>
    Approved-by: Gregory Nutt <gnutt@nuttx.org>

--- configs/nucleo-l476rg/src/stm32_appinit.c
+++ configs/nucleo-l476rg/src/stm32_appinit.c
@@ -193,7 +193,7 @@ int board_app_initialize(uintptr_t arg)
 #ifdef CONFIG_PWM
   /* Initialize PWM and register the PWM device. */
 
-  ret = stm32l4_pwm_setup();
+  ret = stm32_pwm_setup();
   if (ret < 0)
     {
       syslog(LOG_ERR, "ERROR: stm32l4_pwm_setup() failed: %d\n", ret);
