ded871126180e41e8756ef241eaeb1e2c41f9c6c
Michael Jung <mijung@gmx.net>
2019-07-31 07:53:14 -0600

    libs/libc/dirent/:  Added alphasort, scandir and DT_* defines
    - Added an alphasort implementation
    - Added a scandir implementation
    - Added DT_* literals to include/dirent.h: Apparently those are not required by POSIX, but are part of 4.3BSD and available in glibc as well.

--- include/dirent.h
+++ include/dirent.h
@@ -50,9 +50,9 @@
  * Pre-processor Definitions
  ****************************************************************************/
 
-/* File type code for the d_type field in dirent struct.
- * Note that because of the simplified filesystem organization
- * of NuttX, an inode can be BOTH a file and a directory
+/* File type code for the d_type field in dirent structure.
+ * Note that because of the simplified filesystem organization of the NuttX,
+ * top-level, pseudo-file system, an inode can be BOTH a file and a directory
  */
 
 #define DTYPE_UNKNOWN   0         /* The file type could not be determined */
@@ -61,12 +61,30 @@
 #define DTYPE_BLK       (1 << 2)  /* Bit 2: Block device */
 #define DTYPE_DIRECTORY (1 << 3)  /* Bit 3: Directory */
 #define DTYPE_LINK      (1 << 4)  /* Bit 4: Symbolic link */
+#define DTYPE_FIFO      (1 << 5)  /* Bit 5: Named Pipe (FIFO) */
+#define DTYPE_SOCK      (1 << 6)  /* Bit 6: UNIX domain socket */
 
 #define DIRENT_ISFILE(dtype)      (((dtype) & DTYPE_FILE) != 0)
 #define DIRENT_ISCHR(dtype)       (((dtype) & DTYPE_CHR) != 0)
 #define DIRENT_ISBLK(dtype)       (((dtype) & DTYPE_BLK) != 0)
 #define DIRENT_ISDIRECTORY(dtype) (((dtype) & DTYPE_DIRECTORY) != 0)
 #define DIRENT_ISLINK(dtype)      (((dtype) & DTYPE_LINK) != 0)
+
+/* The d_type field of the dirent structure is not specified by POSIX.  It
+ * is a non-standard, 4.5BSD extension that is implemented by most OSs.  A
+ * POSIX compliant OS may not implement the d_type field at all.  Many OS's
+ * (including glibc) may use the following alternative naming for the file
+ * type names:
+ */
+
+#define DT_UNKNOWN DTYPE_UNKNOWN
+#define DT_FIFO    DTYPE_FIFO
+#define DT_CHR     DTYPE_CHR
+#define DT_DIR     DTYPE_DIRECTORY
+#define DT_BLK     DTYPE_BLK
+#define DT_REG     DTYPE_FILE
+#define DT_LNK     DTYPE_LINK
+#define DT_SOCK    DTYPE_SOCK
 
 /****************************************************************************
  * Public Type Definitions
@@ -75,12 +93,15 @@
 /* The POSIX specification requires that the caller of readdir_r provide
  * storage "large enough for a dirent with the d_name member and an array
  * of char containing at least {NAME_MAX} plus one elements.
+ *
+ * POSIX also requires the field d_ino (type ino_t) that provides the file
+ * serial number.  This functionality is not implemented in NuttX.
  */
 
 struct dirent
 {
-  uint8_t   d_type;             /* type of file */
-  char      d_name[NAME_MAX+1]; /* filename */
+  uint8_t  d_type;                /* Type of file */
+  char     d_name[NAME_MAX + 1];  /* File name */
 };
 
 typedef void DIR;
@@ -112,6 +133,12 @@ int        readdir_r(FAR DIR *dirp, FAR struct dirent *entry,
 void       rewinddir(FAR DIR *dirp);
 void       seekdir(FAR DIR *dirp, off_t loc);
 off_t      telldir(FAR DIR *dirp);
+int        scandir(FAR const char *path, FAR struct dirent ***namelist,
+                   CODE int (*filter)(FAR const struct dirent *),
+                   CODE int (*compar)(FAR const struct dirent **,
+                                      FAR const struct dirent **));
+int        alphasort(FAR const struct dirent **a,
+                     FAR const struct dirent **b);
 
 #undef EXTERN
 #if defined(__cplusplus)
