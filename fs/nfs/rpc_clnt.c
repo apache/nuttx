apache/nuttx

commit bd3cc792ff1b69ad571e8245148119d0f7a7e3bb

fs/:  Run all .c files under fs/ through tools/nxstyle.

fs/nfs/rpc_clnt.c
@@ -237,7 +237,7 @@ static int rpcclnt_reply(FAR struct rpcclnt *rpc, int procid, int prog,
       if (error == EAGAIN || error == ETIMEDOUT)
         {
           rpc->rc_timeout = true;
-       }
+        }
     }
 
   /* Get the xid and check that it is an RPC replysvr */
@@ -505,8 +505,10 @@ int rpcclnt_connect(struct rpcclnt *rpc)
   request.mountd.mount.len =  txdr_unsigned(sizeof(request.mountd.mount.rpath));
 
   error = rpcclnt_request(rpc, RPCMNT_MOUNT, RPCPROG_MNT, RPCMNT_VER3,
-                          (FAR void *)&request.mountd, sizeof(struct call_args_mount),
-                          (FAR void *)&response.mdata, sizeof(struct rpc_reply_mount));
+                          (FAR void *)&request.mountd,
+                          sizeof(struct call_args_mount),
+                          (FAR void *)&response.mdata
+                          sizeof(struct rpc_reply_mount));
   if (error != 0)
     {
       ferr("ERROR: rpcclnt_request failed: %d\n", error);
@@ -636,8 +638,10 @@ int rpcclnt_umount(struct rpcclnt *rpc)
   request.sdata.pmap.port = 0;
 
   error = rpcclnt_request(rpc, PMAPPROC_GETPORT, PMAPPROG, PMAPVERS,
-                          (FAR void *)&request.sdata, sizeof(struct call_args_pmap),
-                          (FAR void *)&response.rdata, sizeof(struct rpc_reply_pmap));
+                          (FAR void *)&request.sdata,
+                          sizeof(struct call_args_pmap),
+                          (FAR void *)&response.rdata,
+                          sizeof(struct rpc_reply_pmap));
   if (error != 0)
     {
       ferr("ERROR: rpcclnt_request failed: %d\n", error);
@@ -661,8 +665,10 @@ int rpcclnt_umount(struct rpcclnt *rpc)
   request.mountd.umount.len =  txdr_unsigned(sizeof(request.mountd.umount.rpath));
 
   error = rpcclnt_request(rpc, RPCMNT_UMOUNT, RPCPROG_MNT, RPCMNT_VER3,
-                          (FAR void *)&request.mountd, sizeof(struct call_args_umount),
-                          (FAR void *)&response.mdata, sizeof(struct rpc_reply_umount));
+                          (FAR void *)&request.mountd,
+                          sizeof(struct call_args_umount),
+                          (FAR void *)&response.mdata,
+                          sizeof(struct rpc_reply_umount));
   if (error != 0)
     {
       ferr("ERROR: rpcclnt_request failed: %d\n", error);

fs/aio/aioc_contain.c
@@ -82,6 +82,7 @@ FAR struct aio_container_s *aio_contain(FAR struct aiocb *aiocbp)
 #endif
     FAR void *ptr;
   } u;
+
 #ifdef CONFIG_PRIORITY_INHERITANCE
   struct sched_param param;
 #endif

fs/cromfs/fs_cromfs.c
@@ -515,7 +515,7 @@ static int cromfs_open(FAR struct file *filep, FAR const char *relpath,
       return -EACCES;
     }
 
- /* Locate the node for this relative path */
+  /* Locate the node for this relative path */
 
   node = NULL;
   ret  = cromfs_findnode(fs, &node, relpath);
@@ -960,7 +960,7 @@ static int cromfs_opendir(FAR struct inode *mountpt, FAR const char *relpath,
 
   fs = mountpt->i_private;
 
- /* Locate the node for this relative path */
+  /* Locate the node for this relative path */
 
   node = NULL;
   ret  = cromfs_findnode(fs, &node, relpath);

fs/driver/fs_closeblockdriver.c
@@ -87,7 +87,7 @@ int close_blockdriver(FAR struct inode *inode)
       ferr("ERROR: inode is not a block driver\n");
       ret = -ENOTBLK;
       goto errout;
-   }
+    }
 
   /* Close the block driver.  Not that no mutually exclusive access
    * to the driver is enforced here.  That must be done in the driver

fs/fat/fs_fat32.c
@@ -937,13 +937,13 @@ fat_write_restart:
           if ((sectorindex == 0) && ((buflen >= fs->fs_hwsectorsize) ||
               ((filep->f_pos + buflen) >= ff->ff_size)))
             {
-               /* Flush unwritten data in the sector cache. */
+              /* Flush unwritten data in the sector cache. */
 
-               ret = fat_ffcacheflush(fs, ff);
-               if (ret < 0)
-                 {
-                   goto errout_with_semaphore;
-                 }
+              ret = fat_ffcacheflush(fs, ff);
+              if (ret < 0)
+                {
+                  goto errout_with_semaphore;
+                }
 
               /* Now mark the clean cache buffer as the current sector. */
 
@@ -998,7 +998,7 @@ fat_write_restart:
       byteswritten += writesize;
       buflen       -= writesize;
       sectorindex   = filep->f_pos & SEC_NDXMASK(fs);
-   }
+    }
 
   /* The transfer has completed without error.  Update the file size */
 
@@ -1604,14 +1604,14 @@ static int fat_opendir(FAR struct inode *mountpt, FAR const char *relpath,
 
       if ((DIR_GETATTRIBUTES(direntry) & FATATTR_DIRECTORY) == 0)
         {
-           /* The entry is not a directory */
+          /* The entry is not a directory */
 
-           ret = -ENOTDIR;
-           goto errout_with_semaphore;
+          ret = -ENOTDIR;
+          goto errout_with_semaphore;
         }
       else
         {
-           /* The entry is a directory (but not the root directory) */
+          /* The entry is a directory (but not the root directory) */
 
           dir->u.fat.fd_startcluster =
               ((uint32_t)DIR_GETFSTCLUSTHI(direntry) << 16) |
@@ -2235,7 +2235,7 @@ static int fat_statfs(FAR struct inode *mountpt, FAR struct statfs *buf)
   ret = fat_checkmount(fs);
   if (ret < 0)
     {
-       goto errout_with_semaphore;
+      goto errout_with_semaphore;
     }
 
   /* Fill in the statfs info */
@@ -2392,6 +2392,7 @@ static int fat_mkdir(FAR struct inode *mountpt, FAR const char *relpath,
     {
       goto errout_with_semaphore;
     }
+
   parentsector = fs->fs_currentsector;
 
   /* Allocate a cluster for new directory */

fs/fat/fs_fat32dirent.c
@@ -393,19 +393,19 @@ static inline int fat_parsesfname(const char **path,
 #ifdef CONFIG_FAT_LCNAMES
           if (endndx == 8)
             {
-                /* Is there mixed case in the name? */
+              /* Is there mixed case in the name? */
 
 #ifdef CONFIG_FAT_LFN
-                if (namecase == FATCASE_UPPER)
-                  {
-                    /* Mixed case in the name -- use the long file name */
+              if (namecase == FATCASE_UPPER)
+                {
+                  /* Mixed case in the name -- use the long file name */
 
-                    goto errout;
-                  }
+                  goto errout;
+                }
 
-                /* So far, only lower case in the name */
+              /* So far, only lower case in the name */
 
-                namecase = FATCASE_LOWER;
+              namecase = FATCASE_LOWER;
 #endif
 
               /* Set lower case name bit */
@@ -541,8 +541,9 @@ static inline int fat_parselfname(const char **path,
     }
 
 errout:
-    dirinfo->fd_lfname[0] = '\0';
-    return -EINVAL;
+
+  dirinfo->fd_lfname[0] = '\0';
+  return -EINVAL;
 }
 #endif
 
@@ -634,13 +635,13 @@ static inline int fat_createalias(struct fat_dirinfo_s *dirinfo)
 
   if (namechars < 1)
     {
-       /* Use the extension as the name */
+      /* Use the extension as the name */
 
-       DEBUGASSERT(ext && extchars > 0);
-       src       = ext;
-       ext       = NULL;
-       namechars = extchars;
-       extchars  = 0;
+      DEBUGASSERT(ext && extchars > 0);
+      src       = ext;
+      ext       = NULL;
+      namechars = extchars;
+      extchars  = 0;
     }
   else
     {
@@ -866,7 +867,7 @@ static inline int fat_uniquealias(struct fat_mountpt_s *fs,
 
       for (i = 0; i < tilde - 2; i++)
         {
-          uint8_t nibble = (hash >> (i * 4)) & 0x0F;
+          uint8_t nibble = (hash >> (i * 4)) & 0x0f;
           const char *digits = "0123456789ABCDEF";
           dirinfo->fd_name[tilde - 1 - i] = digits[nibble];
         }
@@ -1067,7 +1068,7 @@ static int fat_findsfnentry(struct fat_mountpt_s *fs,
           dirinfo->fd_seq.ds_lfnoffset   = dirinfo->fd_seq.ds_offset;
           dirinfo->fd_seq.ds_lfncluster  = dirinfo->fd_seq.ds_cluster;
 #endif
-         return OK;
+          return OK;
         }
 
       /* No... get the next directory index and try again */
@@ -1530,6 +1531,7 @@ static inline int fat_allocatelfnentry(struct fat_mountpt_s *fs,
     {
       nentries++;
     }
+
   DEBUGASSERT(nentries > 0 && nentries <= LDIR_MAXLFNS);
 
   /* Plus another for short file name entry that follows the sequence of LFN
@@ -1696,6 +1698,7 @@ static inline int fat_getsfname(uint8_t *direntry, char *buffer,
           ch = tolower(ch);
         }
 #endif
+
       /* Copy the next character into the filename */
 
       *buffer++ = ch;
@@ -1736,6 +1739,7 @@ static inline int fat_getsfname(uint8_t *direntry, char *buffer,
               ch = tolower(ch);
             }
 #endif
+
           /* Copy the next character into the filename */
 
           *buffer++ = ch;
@@ -2095,7 +2099,8 @@ static int fat_putlfname(struct fat_mountpt_s *fs,
    */
 
   startsector                 = fat_cluster2sector(fs, dirinfo->dir.fd_currcluster);
-  dirinfo->dir.fd_index      += (dirinfo->dir.fd_currsector - startsector) * DIRSEC_NDIRS(fs);
+  dirinfo->dir.fd_index      += (dirinfo->dir.fd_currsector - startsector) *
+                                DIRSEC_NDIRS(fs);
 
   /* Make sure that the alias is unique in this directory */
 
@@ -2479,7 +2484,7 @@ int fat_allocatedirentry(struct fat_mountpt_s *fs,
 
       if (cluster)
         {
-         /* Cluster chain can be extended */
+          /* Cluster chain can be extended */
 
           dirinfo->dir.fd_currcluster = cluster;
           dirinfo->dir.fd_currsector  = fat_cluster2sector(fs, cluster);

fs/fat/fs_fat32util.c
@@ -151,7 +151,8 @@ static int fat_checkbootrecord(struct fat_mountpt_s *fs)
   if (fs->fs_rootentcnt != 0)
     {
       notfat32       = true; /* Must be zero for FAT32 */
-      rootdirsectors = (32 * fs->fs_rootentcnt  + fs->fs_hwsectorsize - 1) / fs->fs_hwsectorsize;
+      rootdirsectors = (32 * fs->fs_rootentcnt  + fs->fs_hwsectorsize - 1) /
+                       fs->fs_hwsectorsize;
     }
 
   /* Determine the number of sectors in a FAT. */
@@ -212,7 +213,8 @@ static int fat_checkbootrecord(struct fat_mountpt_s *fs)
 
   /* Get the total number of data sectors */
 
-  ndatasectors = fs->fs_fattotsec - fs->fs_fatresvdseccount - ntotalfatsects - rootdirsectors;
+  ndatasectors = fs->fs_fattotsec - fs->fs_fatresvdseccount -
+                 ntotalfatsects - rootdirsectors;
   if (ndatasectors > fs->fs_hwnsectors)
     {
       fwarn("WARNING: ndatasectors %d fs_hwnsectors: %d\n",
@@ -269,7 +271,8 @@ static int fat_checkbootrecord(struct fat_mountpt_s *fs)
       fs->fs_rootbase = fs->fs_fatbase + ntotalfatsects;
     }
 
-  fs->fs_database     = fs->fs_fatbase + ntotalfatsects + fs->fs_rootentcnt / DIRSEC_NDIRS(fs);
+  fs->fs_database     = fs->fs_fatbase + ntotalfatsects + fs->fs_rootentcnt /
+                        DIRSEC_NDIRS(fs);
   fs->fs_fsifreecount = 0xffffffff;
 
   return OK;
@@ -388,7 +391,7 @@ void fat_semtake(struct fat_mountpt_s *fs)
 
 void fat_semgive(struct fat_mountpt_s *fs)
 {
-   nxsem_post(&fs->fs_sem);
+  nxsem_post(&fs->fs_sem);
 }
 
 /****************************************************************************
@@ -652,13 +655,13 @@ int fat_mount(struct fat_mountpt_s *fs, bool writeable)
    */
 
   if (fs->fs_type == FSTYPE_FAT32)
-  {
+    {
       ret = fat_checkfsinfo(fs);
       if (ret != OK)
-      {
-        goto errout_with_buffer;
-      }
-  }
+        {
+          goto errout_with_buffer;
+        }
+    }
 
   /* We did it! */
 
@@ -751,15 +754,15 @@ int fat_hwread(struct fat_mountpt_s *fs, uint8_t *buffer,  off_t sector,
       struct inode *inode = fs->fs_blkdriver;
       if (inode && inode->u.i_bops && inode->u.i_bops->read)
         {
-          ssize_t nSectorsRead = inode->u.i_bops->read(inode, buffer,
+          ssize_t nsectorsread = inode->u.i_bops->read(inode, buffer,
                                                        sector, nsectors);
-          if (nSectorsRead == nsectors)
+          if (nsectorsread == nsectors)
             {
               ret = OK;
             }
-          else if (nSectorsRead < 0)
+          else if (nsectorsread < 0)
             {
-              ret = nSectorsRead;
+              ret = nsectorsread;
             }
         }
     }
@@ -784,16 +787,16 @@ int fat_hwwrite(struct fat_mountpt_s *fs, uint8_t *buffer, off_t sector,
       struct inode *inode = fs->fs_blkdriver;
       if (inode && inode->u.i_bops && inode->u.i_bops->write)
         {
-          ssize_t nSectorsWritten =
+          ssize_t nsectorswritten =
               inode->u.i_bops->write(inode, buffer, sector, nsectors);
 
-          if (nSectorsWritten == nsectors)
+          if (nsectorswritten == nsectors)
             {
               ret = OK;
             }
-          else if (nSectorsWritten < 0)
+          else if (nsectorswritten < 0)
             {
-              ret = nSectorsWritten;
+              ret = nsectorswritten;
             }
         }
     }
@@ -814,8 +817,9 @@ off_t fat_cluster2sector(FAR struct fat_mountpt_s *fs,  uint32_t cluster)
   cluster -= 2;
   if (cluster >= fs->fs_nclusters - 2)
     {
-       return -EINVAL;
+      return -EINVAL;
     }
+
   return cluster * fs->fs_fatsecperclus + fs->fs_database;
 }
 
@@ -1058,7 +1062,8 @@ int fat_putcluster(struct fat_mountpt_s *fs, uint32_t clusterno,
                 {
                   /* Save the MS four bits of the next cluster */
 
-                  value = (fs->fs_buffer[fatindex] & 0xf0) | ((nextcluster >> 8) & 0x0f);
+                  value = (fs->fs_buffer[fatindex] & 0xf0) |
+                          ((nextcluster >> 8) & 0x0f);
                 }
 
               fs->fs_buffer[fatindex] = value;
@@ -1699,13 +1704,13 @@ int fat_dirextend(FAR struct fat_mountpt_s *fs, FAR struct fat_file_s *ff,
       if (sectndx == 0 && (remaining >= fs->fs_hwsectorsize ||
           (pos + remaining) >= ff->ff_size))
         {
-           /* Flush unwritten data in the sector cache. */
+          /* Flush unwritten data in the sector cache. */
 
-           ret = fat_ffcacheflush(fs, ff);
-           if (ret < 0)
-             {
-               return ret;
-             }
+          ret = fat_ffcacheflush(fs, ff);
+          if (ret < 0)
+            {
+              return ret;
+            }
 
           /* Now mark the clean cache buffer as the current sector. */
 
@@ -1755,7 +1760,7 @@ int fat_dirextend(FAR struct fat_mountpt_s *fs, FAR struct fat_file_s *ff,
       pos       += zerosize;
       remaining -= zerosize;
       sectndx    = pos & SEC_NDXMASK(fs);
-   }
+    }
 
   /* The truncation has completed without error.  Update the file size */
 
@@ -2060,7 +2065,6 @@ int fat_nfreeclusters(struct fat_mountpt_s *fs, off_t *pfreeclusters)
 
       for (sector = 2; sector < fs->fs_nclusters; sector++)
         {
-
           /* If the cluster is unassigned, then increment the count of free clusters */
 
           if ((uint16_t)fat_getcluster(fs, sector) == 0)
@@ -2111,6 +2115,7 @@ int fat_nfreeclusters(struct fat_mountpt_s *fs, off_t *pfreeclusters)
                 {
                   nfreeclusters++;
                 }
+
               offset += 2;
             }
           else
@@ -2125,14 +2130,14 @@ int fat_nfreeclusters(struct fat_mountpt_s *fs, off_t *pfreeclusters)
         }
     }
 
-    fs->fs_fsifreecount = nfreeclusters;
-    if (fs->fs_type == FSTYPE_FAT32)
-      {
-        fs->fs_fsidirty = true;
-      }
+  fs->fs_fsifreecount = nfreeclusters;
+  if (fs->fs_type == FSTYPE_FAT32)
+    {
+      fs->fs_fsidirty = true;
+    }
 
-    *pfreeclusters = nfreeclusters;
-    return OK;
+  *pfreeclusters = nfreeclusters;
+  return OK;
 }
 
 /****************************************************************************

fs/inode/fs_files.c
@@ -263,7 +263,7 @@ int file_dup2(FAR struct file *filep1, FAR struct file *filep2)
 #ifndef CONFIG_DISABLE_MOUNTPOINT
       if (INODE_IS_MOUNTPT(inode))
         {
-         /* Dup the open file on the in the new file structure */
+          /* Dup the open file on the in the new file structure */
 
           ret = inode->u.i_mops->dup(filep1, filep2);
         }
@@ -290,9 +290,10 @@ int file_dup2(FAR struct file *filep1, FAR struct file *filep2)
 
   return OK;
 
-/* Handler various error conditions */
+  /* Handle various error conditions */
 
 errout_with_inode:
+
   inode_release(filep2->f_inode);
   filep2->f_oflags = 0;
   filep2->f_pos    = 0;
@@ -331,12 +332,12 @@ int files_allocate(FAR struct inode *inode, int oflags, off_t pos, int minfd)
     {
       if (!list->fl_files[i].f_inode)
         {
-           list->fl_files[i].f_oflags = oflags;
-           list->fl_files[i].f_pos    = pos;
-           list->fl_files[i].f_inode  = inode;
-           list->fl_files[i].f_priv   = NULL;
-           _files_semgive(list);
-           return i;
+          list->fl_files[i].f_oflags = oflags;
+          list->fl_files[i].f_pos    = pos;
+          list->fl_files[i].f_inode  = inode;
+          list->fl_files[i].f_priv   = NULL;
+          _files_semgive(list);
+          return i;
         }
     }
 

commit c11c18858f014c3a91b3ad0ea1bbb3d27d389b68

fs/nfs/rpc_clnt.c:  Fix a typo introduced with bd3cc792ff1b69ad571e8245148119d0f7a7e3bb.

fs/nfs/rpc_clnt.c
@@ -507,7 +507,7 @@ int rpcclnt_connect(struct rpcclnt *rpc)
   error = rpcclnt_request(rpc, RPCMNT_MOUNT, RPCPROG_MNT, RPCMNT_VER3,
                           (FAR void *)&request.mountd,
                           sizeof(struct call_args_mount),
-                          (FAR void *)&response.mdata
+                          (FAR void *)&response.mdata,
                           sizeof(struct rpc_reply_mount));
   if (error != 0)
     {
@@ -545,8 +545,10 @@ int rpcclnt_connect(struct rpcclnt *rpc)
   request.sdata.pmap.port = 0;
 
   error = rpcclnt_request(rpc, PMAPPROC_GETPORT, PMAPPROG, PMAPVERS,
-                          (FAR void *)&request.sdata, sizeof(struct call_args_pmap),
-                          (FAR void *)&response.rdata, sizeof(struct rpc_reply_pmap));
+                          (FAR void *)&request.sdata,
+                          sizeof(struct call_args_pmap),
+                          (FAR void *)&response.rdata,
+                          sizeof(struct rpc_reply_pmap));
   if (error != 0)
     {
       ferr("ERROR: rpcclnt_request failed: %d\n", error);