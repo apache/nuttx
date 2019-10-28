apache/nuttx

commit 637004e70cc0bfdec6099e9088d34cbc6d99a068

boards/arm/stm32/stm32vldiscvoery/src/:  Fix error found in build testing.  All configurations must provide boar_app_initialize() which is required if CONFIG_LIB_BOARDCTL is enabled.

boards/arm/stm32/stm32vldiscovery/src/stm32_bringup.c
@@ -1,9 +1,8 @@
 /****************************************************************************
- * boards/arm/stm32/stm32vldiscovery/src/stm32vldiscovery.h
+ * boards/arm/stm32/stm32vldiscovery/src/stm32_bringup.c
  *
- *   Copyright (C) 2012 Gregory Nutt. All rights reserved.
+ *   Copyright (C) 2019 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>
- *           Freddie Chopin <freddie_chopin@op.pl>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -34,26 +33,28 @@
  *
  ****************************************************************************/
 
-#ifndef __BOARDS_ARM_STM32_STM32VL_DISCOVERY_SRC_STM32VLDISCOVERY_H
-#define __BOARDS_ARM_STM32_STM32VL_DISCOVERY_SRC_STM32VLDISCOVERY_H
-
 /****************************************************************************
- * Pre-processor Definitions
+ * Included Files
  ****************************************************************************/
 
-/* LED - assume it is on PC9 */
+#include <nuttx/config.h>
 
-#define GPIO_LED1       (GPIO_OUTPUT | GPIO_CNF_OUTPP | GPIO_MODE_50MHz | \
-                         GPIO_OUTPUT_CLEAR | GPIO_PORTC | GPIO_PIN9)
+#ifdef CONFIG_FS_PROCFS
+#  include <sys/mount.h>
+#endif
 
-/* BUTTON - assume it is on PA0 */
+#include <syslog.h>
 
-#define MIN_IRQBUTTON   BUTTON_0
-#define MAX_IRQBUTTON   BUTTON_0
-#define NUM_IRQBUTTONS  1
+#ifdef CONFIG_BUTTONS
+#  include <nuttx/input/buttons.h>
+#endif
 
-#define GPIO_BTN_0      (GPIO_INPUT | GPIO_CNF_INFLOAT | GPIO_MODE_INPUT | \
-                         GPIO_PORTA | GPIO_PIN0)
+#ifdef CONFIG_USERLED
+#  include <nuttx/leds/userled.h>
+#endif
+
+#include "stm32.h"
+#include "stm32f4discovery.h"
 
 /****************************************************************************
  * Public Functions
@@ -68,19 +69,44 @@
  *   CONFIG_BOARD_LATE_INITIALIZE=y :
  *     Called from board_late_initialize().
  *
- *   CONFIG_BOARD_LATE_INITIALIZE=y && CONFIG_LIB_BOARDCTL=y :
+ *   CONFIG_BOARD_LATE_INITIALIZE=n && CONFIG_LIB_BOARDCTL=y :
  *     Called from the NSH library
  *
  ****************************************************************************/
 
-int stm32_bringup(void);
+int stm32_bringup(void)
+{
+  int ret = OK;
 
-/****************************************************************************
- * Name: stm32_led_initialize
- ****************************************************************************/
+#ifdef CONFIG_BUTTONS
+  /* Register the BUTTON driver */
 
-#ifdef CONFIG_ARCH_LEDS
-void stm32_led_initialize(void);
+  ret = btn_lower_initialize("/dev/buttons");
+  if (ret < 0)
+    {
+      syslog(LOG_ERR, "ERROR: btn_lower_initialize() failed: %d\n", ret);
+    }
 #endif
 
-#endif /* __BOARDS_ARM_STM32_STM32VL_DISCOVERY_SRC_STM32VLDISCOVERY_H */
+#ifdef CONFIG_USERLED
+  /* Register the LED driver */
+
+  ret = userled_lower_initialize("/dev/userleds");
+  if (ret < 0)
+    {
+      syslog(LOG_ERR, "ERROR: userled_lower_initialize() failed: %d\n", ret);
+    }
+#endif
+
+#ifdef CONFIG_FS_PROCFS
+  /* Mount the procfs file system */
+
+  ret = mount(NULL, "/proc", "procfs", 0, NULL);
+  if (ret < 0)
+    {
+      syslog(LOG_ERR, "ERROR: Failed to mount procfs at /proc: %d\n", ret);
+    }
+#endif
+
+  return ret;
+}

boards/arm/stm32/stm32vldiscovery/src/Makefile
@@ -37,6 +37,10 @@
 -include $(TOPDIR)/Make.defs
 
 ASRCS =
-CSRCS = stm32_boot.c stm32_leds.c stm32_buttons.c
+CSRCS = stm32_boot.c stm32_bringup.c stm32_leds.c stm32_buttons.c
+
+ifeq ($(CONFIG_LIB_BOARDCTL),y)
+CSRCS += stm32_appinit.c
+endif
 
 include $(TOPDIR)/boards/Board.mk

boards/arm/stm32/stm32vldiscovery/src/stm32_appinit.c
@@ -1,9 +1,8 @@
 /****************************************************************************
- * boards/arm/stm32/stm32vldiscovery/src/stm32vldiscovery.h
+ * boards/arm/stm32/stm32vldiscovery/src/stm32_appinit.c
  *
- *   Copyright (C) 2012 Gregory Nutt. All rights reserved.
+ *   Copyright (C) 2010 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>
- *           Freddie Chopin <freddie_chopin@op.pl>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -34,53 +33,62 @@
  *
  ****************************************************************************/
 
-#ifndef __BOARDS_ARM_STM32_STM32VL_DISCOVERY_SRC_STM32VLDISCOVERY_H
-#define __BOARDS_ARM_STM32_STM32VL_DISCOVERY_SRC_STM32VLDISCOVERY_H
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+#include <nuttx/board.h>
+
+#include "stm32vldiscovery.h"
 
 /****************************************************************************
  * Pre-processor Definitions
  ****************************************************************************/
 
-/* LED - assume it is on PC9 */
-
-#define GPIO_LED1       (GPIO_OUTPUT | GPIO_CNF_OUTPP | GPIO_MODE_50MHz | \
-                         GPIO_OUTPUT_CLEAR | GPIO_PORTC | GPIO_PIN9)
-
-/* BUTTON - assume it is on PA0 */
-
-#define MIN_IRQBUTTON   BUTTON_0
-#define MAX_IRQBUTTON   BUTTON_0
-#define NUM_IRQBUTTONS  1
-
-#define GPIO_BTN_0      (GPIO_INPUT | GPIO_CNF_INFLOAT | GPIO_MODE_INPUT | \
-                         GPIO_PORTA | GPIO_PIN0)
+#ifndef OK
+#  define OK 0
+#endif
 
 /****************************************************************************
  * Public Functions
  ****************************************************************************/
 
 /****************************************************************************
- * Name: stm32_bringup
+ * Name: board_app_initialize
  *
  * Description:
- *   Perform architecture-specific initialization
+ *   Perform application specific initialization.  This function is never
+ *   called directly from application code, but only indirectly via the
+ *   (non-standard) boardctl() interface using the command BOARDIOC_INIT.
  *
- *   CONFIG_BOARD_LATE_INITIALIZE=y :
- *     Called from board_late_initialize().
+ * Input Parameters:
+ *   arg - The boardctl() argument is passed to the board_app_initialize()
+ *         implementation without modification.  The argument has no
+ *         meaning to NuttX; the meaning of the argument is a contract
+ *         between the board-specific initialization logic and the
+ *         matching application logic.  The value cold be such things as a
+ *         mode enumeration value, a set of DIP switch switch settings, a
+ *         pointer to configuration data read from a file or serial FLASH,
+ *         or whatever you would like to do with it.  Every implementation
+ *         should accept zero/NULL as a default configuration.
  *
- *   CONFIG_BOARD_LATE_INITIALIZE=y && CONFIG_LIB_BOARDCTL=y :
- *     Called from the NSH library
+ * Returned Value:
+ *   Zero (OK) is returned on success; a negated errno value is returned on
+ *   any failure to indicate the nature of the failure.
  *
  ****************************************************************************/
 
-int stm32_bringup(void);
+int board_app_initialize(uintptr_t arg)
+{
+#ifdef CONFIG_BOARD_LATE_INITIALIZE
+  /* Board initialization already performed by board_late_initialize() */
 
-/****************************************************************************
- * Name: stm32_led_initialize
- ****************************************************************************/
+  return OK;
+#else
+  /* Perform board-specific initialization */
 
-#ifdef CONFIG_ARCH_LEDS
-void stm32_led_initialize(void);
+  return stm32_bringup();
 #endif
-
-#endif /* __BOARDS_ARM_STM32_STM32VL_DISCOVERY_SRC_STM32VLDISCOVERY_H */
+}

boards/arm/stm32/stm32vldiscovery/src/stm32_boot.c
@@ -1,7 +1,7 @@
 /****************************************************************************
  * boards/arm/stm32/stm32vldiscovery/src/stm32_boot.c
  *
- *   Copyright (C) 2012 Gregory Nutt. All rights reserved.
+ *   Copyright (C) 2012, 2019 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>
  *           Freddie Chopin <freddie_chopin@op.pl>
  *
@@ -42,6 +42,7 @@
 
 #include <debug.h>
 
+#include <nuttx/board.h>
 #include <arch/board/board.h>
 
 #include "up_arch.h"
@@ -69,3 +70,25 @@ void stm32_boardinitialize(void)
   stm32_led_initialize();
 #endif
 }
+
+/****************************************************************************
+ * Name: board_late_initialize
+ *
+ * Description:
+ *   If CONFIG_BOARD_LATE_INITIALIZE is selected, then an additional
+ *   initialization call will be performed in the boot-up sequence to a
+ *   function called board_late_initialize().  board_late_initialize() will be
+ *   called immediately after up_initialize() is called and just before the
+ *   initial application is started.  This additional initialization phase
+ *   may be used, for example, to initialize board-specific device drivers.
+ *
+ ****************************************************************************/
+
+#ifdef CONFIG_BOARD_LATE_INITIALIZE
+void board_late_initialize(void)
+{
+  /* Perform board-specific initialization */
+
+  (void)stm32_bringup();
+}
+#endif

commit 46bec8e030906f9fff146f3ba933dcdf8fcf38bd

arm/stm32/stm32vldiscovery/src/stm32_bringup.c:  Fix a copy paste error introduced with commit 637004e70cc0bfdec6099e9088d34cbc6d99a068.

boards/arm/stm32/stm32vldiscovery/src/stm32_bringup.c
@@ -54,7 +54,7 @@
 #endif
 
 #include "stm32.h"
-#include "stm32f4discovery.h"
+#include "stm32vldiscovery.h"
 
 /****************************************************************************
  * Public Functions