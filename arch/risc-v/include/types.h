46e3ffa2211f2c246d3d432b96db5729e942d342
Masayuki Ishikawa <masayuki.ishikawa@gmail.com>
2019-12-31 12:03:26 +0900

    arch: risc-v: Add support for __LP64__ to types.h

--- arch/risc-v/include/types.h
+++ arch/risc-v/include/types.h
@@ -78,10 +78,17 @@ typedef signed long long   _int64_t;
 typedef unsigned long long _uint64_t;
 #define __INT64_DEFINED
 
+#ifdef __LP64__
+/* A pointer is 8 bytes */
+
+typedef signed long         _intptr_t;
+typedef unsigned long       _uintptr_t;
+#else
 /* A pointer is 4 bytes */
 
 typedef signed int         _intptr_t;
 typedef unsigned int       _uintptr_t;
+#endif
 
 /* This is the size of the interrupt state save returned by irqsave().  */
 
