apache/nuttx

commit 6a3c2aded683e8284e793eb3ee8793d2960ae000

Fix wait loop and void cast (#24)
* Simplify EINTR/ECANCEL error handling

1. Add semaphore uninterruptible wait function
2 .Replace semaphore wait loop with a single uninterruptible wait
3. Replace all sem_xxx to nxsem_xxx

* Unify the void cast usage

1. Remove void cast for function because many place ignore the returned value witout cast
2. Replace void cast for variable with UNUSED macro

drivers/sensors/hc_sr04.c
@@ -149,22 +149,8 @@ static int hcsr04_open(FAR struct file *filep)
 {
   FAR struct inode *inode = filep->f_inode;
   FAR struct hcsr04_dev_s *priv = inode->i_private;
-  int ret;
 
-  /* Get exclusive access */
-
-  do
-    {
-      ret = nxsem_wait(&priv->devsem);
-
-      /* The only case that an error should occur here is if the wait was
-       * awakened by a signal.
-       */
-
-      DEBUGASSERT(ret == OK || ret == -EINTR);
-    }
-  while (ret == -EINTR);
-
+  nxsem_wait_uninterruptible(&priv->devsem);
   nxsem_post(&priv->devsem);
   hcsr04_dbg("OPENED\n");
   return OK;
@@ -174,25 +160,11 @@ static int hcsr04_close(FAR struct file *filep)
 {
   FAR struct inode *inode = filep->f_inode;
   FAR struct hcsr04_dev_s *priv = inode->i_private;
-  int ret;
 
-  /* Get exclusive access */
-
-  do
-    {
-      ret = nxsem_wait(&priv->devsem);
-
-      /* The only case that an error should occur here is if the wait was
-       * awakened by a signal.
-       */
-
-      DEBUGASSERT(ret == OK || ret == -EINTR);
-    }
-  while (ret == -EINTR);
-
+  nxsem_wait_uninterruptible(&priv->devsem);
   nxsem_post(&priv->devsem);
   hcsr04_dbg("CLOSED\n");
-  return ret;
+  return OK;
 }
 
 static ssize_t hcsr04_read(FAR struct file *filep, FAR char *buffer,
@@ -202,41 +174,20 @@ static ssize_t hcsr04_read(FAR struct file *filep, FAR char *buffer,
   FAR struct hcsr04_dev_s *priv = inode->i_private;
   int distance = 0;
   ssize_t length = 0;
-  int ret;
 
   /* Get exclusive access */
 
-  do
-    {
-      ret = nxsem_wait(&priv->devsem);
-
-      /* The only case that an error should occur here is if the wait was
-       * awakened by a signal.
-       */
-
-      DEBUGASSERT(ret == OK || ret == -EINTR);
-    }
-  while (ret == -EINTR);
+  nxsem_wait_uninterruptible(&priv->devsem);
 
   /* Setup and send a pulse to start measuring */
 
-  (void)hcsr04_start_measuring(priv);
+  hcsr04_start_measuring(priv);
 
   /* Wait the convertion to finish */
 
   /* Get exclusive access */
 
-  do
-    {
-      ret = nxsem_wait(&priv->conv_donesem);
-
-      /* The only case that an error should occur here is if the wait was
-       * awakened by a signal.
-       */
-
-      DEBUGASSERT(ret == OK || ret == -EINTR);
-    }
-  while (ret == -EINTR);
+  nxsem_wait_uninterruptible(&priv->conv_donesem);
 
   distance = hcsr04_read_distance(priv);
   if (distance < 0)
@@ -273,17 +224,7 @@ static int hcsr04_ioctl(FAR struct file *filep, int cmd, unsigned long arg)
 
   /* Get exclusive access */
 
-  do
-    {
-      ret = nxsem_wait(&priv->devsem);
-
-      /* The only case that an error should occur here is if the wait was
-       * awakened by a signal.
-       */
-
-      DEBUGASSERT(ret == OK || ret == -EINTR);
-    }
-  while (ret == -EINTR);
+  nxsem_wait_uninterruptible(&priv->devsem);
 
   switch (cmd)
     {
@@ -348,7 +289,7 @@ static int hcsr04_poll(FAR struct file *filep, FAR struct pollfd *fds,
   FAR struct inode *inode;
   FAR struct hcsr04_dev_s *priv;
   uint32_t flags;
-  int ret;
+  int ret = OK;
   int i;
 
   DEBUGASSERT(filep && fds);
@@ -359,17 +300,7 @@ static int hcsr04_poll(FAR struct file *filep, FAR struct pollfd *fds,
 
   /* Get exclusive access */
 
-  do
-    {
-      ret = nxsem_wait(&priv->devsem);
-
-      /* The only case that an error should occur here is if the wait was
-       * awakened by a signal.
-       */
-
-      DEBUGASSERT(ret == OK || ret == -EINTR);
-    }
-  while (ret == -EINTR);
+  nxsem_wait_uninterruptible(&priv->devsem);
 
   if (setup)
     {

wireless/bluetooth/bt_hcicore.c
@@ -439,7 +439,7 @@ static void hci_num_completed_packets(FAR struct bt_buf_s *buf)
 
       while (count--)
         {
-          sem_post(&g_btdev.le_pkts_sem);
+          nxsem_post(&g_btdev.le_pkts_sem);
         }
     }
 }
@@ -978,13 +978,7 @@ static int hci_tx_kthread(int argc, FAR char *argv[])
 
       /* Wait until ncmd > 0 */
 
-      do
-        {
-          ret = nxsem_wait(&g_btdev.ncmd_sem);
-        }
-      while (ret == -EINTR);
-
-      DEBUGASSERT(ret >= 0);
+      nxsem_wait_uninterruptible(&g_btdev.ncmd_sem);
 
       /* Get next command - wait if necessary */
 
@@ -1765,13 +1759,7 @@ int bt_hci_cmd_send_sync(uint16_t opcode, FAR struct bt_buf_s *buf,
            * released while we are waiting.
            */
 
-          do
-            {
-              /* The timed wait could also be awakened by a signal */
-
-              ret = nxsem_timedwait(&sync_sem, &abstime);
-            }
-          while (ret == -EINTR);
+          nxsem_timedwait_uninterruptible(&sync_sem, &abstime);
         }
 
       sched_unlock();

commit 467ece4ec5b56b23a91eb7e79e429ebb19a920f3

Fix the incorrect return value handling in bt_hcicore.c/hc_sr04.c introduced by 6a3c2ade
Report by juniskane

Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

drivers/sensors/hc_sr04.c
@@ -220,7 +220,7 @@ static int hcsr04_ioctl(FAR struct file *filep, int cmd, unsigned long arg)
 {
   FAR struct inode *inode = filep->f_inode;
   FAR struct hcsr04_dev_s *priv = inode->i_private;
-  int ret;
+  int ret = OK;
 
   /* Get exclusive access */
 

wireless/bluetooth/bt_hcicore.c
@@ -1759,7 +1759,7 @@ int bt_hci_cmd_send_sync(uint16_t opcode, FAR struct bt_buf_s *buf,
            * released while we are waiting.
            */
 
-          nxsem_timedwait_uninterruptible(&sync_sem, &abstime);
+          ret = nxsem_timedwait_uninterruptible(&sync_sem, &abstime);
         }
 
       sched_unlock();