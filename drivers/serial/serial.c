784408e617833afbfaf1f5e2d77177164cab8638
Masayuki Ishikawa <masayuki.ishikawa@gmail.com>
2020-01-11 00:10:12 +0900

    drivers: serial: Fix a wrong variable in serial.c in SMP mode.

--- drivers/serial/serial.c
+++ drivers/serial/serial.c
@@ -210,7 +210,7 @@ static int uart_putxmitchar(FAR uart_dev_t *dev, int ch, bool oktoblock)
   int ret;
 
 #ifdef CONFIG_SMP
-  flags = enter_critical_section();
+  irqstate_t flags2 = enter_critical_section();
 #endif
 
   /* Increment to see what the next head pointer will be.  We need to use the "next"
@@ -347,7 +347,7 @@ static int uart_putxmitchar(FAR uart_dev_t *dev, int ch, bool oktoblock)
 err_out:
 
 #ifdef CONFIG_SMP
-  leave_critical_section(flags);
+  leave_critical_section(flags2);
 #endif
 
   return ret;
