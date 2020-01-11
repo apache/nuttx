f7e411333334b9dc50ebcf27a173a8f800181c8e
Gregory Nutt <gnutt@nuttx.org>
2020-01-11 10:13:38 -0600

    binfmt/:  Update copyright dates in all modified files.

--- binfmt/binfmt_execsymtab.c
+++ binfmt/binfmt_execsymtab.c
@@ -1,7 +1,7 @@
 /****************************************************************************
  * binfmt/binfmt_execsymtab.c
  *
- *   Copyright (C) 2013, 2016, 2018 Gregory Nutt. All rights reserved.
+ *   Copyright (C) 2013, 2016, 2018, 2020 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>
  *
  * Redistribution and use in source and binary forms, with or without
--- binfmt/libelf/libelf_iobuffer.c
+++ binfmt/libelf/libelf_iobuffer.c
@@ -1,7 +1,7 @@
 /****************************************************************************
  * binfmt/libelf/libelf_iobuffer.c
  *
- *   Copyright (C) 2012-2013 Gregory Nutt. All rights reserved.
+ *   Copyright (C) 2012-2013, 2020 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>
  *
  * Redistribution and use in source and binary forms, with or without
