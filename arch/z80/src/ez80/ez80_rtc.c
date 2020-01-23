1e3f457c9ef71cbaa751d8ad8348ad1993b956b0
Xiang Xiao <xiaoxiang@xiaomi.com>
2020-01-23 08:17:22 -0600

    Remove TIME_EXTENDED option to more conform C standard
    Gregory Nutt <gnutt@nuttx.org>
    
        Run all .c and .h files modified in this PR through nxstyle and correct all coding standard problems.
    
    Xiang Xiao <xiaoxiang@xiaomi.com>
    
        Remove TIME_EXTENDED option to more conform C standard
    
        Note: the code/data size increment is small

--- arch/z80/src/ez80/ez80_rtc.c
+++ arch/z80/src/ez80/ez80_rtc.c
@@ -72,7 +72,7 @@
 
 /****************************************************************************
  * Private Types
- *****************************************************************************/
+ ****************************************************************************/
 
 /* Raw time from the RTC.  All are binary. */
 
@@ -198,9 +198,7 @@ static void rtc_dumptime(FAR const struct tm *tp, FAR const char *msg)
   rtcinfo("  tm_sec: %08x\n", tp->tm_sec);
   rtcinfo("  tm_min: %08x\n", tp->tm_min);
   rtcinfo(" tm_hour: %08x\n", tp->tm_hour);
-#if defined(CONFIG_LIBC_LOCALTIME) || defined(CONFIG_TIME_EXTENDED)
   rtcinfo(" tm_wday: %08x\n", tp->tm_wday);
-#endif
   rtcinfo(" tm_mday: %08x\n", tp->tm_mday);
   rtcinfo("  tm_mon: %08x\n", tp->tm_mon);
   rtcinfo(" tm_year: %08x\n", tp->tm_year);
@@ -229,7 +227,7 @@ static void rtc_unlock(void)
 
   regval  = inp(EZ80_RTC_CTRL);
   regval |= EZ80_RTC_UNLOCK;
-  outp(EZ80_RTC_CTRL,regval);
+  outp(EZ80_RTC_CTRL, regval);
 }
 
 /****************************************************************************
@@ -252,7 +250,7 @@ static void rtc_lock(void)
 
   regval  = inp(EZ80_RTC_CTRL);
   regval &= ~EZ80_RTC_UNLOCK;
-  outp(EZ80_RTC_CTRL,regval);
+  outp(EZ80_RTC_CTRL, regval);
 }
 
 /****************************************************************************
@@ -453,7 +451,7 @@ int up_rtc_initialize(void)
   return OK;
 }
 
-/************************************************************************************
+/****************************************************************************
  * Name: z80_rtc_irqinitialize
  *
  * Description:
@@ -466,7 +464,7 @@ int up_rtc_initialize(void)
  * Returned Value:
  *   Zero (OK) on success; a negated errno on failure
  *
- ************************************************************************************/
+ ****************************************************************************/
 
 #ifdef CONFIG_RTC_ALARM
 int z80_rtc_irqinitialize(void)
@@ -642,9 +640,7 @@ int ez80_rtc_setalarm(FAR struct alm_setalarm_s *alminfo)
   almregs.sec = alminfo->as_time.tm_sec;
   almregs.min = alminfo->as_time.tm_min;
   almregs.hrs = alminfo->as_time.tm_hour;
-#if defined(CONFIG_LIBC_LOCALTIME) || defined(CONFIG_TIME_EXTENDED)
   almregs.dow = alminfo->as_time.tm_wday;
-#endif
 
   set_raw_alarm(&almregs);
 
@@ -747,9 +743,7 @@ int ez80_rtc_rdalarm(FAR struct tm *almtime)
   almtime->tm_sec  = almregs.sec;
   almtime->tm_min  = almregs.min;
   almtime->tm_hour = almregs.hrs;
-#if defined(CONFIG_LIBC_LOCALTIME) || defined(CONFIG_TIME_EXTENDED)
   almtime->tm_wday = almregs.dow;
-#endif
 
   rtc_dumptime((FAR const struct tm *)almtime, "Returning");
   return OK;
