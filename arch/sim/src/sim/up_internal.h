6e64fea08e7de43e41616bc289ef93ef711624f9
Xiang Xiao <xiaoxiang@xiaomi.com>
2020-02-09 14:36:02 +0800

    arch/sim: fix up_idle call the wrong pthread_yield version
    The logic want the host version but link to NuttX version
    
    Change-Id: I41d7d294d38c9ef6fbbf0f63268fffe637099eb1
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- arch/sim/src/sim/up_internal.h
+++ arch/sim/src/sim/up_internal.h
@@ -245,14 +245,7 @@ void sim_cpu0_start(void);
 /* up_smpsignal.c ***********************************************************/
 
 #ifdef CONFIG_SMP
-void sim_cpu_pause(int cpu, FAR volatile spinlock_t *wait,
-                   FAR volatile unsigned char *paused);
-#endif
-
-/* up_smphook.c *************************************************************/
-
-#ifdef CONFIG_SMP
-void sim_smp_hook(void);
+void up_cpu_started(void);
 #endif
 
 /* up_tickless.c ************************************************************/
