c9590c1e9e0c66c5f0b65f1f2ac29be1471dd36f
Xiang Xiao <xiaoxiang@xiaomi.com>
2020-02-10 01:38:45 +0800

    arch/sim: Drive up_x11update by work to simplify up_idle
    Change-Id: I7f49d0e048b8becdd8b67a04ca894172f17175fa
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- arch/sim/src/sim/up_idle.c
+++ arch/sim/src/sim/up_idle.c
@@ -40,9 +40,8 @@
 
 #include <nuttx/config.h>
 
-#include <time.h>
-
 #include <nuttx/arch.h>
+#include <nuttx/power/pm.h>
 
 #include "up_internal.h"
 
@@ -51,22 +50,6 @@
  ****************************************************************************/
 
 #define PM_IDLE_DOMAIN 0 /* Revisit */
-
-/****************************************************************************
- * Private Data
- ****************************************************************************/
-
-#ifdef CONFIG_SIM_X11FB
-static int g_x11refresh = 0;
-#endif
-
-/****************************************************************************
- * Public Function Prototypes
- ****************************************************************************/
-
-#ifdef CONFIG_SIM_X11FB
-extern void up_x11update(void);
-#endif
 
 /****************************************************************************
  * Public Functions
@@ -142,26 +125,11 @@ void up_idle(void)
   }
 #endif
 
-#if defined(CONFIG_SIM_WALLTIME) || defined(CONFIG_SIM_X11FB)
+#ifdef CONFIG_SIM_WALLTIME
   /* Wait a bit so that the nxsched_process_timer() is called close to the
    * correct rate.
    */
 
   up_hostusleep(1000000 / CLK_TCK);
-
-  /* Handle X11-related events */
-
-#ifdef CONFIG_SIM_X11FB
-  if (g_x11initialized)
-    {
-      /* Update the display periodically */
-
-      g_x11refresh += 1000000 / CLK_TCK;
-      if (g_x11refresh > 500000)
-        {
-          up_x11update();
-        }
-    }
-#endif
 #endif
 }
