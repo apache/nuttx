apache/nuttx

commit fd3e2c59168cbb8f34713c2cceaf01c1786e1a2a

Make.defs:  Fix printed "|| { ar blablabla FAILED!" after $(Q) removed

boards/z16/z16f/z16f2800100zcog/scripts/Make.defs
@@ -194,7 +194,7 @@ define MOVEOBJ
 endef
 
 define ARCHIVE
-	for %%G in ($(2)) do ( "$(AR)" $(ARFLAGS) $1=-+%%G )
+	$(Q) for %%G in ($(2)) do ( "$(AR)" $(ARFLAGS) $1=-+%%G )
 endef
 
 define CLEAN
@@ -227,8 +227,8 @@ define MOVEOBJ
 endef
 
 define ARCHIVE
-	for __obj in $(2) ; do \
-		"$(AR)" $(ARFLAGS) $1=-+$$__obj || { echo "$(AR) $1=-+$$__obj FAILED!" ; exit 1 ; } \
+	$(Q) for __obj in $(2) ; do \
+		"$(AR)" $(ARFLAGS) $1=-+$$__obj \
 	done
 endef
 

boards/z80/z8/z8encore000zco/configs/ostest/Make.defs
@@ -223,7 +223,7 @@ define MOVEOBJ
 endef
 
 define ARCHIVE
-	for %%G in ($(2)) do ( "$(AR)" $(ARFLAGS) $1=-+%%G )
+	$(Q) for %%G in ($(2)) do ( "$(AR)" $(ARFLAGS) $1=-+%%G )
 endef
 
 define CLEAN
@@ -257,8 +257,8 @@ define MOVEOBJ
 endef
 
 define ARCHIVE
-	for __obj in $(2) ; do \
-		"$(AR)" $(ARFLAGS) $1=-+$$__obj || { echo "$(AR) $1=-+$$__obj FAILED!" ; exit 1 ; } \
+	$(Q) for __obj in $(2) ; do \
+		"$(AR)" $(ARFLAGS) $1=-+$$__obj \
 	done
 endef
 

boards/z80/z8/z8f64200100kit/configs/ostest/Make.defs
@@ -229,7 +229,7 @@ define MOVEOBJ
 endef
 
 define ARCHIVE
-	for %%G in ($(2)) do ( "$(AR)" $(ARFLAGS) $1=-+%%G )
+	$(Q) for %%G in ($(2)) do ( "$(AR)" $(ARFLAGS) $1=-+%%G )
 endef
 
 define CLEAN
@@ -257,8 +257,8 @@ define ASSEMBLE
 endef
 
 define ARCHIVE
-	for __obj in $(2) ; do \
-		"$(AR)" $(ARFLAGS) $1=-+$$__obj || { echo "$(AR) $1=-+$$__obj FAILED!" ; exit 1 ; } \
+	$(Q) for __obj in $(2) ; do \
+		"$(AR)" $(ARFLAGS) $1=-+$$__obj \
 	done
 endef
 

tools/Config.mk
@@ -213,15 +213,9 @@ endef
 #
 #   CONFIG_WINDOWS_NATIVE - Defined for a Windows native build
 
-ifeq ($(CONFIG_WINDOWS_NATIVE),y)
 define ARCHIVE
-	$(AR) $1 $(2)
+	$(Q) $(AR) $1 $(2)
 endef
-else
-define ARCHIVE
-	$(AR) $1 $(2) || { echo "$(AR) $1 FAILED!" ; exit 1 ; }
-endef
-endif
 
 # PRELINK - Prelink a list of files
 # This is useful when files were compiled with fvisibility=hidden.

commit 1177d6b2d87fcfe83fe0b00786d083cc2f8f65ac

Make.defs: Fix flock: failed to execute @: No such file or directory
remove $(Q) in ARCHIVE which is added back in commit fd3e2c59168cbb accidentally

Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

boards/z16/z16f/z16f2800100zcog/scripts/Make.defs
@@ -194,7 +194,7 @@ define MOVEOBJ
 endef
 
 define ARCHIVE
-	$(Q) for %%G in ($(2)) do ( "$(AR)" $(ARFLAGS) $1=-+%%G )
+	for %%G in ($(2)) do ( "$(AR)" $(ARFLAGS) $1=-+%%G )
 endef
 
 define CLEAN
@@ -227,7 +227,7 @@ define MOVEOBJ
 endef
 
 define ARCHIVE
-	$(Q) for __obj in $(2) ; do \
+	for __obj in $(2) ; do \
 		"$(AR)" $(ARFLAGS) $1=-+$$__obj \
 	done
 endef

boards/z80/ez80/makerlisp/scripts/Make.defs
@@ -157,7 +157,7 @@ define MOVEOBJ
 endef
 
 define ARCHIVE
-	$(Q) for %%G in ($(2)) do ( $(AR) $(ARFLAGS) $1=-+%%G )
+	for %%G in ($(2)) do ( $(AR) $(ARFLAGS) $1=-+%%G )
 endef
 
 define CLEAN
@@ -191,7 +191,7 @@ define MOVEOBJ
 endef
 
 define ARCHIVE
-	$(Q) for __obj in $(2) ; do \
+	for __obj in $(2) ; do \
 		$(AR) $(ARFLAGS) $1=-+$$__obj \
 	done
 endef

boards/z80/z8/z8encore000zco/configs/ostest/Make.defs
@@ -223,7 +223,7 @@ define MOVEOBJ
 endef
 
 define ARCHIVE
-	$(Q) for %%G in ($(2)) do ( "$(AR)" $(ARFLAGS) $1=-+%%G )
+	for %%G in ($(2)) do ( "$(AR)" $(ARFLAGS) $1=-+%%G )
 endef
 
 define CLEAN
@@ -257,7 +257,7 @@ define MOVEOBJ
 endef
 
 define ARCHIVE
-	$(Q) for __obj in $(2) ; do \
+	for __obj in $(2) ; do \
 		"$(AR)" $(ARFLAGS) $1=-+$$__obj \
 	done
 endef

boards/z80/z8/z8f64200100kit/configs/ostest/Make.defs
@@ -229,7 +229,7 @@ define MOVEOBJ
 endef
 
 define ARCHIVE
-	$(Q) for %%G in ($(2)) do ( "$(AR)" $(ARFLAGS) $1=-+%%G )
+	for %%G in ($(2)) do ( "$(AR)" $(ARFLAGS) $1=-+%%G )
 endef
 
 define CLEAN
@@ -257,7 +257,7 @@ define ASSEMBLE
 endef
 
 define ARCHIVE
-	$(Q) for __obj in $(2) ; do \
+	for __obj in $(2) ; do \
 		"$(AR)" $(ARFLAGS) $1=-+$$__obj \
 	done
 endef

tools/Config.mk
@@ -214,7 +214,7 @@ endef
 #   CONFIG_WINDOWS_NATIVE - Defined for a Windows native build
 
 define ARCHIVE
-	$(Q) $(AR) $1 $(2)
+	$(AR) $1 $(2)
 endef
 
 # PRELINK - Prelink a list of files