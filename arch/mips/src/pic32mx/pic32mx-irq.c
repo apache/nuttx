ac4049682fe024768daa1129b7ba04a1c01613f0
Ouss4 <abdelatif.guettouche@gmail.com>
2020-02-21 23:47:07 +0000

    arch/mips: Fix the SW0 priority set by commit eb1adca It must be strictly greater than IPL to get serviced.

--- arch/mips/src/pic32mx/pic32mx-irq.c
+++ arch/mips/src/pic32mx/pic32mx-irq.c
@@ -113,7 +113,7 @@ void up_irqinitialize(void)
 
   /* Set the Software Interrupt0 to a special priority */
 
-  up_prioritize_irq(PIC32MX_IRQSRC_CS0, (CHIP_MAX_PRIORITY - 1) << 2);
+  up_prioritize_irq(PIC32MX_IRQSRC_CS0, (CHIP_SW0_PRIORITY << 2));
 
   /* Set the BEV bit in the STATUS register */
 
