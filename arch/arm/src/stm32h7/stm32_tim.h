e513b0b99f4586a02841d47fb7e67318341abfdc
Андрей Заболотный <zapparello@ya.ru>
2020-03-06 11:45:26 +0300

    stm32h7: New timer output mode STM32_TIM_CH_OUTTOGGLE. In this mode timer generates a square waveform on given timer channel. The maximal waveform freq is timer clock divided by 4 (prescaler 1, period 1 results in 2 clocks '0' and 2 clocks '1').

--- arch/arm/src/stm32h7/stm32_tim.h
+++ arch/arm/src/stm32h7/stm32_tim.h
@@ -137,11 +137,11 @@ typedef enum
 
   /* MODES: */
 
-  STM32_TIM_CH_MODE_MASK      = 0x06,
+  STM32_TIM_CH_MODE_MASK      = 0x0e,
 
   /* Output Compare Modes */
-
   STM32_TIM_CH_OUTPWM         = 0x04,    /* Enable standard PWM mode, active high when counter < compare */
+  STM32_TIM_CH_OUTTOGGLE      = 0x08,    /* Toggle TIM_CHx output on UEV */
 #if 0
   STM32_TIM_CH_OUTCOMPARE     = 0x06,
 
