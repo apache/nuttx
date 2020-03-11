54abec0bbd7fcea9b477f0b720d81c38a89fe28d
Gregory Nutt <gnutt@nuttx.org>
2020-03-11 14:23:38 -0600

    sched/wqueue:  Update to Apache 2.0 headers.
    Also run files through nxstyle to assurand coding standard compliance.

--- sched/wqueue/kwork_lpthread.c
+++ sched/wqueue/kwork_lpthread.c
@@ -1,35 +1,20 @@
 /****************************************************************************
  * sched/wqueue/work_lpthread.c
  *
- *   Copyright (C) 2009-2014 Gregory Nutt. All rights reserved.
- *   Author: Gregory Nutt <gnutt@nuttx.org>
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- * 3. Neither the name NuttX nor the names of its contributors may be
- *    used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
- * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
- * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
  *
  ****************************************************************************/
 
@@ -131,23 +116,23 @@ static int work_lpthread(int argc, char *argv[])
       else
 #endif
         {
-          /* Perform garbage collection.  This cleans-up memory de-allocations
-           * that were queued because they could not be freed in that execution
-           * context (for example, if the memory was freed from an interrupt
-           * handler).
+          /* Perform garbage collection.  This cleans-up memory de-
+           * allocations that were queued because they could not be freed in
+           * that execution context (for example, if the memory was freed
+           * from an interrupt handler).
            *
-           * NOTE: If the work thread is disabled, this clean-up is performed by
-           * the IDLE thread (at a very, very low priority).
+           * NOTE: If the work thread is disabled, this clean-up is
+           * performed by the IDLE thread (at a very, very low priority).
            *
-           * In the event of multiple low priority threads, on index == 0 will do
-           * the garbage collection.
+           * In the event of multiple low priority threads, on index == 0
+           * will do the garbage collection.
            */
 
           sched_garbage_collection();
 
           /* Then process queued work.  work_process will not return until:
-           * (1) there is no further work in the work queue, and (2) signal is
-           * triggered, or delayed work expires.
+           * (1) there is no further work in the work queue, and (2) signal
+           * is triggered, or delayed work expires.
            */
 
           work_process((FAR struct kwork_wqueue_s *)&g_lpwork, 0);
