9e6dfe1c678bc6524b612462c0ef2dc52a3f0f0b
YAMAMOTO Takashi <yamamoto@midokura.com>
2020-03-19 16:55:16 +0900

    Appease nxstyle complaints in files from vpnkit

--- arch/sim/src/sim/vpnkit/negotiate.c
+++ arch/sim/src/sim/vpnkit/negotiate.c
@@ -36,61 +36,91 @@
  * limitations under the License.
  ****************************************************************************/
 
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include "protocol.h"
 
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
 #define ERROR(fmt, ...) \
         fprintf(stderr, "up_vpnkit: " fmt "\r\n", ##__VA_ARGS__)
 #define INFO(fmt, ...) \
         fprintf(stderr, "up_vpnkit: " fmt "\r\n", ##__VA_ARGS__)
 
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
 /* Negotiate a vmnet connection, returns 0 on success and 1 on error. */
+
 int negotiate(int fd, struct vif_info *vif)
 {
-	enum command command = ethernet;
-	struct init_message *me;
-	struct ethernet_args args;
-	struct init_message you;
-	char *txt;
+  enum command command = ethernet;
+  struct init_message *me;
+  struct ethernet_args args;
+  struct init_message you;
+  char *txt;
 
-	me = create_init_message();
-	if (!me)
-		goto err;
+  me = create_init_message();
+  if (!me)
+    {
+      goto err;
+    }
 
-	if (write_init_message(fd, me) == -1)
-		goto err;
+  if (write_init_message(fd, me) == -1)
+    {
+      goto err;
+    }
 
-	if (read_init_message(fd, &you) == -1)
-		goto err;
+  if (read_init_message(fd, &you) == -1)
+    {
+      goto err;
+    }
 
-	if (me->version != you.version) {
-		ERROR("Server did not accept our protocol version (client: %d, server: %d)", me->version, you.version);
-		goto err;
-	}
+  if (me->version != you.version)
+    {
+      ERROR("Server did not accept our protocol version "
+            "(client: %d, server: %d)", me->version, you.version);
+      goto err;
+    }
 
-	txt = print_init_message(&you);
-	if (!txt)
-		goto err;
+  txt = print_init_message(&you);
+  if (!txt)
+    {
+      goto err;
+    }
 
-	INFO("Server reports %s", txt);
-	free(txt);
+  INFO("Server reports %s", txt);
+  free(txt);
 
-	if (write_command(fd, &command) == -1)
-		goto err;
+  if (write_command(fd, &command) == -1)
+    {
+      goto err;
+    }
 
-	/* We don't need a uuid */
-	memset(&args.uuid_string[0], 0, sizeof(args.uuid_string));
-	if (write_ethernet_args(fd, &args) == -1)
-		goto err;
+  /* We don't need a uuid */
 
-	if (read_vif_response(fd, vif) == -1)
-		goto err;
+  memset(&args.uuid_string[0], 0, sizeof(args.uuid_string));
+  if (write_ethernet_args(fd, &args) == -1)
+    {
+      goto err;
+    }
 
-	return 0;
+  if (read_vif_response(fd, vif) == -1)
+    {
+      goto err;
+    }
+
+  return 0;
 err:
-	ERROR("Failed to negotiate vmnet connection");
-	return 1;
+  ERROR("Failed to negotiate vmnet connection");
+  return 1;
 }
