4829c3d2abd59f4d930e3af5d21a932f44c9da47
Gregory Nutt <gnutt@nuttx.org>
2020-03-30 16:32:20 -0600

    Check return from nxsem_wait_initialize()
    Resolution of Issue 619 will require multiple steps, this part of the first step in that resolution:  Every call to nxsem_wait_uninterruptible() must handle the return value from nxsem_wait_uninterruptible properly.  This commit is only for those files under drivers/input.

--- drivers/input/ft5x06.h
+++ drivers/input/ft5x06.h
@@ -65,76 +65,77 @@
  ****************************************************************************/
 
 /* WARNING: Some definitions may apply only to the FT5336 */
+
 /* FT5x06 maximum number of simultaneously detected touches. */
 
-#define FT5x06_MAX_TOUCHES                (5)
+#define FT5X06_MAX_TOUCHES                (5)
 
 /* FT5x06 raw touch data length. */
 
-#define FT5x06_TOUCH_DATA_LEN             (0x20)
+#define FT5X06_TOUCH_DATA_LEN             (0x20)
 
 /* FT5x06 register addresses */
 
-#define FT5x06_TOUCH_MODE_REG             (0x00) /* Mode register */
-#define FT5x06_TOUCH_GESTID_REG           (0x01) /* Gesture ID register */
-#define FT5x06_TOUCH_STAT_REG             (0x02) /* Touch data status */
+#define FT5X06_TOUCH_MODE_REG             (0x00) /* Mode register */
+#define FT5X06_TOUCH_GESTID_REG           (0x01) /* Gesture ID register */
+#define FT5X06_TOUCH_STAT_REG             (0x02) /* Touch data status */
                                                  /* See struct ft5x06_touch_point_s */
-#define FT5x06_TH_GROUP_REG               (0x80) /* Threshold for touch detection */
-#define FT5x06_TH_DIFF_REG                (0x85) /* Filter function coefficients */
-#define FT5x06_CTRL_REG                   (0x86) /* Control register */
-#define FT5x06_TIMEENTERMONITOR_REG       (0x87) /* Time switching Active to Monitor */
-#define FT5x06_PERIODACTIVE_REG           (0x88) /* Report rate in Active mode */
-#define FT5x06_PERIODMONITOR_REG          (0x89) /* Report rate in Monitor mode */
-#define FT5x06_RADIAN_VALUE_REG           (0x91) /* Minimum allowing angle */
-#define FT5x06_OFFSET_LEFT_RIGHT_REG      (0x92) /* Minimum offset */
-#define FT5x06_OFFSET_UP_DOWN_REG         (0x93) /* Maximum offset */
-#define FT5x06_DISTANCE_LEFT_RIGHT_REG    (0x94) /* Minimum distance */
-#define FT5x06_DISTANCE_UP_DOWN_REG       (0x95) /* Minimum distance */
-#define FT5x06_DISTANCE_ZOOM_REG          (0x96) /* Maximum distance */
-#define FT5x06_LIB_VER_H_REG              (0xa1) /* MS LIB version */
-#define FT5x06_LIB_VER_L_REG              (0xa2) /* LS LIB version */
-#define FT5x06_CIPHER_REG                 (0xa3) /* Chip selecting */
-#define FT5x06_GMODE_REG                  (0xa4) /* Interrupt mode */
-#define FT5x06_PWR_MODE_REG               (0xa5) /* Power mode */
-#define FT5x06_FIRMID_REG                 (0xa6) /* Firmware version */
-#define FT5x06_CHIP_ID_REG                (0xa8) /* Chip ID */
-#define FT5x06_RELEASE_CODE_ID_REG        (0xaf) /* Release code version */
-#define FT5x06_STATE_REG                  (0xbc) /* Current operating mode */
+#define FT5X06_TH_GROUP_REG               (0x80) /* Threshold for touch detection */
+#define FT5X06_TH_DIFF_REG                (0x85) /* Filter function coefficients */
+#define FT5X06_CTRL_REG                   (0x86) /* Control register */
+#define FT5X06_TIMEENTERMONITOR_REG       (0x87) /* Time switching Active to Monitor */
+#define FT5X06_PERIODACTIVE_REG           (0x88) /* Report rate in Active mode */
+#define FT5X06_PERIODMONITOR_REG          (0x89) /* Report rate in Monitor mode */
+#define FT5X06_RADIAN_VALUE_REG           (0x91) /* Minimum allowing angle */
+#define FT5X06_OFFSET_LEFT_RIGHT_REG      (0x92) /* Minimum offset */
+#define FT5X06_OFFSET_UP_DOWN_REG         (0x93) /* Maximum offset */
+#define FT5X06_DISTANCE_LEFT_RIGHT_REG    (0x94) /* Minimum distance */
+#define FT5X06_DISTANCE_UP_DOWN_REG       (0x95) /* Minimum distance */
+#define FT5X06_DISTANCE_ZOOM_REG          (0x96) /* Maximum distance */
+#define FT5X06_LIB_VER_H_REG              (0xa1) /* MS LIB version */
+#define FT5X06_LIB_VER_L_REG              (0xa2) /* LS LIB version */
+#define FT5X06_CIPHER_REG                 (0xa3) /* Chip selecting */
+#define FT5X06_GMODE_REG                  (0xa4) /* Interrupt mode */
+#define FT5X06_PWR_MODE_REG               (0xa5) /* Power mode */
+#define FT5X06_FIRMID_REG                 (0xa6) /* Firmware version */
+#define FT5X06_CHIP_ID_REG                (0xa8) /* Chip ID */
+#define FT5X06_RELEASE_CODE_ID_REG        (0xaf) /* Release code version */
+#define FT5X06_STATE_REG                  (0xbc) /* Current operating mode */
 
-#define FT5x06_TOUCH_DATA_STARTREG        (1)  /* Address where data begins */
+#define FT5X06_TOUCH_DATA_STARTREG        (1)    /* Address where data begins */
 
 /* Possible values of the DEV_MODE register */
 
-#define FT5x06_DEV_MODE_WORKING           (0x00)
-#define FT5x06_DEV_MODE_FACTORY           (0x04)
+#define FT5X06_DEV_MODE_WORKING           (0x00)
+#define FT5X06_DEV_MODE_FACTORY           (0x04)
 
 /* Possible values of the GEST_ID register */
 
-#define FT5x06_GEST_ID_NO_GESTURE         (0x00)
-#define FT5x06_GEST_ID_MOVE_UP            (0x10)
-#define FT5x06_GEST_ID_MOVE_RIGHT         (0x14)
-#define FT5x06_GEST_ID_MOVE_DOWN          (0x18)
-#define FT5x06_GEST_ID_MOVE_LEFT          (0x1C)
-#define FT5x06_GEST_ID_SINGLE_CLICK       (0x20)
-#define FT5x06_GEST_ID_DOUBLE_CLICK       (0x22)
-#define FT5x06_GEST_ID_ROTATE_CLOCKWISE   (0x28)
-#define FT5x06_GEST_ID_ROTATE_C_CLOCKWISE (0x29)
-#define FT5x06_GEST_ID_ZOOM_IN            (0x40)
-#define FT5x06_GEST_ID_ZOOM_OUT           (0x49)
+#define FT5X06_GEST_ID_NO_GESTURE         (0x00)
+#define FT5X06_GEST_ID_MOVE_UP            (0x10)
+#define FT5X06_GEST_ID_MOVE_RIGHT         (0x14)
+#define FT5X06_GEST_ID_MOVE_DOWN          (0x18)
+#define FT5X06_GEST_ID_MOVE_LEFT          (0x1c)
+#define FT5X06_GEST_ID_SINGLE_CLICK       (0x20)
+#define FT5X06_GEST_ID_DOUBLE_CLICK       (0x22)
+#define FT5X06_GEST_ID_ROTATE_CLOCKWISE   (0x28)
+#define FT5X06_GEST_ID_ROTATE_C_CLOCKWISE (0x29)
+#define FT5X06_GEST_ID_ZOOM_IN            (0x40)
+#define FT5X06_GEST_ID_ZOOM_OUT           (0x49)
 
-/* Values related to FT5x06_CTRL_REG */
+/* Values related to FT5X06_CTRL_REG */
 
-#define FT5x06_CTRL_KEEP_ACTIVE_MODE      (0x00)
-#define FT5x06_CTRL_KEEP_AUTO_SWITCH_MONITOR_MODE (0x01)
+#define FT5X06_CTRL_KEEP_ACTIVE_MODE      (0x00)
+#define FT5X06_CTRL_KEEP_AUTO_SWITCH_MONITOR_MODE (0x01)
 
-/* Possible values of FT5x06_GMODE_REG */
+/* Possible values of FT5X06_GMODE_REG */
 
-#define FT5x06_G_MODE_INTERRUPT_POLLING   (0x00)
-#define FT5x06_G_MODE_INTERRUPT_TRIGGER   (0x01)
+#define FT5X06_G_MODE_INTERRUPT_POLLING   (0x00)
+#define FT5X06_G_MODE_INTERRUPT_TRIGGER   (0x01)
 
-/* Possible values of FT5x06_CHIP_ID_REG */
+/* Possible values of FT5X06_CHIP_ID_REG */
 
-#define FT5x06_ID_VALUE                   (0x51)
+#define FT5X06_ID_VALUE                   (0x51)
 
 /* Operations on struct ft5x06_touch_point_s */
 
@@ -149,10 +150,10 @@
 
 enum touch_event_e
 {
-  FT5x06_DOWN    = 0,  /* The state changed to touched */
-  FT5x06_UP      = 1,  /* The state changed to not touched */
-  FT5x06_CONTACT = 2,  /* There is a continuous touch being detected */
-  FT5x06_INVALID = 3   /* No touch information available */
+  FT5X06_DOWN    = 0,  /* The state changed to touched */
+  FT5X06_UP      = 1,  /* The state changed to not touched */
+  FT5X06_CONTACT = 2,  /* There is a continuous touch being detected */
+  FT5X06_INVALID = 3   /* No touch information available */
 };
 
 /* Describes on touchpoint returned by the FT5x06 */
@@ -173,7 +174,7 @@ struct ft5x06_touch_data_s
 {
   uint8_t gestid;      /* Gesture ID */
   uint8_t tdstatus;    /* Touch status */
-  struct ft5x06_touch_point_s touch[FT5x06_MAX_TOUCHES];
+  struct ft5x06_touch_point_s touch[FT5X06_MAX_TOUCHES];
 };
 
 /****************************************************************************
