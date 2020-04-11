554d56b8752119ade4411cde9dbc1dfc5fdb8ac0
Şükrü Bahadır Arslan <sukrubahadirarslan@gmail.com>
2020-04-11 09:11:18 -0600

    sched/sched_setpriority.c:  DEBUGVERIFY, not DEBUGASSERT.
    Hello,
    I am testing priority inversion feauture of Nuttx scheduler. I encounter with problem in tests.
    Here is problem:
    nxsched_readytorun_setpriority() function not works as expected when DEBUG_ASSERTION not enabled. Because sched_removereadytorun() and sched_addreadytorun()  is not called in this case. These functions wrapped with DEBUGASSERT macro and with the effect of DEBUGASSERT macro, sched_removereadytorun() and sched_addreadytorun() not called when DEBUG_ASSERTION not enabled. Therefore priority inversion not works as expected.
    
    Could you check this problem ? Is this a bug, if not what is the purpose of this code ?
    Thanks,
    Şükrü Bahadır Arslan

--- sched/sched/sched_setpriority.c
+++ sched/sched/sched_setpriority.c
@@ -1,36 +1,20 @@
 /****************************************************************************
  * sched/sched/sched_setpriority.c
  *
- *   Copyright (C) 2009, 2013, 2016, 2018-2019 Gregory Nutt. All rights
- *     reserved.
- *   Author: Gregory Nutt <gnutt@nuttx.org>
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- * 3. Neither the name NuttX nor the names of its contributors may be
- *    used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
- * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
- * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
  *
  ****************************************************************************/
 
@@ -248,17 +232,25 @@ static void nxsched_readytorun_setpriority(FAR struct tcb_s *tcb,
 
   else
     {
-      /* Remove the TCB from the ready-to-run task list that it resides in */
+      /* Remove the TCB from the ready-to-run task list that it resides in.
+       * It should not be at the head of the list.
+       */
 
-      DEBUGASSERT(!sched_removereadytorun(tcb));
+      bool check = sched_removereadytorun(tcb);
+      DEBUGASSERT(check == false);
+      UNUSED(check);
 
       /* Change the task priority */
 
       tcb->sched_priority = (uint8_t)sched_priority;
 
-      /* Put it back into the correct ready-to-run task list */
+      /* Put it back into the correct ready-to-run task list.  It must not
+       * end up at the head of the list.
+       */
 
-      DEBUGASSERT(!sched_addreadytorun(tcb));
+      check = sched_addreadytorun(tcb);
+      DEBUGASSERT(check == false);
+      UNUSED(check);
     }
 }
 
@@ -359,7 +351,9 @@ int nxsched_setpriority(FAR struct tcb_s *tcb, int sched_priority)
 
   flags = enter_critical_section();
 
-  /* There are three major cases (and two sub-cases) that must be considered: */
+  /* There are three major cases (and two sub-cases) that must be
+   * considered:
+   */
 
   switch (tcb->task_state)
     {
