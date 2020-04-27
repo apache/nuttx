apache/nuttx

commit 3b9efc95a2e0e923f34a857c06b45596beb23552

Format all shell scripts in tools folder
Change-Id: Ieef2eb93d56c03972b4dc63a1c55aabf1fb0d169
Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

tools/incdir.sh
@@ -41,44 +41,44 @@ usage="USAGE: $progname [-w] [-d] [-h] <compiler-path> <dir1> [<dir2> [<dir3> ..
 advice="Try '$progname -h' for more information"
 
 while [ ! -z "$1" ]; do
-	case $1 in
-	-d )
-		set -x
-		;;
-	-w )
-		wintool=y
-		;;
-	-s )
-		pathtype=system
-		;;
-	-h )
-		echo "$progname is a tool for flexible generation of include path arguments for a"
-		echo "variety of different compilers in a variety of compilation environments"
-		echo ""
-		echo $usage
-		echo ""
-		echo "Where:"
-		echo "	<compiler-path>"
-		echo "		The full path to your compiler"
-		echo "	<dir1> [<dir2> [<dir3> ...]]"
-		echo "		A list of include directories"
-		echo "	-w"
-		echo "		The compiler is a Windows native tool and requires Windows"
-		echo "		style pathnames like C:\\Program Files"
-		echo "	-s"
-		echo "		Generate standard, system header file paths instead of normal user"
-		echo "		header file paths."
-		echo "	-d"
-		echo "		Enable script debug"
-		echo "	-h"
-		echo "		Shows this help text and exits."
-		exit 0
-		;;
-	* )
-		break;
-		;;
-	esac
-	shift
+  case $1 in
+  -d )
+    set -x
+    ;;
+  -w )
+    wintool=y
+    ;;
+  -s )
+    pathtype=system
+    ;;
+  -h )
+    echo "$progname is a tool for flexible generation of include path arguments for a"
+    echo "variety of different compilers in a variety of compilation environments"
+    echo ""
+    echo $usage
+    echo ""
+    echo "Where:"
+    echo "  <compiler-path>"
+    echo "    The full path to your compiler"
+    echo "  <dir1> [<dir2> [<dir3> ...]]"
+    echo "    A list of include directories"
+    echo "  -w"
+    echo "    The compiler is a Windows native tool and requires Windows"
+    echo "    style pathnames like C:\\Program Files"
+    echo "  -s"
+    echo "    Generate standard, system header file paths instead of normal user"
+    echo "    header file paths."
+    echo "  -d"
+    echo "    Enable script debug"
+    echo "  -h"
+    echo "    Shows this help text and exits."
+    exit 0
+    ;;
+  * )
+    break;
+    ;;
+  esac
+  shift
 done
 
 ccpath=$1
@@ -86,17 +86,17 @@ shift
 dirlist=$@
 
 if [ -z "$ccpath" ]; then
-	echo "Missing compiler path"
-	echo $usage
-	echo $advice
-	exit 1
+  echo "Missing compiler path"
+  echo $usage
+  echo $advice
+  exit 1
 fi
 
 if [ -z "$dirlist" ]; then
-	echo "Missing include directory list"
-	echo $usage
-	echo $advice
-	exit 1
+  echo "Missing include directory list"
+  echo $usage
+  echo $advice
+  exit 1
 fi
 
 #
@@ -132,32 +132,32 @@ fi
 sdcc=`echo $ccpath | grep sdcc`
 
 if [ "X$os" = "XCygwin" ]; then
-	# We can treat Cygwin native toolchains just like Linux native
-	# toolchains in the Linux.  Let's assume:
-	# 1. GCC or SDCC are the only possible Cygwin native compilers
-	# 2. If this is a Window native GCC version, then -w must be
-	#    provided on the command line (wintool=y)
+  # We can treat Cygwin native toolchains just like Linux native
+  # toolchains in the Linux.  Let's assume:
+  # 1. GCC or SDCC are the only possible Cygwin native compilers
+  # 2. If this is a Window native GCC version, then -w must be
+  #    provided on the command line (wintool=y)
 
-	if [ -z "$gcc" -a -z "$sdcc" ]; then
-		# Not GCC or SDCC, must be Windows native
-		windows=yes
-		compiler=`cygpath -u "$ccpath"`
-	else
-		if [ "X$wintool" == "Xy" ]; then
-			# It is a native GCC or SDCC compiler
-			windows=yes
-			compiler=`cygpath -u "$ccpath"`
-		else
-			# GCC or SDCC and not for Windows
-			windows=no
-			compiler="$ccpath"
-		fi
-	fi
+  if [ -z "$gcc" -a -z "$sdcc" ]; then
+    # Not GCC or SDCC, must be Windows native
+    windows=yes
+    compiler=`cygpath -u "$ccpath"`
+  else
+    if [ "X$wintool" == "Xy" ]; then
+      # It is a native GCC or SDCC compiler
+      windows=yes
+      compiler=`cygpath -u "$ccpath"`
+    else
+      # GCC or SDCC and not for Windows
+      windows=no
+      compiler="$ccpath"
+    fi
+  fi
 else
-	# Otherwise, we must be in a Linux environment where there are
-	# only Linux native toolchains
-	windows=no
-	compiler="$ccpath"
+  # Otherwise, we must be in a Linux environment where there are
+  # only Linux native toolchains
+  windows=no
+  compiler="$ccpath"
 fi
 exefile=`basename "$compiler"`
 
@@ -165,25 +165,25 @@ exefile=`basename "$compiler"`
 # a special output format as well as special paths
 
 if [ "X$exefile" = "Xez8cc.exe" -o "X$exefile" = "Xzneocc.exe" -o "X$exefile" = "Xez80cc.exe" ]; then
-	fmt=zds
+  fmt=zds
 else
-	fmt=std
+  fmt=std
 fi
 
 # Select system or user header file path command line option
 
 if [ "X$fmt" = "Xzds" ]; then
-	if [ "X$pathtype" = "Xsystem" ]; then
-		cmdarg=-stdinc:
-	else
-		cmdarg=-usrinc:
-	fi
+  if [ "X$pathtype" = "Xsystem" ]; then
+    cmdarg=-stdinc:
+  else
+    cmdarg=-usrinc:
+  fi
 else
-	if [ "X$pathtype" = "Xsystem" ]; then
-		cmdarg=-isystem
-	else
-		cmdarg=-I
-	fi
+  if [ "X$pathtype" = "Xsystem" ]; then
+    cmdarg=-isystem
+  else
+    cmdarg=-I
+  fi
 fi
 
 # Now process each directory in the directory list
@@ -191,47 +191,45 @@ fi
 unset response
 for dir in $dirlist; do
 
-	# Verify that the include directory exists
+  # Verify that the include directory exists
 
-	if [ ! -d $dir ]; then
-		echo "Include path '$dir' does not exist"
-		echo $showusage
-		exit 1
-	fi
+  if [ ! -d $dir ]; then
+    echo "Include path '$dir' does not exist"
+    echo $showusage
+    exit 1
+  fi
 
-	# Check if the path needs to be extended for Windows-based tools under Cygwin
+  # Check if the path needs to be extended for Windows-based tools under Cygwin
 
-	if [ "X$windows" = "Xyes" ]; then
-		path=`cygpath -w $dir`
-	else
-		path=$dir
-	fi
+  if [ "X$windows" = "Xyes" ]; then
+    path=`cygpath -w $dir`
+  else
+    path=$dir
+  fi
 
-	# Handle the output using the selected format
+  # Handle the output using the selected format
 
-	if [ "X$fmt" = "Xzds" ]; then
-		# Treat the first directory differently
+  if [ "X$fmt" = "Xzds" ]; then
+    # Treat the first directory differently
 
-		if [ -z "$response" ]; then
-			response="${cmdarg}'"${path}
-		else
-			response=${response}";${path}"
-		fi
-	else
-		# Treat the first directory differently
+    if [ -z "$response" ]; then
+      response="${cmdarg}'"${path}
+    else
+      response=${response}";${path}"
+    fi
+  else
+    # Treat the first directory differently
 
-		if [ -z "$response" ]; then
-			response="${cmdarg} \"$path\""
-		else
-			response="${response} ${cmdarg} \"$path\""
-		fi
-	fi
+    if [ -z "$response" ]; then
+      response="${cmdarg} \"$path\""
+    else
+      response="${response} ${cmdarg} \"$path\""
+    fi
+  fi
 done
 
 if [ "X$fmt" = "Xzds" ]; then
-	response=$response"'"
+  response=$response"'"
 fi
 
 echo $response
-
-

tools/configure.bat
@@ -115,10 +115,10 @@ echo  -f:
 echo    Informs the tool that it should use POSIX style paths like /usr/local/bin.
 echo    By default, Windows style paths like C:\\Program Files are used.
 echo  -l selects the Linux (l) host environment.  The [-c^|u^|n] options
-echo     select one of the Windows environments.  Default:  Use host setup
-echo     in the defconfig file
+echo    select one of the Windows environments.  Default:  Use host setup
+echo    in the defconfig file
 echo  [-c^|u^|n] selects the Windows host and a Windows environment:  Cygwin (c),
-echo     Ubuntu under Windows 10 (u), or Windows native (n).  Default Cygwin
+echo    Ubuntu under Windows 10 (u), or Windows native (n).  Default Cygwin
 echo  -a ^<app-dir^>:
 echo    Informs the configuration tool where the application build
 echo    directory.  This is a relative path from the top-level NuttX

tools/configure.sh
@@ -75,52 +75,52 @@ skip=0
 
 while [ ! -z "$1" ]; do
   case "$1" in
-    -a )
-      shift
-      appdir=$1
-      ;;
-    -c )
-      host=windows
-      wenv=cygwin
-      ;;
-    -d )
-      debug=y
-      set -x
-      ;;
-    -g )
-      host=windows
-      wenv=msys
-      ;;
-    -h )
+  -a )
+    shift
+    appdir=$1
+    ;;
+  -c )
+    host=windows
+    wenv=cygwin
+    ;;
+  -d )
+    debug=y
+    set -x
+    ;;
+  -g )
+    host=windows
+    wenv=msys
+    ;;
+  -h )
+    echo "$USAGE"
+    exit 0
+    ;;
+  -l )
+    host=linux
+    ;;
+  -m )
+    host=macos
+    ;;
+  -n )
+    host=windows
+    wenv=native
+    ;;
+  -s )
+    skip=1
+    ;;
+  -u )
+    host=windows
+    wenv=ubuntu
+    ;;
+  *)
+    if [ ! -z "${boardconfig}" ]; then
+      echo ""
+      echo "<board/config> defined twice"
       echo "$USAGE"
-      exit 0
-      ;;
-    -l )
-      host=linux
-      ;;
-    -m )
-      host=macos
-      ;;
-    -n )
-      host=windows
-      wenv=native
-      ;;
-    -s )
-      skip=1
-      ;;
-    -u )
-      host=windows
-      wenv=ubuntu
-      ;;
-    *)
-      if [ ! -z "${boardconfig}" ]; then
-        echo ""
-        echo "<board/config> defined twice"
-        echo "$USAGE"
-        exit 1
-      fi
-      boardconfig=$1
-      ;;
+      exit 1
+    fi
+    boardconfig=$1
+    ;;
   esac
   shift
 done
@@ -330,43 +330,43 @@ if [ ! -z "$host" ]; then
   sed -i -e "/CONFIG_SIM_X8664_SYSTEMV/d" ${dest_config}
 
   case "$host" in
-    "linux")
-      echo "  Select CONFIG_HOST_LINUX=y"
-      echo "CONFIG_HOST_LINUX=y" >> "${dest_config}"
-      echo "CONFIG_SIM_X8664_SYSTEMV=y" >> "${dest_config}"
+  "linux")
+    echo "  Select CONFIG_HOST_LINUX=y"
+    echo "CONFIG_HOST_LINUX=y" >> "${dest_config}"
+    echo "CONFIG_SIM_X8664_SYSTEMV=y" >> "${dest_config}"
+    ;;
+
+  "macos")
+    echo "  Select CONFIG_HOST_MACOS=y"
+    echo "CONFIG_HOST_MACOS=y" >> "${dest_config}"
+    ;;
+
+  "windows")
+    echo "  Select CONFIG_HOST_WINDOWS=y"
+    echo "CONFIG_HOST_WINDOWS=y" >> "${dest_config}"
+    echo "CONFIG_SIM_X8664_MICROSOFT=y" >> "${dest_config}"
+
+    case "$wenv" in
+    "cygwin")
+      echo "  Select CONFIG_WINDOWS_CYGWIN=y"
+      echo "CONFIG_WINDOWS_CYGWIN=y" >> "${dest_config}"
       ;;
 
-    "macos")
-      echo "  Select CONFIG_HOST_MACOS=y"
-      echo "CONFIG_HOST_MACOS=y" >> "${dest_config}"
+    "msys")
+      echo "  Select CONFIG_WINDOWS_MSYS=y"
+      echo "CONFIG_WINDOWS_MSYS=y" >> "${dest_config}"
       ;;
 
-    "windows")
-      echo "  Select CONFIG_HOST_WINDOWS=y"
-      echo "CONFIG_HOST_WINDOWS=y" >> "${dest_config}"
-      echo "CONFIG_SIM_X8664_MICROSOFT=y" >> "${dest_config}"
+    "ubuntu")
+      echo "  Select CONFIG_WINDOWS_UBUNTU=y"
+      echo "CONFIG_WINDOWS_UBUNTU=y" >> "${dest_config}"
+      ;;
 
-      case "$wenv" in
-          "cygwin")
-            echo "  Select CONFIG_WINDOWS_CYGWIN=y"
-            echo "CONFIG_WINDOWS_CYGWIN=y" >> "${dest_config}"
-            ;;
-
-          "msys")
-            echo "  Select CONFIG_WINDOWS_MSYS=y"
-            echo "CONFIG_WINDOWS_MSYS=y" >> "${dest_config}"
-            ;;
-
-          "ubuntu")
-            echo "  Select CONFIG_WINDOWS_UBUNTU=y"
-            echo "CONFIG_WINDOWS_UBUNTU=y" >> "${dest_config}"
-            ;;
-
-          *)
-            echo "  Select CONFIG_WINDOWS_NATIVE=y"
-            echo "CONFIG_WINDOWS_NATIVE=y" >> "${dest_config}"
-            ;;
-      esac
+    *)
+      echo "  Select CONFIG_WINDOWS_NATIVE=y"
+      echo "CONFIG_WINDOWS_NATIVE=y" >> "${dest_config}"
+      ;;
+    esac
   esac
 fi
 

tools/copydir.bat
@@ -96,7 +96,7 @@ goto :End
 :ShowUsage
 echo USAGE: %0 ^<src^> ^<dest^>
 echo Where:
-echo   ^<src^> is the source directory to be copied
-echo   ^<dest^> is the destination directory to be created
+echo  ^<src^> is the source directory to be copied
+echo  ^<dest^> is the destination directory to be created
 
 :End

tools/copydir.sh
@@ -52,8 +52,8 @@ dest=$2
 # Verify that arguments were provided
 
 if [ -z "${src}" -o -z "${dest}" ]; then
-	echo "Missing src and/or dest arguments"
-	exit 1
+  echo "Missing src and/or dest arguments"
+  exit 1
 fi
 
 # Check if something already exists at the destination path replace it with
@@ -62,40 +62,39 @@ fi
 # report that it does not exist.
 
 if [ -h "${dest}" ]; then
-	rm -f "${dest}"
+  rm -f "${dest}"
 else
 
-	# If the path exists and is a directory that contains the "fake link"
-	# mark, then treat it like a soft link (i.e., remove the directory)
+  # If the path exists and is a directory that contains the "fake link"
+  # mark, then treat it like a soft link (i.e., remove the directory)
 
-	if [ -d "${dest}" -a -f "${dest}/.fakelnk" ]; then
-		rm -rf "${dest}"
-	else
+  if [ -d "${dest}" -a -f "${dest}/.fakelnk" ]; then
+    rm -rf "${dest}"
+  else
 
-		# Does anything exist at the destination path?
+    # Does anything exist at the destination path?
 
-		if [ -e "${dest}" ]; then
+    if [ -e "${dest}" ]; then
 
-			# It is something else (like a file) or directory that does
-			# not contain the "fake link" mark
+      # It is something else (like a file) or directory that does
+      # not contain the "fake link" mark
 
-			echo "${dest} already exists but is not a symbolic link"
-			exit 1
-		fi
-	fi
+      echo "${dest} already exists but is not a symbolic link"
+      exit 1
+    fi
+  fi
 fi
 
 # Verify that a directory exists at the source path
 
 if [ ! -d "${src}" ]; then
-	echo "No directory at ${src}"
-	exit 1
+  echo "No directory at ${src}"
+  exit 1
 fi
 
 # Copy the directory
 
 cp -a "${src}" "${dest}" || \
-	{ echo "Failed to create link: $dest" ; rm -rf ${dest} ; exit 1 ; }
+  { echo "Failed to create link: $dest" ; rm -rf ${dest} ; exit 1 ; }
 touch "${dest}/.fakelnk" || \
-	{ echo "Failed to touch ${dest}/.fakelnk" ; rm -rf ${dest} ; exit 1 ; }
-
+  { echo "Failed to touch ${dest}/.fakelnk" ; rm -rf ${dest} ; exit 1 ; }

tools/define.bat
@@ -165,14 +165,14 @@ echo definitions arguments for a variety of diffent ccpaths in a variety of
 echo compilation environments"
 echo USAGE:%progname% [-h] ^<compiler-path^> [-val ^<^val1^>] [^<def2^> [-val ^<val2^>] [^<def3^> [-val ^<val3^>] ...]]
 echo Where:"
-echo 	^<compiler-path^>
-echo 		The full path to your ccpath
-echo 	^<def1^> ^<def2^> ^<def3^> ...
-echo 		A list of pre-preprocesser variable names to be defined.
-echo 	[-val ^<val1^>] [-val ^<val2^>] [-val ^<val3^>] ...
-echo 		optional values to be assigned to each pre-processor variable.
-echo 		If not supplied, the variable will be defined with no explicit value.
-echo 	-h
-echo 		Show this text and exit
+echo  ^<compiler-path^>
+echo    The full path to your ccpath
+echo  ^<def1^> ^<def2^> ^<def3^> ...
+echo    A list of pre-preprocesser variable names to be defined.
+echo  [-val ^<val1^>] [-val ^<val2^>] [-val ^<val3^>] ...
+echo    optional values to be assigned to each pre-processor variable.
+echo    If not supplied, the variable will be defined with no explicit value.
+echo  -h
+echo    Show this text and exit
 
 :End

tools/define.sh
@@ -42,39 +42,39 @@ usage="USAGE: $progname [-w] [-d] [-h] <compiler-path> <def1>[=val1] [<def2>[=va
 advice="Try '$progname -h' for more information"
 
 while [ ! -z "$1" ]; do
-	case $1 in
-	-d )
-		set -x
-		;;
-	-w )
-		wintool=y
-		;;
-	-h )
-		echo "$progname is a tool for flexible generation of command line pre-processor"
-		echo "definitions arguments for a variety of diffent compilers in a variety of"
-		echo "compilation environments"
-		echo ""
-		echo $usage
-		echo ""
-		echo "Where:"
-		echo "	<compiler-path>"
-		echo "		The full path to your compiler"
-		echo "	<def1> <def2> [<def3> ..."
-		echo "		A list of pre-preprocesser variable names to be defined."
-		echo "	[=val1] [=val2] [=val3]"
-		echo "		optional values to be assigned to each pre-processor variable."
-		echo "		If not supplied, the variable will be defined with no explicit value."
-		echo "	-w"
-		echo "		The compiler is a Windows native tool and requires Windows"
-		echo "		style pathnames like C:\\Program Files"
-		echo "	-d"
-		echo "		Enable script debug"
-		;;
-	* )
-		break;
-		;;
-	esac
-	shift
+  case $1 in
+  -d )
+    set -x
+    ;;
+  -w )
+    wintool=y
+    ;;
+  -h )
+    echo "$progname is a tool for flexible generation of command line pre-processor"
+    echo "definitions arguments for a variety of diffent compilers in a variety of"
+    echo "compilation environments"
+    echo ""
+    echo $usage
+    echo ""
+    echo "Where:"
+    echo "  <compiler-path>"
+    echo "    The full path to your compiler"
+    echo "  <def1> <def2> [<def3> ..."
+    echo "    A list of pre-preprocesser variable names to be defined."
+    echo "  [=val1] [=val2] [=val3]"
+    echo "    optional values to be assigned to each pre-processor variable."
+    echo "    If not supplied, the variable will be defined with no explicit value."
+    echo "  -w"
+    echo "    The compiler is a Windows native tool and requires Windows"
+    echo "    style pathnames like C:\\Program Files"
+    echo "  -d"
+    echo "    Enable script debug"
+    ;;
+  * )
+    break;
+    ;;
+  esac
+  shift
 done
 
 ccpath=$1
@@ -82,17 +82,17 @@ shift
 varlist=$@
 
 if [ -z "$ccpath" ]; then
-	echo "Missing compiler path"
-	echo $usage
-	echo $advice
-	exit 1
+  echo "Missing compiler path"
+  echo $usage
+  echo $advice
+  exit 1
 fi
 
 if [ -z "$varlist" ]; then
-	echo "Missing definition list"
-	echo $usage
-	echo $advice
-	exit 1
+  echo "Missing definition list"
+  echo $usage
+  echo $advice
+  exit 1
 fi
 
 #
@@ -121,37 +121,37 @@ gcc=`echo $ccpath | grep gcc`
 sdcc=`echo $ccpath | grep sdcc`
 
 if [ "X$os" = "XCygwin" ]; then
-	#
-	# We can treat Cygwin native toolchains just like Linux native
-	# toolchains in the Linux.  Let's assume:
-	# 1. GCC or SDCC are the only possible Cygwin native compilers
-	# 2. If this is a Window native GCC version, then -w provided
-	#    on the command line (wintool=y)
+  #
+  # We can treat Cygwin native toolchains just like Linux native
+  # toolchains in the Linux.  Let's assume:
+  # 1. GCC or SDCC are the only possible Cygwin native compilers
+  # 2. If this is a Window native GCC version, then -w provided
+  #    on the command line (wintool=y)
 
-	if [ -z "$gcc" -a -z "$sdcc" ]; then
-		#
-		# Not GCC or SDCC, must be Windows native
-		#
-		compiler=`cygpath -u "$ccpath"`
-	else
-		if [ "X$wintool" == "Xy" ]; then
-			#
-			# It is a native GCC or SDCC compiler
-			#
-			compiler=`cygpath -u "$ccpath"`
-		else
-			#
-			# GCC or SDCC and not for Windows
-			#
-			compiler="$ccpath"
-		fi
-	fi
+  if [ -z "$gcc" -a -z "$sdcc" ]; then
+    #
+    # Not GCC or SDCC, must be Windows native
+    #
+    compiler=`cygpath -u "$ccpath"`
+  else
+    if [ "X$wintool" == "Xy" ]; then
+      #
+      # It is a native GCC or SDCC compiler
+      #
+      compiler=`cygpath -u "$ccpath"`
+    else
+      #
+      # GCC or SDCC and not for Windows
+      #
+      compiler="$ccpath"
+    fi
+  fi
 else
-	#
-	# Otherwise, we must be in a Linux environment where there are
-	# only Linux native toolchains
-	#
-	compiler="$ccpath"
+  #
+  # Otherwise, we must be in a Linux environment where there are
+  # only Linux native toolchains
+  #
+  compiler="$ccpath"
 fi
 exefile=`basename "$compiler"`
 
@@ -159,9 +159,9 @@ exefile=`basename "$compiler"`
 # a special output format as well as special paths
 
 if [ "X$exefile" = "Xez8cc.exe" -o "X$exefile" = "Xzneocc.exe" -o "X$exefile" = "Xez80cc.exe" ]; then
-	fmt=define
+  fmt=define
 else
-	fmt=std
+  fmt=std
 fi
 
 # Now process each definition in the definition list
@@ -169,60 +169,58 @@ fi
 unset response
 for vardef in $varlist; do
 
-	varname=`echo $vardef | cut -d'=' -f1`
-	if [ "X$varname" != "X$vardef" ]; then
-		varvalue=`echo $vardef | cut -d'=' -f2`
-	else
-		unset varvalue
-	fi
+  varname=`echo $vardef | cut -d'=' -f1`
+  if [ "X$varname" != "X$vardef" ]; then
+    varvalue=`echo $vardef | cut -d'=' -f2`
+  else
+    unset varvalue
+  fi
 
-	# Handle the output depending on if there is a value for the variable or not
+  # Handle the output depending on if there is a value for the variable or not
 
-	if [ -z "$varvalue" ]; then
+  if [ -z "$varvalue" ]; then
 
-		# Handle the output using the selected format
+    # Handle the output using the selected format
 
-		if [ "X$fmt" = "Xdefine" ]; then
-			# Treat the first definition differently
+    if [ "X$fmt" = "Xdefine" ]; then
+      # Treat the first definition differently
 
-			if [ -z "$response" ]; then
-				response="-define:"$varname
-			else
-				response=$response" -define:$varname"
-			fi
-		else
-			# Treat the first definition differently
+      if [ -z "$response" ]; then
+        response="-define:"$varname
+      else
+        response=$response" -define:$varname"
+      fi
+    else
+      # Treat the first definition differently
 
-			if [ -z "$response" ]; then
-				response=-D$varname
-			else
-				response=$response" -D$varname"
-			fi
-		fi
-	else
+      if [ -z "$response" ]; then
+        response=-D$varname
+      else
+        response=$response" -D$varname"
+      fi
+    fi
+  else
 
-		# Handle the output using the selected format
+    # Handle the output using the selected format
 
-		if [ "X$fmt" = "Xdefine" ]; then
-			# Treat the first definition differently
+    if [ "X$fmt" = "Xdefine" ]; then
+      # Treat the first definition differently
 
-			if [ -z "$response" ]; then
-				response="-define:"$varname=$varvalue
-			else
-				response=$response" -define:$varname=$varvalue"
-			fi
-		else
-			# Treat the first definition differently
+      if [ -z "$response" ]; then
+        response="-define:"$varname=$varvalue
+      else
+        response=$response" -define:$varname=$varvalue"
+      fi
+    else
+      # Treat the first definition differently
 
-			if [ -z "$response" ]; then
-				response=-D$varname=$varvalue
-			else
-				response=$response" -D$varname=$varvalue"
-			fi
-		fi
-	fi
+      if [ -z "$response" ]; then
+        response=-D$varname=$varvalue
+      else
+        response=$response" -D$varname=$varvalue"
+      fi
+    fi
+  fi
 done
 
 echo $response
-
-

tools/incdir.bat
@@ -151,15 +151,15 @@ echo %progname% is a tool for flexible generation of include path arguments for 
 echo variety of different compilers in a variety of compilation environments
 echo USAGE: %progname% [-w] [-d] [-s] [-h] ^<compiler-path^> ^<dir1^> [^<dir2^> [^<dir3^> ...]]
 echo Where:
-echo   ^<compiler-path^>
-echo       The full path to your compiler
-echo   ^<dir1^> [^<dir2^> [^<dir3^> ...]]
-echo       A list of include directories
-echo   -w, -d
-echo       For compatibility with incdir.sh (ignored)
-echo   -s
-echo       Generate standard, system header file paths instead of normal user
-echo       header file paths.
-echo   -h
-echo       Shows this help text and exits.
+echo  ^<compiler-path^>
+echo    The full path to your compiler
+echo  ^<dir1^> [^<dir2^> [^<dir3^> ...]]
+echo    A list of include directories
+echo  -w, -d
+echo    For compatibility with incdir.sh (ignored)
+echo  -s
+echo    Generate standard, system header file paths instead of normal user
+echo    header file paths.
+echo  -h
+echo    Shows this help text and exits.
 :End

commit 73ad81dacd6fa7465937436629970199b7c833fa

tools/incdir.sh:  Correct error in select of include path
Commit 3b9efc95a2e introduced an error in the generation of include file paths.  The logic that determined if cygpath should be called to create a Windows native path for the case of Cygwin using a native toolchain was incorrect.  This corrects this warning:

 ./tools/configure.sh -c sim:nsh
$ make
<CUT>
make[1]: Entering directory '/home/btashton/apache/apps'
make[2]: Entering directory '/home/btashton/apache/apps/builtin'
./exec_builtin.c:54:10: fatal error: builtin/builtin.h: No such file or directory
   54 | #include "builtin/builtin.h"
      |          ^~~~~~~~~~~~~~~~~~~
compilation terminated.
ERROR: cc failed: 1
       command: cc -MT ./exec_builtin.home.btashton.apache.apps.builtin.o  -M -Wall -Wstrict-prototypes -Wshadow -Wundef -g -fno-builtin -fno-common -I. -isystem /home/btashton/apache/nuttx/include -D__KERNEL__ -pipe -I C:\cygwin64\home\btashton\apache\apps\include ./exec_builtin.c
make[2]: *** [/home/btashton/apache/apps/Application.mk:224: .depend] Error 1
make[2]: Leaving directory '/home/btashton/apache/apps/builtin'
make[1]: *** [Makefile:67: /home/btashton/apache/apps/builtin_depend] Error 2

In this case a Cygwin POSIX toolchain is being used by the path in the CFLAGS to apps/include is incorrectly a Windows native path.  This error is corrected by this change to tools/incdir.sh

tools/incdir.sh
@@ -200,8 +200,10 @@ for dir in $dirlist; do
   fi
 
   # Check if the path needs to be extended for Windows-based tools under Cygwin
+  # windows=yes:  We are building for a windows platform
+  # wintool=y:    The platform is Cygwin and we are using a windows native tool
 
-  if [ "X$windows" = "Xyes" ]; then
+  if [ "X$windows" = "Xyes" -a "X$wintool" == "Xy" ]; then
     path=`cygpath -w $dir`
   else
     path=$dir