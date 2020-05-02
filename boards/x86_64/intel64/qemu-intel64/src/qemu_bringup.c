dd309aeadd7ea8179aaefbf0d5dbf003b5173694
Brennan Ashton <bashton@brennanashton.com>
2020-05-02 15:38:30 -0700

    x86_64: Add nsh configuration with procfs bringup
    Signed-off-by: Brennan Ashton <bashton@brennanashton.com>

--- boards/x86_64/intel64/qemu-intel64/src/qemu_bringup.c
+++ boards/x86_64/intel64/qemu-intel64/src/qemu_bringup.c
@@ -1,5 +1,5 @@
 /****************************************************************************
- * boards/x86_64/intel64/qemu/src/qemu_appinit.c
+ * boards/x86_64/intel64/qemu-intel64/src/qemu_bringup.c
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -23,9 +23,16 @@
  ****************************************************************************/
 
 #include <nuttx/config.h>
-#include <nuttx/board.h>
 
-#include "up_internal.h"
+#include <sys/mount.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <syslog.h>
+#include <errno.h>
+
+#include <nuttx/board.h>
+#include <nuttx/input/buttons.h>
+
 #include "qemu_intel64.h"
 
 /****************************************************************************
@@ -33,39 +40,22 @@
  ****************************************************************************/
 
 /****************************************************************************
- * Name: board_app_initialize
- *
- * Description:
- *   Perform application specific initialization.  This function is never
- *   called directly from application code, but only indirectly via the
- *   (non-standard) boardctl() interface using the command BOARDIOC_INIT.
- *
- * Input Parameters:
- *   arg - The boardctl() argument is passed to the board_app_initialize()
- *         implementation without modification.  The argument has no
- *         meaning to NuttX; the meaning of the argument is a contract
- *         between the board-specific initialization logic and the
- *         matching application logic.  The value cold be such things as a
- *         mode enumeration value, a set of DIP switch switch settings, a
- *         pointer to configuration data read from a file or serial FLASH,
- *         or whatever you would like to do with it.  Every implementation
- *         should accept zero/NULL as a default configuration.
- *
- * Returned Value:
- *   Zero (OK) is returned on success; a negated errno value is returned on
- *   any failure to indicate the nature of the failure.
- *
+ * Name: qemu_bringup
  ****************************************************************************/
 
-int board_app_initialize(uintptr_t arg)
+int qemu_bringup(void)
 {
-#ifdef CONFIG_BOARD_LATE_INITIALIZE
-  /* Board initialization already performed by board_late_initialize() */
+  int ret = OK;
 
-  return OK;
-#else
-  /* Perform board-specific initialization */
+#ifdef CONFIG_FS_PROCFS
+  /* Mount the procfs file system */
 
-  return qemu_bringup();
+  ret = mount(NULL, "/proc", "procfs", 0, NULL);
+  if (ret < 0)
+    {
+      serr("ERROR: Failed to mount procfs at %s: %d\n", "/proc", ret);
+    }
 #endif
+
+  return ret;
 }
