51e9afedbb2a1d62fedf4df950325e5fe697f505
Xiang Xiao <xiaoxiang@xiaomi.com>
2020-05-04 21:32:03 +0800

    fs: Move inode_checkflags from include/nuttx/fs/fs.h to fs/inode/inode.h
    since this function is only used inside fs subsystem
    
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- fs/inode/inode.h
+++ fs/inode/inode.h
@@ -187,6 +187,16 @@ int inode_semtake(void);
 void inode_semgive(void);
 
 /****************************************************************************
+ * Name: inode_checkflags
+ *
+ * Description:
+ *   Check if the access described by 'oflags' is supported on 'inode'
+ *
+ ****************************************************************************/
+
+int inode_checkflags(FAR struct inode *inode, int oflags);
+
+/****************************************************************************
  * Name: inode_search
  *
  * Description:
