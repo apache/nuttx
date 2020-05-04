18f97bf2f8f88fe41b77b2ffee8ffb3447ef98fa
Yang Chung-Fan <sonic.tw.tp@gmail.com>
2020-05-04 18:44:11 +0900

    pcie: add framework
    Squashed commits:
    
    1. x86_64: qemu: implement pci-e functions and enumerate pci-e devices on boot
    
    2. virt: add qemu pci-testdev driver
    
    3. pcie: types array should be null terminated
    
    4. pcie: enable don't take flags, hardcoded enabling flags
    
    5. pcie: checking bar > 4 for 64bit bars are sufficient
    
    6. pcie: qemu: remove not used header
    
    7. pcie: qemu: return -EINVAL if buffer argument is NULL
    
    8. pcie: make pcie enumerate routine as common instead of architecture dependent
    
    9. pcie: cosmetic changes to fit check tools
    
    10. pcie: create MSI/MSIX related marcos and simplify the msi/msix routines

--- include/debug.h
+++ include/debug.h
@@ -920,6 +920,24 @@
 #  define ipcinfo     _none
 #endif
 
+#ifdef CONFIG_DEBUG_PCIE_ERROR
+#  define pcierr       _err
+#else
+#  define pcierr      _none
+#endif
+
+#ifdef CONFIG_DEBUG_PCIE_WARN
+#  define pciwarn     _warn
+#else
+#  define pciwarn     _none
+#endif
+
+#ifdef CONFIG_DEBUG_PCIE_INFO
+#  define pciinfo     _info
+#else
+#  define pciinfo     _none
+#endif
+
 /* Buffer dumping macros do not depend on varargs */
 
 #ifdef CONFIG_DEBUG_ERROR
