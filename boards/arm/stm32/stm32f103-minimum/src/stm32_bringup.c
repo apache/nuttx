c43b2c2884ac8f7d24d378bf9785470d8ec4455b
Matias Nitsche <mnitsche@dc.uba.ar>
2020-05-08 14:35:51 -0300

    stm32: move nunchuck driver to common board logic

--- boards/arm/stm32/stm32f103-minimum/src/stm32_bringup.c
+++ boards/arm/stm32/stm32f103-minimum/src/stm32_bringup.c
@@ -101,6 +101,10 @@
 #include "stm32_veml6070.h"
 #endif
 
+#ifdef CONFIG_INPUT_NUNCHUCK
+#include "stm32_nunchuck.h"
+#endif
+
 /****************************************************************************
  * Pre-processor Definitions
  ****************************************************************************/
@@ -360,7 +364,7 @@ int stm32_bringup(void)
 #ifdef CONFIG_INPUT_NUNCHUCK
   /* Register the Nunchuck driver */
 
-  ret = nunchuck_initialize("/dev/nunchuck0");
+  ret = board_nunchuck_initialize(0, 1);
   if (ret < 0)
     {
       syslog(LOG_ERR, "ERROR: nunchuck_initialize() failed: %d\n", ret);
