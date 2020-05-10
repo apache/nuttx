153eee6de2ec204a886f0edc4e7a3021ab5e8fc4
Gregory Nutt <gnutt@nuttx.org>
2020-05-10 07:13:21 -0600

    Make more OS internal names consistent
    1. Internal scheduler functions should begin with nxsched_, not sched_
    2. Follow the consistent naming patter of https://cwiki.apache.org/confluence/display/NUTTX/Naming+of+OS+Internal+Functions
    
        # clock_systimer -> clock_systime_tick
        # clock_systimespec -> clock_systime_timespec
        sched_oneshot_extclk -> nxsched_oneshot_extclk
        sched_period_extclk -> nxsched_period_extclk
        # nxsem_setprotocol -> nxsem_set_protocol
        # nxsem_getprotocol -> nxsem_get_protocol
        # nxsem_getvalue -> nxsem_get_value
        nxsem_initholders -> nxsem_initialize_holders
        nxsem_addholder -> nxsem_add_holder
        nxsem_addholder_tcb -> nxsem_add_holder_tcb
        nxsem_boostpriority -> nxsem_boost_priority
        nxsem_releaseholder -> nxsem_release_holder
        nxsem_restorebaseprio -> nxsem_restore_baseprio
    
    Some planned name changed were skipped for now because they effect too many files (and would require many hours of coding style fixups).

--- sched/semaphore/sem_post.c
+++ sched/semaphore/sem_post.c
@@ -1,36 +1,20 @@
 /****************************************************************************
  * sched/semaphore/sem_post.c
  *
- *   Copyright (C) 2007-2009, 2012-2013, 2016-2017 Gregory Nutt. All rights
- *     reserved.
- *   Author: Gregory Nutt <gnutt@nuttx.org>
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- * 3. Neither the name NuttX nor the names of its contributors may be
- *    used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
- * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
- * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
  *
  ****************************************************************************/
 
@@ -113,7 +97,7 @@ int nxsem_post(FAR sem_t *sem)
        *
        * NOTE:  When semaphores are used for signaling purposes, the holder
        * of the semaphore may not be this thread!  In this case,
-       * nxsem_releaseholder() will do nothing.
+       * nxsem_release_holder() will do nothing.
        *
        * In the case of a mutex this could be simply resolved since there is
        * only one holder but for the case of counting semaphores, there may
@@ -125,7 +109,7 @@ int nxsem_post(FAR sem_t *sem)
        * initialized if the semaphore is to used for signaling purposes.
        */
 
-      nxsem_releaseholder(sem);
+      nxsem_release_holder(sem);
       sem->semcount++;
 
 #ifdef CONFIG_PRIORITY_INHERITANCE
@@ -161,7 +145,7 @@ int nxsem_post(FAR sem_t *sem)
                * it is awakened.
                */
 
-              nxsem_addholder_tcb(stcb, sem);
+              nxsem_add_holder_tcb(stcb, sem);
 
               /* It is, let the task take the semaphore */
 
@@ -187,7 +171,7 @@ int nxsem_post(FAR sem_t *sem)
        */
 
 #ifdef CONFIG_PRIORITY_INHERITANCE
-      nxsem_restorebaseprio(stcb, sem);
+      nxsem_restore_baseprio(stcb, sem);
       sched_unlock();
 #endif
       ret = OK;
