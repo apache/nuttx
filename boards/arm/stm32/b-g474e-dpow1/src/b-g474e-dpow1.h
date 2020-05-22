70b6c9396c74926bb713aaf815f5abf4e30a99e8
Nathan Hartman <59230071+hartmannathan@users.noreply.github.com>
2020-05-22 11:52:17 -0400

    Add support for STM32G474: Add B-G474E-DPOW1 board
    Add support for the STM32G474 family of microcontrollers and the
    B-G474E-DPOW1 Discovery Board, which features a STM32G474RET6.
    
    This is a major pull request as it adds support for an entirely
    new family of STM32. This support is implemented in
    arch/arm/src/stm32 and shares implementation with other STM32
    families supported by that code, such as the 'L15xx, 'F10xx,
    'F20xx, 'F3xxx, and 'F4xxx.
    
    boards/Kconfig:
    
        * Make NuttX recognize the existence of b-g474e-dpow1,
          the B-G474E-DPOW1 Discovery Board.
    
    boards/arm/stm32/b-g474e-dpow1/Kconfig:
    boards/arm/stm32/b-g474e-dpow1/README.txt:
    boards/arm/stm32/b-g474e-dpow1/configs/nsh/defconfig:
    boards/arm/stm32/b-g474e-dpow1/include/board.h:
    boards/arm/stm32/b-g474e-dpow1/scripts/Make.defs:
    boards/arm/stm32/b-g474e-dpow1/scripts/ld.script:
    boards/arm/stm32/b-g474e-dpow1/src/.gitignore:
    boards/arm/stm32/b-g474e-dpow1/src/Make.defs:
    boards/arm/stm32/b-g474e-dpow1/src/b-g474e-dpow1.h:
    boards/arm/stm32/b-g474e-dpow1/src/stm32_appinit.c:
    boards/arm/stm32/b-g474e-dpow1/src/stm32_autoleds.c:
    boards/arm/stm32/b-g474e-dpow1/src/stm32_boot.c:
    boards/arm/stm32/b-g474e-dpow1/src/stm32_userleds.c:
    
        * Add minimal support for the B-G474E-DPOW1 Discovery
          Board. The board boots successfully through to the
          NSH prompt. NSH runs and is responsive.
    
    With big thanks for detailed code review and suggestions:
        David Sidrane (davids5)
        Mateusz Szafoni (raiden00)
        Abdelatif Guettouche (Ouss4)

---
+++ boards/arm/stm32/b-g474e-dpow1/src/b-g474e-dpow1.h
@@ -0,0 +1,77 @@
+/****************************************************************************
+ *  boards/arm/stm32/b-g474e-dpow1/src/b-g474e-dpow1.h
+ *
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.  The
+ *  ASF licenses this file to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance with the
+ *  License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ *  License for the specific language governing permissions and limitations
+ *  under the License.
+ *
+ ****************************************************************************/
+
+#ifndef __BOARDS_ARM_STM32_B_G474E_DPOW1_SRC_B_G474E_DPOW1_H
+#define __BOARDS_ARM_STM32_B_G474E_DPOW1_SRC_B_G474E_DPOW1_H
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+/* LED definitions **********************************************************/
+
+/* The B-G474E-DPOW1 Discovery kit has four user LEDs:
+ *
+ * | Symbol     | PCB | Color  |
+ * |------------|-----|--------|
+ * | BOARD_LED1 | LD2 | Blue   |
+ * | BOARD_LED2 | LD3 | Orange |
+ * | BOARD_LED3 | LD4 | Green  |
+ * | BOARD_LED4 | LD5 | Red    |
+ *
+ * If CONFIG_ARCH_LEDS is not defined, then the user can control the LED.
+ *
+ * Pin assignments follow:
+ */
+
+#define GPIO_LED1                      (GPIO_OUTPUT | GPIO_PUSHPULL | \
+                                        GPIO_SPEED_5MHz | \
+                                        GPIO_OUTPUT_CLEAR | \
+                                        GPIO_PORTA | GPIO_PIN15)
+#define GPIO_LED2                      (GPIO_OUTPUT | GPIO_PUSHPULL | \
+                                        GPIO_SPEED_5MHz | \
+                                        GPIO_OUTPUT_CLEAR | \
+                                        GPIO_PORTB | GPIO_PIN1)
+#define GPIO_LED3                      (GPIO_OUTPUT | GPIO_PUSHPULL | \
+                                        GPIO_SPEED_5MHz | \
+                                        GPIO_OUTPUT_CLEAR | \
+                                        GPIO_PORTB | GPIO_PIN7)
+#define GPIO_LED4                      (GPIO_OUTPUT | GPIO_PUSHPULL | \
+                                        GPIO_SPEED_5MHz | \
+                                        GPIO_OUTPUT_CLEAR | \
+                                        GPIO_PORTB | GPIO_PIN5)
+
+#define LED_DRIVER_PATH                "/dev/userleds"
+
+/****************************************************************************
+ * Public Data
+ ****************************************************************************/
+
+/****************************************************************************
+ * Public Function Prototypes
+ ****************************************************************************/
+
+#endif /* __BOARDS_ARM_STM32_B_G474E_DPOW1_SRC_B_G474E_DPOW1_H */
---
+++ boards/arm/stm32/b-g474e-dpow1/src/stm32_boot.c
@@ -0,0 +1,70 @@
+/****************************************************************************
+ *  boards/arm/stm32/b-g474e-dpow1/src/stm32_boot.c
+ *
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.  The
+ *  ASF licenses this file to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance with the
+ *  License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ *  License for the specific language governing permissions and limitations
+ *  under the License.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+#include <nuttx/board.h>
+#include <arch/board/board.h>
+
+#include "b-g474e-dpow1.h"
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Private Function Prototypes
+ ****************************************************************************/
+
+/****************************************************************************
+ * Private Data
+ ****************************************************************************/
+
+/****************************************************************************
+ * Private Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: stm32_boardinitialize
+ *
+ * Description:
+ *   All STM32 architectures must provide the following entry point.  This
+ *   entry point is called early in the initialization -- after all memory
+ *   has been configured and mapped but before any devices have been
+ *   initialized.
+ *
+ ****************************************************************************/
+
+void stm32_boardinitialize(void)
+{
+#if defined(CONFIG_ARCH_LEDS)
+  /* Configure on-board LEDs if LED support has been selected. */
+
+  board_autoled_initialize();
+#endif
+}
