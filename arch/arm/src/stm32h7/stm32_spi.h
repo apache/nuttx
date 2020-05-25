e1babbfef335ad0370080ec4cce5514f56401698
Jukka Laitinen <jukka.laitinen@intel.com>
2020-05-25 12:13:54 +0300

    arch/arm/src/stm32h7/stm32_spi.h: Split long lines
    Signed-off-by: Jukka Laitinen <jukka.laitinen@intel.com>

--- arch/arm/src/stm32h7/stm32_spi.h
+++ arch/arm/src/stm32h7/stm32_spi.h
@@ -1,4 +1,4 @@
-/************************************************************************************
+/*****************************************************************************
  * arch/arm/src/stm32h7/stm32_spi.h
  *
  *   Copyright (C) 2018 Gregory Nutt. All rights reserved.
@@ -33,14 +33,14 @@
  * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  *
- ************************************************************************************/
+ *****************************************************************************/
 
 #ifndef __ARCH_ARM_SRC_STM32H7_STM32_SPI_H
 #define __ARCH_ARM_SRC_STM32H7_STM32_SPI_H
 
-/************************************************************************************
+/*****************************************************************************
  * Included Files
- ************************************************************************************/
+ *****************************************************************************/
 
 #include <nuttx/config.h>
 
@@ -48,10 +48,6 @@
 
 #include "chip.h"
 #include "hardware/stm32_spi.h"
-
-/************************************************************************************
- * Public Functions
- ************************************************************************************/
 
 #ifndef __ASSEMBLY__
 
@@ -66,7 +62,11 @@ extern "C"
 
 struct spi_dev_s; /* Forward reference */
 
-/************************************************************************************
+/*****************************************************************************
+ * Public Function Prototypes
+ *****************************************************************************/
+
+/*****************************************************************************
  * Name: stm32_spibus_initialize
  *
  * Description:
@@ -78,84 +78,92 @@ struct spi_dev_s; /* Forward reference */
  * Returned Value:
  *   Valid SPI device structure reference on success; a NULL on failure
  *
- ************************************************************************************/
+ *****************************************************************************/
 
 FAR struct spi_dev_s *stm32_spibus_initialize(int bus);
 
-/************************************************************************************
+/*****************************************************************************
  * Name:  stm32_spi1/2/...select and stm32_spi1/2/...status
  *
  * Description:
- *   The external functions, stm32_spi1/2/...select, stm32_spi1/2/...status, and
- *   stm32_spi1/2/...cmddata must be provided by board-specific logic.  These are
- *   implementations of the select, status, and cmddata methods of the SPI interface
- *   defined by struct spi_ops_s (see include/nuttx/spi/spi.h). All other methods
- *   (including stm32_spibus_initialize()) are provided by common STM32 logic.  To use this
- *   common SPI logic on your board:
+ *   The external functions, stm32_spi1/2/...select, stm32_spi1/2/...status,
+ *   and stm32_spi1/2/...cmddata must be provided by board-specific logic.
+ *   These are implementations of the select, status, and cmddata methods of
+ *   the SPI interface defined by struct spi_ops_s (see
+ *   include/nuttx/spi/spi.h).
+ *   All other methods (including stm32_spibus_initialize()) are provided by
+ *   common STM32 logic.  To use this common SPI logic on your board:
  *
  *   1. Provide logic in stm32_boardinitialize() to configure SPI chip select
  *      pins.
- *   2. Provide stm32_spi1/2/...select() and stm32_spi1/2/...status() functions in your
- *      board-specific logic.  These functions will perform chip selection and
- *      status operations using GPIOs in the way your board is configured.
- *   3. If CONFIG_SPI_CMDDATA is defined in your NuttX configuration file, then
- *      provide stm32_spi1/2/...cmddata() functions in your board-specific logic.
- *      These functions will perform cmd/data selection operations using GPIOs in the
- *      way your board is configured.
+ *   2. Provide stm32_spi1/2/...select() and stm32_spi1/2/...status()
+ *      functions in your board-specific logic.  These functions will perform
+ *      chip selection and status operations using GPIOs in the way your board
+ *      is configured.
+ *   3. If CONFIG_SPI_CMDDATA is defined in your NuttX configuration file,
+ *      then provide stm32_spi1/2/...cmddata() functions in your board-
+ *      specific logic. These functions will perform cmd/data selection
+ *      operations using GPIOs in the way your board is configured.
  *   4. Add a calls to stm32_spibus_initialize() in your low level application
  *      initialization logic
- *   5. The handle returned by stm32_spibus_initialize() may then be used to bind the
- *      SPI driver to higher level logic (e.g., calling
+ *   5. The handle returned by stm32_spibus_initialize() may then be used to
+ *      bind the SPI driver to higher level logic (e.g., calling
  *      mmcsd_spislotinitialize(), for example, will bind the SPI driver to
  *      the SPI MMC/SD driver).
  *
- ************************************************************************************/
+ *****************************************************************************/
 
 #ifdef CONFIG_STM32H7_SPI1
-void stm32_spi1select(FAR struct spi_dev_s *dev, uint32_t devid, bool selected);
+void stm32_spi1select(FAR struct spi_dev_s *dev, uint32_t devid,
+                      bool selected);
 uint8_t stm32_spi1status(FAR struct spi_dev_s *dev, uint32_t devid);
 int stm32_spi1cmddata(FAR struct spi_dev_s *dev, uint32_t devid, bool cmd);
 #endif
 
 #ifdef CONFIG_STM32H7_SPI2
-void stm32_spi2select(FAR struct spi_dev_s *dev, uint32_t devid, bool selected);
+void stm32_spi2select(FAR struct spi_dev_s *dev, uint32_t devid,
+                      bool selected);
 uint8_t stm32_spi2status(FAR struct spi_dev_s *dev, uint32_t devid);
 int stm32_spi2cmddata(FAR struct spi_dev_s *dev, uint32_t devid, bool cmd);
 #endif
 
 #ifdef CONFIG_STM32H7_SPI3
-void stm32_spi3select(FAR struct spi_dev_s *dev, uint32_t devid, bool selected);
+void stm32_spi3select(FAR struct spi_dev_s *dev, uint32_t devid,
+                      bool selected);
 uint8_t stm32_spi3status(FAR struct spi_dev_s *dev, uint32_t devid);
 int stm32_spi3cmddata(FAR struct spi_dev_s *dev, uint32_t devid, bool cmd);
 #endif
 
 #ifdef CONFIG_STM32H7_SPI4
-void stm32_spi4select(FAR struct spi_dev_s *dev, uint32_t devid, bool selected);
+  void stm32_spi4select(FAR struct spi_dev_s *dev, uint32_t devid,
+                        bool selected);
 uint8_t stm32_spi4status(FAR struct spi_dev_s *dev, uint32_t devid);
 int stm32_spi4cmddata(FAR struct spi_dev_s *dev, uint32_t devid, bool cmd);
 #endif
 
 #ifdef CONFIG_STM32H7_SPI5
-void stm32_spi5select(FAR struct spi_dev_s *dev, uint32_t devid, bool selected);
+void stm32_spi5select(FAR struct spi_dev_s *dev, uint32_t devid,
+                      bool selected);
 uint8_t stm32_spi5status(FAR struct spi_dev_s *dev, uint32_t devid);
 int stm32_spi5cmddata(FAR struct spi_dev_s *dev, uint32_t devid, bool cmd);
 #endif
 
 #ifdef CONFIG_STM32H7_SPI6
-void stm32_spi6select(FAR struct spi_dev_s *dev, uint32_t devid, bool selected);
+void stm32_spi6select(FAR struct spi_dev_s *dev, uint32_t devid,
+                      bool selected);
 uint8_t stm32_spi6status(FAR struct spi_dev_s *dev, uint32_t devid);
 int stm32_spi6cmddata(FAR struct spi_dev_s *dev, uint32_t devid, bool cmd);
 #endif
 
-/************************************************************************************
+/*****************************************************************************
  * Name: stm32_spi1/2/...register
  *
  * Description:
- *   If the board supports a card detect callback to inform the SPI-based MMC/SD
- *   driver when an SD card is inserted or removed, then CONFIG_SPI_CALLBACK should
- *   be defined and the following function(s) must be implemented.  These functions
- *   implements the registercallback method of the SPI interface (see
- *   include/nuttx/spi/spi.h for details)
+ *   If the board supports a card detect callback to inform the SPI-based
+ *   MMC/SD driver when an SD card is inserted or removed, then
+ *   CONFIG_SPI_CALLBACK should be defined and the following function(s) must
+ *   be implemented.  These functions implements the registercallback method
+ *   of the SPI interface (see include/nuttx/spi/spi.h for details)
  *
  * Input Parameters:
  *   dev -      Device-specific state data
@@ -165,7 +173,7 @@ int stm32_spi6cmddata(FAR struct spi_dev_s *dev, uint32_t devid, bool cmd);
  * Returned Value:
  *   0 on success; negated errno on failure.
  *
- ****************************************************************************/
+ *****************************************************************************/
 
 #ifdef CONFIG_SPI_CALLBACK
 #ifdef CONFIG_STM32H7_SPI1
