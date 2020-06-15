a53e5d0e8cf52a85fcc8ff9930a9d5672bc23dfc
Xiang Xiao <xiaoxiang@xiaomi.com>
2020-06-15 15:22:53 +0800

    libc/locale: Add the mininal support for locale_t operation
    include duplocale, freelocale, newlocale and uselocale
    
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>
    Change-Id: I9912003847dec660ae5c62836d4d56ebe0718869

---
+++ libs/libc/locale/lib_uselocale.c
@@ -0,0 +1,47 @@
+/****************************************************************************
+ * libs/libc/locale/lib_uselocale.c
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+#include <locale.h>
+
+#ifdef CONFIG_LIBC_LOCALE
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: uselocale
+ *
+ * Description:
+ *   locales are not supported by NuttX
+ *
+ ****************************************************************************/
+
+locale_t uselocale(locale_t newloc)
+{
+  return localeconv();
+}
+#endif
