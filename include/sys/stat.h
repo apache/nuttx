b598ab43d75659251d945ea0a20f0e2409f25ed4
Xiang Xiao <xiaoxiang@xiaomi.com>
2020-06-22 16:44:32 +0800

    fs: Implement lstat function
    specified here:
    https://pubs.opengroup.org/onlinepubs/009695399/functions/lstat.html
    
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>
    Change-Id: Iefc23a02d425ff84fa4027aea7da1181b01eced7

--- include/sys/stat.h
+++ include/sys/stat.h
@@ -156,6 +156,7 @@ extern "C"
 int mkdir(FAR const char *pathname, mode_t mode);
 int mkfifo(FAR const char *pathname, mode_t mode);
 int stat(FAR const char *path, FAR struct stat *buf);
+int lstat(FAR const char *path, FAR struct stat *buf);
 int fstat(int fd, FAR struct stat *buf);
 int chmod(FAR const char *path, mode_t mode);
 int fchmod(int fd, mode_t mode);
