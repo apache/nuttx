8705ddc0179cc946a55bdf73da3f52fae808e886
chao.an <anchao@xiaomi.com>
2020-07-01 10:29:57 +0800

    make/export: use LDNAME instead of LDSCRIPT
    use LDNAME instead of LDSCRIPT to avoid invalid native path
    exported to the import build
    
    Change-Id: I1ba3cfdbfca02447941dc32a292fe4d76c1ced04
    Signed-off-by: chao.an <anchao@xiaomi.com>

--- tools/mkexport.sh
+++ tools/mkexport.sh
@@ -260,7 +260,7 @@ echo "HOSTINCLUDES     = ${HOSTINCLUDES}" >>"${EXPORTDIR}/scripts/Make.defs"
 echo "HOSTCFLAGS       = ${HOSTCFLAGS}" >>"${EXPORTDIR}/scripts/Make.defs"
 echo "HOSTLDFLAGS      = ${HOSTLDFLAGS}" >>"${EXPORTDIR}/scripts/Make.defs"
 echo "HOSTEXEEXT       = ${HOSTEXEEXT}" >>"${EXPORTDIR}/scripts/Make.defs"
-echo "LDSCRIPT         = ${LDSCRIPT}" >>"${EXPORTDIR}/scripts/Make.defs"
+echo "LDNAME           = ${LDNAME}" >>"${EXPORTDIR}/scripts/Make.defs"
 
 # Additional compilation options when the kernel is built
 
