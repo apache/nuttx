1076b748a25d613b40c5ad1e33750cb4a48e911f
Adam Feuer <adam@adamfeuer.com>
2020-07-06 12:38:45 -0700

    changed header comment filename to match current

--- tools/checkrelease.sh
+++ tools/checkrelease.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #############################################################################
-# tools/check-nuttx-release.sh
+# tools/checkrelease.sh
 #
 # Licensed to the Apache Software Foundation (ASF) under one or more
 # contributor license agreements.  See the NOTICE file distributed with
