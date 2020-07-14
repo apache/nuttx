fef8aecbb97cd40a48a387eaceb6f15547cb577f
ligd <liguiding1@xiaomi.com>
2020-07-14 11:21:03 +0800

    signal: add SIGQUIT & SIGTERM support
    SIGQUIT SIGTERM are equal with SIGINT now
    
    Change-Id: Iefc084d58db28003dc40a17f1ff3fbd7a0b716ed

--- sched/signal/sig_default.c
+++ sched/signal/sig_default.c
@@ -140,6 +140,8 @@ static const struct nxsig_defaction_s g_defactions[] =
 #ifdef CONFIG_SIG_SIGKILL_ACTION
   { SIGINT,  0,                nxsig_abnormal_termination },
   { SIGKILL, SIG_FLAG_NOCATCH, nxsig_abnormal_termination },
+  { SIGQUIT, 0,                nxsig_abnormal_termination },
+  { SIGTERM, 0,                nxsig_abnormal_termination },
 #endif
 #ifdef CONFIG_SIG_SIGPIPE_ACTION
   { SIGPIPE, 0,                nxsig_abnormal_termination }
