06a223976cc41148e1f6f06fd7f0afec3eb1e042
Matias N <matias@protobits.dev>
2020-07-15 23:11:31 -0300

    initial support for nrf52832-sparkfun board

---
+++ boards/arm/nrf52/nrf52832-sparkfun/README.txt
@@ -0,0 +1,5 @@
+README
+======
+
+README for NuttX port to Sparkfun NRF52832 breakout board.
+
---
+++ boards/arm/nrf52/nrf52832-sparkfun/src/nrf52_autoleds.c
@@ -0,0 +1,142 @@
+/****************************************************************************
+ * boards/arm/nrf52/nrf52832-sparkfun.h/src/lpc43_autoleds.c
+ *
+ *   Copyright (C) 2018 Gregory Nutt. All rights reserved.
+ *   Author: Gregory Nutt <gnutt@nuttx.org>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name NuttX nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+/* The NRF52 Feather board has 2 user-controllable LEDs:
+ *
+ *   LED   MCU
+ *   LED1  PIN0-17
+ *   LED2  PIN0-19
+ *
+ * A low output illuminates the LED.
+ */
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <debug.h>
+
+#include <nuttx/board.h>
+#include <arch/board/board.h>
+
+#include "chip.h"
+#include "arm_arch.h"
+#include "arm_internal.h"
+
+#include "nrf52832-sparkfun.h"
+
+#ifdef CONFIG_ARCH_LEDS
+
+#define LED_ON 1
+#define LED_OFF 0
+
+/* This array maps an LED number to GPIO pin configuration */
+
+static const uint32_t g_ledcfg[BOARD_NLEDS] =
+{
+  GPIO_LED1
+};
+
+/****************************************************************************
+ * Private Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: led_dumppins
+ ****************************************************************************/
+
+#ifdef LED_VERBOSE
+static void led_dumppins(FAR const char *msg)
+{
+  nrf52_pin_dump(PINCONFIG_LED, msg);
+  nrf52_gpio_dump(GPIO_LED, msg);
+}
+#else
+#  define led_dumppins(m)
+#endif
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: board_autoled_initialize
+ ****************************************************************************/
+
+void board_autoled_initialize(void)
+{
+  int i;
+
+  /* Configure LED pin as a GPIO outputs */
+
+  led_dumppins("board_autoled_initialize() Entry)");
+
+  for (i = 0; i < BOARD_NLEDS; i++)
+    {
+      nrf52_gpio_config(g_ledcfg[i]);
+    }
+
+  led_dumppins("board_autoled_initialize() Exit");
+}
+
+/****************************************************************************
+ * Name: board_autoled_on
+ ****************************************************************************/
+
+void board_autoled_on(int led)
+{
+  if ((unsigned)led < BOARD_NLEDS)
+    {
+      nrf52_gpio_write(g_ledcfg[led], LED_ON);
+    }
+}
+
+/****************************************************************************
+ * Name: board_autoled_off
+ ****************************************************************************/
+
+void board_autoled_off(int led)
+{
+  if ((unsigned)led < BOARD_NLEDS)
+    {
+      nrf52_gpio_write(g_ledcfg[led], LED_OFF);
+    }
+}
+
+#endif /* CONFIG_ARCH_LEDS */
