apache/nuttx

commit 10903b5b2e7ee8841718b8df016626d80e3605dd

fs/smartfs: Fixed filesystem corruption when writing to small file after open(... O_APPEND)

fs/smartfs/smartfs_smart.c
@@ -181,6 +181,9 @@ static int smartfs_open(FAR struct file *filep, const char *relpath,
   uint16_t                  parentdirsector;
   const char               *filename;
   struct smartfs_ofile_s   *sf;
+#ifdef CONFIG_SMARTFS_USE_SECTOR_BUFFER
+  struct smart_read_write_s readwrite;
+#endif
 
   /* Sanity checks */
 
@@ -317,12 +320,35 @@ static int smartfs_open(FAR struct file *filep, const char *relpath,
 
   /* Now perform the "open" on the file in direntry */
 
-  sf->oflags = oflags;
-  sf->crefs = 1;
-  sf->filepos = 0;
-  sf->curroffset = sizeof(struct smartfs_chain_header_s);
-  sf->currsector = sf->entry.firstsector;
+  sf->oflags       = oflags;
+  sf->crefs        = 1;
+  sf->filepos      = 0;
+  sf->curroffset   = sizeof(struct smartfs_chain_header_s);
+  sf->currsector   = sf->entry.firstsector;
   sf->byteswritten = 0;
+
+#ifdef CONFIG_SMARTFS_USE_SECTOR_BUFFER
+
+  /* When using sector buffering, current sector with its header should always
+   * be present in sf->buffer. Otherwise data corruption may arise when writing.
+   */
+
+  if (sf->currsector != SMARTFS_ERASEDSTATE_16BIT)
+    {
+      readwrite.logsector = sf->currsector;
+      readwrite.offset    = 0;
+      readwrite.count     = fs->fs_llformat.availbytes;
+      readwrite.buffer    = (uint8_t *) sf->buffer;
+
+      ret = FS_IOCTL(fs, BIOC_READSECT, (unsigned long) &readwrite);
+      if (ret < 0)
+        {
+          ferr("ERROR: Error %d reading sector %d header\n",
+               ret, sf->currsector);
+          goto errout_with_buffer;
+        }
+    }
+#endif
 
   /* Test if we opened for APPEND mode.  If we did, then seek to the
    * end of the file.

commit 0b91c273ea1d87387809d186194ae4766f3ff82d

fs/smartfs: Fix file size corruption when opening with overwriting mode
If a existing file is opened with overwriting mode e.g. fopen(file, "w+"),
the file size will be incorrect after writing any data to the file.
This bug is caused by previous commit 10903b5, and its changes should be
limited to only O_APPEND mode.

fs/smartfs/smartfs_smart.c
@@ -331,10 +331,11 @@ static int smartfs_open(FAR struct file *filep, const char *relpath,
 
   /* When using sector buffering, current sector with its header should
    * always be present in sf->buffer. Otherwise data corruption may arise
-   * when writing.
+   * when writing. However, this does not apply when overwriting without
+   * append mode.
    */
 
-  if (sf->currsector != SMARTFS_ERASEDSTATE_16BIT)
+  if ((sf->currsector != SMARTFS_ERASEDSTATE_16BIT) && (oflags & O_APPEND))
     {
       readwrite.logsector = sf->currsector;
       readwrite.offset    = 0;