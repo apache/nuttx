be19f4f84fba43918da408296f22399e3763187b
Masayuki Ishikawa <masayuki.ishikawa@gmail.com>
2020-07-30 09:08:55 +0900

    boards: lc823450-xgevk: Fix style violations
    Summary:
    - Fix style violations under lc823450-xgevk
    
    Impact:
    - No impact
    
    Testing:
    - Build check only
    
    Signed-off-by: Masayuki Ishikawa <Masayuki.Ishikawa@jp.sony.com>

--- boards/arm/lc823450/lc823450-xgevk/src/lc823450_boot.c
+++ boards/arm/lc823450/lc823450-xgevk/src/lc823450_boot.c
@@ -55,9 +55,9 @@
  * Description:
  *   If CONFIG_BOARD_LATE_INITIALIZE is selected, then an additional
  *   initialization call will be performed in the boot-up sequence to a
- *   function called board_late_initialize().  board_late_initialize() will be
- *   called immediately after up_intitialize() is called and just before the
- *   initial application is started.  This additional initialization phase
+ *   function called board_late_initialize().  board_late_initialize() will
+ *   be called immediately after up_intitialize() is called and just before
+ *   the initial application is started. This additional initialization phase
  *   may be used, for example, to initialize board-specific device drivers.
  *
  ****************************************************************************/
