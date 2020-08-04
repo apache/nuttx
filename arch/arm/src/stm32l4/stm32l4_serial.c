579fbe2e52e83f6bbf092d3d474b7869bbdbfc96
David Sidrane <David.Sidrane@NscDg.com>
2020-08-04 06:41:27 -0700

    stm32l4 serial:Add SINGLEWIRE PUSHPULL option

--- arch/arm/src/stm32l4/stm32l4_serial.c
+++ arch/arm/src/stm32l4/stm32l4_serial.c
@@ -1873,7 +1873,9 @@ static int stm32l4serial_ioctl(FAR struct file *filep, int cmd,
 
         if ((arg & SER_SINGLEWIRE_ENABLED) != 0)
           {
-            uint32_t gpio_val = GPIO_OPENDRAIN;
+            uint32_t gpio_val = (arg & SER_SINGLEWIRE_PUSHPULL) ==
+                                 SER_SINGLEWIRE_PUSHPULL ?
+                                 GPIO_PUSHPULL : GPIO_OPENDRAIN;
             gpio_val |=
                 (arg & SER_SINGLEWIRE_PULL_MASK) == SER_SINGLEWIRE_PULLUP ?
                   GPIO_PULLUP : GPIO_FLOAT;
