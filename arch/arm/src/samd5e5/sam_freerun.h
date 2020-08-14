c82a5d12b9ab38629e11951dd15b83a562f9b88f
leomarradke <leomar@falker.com.br>
2020-08-14 15:23:40 -0300

    arch: samd5e5 : Oneshot, freerun and tickless available support. All support runs on Timer/Counter (TC). Some fixes in external interrupt controller (EIC) and clockconfig.
    Testing:
    - Build check only.
    
    Signed-off-by: Leomar Mateus Radke  <leomar@falker.com.br>

---
+++ arch/arm/src/samd5e5/sam_freerun.h
@@ -0,0 +1,151 @@
+/****************************************************************************
+ * arch/arm/src/samd5e5/sam_freerun.h
+ *
+ *   Copyright 2020 Falker Automacao Agricola LTDA.
+ *   Author: Leomar Mateus Radke <leomar@falker.com.br>
+ *   Author: Ricardo Wartchow <wartchow@gmail.com>
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+#ifndef __ARCH_ARM_SRC_SAMD5E5_SAM_FREERUN_H
+#define __ARCH_ARM_SRC_SAMD5E5_SAM_FREERUN_H
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+#include <stdint.h>
+#include <time.h>
+
+#include "sam_tc.h"
+
+#ifdef CONFIG_SAMD5E5_FREERUN
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+#define FREERUN_INITIALIZED(s) (((s)->tch) != NULL)
+
+/****************************************************************************
+ * Public Types
+ ****************************************************************************/
+
+/* The freerun client must allocate an instance of this structure and called
+ * sam_freerun_initialize() before using the freerun facilities.  The client
+ * should not access the contents of this structure directly since the
+ * contents are subject to change.
+ */
+
+struct sam_freerun_s
+{
+  uint8_t chan;              /* The timer/counter in use */
+  bool running;              /* True: the timer is running */
+  uint32_t overflow;         /* Timer counter overflow */
+  TC_HANDLE tch;             /* Handle returned by sam_tc_initialize() */
+};
+
+/****************************************************************************
+ * Public Data
+ ****************************************************************************/
+
+#undef EXTERN
+#if defined(__cplusplus)
+#define EXTERN extern "C"
+extern "C"
+{
+#else
+#define EXTERN extern
+#endif
+
+/****************************************************************************
+ * Public Function Prototypes
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: sam_freerun_initialize
+ *
+ * Description:
+ *   Initialize the freerun timer wrapper
+ *
+ * Input Parameters:
+ *   freerun    Caller allocated instance of the freerun state structure
+ *   chan       Timer counter channel to be used.  See the TC_CHAN*
+ *              definitions in arch/arm/src/sama5/sam_tc.h.
+ *   resolution The required resolution of the timer in units of
+ *              microseconds.  NOTE that the range is restricted to the
+ *              range of uint16_t (excluding zero).
+ *
+ * Returned Value:
+ *   Zero (OK) is returned on success; a negated errno value is returned
+ *   on failure.
+ *
+ ****************************************************************************/
+
+int sam_freerun_initialize(struct sam_freerun_s *freerun, int chan,
+                           uint16_t resolution);
+
+/****************************************************************************
+ * Name: sam_freerun_counter
+ *
+ * Description:
+ *   Read the counter register of the free-running timer.
+ *
+ * Input Parameters:
+ *   freerun Caller allocated instance of the freerun state structure.  This
+ *           structure must have been previously initialized via a call to
+ *           sam_freerun_initialize();
+ *   ts      The location in which to return the time remaining on the
+ *           oneshot timer.
+ *
+ * Returned Value:
+ *   Zero (OK) is returned on success; a negated errno value is returned
+ *   on failure.
+ *
+ ****************************************************************************/
+
+int sam_freerun_counter(struct sam_freerun_s *freerun, struct timespec *ts);
+
+/****************************************************************************
+ * Name: sam_freerun_uninitialize
+ *
+ * Description:
+ *   Stop the free-running timer and release all resources that it uses.
+ *
+ * Input Parameters:
+ *   freerun Caller allocated instance of the freerun state structure.  This
+ *           structure must have been previously initialized via a call to
+ *           sam_freerun_initialize();
+ *
+ * Returned Value:
+ *   Zero (OK) is returned on success; a negated errno value is returned
+ *   on failure.
+ *
+ ****************************************************************************/
+
+int sam_freerun_uninitialize(struct sam_freerun_s *freerun);
+
+#undef EXTERN
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* CONFIG_SAMD5E5_FREERUN */
+#endif /* __ARCH_ARM_SRC_SAMD5E5_SAM_FREERUN_H */
