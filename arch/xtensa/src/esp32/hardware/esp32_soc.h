1e9ef469dc91d2a3e4f2f030a6ce37c1e25151ed
chenwen <chenwen@espressif.com>
2020-08-20 09:03:13 -0300

    xtensa/esp32: Add functions to switch CPU frequency from 80MHz to 240Mhz

--- arch/xtensa/src/esp32/hardware/esp32_soc.h
+++ arch/xtensa/src/esp32/hardware/esp32_soc.h
@@ -168,7 +168,7 @@
 
 /* Set bits of register controlled by mask */
 
-#define SET_PERI_REG_MASK(reg, mask)   WRITE_PERI_REG((reg), (READ_PERI_REG(reg)|(mask))) */
+#define SET_PERI_REG_MASK(reg, mask)   WRITE_PERI_REG((reg), (READ_PERI_REG(reg)|(mask)))
 
 /* Get bits of register controlled by mask */
 
@@ -192,6 +192,7 @@
 #define CPU_CLK_FREQ_ROM                        APB_CLK_FREQ_ROM
 #define CPU_CLK_FREQ                            APB_CLK_FREQ
 #define APB_CLK_FREQ                            80*1000000       /* Unit: Hz */
+#define REF_CLK_FREQ                            ( 1000000 )
 #define UART_CLK_FREQ                           APB_CLK_FREQ
 #define WDT_CLK_FREQ                            APB_CLK_FREQ
 #define TIMER_CLK_FREQ                          (80000000>>4)    /* 80MHz divided by 16 */
@@ -370,5 +371,160 @@
 #define ETS_UART1_INUM              5
 
 /* Other interrupt numbers should be managed by the user */
+
+#define DR_REG_APB_CTRL_BASE                    0x3ff66000    /* Old name for SYSCON, to be removed */
+#define APB_CTRL_SYSCLK_CONF_REG          (DR_REG_APB_CTRL_BASE + 0x0)
+#define APB_CTRL_XTAL_TICK_CONF_REG          (DR_REG_APB_CTRL_BASE + 0x4)
+
+/* APB_CTRL_PRE_DIV_CNT : R/W ;bitpos:[9:0] ;default: 10'h0 ; */
+
+#define APB_CTRL_PRE_DIV_CNT  0x000003FF
+#define APB_CTRL_PRE_DIV_CNT_M  ((APB_CTRL_PRE_DIV_CNT_V)<<(APB_CTRL_PRE_DIV_CNT_S))
+#define APB_CTRL_PRE_DIV_CNT_V  0x3FF
+#define APB_CTRL_PRE_DIV_CNT_S  0
+
+#define I2C_BBPLL_IR_CAL_DELAY         0
+#define I2C_BBPLL_IR_CAL_EXT_CAP       1
+#define I2C_BBPLL_OC_ENB_FCAL          4
+#define I2C_BBPLL_OC_ENB_VCON          10
+#define I2C_BBPLL_BBADC_CAL_7_0        12
+
+#define I2C_BBPLL_OC_LREF        2
+#define I2C_BBPLL_OC_LREF_MSB    7
+#define I2C_BBPLL_OC_LREF_LSB    7
+
+#define I2C_BBPLL_OC_DIV_7_0        3
+#define I2C_BBPLL_OC_DIV_7_0_MSB    7
+#define I2C_BBPLL_OC_DIV_7_0_LSB    0
+
+#define I2C_BBPLL_BBADC_DSMP        9
+#define I2C_BBPLL_BBADC_DSMP_MSB    7
+#define I2C_BBPLL_BBADC_DSMP_LSB    4
+
+#define I2C_BBPLL_OC_DCUR        5
+#define I2C_BBPLL_OC_DCUR_MSB    2
+#define I2C_BBPLL_OC_DCUR_LSB    0
+
+#define I2C_BBPLL_ENDIV5        11
+
+#define I2C_BBPLL           0x66
+#define I2C_BBPLL_HOSTID    4
+
+#define I2C_WRITEREG_RTC(block, reg_add, indata) \
+      rom_i2c_writeReg(block, block##_HOSTID,  reg_add, indata)
+
+/* BBPLL configuration values */
+
+#define BBPLL_ENDIV5_VAL_320M       0x43
+#define BBPLL_BBADC_DSMP_VAL_320M   0x84
+#define BBPLL_ENDIV5_VAL_480M       0xc3
+#define BBPLL_BBADC_DSMP_VAL_480M   0x74
+
+#define BBPLL_IR_CAL_DELAY_VAL      0x18
+#define BBPLL_IR_CAL_EXT_CAP_VAL    0x20
+#define BBPLL_OC_ENB_FCAL_VAL       0x9a
+#define BBPLL_OC_ENB_VCON_VAL       0x00
+#define BBPLL_BBADC_CAL_7_0_VAL     0x00
+
+#define EFUSE_BLK0_RDATA5_REG          (DR_REG_EFUSE_BASE + 0x014)
+#define EFUSE_RD_VOL_LEVEL_HP_INV    0x03
+#define EFUSE_RD_VOL_LEVEL_HP_INV_S  22
+
+#define REG_TIMG_BASE(i)       (DR_REG_TIMERGROUP0_BASE + i*0x1000)
+#define TIMG_RTCCALICFG_REG(i)          (REG_TIMG_BASE(i) + 0x0068)
+
+#define RTC_CNTL_OPTIONS0_REG          (DR_REG_RTCCNTL_BASE + 0x0)
+#define RTC_CNTL_STORE5_REG          (DR_REG_RTCCNTL_BASE + 0xb4)
+
+#define RTC_APB_FREQ_REG        RTC_CNTL_STORE5_REG
+#define RTC_CNTL_REG          (DR_REG_RTCCNTL_BASE + 0x7c)
+
+#define RTC_CNTL_CLK_CONF_REG          (DR_REG_RTCCNTL_BASE + 0x70)
+
+#define RTC_CNTL_ANA_CONF_REG          (DR_REG_RTCCNTL_BASE + 0x30)
+
+#define RTC_CNTL_STORE4_REG          (DR_REG_RTCCNTL_BASE + 0xb0)
+#define RTC_XTAL_FREQ_REG       RTC_CNTL_STORE4_REG
+
+/* Approximate mapping of voltages to RTC_CNTL_DBIAS_WAK, RTC_CNTL_DBIAS_SLP,
+ * RTC_CNTL_DIG_DBIAS_WAK, RTC_CNTL_DIG_DBIAS_SLP values.
+ * Valid if RTC_CNTL_DBG_ATTEN is 0.
+ */
+
+#define RTC_CNTL_DBIAS_1V00 2
+#define RTC_CNTL_DBIAS_1V10 4
+#define RTC_CNTL_DBIAS_1V25 7
+
+/* RTC_CNTL_DIG_DBIAS_WAK : R/W ;bitpos:[13:11] ;default: 3'd4 ; */
+
+#define RTC_CNTL_DIG_DBIAS_WAK  0x00000007
+#define RTC_CNTL_DIG_DBIAS_WAK_S  11
+
+/* RTC_CNTL_SOC_CLK_SEL : R/W ;bitpos:[28:27] ;default: 2'd0 ;
+ * description: SOC clock sel. 0: XTAL  1: PLL  2: CK8M  3: APLL
+ */
+
+#define RTC_CNTL_SOC_CLK_SEL  0x00000003
+#define RTC_CNTL_SOC_CLK_SEL_S  27
+#define RTC_CNTL_SOC_CLK_SEL_XTL    0
+#define RTC_CNTL_SOC_CLK_SEL_PLL    1
+#define RTC_CNTL_SOC_CLK_SEL_8M     2
+#define RTC_CNTL_SOC_CLK_SEL_APLL   3
+
+/* Core voltage needs to be increased in two cases:
+ * 1. running at 240 MHz
+ * 2. running with 80MHz Flash frequency
+ * There is a record in efuse which indicates the
+ * proper voltage for these two cases.
+ */
+
+#define RTC_CNTL_DBIAS_HP_VOLT         (RTC_CNTL_DBIAS_1V25 - (REG_GET_FIELD(EFUSE_BLK0_RDATA5_REG, EFUSE_RD_VOL_LEVEL_HP_INV)))
+#ifdef CONFIG_ESPTOOLPY_FLASHFREQ_80M
+#define DIG_DBIAS_80M_160M  RTC_CNTL_DBIAS_HP_VOLT
+#else
+#define DIG_DBIAS_80M_160M  RTC_CNTL_DBIAS_1V10
+#endif
+#define DIG_DBIAS_240M      RTC_CNTL_DBIAS_HP_VOLT
+#define DIG_DBIAS_XTAL      RTC_CNTL_DBIAS_1V10
+#define DIG_DBIAS_2M        RTC_CNTL_DBIAS_1V00
+
+#define DELAY_PLL_DBIAS_RAISE           3
+#define DELAY_PLL_ENABLE_WITH_150K      80
+#define DELAY_PLL_ENABLE_WITH_32K       160
+
+/* RTC_CNTL_BB_I2C_FORCE_PD : R/W ;bitpos:[6] ;default: 1'b0 ;
+ * description: BB_I2C force power down
+ */
+
+#define RTC_CNTL_BB_I2C_FORCE_PD  (BIT(6))
+
+/* RTC_CNTL_BBPLL_FORCE_PD : R/W ;bitpos:[10] ;default: 1'b0 ;
+ * description: BB_PLL force power down
+ */
+
+#define RTC_CNTL_BBPLL_FORCE_PD  (BIT(10))
+
+/* RTC_CNTL_BBPLL_I2C_FORCE_PD : R/W ;bitpos:[8] ;default: 1'b0 ;
+ * description: BB_PLL _I2C force power down
+ */
+
+#define RTC_CNTL_BBPLL_I2C_FORCE_PD  (BIT(8))
+
+/* RTC_CNTL_PLLA_FORCE_PD : R/W ;bitpos:[23] ;default: 1'b1 ;
+ * description: PLLA force power down
+ */
+
+#define RTC_CNTL_PLLA_FORCE_PD  (BIT(23))
+#define RTC_CNTL_PLLA_FORCE_PD_S  23
+
+/* RTC_CNTL_BIAS_I2C_FORCE_PD : R/W ;bitpos:[18] ;default: 1'b0 ;
+ * description: BIAS_I2C force power down
+ */
+
+#define RTC_CNTL_BIAS_I2C_FORCE_PD  (BIT(18))
+
+#define MHZ (1000000)
+#define RTC_PLL_FREQ_320M   320
+#define RTC_PLL_FREQ_480M   480
 
 #endif /* __ARCH_XTENSA_SRC_ESP32_HARDWARE_ESP32_SOC_H */
