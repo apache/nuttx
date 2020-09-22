apache/nuttx

commit 34b34e2d4519956f9a61f5985aca29947c4b8b23

Fix: ensure archive files do not carry object files from prior builds
In some cases, when NuttX configuration changes and this makes the
object list used to build one of the .a libraries change as well,
since the command used to build it is "ar crs" and this simply appends
the list of object files, the library could still include object
files from prior builds. This commit modifies the ARCHIVE macro to
erase the .a file if it already exists.

Since in some cases this behavior was actually expected (object
files from a subdirectory were appended to a library created one
level above) I added a ARCHIVE_ADD which works as ARCHIVE did.

This change should greatly improve behavior of building after
configuration changes.

tools/Config.mk
@@ -300,8 +300,8 @@ define INSTALL_LIB
 	$(Q) install -m 0644 $1 $2
 endef
 
-# ARCHIVE - Add a list of files to an archive
-# Example: $(call ARCHIVE, archive-file, "file1 file2 file3 ...")
+# ARCHIVE_ADD - Add a list of files to an archive
+# Example: $(call ARCHIVE_ADD, archive-file, "file1 file2 file3 ...")
 #
 # Note: The fileN strings may not contain spaces or  characters that may be
 # interpreted strangely by the shell
@@ -316,8 +316,18 @@ endef
 #
 #   CONFIG_WINDOWS_NATIVE - Defined for a Windows native build
 
+define ARCHIVE_ADD
+	@echo "AR (add): $(notdir $1) $(2)"
+	$(Q) $(AR) $1 $(2)
+endef
+
+# ARCHIVE - Same as above, but ensure the archive is
+# created from scratch
+
 define ARCHIVE
-	$(AR) $1 $(2)
+	@echo "AR (create): $(notdir $1) $(2)"
+	$(Q) $(RM) $1
+	$(Q) $(AR) $1 $(2)
 endef
 
 # PRELINK - Prelink a list of files

boards/Board.mk
@@ -105,12 +105,7 @@ $(CXXOBJS) $(LINKOBJS): %$(OBJEXT): %.cxx
 	$(call COMPILEXX, $<, $@)
 
 libboard$(LIBEXT): $(OBJS) $(CXXOBJS)
-ifneq ($(OBJS),)
-	$(call ARCHIVE, $@, $(OBJS))
-endif
-ifneq ($(CXXOBJS),)
-	$(call ARCHIVE, $@, $(CXXOBJS))
-endif
+	$(call ARCHIVE, $@, $(OBJS) $(CXXOBJS))
 
 .depend: Makefile $(SRCS) $(CXXSRCS) $(RCSRCS) $(TOPDIR)$(DELIM).config
 ifneq ($(ZDSVERSION),)

libs/libc/zoneinfo/Makefile
@@ -61,7 +61,7 @@ $(COBJS): %$(OBJEXT): %.c
 	$(call COMPILE, $<, $@)
 
 .built: .tzbuilt romfs $(OBJS)
-	$(call ARCHIVE, ..$(DELIM)$(BIN), $(OBJS))
+	$(call ARCHIVE_ADD, ..$(DELIM)$(BIN), $(OBJS))
 	$(Q) touch .built
 
 # ROMFS file system containing the TZ database

commit 06dc511573fd62ba5e9099faf0d38a44c2717201

Fix Cygwin build broken by commit 34b34e2d451
Commit 34b34e2d451 uses the full path to libapps.a and introduced the use of the Make notdir command.  That command breaks the Cygwin build because when a native Windows toolchain is used, the full path to libapps.a is a Windows-sytle path and the Make notdir command (like most other GNU Make commands) fails if it is passed a Windows-style path.  This commit replaces the Make notdir command with the Bash basename command which can handle Windows paths.

tools/Config.mk
@@ -317,7 +317,7 @@ endef
 #   CONFIG_WINDOWS_NATIVE - Defined for a Windows native build
 
 define ARCHIVE_ADD
-	@echo "AR (add): $(notdir $1) $(2)"
+	@echo "AR (add): ${shell basename $(1)} $(2)"
 	$(Q) $(AR) $1 $(2)
 endef
 
@@ -325,7 +325,7 @@ endef
 # created from scratch
 
 define ARCHIVE
-	@echo "AR (create): $(notdir $1) $(2)"
+	@echo "AR (create): ${shell basename $(1)} $(2)"
 	$(Q) $(RM) $1
 	$(Q) $(AR) $1 $(2)
 endef