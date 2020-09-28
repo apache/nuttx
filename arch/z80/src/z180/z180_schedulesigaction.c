971c5d41fad3197df6769e5d41fbca0c6565aa92
Yoshinori Sugino <ysgn0101@gmail.com>
2020-09-28 10:18:26 +0900

    Fix typos

--- arch/z80/src/z180/z180_schedulesigaction.c
+++ arch/z80/src/z180/z180_schedulesigaction.c
@@ -89,7 +89,7 @@ static void z180_sigsetup(FAR struct tcb_s *tcb, sig_deliver_t sigdeliver,
  *   This function is called by the OS when one or more
  *   signal handling actions have been queued for execution.
  *   The architecture specific code must configure things so
- *   that the 'igdeliver' callback is executed on the thread
+ *   that the 'sigdeliver' callback is executed on the thread
  *   specified by 'tcb' as soon as possible.
  *
  *   This function may be called from interrupt handling logic.
