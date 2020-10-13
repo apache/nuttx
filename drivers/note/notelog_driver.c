0941bad877d1e4a5513eb067c51ca675b628c229
Xiang Xiao <xiaoxiang@xiaomi.com>
2020-10-13 20:19:28 +0800

    note: Move up_schednote.c to drivers/note folder
    since it is general enough to work on other target
    
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- drivers/note/notelog_driver.c
+++ drivers/note/notelog_driver.c
@@ -1,5 +1,5 @@
 /****************************************************************************
- * arch/sim/src/sim/up_schednote.c
+ * drivers/note/notelog_driver.c
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
