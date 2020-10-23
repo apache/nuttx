625eef20f00fbe9bdb4995ad9dfb24b71227e954
Xiang Xiao <xiaoxiang@xiaomi.com>
2020-10-23 11:47:51 +0800

    arch: Remove the special check for idle thread in up_use_stack
    since the idle thread don't call up_use_stack anymore
    
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- arch/hc/src/common/up_usestack.c
+++ arch/hc/src/common/up_usestack.c
@@ -110,10 +110,7 @@ int up_use_stack(struct tcb_s *tcb, void *stack, size_t stack_size)
    */
 
 #ifdef CONFIG_STACK_COLORATION
-  if (tcb->pid != 0)
-    {
-      memset(tcb->stack_alloc_ptr, 0xaa, stack_size);
-    }
+  memset(tcb->stack_alloc_ptr, 0xaa, stack_size);
 #endif
 
   /* The CPU12 uses a push-down stack: the stack grows
--- arch/x86/src/i486/up_usestack.c
+++ arch/x86/src/i486/up_usestack.c
@@ -111,10 +111,7 @@ int up_use_stack(struct tcb_s *tcb, void *stack, size_t stack_size)
    */
 
 #ifdef CONFIG_STACK_COLORATION
-  if (tcb->pid != 0)
-    {
-      memset(tcb->stack_alloc_ptr, 0xaa, stack_size);
-    }
+  memset(tcb->stack_alloc_ptr, 0xaa, stack_size);
 #endif
 
   /* The i486 uses a push-down stack:  the stack grows toward loweraddresses
