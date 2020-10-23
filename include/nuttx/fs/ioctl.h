2ed6e132410b2983f321cce5559d5dadb903685e
dongjiuzhu <dongjiuzhu1@xiaomi.com>
2020-10-23 16:36:11 +0800

    driver/rc: support IR remote control
    N/A
    
    guide link: https://www.kernel.org/doc/html/v4.14/media/uapi/rc/lirc-dev.html
    
    Change-Id: I84abfc12578d444d72c8c0df31d06d1a85ce0725
    Signed-off-by: dongjiuzhu <dongjiuzhu1@xiaomi.com>

--- include/nuttx/fs/ioctl.h
+++ include/nuttx/fs/ioctl.h
@@ -99,6 +99,7 @@
 #define _RPTUNBASE      (0x2b00) /* Remote processor tunnel ioctl commands */
 #define _NOTECTLBASE    (0x2c00) /* Note filter control ioctl commands*/
 #define _NOTERAMBASE    (0x2d00) /* Noteram device ioctl commands*/
+#define _RCIOCBASE      (0x2e00) /* Remote Control device ioctl commands */
 #define _WLIOCBASE      (0x8b00) /* Wireless modules ioctl network commands */
 
 /* boardctl() commands share the same number space */
@@ -538,6 +539,11 @@
 
 #define _NOTERAMIOCVALID(c) (_IOC_TYPE(c) == _NOTERAMBASE)
 #define _NOTERAMIOC(nr)     _IOC(_NOTERAMBASE, nr)
+
+/* Remote Control drivers ***************************************************/
+
+#define _RCIOCVALID(c)    (_IOC_TYPE(c)==_RCIOCBASE)
+#define _RCIOC(nr)        _IOC(_RCIOCBASE,nr)
 
 /* Wireless driver network ioctl definitions ********************************/
 
