199c0f2843efde893258f197bac47cf150cd1b6a
Matias N <matias@protobits.dev>
2020-10-28 23:51:33 -0300

    bt_conn.c: use definition names instead of hardcoded numbers

--- wireless/bluetooth/bt_conn.c
+++ wireless/bluetooth/bt_conn.c
@@ -298,7 +298,7 @@ void bt_conn_receive(FAR struct bt_conn_s *conn, FAR struct bt_buf_s *buf,
 
   switch (flags)
     {
-      case 0x02:
+      case BT_HCI_ACL_NEW:
 
         /* First packet */
 
@@ -323,7 +323,7 @@ void bt_conn_receive(FAR struct bt_conn_s *conn, FAR struct bt_buf_s *buf,
 
         break;
 
-      case 0x01:
+      case BT_HCI_ACL_CONTINUATION:
 
         /* Continuation */
 
