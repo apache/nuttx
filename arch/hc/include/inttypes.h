49008b586248726c5211a77f5c17535348aac2ec
YAMAMOTO Takashi <yamamoto@midokura.com>
2020-11-06 08:22:10 +0900

    hc inttypes.h: Remove PRI/SCN macros for fast and least types

--- arch/hc/include/inttypes.h
+++ arch/hc/include/inttypes.h
@@ -51,32 +51,12 @@
 #  define PRId32      "ld"
 #  define PRId64      "lld"
 
-#  define PRIdLEAST8  "d"
-#  define PRIdLEAST16 "d"
-#  define PRIdLEAST32 "ld"
-#  define PRIdLEAST64 "lld"
-
-#  define PRIdFAST8   "d"
-#  define PRIdFAST16  "d"
-#  define PRIdFAST32  "ld"
-#  define PRIdFAST64  "lld"
-
 #  define PRIdPTR     "d"
 
 #  define PRIi8       "i"
 #  define PRIi16      "i"
 #  define PRIi32      "li"
 #  define PRIi64      "lli"
-
-#  define PRIiLEAST8  "i"
-#  define PRIiLEAST16 "i"
-#  define PRIiLEAST32 "li"
-#  define PRIiLEAST64 "lli"
-
-#  define PRIiFAST8   "i"
-#  define PRIiFAST16  "i"
-#  define PRIiFAST32  "li"
-#  define PRIiFAST64  "lli"
 
 #  define PRIiPTR     "i"
 
@@ -85,32 +65,12 @@
 #  define PRIo32      "lo"
 #  define PRIo64      "llo"
 
-#  define PRIoLEAST8  "o"
-#  define PRIoLEAST16 "o"
-#  define PRIoLEAST32 "lo"
-#  define PRIoLEAST64 "llo"
-
-#  define PRIoFAST8   "o"
-#  define PRIoFAST16  "o"
-#  define PRIoFAST32  "lo"
-#  define PRIoFAST64  "llo"
-
 #  define PRIoPTR     "o"
 
 #  define PRIu8       "u"
 #  define PRIu16      "u"
 #  define PRIu32      "lu"
 #  define PRIu64      "llu"
-
-#  define PRIuLEAST8  "u"
-#  define PRIuLEAST16 "u"
-#  define PRIuLEAST32 "lu"
-#  define PRIuLEAST64 "llu"
-
-#  define PRIuFAST8   "u"
-#  define PRIuFAST16  "u"
-#  define PRIuFAST32  "lu"
-#  define PRIuFAST64  "llu"
 
 #  define PRIuPTR     "u"
 
@@ -119,32 +79,12 @@
 #  define PRIx32      "lx"
 #  define PRIx64      "llx"
 
-#  define PRIxLEAST8  "x"
-#  define PRIxLEAST16 "x"
-#  define PRIxLEAST32 "lx"
-#  define PRIxLEAST64 "llx"
-
-#  define PRIxFAST8   "x"
-#  define PRIxFAST16  "x"
-#  define PRIxFAST32  "lx"
-#  define PRIxFAST64  "llx"
-
 #  define PRIxPTR     "x"
 
 #  define PRIX8       "X"
 #  define PRIX16      "X"
 #  define PRIX32      "lX"
 #  define PRIX64      "llX"
-
-#  define PRIXLEAST8  "X"
-#  define PRIXLEAST16 "X"
-#  define PRIXLEAST32 "lX"
-#  define PRIXLEAST64 "llX"
-
-#  define PRIXFAST8   "X"
-#  define PRIXFAST16  "X"
-#  define PRIXFAST32  "lX"
-#  define PRIXFAST64  "llX"
 
 #  define PRIXPTR     "X"
 
@@ -153,32 +93,12 @@
 #  define SCNd32      "ld"
 #  define SCNd64      "lld"
 
-#  define SCNdLEAST8  "hhd"
-#  define SCNdLEAST16 "d"
-#  define SCNdLEAST32 "ld"
-#  define SCNdLEAST64 "lld"
-
-#  define SCNdFAST8   "hhd"
-#  define SCNdFAST16  "d"
-#  define SCNdFAST32  "ld"
-#  define SCNdFAST64  "lld"
-
 #  define SCNdPTR     "d"
 
 #  define SCNi8       "hhi"
 #  define SCNi16      "i"
 #  define SCNi32      "li"
 #  define SCNi64      "lli"
-
-#  define SCNiLEAST8  "hhi"
-#  define SCNiLEAST16 "i"
-#  define SCNiLEAST32 "li"
-#  define SCNiLEAST64 "lli"
-
-#  define SCNiFAST8   "hhi"
-#  define SCNiFAST16  "i"
-#  define SCNiFAST32  "li"
-#  define SCNiFAST64  "lli"
 
 #  define SCNiPTR     "i"
 
@@ -187,16 +107,6 @@
 #  define SCNo32      "lo"
 #  define SCNo64      "llo"
 
-#  define SCNoLEAST8  "hho"
-#  define SCNoLEAST16 "o"
-#  define SCNoLEAST32 "lo"
-#  define SCNoLEAST64 "llo"
-
-#  define SCNoFAST8   "hho"
-#  define SCNoFAST16  "o"
-#  define SCNoFAST32  "lo"
-#  define SCNoFAST64  "llo"
-
 #  define SCNoPTR     "o"
 
 #  define SCNu8       "hhu"
@@ -204,32 +114,12 @@
 #  define SCNu32      "lu"
 #  define SCNu64      "llu"
 
-#  define SCNuLEAST8  "hhu"
-#  define SCNuLEAST16 "u"
-#  define SCNuLEAST32 "lu"
-#  define SCNuLEAST64 "llu"
-
-#  define SCNuFAST8   "hhu"
-#  define SCNuFAST16  "u"
-#  define SCNuFAST32  "lu"
-#  define SCNuFAST64  "llu"
-
 #  define SCNuPTR     "u"
 
 #  define SCNx8       "hhx"
 #  define SCNx16      "x"
 #  define SCNx32      "lx"
 #  define SCNx64      "llx"
-
-#  define SCNxLEAST8  "hhx"
-#  define SCNxLEAST16 "x"
-#  define SCNxLEAST32 "lx"
-#  define SCNxLEAST64 "llx"
-
-#  define SCNxFAST8   "hhx"
-#  define SCNxFAST16  "x"
-#  define SCNxFAST32  "lx"
-#  define SCNxFAST64  "llx"
 
 #  define SCNxPTR     "x"
 
@@ -250,32 +140,12 @@
 #  define PRId32      "d"
 #  define PRId64      "lld"
 
-#  define PRIdLEAST8  "d"
-#  define PRIdLEAST16 "d"
-#  define PRIdLEAST32 "d"
-#  define PRIdLEAST64 "lld"
-
-#  define PRIdFAST8   "d"
-#  define PRIdFAST16  "d"
-#  define PRIdFAST32  "d"
-#  define PRIdFAST64  "lld"
-
 #  define PRIdPTR     "d"
 
 #  define PRIi8       "i"
 #  define PRIi16      "i"
 #  define PRIi32      "i"
 #  define PRIi64      "lli"
-
-#  define PRIiLEAST8  "i"
-#  define PRIiLEAST16 "i"
-#  define PRIiLEAST32 "i"
-#  define PRIiLEAST64 "lli"
-
-#  define PRIiFAST8   "i"
-#  define PRIiFAST16  "i"
-#  define PRIiFAST32  "i"
-#  define PRIiFAST64  "lli"
 
 #  define PRIiPTR     "i"
 
@@ -284,32 +154,12 @@
 #  define PRIo32      "o"
 #  define PRIo64      "llo"
 
-#  define PRIoLEAST8  "o"
-#  define PRIoLEAST16 "o"
-#  define PRIoLEAST32 "o"
-#  define PRIoLEAST64 "llo"
-
-#  define PRIoFAST8   "o"
-#  define PRIoFAST16  "o"
-#  define PRIoFAST32  "o"
-#  define PRIoFAST64  "llo"
-
 #  define PRIoPTR     "o"
 
 #  define PRIu8       "u"
 #  define PRIu16      "u"
 #  define PRIu32      "u"
 #  define PRIu64      "llu"
-
-#  define PRIuLEAST8  "u"
-#  define PRIuLEAST16 "u"
-#  define PRIuLEAST32 "u"
-#  define PRIuLEAST64 "llu"
-
-#  define PRIuFAST8   "u"
-#  define PRIuFAST16  "u"
-#  define PRIuFAST32  "u"
-#  define PRIuFAST64  "llu"
 
 #  define PRIuPTR     "u"
 
@@ -318,32 +168,12 @@
 #  define PRIx32      "x"
 #  define PRIx64      "llx"
 
-#  define PRIxLEAST8  "x"
-#  define PRIxLEAST16 "x"
-#  define PRIxLEAST32 "x"
-#  define PRIxLEAST64 "llx"
-
-#  define PRIxFAST8   "x"
-#  define PRIxFAST16  "x"
-#  define PRIxFAST32  "x"
-#  define PRIxFAST64  "llx"
-
 #  define PRIxPTR     "x"
 
 #  define PRIX8       "X"
 #  define PRIX16      "X"
 #  define PRIX32      "X"
 #  define PRIX64      "llX"
-
-#  define PRIXLEAST8  "X"
-#  define PRIXLEAST16 "X"
-#  define PRIXLEAST32 "X"
-#  define PRIXLEAST64 "llX"
-
-#  define PRIXFAST8   "X"
-#  define PRIXFAST16  "X"
-#  define PRIXFAST32  "X"
-#  define PRIXFAST64  "llX"
 
 #  define PRIXPTR     "X"
 
@@ -352,32 +182,12 @@
 #  define SCNd32      "d"
 #  define SCNd64      "lld"
 
-#  define SCNdLEAST8  "hhd"
-#  define SCNdLEAST16 "hd"
-#  define SCNdLEAST32 "d"
-#  define SCNdLEAST64 "lld"
-
-#  define SCNdFAST8   "hhd"
-#  define SCNdFAST16  "hd"
-#  define SCNdFAST32  "d"
-#  define SCNdFAST64  "lld"
-
 #  define SCNdPTR     "d"
 
 #  define SCNi8       "hhi"
 #  define SCNi16      "hi"
 #  define SCNi32      "i"
 #  define SCNi64      "lli"
-
-#  define SCNiLEAST8  "hhi"
-#  define SCNiLEAST16 "hi"
-#  define SCNiLEAST32 "i"
-#  define SCNiLEAST64 "lli"
-
-#  define SCNiFAST8   "hhi"
-#  define SCNiFAST16  "hi"
-#  define SCNiFAST32  "i"
-#  define SCNiFAST64  "lli"
 
 #  define SCNiPTR     "i"
 
@@ -386,16 +196,6 @@
 #  define SCNo32      "o"
 #  define SCNo64      "llo"
 
-#  define SCNoLEAST8  "hho"
-#  define SCNoLEAST16 "ho"
-#  define SCNoLEAST32 "o"
-#  define SCNoLEAST64 "llo"
-
-#  define SCNoFAST8   "hho"
-#  define SCNoFAST16  "ho"
-#  define SCNoFAST32  "o"
-#  define SCNoFAST64  "llo"
-
 #  define SCNoPTR     "o"
 
 #  define SCNu8       "hhu"
@@ -403,32 +203,12 @@
 #  define SCNu32      "u"
 #  define SCNu64      "llu"
 
-#  define SCNuLEAST8  "hhu"
-#  define SCNuLEAST16 "hu"
-#  define SCNuLEAST32 "u"
-#  define SCNuLEAST64 "llu"
-
-#  define SCNuFAST8   "hhu"
-#  define SCNuFAST16  "hu"
-#  define SCNuFAST32  "u"
-#  define SCNuFAST64  "llu"
-
 #  define SCNuPTR     "u"
 
 #  define SCNx8       "hhx"
 #  define SCNx16      "hx"
 #  define SCNx32      "x"
 #  define SCNx64      "llx"
-
-#  define SCNxLEAST8  "hhx"
-#  define SCNxLEAST16 "hx"
-#  define SCNxLEAST32 "x"
-#  define SCNxLEAST64 "llx"
-
-#  define SCNxFAST8   "hhx"
-#  define SCNxFAST16  "hx"
-#  define SCNxFAST32  "x"
-#  define SCNxFAST64  "llx"
 
 #  define SCNxPTR     "x"
 
