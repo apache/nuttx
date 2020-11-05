cac90d77b71810b78c6149b6f3d735d1f1663797
YAMAMOTO Takashi <yamamoto@midokura.com>
2020-11-06 08:41:19 +0900

    z80 inttypes.h: Remove PRI/SCN macros for fast and least types

--- arch/z80/include/z80/inttypes.h
+++ arch/z80/include/z80/inttypes.h
@@ -48,27 +48,11 @@
 #define PRId16      "d"
 #define PRId32      "ld"
 
-#define PRIdLEAST8  "d"
-#define PRIdLEAST16 "d"
-#define PRIdLEAST32 "ld"
-
-#define PRIdFAST8   "d"
-#define PRIdFAST16  "d"
-#define PRIdFAST32  "ld"
-
 #define PRIdPTR     "d"
 
 #define PRIi8       "i"
 #define PRIi16      "i"
 #define PRIi32      "li"
-
-#define PRIiLEAST8  "i"
-#define PRIiLEAST16 "i"
-#define PRIiLEAST32 "li"
-
-#define PRIiFAST8   "i"
-#define PRIiFAST16  "i"
-#define PRIiFAST32  "li"
 
 #define PRIiPTR     "i"
 
@@ -76,27 +60,11 @@
 #define PRIo16      "o"
 #define PRIo32      "lo"
 
-#define PRIoLEAST8  "o"
-#define PRIoLEAST16 "o"
-#define PRIoLEAST32 "lo"
-
-#define PRIoFAST8   "o"
-#define PRIoFAST16  "o"
-#define PRIoFAST32  "lo"
-
 #define PRIoPTR     "o"
 
 #define PRIu8       "u"
 #define PRIu16      "u"
 #define PRIu32      "lu"
-
-#define PRIuLEAST8  "u"
-#define PRIuLEAST16 "u"
-#define PRIuLEAST32 "lu"
-
-#define PRIuFAST8   "u"
-#define PRIuFAST16  "u"
-#define PRIuFAST32  "lu"
 
 #define PRIuPTR     "u"
 
@@ -104,27 +72,11 @@
 #define PRIx16      "x"
 #define PRIx32      "lx"
 
-#define PRIxLEAST8  "x"
-#define PRIxLEAST16 "x"
-#define PRIxLEAST32 "lx"
-
-#define PRIxFAST8   "x"
-#define PRIxFAST16  "x"
-#define PRIxFAST32  "lx"
-
 #define PRIxPTR     "x"
 
 #define PRIX8       "X"
 #define PRIX16      "X"
 #define PRIX32      "lX"
-
-#define PRIXLEAST8  "X"
-#define PRIXLEAST16 "X"
-#define PRIXLEAST32 "lX"
-
-#define PRIXFAST8   "X"
-#define PRIXFAST16  "X"
-#define PRIXFAST32  "lX"
 
 #define PRIXPTR     "X"
 
@@ -132,27 +84,11 @@
 #define SCNd16      "d"
 #define SCNd32      "ld"
 
-#define SCNdLEAST8  "hhd"
-#define SCNdLEAST16 "d"
-#define SCNdLEAST32 "ld"
-
-#define SCNdFAST8   "hhd"
-#define SCNdFAST16  "d"
-#define SCNdFAST32  "ld"
-
 #define SCNdPTR     "d"
 
 #define SCNi8       "hhi"
 #define SCNi16      "i"
 #define SCNi32      "li"
-
-#define SCNiLEAST8  "hhi"
-#define SCNiLEAST16 "i"
-#define SCNiLEAST32 "li"
-
-#define SCNiFAST8   "hhi"
-#define SCNiFAST16  "i"
-#define SCNiFAST32  "li"
 
 #define SCNiPTR     "i"
 
@@ -160,41 +96,17 @@
 #define SCNo16      "o"
 #define SCNo32      "lo"
 
-#define SCNoLEAST8  "hho"
-#define SCNoLEAST16 "o"
-#define SCNoLEAST32 "lo"
-
-#define SCNoFAST8   "hho"
-#define SCNoFAST16  "o"
-#define SCNoFAST32  "lo"
-
 #define SCNoPTR     "o"
 
 #define SCNu8       "hhu"
 #define SCNu16      "u"
 #define SCNu32      "lu"
 
-#define SCNuLEAST8  "hhu"
-#define SCNuLEAST16 "u"
-#define SCNuLEAST32 "lu"
-
-#define SCNuFAST8   "hhu"
-#define SCNuFAST16  "u"
-#define SCNuFAST32  "lu"
-
 #define SCNuPTR     "u"
 
 #define SCNx8       "hhx"
 #define SCNx16      "x"
 #define SCNx32      "lx"
-
-#define SCNxLEAST8  "hhx"
-#define SCNxLEAST16 "x"
-#define SCNxLEAST32 "lx"
-
-#define SCNxFAST8   "hhx"
-#define SCNxFAST16  "x"
-#define SCNxFAST32  "lx"
 
 #define SCNxPTR     "x"
 
