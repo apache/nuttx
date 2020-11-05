577d72258af400755577c6a20a04a16a282358dc
YAMAMOTO Takashi <yamamoto@midokura.com>
2020-11-06 08:13:16 +0900

    sim inttypes.h: Remove PRI/SCN macros for fast and least types

--- arch/sim/include/inttypes.h
+++ arch/sim/include/inttypes.h
@@ -49,60 +49,20 @@
 #  define PRId32      "d"
 #  define PRId64      "lld"
 
-#  define PRIdLEAST8  "d"
-#  define PRIdLEAST16 "d"
-#  define PRIdLEAST32 "d"
-#  define PRIdLEAST64 "lld"
-
-#  define PRIdFAST8   "d"
-#  define PRIdFAST16  "d"
-#  define PRIdFAST32  "d"
-#  define PRIdFAST64  "lld"
-
 #  define PRIi8       "i"
 #  define PRIi16      "i"
 #  define PRIi32      "i"
 #  define PRIi64      "lli"
-
-#  define PRIiLEAST8  "i"
-#  define PRIiLEAST16 "i"
-#  define PRIiLEAST32 "i"
-#  define PRIiLEAST64 "lli"
-
-#  define PRIiFAST8   "i"
-#  define PRIiFAST16  "i"
-#  define PRIiFAST32  "i"
-#  define PRIiFAST64  "lli"
 
 #  define PRIo8       "o"
 #  define PRIo16      "o"
 #  define PRIo32      "o"
 #  define PRIo64      "llo"
 
-#  define PRIoLEAST8  "o"
-#  define PRIoLEAST16 "o"
-#  define PRIoLEAST32 "o"
-#  define PRIoLEAST64 "llo"
-
-#  define PRIoFAST8   "o"
-#  define PRIoFAST16  "o"
-#  define PRIoFAST32  "o"
-#  define PRIoFAST64  "llo"
-
 #  define PRIu8       "u"
 #  define PRIu16      "u"
 #  define PRIu32      "u"
 #  define PRIu64      "llu"
-
-#  define PRIuLEAST8  "u"
-#  define PRIuLEAST16 "u"
-#  define PRIuLEAST32 "u"
-#  define PRIuLEAST64 "llu"
-
-#  define PRIuFAST8   "u"
-#  define PRIuFAST16  "u"
-#  define PRIuFAST32  "u"
-#  define PRIuFAST64  "llu"
 
 #  define PRIuMAX     "llu"
 
@@ -111,45 +71,15 @@
 #  define PRIx32      "x"
 #  define PRIx64      "llx"
 
-#  define PRIxLEAST8  "x"
-#  define PRIxLEAST16 "x"
-#  define PRIxLEAST32 "x"
-#  define PRIxLEAST64 "llx"
-
-#  define PRIxFAST8   "x"
-#  define PRIxFAST16  "x"
-#  define PRIxFAST32  "x"
-#  define PRIxFAST64  "llx"
-
 #  define PRIX8       "X"
 #  define PRIX16      "X"
 #  define PRIX32      "X"
 #  define PRIX64      "llX"
 
-#  define PRIXLEAST8  "X"
-#  define PRIXLEAST16 "X"
-#  define PRIXLEAST32 "X"
-#  define PRIXLEAST64 "llX"
-
-#  define PRIXFAST8   "X"
-#  define PRIXFAST16  "X"
-#  define PRIXFAST32  "X"
-#  define PRIXFAST64  "llX"
-
 #  define SCNd8       "hhd"
 #  define SCNd16      "hd"
 #  define SCNd32      "d"
 #  define SCNd64      "lld"
-
-#  define SCNdLEAST8  "hhd"
-#  define SCNdLEAST16 "hd"
-#  define SCNdLEAST32 "d"
-#  define SCNdLEAST64 "lld"
-
-#  define SCNdFAST8   "hhd"
-#  define SCNdFAST16  "hd"
-#  define SCNdFAST32  "d"
-#  define SCNdFAST64  "lld"
 
 #  define SCNdMAX     "lld"
 
