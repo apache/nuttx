1847a67e052487158c281465087778afb23cb21c
chenwen <chenwen@espressif.com>
2020-11-06 16:13:08 +0800

    xtensa/esp32: Add sleep reject reasons

--- arch/xtensa/src/esp32/esp32_rtc.c
+++ arch/xtensa/src/esp32/esp32_rtc.c
@@ -1353,9 +1353,10 @@ void IRAM_ATTR esp32_rtc_sleep_init(uint32_t flags)
  *
  ****************************************************************************/
 
-void IRAM_ATTR esp32_rtc_sleep_start(uint32_t wakeup_opt,
+int IRAM_ATTR esp32_rtc_sleep_start(uint32_t wakeup_opt,
                                      uint32_t reject_opt)
 {
+  int reject;
   REG_SET_FIELD(RTC_CNTL_WAKEUP_STATE_REG, RTC_CNTL_WAKEUP_ENA, wakeup_opt);
   putreg32((uint32_t)reject_opt, RTC_CNTL_SLP_REJECT_CONF_REG);
 
@@ -1368,6 +1369,8 @@ void IRAM_ATTR esp32_rtc_sleep_start(uint32_t wakeup_opt,
 
   /* In deep sleep mode, we never get here */
 
+  reject = REG_GET_FIELD(RTC_CNTL_INT_RAW_REG, RTC_CNTL_SLP_REJECT_INT_RAW);
+
   modifyreg32(RTC_CNTL_INT_CLR_REG, 0,
               RTC_CNTL_SLP_REJECT_INT_CLR | RTC_CNTL_SLP_WAKEUP_INT_CLR);
 
@@ -1375,4 +1378,5 @@ void IRAM_ATTR esp32_rtc_sleep_start(uint32_t wakeup_opt,
 
   REG_SET_FIELD(RTC_CNTL_BIAS_CONF_REG, RTC_CNTL_DBG_ATTEN,
                 RTC_CNTL_DBG_ATTEN_DEFAULT);
+  return reject;
 }
