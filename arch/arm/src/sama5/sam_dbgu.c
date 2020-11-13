5f3153ac3c658b613777a4eacbb37903f9aa562c
YAMAMOTO Takashi <yamamoto@midokura.com>
2020-11-13 13:27:21 +0900

    arch/arm/src/sama5/sam_dbgu.c: Fix a type mismatch

--- arch/arm/src/sama5/sam_dbgu.c
+++ arch/arm/src/sama5/sam_dbgu.c
@@ -92,7 +92,7 @@ static int  dbgu_attach(struct uart_dev_s *dev);
 static void dbgu_detach(struct uart_dev_s *dev);
 static int  dbgu_interrupt(int irq, void *context, FAR void *arg);
 static int  dbgu_ioctl(struct file *filep, int cmd, unsigned long arg);
-static int  dbgu_receive(struct uart_dev_s *dev, uint32_t *status);
+static int  dbgu_receive(struct uart_dev_s *dev, unsigned int *status);
 static void dbgu_rxint(struct uart_dev_s *dev, bool enable);
 static bool dbgu_rxavailable(struct uart_dev_s *dev);
 static void dbgu_send(struct uart_dev_s *dev, int ch);
@@ -437,7 +437,7 @@ static int dbgu_ioctl(struct file *filep, int cmd, unsigned long arg)
  *
  ****************************************************************************/
 
-static int dbgu_receive(struct uart_dev_s *dev, uint32_t *status)
+static int dbgu_receive(struct uart_dev_s *dev, unsigned int *status)
 {
   struct dbgu_dev_s *priv = (struct dbgu_dev_s *)dev->priv;
 
