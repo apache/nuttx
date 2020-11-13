4b269aaea54d9360a92c36667b93b5ef1f7895d5
YAMAMOTO Takashi <yamamoto@midokura.com>
2020-11-13 12:58:38 +0900

    arch/arm/src/lpc54xx/lpc54_serial.c: Fix a type mismatch

--- arch/arm/src/lpc54xx/lpc54_serial.c
+++ arch/arm/src/lpc54xx/lpc54_serial.c
@@ -407,7 +407,7 @@ static int  lpc54_attach(struct uart_dev_s *dev);
 static void lpc54_detach(struct uart_dev_s *dev);
 static int  lpc54_interrupt(int irq, void *context, FAR void *arg);
 static int  lpc54_ioctl(struct file *filep, int cmd, unsigned long arg);
-static int  lpc54_receive(struct uart_dev_s *dev, uint32_t *status);
+static int  lpc54_receive(struct uart_dev_s *dev, unsigned int *status);
 static void lpc54_rxint(struct uart_dev_s *dev, bool enable);
 static bool lpc54_rxavailable(struct uart_dev_s *dev);
 static void lpc54_send(struct uart_dev_s *dev, int ch);
@@ -1199,7 +1199,7 @@ static int lpc54_ioctl(struct file *filep, int cmd, unsigned long arg)
  *
  ****************************************************************************/
 
-static int lpc54_receive(struct uart_dev_s *dev, uint32_t *status)
+static int lpc54_receive(struct uart_dev_s *dev, unsigned int *status)
 {
   struct lpc54_dev_s *priv = (struct lpc54_dev_s *)dev->priv;
   uint32_t fiford;
