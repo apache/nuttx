d8608001fb1a5736337587cfb4e65b03d4e1749c
YAMAMOTO Takashi <yamamoto@midokura.com>
2020-11-13 19:40:33 +0900

    arch/arm/src/lc823450/lc823450_mtd.c: Fix a printf format warning

--- arch/arm/src/lc823450/lc823450_mtd.c
+++ arch/arm/src/lc823450/lc823450_mtd.c
@@ -26,6 +26,7 @@
 
 #include <sys/types.h>
 
+#include <inttypes.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <assert.h>
@@ -788,7 +789,7 @@ int lc823450_mtd_uninitialize(uint32_t devno)
       return -ENODEV;
     }
 
-  snprintf(devname, 16, "/dev/mtdblock%d", devno);
+  snprintf(devname, 16, "/dev/mtdblock%" PRId32, devno);
 
 #ifdef CONFIG_MTD_REGISTRATION
   mtd_unregister(g_mtdmaster[ch]);
