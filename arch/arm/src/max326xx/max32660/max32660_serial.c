dec37ee7dc321ae1495089b7fb3715d8040b3434
YAMAMOTO Takashi <yamamoto@midokura.com>
2020-11-13 20:11:30 +0900

    arch/arm/src/max326xx/max32660/max32660_serial.c: Fix a printf format warning

--- arch/arm/src/max326xx/max32660/max32660_serial.c
+++ arch/arm/src/max326xx/max32660/max32660_serial.c
@@ -158,7 +158,7 @@ static int  max326_attach(struct uart_dev_s *dev);
 static void max326_detach(struct uart_dev_s *dev);
 static int  max326_interrupt(int irq, void *context, void *arg);
 static int  max326_ioctl(struct file *filep, int cmd, unsigned long arg);
-static int  max326_receive(struct uart_dev_s *dev, uint32_t *status);
+static int  max326_receive(struct uart_dev_s *dev, unsigned int *status);
 static void max326_rxint(struct uart_dev_s *dev, bool enable);
 static bool max326_rxavailable(struct uart_dev_s *dev);
 static void max326_send(struct uart_dev_s *dev, int ch);
@@ -631,7 +631,7 @@ static int max326_ioctl(struct file *filep, int cmd, unsigned long arg)
  *
  ****************************************************************************/
 
-static int max326_receive(struct uart_dev_s *dev, uint32_t *status)
+static int max326_receive(struct uart_dev_s *dev, unsigned int *status)
 {
   struct max326_dev_s *priv = (struct max326_dev_s *)dev->priv;
 
