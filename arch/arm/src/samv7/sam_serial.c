257af85b3da461e1f62f8240d1b5b04ef994c940
YAMAMOTO Takashi <yamamoto@midokura.com>
2020-11-13 14:52:08 +0900

    arch/arm/src/samv7/sam_serial.c: Fix a type mismatch

--- arch/arm/src/samv7/sam_serial.c
+++ arch/arm/src/samv7/sam_serial.c
@@ -355,7 +355,7 @@ static int  sam_attach(struct uart_dev_s *dev);
 static void sam_detach(struct uart_dev_s *dev);
 static int  sam_interrupt(int irq, void *context, FAR void *arg);
 static int  sam_ioctl(struct file *filep, int cmd, unsigned long arg);
-static int  sam_receive(struct uart_dev_s *dev, uint32_t *status);
+static int  sam_receive(struct uart_dev_s *dev, unsigned int *status);
 static void sam_rxint(struct uart_dev_s *dev, bool enable);
 static bool sam_rxavailable(struct uart_dev_s *dev);
 static void sam_send(struct uart_dev_s *dev, int ch);
@@ -1255,7 +1255,7 @@ static int sam_ioctl(struct file *filep, int cmd, unsigned long arg)
  *
  ****************************************************************************/
 
-static int sam_receive(struct uart_dev_s *dev, uint32_t *status)
+static int sam_receive(struct uart_dev_s *dev, unsigned int *status)
 {
   struct sam_dev_s *priv = (struct sam_dev_s *)dev->priv;
 
