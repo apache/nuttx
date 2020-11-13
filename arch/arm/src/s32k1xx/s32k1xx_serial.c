0d0aae2f002bf269d429809d5613eac69e064b61
YAMAMOTO Takashi <yamamoto@midokura.com>
2020-11-13 15:59:26 +0900

    arch/arm/src/s32k1xx/s32k1xx_serial.c: Fix a type mismatch

--- arch/arm/src/s32k1xx/s32k1xx_serial.c
+++ arch/arm/src/s32k1xx/s32k1xx_serial.c
@@ -199,7 +199,7 @@ static int  s32k1xx_attach(struct uart_dev_s *dev);
 static void s32k1xx_detach(struct uart_dev_s *dev);
 static int  s32k1xx_interrupt(int irq, void *context, FAR void *arg);
 static int  s32k1xx_ioctl(struct file *filep, int cmd, unsigned long arg);
-static int  s32k1xx_receive(struct uart_dev_s *dev, uint32_t *status);
+static int  s32k1xx_receive(struct uart_dev_s *dev, unsigned int *status);
 static void s32k1xx_rxint(struct uart_dev_s *dev, bool enable);
 static bool s32k1xx_rxavailable(struct uart_dev_s *dev);
 static void s32k1xx_send(struct uart_dev_s *dev, int ch);
@@ -955,7 +955,7 @@ static int s32k1xx_ioctl(struct file *filep, int cmd, unsigned long arg)
  *
  ****************************************************************************/
 
-static int s32k1xx_receive(struct uart_dev_s *dev, uint32_t *status)
+static int s32k1xx_receive(struct uart_dev_s *dev, unsigned int *status)
 {
   struct s32k1xx_uart_s *priv = (struct s32k1xx_uart_s *)dev->priv;
   uint32_t rxd;
