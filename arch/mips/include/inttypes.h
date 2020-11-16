d9936d33f242fcacc69533574b195b0f9d30a38b
YAMAMOTO Takashi <yamamoto@midokura.com>
2020-11-16 14:27:44 +0900

    mips: Use types matching the compiler types
        root@212cf3f52994:/tools# p32-gcc -mlong32 -dM -E - < /dev/null | grep "__INT.*_TYPE__"|sort
        #define __INT16_TYPE__ short int
        #define __INT32_TYPE__ long int
        #define __INT64_TYPE__ long long int
        #define __INT8_TYPE__ signed char
        #define __INTMAX_TYPE__ long long int
        #define __INTPTR_TYPE__ int
        #define __INT_FAST16_TYPE__ int
        #define __INT_FAST32_TYPE__ int
        #define __INT_FAST64_TYPE__ long long int
        #define __INT_FAST8_TYPE__ int
        #define __INT_LEAST16_TYPE__ short int
        #define __INT_LEAST32_TYPE__ long int
        #define __INT_LEAST64_TYPE__ long long int
        #define __INT_LEAST8_TYPE__ signed char
        root@212cf3f52994:/tools#

--- arch/mips/include/inttypes.h
+++ arch/mips/include/inttypes.h
@@ -46,89 +46,89 @@
 
 #define PRId8       "d"
 #define PRId16      "d"
-#define PRId32      "d"
+#define PRId32      "ld"
 #define PRId64      "lld"
 
 #define PRIdPTR     "d"
 
 #define PRIi8       "i"
 #define PRIi16      "i"
-#define PRIi32      "i"
+#define PRIi32      "li"
 #define PRIi64      "lli"
 
 #define PRIiPTR     "i"
 
 #define PRIo8       "o"
 #define PRIo16      "o"
-#define PRIo32      "o"
+#define PRIo32      "lo"
 #define PRIo64      "llo"
 
 #define PRIoPTR     "o"
 
 #define PRIu8       "u"
 #define PRIu16      "u"
-#define PRIu32      "u"
+#define PRIu32      "lu"
 #define PRIu64      "llu"
 
 #define PRIuPTR     "u"
 
 #define PRIx8       "x"
 #define PRIx16      "x"
-#define PRIx32      "x"
+#define PRIx32      "lx"
 #define PRIx64      "llx"
 
 #define PRIxPTR     "x"
 
 #define PRIX8       "X"
 #define PRIX16      "X"
-#define PRIX32      "X"
+#define PRIX32      "lX"
 #define PRIX64      "llX"
 
 #define PRIXPTR     "X"
 
 #define SCNd8       "hhd"
 #define SCNd16      "hd"
-#define SCNd32      "d"
+#define SCNd32      "ld"
 #define SCNd64      "lld"
 
 #define SCNdPTR     "d"
 
 #define SCNi8       "hhi"
 #define SCNi16      "hi"
-#define SCNi32      "i"
+#define SCNi32      "li"
 #define SCNi64      "lli"
 
 #define SCNiPTR     "i"
 
 #define SCNo8       "hho"
 #define SCNo16      "ho"
-#define SCNo32      "o"
+#define SCNo32      "lo"
 #define SCNo64      "llo"
 
 #define SCNoPTR     "o"
 
 #define SCNu8       "hhu"
 #define SCNu16      "hu"
-#define SCNu32      "u"
+#define SCNu32      "lu"
 #define SCNu64      "llu"
 
 #define SCNuPTR     "u"
 
 #define SCNx8       "hhx"
 #define SCNx16      "hx"
-#define SCNx32      "x"
+#define SCNx32      "lx"
 #define SCNx64      "llx"
 
 #define SCNxPTR     "x"
 
 #define INT8_C(x)   x
 #define INT16_C(x)  x
-#define INT32_C(x)  x
+#define INT32_C(x)  x ## l
 #define INT64_C(x)  x ## ll
 
 #define UINT8_C(x)  x
 #define UINT16_C(x) x
-#define UINT32_C(x) x ## u
+#define UINT32_C(x) x ## ul
 #define UINT64_C(x) x ## ull
 
 #endif /* __ARCH_MIPS_INCLUDE_INTTYPES_H */
