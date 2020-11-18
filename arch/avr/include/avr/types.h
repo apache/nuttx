9c1d235329425271a6d3f204bdf9a49ce23d20bd
YAMAMOTO Takashi <yamamoto@midokura.com>
2020-11-18 13:47:33 +0900

    avr: Add _intmax_t and _uintmax_t

--- arch/avr/include/avr/types.h
+++ arch/avr/include/avr/types.h
@@ -79,6 +79,9 @@ typedef double double_t;
 
 #define __INT64_DEFINED
 
+typedef _int64_t           _intmax_t;
+typedef _uint64_t          _uintmax_t;
+
 /* A (near) size is 2 bytes */
 
 #if defined(__SIZE_TYPE__)
