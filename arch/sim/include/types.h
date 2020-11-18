c97f6f1b59a503758e3c5dd151f89f669654b3c2
YAMAMOTO Takashi <yamamoto@midokura.com>
2020-11-18 21:28:45 +0900

    sim: Add _intmax_t and _uintmax_t

--- arch/sim/include/types.h
+++ arch/sim/include/types.h
@@ -76,6 +76,14 @@ typedef signed long long   _int64_t;
 typedef unsigned long long _uint64_t;
 #define __INT64_DEFINED
 
+#if defined(__APPLE_CC__)
+typedef signed long        _intmax_t;
+typedef unsigned long      _uintmax_t;
+#else
+typedef _int64_t           _intmax_t;
+typedef _uint64_t          _uintmax_t;
+#endif
+
 #if defined(CONFIG_HOST_X86_64) && !defined(CONFIG_SIM_M32)
 /* 64-bit build on 64-bit machine: A size is 8 bytes */
 
