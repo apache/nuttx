7e3b56d227e1c02dd5d04a36c853ef3cc3db581a
Brennan Ashton <bashton@brennanashton.com>
2020-11-21 18:53:57 -0800

    usb: Do not try to print verbose trace when disabled
    Right now if usb tracing is enabled but verbose is disabled
    a debug assert will be triggered when ever a verbose trace point
    is hit.  Instead of trying to print the NULL message, just return
    early.
    
    Signed-off-by: Brennan Ashton <bashton@brennanashton.com>

--- drivers/usbhost/usbhost_trace.c
+++ drivers/usbhost/usbhost_trace.c
@@ -170,6 +170,7 @@ void usbhost_trace_common(uint32_t event)
             }
         }
     }
+
   leave_critical_section(flags);
 }
 #endif /* CONFIG_USBHOST_TRACE */
@@ -202,7 +203,10 @@ void usbhost_trace1(uint16_t id, uint32_t u23)
   /* Get the format associated with the trace */
 
   fmt = usbhost_trformat1(id);
-  DEBUGASSERT(fmt);
+  if (fmt == NULL)
+    {
+      return;
+    }
 
   /* Just print the data using syslog() */
 
@@ -220,7 +224,10 @@ void usbhost_trace2(uint16_t id, uint8_t u7, uint16_t u16)
   /* Get the format associated with the trace */
 
   fmt = usbhost_trformat2(id);
-  DEBUGASSERT(fmt);
+  if (fmt == NULL)
+    {
+      return;
+    }
 
   /* Just print the data using syslog() */
 
