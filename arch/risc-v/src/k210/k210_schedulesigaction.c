35449e4d14dcffc0790a3e4e5d6a0301a553abc9
YAMAMOTO Takashi <yamamoto@midokura.com>
2020-11-22 16:53:00 +0900

    risc-v: Don't use non existent "saved_status"
    It seems like a copy-and-paste leftover from mips.
    Replace them with saved_int_ctx.
    (Shouldn't these files inherit the copyright notice from mips?)

--- arch/risc-v/src/k210/k210_schedulesigaction.c
+++ arch/risc-v/src/k210/k210_schedulesigaction.c
@@ -173,7 +173,7 @@ void up_schedule_sigaction(struct tcb_s *tcb, sig_deliver_t sigdeliver)
               up_savestate(tcb->xcp.regs);
 
               sinfo("PC/STATUS Saved: %016x/%016x New: %016x/%016x\n",
-                    tcb->xcp.saved_epc, tcb->xcp.saved_status,
+                    tcb->xcp.saved_epc, tcb->xcp.saved_int_ctx,
                     CURRENT_REGS[REG_EPC], CURRENT_REGS[REG_INT_CTX]);
             }
         }
@@ -208,7 +208,7 @@ void up_schedule_sigaction(struct tcb_s *tcb, sig_deliver_t sigdeliver)
           tcb->xcp.regs[REG_INT_CTX]  = int_ctx;
 
           sinfo("PC/STATUS Saved: %016x/%016x New: %016x/%016x\n",
-                tcb->xcp.saved_epc, tcb->xcp.saved_status,
+                tcb->xcp.saved_epc, tcb->xcp.saved_int_ctx,
                 tcb->xcp.regs[REG_EPC], tcb->xcp.regs[REG_INT_CTX]);
         }
     }
--- arch/risc-v/src/nr5m100/nr5_schedulesigaction.c
+++ arch/risc-v/src/nr5m100/nr5_schedulesigaction.c
@@ -167,7 +167,7 @@ void up_schedule_sigaction(struct tcb_s *tcb, sig_deliver_t sigdeliver)
               up_savestate(tcb->xcp.regs);
 
               sinfo("PC/STATUS Saved: %08x/%08x New: %08x/%08x\n",
-                    tcb->xcp.saved_epc, tcb->xcp.saved_status,
+                    tcb->xcp.saved_epc, tcb->xcp.saved_int_ctx,
                     g_current_regs[REG_EPC], g_current_regs[REG_STATUS]);
             }
         }
@@ -200,7 +200,7 @@ void up_schedule_sigaction(struct tcb_s *tcb, sig_deliver_t sigdeliver)
           tcb->xcp.regs[REG_INT_CTX]  = int_ctx;
 
           sinfo("PC/STATUS Saved: %08x/%08x New: %08x/%08x\n",
-                tcb->xcp.saved_epc, tcb->xcp.saved_status,
+                tcb->xcp.saved_epc, tcb->xcp.saved_int_ctx,
                 tcb->xcp.regs[REG_EPC], tcb->xcp.regs[REG_STATUS]);
         }
     }
