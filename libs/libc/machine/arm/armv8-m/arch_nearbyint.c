b44b05c9b6711c3660fde039da6d88165715a7c0
Xiang Xiao <xiaoxiang@xiaomi.com>
2020-11-26 20:06:21 +0800

    libs/libc/machine/arm: Rename armv8 to armv8-m
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- libs/libc/machine/arm/armv8-m/arch_nearbyint.c
+++ libs/libc/machine/arm/armv8-m/arch_nearbyint.c
@@ -1,5 +1,5 @@
 /****************************************************************************
- * libs/libc/machine/arm/armv8/arch_floorf.c
+ * libs/libc/machine/arm/armv8-m/arch_nearbyint.c
  *
  *   Copyright (C) 2017 Gregory Nutt. All rights reserved.
  *
@@ -43,15 +43,15 @@
  * Public Functions
  ****************************************************************************/
 
-#if __ARM_ARCH >= 8 && !defined (__SOFTFP__)
+#if __ARM_ARCH >= 8 && (__ARM_FP & 0x8) && !defined (__SOFTFP__)
 
-float floorf(float x)
+double nearbyint(double x)
 {
-  float result;
-  asm volatile ( "vrintm.f32\t%0, %1" : "=t" (result) : "t" (x) );
+  double result;
+  asm volatile ("vrintr.f64\t%P0, %P1" : "=w" (result) : "w" (x));
   return result;
 }
 
 #else
-#  warning floorf() not built
+#  warning nearbyint() not built
 #endif
