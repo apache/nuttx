3ec743f72f5e9e603860a070ce796b9a6b9f9003
Masayuki Ishikawa <masayuki.ishikawa@gmail.com>
2020-11-28 20:57:44 +0900

    arch: sim: Fix comments in up_setjmp64.S and up_smpsignal.c
    Signed-off-by: Masayuki Ishikawa <Masayuki.Ishikawa@jp.sony.com>

--- arch/sim/src/sim/up_smpsignal.c
+++ arch/sim/src/sim/up_smpsignal.c
@@ -1,5 +1,5 @@
 /****************************************************************************
- * arch/sim/src/sim/up_simsmp.c
+ * arch/sim/src/sim/up_smpsignal.c
  *
  *   Copyright (C) 2016 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>
