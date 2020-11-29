89b0c97316f7fa36c9e3f4387bbda5ff75418b1a
Yoshinori Sugino <ysgn0101@gmail.com>
2020-11-29 13:27:11 +0900

    sched/sched/sched_waitpid.c: Fix return value
    waitpid should return the process ID of the child whose state has changed even if pid is -1.

--- sched/sched/sched_waitpid.c
+++ sched/sched/sched_waitpid.c
@@ -332,6 +332,7 @@ pid_t nx_waitpid(pid_t pid, int *stat_loc, int options)
               /* The child has exited. Return the saved exit status */
 
               *stat_loc = child->ch_status << 8;
+              pid = child->ch_pid;
 
               /* Discard the child entry and break out of the loop */
 
