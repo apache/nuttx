8b4b451521f48545995f4924dbbd31c70d887d80
YAMAMOTO Takashi <yamamoto@midokura.com>
2020-11-29 16:31:40 +0900

    arch/arm/src/lc823450/lc823450_serial.c: Fix a syslog format

--- arch/arm/src/lc823450/lc823450_serial.c
+++ arch/arm/src/lc823450/lc823450_serial.c
@@ -25,6 +25,7 @@
 #include <nuttx/config.h>
 
 #include <sys/types.h>
+#include <inttypes.h>
 #include <stdint.h>
 #include <stdbool.h>
 #include <unistd.h>
@@ -474,7 +475,7 @@ static int up_setup(struct uart_dev_s *dev)
 
   if (udiv < 0)
     {
-      serr("ERROR: baud = %d\n", priv->baud);
+      serr("ERROR: baud = %" PRId32 "\n", priv->baud);
       return -EINVAL;
     }
 
