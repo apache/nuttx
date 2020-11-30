353600c8875eef8c342a3fffeece0b43abeb9a98
YAMAMOTO Takashi <yamamoto@midokura.com>
2020-11-30 20:08:50 +0900

    arch/arm/src/kinetis/kinetis_spi.c: Fix syslog formats

--- arch/arm/src/kinetis/kinetis_spi.c
+++ arch/arm/src/kinetis/kinetis_spi.c
@@ -65,6 +65,7 @@
 #include <nuttx/config.h>
 
 #include <sys/types.h>
+#include <inttypes.h>
 #include <stdint.h>
 #include <limits.h>
 #include <stdbool.h>
@@ -714,7 +715,7 @@ static uint32_t spi_setfrequency(FAR struct spi_dev_s *dev, uint32_t frequency)
 
   priv->frequency = frequency;
 
-  spiinfo("Frequency %d->%d\n", frequency, priv->actual);
+  spiinfo("Frequency %" PRId32 "->%" PRId32 "\n", frequency, priv->actual);
   return priv->actual;
 }
 
