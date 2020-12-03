84f2aa94369795bde5889d7fe19bd17a2c5b1a75
Michal Lenc <lencmich@fel.cvut.cz>
2020-12-03 18:38:39 +0100

    boards/arm/imxrt/teensy-4.x: fixed build error and nx style
    Signed-off-by: Michal Lenc <lencmich@fel.cvut.cz>

--- boards/arm/imxrt/teensy-4.x/src/imxrt_flexspi_nor_flash.h
+++ boards/arm/imxrt/teensy-4.x/src/imxrt_flexspi_nor_flash.h
@@ -18,7 +18,7 @@
  *
  ****************************************************************************/
 #ifndef __BOARDS_ARM_IMXRT_TEENSY_4X_SRC_IMXRT_FLEXSPI_NOR_FLASH_H
-#  define __BOARDS_ARM_IMXRT_TEENSY_4X_SRC_IMXRT_FLEXSPI_NOR_FLASH_H
+#define __BOARDS_ARM_IMXRT_TEENSY_4X_SRC_IMXRT_FLEXSPI_NOR_FLASH_H
 
 /****************************************************************************
  * Included Files
@@ -188,177 +188,177 @@
 /* Definitions for FlexSPI Serial Clock Frequency */
 
 enum flexspi_serial_clkfreq_e
-  {
-    FLEXSPI_SERIAL_CLKFREQ_30MHz = 1,
-    FLEXSPI_SERIAL_CLKFREQ_50MHz = 2,
-    FLEXSPI_SERIAL_CLKFREQ_60MHz = 3,
-    FLEXSPI_SERIAL_CLKFREQ_75MHz = 4,
-    FLEXSPI_SERIAL_CLKFREQ_80MHz = 5,
-    FLEXSPI_SERIAL_CLKFREQ_100MHz = 6,
-    FLEXSPI_SERIAL_CLKFREQ_133MHz = 7,
-    FLEXSPI_SERIAL_CLKFREQ_166MHz = 8,
-    FLEXSPI_SERIAL_CLKFREQ_200MHz = 9,
-  };
+{
+  FLEXSPI_SERIAL_CLKFREQ_30MHz = 1,
+  FLEXSPI_SERIAL_CLKFREQ_50MHz = 2,
+  FLEXSPI_SERIAL_CLKFREQ_60MHz = 3,
+  FLEXSPI_SERIAL_CLKFREQ_75MHz = 4,
+  FLEXSPI_SERIAL_CLKFREQ_80MHz = 5,
+  FLEXSPI_SERIAL_CLKFREQ_100MHz = 6,
+  FLEXSPI_SERIAL_CLKFREQ_133MHz = 7,
+  FLEXSPI_SERIAL_CLKFREQ_166MHz = 8,
+  FLEXSPI_SERIAL_CLKFREQ_200MHz = 9,
+};
 
 /* FlexSPI clock configuration type */
 
 enum flexspi_serial_clockmode_e
-  {
-    FLEXSPI_CLKMODE_SDR,
-    FLEXSPI_CLKMODE_DDR,
-  };
+{
+  FLEXSPI_CLKMODE_SDR,
+  FLEXSPI_CLKMODE_DDR,
+};
 
 /* FlexSPI Read Sample Clock Source definition */
 
 enum flash_read_sample_clk_e
-  {
-    FLASH_READ_SAMPLE_CLK_LOOPBACK_INTERNELLY = 0,
-    FLASH_READ_SAMPLE_CLK_LOOPBACK_FROM_DQSPAD = 1,
-    FLASH_READ_SAMPLE_CLK_LOOPBACK_FROM_SCKPAD = 2,
-    FLASH_READ_SAMPLE_CLK_EXTERNALINPUT_FROM_DQSPAD = 3,
-  };
+{
+  FLASH_READ_SAMPLE_CLK_LOOPBACK_INTERNELLY = 0,
+  FLASH_READ_SAMPLE_CLK_LOOPBACK_FROM_DQSPAD = 1,
+  FLASH_READ_SAMPLE_CLK_LOOPBACK_FROM_SCKPAD = 2,
+  FLASH_READ_SAMPLE_CLK_EXTERNALINPUT_FROM_DQSPAD = 3,
+};
 
 /*  Misc feature bit definitions */
 
 enum flash_misc_feature_e
-  {
-    FLEXSPIMISC_OFFSET_DIFFCLKEN = 0,   /* Bit for Differential clock enable */
-    FLEXSPIMISC_OFFSET_CK2EN = 1,       /* Bit for CK2 enable */
-    FLEXSPIMISC_OFFSET_PARALLELEN = 2,  /* Bit for Parallel mode enable */
+{
+  FLEXSPIMISC_OFFSET_DIFFCLKEN = 0,   /* Bit for Differential clock enable */
+  FLEXSPIMISC_OFFSET_CK2EN = 1,       /* Bit for CK2 enable */
+  FLEXSPIMISC_OFFSET_PARALLELEN = 2,  /* Bit for Parallel mode enable */
 
-    FLEXSPIMISC_OFFSET_WORD_ADDRESSABLE_EN = 3, /* Bit for Word Addressable
+  FLEXSPIMISC_OFFSET_WORD_ADDRESSABLE_EN = 3, /* Bit for Word Addressable
                                                  * enable */
-    FLEXSPIMISC_OFFSET_SAFECONFIG_FREQ_EN = 4,  /* Bit for Safe Configuration
+  FLEXSPIMISC_OFFSET_SAFECONFIG_FREQ_EN = 4,  /* Bit for Safe Configuration
                                                  * Frequency enable */
 
-    FLEXSPIMISC_OFFSET_PAD_SETTING_OVERRIDE_EN = 5,     /* Bit for Pad setting
+  FLEXSPIMISC_OFFSET_PAD_SETTING_OVERRIDE_EN = 5,     /* Bit for Pad setting
                                                          * override enable */
 
-    FLEXSPIMISC_OFFSET_DDR_MODE_EN = 6, /* Bit for DDR clock confiuration
+  FLEXSPIMISC_OFFSET_DDR_MODE_EN = 6, /* Bit for DDR clock confiuration
                                          * indication. */
-  };
+};
 
 /* Flash Type Definition */
 
 enum flash_flash_type_e
-  {
-    FLEXSPI_DEVICE_TYPE_SERIAL_NOR = 1,  /* Flash devices are Serial NOR */
-    FLEXSPI_DEVICE_TYPE_SERIAL_NAND = 2, /* Flash devices are Serial
+{
+  FLEXSPI_DEVICE_TYPE_SERIAL_NOR = 1,  /* Flash devices are Serial NOR */
+  FLEXSPI_DEVICE_TYPE_SERIAL_NAND = 2, /* Flash devices are Serial
                                          * NAND */
-    FLEXSPI_DEVICE_TYPE_SERIAL_RAM = 3,  /* Flash devices are Serial
+  FLEXSPI_DEVICE_TYPE_SERIAL_RAM = 3,  /* Flash devices are Serial
                                          * RAM/HyperFLASH */
 
-    FLEXSPI_DEVICE_TYPE_MCP_NOR_NAND = 0x12,    /* Flash device is MCP device,
+  FLEXSPI_DEVICE_TYPE_MCP_NOR_NAND = 0x12,    /* Flash device is MCP device,
                                                  * A1 is Serial NOR, A2 is
                                                  * Serial NAND */
-    FLEXSPI_DEVICE_TYPE_MCP_NOR_RAM = 0x13,     /* Flash device is MCP device,
+  FLEXSPI_DEVICE_TYPE_MCP_NOR_RAM = 0x13,     /* Flash device is MCP device,
                                                  * A1 is Serial NOR, A2 is
                                                  * Serial RAMs */
-  };
+};
 
 /* Flash Pad Definitions */
 
 enum flash_flash_pad_e
-  {
-    SERIAL_FLASH_1PAD = 1,
-    SERIAL_FLASH_2PADS = 2,
-    SERIAL_FLASH_4PADS = 4,
-    SERIAL_FLASH_8PADS = 8,
-  };
+{
+  SERIAL_FLASH_1PAD = 1,
+  SERIAL_FLASH_2PADS = 2,
+  SERIAL_FLASH_4PADS = 4,
+  SERIAL_FLASH_8PADS = 8,
+};
 
 /* Flash Configuration Command Type */
 
 enum flash_config_cmd_e
-  {
-    DEVICE_CONFIG_CMD_TYPE_GENERIC,     /* Generic command, for example:
+{
+  DEVICE_CONFIG_CMD_TYPE_GENERIC,     /* Generic command, for example:
                                          * configure dummy cycles, drive
                                          * strength, etc */
-    DEVICE_CONFIG_CMD_TYPE_QUADENABLE,  /* Quad Enable command */
-    DEVICE_CONFIG_CMD_TYPE_SPI2XPI,     /* Switch from SPI to DPI/QPI/OPI mode */
-    DEVICE_CONFIG_CMD_TYPE_XPI2SPI,     /* Switch from DPI/QPI/OPI to SPI mode */
-    DEVICE_CONFIG_CMD_TYPE_SPI2NO_CMD,  /* Switch to 0-4-4/0-8-8 mode */
-    DEVICE_CONFIG_CMD_TYPE_RESET,       /* Reset device command */
-  };
+  DEVICE_CONFIG_CMD_TYPE_QUADENABLE,  /* Quad Enable command */
+  DEVICE_CONFIG_CMD_TYPE_SPI2XPI,     /* Switch from SPI to DPI/QPI/OPI mode */
+  DEVICE_CONFIG_CMD_TYPE_XPI2SPI,     /* Switch from DPI/QPI/OPI to SPI mode */
+  DEVICE_CONFIG_CMD_TYPE_SPI2NO_CMD,  /* Switch to 0-4-4/0-8-8 mode */
+  DEVICE_CONFIG_CMD_TYPE_RESET,       /* Reset device command */
+};
 
 /* FlexSPI LUT Sequence structure */
 
 struct flexspi_lut_seq_s
-  {
-    uint8_t seq_num;            /* Sequence Number, valid number: 1-16 */
-    uint8_t seq_id;             /* Sequence Index, valid number: 0-15 */
-    uint16_t reserved;
-  };
+{
+  uint8_t seq_num;            /* Sequence Number, valid number: 1-16 */
+  uint8_t seq_id;             /* Sequence Index, valid number: 0-15 */
+  uint16_t reserved;
+};
 
 /* FlexSPI Memory Configuration Block */
 
 struct flexspi_mem_config_s
-  {
-    uint32_t tag;
-    uint32_t version;
-    uint32_t reserved0;
-    uint8_t read_sample_clksrc;
-    uint8_t cs_hold_time;
-    uint8_t cs_setup_time;
-    uint8_t column_address_width;       /* [0x00f-0x00f] Column Address with,
+{
+  uint32_t tag;
+  uint32_t version;
+  uint32_t reserved0;
+  uint8_t read_sample_clksrc;
+  uint8_t cs_hold_time;
+  uint8_t cs_setup_time;
+  uint8_t column_address_width;       /* [0x00f-0x00f] Column Address with,
                                          * for HyperBus protocol, it is fixed
                                          * to 3, For Serial NAND, need to refer
                                          * to datasheet */
-    uint8_t device_mode_cfg_enable;
-    uint8_t device_mode_type;
-    uint16_t wait_time_cfg_commands;
-    struct flexspi_lut_seq_s device_mode_seq;
-    uint32_t device_mode_arg;
-    uint8_t config_cmd_enable;
-    uint8_t config_mode_type[3];
-    struct flexspi_lut_seq_s config_cmd_seqs[3];
-    uint32_t reserved1;
-    uint32_t config_cmd_args[3];
-    uint32_t reserved2;
-    uint32_t controller_misc_option;
-    uint8_t device_type;
-    uint8_t sflash_pad_type;
-    uint8_t serial_clk_freq;
-    uint8_t lut_custom_seq_enable;
-    uint32_t reserved3[2];
-    uint32_t sflash_a1size;
-    uint32_t sflash_a2size;
-    uint32_t sflash_b1size;
-    uint32_t sflash_b2size;
-    uint32_t cspad_setting_override;
-    uint32_t sclkpad_setting_override;
-    uint32_t datapad_setting_override;
-    uint32_t dqspad_setting_override;
-    uint32_t timeout_in_ms;
-    uint32_t command_interval;
-    uint16_t data_valid_time[2];
-    uint16_t busy_offset;
-    uint16_t busybit_polarity;
-    uint32_t lookup_table[64];
-    struct flexspi_lut_seq_s lut_customseq[12];
-    uint32_t reserved4[4];
-  };
+  uint8_t device_mode_cfg_enable;
+  uint8_t device_mode_type;
+  uint16_t wait_time_cfg_commands;
+  struct flexspi_lut_seq_s device_mode_seq;
+  uint32_t device_mode_arg;
+  uint8_t config_cmd_enable;
+  uint8_t config_mode_type[3];
+  struct flexspi_lut_seq_s config_cmd_seqs[3];
+  uint32_t reserved1;
+  uint32_t config_cmd_args[3];
+  uint32_t reserved2;
+  uint32_t controller_misc_option;
+  uint8_t device_type;
+  uint8_t sflash_pad_type;
+  uint8_t serial_clk_freq;
+  uint8_t lut_custom_seq_enable;
+  uint32_t reserved3[2];
+  uint32_t sflash_a1size;
+  uint32_t sflash_a2size;
+  uint32_t sflash_b1size;
+  uint32_t sflash_b2size;
+  uint32_t cspad_setting_override;
+  uint32_t sclkpad_setting_override;
+  uint32_t datapad_setting_override;
+  uint32_t dqspad_setting_override;
+  uint32_t timeout_in_ms;
+  uint32_t command_interval;
+  uint16_t data_valid_time[2];
+  uint16_t busy_offset;
+  uint16_t busybit_polarity;
+  uint32_t lookup_table[64];
+  struct flexspi_lut_seq_s lut_customseq[12];
+  uint32_t reserved4[4];
+};
 
 /* Serial NOR configuration block */
 
 struct flexspi_nor_config_s
-  {
-    struct flexspi_mem_config_s mem_config;     /* Common memory configuration
+{
+  struct flexspi_mem_config_s mem_config;     /* Common memory configuration
                                                  * info via FlexSPI */
 
-    uint32_t page_size;             /* Page size of Serial NOR */
-    uint32_t sector_size;           /* Sector size of Serial NOR */
-    uint8_t ipcmd_serial_clkfreq;   /* Clock frequency for IP command */
-    uint8_t is_uniform_blocksize;   /* Sector/Block size is the same */
-    uint8_t reserved0[2];           /* Reserved for future use */
-    uint8_t serial_nor_type;        /* Serial NOR Flash type: 0/1/2/3 */
-    uint8_t need_exit_nocmdmode;    /* Need to exit NoCmd mode before other
+  uint32_t page_size;             /* Page size of Serial NOR */
+  uint32_t sector_size;           /* Sector size of Serial NOR */
+  uint8_t ipcmd_serial_clkfreq;   /* Clock frequency for IP command */
+  uint8_t is_uniform_blocksize;   /* Sector/Block size is the same */
+  uint8_t reserved0[2];           /* Reserved for future use */
+  uint8_t serial_nor_type;        /* Serial NOR Flash type: 0/1/2/3 */
+  uint8_t need_exit_nocmdmode;    /* Need to exit NoCmd mode before other
                                      * IP command */
-    uint8_t halfclk_for_nonreadcmd; /* Half the Serial Clock for non-read
+  uint8_t halfclk_for_nonreadcmd; /* Half the Serial Clock for non-read
                                      * command: true/false */
-    uint8_t need_restore_nocmdmode; /* Need to Restore NoCmd mode after IP
+  uint8_t need_restore_nocmdmode; /* Need to Restore NoCmd mode after IP
                                      * command execution */
-    uint32_t blocksize;             /* Block size */
-    uint32_t reserve2[11];          /* Reserved for future use */
-  };
+  uint32_t blocksize;             /* Block size */
+  uint32_t reserve2[11];          /* Reserved for future use */
+};
 
 #endif  /* __BOARDS_ARM_IMXRT_TEENSY_4_SRC_IMXRT_FLEXSPI_NOR_FLASH_H */
