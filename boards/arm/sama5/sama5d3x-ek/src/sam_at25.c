25ea93cb4ae63b12e2f5f8410bc52bf0327f01d8
YAMAMOTO Takashi <yamamoto@midokura.com>
2020-12-05 08:26:06 +0900

    boards/arm/sama5/sama5d3x-ek/src/sam_at25.c: Fix a syslog format

--- boards/arm/sama5/sama5d3x-ek/src/sam_at25.c
+++ boards/arm/sama5/sama5d3x-ek/src/sam_at25.c
@@ -97,7 +97,7 @@ int sam_at25_automount(int minor)
       if (!mtd)
         {
           ferr("ERROR: Failed to bind SPI port %d "
-               "to the AT25 FLASH driver\n");
+               "to the AT25 FLASH driver\n", AT25_PORT);
           return -ENODEV;
         }
 
