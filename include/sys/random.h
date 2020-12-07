8a7703b2be1fb06962f606f62296296cecae2736
chao.an <anchao@xiaomi.com>
2020-12-07 11:13:53 +0800

    libs/libc/getrandom(2): add ORed bit mask definition
    Change-Id: I1a805420f3ace0b844452dc126d04534680e96f2
    Signed-off-by: chao.an <anchao@xiaomi.com>

--- include/sys/random.h
+++ include/sys/random.h
@@ -45,6 +45,22 @@
 #include <stddef.h>
 
 /****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+/*
+ * Flags for getrandom(2)
+ *
+ * GRND_NONBLOCK  Don't block and return EAGAIN instead
+ * GRND_RANDOM    No effect
+ * GRND_INSECURE  Return non-cryptographic random bytes
+ */
+
+#define GRND_NONBLOCK   0x0001
+#define GRND_RANDOM     0x0002
+#define GRND_INSECURE   0x0004
+
+/****************************************************************************
  * Public Function Prototypes
  ****************************************************************************/
 
