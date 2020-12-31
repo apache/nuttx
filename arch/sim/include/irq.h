f0f09b913c5b80d46c28900b168ec20a3ec86801
Jiuzhu Dong <dongjiuzhu1@xiaomi.com>
2020-12-31 16:27:08 +0800

    vfork: support sim vfork
    MIRTOS-130
    
    Change-Id: I15920bcbacfc5ea519cfe12c39cb64dfe6365838
    Signed-off-by: Jiuzhu Dong <dongjiuzhu1@xiaomi.com>

--- arch/sim/include/irq.h
+++ arch/sim/include/irq.h
@@ -52,12 +52,15 @@
   /* Storage order: %rbx, %rsp, %rbp, %r12, %r13, %r14, %r15, %rip */
 
 #  define XCPTCONTEXT_REGS    8
+#  define XCPTCONTEXT_SIZE    (8 * XCPTCONTEXT_REGS)
 #elif defined(CONFIG_HOST_X86) || defined(CONFIG_SIM_M32)
   /* Storage order: %ebx, %esi, %edi, %ebp, sp, and return PC */
 
 #  define XCPTCONTEXT_REGS    6
+#  define XCPTCONTEXT_SIZE    (4 * XCPTCONTEXT_REGS)
 #elif defined(CONFIG_HOST_ARM)
 #  define XCPTCONTEXT_REGS    16
+#  define XCPTCONTEXT_SIZE    (4 * XCPTCONTEXT_REGS)
 #endif
 
 /****************************************************************************
