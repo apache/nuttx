5a7d988a43a0b4c095d1d7e7d7310d5ae436f9a6
Xiang Xiao <xiaoxiang@xiaomi.com>
2021-01-02 22:45:24 +0800

    fs: Remove _files_close and reuse file_close
    to save the code space
    
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>
    Change-Id: I08455010ba121a61b0e29334b580aa83e69a9418

--- fs/inode/fs_files.c
+++ fs/inode/fs_files.c
@@ -56,50 +56,6 @@ static int _files_semtake(FAR struct filelist *list)
 #define _files_semgive(list) nxsem_post(&list->fl_sem)
 
 /****************************************************************************
- * Name: _files_close
- *
- * Description:
- *   Close an inode (if open)
- *
- * Assumptions:
- *   Caller holds the list semaphore because the file descriptor will be
- *   freed.
- *
- ****************************************************************************/
-
-static int _files_close(FAR struct file *filep)
-{
-  struct inode *inode = filep->f_inode;
-  int ret = OK;
-
-  /* Check if the struct file is open (i.e., assigned an inode) */
-
-  if (inode)
-    {
-      /* Close the file, driver, or mountpoint. */
-
-      if (inode->u.i_ops && inode->u.i_ops->close)
-        {
-          /* Perform the close operation */
-
-          ret = inode->u.i_ops->close(filep);
-        }
-
-      /* And release the inode */
-
-      inode_release(inode);
-
-      /* Release the file descriptor */
-
-      filep->f_oflags  = 0;
-      filep->f_pos     = 0;
-      filep->f_inode = NULL;
-    }
-
-  return ret;
-}
-
-/****************************************************************************
  * Public Functions
  ****************************************************************************/
 
@@ -152,7 +108,7 @@ void files_releaselist(FAR struct filelist *list)
 
   for (i = CONFIG_NFILE_DESCRIPTORS; i > 0; i--)
     {
-      _files_close(&list->fl_files[i - 1]);
+      file_close(&list->fl_files[i - 1]);
     }
 
   /* Destroy the semaphore */
@@ -217,7 +173,7 @@ int file_dup2(FAR struct file *filep1, FAR struct file *filep2)
    * close the file and release the inode.
    */
 
-  ret = _files_close(filep2);
+  ret = file_close(filep2);
   if (ret < 0)
     {
       /* An error occurred while closing the driver */
@@ -376,7 +332,7 @@ int files_close(int fd)
   ret = _files_semtake(list);
   if (ret >= 0)
     {
-      ret = _files_close(&list->fl_files[fd]);
+      ret = file_close(&list->fl_files[fd]);
       _files_semgive(list);
     }
 
