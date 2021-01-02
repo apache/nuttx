1e5bfa623aa93b918566e8dc0e2f9c1a1037f45e
Xiang Xiao <xiaoxiang@xiaomi.com>
2021-01-03 00:53:46 +0800

    fs: file_dup2 shouldn't hold the file list lock
    the argument passed to file_dup2 doesn't always come from task file list
    so it doesn't make sense to hold the file list lock and then it is better
    to do the protection in the new function files_dupfd2
    
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>
    Change-Id: Ibf02cea9b0b275e7472f9c04fd66b9242285b957

--- fs/inode/inode.h
+++ fs/inode/inode.h
@@ -390,6 +390,20 @@ int files_allocate(FAR struct inode *inode, int oflags, off_t pos,
                    FAR void *priv, int minfd);
 
 /****************************************************************************
+ * Name: files_dupfd2
+ *
+ * Description:
+ *   Clone a file descriptor to a specific descriptor number.
+ *
+ * Returned Value:
+ *   fd2 is returned on success; a negated errno value is return on
+ *   any failure.
+ *
+ ****************************************************************************/
+
+int files_dupfd2(int fd1, int fd2);
+
+/****************************************************************************
  * Name: files_close
  *
  * Description:
