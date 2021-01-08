5b8ca6569fa26ad8706e266700557ed81122e620
ligd <liguiding1@xiaomi.com>
2021-01-08 22:47:43 +0800

    fs/mount: fix mount cmd break out when meet stat error
    Change-Id: I9307fe67837a6519fffa1844fcfd794f735d2fdd
    Signed-off-by: ligd <liguiding1@xiaomi.com>

--- fs/mount/fs_foreachmountpoint.c
+++ fs/mount/fs_foreachmountpoint.c
@@ -106,7 +106,7 @@ static int mountpoint_filter(FAR struct inode *node,
 
       if (pathlen + namlen > PATH_MAX)
         {
-          return -ENAMETOOLONG;
+          return OK;
         }
 
       /* Append the inode name to the directory path */
@@ -115,8 +115,7 @@ static int mountpoint_filter(FAR struct inode *node,
 
       /* Get the status of the file system */
 
-      ret = node->u.i_mops->statfs(node, &statbuf);
-      if (ret == OK)
+      if (node->u.i_mops->statfs(node, &statbuf) == OK)
         {
           /* And pass the full path and file system status to the handler */
 
