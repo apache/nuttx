533a7319d9a0f2b5155d923de6a7dfafb21675dd
Xiang Xiao <xiaoxiang@xiaomi.com>
2021-02-20 20:00:57 +0800

    libc: Move stack check stuff from libc/stdlib/ to libc/assert/
    since the new location is more reasonable
    
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- libs/libc/assert/lib_stackchk.c
+++ libs/libc/assert/lib_stackchk.c
@@ -1,5 +1,5 @@
 /****************************************************************************
- * libs/libc/stdlib/lib_stackchk.c
+ * libs/libc/assert/lib_stackchk.c
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
