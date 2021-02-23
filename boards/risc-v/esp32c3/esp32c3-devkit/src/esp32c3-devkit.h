abab83b216c3c940849a7e9bc47c05f3d8239a5e
Gustavo Henrique Nihei <gustavo.nihei@espressif.com>
2021-02-23 19:17:41 -0300

    boards/esp32c3-devkit: Add support for I2C on board bringup

--- boards/risc-v/esp32c3/esp32c3-devkit/src/esp32c3-devkit.h
+++ boards/risc-v/esp32c3/esp32c3-devkit/src/esp32c3-devkit.h
@@ -86,6 +86,21 @@ int board_wdt_init(void);
 #endif
 
 /****************************************************************************
+ * Name: board_i2c_init
+ *
+ * Description:
+ *   Configure the I2C driver.
+ *
+ * Returned Value:
+ *   Zero (OK) is returned on success; A negated errno value is returned
+ *   to indicate the nature of any failure.
+ *
+ ****************************************************************************/
+#ifdef CONFIG_I2C_DRIVER
+int board_i2c_init(void);
+#endif
+
+/****************************************************************************
  * Name: board_tim_init
  *
  * Description:
