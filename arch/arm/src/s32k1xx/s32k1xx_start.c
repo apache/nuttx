49bd22ec5c2a54ad86714bf458a6911c6f171669
David Sidrane <David.Sidrane@NscDg.com>
2021-02-23 12:25:05 -0800

    s32k1xx:Support ramfunc

--- arch/arm/src/s32k1xx/s32k1xx_start.c
+++ arch/arm/src/s32k1xx/s32k1xx_start.c
@@ -332,6 +332,20 @@ void __start(void)
     }
 #endif
 
+  /* Copy any necessary code sections from FLASH to RAM.  The correct
+   * destination in SRAM is given by _sramfuncs and _eramfuncs.  The
+   * temporary location is in flash after the data initialization code
+   * at _framfuncs.  This should be done before s32k1xx_clockconfig() is
+   * called (in case it has some dependency on initialized C variables).
+   */
+
+#ifdef CONFIG_ARCH_RAMFUNCS
+  for (src = &_framfuncs, dest = &_sramfuncs; dest < &_eramfuncs; )
+    {
+      *dest++ = *src++;
+    }
+#endif
+
   /* Configure the clocking and the console uart so that we can get debug
    * output as soon as possible.  NOTE: That this logic must not assume that
    * .bss or .data have been initialized.
