59649d6e5014d7566f93a0a5e6e18e592b36d2e2
mage1 <mage1@xiaomi.com>
2021-03-11 16:28:55 +0800

    mm: add heap policy and anta logic to support mm debug on sim platform
    since atan tool will enhance memory debug operation.
    
    Change-Id: Ic953755faff156832e84b6a764452751dc14f0e3

--- arch/sim/src/sim/up_internal.h
+++ arch/sim/src/sim/up_internal.h
@@ -224,6 +224,11 @@ void up_longjmp(void *jb, int val);
 void *host_alloc_heap(size_t sz);
 void *host_alloc_shmem(const char *name, size_t size, int master);
 void  host_free_shmem(void *mem);
+void *host_malloc(size_t size);
+void host_free(void *mem);
+void *host_realloc(void *oldmem, size_t size);
+void *host_calloc(size_t n, size_t elem_size);
+void *host_memalign(size_t alignment, size_t size);
 
 /* up_hosttime.c ************************************************************/
 
