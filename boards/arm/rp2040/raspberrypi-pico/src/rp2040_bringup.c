5699dd86ebcbb764e2a0b6244fb236d1b3ab49d6
Yuichi Nakamura <y.512.nakamura@gmail.com>
2021-03-16 00:32:54 +0900

    boards/raspberrypi-pico: Pico Display Pack (ST7789 LCD) support

--- boards/arm/rp2040/raspberrypi-pico/src/rp2040_bringup.c
+++ boards/arm/rp2040/raspberrypi-pico/src/rp2040_bringup.c
@@ -33,6 +33,10 @@
 
 #include "rp2040_pico.h"
 
+#ifdef CONFIG_VIDEO_FB
+#  include <nuttx/video/fb.h>
+#endif
+
 /****************************************************************************
  * Public Functions
  ****************************************************************************/
@@ -119,6 +123,14 @@ int rp2040_bringup(void)
   if (ret < 0)
     {
       syslog(LOG_ERR, "ERROR: rp2040_ina219_initialize() failed: %d\n", ret);
+    }
+#endif
+
+#ifdef CONFIG_VIDEO_FB
+  ret = fb_register(0, 0);
+  if (ret < 0)
+    {
+      _err("ERROR: Failed to initialize Frame Buffer Driver.\n");
     }
 #endif
 
