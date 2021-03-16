e966c30cb79933c6ee90f4f963b9c5e9e8119696
Jiuzhu Dong <dongjiuzhu1@xiaomi.com>
2021-03-16 14:47:11 +0800

    rpserver/rpproxy: support set/get rtc time by rpmsg
    N/A
    
    Change-Id: Ib223fd85f7862241bc3c2dd897b3364d7b0fede3
    Signed-off-by: Jiuzhu Dong <dongjiuzhu1@xiaomi.com>

--- arch/sim/src/sim/up_rptun.c
+++ arch/sim/src/sim/up_rptun.c
@@ -237,6 +237,12 @@ int up_rptun_init(void)
   syslog_rpmsg_server_init();
 #endif
 
+#if CONFIG_SIM_RPTUN_MASTER
+  rpmsg_rtc_server_initialize();
+#else
+  up_rtc_set_lowerhalf(rpmsg_rtc_initialize("server", 0));
+#endif
+
 #ifdef CONFIG_FS_HOSTFS_RPMSG
   hostfs_rpmsg_init("server");
 #endif
