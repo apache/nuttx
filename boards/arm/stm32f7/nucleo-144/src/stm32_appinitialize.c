b9ea326d15c8403909cc4ccba349b778aa1bfb04
Roberto Bucher <roberto.bucher@supsi.ch>
2021-03-21 10:04:15 +0100

    Eliminated double arm_netinitialize function

--- boards/arm/stm32f7/nucleo-144/src/stm32_appinitialize.c
+++ boards/arm/stm32f7/nucleo-144/src/stm32_appinitialize.c
@@ -224,9 +224,3 @@ int board_app_initialize(uintptr_t arg)
   UNUSED(ret);
   return OK;
 }
-
-#if defined(CONFIG_NET) && !defined(CONFIG_NETDEV_LATEINIT)
-void arm_netinitialize(void)
-{
-}
-#endif
