0da84bed89a8c9153f1d528ffb6e5accb6eb3fe7
Roberto Bucher <roberto.bucher@supsi.ch>
2021-03-24 11:24:57 +0100

    Avoid warning related to not used functions in rwbuffer.c

--- drivers/rwbuffer.c
+++ drivers/rwbuffer.c
@@ -71,15 +71,18 @@ static ssize_t rwb_read_(FAR struct rwbuffer_s *rwb, off_t startblock,
  * Name: rwb_semtake
  ****************************************************************************/
 
+#if defined(CONFIG_DRVR_WRITEBUFFER) && CONFIG_DRVR_WRDELAY != 0
 static int rwb_semtake(FAR sem_t *sem)
 {
   return nxsem_wait_uninterruptible(sem);
 }
+#endif
 
 /****************************************************************************
  * Name: rwb_forcetake
  ****************************************************************************/
 
+#if defined(CONFIG_DRVR_WRITEBUFFER) && CONFIG_DRVR_WRDELAY != 0
 static int rwb_forcetake(FAR sem_t *sem)
 {
   int result;
@@ -103,6 +106,7 @@ static int rwb_forcetake(FAR sem_t *sem)
 
   return ret;
 }
+#endif
 
 /****************************************************************************
  * Name: rwb_semgive
