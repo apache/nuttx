da65128b8c1c01e335dbb751eaa18b723d87c988
Alin Jerpelea <alin.jerpelea@sony.com>
2021-03-24 09:14:53 +0100

    arch: arm: armv8-m: fix nxstyle errors
    Fix nxstyle errors to pass CI
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- arch/arm/src/armv8-m/dwt.h
+++ arch/arm/src/armv8-m/dwt.h
@@ -1,11 +1,12 @@
-/***********************************************************************************************
+/****************************************************************************
  * arch/arm/src/armv8-m/dwt.h
  *
  *   Copyright (c) 2009 - 2013 ARM LIMITED
  *
  *  All rights reserved.
  *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions are met:
+ *  modification, are permitted provided that the following conditions
+ *  are met:
  *
  *  - Redistributions of source code must retain the above copyright
  *    notice, this list of conditions and the following disclaimer.
@@ -16,17 +17,17 @@
  *    to endorse or promote products derived from this software without
  *    specific prior written permission.
  *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- *  ARE DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDERS AND CONTRIBUTORS BE
- *  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- *  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- *  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *  POSSIBILITY OF SUCH DAMAGE.
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ *  TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ *  PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDERS
+ *  AND CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ *  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ *  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ *  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ *  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  *   Copyright (C) 2014 Pierre-noel Bouteville . All rights reserved.
  *   Author: Pierre-noel Bouteville <pnb990@gmail.com>
@@ -58,22 +59,22 @@
  * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  *
- ***********************************************************************************************/
+ ****************************************************************************/
 
 #ifndef __ARCH_ARM_SRC_ARMV8_M_DWT_H
 #define __ARCH_ARM_SRC_ARMV8_M_DWT_H
 
-/***********************************************************************************************
+/****************************************************************************
  * Pre-processor Definitions
- ***********************************************************************************************/
+ ****************************************************************************/
 
-/* Data Watchpoint and Trace Register (DWT) Definitions ****************************************/
+/* Data Watchpoint and Trace Register (DWT) Definitions *********************/
 
-/* DWT Register Base Address *******************************************************************/
+/* DWT Register Base Address ************************************************/
 
 #define DWT_BASE                     (0xe0001000ul)
 
-/* DWT Register Addresses **********************************************************************/
+/* DWT Register Addresses ***************************************************/
 
 #define DWT_CTRL                     (DWT_BASE + 0x0000)  /* Control Register */
 #define DWT_CYCCNT                   (DWT_BASE + 0x0004)  /* Cycle Count Register */
@@ -96,7 +97,7 @@
 #define DWT_MASK3                    (DWT_BASE + 0x0054)  /* Mask Register 3 */
 #define DWT_FUNCTION3                (DWT_BASE + 0x0058)  /* Function Register 3 */
 
-/* DWT Register Bit Field Definitions **********************************************************/
+/* DWT Register Bit Field Definitions ***************************************/
 
 /* DWT CTRL */
 
