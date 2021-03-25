203692fa5466d0e7e9639584e6855351f1ce584b
Alin Jerpelea <alin.jerpelea@sony.com>
2021-03-25 07:10:25 +0100

    arch: arm: arm: Author Gregory Nutt: update licenses to Apache
    Gregory Nutt has submitted the SGA and we can migrate the licenses
     to Apache.
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- arch/arm/src/arm/arm_pginitialize.c
+++ arch/arm/src/arm/arm_pginitialize.c
@@ -1,6 +1,5 @@
 /****************************************************************************
  * arch/arm/src/arm/arm_pginitialize.c
- * Initialize the MMU for on-demand paging support.
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
