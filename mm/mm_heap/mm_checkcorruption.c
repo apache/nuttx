d8bc2f2035047c23eb32d161705f2415bd7c856d
ligd <liguiding1@xiaomi.com>
2021-03-29 21:36:46 +0800

    mm/mm_heap: fix compile failed when open DEBUG_MM
    Change-Id: Id21e9619e4800ec9f02f818ec86d80cdd99d9aae
    Signed-off-by: ligd <liguiding1@xiaomi.com>

--- mm/mm_heap/mm_checkcorruption.c
+++ mm/mm_heap/mm_checkcorruption.c
@@ -31,6 +31,8 @@
 #include <nuttx/mm/mm.h>
 #include <nuttx/irq.h>
 
+#include "mm_heap/mm.h"
+
 /****************************************************************************
  * Public Functions
  ****************************************************************************/
@@ -45,8 +47,13 @@
 
 void mm_checkcorruption(FAR struct mm_heap_s *heap)
 {
+  FAR struct mm_heap_impl_s *heap_impl;
   FAR struct mm_allocnode_s *node;
   FAR struct mm_allocnode_s *prev;
+
+  DEBUGASSERT(MM_IS_VALID(heap));
+  heap_impl = heap->mm_impl;
+
 #if CONFIG_MM_REGIONS > 1
   int region;
 #else
@@ -56,7 +63,7 @@ void mm_checkcorruption(FAR struct mm_heap_s *heap)
   /* Visit each region */
 
 #if CONFIG_MM_REGIONS > 1
-  for (region = 0; region < heap->mm_nregions; region++)
+  for (region = 0; region < heap_impl->mm_nregions; region++)
 #endif
     {
       irqstate_t flags = 0;
@@ -69,7 +76,7 @@ void mm_checkcorruption(FAR struct mm_heap_s *heap)
 
       if (up_interrupt_context() || sched_idletask())
         {
-          if (heap->mm_counts_held)
+          if (heap_impl->mm_counts_held)
             {
 #if CONFIG_MM_REGIONS > 1
               continue;
@@ -85,8 +92,8 @@ void mm_checkcorruption(FAR struct mm_heap_s *heap)
           mm_takesemaphore(heap);
         }
 
-      for (node = heap->mm_heapstart[region];
-           node < heap->mm_heapend[region];
+      for (node = heap_impl->mm_heapstart[region];
+           node < heap_impl->mm_heapend[region];
            node = (FAR struct mm_allocnode_s *)
                   ((FAR char *)node + node->size))
         {
@@ -113,7 +120,7 @@ void mm_checkcorruption(FAR struct mm_heap_s *heap)
           prev = node;
         }
 
-      assert(node == heap->mm_heapend[region]);
+      assert(node == heap_impl->mm_heapend[region]);
 
       if (up_interrupt_context() || sched_idletask())
         {
