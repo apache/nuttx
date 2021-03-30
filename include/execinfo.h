27dd771336389b90f5cccf5b369b6a8a48b30379
chao.an <anchao@xiaomi.com>
2021-03-30 17:48:42 +0800

    libc/execinfo: add backtrace support based on EABI Unwinder
    1. Flags < -funwind-tables > is required
    2. Keep the section ".exidx" in linker script
    
    Signed-off-by: chao.an <anchao@xiaomi.com>

---
+++ include/execinfo.h
@@ -0,0 +1,34 @@
+/****************************************************************************
+ * include/execinfo.h
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+#ifndef __INCLUDE_EXECINFO_H
+#define __INCLUDE_EXECINFO_H
+
+/****************************************************************************
+ * Public Function Prototypes
+ ****************************************************************************/
+
+/* Store up to SIZE return address of the current program state in
+ * ARRAY and return the exact number of values stored.
+ */
+
+extern int backtrace(FAR void **buffer, int size);
+
+#endif /* __INCLUDE_EXECINFO_H */
