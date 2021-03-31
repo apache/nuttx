f5f4440742a893508790ad1bd79e38163d5b96f5
ligd <liguiding1@xiaomi.com>
2021-03-31 22:11:59 +0800

    sim: remove CONFIG_SIM_WALLTIME fast timing maybe harmful for IOs
    Change-Id: Ic4ff5ec0aa99d2d229a07c4d1a9a4dae738d3cd9
    Signed-off-by: ligd <liguiding1@xiaomi.com>

--- arch/sim/src/sim/up_simsmp.c
+++ arch/sim/src/sim/up_simsmp.c
@@ -94,9 +94,7 @@ extern uint8_t g_nx_initstate;
 static void *sim_idle_trampoline(void *arg)
 {
   struct sim_cpuinfo_s *cpuinfo = (struct sim_cpuinfo_s *)arg;
-#ifdef CONFIG_SIM_WALLTIME
   uint64_t now = 0;
-#endif
   int ret;
 
   /* Set the CPU number for the CPU thread */
@@ -131,16 +129,10 @@ static void *sim_idle_trampoline(void *arg)
 
   for (; ; )
     {
-#ifdef CONFIG_SIM_WALLTIME
       /* Wait a bit so that the timing is close to the correct rate. */
 
       now += 1000 * CONFIG_USEC_PER_TICK;
       host_sleepuntil(now);
-#else
-      /* Give other pthreads/CPUs a shot */
-
-      sched_yield();
-#endif
     }
 
   return NULL;
