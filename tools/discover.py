2634d304e9a348b99f6ef133e7405608016c8ab9
Brennan Ashton <bashton@brennanashton.com>
2021-04-04 16:54:09 -0700

    Port tools/discover.py to py3 and lint

--- tools/discover.py
+++ tools/discover.py
@@ -26,58 +26,64 @@ from socket import *
 PORT = 96
 
 DISCOVER_PROTO_ID = 0x99
-DISCOVER_ALL = 0xff         # 0xff means all devices
+DISCOVER_ALL = 0xFF  # 0xff means all devices
 DISCOVER_REQUEST = 0x01
 DISCOVER_RESPONSE = 0x02
 DISCOVER_REQUEST_SIZE = 4
 DISCOVER_RESPONSE_SIZE = 35
 
+
 def check_sum(data):
     chksum = 0
     for c in data[:-1]:
         chksum -= c
-    return (chksum & 0xff) == data[-1]
+    return (chksum & 0xFF) == data[-1]
+
 
 def send_discover(socket):
-    cmd = array.array('B', [0] * DISCOVER_REQUEST_SIZE)
-    cmd[0] = DISCOVER_PROTO_ID # Tag for identification of the protocol
+    cmd = array.array("B", [0] * DISCOVER_REQUEST_SIZE)
+    cmd[0] = DISCOVER_PROTO_ID  # Tag for identification of the protocol
     cmd[1] = DISCOVER_REQUEST  # Request command
     cmd[2] = DISCOVER_ALL
     chksum = 0
     for c in cmd[:3]:
         chksum -= c
-    cmd[3] = chksum & 0xff
+    cmd[3] = chksum & 0xFF
 
-    socket.sendto(cmd, ('<broadcast>', PORT))
+    socket.sendto(cmd, ("<broadcast>", PORT))
+
 
 def read_responses(socket):
     res = []
-    response = array.array('B', [0] * DISCOVER_RESPONSE_SIZE)
+    response = array.array("B", [0] * DISCOVER_RESPONSE_SIZE)
     try:
         while 1:
             size, src = socket.recvfrom_into(response)
-            if (size == DISCOVER_RESPONSE_SIZE
+            if (
+                size == DISCOVER_RESPONSE_SIZE
                 and response[0] == DISCOVER_PROTO_ID
                 and response[1] == DISCOVER_RESPONSE
-                and check_sum(response)):
+                and check_sum(response)
+            ):
 
                 dev = {}
-                dev['addr'] = src[0]
-                dev['descr'] = response[2:-1].tostring().rstrip('\0')
+                dev["addr"] = src[0]
+                dev["descr"] = response[2:-1].tostring().rstrip("\0")
                 res.append(dev)
 
     except timeout:
         return res
 
-if __name__ == '__main__':
-    print 'Sending discover...'
+
+if __name__ == "__main__":
+    print("Sending discover...")
 
     s = socket(AF_INET, SOCK_DGRAM)
-    s.bind(('0.0.0.0', PORT))
+    s.bind(("0.0.0.0", PORT))
     s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
     s.settimeout(1.0)
     send_discover(s)
     devices = read_responses(s)
     socket.close(s)
 
-    print devices
+    print(devices)
