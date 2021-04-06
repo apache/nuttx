72041911ce8d78046a5c5849c263f18e4392ea48
Alin Jerpelea <alin.jerpelea@sony.com>
2021-04-06 10:57:38 +0200

    NuttX: Marco Krahl: update licenses to Apache
    Marco Krahl has submitted the ICLA and we can migrate the licenses
     to Apache.
    
    Gregory Nutt, S.A has submitted the SGA and we can migrate the licenses
     to Apache.
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- boards/arm/stm32/stm32f429i-disco/src/stm32_ili93414ws.c
+++ boards/arm/stm32/stm32f429i-disco/src/stm32_ili93414ws.c
@@ -1,45 +1,29 @@
 /****************************************************************************
  * boards/arm/stm32/stm32f429i-disco/src/stm32_ili93414ws.c
  *
- * Driver for the ILI9341 Single Chip LCD driver connected
- * via 4 wire serial (spi) mcu interface
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
  *
- *   Copyright (C) 2014 Marco Krahl. All rights reserved.
- *   Author: Marco Krahl <ocram.lhark@gmail.com>
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * References: ILI9341_DS_V1.10.pdf (Rev: 1.10), "a-Si TFT LCD Single Chip
- *             Driver 240RGBx320 Resolution and 262K color", ILI TECHNOLOGY
- *             CORP., http://www.ilitek.com.
- *             ILI TECHNOLOGY CORP., http://www.ilitek.com.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- * 3. Neither the name NuttX nor the names of its contributors may be
- *    used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
- * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
- * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
  *
  ****************************************************************************/
+
+/* References:
+ * ILI9341_DS_V1.10.pdf (Rev: 1.10), "a-Si TFT LCD Single Chip
+ * Driver 240RGBx320 Resolution and 262K color", ILI TECHNOLOGY
+ * CORP., http://www.ilitek.com.
+ * ILI TECHNOLOGY CORP., http://www.ilitek.com.
+ */
 
 /****************************************************************************
  * Included Files
