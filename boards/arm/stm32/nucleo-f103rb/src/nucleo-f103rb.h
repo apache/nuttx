ccd938a400a21707ea051e083fbd1dab18282fb6
raiden00pl <raiden00@railab.me>
2021-04-10 19:17:58 +0200

    nucleo-f103rb: refactor bringup logic for consistency with other boards

--- boards/arm/stm32/nucleo-f103rb/src/nucleo-f103rb.h
+++ boards/arm/stm32/nucleo-f103rb/src/nucleo-f103rb.h
@@ -82,6 +82,26 @@
  ****************************************************************************/
 
 /****************************************************************************
+ * Name: stm32_bringup
+ *
+ * Description:
+ *   Perform architecture specific initialization
+ *
+ *   CONFIG_LIB_BOARDCTL=y:
+ *     If CONFIG_NSH_ARCHINITIALIZE=y:
+ *       Called from the NSH library (or other application)
+ *     Otherwise, assumed to be called from some other application.
+ *
+ *   Otherwise CONFIG_BOARD_LATE_INITIALIZE=y:
+ *     Called from board_late_initialize().
+ *
+ *   Otherwise, bad news:  Never called
+ *
+ ****************************************************************************/
+
+int stm32_bringup(void);
+
+/****************************************************************************
  * Name: stm32_pwm_setup
  *
  * Description:
