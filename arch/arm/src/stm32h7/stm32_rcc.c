e825e17fc26223b889b72d49f3dc95688441bb22
Anthony Merlino <anthony@vergeaero.com>
2021-04-10 20:09:24 -0400

    stm32h7: Add support for IO compensation.

--- arch/arm/src/stm32h7/stm32_rcc.c
+++ arch/arm/src/stm32h7/stm32_rcc.c
@@ -35,6 +35,7 @@
 #include "arm_arch.h"
 
 #include "hardware/stm32_flash.h"
+#include "stm32_gpio.h"
 #include "stm32_rcc.h"
 #include "stm32_pwr.h"
 
@@ -120,6 +121,12 @@ void stm32_clockconfig(void)
   /* Enable peripheral clocking */
 
   rcc_enableperipherals();
+
+#ifdef CONFIG_STM32H7_SYSCFG_IOCOMPENSATION
+  /* Enable I/O Compensation */
+
+  stm32_iocompensation();
+#endif
 }
 
 /****************************************************************************
