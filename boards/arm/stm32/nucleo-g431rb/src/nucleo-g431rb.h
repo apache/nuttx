5e59d3753ab72fafb2339cb64895e4b712960bca
raiden00pl <raiden00@railab.me>
2021-04-14 15:08:25 +0200

    nucleo-g431rb: fixes for button

--- boards/arm/stm32/nucleo-g431rb/src/nucleo-g431rb.h
+++ boards/arm/stm32/nucleo-g431rb/src/nucleo-g431rb.h
@@ -57,6 +57,24 @@
 
 #define LED_DRIVER_PATH                "/dev/userleds"
 
+/* Button definitions *******************************************************/
+
+/* The Nucleo G431RB supports two buttons; only one button is controllable
+ * by software:
+ *
+ *   B1 USER:  user button connected to the I/O PC13 of the STM32G431RB.
+ *   B2 RESET: push button connected to NRST is used to RESET the
+ *             STM32G431R.
+ *
+ * NOTE that EXTI interrupts are configured.
+ */
+
+#define MIN_IRQBUTTON  BUTTON_USER
+#define MAX_IRQBUTTON  BUTTON_USER
+#define NUM_IRQBUTTONS 1
+
+#define GPIO_BTN_USER  (GPIO_INPUT|GPIO_FLOAT|GPIO_EXTI|GPIO_PORTC|GPIO_PIN13)
+
 /* PWM */
 
 #define NUCLEOG431RB_PWMTIMER   1
