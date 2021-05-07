8ec76ce297445ab8535ff1a64087c18df4009d5d
liuhaitao <liuhaitao@xiaomi.com>
2021-05-07 22:22:01 +0800

    Config.mk: restore ARCHIVE to fix build break
    Change-Id: Iafae4f4c48afb0df8b169ec1106e348cc4c0fc9e
    Signed-off-by: liuhaitao <liuhaitao@xiaomi.com>

--- tools/Config.mk
+++ tools/Config.mk
@@ -337,9 +337,7 @@ endef
 # created from scratch
 
 define ARCHIVE
-	@echo "AR (create): ${shell basename $(1)} $(2)"
-	$(Q) $(RM) $1
-	$(Q) $(AR) $1 $(2)
+	$(AR) $1 $(2)
 endef
 
 # PRELINK - Prelink a list of files
