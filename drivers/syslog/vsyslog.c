3c33d253b47720fb98c36b1ba70e5269c824418f
Xiang Xiao <xiaoxiang@xiaomi.com>
2021-05-17 22:38:22 +0800

    drivers/syslog: Remove emergstream implementation and caller
    since emergstream always call syslog_force, but syslog_force is designed
    for the interrupt context, and then doesn't mean any emergency thing.
    On the other hand, emergstream has other bad side effect:
    1.Can't output to file or dev channel
    2.Can't work well with the interrupt buffer
    
    Change-Id: I1c7af74f82da93ffa4ebe0e6ac25634aeb73a890
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>
    Signed-off-by: Jiuzhu Dong <dongjiuzhu1@xiaomi.com>

--- drivers/syslog/vsyslog.c
+++ drivers/syslog/vsyslog.c
@@ -116,22 +116,10 @@ int nx_vsyslog(int priority, FAR const IPTR char *fmt, FAR va_list *ap)
 #endif
 
   /* Wrap the low-level output in a stream object and let lib_vsprintf
-   * do the work.  NOTE that emergency priority output is handled
-   * differently.. it will use the SYSLOG emergency stream.
+   * do the work.
    */
 
-  if (priority == LOG_EMERG)
-    {
-      /* Use the SYSLOG emergency stream */
-
-      emergstream(&stream.public);
-    }
-  else
-    {
-      /* Use the normal SYSLOG stream */
-
-      syslogstream_create(&stream);
-    }
+  syslogstream_create(&stream);
 
 #if defined(CONFIG_SYSLOG_TIMESTAMP)
   /* Prepend the message with the current time, if available */
@@ -235,10 +223,7 @@ int nx_vsyslog(int priority, FAR const IPTR char *fmt, FAR va_list *ap)
 #ifdef CONFIG_SYSLOG_BUFFER
   /* Flush and destroy the syslog stream buffer */
 
-  if (priority != LOG_EMERG)
-    {
-      syslogstream_destroy(&stream);
-    }
+  syslogstream_destroy(&stream);
 #endif
 
   return ret;
