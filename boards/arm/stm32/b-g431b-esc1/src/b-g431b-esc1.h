36261ceebe792eb8da0e191d76fe01b0aa1f0521
raiden00pl <raiden00@railab.me>
2021-05-28 09:22:49 +0200

    boards/b-g431b-esc1: add support for FOC configurations

--- boards/arm/stm32/b-g431b-esc1/src/b-g431b-esc1.h
+++ boards/arm/stm32/b-g431b-esc1/src/b-g431b-esc1.h
@@ -98,4 +98,28 @@
 
 int stm32_bringup(void);
 
+/****************************************************************************
+ * Name: stm32_adc_setup
+ *
+ * Description:
+ *   Initialize ADC and register the ADC driver.
+ *
+ ****************************************************************************/
+
+#ifdef CONFIG_ADC
+int stm32_adc_setup(void);
+#endif
+
+/****************************************************************************
+ * Name: stm32_foc_setup
+ *
+ * Description:
+ *  Initialize FOC peripheral for the board.
+ *
+ ****************************************************************************/
+
+#ifdef CONFIG_STM32_FOC
+int stm32_foc_setup(void);
+#endif
+
 #endif /* __BOARDS_ARM_STM32_B_G431B_ESC1_SRC_B_G431B_ESC1_H */
