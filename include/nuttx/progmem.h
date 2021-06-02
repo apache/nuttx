1518242bb6cd29be31aab65779f185d7c9151b80
Anthony Merlino <anthony@vergeaero.com>
2021-06-02 19:02:38 -0400

    progmem.h: Modify interface to avoid terms like page, sector, and block, as these terms are used interchangeably by different silicon vendors.

--- include/nuttx/progmem.h
+++ include/nuttx/progmem.h
@@ -49,96 +49,119 @@ extern "C"
  * Public Functions Definitions
  ****************************************************************************/
 
+/* NOTE: Since vendors use the terms page, block, and sector interchangably,
+ * it causes confusion and therefore a neutral term is used. We use the term
+ * "index" to refer to the ordinal number of erasable units of memory.
+ */
+
 /****************************************************************************
- * Name: up_progmem_neraseblocks
+ * Name: up_progmem_maxeraseindex
  *
  * Description:
- *   Return number of erase blocks
+ *   Return the total number of erasable units of memory.
  *
  ****************************************************************************/
 
-size_t up_progmem_neraseblocks(void);
+size_t up_progmem_maxeraseindex(void);
 
 /****************************************************************************
  * Name: up_progmem_isuniform
  *
  * Description:
- *  Is program memory uniform or erase page and read/write page size differs?
+ *  Are all erasable units of memory the same size?
+ *
+ * Returned Value:
+ *   true if all erasable units are the same size, false if they vary in size
  *
  ****************************************************************************/
 
 bool up_progmem_isuniform(void);
 
 /****************************************************************************
- * Name: up_progmem_pagesize
+ * Name: up_progmem_writegranularity
  *
  * Description:
- *   Return read/write page size
+ *   Return the number of bytes of the smallest allowable write operation.
+ *   All writes MUST be a multiple of the granularity. Some FLASH memories
+ *   require write operations in multiples of 8-bit, 16-bit, 32-bit, 128-bit,
+ *   256-bit, etc. The caller must ensure the size of the data passed into
+ *   up_progmem_write is an even multiple of the writegranularity. Padding
+ *   may be required.
+ *
+ * Returned Value:
+ *   Required granularity of a write operation for the FLASH memory.
  *
  ****************************************************************************/
 
-size_t up_progmem_pagesize(size_t page);
+size_t up_progmem_writegranularity(void);
 
 /****************************************************************************
  * Name: up_progmem_erasesize
  *
  * Description:
- *  Return erase block size. Must be a multiple of the read/write page size.
+ *   Return the erase size for a specified index. Will always be a multiple of
+ *   the write granularity.
+ *
+ * Input Parameters:
+ *   index - ordinal number of the set of erasable units of memory
+ *
+ * Returned Value:
+ *   Erase size for erasable unit of memory, SIZE_MAX if index is invalid.
  *
  ****************************************************************************/
 
-size_t up_progmem_erasesize(size_t block);
+size_t up_progmem_erasesize(size_t index);
 
 /****************************************************************************
- * Name: up_progmem_getpage
+ * Name: up_progmem_getindex
  *
  * Description:
- *   Address to read/write page conversion
+ *   Get the index of memory, given a FLASH memory address
  *
  * Input Parameters:
  *  addr - Address with or without flash offset
- *         (absolute or aligned to page0)
+ *         (absolute or aligned to sector0)
  *
  * Returned Value:
- *   Page or negative value on error.
+ *   Index or negative value on error.
  *   The following errors are reported (errno is not set!):
  *
  *     -EFAULT: On invalid address
  *
  ****************************************************************************/
 
-ssize_t up_progmem_getpage(size_t addr);
+ssize_t up_progmem_getindex(size_t addr);
 
 /****************************************************************************
  * Name: up_progmem_getaddress
  *
  * Description:
- *   Read/write page to address conversion
+ *   Get the base address of a unit of memory, given an index
  *
  * Input Parameters:
- *   page - page index
+ *   index - ordinal number of the set of erasable units of memory
  *
  * Returned Value:
- *   Base address of given page, SIZE_MAX if page index is not valid.
+ *   Base address of given section, SIZE_MAX if index is not valid.
  *
  ****************************************************************************/
 
-size_t up_progmem_getaddress(size_t page);
+size_t up_progmem_getaddress(size_t index);
 
 /****************************************************************************
- * Name: up_progmem_eraseblock
+ * Name: up_progmem_erase
  *
  * Description:
- *   Erase selected erase block.
+ *   Erase a specified unit of memory, given an index
  *
  * Input Parameters:
- *   block - The erase block index to be erased.
+ *   index - ordinal number of the set of erasable units of memory
  *
  * Returned Value:
- *   block size or negative value on error.
+ *   section size or negative value on error.
  *   The following errors are reported (errno is not set!):
  *
- *     -EFAULT: On invalid page
+ *     -EFAULT: On invalid section
  *     -EIO:    On unsuccessful erase
  *     -EROFS:  On access to write protected area
  *     -EACCES: Insufficient permissions (read/write protected)
@@ -147,27 +170,27 @@ size_t up_progmem_getaddress(size_t page);
  *
  ****************************************************************************/
 
-ssize_t up_progmem_eraseblock(size_t block);
+ssize_t up_progmem_erase(size_t index);
 
 /****************************************************************************
- * Name: up_progmem_ispageerased
+ * Name: up_progmem_issectionerased
  *
  * Description:
- *   Checks whether erase page is erased
+ *   Checks whether a specified unit of memory is erased
  *
  * Input Parameters:
- *   page - The page index to be checked.
+ *   index - ordinal number of the set of erasable units of memory
  *
  * Returned Value:
  *   Returns number of bytes NOT erased or negative value on error. If it
- *   returns zero then complete page is erased.
+ *   returns zero then complete section is erased.
  *
  *   The following errors are reported:
- *     -EFAULT: On invalid page
+ *     -EFAULT: On invalid index
  *
  ****************************************************************************/
 
-ssize_t up_progmem_ispageerased(size_t page);
+ssize_t up_progmem_iserased(size_t index);
 
 /****************************************************************************
  * Name: up_progmem_write
@@ -175,12 +198,14 @@ ssize_t up_progmem_ispageerased(size_t page);
  * Description:
  *   Program data at given address
  *
- *   Note: this function is not limited to single page and nor it requires
- *   the address be aligned inside the page boundaries.
+ *   Note: This function may cross memory boundaries so long as the caller
+ *         has ensured all units of memory that will be programmed have been
+ *         erased. It also does not require that the write be aligned with the
+ *         beginning of a unit of memory.
  *
  * Input Parameters:
  *   addr  - Address with or without flash offset
- *           (absolute or aligned to page0)
+ *           (absolute or aligned to sector0)
  *   buf   - Pointer to buffer
  *   count - Number of bytes to write
  *
@@ -188,7 +213,7 @@ ssize_t up_progmem_ispageerased(size_t page);
  *   Bytes written or negative value on error.  The following errors are
  *   reported (errno is not set!)
  *
- *     EINVAL: If count is not aligned with the flash boundaries (i.e.
+ *     EINVAL: If count is not aligned with the write granularity (i.e.
  *             some MCU's require per half-word or even word access)
  *     EFAULT: On invalid address
  *     EIO:    On unsuccessful write
@@ -207,12 +232,14 @@ ssize_t up_progmem_write(size_t addr, FAR const void *buf, size_t count);
  * Description:
  *   Read data at given address
  *
- *   Note: this function is not limited to single page and nor it requires
- *   the address be aligned inside the page boundaries.
+ *   NOTE: This function does not require the address to be aligned with a
+ *         unit of memory. It is also not limited to single unit of memory
+ *         (i.e. multiple units of memory may be read, including partial unit
+ *         reads)
  *
  * Input Parameters:
  *   addr  - Address with or without flash offset
- *           (absolute or aligned to page0)
+ *           (absolute or aligned to sector0)
  *   buf   - Pointer to buffer
  *   count - Number of bytes to read
  *
@@ -233,6 +260,67 @@ ssize_t up_progmem_write(size_t addr, FAR const void *buf, size_t count);
 #ifdef CONFIG_ARCH_HAVE_PROGMEM_READ
 ssize_t up_progmem_read(size_t addr, FAR void *buf, size_t count);
 #endif
+
+/****************************************************************************
+ * Name: up_progmem_enableprogramming
+ *
+ * Description:
+ *   Enable the ability to erase/write the FLASH memory.
+ *
+ *   NOTE: Write-protection may still be required for an individual
+ *         unit of memory.
+ *
+ * Returned Value:
+ *   0 on success, or negative value on error.
+ *
+ ****************************************************************************/
+
+int up_progmem_enableprogramming(void);
+
+/****************************************************************************
+ * Name: up_progmem_disableprogramming
+ *
+ * Description:
+ *   Disable the ability to erase/write the FLASH memory.
+ *
+ * Returned Value:
+ *   0 on success, or negative value on error.
+ *
+ ****************************************************************************/
+
+int up_progmem_disableprogramming(void);
+
+/****************************************************************************
+ * Name: up_progmem_enablewriteprotect
+ *
+ * Description:
+ *   Enable write protection for the specified unit of memory
+ *
+ * Input Parameters:
+ *   index - ordinal number of the set of erasable units of memory
+ *
+ * Returned Value:
+ *   0 on success, or negative value on error.
+ *
+ ****************************************************************************/
+
+int up_progmem_enablewriteprotect(size_t index);
+
+/****************************************************************************
+ * Name: up_progmem_disablewriteprotect
+ *
+ * Description:
+ *   Disable write protection for the specified unit of memory
+ *
+ * Input Parameters:
+ *   index - ordinal number of the set of erasable units of memory
+ *
+ * Returned Value:
+ *   0 on success, or negative value on error.
+ *
+ ****************************************************************************/
+
+int up_progmem_disablewriteprotect(size_t index);
 
 #undef EXTERN
 #if defined(__cplusplus)
