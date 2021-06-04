81954fc6449176a71842220f2b8b9859f5965224
Peter van der Perk <peter.vanderperk@nxp.com>
2021-06-04 15:53:31 +0200

    UCANS32K146 add support for B revision

--- boards/arm/s32k1xx/rddrone-uavcan146/src/s32k1xx_bringup.c
+++ boards/arm/s32k1xx/rddrone-uavcan146/src/s32k1xx_bringup.c
@@ -23,7 +23,7 @@
  ****************************************************************************/
 
 #include <nuttx/config.h>
-
+#include <arch/board/board.h>
 #include <sys/types.h>
 #include <syslog.h>
 
@@ -145,6 +145,24 @@ int s32k1xx_bringup(void)
   /* Register EEEPROM block device */
 
   s32k1xx_eeeprom_register(0, 4096);
+#endif
+
+#ifdef CONFIG_S32K1XX_FLEXCAN
+  s32k1xx_pinconfig(BOARD_REVISION_DETECT_PIN);
+
+  if (s32k1xx_gpioread(BOARD_REVISION_DETECT_PIN))
+    {
+      /* STB high -> active CAN phy */
+
+      s32k1xx_pinconfig(PIN_CAN0_STB  | GPIO_OUTPUT_ONE);
+    }
+  else
+    {
+      /* STB low -> active CAN phy */
+
+      s32k1xx_pinconfig(PIN_CAN0_STB  | GPIO_OUTPUT_ZERO);
+    }
+
 #endif
 
   return ret;
