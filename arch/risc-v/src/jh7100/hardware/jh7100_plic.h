b983897a1408e637763ae0c9b1ea837517f2d9c6
Robert Lipe <robertlipe@gmail.com>
2021-06-22 07:16:09 -0500

    Add rest of BeagleV Starlight JH7100 and JH7110 work.
    Largely borrowed from C906.
    Tested on pre-release hardware and on Renode.

---
+++ arch/risc-v/src/jh7100/hardware/jh7100_plic.h
@@ -0,0 +1,43 @@
+/****************************************************************************
+ * arch/risc-v/src/jh7100/hardware/jh7100_plic.h
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+#ifndef __ARCH_RISCV_SRC_jh7100_HARDWARE_JH7100_PLIC_H
+#define __ARCH_RISCV_SRC_jh7100_HARDWARE_JH7100_PLIC_H
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+#define JH7100_PLIC_PRIORITY    (JH7100_PLIC_BASE + 0x000000) // Zero is invalid. Starts at 1.
+#define JH7100_PLIC_IP0         (JH7100_PLIC_BASE + 0x001000) // Interrupt Pending.
+#define JH7100_PLIC_IP1         (JH7100_PLIC_BASE + 0x001004)
+#define JH7100_PLIC_MIE0        (JH7100_PLIC_BASE + 0x002000)
+#define JH7100_PLIC_MIE1        (JH7100_PLIC_BASE + 0x002004)
+#define JH7100_PLIC_SIE0        (JH7100_PLIC_BASE + 0x002080)
+#define JH7100_PLIC_SIE1        (JH7100_PLIC_BASE + 0x002084)
+
+/* This exists in D1, but not U74 */
+// #define JH7100_PLIC_CTRL        (JH7100_PLIC_BASE + 0x1FFFFC)
+#define JH7100_PLIC_MTHRESHOLD  (JH7100_PLIC_BASE + 0x200000)
+#define JH7100_PLIC_MCLAIM      (JH7100_PLIC_BASE + 0x200004)
+#define JH7100_PLIC_STHRESHOLD  (JH7100_PLIC_BASE + 0x201000)
+#define JH7100_PLIC_SCLAIM      (JH7100_PLIC_BASE + 0x201004)
+
+#endif /* __ARCH_RISCV_SRC_jh7100_HARDWARE_JH7100_PLIC_H */
---
+++ arch/risc-v/src/jh7100/hardware/jh7100_uart.h
@@ -0,0 +1,93 @@
+/****************************************************************************
+ * arch/risc-v/src/jh7100/hardware/jh7100_uart.h
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+#ifndef ARCH_RISCV_SRC_JH7100_CHIP_JH7100_UART_H
+#define ARCH_RISCV_SRC_JH7100_CHIP_JH7100_UART_H
+
+/* The JH7100 and JH7110 have a 16550-compatible UART. Refactor this away. */
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+#define UART_TXDATA_OFFSET   0x00
+#define UART_RXDATA_OFFSET   0x00
+#define UART_TXCTL_OFFSET    0x04
+#define UART_RXCTL_OFFSET    0x04
+#define UART_IE_OFFSET       0x04
+#define UART_IIR_OFFSET      0x08
+#define UART_DIV_OFFSET      0x04
+#define UART_LSR_OFFSET      0x14
+#define UART_MSR_OFFSET      0x18
+#define UART_USR_OFFSET      0x7c
+
+#define CONFIG_UART0_SERIAL_CONSOLE 1
+#define CONFIG_JH7100_UART0 1
+
+#ifdef CONFIG_JH7100_UART0
+#  define JH7100_UART0_TXDATA  (JH7100_UART0_BASE + UART_TXDATA_OFFSET)
+#  define JH7100_UART0_RXDATA  (JH7100_UART0_BASE + UART_RXDATA_OFFSET)
+#  define JH7100_UART0_TXCTRL  (JH7100_UART0_BASE + UART_TXCTRL_OFFSET)
+#  define JH7100_UART0_RXCTRL  (JH7100_UART0_BASE + UART_RXCTRL_OFFSET)
+#  define JH7100_UART0_IE      (JH7100_UART0_BASE + UART_IE_OFFSET)
+#  define JH7100_UART0_IP      (JH7100_UART0_BASE + UART_IP_OFFSET)
+#  define JH7100_UART0_DIV     (JH7100_UART0_BASE + UART_DIV_OFFSET)
+#  define JH7100_UART0_LSR     (JH7100_UART0_BASE + UART_LSR_OFFSET)
+#  define JH7100_UART0_MSR     (JH7100_UART0_BASE + UART_MSR_OFFSET)
+#  define JH7100_UART0_USR     (JH7100_UART0_BASE + UART_USR_OFFSET)
+#endif
+
+#define USR_UART_BUSY           0x01
+#define USR_UART_TFE            0x04
+#define USR_UART_RFNE           0x08
+#define LSR_DATA_READY          0x01
+#define LSR_THR_EMPTY           0x20
+#define IER_RDA_INT_ENABLE      0x01
+#define IER_THRE_INT_ENABLE     0x02
+#define IIR_RECV_LINE_ENABLE    0x04
+#define IIR_NO_ISQ_PEND         0x01
+
+#define LCR_SET_DLAB            0x80   /* enable r/w DLR to set the baud rate */
+#define LCR_PARITY_ENABLE       0x08   /* parity enabled */
+#define LCR_PARITY_EVEN         0x10   /* Even parity enabled */
+#define LCR_PARITY_ODD          0xef   /* Odd parity enabled */
+#define LCR_WORD_SIZE_5         0xfc   /* the data length is 5 bits */
+#define LCR_WORD_SIZE_6         0x01   /* the data length is 6 bits */
+#define LCR_WORD_SIZE_7         0x02   /* the data length is 7 bits */
+#define LCR_WORD_SIZE_8         0x03   /* the data length is 8 bits */
+#define LCR_STOP_BIT1           0xfb   /* 1 stop bit */
+#define LCR_STOP_BIT2           0x04   /* 1.5 stop bit */
+
+#define DW_LSR_PFE              0x80
+#define DW_LSR_TEMT             0x40
+#define DW_LSR_THRE             0x40
+#define DW_LSR_BI               0x10
+#define DW_LSR_FE               0x08
+#define DW_LSR_PE               0x04
+#define DW_LSR_OE               0x02
+#define DW_LSR_DR               0x01
+#define DW_LSR_TRANS_EMPTY      0x20
+
+#define DW_IIR_THR_EMPTY        0x02    /* threshold empty */
+#define DW_IIR_RECV_DATA        0x04    /* received data available */
+#define DW_IIR_RECV_LINE        0x06    /* receiver line status */
+#define DW_IIR_CHAR_TIMEOUT     0x0c    /* character timeout */
+
+#endif /* ARCH_RISCV_SRC_JH7100_CHIP_JH7100_UART_H */
