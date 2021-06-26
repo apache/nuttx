8ebf9c92cf244116d9c9cd6e12b4427771ea9ec0
Xiang Xiao <xiaoxiang@xiaomi.com>
2021-06-27 04:10:56 +0800

    arch/sim: Implement malloc_size for the custom heap
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- arch/sim/src/sim/up_hostmemory.c
+++ arch/sim/src/sim/up_hostmemory.c
@@ -31,6 +31,12 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 
+#ifdef __APPLE__
+#include <malloc/malloc.h>
+#else
+#include <malloc.h>
+#endif
+
 #include "up_internal.h"
 
 /****************************************************************************
@@ -116,6 +122,15 @@ void *host_alloc_shmem(const char *name, size_t size, int master)
 void host_free_shmem(void *mem)
 {
   munmap(mem, 0);
+}
+
+size_t host_malloc_size(void *mem)
+{
+#ifdef __APPLE__
+  return malloc_size(mem);
+#else
+  return malloc_usable_size(mem);
+#endif
 }
 
 void *host_memalign(size_t alignment, size_t size)
