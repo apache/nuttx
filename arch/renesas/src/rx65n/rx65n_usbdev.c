707404d415de3148f661394a970a8aa5f0cf105e
Masanari Iida <standby24x7@gmail.com>
2021-07-03 20:56:03 +0900

    rx65n: Add missing parameters in printf
    Printf format string requires 2 parameters, but 0
    parameter was given. Add these missing parameters.
    
    Signed-off-by: Masanari Iida <standby24x7@gmail.com>

--- arch/renesas/src/rx65n/rx65n_usbdev.c
+++ arch/renesas/src/rx65n/rx65n_usbdev.c
@@ -2889,7 +2889,7 @@ static int rx65n_epconfigure(FAR struct usbdev_ep_s *ep,
   if (!ep || !desc)
     {
       usbtrace(TRACE_DEVERROR(RX65N_TRACEERR_INVALIDPARMS), 0);
-      printf("ERROR: ep=%p desc=%p\n");
+      printf("ERROR: ep=%p desc=%p\n", ep, desc);
       return -EINVAL;
     }
 #endif
