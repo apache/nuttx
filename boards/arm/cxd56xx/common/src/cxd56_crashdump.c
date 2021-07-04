751f7973e9ce1de810aa3c6ab032e9fd03599fa7
SPRESENSE <41312067+SPRESENSE@users.noreply.github.com>
2021-07-04 12:25:31 +0900

    boards: cxd56xx: Fix crashdump compile error in SMP
    In CONFIG_SMP=y, use arm_intstack_top() instead of g_intstacktop
    to get the base address of interrupt stack.

--- boards/arm/cxd56xx/common/src/cxd56_crashdump.c
+++ boards/arm/cxd56xx/common/src/cxd56_crashdump.c
@@ -189,7 +189,11 @@ void board_crashdump(uintptr_t currentsp, FAR void *tcb,
 #if CONFIG_ARCH_INTERRUPTSTACK > 3
   /* Get the limits on the interrupt stack memory */
 
+#ifdef CONFIG_SMP
+  pdump->info.stacks.interrupt.top = (uint32_t)arm_intstack_top();
+#else
   pdump->info.stacks.interrupt.top = (uint32_t)&g_intstacktop;
+#endif
   pdump->info.stacks.interrupt.size = (CONFIG_ARCH_INTERRUPTSTACK & ~3);
 
   /* If In interrupt Context save the interrupt stack data centered
