a0f2f6c36243ea6afabcae3138128b790c1f8577
Xiang Xiao <xiaoxiang@xiaomi.com>
2021-07-11 17:44:25 +0800

    fs: Move umask to task_info_s
    and implement the related semantic correctly
    
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>
    Change-Id: I0587341e2c505e41b5e24e55dba039ed200d65f2

--- fs/mqueue/mq_open.c
+++ fs/mqueue/mq_open.c
@@ -24,6 +24,7 @@
 
 #include <nuttx/config.h>
 
+#include <sys/stat.h>
 #include <stdbool.h>
 #include <stdarg.h>
 #include <stdio.h>
@@ -187,6 +188,8 @@ static int file_mq_vopen(FAR struct file *mq, FAR const char *mq_name,
       mode = va_arg(ap, mode_t);
       attr = va_arg(ap, FAR struct mq_attr *);
     }
+
+  mode &= ~getumask();
 
   /* Skip over any leading '/'.  All message queue paths are relative to
    * CONFIG_FS_MQUEUE_MPATH.
