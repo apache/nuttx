f52757f90a3de3da57af5ea6d7e8ad7993236dc0
chao.an <anchao@xiaomi.com>
2021-07-14 17:30:44 +0800

    net/wrb: add tcp/udp_inqueue_wrb_size() interface
    Signed-off-by: chao.an <anchao@xiaomi.com>

--- net/udp/udp_sendto_buffered.c
+++ net/udp/udp_sendto_buffered.c
@@ -106,6 +106,38 @@ static uint16_t sendto_eventhandler(FAR struct net_driver_s *dev,
  ****************************************************************************/
 
 /****************************************************************************
+ * Name: udp_inqueue_wrb_size
+ *
+ * Description:
+ *   Get the in-queued write buffer size from connection
+ *
+ * Input Parameters:
+ *   conn - The UDP connection of interest
+ *
+ * Assumptions:
+ *   Called from user logic with the network locked.
+ *
+ ****************************************************************************/
+
+static uint32_t udp_inqueue_wrb_size(FAR struct udp_conn_s *conn)
+{
+  FAR struct udp_wrbuffer_s *wrb;
+  FAR sq_entry_t *entry;
+  uint32_t total = 0;
+
+  if (conn)
+    {
+      for (entry = sq_peek(&conn->write_q); entry; entry = sq_next(entry))
+        {
+          wrb = (FAR struct udp_wrbuffer_s *)entry;
+          total += wrb->wb_iob->io_pktlen;
+        }
+    }
+
+  return total;
+}
+
+/****************************************************************************
  * Name: sendto_writebuffer_release
  *
  * Description:
