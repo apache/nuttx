c3464335ed654db96f5388bdaed534a561f43a71
Xiang Xiao <xiaoxiang@xiaomi.com>
2021-07-14 23:18:35 +0800

    arch: Fix rtcb can't found error
    use the same condition check in declaration and reference
    
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>
    Change-Id: I7b05316e914708fceeddac394d784ee3720a3c1b

--- arch/arm/src/arm/arm_assert.c
+++ arch/arm/src/arm/arm_assert.c
@@ -306,22 +306,18 @@ static void _up_assert(void)
 
 void up_assert(const char *filename, int lineno)
 {
-#if CONFIG_TASK_NAME_SIZE > 0 && defined(CONFIG_DEBUG_ALERT)
-  struct tcb_s *rtcb = running_task();
-#endif
-
   board_autoled_on(LED_ASSERTION);
 
   /* Flush any buffered SYSLOG data (prior to the assertion) */
 
   syslog_flush();
 
-#if CONFIG_TASK_NAME_SIZE > 0 && defined(CONFIG_DEBUG_ALERT)
+#if CONFIG_TASK_NAME_SIZE > 0
   _alert("Assertion failed at file:%s line: %d task: %s\n",
-        filename, lineno, rtcb->name);
+         filename, lineno, running_task()->name);
 #else
   _alert("Assertion failed at file:%s line: %d\n",
-        filename, lineno);
+         filename, lineno);
 #endif
 
   up_dumpstate();
