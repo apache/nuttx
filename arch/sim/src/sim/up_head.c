e98220c81ac0ddb6deb339d147cce3d1276a8a74
Jiuzhu Dong <dongjiuzhu1@xiaomi.com>
2021-07-16 18:17:10 +0800

    sim/cmdline: save boot cmdline to g_argc g_argv
    Change-Id: I989850a09528e3868957284c9f419d0992ae8d1f
    Signed-off-by: Jiuzhu Dong <dongjiuzhu1@xiaomi.com>

--- arch/sim/src/sim/up_head.c
+++ arch/sim/src/sim/up_head.c
@@ -38,6 +38,13 @@
 #include "up_internal.h"
 
 /****************************************************************************
+ * Public Data
+ ****************************************************************************/
+
+int g_argc;
+char **g_argv;
+
+/****************************************************************************
  * Private Data
  ****************************************************************************/
 
@@ -62,6 +69,9 @@ static char g_logbuffer[4096];
 
 int main(int argc, char **argv, char **envp)
 {
+  g_argc = argc;
+  g_argv = argv;
+
 #ifdef CONFIG_SYSLOG_RPMSG
   syslog_rpmsg_init_early(g_logbuffer, sizeof(g_logbuffer));
 #endif
