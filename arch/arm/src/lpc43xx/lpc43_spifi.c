ddbc5e4b2ced603c9bfc34a1be0e0b034f8cf5a4
Gustavo Henrique Nihei <gustavo.nihei@espressif.com>
2021-07-16 14:15:51 -0300

    arm/lpc43xx: Implement MTDIOC_ERASESTATE for SPIFI Flash driver
    Signed-off-by: Gustavo Henrique Nihei <gustavo.nihei@espressif.com>

--- arch/arm/src/lpc43xx/lpc43_spifi.c
+++ arch/arm/src/lpc43xx/lpc43_spifi.c
@@ -895,6 +895,15 @@ static int lpc43_ioctl(FAR struct mtd_dev_s *dev, int cmd, unsigned long arg)
         }
         break;
 
+      case MTDIOC_ERASESTATE:
+        {
+          FAR uint8_t *result = (FAR uint8_t *)arg;
+          *result = SPIFI_ERASED_STATE;
+
+          ret = OK;
+        }
+        break;
+
       case MTDIOC_XIPBASE:
       default:
         ret = -ENOTTY; /* Bad command */
