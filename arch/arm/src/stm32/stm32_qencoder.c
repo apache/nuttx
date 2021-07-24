4985f471552cfc09058aa1196806e7fc70fcbee9
Michal Lenc <michallenc@seznam.cz>
2021-07-24 10:16:51 +0200

    arch/arm/src/stm32/stm32_qencoder.c: print uint32_t by using standard format PRIx32
    Signed-off-by: Michal Lenc <michallenc@seznam.cz>

--- arch/arm/src/stm32/stm32_qencoder.c
+++ arch/arm/src/stm32/stm32_qencoder.c
@@ -28,6 +28,7 @@
 #include <assert.h>
 #include <errno.h>
 #include <debug.h>
+#include <inttypes.h>
 
 #include <nuttx/arch.h>
 #include <nuttx/irq.h>
@@ -1039,7 +1040,9 @@ static int stm32_shutdown(FAR struct qe_lowerhalf_s *lower)
   putreg32(regval, regaddr);
   leave_critical_section(flags);
 
-  sninfo("regaddr: %08x resetbit: %08x\n", regaddr, resetbit);
+  sninfo("regaddr: %08" PRIx32 " resetbit: %08" PRIx32 "\n",
+         regaddr, resetbit);
+
   stm32_dumpregs(priv, "After stop");
 
   /* Disable clocking to the timer */
