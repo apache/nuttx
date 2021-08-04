e783f6e648083f15516a8f861c9f0b7db95efb37
Xiang Xiao <xiaoxiang@xiaomi.com>
2021-08-04 21:31:05 +0800

    libc/gettext: Change g_domain to array
    otherwise textdomain will deference NULL pointer in the default case
    
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- libs/libc/locale/lib_gettext.c
+++ libs/libc/locale/lib_gettext.c
@@ -77,7 +77,7 @@ static sem_t g_sem = SEM_INITIALIZER(1);
 static FAR struct mofile_s *g_mofile;
 
 #ifndef CONFIG_BUILD_KERNEL
-static FAR char *g_domain;
+static FAR char g_domain[NAME_MAX];
 #endif
 
 /****************************************************************************
