08e58739006484febb8e83cc2da630fc32c31c40
Xiang Xiao <xiaoxiang@xiaomi.com>
2021-08-06 12:13:44 +0800

    arch/armv8-m: Update per the latest architecture reference manual
    https://developer.arm.com/documentation/ddi0553/latest
    
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- arch/arm/src/armv8-m/arm_mpu.c
+++ arch/arm/src/armv8-m/arm_mpu.c
@@ -44,141 +44,12 @@
  * Private Data
  ****************************************************************************/
 
-/* These sets represent the set of disabled memory sub-regions.  A bit set
- * corresponds to a disabled sub-region; the LS bit corresponds to the first
- * region.
- *
- * The g_ms_regionmask array is indexed by the number of subregions at the
- * end of the region:  0 means no sub-regions are available(0xff) and 8 means
- * all subregions are available (0x00).
- */
-
-static const uint8_t g_ms_regionmask[9] =
-{
-  0xff, 0xfe, 0xfc, 0xf8, 0xf0, 0xe0, 0xc0, 0x80, 0x00
-};
-
-/* The g_ls_regionmask array is indexed by the number of subregions at the
- * beginning of the region:  0 means no sub-regions need be disabled (0x00)
- * and 8 means all subregions must be disabled (0xff).
- */
-
-static const uint8_t g_ls_regionmask[9] =
-{
-  0x00, 0x01, 0x03, 0x07, 0x0f, 0x1f, 0x3f, 0x7f, 0xff
-};
-
 /* The next available region number */
 
 static uint8_t g_region;
 
 /****************************************************************************
  * Private Functions
- ****************************************************************************/
-
-/****************************************************************************
- * Name: mpu_subregion_ms
- *
- * Description:
- *   Given (1) the size of the memory to be mapped and (2) the log2 size
- *   of the mapping to use, determine the minimal sub-region set at the
- *   to be disabled at the higher end of the region.
- *
- * Assumption:
- *   l2size has the same properties as the return value from
- *   mpu_log2regionceil()
- *
- ****************************************************************************/
-
-static inline uint32_t mpu_subregion_ms(size_t size, uint8_t l2size)
-{
-  unsigned int nsrs;
-  uint32_t     asize;
-  uint32_t     mask;
-
-  /* Examples with l2size = 12:
-   *
-   *         Shifted Adjusted        Number      Sub-Region
-   * Size    Mask    Size      Shift Sub-Regions Bitset
-   * 0x1000  0x01ff  0x1000    9     8           0x00
-   * 0x0c00  0x01ff  0x0c00    9     6           0xc0
-   * 0x0c40  0x01ff  0x0e00    9     7           0x80
-   */
-
-  if (l2size < 32)
-    {
-      mask  = ((1 << l2size) - 1) >> 3; /* Shifted mask */
-    }
-
-  /* The 4Gb region size is a special case */
-
-  else
-    {
-      /* NOTE: There is no way to represent a 4Gb region size in the 32-bit
-       * input.
-       */
-
-      mask = 0x1fffffff;          /* Shifted mask */
-    }
-
-  asize = (size + mask) & ~mask;  /* Adjusted size */
-  nsrs  = asize >> (l2size - 3);  /* Number of subregions */
-  return g_ms_regionmask[nsrs];
-}
-
-/****************************************************************************
- * Name: mpu_subregion_ls
- *
- * Description:
- *   Given (1) the offset to the beginning of data in the region and (2) the
- *   log2 size of the mapping to use, determine the minimal sub-region set
- *   to span that memory region sub-region set at the to be disabled at the
- *   lower end of the region
- *
- * Assumption:
- *   l2size has the same properties as the return value from
- *   mpu_log2regionceil()
- *
- ****************************************************************************/
-
-static inline uint32_t mpu_subregion_ls(size_t offset, uint8_t l2size)
-{
-  unsigned int nsrs;
-  uint32_t     aoffset;
-  uint32_t     mask;
-
-  /* Examples with l2size = 12:
-   *
-   *         Shifted Adjusted        Number      Sub-Region
-   * Offset  Mask    Offset    Shift Sub-Regions Bitset
-   * 0x0000  0x01ff  0x0000    9     8           0x00
-   * 0x0400  0x01ff  0x0400    9     6           0x03
-   * 0x02c0  0x01ff  0x0200    9     7           0x01
-   */
-
-  if (l2size < 32)
-    {
-      mask  = ((1 << l2size)-1) >> 3; /* Shifted mask */
-    }
-
-  /* The 4Gb region size is a special case */
-
-  else
-    {
-      /* NOTE: There is no way to represent a 4Gb region size in the 32-bit
-       * input.
-       */
-
-      mask = 0x1fffffff;              /* Shifted mask */
-    }
-
-  aoffset = offset & ~mask;           /* Adjusted offset */
-  nsrs    = aoffset >> (l2size - 3);  /* Number of subregions */
-  return g_ls_regionmask[nsrs];
-}
-
-/****************************************************************************
- * Public Functions
  ****************************************************************************/
 
 /****************************************************************************
@@ -201,105 +72,8 @@ unsigned int mpu_allocregion(void)
 }
 
 /****************************************************************************
- * Name: mpu_log2regionceil
- *
- * Description:
- *   Determine the smallest value of l2size (log base 2 size) such that the
- *   following is true:
- *
- *   size <= (1 << l2size)
- *
+ * Public Functions
  ****************************************************************************/
-
-uint8_t mpu_log2regionceil(size_t size)
-{
-  uint8_t l2size;
-
-  /* The minimum permitted region size is 32 bytes (log2(32) = 5. */
-
-  for (l2size = 5; l2size < 32 && size > (1 << l2size); l2size++);
-  return l2size;
-}
-
-/****************************************************************************
- * Name: mpu_log2regionfloor
- *
- * Description:
- *   Determine the largest value of l2size (log base 2 size) such that the
- *   following is true:
- *
- *   size >= (1 << l2size)
- *
- ****************************************************************************/
-
-uint8_t mpu_log2regionfloor(size_t size)
-{
-  uint8_t l2size = mpu_log2regionceil(size);
-
-  if (l2size > 4 && size < (1 << l2size))
-    {
-      l2size--;
-    }
-
-  return l2size;
-}
-
-/****************************************************************************
- * Name: mpu_subregion
- *
- * Description:
- *   Given the size of the (1) memory to be mapped and (2) the log2 size
- *   of the mapping to use, determine the minimal sub-region set to span
- *   that memory region.
- *
- * Assumption:
- *   l2size has the same properties as the return value from
- *   mpu_log2regionceil()
- *
- ****************************************************************************/
-
-uint32_t mpu_subregion(uintptr_t base, size_t size, uint8_t l2size)
-{
-  uint32_t mask;
-  size_t offset;
-  uint32_t ret;
-
-  /* Eight subregions are supported.  The representation is as an 8-bit
-   * value with the LS bit corresponding to subregion 0.  A bit is set
-   * to disable the sub-region.
-   *
-   * l2size: Log2 of the actual region size is <= (1 << l2size);
-   */
-
-  DEBUGASSERT(l2size > 4 && size <= (1 << l2size));
-
-  /* For region sizes of 32, 64, and 128 bytes, the effect of setting
-   * one or more bits of the SRD field to 1 is UNPREDICTABLE.
-   */
-
-  if (l2size < 8)
-    {
-      return 0;
-    }
-
-  /* Calculate the offset of the base address into the aligned region. */
-
-  mask   = (1 << l2size) - 1;
-  offset = base & mask;
-
-  /* Calculate the mask need to handle disabled subregions at the end of the
-   * region
-   */
-
-  ret = mpu_subregion_ms(size + offset, l2size);
-
-  /* Then OR in the mask need to handle disabled subregions at the beginning
-   * of the region.
-   */
-
-  ret |= mpu_subregion_ls(offset, l2size);
-  return ret;
-}
 
 /****************************************************************************
  * Name: mpu_control
@@ -312,6 +86,15 @@ uint32_t mpu_subregion(uintptr_t base, size_t size, uint8_t l2size)
 void mpu_control(bool enable, bool hfnmiena, bool privdefena)
 {
   uint32_t regval = 0;
+
+  putreg32((MPU_MAIR_STRONGLY_ORDER <<  0) |
+           (MPU_MAIR_DEVICE         <<  8) |
+           (MPU_MAIR_NONCACHEABLE   << 16) |
+           (MPU_MAIR_WRITE_THROUGH  << 24),
+           MPU_MAIR0);
+
+  putreg32((MPU_MAIR_WRITE_BACK     <<  0),
+           MPU_MAIR1);
 
   if (enable)
     {
@@ -340,13 +123,10 @@ void mpu_control(bool enable, bool hfnmiena, bool privdefena)
  ****************************************************************************/
 
 void mpu_configure_region(uintptr_t base, size_t size,
-                                        uint32_t flags)
+                          uint32_t flags1, uint32_t flags2)
 {
   unsigned int region = mpu_allocregion();
-  uint32_t     regval;
-  uint8_t      l2size;
-  uint8_t      subregions;
-  uintptr_t    alignedbase;
+  uintptr_t    limit;
 
   /* Ensure the base address alignment
    *
@@ -356,34 +136,15 @@ void mpu_configure_region(uintptr_t base, size_t size,
    * aligns with the size of the selected region."
    */
 
-  alignedbase  = base & MPU_RBAR_ADDR_MASK;
-  l2size       = mpu_log2regionceil(size + base - alignedbase);
-  alignedbase &= ~((1 << l2size) - 1);
-  l2size       = mpu_log2regionceil(size + base - alignedbase);
-
-  DEBUGASSERT(alignedbase + (1 << l2size) >= base + size);
-  DEBUGASSERT(l2size == 5 ||
-              alignedbase + (1 << (l2size - 1)) < base + size);
-  DEBUGASSERT((alignedbase & MPU_RBAR_ADDR_MASK) == alignedbase);
-  DEBUGASSERT((alignedbase & ((1 << l2size) - 1)) == 0);
+  limit = (base + size) & MPU_RLAR_LIMIT_MASK;
+  base &= MPU_RBAR_BASE_MASK;
 
   /* Select the region */
 
   putreg32(region, MPU_RNR);
 
-  /* Select the region base address */
+  /* Set the region base, limit and attribute */
 
-  putreg32(alignedbase | region | MPU_RBAR_VALID, MPU_RBAR);
-
-  /* Select the region size and the sub-region map */
-
-  subregions = mpu_subregion(base, size, l2size);
-
-  /* The configure the region */
-
-  regval = MPU_RASR_ENABLE                              | /* Enable region  */
-           MPU_RASR_SIZE_LOG2((uint32_t)l2size)         | /* Region size    */
-           ((uint32_t)subregions << MPU_RASR_SRD_SHIFT) | /* Sub-regions    */
-           flags;
-  putreg32(regval, MPU_RASR);
+  putreg32(base | flags1, MPU_RBAR);
+  putreg32(limit | flags2 | MPU_RLAR_ENABLE, MPU_RLAR);
 }
--- arch/arm/src/armv8-m/dwt.h
+++ arch/arm/src/armv8-m/dwt.h
@@ -85,17 +85,50 @@
 #define DWT_FOLDCNT                  (DWT_BASE + 0x0018)  /* Folded-instruction Count Register */
 #define DWT_PCSR                     (DWT_BASE + 0x001c)  /* Program Counter Sample Register */
 #define DWT_COMP0                    (DWT_BASE + 0x0020)  /* Comparator Register 0 */
-#define DWT_MASK0                    (DWT_BASE + 0x0024)  /* Mask Register 0 */
 #define DWT_FUNCTION0                (DWT_BASE + 0x0028)  /* Function Register 0 */
+#define DWT_MASK0                    (DWT_BASE + 0x002c)  /* Mask Register 0 */
 #define DWT_COMP1                    (DWT_BASE + 0x0030)  /* Comparator Register 1 */
-#define DWT_MASK1                    (DWT_BASE + 0x0034)  /* Mask Register 1 */
 #define DWT_FUNCTION1                (DWT_BASE + 0x0038)  /* Function Register 1 */
+#define DWT_MASK1                    (DWT_BASE + 0x003c)  /* Mask Register 1 */
 #define DWT_COMP2                    (DWT_BASE + 0x0040)  /* Comparator Register 2 */
-#define DWT_MASK2                    (DWT_BASE + 0x0044)  /* Mask Register 2 */
 #define DWT_FUNCTION2                (DWT_BASE + 0x0048)  /* Function Register 2 */
+#define DWT_MASK2                    (DWT_BASE + 0x004c)  /* Mask Register 2 */
 #define DWT_COMP3                    (DWT_BASE + 0x0050)  /* Comparator Register 3 */
-#define DWT_MASK3                    (DWT_BASE + 0x0054)  /* Mask Register 3 */
 #define DWT_FUNCTION3                (DWT_BASE + 0x0058)  /* Function Register 3 */
+#define DWT_MASK3                    (DWT_BASE + 0x005c)  /* Mask Register 3 */
+#define DWT_COMP4                    (DWT_BASE + 0x0060)  /* Comparator Register 4 */
+#define DWT_FUNCTION4                (DWT_BASE + 0x0068)  /* Function Register 4 */
+#define DWT_MASK4                    (DWT_BASE + 0x006c)  /* Mask Register 4 */
+#define DWT_COMP5                    (DWT_BASE + 0x0070)  /* Comparator Register 5 */
+#define DWT_FUNCTION5                (DWT_BASE + 0x0078)  /* Function Register 5 */
+#define DWT_MASK5                    (DWT_BASE + 0x007c)  /* Mask Register 5 */
+#define DWT_COMP6                    (DWT_BASE + 0x0080)  /* Comparator Register 6 */
+#define DWT_FUNCTION6                (DWT_BASE + 0x0088)  /* Function Register 6 */
+#define DWT_MASK6                    (DWT_BASE + 0x008c)  /* Mask Register 6 */
+#define DWT_COMP7                    (DWT_BASE + 0x0090)  /* Comparator Register 7 */
+#define DWT_FUNCTION7                (DWT_BASE + 0x0098)  /* Function Register 7 */
+#define DWT_MASK7                    (DWT_BASE + 0x009c)  /* Mask Register 7 */
+#define DWT_COMP8                    (DWT_BASE + 0x00a0)  /* Comparator Register 8 */
+#define DWT_FUNCTION8                (DWT_BASE + 0x00a8)  /* Function Register 8 */
+#define DWT_MASK8                    (DWT_BASE + 0x00ac)  /* Mask Register 8 */
+#define DWT_COMP9                    (DWT_BASE + 0x00b0)  /* Comparator Register 9 */
+#define DWT_FUNCTION9                (DWT_BASE + 0x00b8)  /* Function Register 9 */
+#define DWT_MASK9                    (DWT_BASE + 0x00bc)  /* Mask Register 9 */
+#define DWT_COMP10                   (DWT_BASE + 0x00c0)  /* Comparator Register 10 */
+#define DWT_FUNCTION10               (DWT_BASE + 0x00c8)  /* Function Register 10 */
+#define DWT_MASK10                   (DWT_BASE + 0x00cc)  /* Mask Register 10 */
+#define DWT_COMP11                   (DWT_BASE + 0x00d0)  /* Comparator Register 11 */
+#define DWT_FUNCTION11               (DWT_BASE + 0x00d8)  /* Function Register 11 */
+#define DWT_MASK11                   (DWT_BASE + 0x00dc)  /* Mask Register 11 */
+#define DWT_COMP12                   (DWT_BASE + 0x00e0)  /* Comparator Register 12 */
+#define DWT_FUNCTION12               (DWT_BASE + 0x00e8)  /* Function Register 12 */
+#define DWT_MASK12                   (DWT_BASE + 0x00ec)  /* Mask Register 12 */
+#define DWT_COMP13                   (DWT_BASE + 0x00f0)  /* Comparator Register 13 */
+#define DWT_FUNCTION13               (DWT_BASE + 0x00f8)  /* Function Register 13 */
+#define DWT_MASK13                   (DWT_BASE + 0x00fc)  /* Mask Register 13 */
+#define DWT_COMP14                   (DWT_BASE + 0x0100)  /* Comparator Register 14 */
+#define DWT_FUNCTION14               (DWT_BASE + 0x0108)  /* Function Register 14 */
+#define DWT_MASK14                   (DWT_BASE + 0x010c)  /* Mask Register 14 */
 
 /* DWT Register Bit Field Definitions ***************************************/
 
@@ -111,6 +144,8 @@
 #define DWT_CTRL_NOCYCCNT_MASK        (0x1ul << DWT_CTRL_NOCYCCNT_SHIFT)
 #define DWT_CTRL_NOPRFCNT_SHIFT       24
 #define DWT_CTRL_NOPRFCNT_MASK        (0x1ul << DWT_CTRL_NOPRFCNT_SHIFT)
+#define DWT_CTRL_CYCDISS_SHIFT        23
+#define DWT_CTRL_CYCDISS_MASK         (0x1ul << DWT_CTRL_CYCDISS_SHIFT)
 #define DWT_CTRL_CYCEVTENA_SHIFT      22
 #define DWT_CTRL_CYCEVTENA_MASK       (0x1ul << DWT_CTRL_CYCEVTENA_SHIFT)
 #define DWT_CTRL_FOLDEVTENA_SHIFT     21
