46679c7db3e9173cf0bb14769f7999ea99416209
Neo Xu <xuxingliang@xiaomi.com>
2021-08-31 16:43:20 +0800

    libc/gettext: Initialize path field when creating mofile struct.
    Signed-off-by: Xu Xingliang <xuxingliang@xiaomi.com>

--- libs/libc/locale/lib_gettext.c
+++ libs/libc/locale/lib_gettext.c
@@ -312,6 +312,7 @@ FAR char *dcngettext(FAR const char *domainname,
           return notrans;
         }
 
+      strlcpy(mofile->path, path, PATH_MAX);
       mofile->map = momap(path, &mofile->size);
       if (mofile->map == MAP_FAILED)
         {
