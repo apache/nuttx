e8d7779ab1b82fa9de8f61f44a6528a29a0cddf9
chao.an <anchao@xiaomi.com>
2021-09-02 22:27:42 +0800

    sys/socket/scm: add more socket message control helper
    Signed-off-by: chao.an <anchao@xiaomi.com>

--- include/sys/socket.h
+++ include/sys/socket.h
@@ -260,6 +260,14 @@
    (FAR struct cmsghdr *)NULL)
 #define CMSG_FIRSTHDR(msg) \
   __CMSG_FIRSTHDR((msg)->msg_control, (msg)->msg_controllen)
+#define CMSG_OK(mhdr, cmsg) ((cmsg)->cmsg_len >= sizeof(struct cmsghdr) && \
+                            (cmsg)->cmsg_len <= (unsigned long) \
+                            ((mhdr)->msg_controllen - \
+                             ((char *)(cmsg) - (char *)(mhdr)->msg_control)))
+#define for_each_cmsghdr(cmsg, msg) \
+       for (cmsg = CMSG_FIRSTHDR(msg); \
+            cmsg; \
+            cmsg = CMSG_NXTHDR(msg, cmsg))
 
 /* "Socket"-level control message types: */
 
