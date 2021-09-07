af949ba40d635e1059c18b698472592ac708814d
liangdongdong <liangdongdong@xiaomi.com>
2021-09-07 21:17:48 +0800

    feature: bsp: add bq25618 driver
    CHAMPION-105
    
    Signed-off-by: liangdongdong<liangdongdong@xiaomi.com>
    Change-Id: I061f297e7a65137b610f6fe4edc0305bf86934b9

--- include/nuttx/power/battery_charger.h
+++ include/nuttx/power/battery_charger.h
@@ -288,6 +288,42 @@ FAR struct battery_charger_dev_s *bq2429x_initialize(
                                      int current);
 #endif
 
+/****************************************************************************
+ * Name: bq25618_initialize
+ *
+ * Description:
+ *   Initialize the BQ25618 battery driver and return an instance of the
+ *   lower-half interface that may be used with battery_charger_register().
+ *
+ *   This driver requires:
+ *
+ *   CONFIG_BATTERY_CHARGER - Upper half battery charger driver support
+ *   CONFIG_I2C - I2C support
+ *   CONFIG_I2C_BQ25618 - And the driver must be explicitly selected.
+ *
+ * Input Parameters:
+ *   i2c       - An instance of the I2C interface to use to communicate with
+ *               the BQ25618
+ *   addr      - The I2C address of the BQ25618 (Better be 0x6A).
+ *   frequency - The I2C frequency
+ *   current   - The input current our power-supply can offer to charger
+ *
+ * Returned Value:
+ *   A pointer to the initialized battery driver instance.  A NULL pointer
+ *   is returned on a failure to initialize the BQ25618 lower half.
+ *
+ ****************************************************************************/
+
+#if defined(CONFIG_I2C) && defined(CONFIG_I2C_BQ25618)
+
+struct i2c_master_s;
+FAR struct battery_charger_dev_s *bq25618_initialize(
+                                    FAR struct i2c_master_s *i2c,
+                                     uint8_t addr,
+                                     uint32_t frequency,
+                                     int current);
+#endif
+
 #undef EXTERN
 #ifdef __cplusplus
 }
