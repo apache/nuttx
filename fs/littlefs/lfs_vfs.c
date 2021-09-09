b733a58599a85b90993894ad80a0b20c34e10366
zhouliang3 <zhouliang3@xiaomi.com>
2021-09-09 20:48:04 +0800

    fs/littlefs: revert fstat and use lfs_file_attr function
    Signed-off-by: zhouliang3 <zhouliang3@xiaomi.com>

--- fs/littlefs/lfs_vfs.c
+++ fs/littlefs/lfs_vfs.c
@@ -726,6 +726,7 @@ static int littlefs_fstat(FAR const struct file *filep, FAR struct stat *buf)
   FAR struct littlefs_mountpt_s *fs;
   FAR struct littlefs_file_s *priv;
   FAR struct inode *inode;
+  struct littlefs_attr_s attr;
   char path[LFS_NAME_MAX];
   int ret;
 
@@ -745,20 +746,43 @@ static int littlefs_fstat(FAR const struct file *filep, FAR struct stat *buf)
       return ret;
     }
 
-  ret = lfs_file_path(&fs->lfs, &priv->file, path, sizeof(path));
+  buf->st_size = lfs_file_size(&fs->lfs, &priv->file);
+  if (buf->st_size < 0)
+    {
+      ret = littlefs_convert_result(buf->st_size);
+      goto errout;
+    }
+
+  ret = littlefs_convert_result(lfs_file_getattr(&fs->lfs, &priv->file, 0,
+                                                 &attr, sizeof(attr)));
+  if (ret < 0)
+    {
+      if (ret != -ENODATA)
+        {
+          goto errout;
+        }
+
+      memset(&attr, 0, sizeof(attr));
+      attr.at_mode = S_IRWXG | S_IRWXU | S_IRWXO;
+    }
+
+  ret = 0;
+  buf->st_mode         = attr.at_mode | S_IFREG;
+  buf->st_uid          = attr.at_uid;
+  buf->st_gid          = attr.at_gid;
+  buf->st_atim.tv_sec  = attr.at_atim / 1000000000ull;
+  buf->st_atim.tv_nsec = attr.at_atim % 1000000000ull;
+  buf->st_mtim.tv_sec  = attr.at_mtim / 1000000000ull;
+  buf->st_mtim.tv_nsec = attr.at_mtim % 1000000000ull;
+  buf->st_ctim.tv_sec  = attr.at_ctim / 1000000000ull;
+  buf->st_ctim.tv_nsec = attr.at_ctim % 1000000000ull;
+  buf->st_blksize      = fs->cfg.block_size;
+  buf->st_blocks       = (buf->st_size + buf->st_blksize - 1) /
+                         buf->st_blksize;
+
+errout:
   nxmutex_unlock(&fs->lock);
-  if (ret < 0)
-    {
-      return ret;
-    }
-
-  ret = littlefs_stat(inode, path, buf);
-  if (ret < 0)
-    {
-      return ret;
-    }
-
-  return OK;
+  return ret;
 }
 
 static int littlefs_fchstat(FAR const struct file *filep,
@@ -767,6 +791,7 @@ static int littlefs_fchstat(FAR const struct file *filep,
   FAR struct littlefs_mountpt_s *fs;
   FAR struct littlefs_file_s *priv;
   FAR struct inode *inode;
+  struct littlefs_attr_s attr;
   char path[LFS_NAME_MAX];
   int ret;
 
@@ -784,20 +809,59 @@ static int littlefs_fchstat(FAR const struct file *filep,
       return ret;
     }
 
-  ret = lfs_file_path(&fs->lfs, &priv->file, path, sizeof(path));
+  ret = littlefs_convert_result(lfs_file_getattr(&fs->lfs, &priv->file,
+                                                 0, &attr, sizeof(attr)));
+  if (ret < 0)
+    {
+      if (ret != -ENODATA)
+        {
+          goto errout;
+        }
+
+      memset(&attr, 0, sizeof(attr));
+      attr.at_mode = S_IRWXG | S_IRWXU | S_IRWXO;
+    }
+
+  if ((CH_STAT_MODE & flags) == CH_STAT_MODE)
+    {
+      attr.at_mode = buf->st_mode;
+    }
+
+  if ((CH_STAT_UID & flags) == CH_STAT_UID)
+    {
+      attr.at_uid = buf->st_uid;
+    }
+
+  if ((CH_STAT_GID & flags) == CH_STAT_GID)
+    {
+      attr.at_gid = buf->st_gid;
+    }
+
+  attr.at_ctim = 1000000000ull * buf->st_ctim.tv_sec +
+                 buf->st_ctim.tv_nsec;
+
+  if ((CH_STAT_ATIME & flags) == CH_STAT_ATIME)
+    {
+      attr.at_atim = 1000000000ull * buf->st_atim.tv_sec +
+                     buf->st_atim.tv_nsec;
+    }
+
+  if ((CH_STAT_MTIME & flags) == CH_STAT_MTIME)
+    {
+      attr.at_mtim = 1000000000ull * buf->st_mtim.tv_sec +
+                     buf->st_mtim.tv_nsec;
+    }
+
+  ret = littlefs_convert_result(lfs_file_setattr(&fs->lfs, &priv->file, 0,
+                                                 &attr, sizeof(attr)));
+  if (ret < 0)
+    {
+      goto errout;
+    }
+
+errout:
   nxmutex_unlock(&fs->lock);
-  if (ret < 0)
-    {
-      return ret;
-    }
-
-  ret = littlefs_chstat(inode, path, buf, flags);
-  if (ret < 0)
-    {
-      return ret;
-    }
-
-  return OK;
+  return ret;
 }
 
 /****************************************************************************
@@ -1587,7 +1651,6 @@ static int littlefs_stat(FAR struct inode *mountpt, FAR const char *relpath,
   buf->st_mtim.tv_nsec = attr.at_mtim % 1000000000ull;
   buf->st_ctim.tv_sec  = attr.at_ctim / 1000000000ull;
   buf->st_ctim.tv_nsec = attr.at_ctim % 1000000000ull;
-  buf->st_size         = info.size;
   buf->st_blksize      = fs->cfg.block_size;
   buf->st_blocks       = (buf->st_size + buf->st_blksize - 1) /
                          buf->st_blksize;
@@ -1595,10 +1658,12 @@ static int littlefs_stat(FAR struct inode *mountpt, FAR const char *relpath,
   if (info.type == LFS_TYPE_REG)
     {
       buf->st_mode |= S_IFREG;
+      buf->st_size = info.size;
     }
   else
     {
       buf->st_mode |= S_IFDIR;
+      buf->st_size = 0;
     }
 
 errout:
