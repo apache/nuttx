2b28816d63da2364342c59813c19db4193f2cc03
Jari van Ewijk <jari.vanewijk@nxp.com>
2021-09-14 17:44:59 +0200

    S32K146EVB cleanup

--- boards/arm/s32k1xx/s32k146evb/src/s32k1xx_spi.c
+++ boards/arm/s32k1xx/s32k146evb/src/s32k1xx_spi.c
@@ -23,7 +23,9 @@
  ****************************************************************************/
 
 #include <nuttx/config.h>
+#include <nuttx/compiler.h>
 
+#include <sys/types.h>
 #include <stdint.h>
 #include <stdbool.h>
 #include <errno.h>
@@ -31,17 +33,15 @@
 
 #include <nuttx/spi/spi.h>
 #include <nuttx/spi/spi_transfer.h>
+
+#include "s32k1xx_pin.h"
+#include "s32k1xx_lpspi.h"
+
 #include <arch/board/board.h>
 
-#include "arm_arch.h"
-
-#include "s32k1xx_config.h"
-#include "s32k1xx_lpspi.h"
-#include "s32k1xx_pin.h"
 #include "s32k146evb.h"
 
-#if defined(CONFIG_S32K1XX_LPSPI0) || defined(CONFIG_S32K1XX_LPSPI1) || \
-    defined(CONFIG_S32K1XX_LPSPI2)
+#ifdef CONFIG_S32K1XX_LPSPI
 
 /****************************************************************************
  * Public Functions
@@ -51,80 +51,115 @@
  * Name: s32k1xx_spidev_initialize
  *
  * Description:
- *   Called to configure SPI chip select GPIO pins for the S32K144EVB board.
+ *   Configure chip select pins, initialize the SPI driver and register
+ *   /dev/spiN devices.
  *
  ****************************************************************************/
 
-void weak_function s32k1xx_spidev_initialize(void)
+int weak_function s32k1xx_spidev_initialize(void)
 {
+  int ret = OK;
+
 #ifdef CONFIG_S32K1XX_LPSPI0
+  /* LPSPI0 *****************************************************************/
+
+  /* Configure LPSPI0 peripheral chip select */
+
   s32k1xx_pinconfig(PIN_LPSPI0_PCS);
 
-#ifdef CONFIG_SPI_DRIVER
-  struct spi_dev_s *g_lpspi0;
-  g_lpspi0 = s32k1xx_lpspibus_initialize(0);
+#  ifdef CONFIG_SPI_DRIVER
+  /* Initialize the SPI driver for LPSPI0 */
 
-  if (!g_lpspi0)
+  struct spi_dev_s *g_lpspi0 = s32k1xx_lpspibus_initialize(0);
+  if (g_lpspi0 == NULL)
     {
-      spierr("ERROR: [boot] FAILED to initialize LPSPI0\n");
+      spierr("ERROR: FAILED to initialize LPSPI0\n");
+      return -ENODEV;
     }
 
-  spi_register(g_lpspi0, 0);
-#endif
-#endif
+  ret = spi_register(g_lpspi0, 0);
+  if (ret < 0)
+    {
+      spierr("ERROR: FAILED to register LPSPI0 driver\n");
+      return ret;
+    }
+#  endif /* CONFIG_SPI_DRIVER */
+#endif /* CONFIG_S32K1XX_LPSPI0 */
 
 #ifdef CONFIG_S32K1XX_LPSPI1
+  /* LPSPI1 *****************************************************************/
+
+  /* Configure LPSPI1 peripheral chip select */
+
   s32k1xx_pinconfig(PIN_LPSPI1_PCS);
 
-#ifdef CONFIG_SPI_DRIVER
-  struct spi_dev_s *g_lpspi1;
-  g_lpspi1 = s32k1xx_lpspibus_initialize(1);
+#  ifdef CONFIG_SPI_DRIVER
+  /* Initialize the SPI driver for LPSPI1 */
 
-  if (!g_lpspi1)
+  struct spi_dev_s *g_lpspi1 = s32k1xx_lpspibus_initialize(1);
+  if (g_lpspi1 == NULL)
     {
-      spierr("ERROR: [boot] FAILED to initialize LPSPI1\n");
+      spierr("ERROR: FAILED to initialize LPSPI1\n");
+      return -ENODEV;
     }
 
-  spi_register(g_lpspi1, 1);
-#endif
-#endif
+  ret = spi_register(g_lpspi1, 1);
+  if (ret < 0)
+    {
+      spierr("ERROR: FAILED to register LPSPI1 driver\n");
+      return ret;
+    }
+#  endif /* CONFIG_SPI_DRIVER */
+#endif /* CONFIG_S32K1XX_LPSPI1 */
 
 #ifdef CONFIG_S32K1XX_LPSPI2
+  /* LPSPI2 *****************************************************************/
+
+  /* Configure LPSPI2 peripheral chip select */
+
   s32k1xx_pinconfig(PIN_LPSPI2_PCS);
 
-#ifdef CONFIG_SPI_DRIVER
-  struct spi_dev_s *g_lpspi2;
-  g_lpspi2 = s32k1xx_lpspibus_initialize(2);
+#  ifdef CONFIG_SPI_DRIVER
+  /* Initialize the SPI driver for LPSPI2 */
 
-  if (!g_lpspi2)
+  struct spi_dev_s *g_lpspi2 = s32k1xx_lpspibus_initialize(2);
+  if (g_lpspi2 == NULL)
     {
-      spierr("ERROR: [boot] FAILED to initialize LPSPI2\n");
+      spierr("ERROR: FAILED to initialize LPSPI2\n");
+      return -ENODEV;
     }
 
-  spi_register(g_lpspi2, 2);
-#endif
-#endif
+  ret = spi_register(g_lpspi2, 2);
+  if (ret < 0)
+    {
+      spierr("ERROR: FAILED to register LPSPI2 driver\n");
+      return ret;
+    }
+#  endif /* CONFIG_SPI_DRIVER */
+#endif /* CONFIG_S32K1XX_LPSPI2 */
+
+  return ret;
 }
 
 /****************************************************************************
- * Name:  s32k1xx_lpspi0/1/2select and s32k1xx_lpspi0/1/2status
+ * Name: s32k1xx_lpspiNselect and s32k1xx_lpspiNstatus
  *
  * Description:
- *   The external functions, s32k1xx_lpspi0/1/2select and
- *   s32k1xx_lpspi0/1/2status must be provided by board-specific logic.
- *   They are implementations of the select and status methods of the SPI
- *   interface defined by struct spi_ops_s (see include/nuttx/spi/spi.h).
- *   All other methods (including s32k1xx_lpspibus_initialize()) are provided
- *   by common logic.  To use this common SPI logic on your board:
+ *   The external functions, s32k1xx_lpspiNselect and s32k1xx_lpspiNstatus
+ *   must be provided by board-specific logic.  They are implementations of
+ *   the select and status methods of the SPI interface defined by struct
+ *   spi_ops_s (see include/nuttx/spi/spi.h).  All other methods (including
+ *   s32k1xx_lpspibus_initialize()) are provided by common logic.  To use
+ *   this common SPI logic on your board:
  *
  *   1. Provide logic in s32k1xx_boardinitialize() to configure SPI chip
  *      select pins.
- *   2. Provide s32k1xx_lpspi0/1/2select() and s32k1xx_lpspi0/1/2status()
- *      functions in your board-specific logic.  These functions will perform
- *      chip selection and status operations using GPIOs in the way your
- *      board is configured.
+ *   2. Provide s32k1xx_lpspiNselect() and s32k1xx_lpspiNstatus() functions
+ *      in your board-specific logic.  These functions will perform chip
+ *      selection and status operations using GPIOs in the way your board is
+ *      configured.
  *   3. Add a calls to s32k1xx_lpspibus_initialize() in your low level
- *      application initialization logic
+ *      application initialization logic.
  *   4. The handle returned by s32k1xx_lpspibus_initialize() may then be used
  *      to bind the SPI driver to higher level logic (e.g., calling
  *      mmcsd_spislotinitialize(), for example, will bind the SPI driver to
@@ -133,10 +168,12 @@ void weak_function s32k1xx_spidev_initialize(void)
  ****************************************************************************/
 
 #ifdef CONFIG_S32K1XX_LPSPI0
+/* LPSPI0 *******************************************************************/
+
 void s32k1xx_lpspi0select(FAR struct spi_dev_s *dev, uint32_t devid,
-                        bool selected)
+                          bool selected)
 {
-  spiinfo("devid: %d CS: %s\n", (int)devid,
+  spiinfo("devid: %" PRId32 ", CS: %s\n", devid,
           selected ? "assert" : "de-assert");
 
   s32k1xx_gpiowrite(PIN_LPSPI0_PCS, !selected);
@@ -146,13 +183,15 @@ uint8_t s32k1xx_lpspi0status(FAR struct spi_dev_s *dev, uint32_t devid)
 {
   return 0;
 }
-#endif
+#endif /* CONFIG_S32K1XX_LPSPI0 */
 
 #ifdef CONFIG_S32K1XX_LPSPI1
-void s32k1xx_lpspi1select(FAR struct spi_dev_s *dev,
-                        uint32_t devid, bool selected)
+/* LPSPI1 *******************************************************************/
+
+void s32k1xx_lpspi1select(FAR struct spi_dev_s *dev, uint32_t devid,
+                          bool selected)
 {
-  spiinfo("devid: %d CS: %s\n", (int)devid,
+  spiinfo("devid: %" PRId32 ", CS: %s\n", devid,
           selected ? "assert" : "de-assert");
 
   s32k1xx_gpiowrite(PIN_LPSPI1_PCS, !selected);
@@ -162,14 +201,16 @@ uint8_t s32k1xx_lpspi1status(FAR struct spi_dev_s *dev, uint32_t devid)
 {
   return 0;
 }
-#endif
+#endif /* CONFIG_S32K1XX_LPSPI1 */
 
 #ifdef CONFIG_S32K1XX_LPSPI2
-void s32k1xx_lpspi2select(FAR struct spi_dev_s *dev,
-                        uint32_t devid, bool selected)
+/* LPSPI2 *******************************************************************/
+
+void s32k1xx_lpspi2select(FAR struct spi_dev_s *dev, uint32_t devid,
+                          bool selected)
 {
-  spiinfo("devid: %d CS: %s\n", (int)devid,
-           selected ? "assert" : "de-assert");
+  spiinfo("devid: %" PRId32 ", CS: %s\n", devid,
+          selected ? "assert" : "de-assert");
 
   s32k1xx_gpiowrite(PIN_LPSPI2_PCS, !selected);
 }
@@ -178,6 +219,5 @@ uint8_t s32k1xx_lpspi2status(FAR struct spi_dev_s *dev, uint32_t devid)
 {
   return 0;
 }
-#endif
-
-#endif /* CONFIG_S32K1XX_LPSPI0 || CONFIG_S32K1XX_LPSPI01 || CONFIG_S32K1XX_LPSPI2 */
+#endif /* CONFIG_S32K1XX_LPSPI2 */
+#endif /* CONFIG_S32K1XX_LPSPI */
