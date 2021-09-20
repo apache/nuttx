4878b4960ab57c3779e9c0fa334e538326715e57
P.Brier <git@pbrier.nl>
2021-09-20 14:14:10 +0200

    Fixed imxrt compilation error with arm-none-eabi-gcc (15:9-2019-q4-0ubuntu1) 9.2.1 20191025 (release) [ARM/arm-9-branch revision 277599] due to incorrect scope of variable and wrong linker arguments to gcc

--- arch/arm/src/imxrt/imxrt_flexcan.c
+++ arch/arm/src/imxrt/imxrt_flexcan.c
@@ -1455,7 +1455,7 @@ static int imxrt_ioctl(struct net_driver_s *dev, int cmd,
       (FAR struct imxrt_driver_s *)dev->d_private;
 
   int ret;
-
+  struct flexcan_timeseg data_timing;
   switch (cmd)
     {
       case SIOCGCANBITRATE: /* Get bitrate from a CAN controller */
@@ -1499,7 +1499,6 @@ static int imxrt_ioctl(struct net_driver_s *dev, int cmd,
 
           if (priv->canfd_capable)
           {
-            struct flexcan_timeseg data_timing;
             data_timing.bitrate = req->data_bitrate * 1000;
             data_timing.samplep = req->data_samplep;
 
