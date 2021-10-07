a3b257843d8c1a7bb515484a1367825b172b0057
Eero Nurkkala <eero.nurkkala@offcode.fi>
2021-10-07 07:19:28 +0300

    mpfs: ddr: add DDR training
    This adds DDR training. The training has a small chance of failing,
    and then the training is restarted.
    
    DDR training cannot be done meaningfully while the software is
    in DDR. If the system is intended to run from eNVM, like a
    bootloader, the linker script should be tuned to utilize the envm
    region as follows:
    
      envm (rx)   : ORIGIN = 0x20220100, LENGTH = 128K - 256
      l2lim (rwx) : ORIGIN = 0x08000000, LENGTH = 1024k
    
    256 bytes are reserved for the system; The fixed block may be
    installed from the 'hart-software-services' -repository:
    https://github.com/polarfire-soc/hart-software-services.git
    
    For example, the 256-byte image: hss-envm-wrapper-bm1-dummySbic.bin
    may be prepended on the nuttx bootloader image in the following
    manner:
    
     cat hss-envm-wrapper-bm1-dummySbic.bin > nuttx_bootloader.bin
     cat nuttx.bin >> nuttx_bootloader.bin
     riscv64-unknown-elf-objcopy -I binary -O ihex --change-section-lma
      *+0x20220000 nuttx_bootloader.bin flashable_image.hex
    
    This provides an image 'flashable_image.hex' that may be flashed on
    the eNVM region via Microsemi Libero tool.
    
    Signed-off-by: Eero Nurkkala <eero.nurkkala@offcode.fi>

---
+++ arch/risc-v/src/mpfs/mpfs_ddr.h
@@ -0,0 +1,47 @@
+/****************************************************************************
+ * arch/risc-v/src/mpfs/mpfs_ddr.h
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+#ifndef __ARCH_RISCV_SRC_MPFS_MPFS_DDR_H
+#define __ARCH_RISCV_SRC_MPFS_MPFS_DDR_H
+
+/****************************************************************************
+ * Public Function Prototypes
+ ****************************************************************************/
+
+#ifndef __ASSEMBLY__
+
+#undef EXTERN
+#if defined(__cplusplus)
+#define EXTERN extern "C"
+extern "C"
+{
+#else
+#define EXTERN extern
+#endif
+
+EXTERN int mpfs_ddr_init(void);
+
+#if defined(__cplusplus)
+}
+#endif
+#undef EXTERN
+
+#endif /* __ASSEMBLY__ */
+#endif /* __ARCH_RISCV_SRC_MPFS_MPFS_DDR_H */
