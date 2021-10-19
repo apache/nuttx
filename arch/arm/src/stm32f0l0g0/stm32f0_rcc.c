444b9a4e5b33b2a8e64c11db8024f85540b2a3b6
Alexander Oryshchenko <alexander.oryshchenko@ge.com>
2021-10-19 17:42:55 +0300

    Added ARCH_BOARD_STM32F0G0L0_CUSTOM_CLOCKCONFIG option to stm32f0/g0/l0 chip configiuration

--- arch/arm/src/stm32f0l0g0/stm32f0_rcc.c
+++ arch/arm/src/stm32f0l0g0/stm32f0_rcc.c
@@ -418,7 +418,7 @@ static inline void rcc_enableapb2(void)
  *
  ****************************************************************************/
 
-#ifndef CONFIG_ARCH_BOARD_STM32_CUSTOM_CLOCKCONFIG
+#ifndef CONFIG_ARCH_BOARD_STM32F0G0L0_CUSTOM_CLOCKCONFIG
 static void stm32_stdclockconfig(void)
 {
   uint32_t regval;
