ce1ceae14dbfa474c71a254c22a7164fdb83bcb3
Xiang Xiao <xiaoxiang@xiaomi.com>
2021-10-31 23:46:31 +0800

    libc: Move lib_filesem.c and lib_stream.c to libc/stdio
    since it make more sense to put all FILE functions in one place
    
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- libs/libc/stdio/lib_libfilesem.c
+++ libs/libc/stdio/lib_libfilesem.c
@@ -1,5 +1,5 @@
 /****************************************************************************
- * libs/libc/misc/lib_filesem.c
+ * libs/libc/stdio/lib_libfilesem.c
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
