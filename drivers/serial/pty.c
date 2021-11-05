1afdb06981b7a1e3bf51beca8e272de5694a7885
fangzhenwei <fangzhenwei@xiaomi.com>
2021-11-05 14:46:39 +0800

    nuttx/pty:pty FIONBIO pass to pipe control
    1. pass FIONBIO ioctl to pipe
    2. resolve the return value error
    
    Signed-off-by: fangzhenwei <fangzhenwei@xiaomi.com>

--- drivers/serial/pty.c
+++ drivers/serial/pty.c
@@ -874,6 +874,12 @@ static int pty_ioctl(FAR struct file *filep, int cmd, unsigned long arg)
         }
         break;
 
+      case FIONBIO:
+        {
+          ret = file_ioctl(&dev->pd_sink, cmd, arg);
+        }
+        break;
+
       /* Any unrecognized IOCTL commands will be passed to the contained
        * pipe driver.
        *
@@ -892,7 +898,7 @@ static int pty_ioctl(FAR struct file *filep, int cmd, unsigned long arg)
               ret = file_ioctl(&dev->pd_sink, cmd, arg);
             }
 #else
-          ret = ENOTTY;
+          ret = -ENOTTY;
 #endif
         }
         break;
