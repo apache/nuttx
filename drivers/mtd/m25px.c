e016332caddc352c606bfb44290d237793ecb8cf
TimJTi <56726697+TimJTi@users.noreply.github.com>
2021-11-08 13:16:47 +0000

    Update m25px.c
    Add M25P_MTQ256

--- drivers/mtd/m25px.c
+++ drivers/mtd/m25px.c
@@ -98,6 +98,7 @@
 #define M25P_M25P64_CAPACITY       0x17 /* 64 M-bit */
 #define M25P_M25P128_CAPACITY      0x18 /* 128 M-bit */
 #define M25P_MT25Q128_CAPACITY     0x18 /* 128 M-bit */
+#define M25P_MT25Q256_CAPACITY     0x19 /* 256 M-bit */
 
 /*  M25P1 capacity is 131,072 bytes:
  *  (4 sectors) * (32,768 bytes per sector)
@@ -173,6 +174,17 @@
 #define M25P_MT25Q128_PAGE_SHIFT    8     /* Page size 1 << 8 = 256 */
 #define M25P_MT25Q128_NPAGES        65536
 #define M25P_MT25Q128_SUBSECT_SHIFT 12    /* Sub-Sector size 1 << 12 = 4,096 */
+
+/*  MT25Q256 capacity is 33,554,432 bytes:
+ *  (512 sectors) * (65,536 bytes per sector)
+ *  (65536 pages) * (256 bytes per page)
+ */
+
+#define M25P_MT25Q256_SECTOR_SHIFT  16    /* Sector size 1 << 16 = 65,536 */
+#define M25P_MT25Q256_NSECTORS      512
+#define M25P_MT25Q256_PAGE_SHIFT    8     /* Page size 1 << 8 = 256 */
+#define M25P_MT25Q256_NPAGES        65536
+#define M25P_MT25Q256_SUBSECT_SHIFT 12    /* Sub-Sector size 1 << 12 = 4,096 */
 
 /* Instructions */
 
@@ -462,6 +474,19 @@ static inline int m25p_readid(struct m25p_dev_s *priv)
 #endif
           return OK;
         }
+      else if (capacity == M25P_MT25Q256_CAPACITY)
+        {
+          /* Save the FLASH geometry */
+
+          priv->sectorshift    = M25P_MT25Q256_SECTOR_SHIFT;
+          priv->nsectors       = M25P_MT25Q256_NSECTORS;
+          priv->pageshift      = M25P_MT25Q256_PAGE_SHIFT;
+          priv->npages         = M25P_MT25Q256_NPAGES;
+#ifdef CONFIG_M25P_SUBSECTOR_ERASE
+          priv->subsectorshift = M25P_MT25Q256_SUBSECT_SHIFT;
+#endif
+          return OK;
+        }        
     }
 
   return -ENODEV;
