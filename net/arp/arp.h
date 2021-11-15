a9513a4bf6ae245d6b1a3e4d51a8ef4275002039
songlinzhang <songlinzhang@xiaomi.com>
2021-11-15 17:50:41 +0800

    Add arp_cleanup
    N/A
    
    Signed-off-by: songlinzhang <songlinzhang@xiaomi.com>
    Change-Id: I2add739b7e09497d28b5ea8f3db75f9e4cc08d78
    (cherry picked from commit ed4b87f8b84e561a8ba9e1591d19403328d402e7)

--- net/arp/arp.h
+++ net/arp/arp.h
@@ -359,6 +359,22 @@ int arp_find(in_addr_t ipaddr, FAR struct ether_addr *ethaddr);
 void arp_delete(in_addr_t ipaddr);
 
 /****************************************************************************
+ * Name: arp_cleanup
+ *
+ * Description:
+ *   Clear the ARP table on the network device
+ *
+ * Input Parameters:
+ *   ifname - Device name
+ *
+ * Assumptions
+ *   The network is locked to assure exclusive access to the ARP table.
+ *
+ ****************************************************************************/
+
+void arp_cleanup(FAR const char *ifname);
+
+/****************************************************************************
  * Name: arp_update
  *
  * Description:
@@ -368,6 +384,7 @@ void arp_delete(in_addr_t ipaddr);
  * Input Parameters:
  *   ipaddr  - The IP address as an inaddr_t
  *   ethaddr - Refers to a HW address uint8_t[IFHWADDRLEN]
+ *   ifname  - Device name
  *
  * Returned Value:
  *   Zero (OK) if the ARP table entry was successfully modified.  A negated
@@ -378,7 +395,8 @@ void arp_delete(in_addr_t ipaddr);
  *
  ****************************************************************************/
 
-int arp_update(in_addr_t ipaddr, FAR uint8_t *ethaddr);
+int arp_update(in_addr_t ipaddr, FAR uint8_t *ethaddr,
+               FAR const char *ifname);
 
 /****************************************************************************
  * Name: arp_hdr_update
@@ -390,6 +408,7 @@ int arp_update(in_addr_t ipaddr, FAR uint8_t *ethaddr);
  * Input Parameters:
  *   pipaddr - Refers to an IP address uint16_t[2] in network order
  *   ethaddr - Refers to a HW address uint8_t[IFHWADDRLEN]
+ *   ifname  - Device name
  *
  * Returned Value:
  *   Zero (OK) if the ARP table entry was successfully modified.  A negated
@@ -400,7 +419,8 @@ int arp_update(in_addr_t ipaddr, FAR uint8_t *ethaddr);
  *
  ****************************************************************************/
 
-void arp_hdr_update(FAR uint16_t *pipaddr, FAR uint8_t *ethaddr);
+void arp_hdr_update(FAR uint16_t *pipaddr, FAR uint8_t *ethaddr,
+                    FAR const char *ifname);
 
 /****************************************************************************
  * Name: arp_snapshot
@@ -462,8 +482,9 @@ void arp_dump(FAR struct arp_hdr_s *arp);
 #  define arp_notify(i)
 #  define arp_find(i,e) (-ENOSYS)
 #  define arp_delete(i)
-#  define arp_update(i,m);
-#  define arp_hdr_update(i,m);
+#  define arp_cleanup(n)
+#  define arp_update(i,m,n);
+#  define arp_hdr_update(i,m,n);
 #  define arp_snapshot(s,n) (0)
 #  define arp_dump(arp)
 
