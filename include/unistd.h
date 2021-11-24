624b8d0776fd1180172916bbbfa7bfb492f04ea6
anjiahao <anjiahao@xiaomi.com>
2021-11-24 16:57:16 +0800

    add MAXHOSTNAMELEN in headfile
    Signed-off-by: anjiahao <anjiahao@xiaomi.com>

--- include/unistd.h
+++ include/unistd.h
@@ -27,6 +27,7 @@
 
 #include <sys/types.h>
 #include <nuttx/compiler.h>
+#include <limits.h>
 
 /****************************************************************************
  * Pre-processor Definitions
@@ -255,8 +256,6 @@
 #define STDERR_FILENO                    2       /* File number of stderr */
 #define STDIN_FILENO                     0       /* File number of stdin */
 #define STDOUT_FILENO                    1       /* File number of stdout */
-
-#define HOST_NAME_MAX                    32
 
 /* Helpers and legacy compatibility definitions */
 
