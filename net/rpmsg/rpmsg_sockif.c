f64bad3ff95a33fd6f0da0e57a8b7c0f04ad8f77
ligd <liguiding1@xiaomi.com>
2021-11-25 11:59:38 +0800

    socket_rpmsg: fix conn->psock NULL in ept_cb
    Change-Id: Ic868378cc94cd093187c3034fd64927a30824c93
    Signed-off-by: ligd <liguiding1@xiaomi.com>

--- net/rpmsg/rpmsg_sockif.c
+++ net/rpmsg/rpmsg_sockif.c
@@ -502,8 +502,6 @@ static void rpmsg_socket_ns_bind(FAR struct rpmsg_device *rdev,
       return;
     }
 
-  rpmsg_socket_ns_bound(&new->ept);
-
   strcpy(new->rpaddr.rp_cpu, rpmsg_get_cpuname(rdev));
   strcpy(new->rpaddr.rp_name, name + RPMSG_SOCKET_NAME_PREFIX_LEN);
 
@@ -526,6 +524,8 @@ static void rpmsg_socket_ns_bind(FAR struct rpmsg_device *rdev,
   new->psock = server->psock;
 
   rpmsg_socket_unlock(&server->recvlock);
+
+  rpmsg_socket_ns_bound(&new->ept);
 
   rpmsg_socket_post(&server->recvsem);
   rpmsg_socket_pollnotify(server, POLLIN);
