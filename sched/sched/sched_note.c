8154e394b4b91e47c459843069a6f9352038d565
zhanghu6 <zhanghu6@xiaomi.com>
2021-12-03 12:26:42 +0800

    note dump: add CONFIG_SCHED_INSTRUMENTATION_SWITCH
    1. If CONFIG_SCHED_INSTRUMENTATION_SWITCH is enabled, then these additional interfaces are expected:
            sched_note_suspend
            sched_note_resume
    at the same time，If CONFIG_SMP is enabled，then expected：
            sched_note_cpu_pause
            sched_note_cpu_paused
            sched_note_cpu_resume
            sched_note_cpu_resumed
    2. other board if enable SCHED_INSTRUMENTATION，SCHED_INSTRUMENTATION_SWITCH to defconfig.

--- sched/sched/sched_note.c
+++ sched/sched/sched_note.c
@@ -451,6 +451,7 @@ void sched_note_stop(FAR struct tcb_s *tcb)
   sched_note_add(&note, sizeof(struct note_stop_s));
 }
 
+#ifdef CONFIG_SCHED_INSTRUMENTATION_SWITCH
 void sched_note_suspend(FAR struct tcb_s *tcb)
 {
   struct note_suspend_s note;
@@ -488,6 +489,7 @@ void sched_note_resume(FAR struct tcb_s *tcb)
 
   sched_note_add(&note, sizeof(struct note_resume_s));
 }
+#endif
 
 #ifdef CONFIG_SMP
 void sched_note_cpu_start(FAR struct tcb_s *tcb, int cpu)
@@ -529,6 +531,7 @@ void sched_note_cpu_started(FAR struct tcb_s *tcb)
   sched_note_add(&note, sizeof(struct note_cpu_started_s));
 }
 
+#ifdef CONFIG_SCHED_INSTRUMENTATION_SWITCH
 void sched_note_cpu_pause(FAR struct tcb_s *tcb, int cpu)
 {
   struct note_cpu_pause_s note;
@@ -606,6 +609,7 @@ void sched_note_cpu_resumed(FAR struct tcb_s *tcb)
 
   sched_note_add(&note, sizeof(struct note_cpu_resumed_s));
 }
+#endif
 #endif
 
 #ifdef CONFIG_SCHED_INSTRUMENTATION_PREEMPTION
