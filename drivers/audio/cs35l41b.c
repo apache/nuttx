e987da3526dcae4c551aa8df25ae6362ff94b808
genghuihui <genghuihui@xiaomi.com>
2021-12-03 10:53:52 +0800

    modify smart pa gain
    CHAMPION-236
    
    modify cs35l41b output gain
    
    Signed-off-by: genghuihui <genghuihui@xiaomi.com>
    Change-Id: I87fd6d3889b7624da8a6b5e169b855bfc23c3db1

--- drivers/audio/cs35l41b.c
+++ drivers/audio/cs35l41b.c
@@ -696,7 +696,7 @@ static int cs35l41b_configure(FAR struct audio_lowerhalf_s *dev,
           if (!priv->is_bypassed)
             {
               if (cs35l41b_set_gain(priv,
-                                    CS35L41B_AMP_GAIN_PCM_5P5DB) == ERROR)
+                                    CS35L41B_AMP_GAIN_PCM_9P5DB) == ERROR)
                 {
                   auderr("cs35l41b_set_gain error\n");
                   return ERROR;
