f57fe4987ee23ffe84143dc16b8ff837244f3be1
Huang Qi <huangqi3@xiaomi.com>
2021-12-05 21:41:44 +0800

    arch/risc-v: Introduce basic support for qemu rv32
    Signed-off-by: Huang Qi <huangqi3@xiaomi.com>

---
+++ arch/risc-v/src/qemu-rv32/chip.h
@@ -0,0 +1,45 @@
+/****************************************************************************
+ * arch/risc-v/src/qemu-rv32/chip.h
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+#ifndef __ARCH_RISCV_SRC_QEMU_RV32_CHIP_H
+#define __ARCH_RISCV_SRC_QEMU_RV32_CHIP_H
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+/* Include the chip capabilities file */
+
+#include <arch/qemu-rv32/chip.h>
+
+#ifndef __ASSEMBLY__
+
+/* Include the chip interrupt definition file */
+
+/* Serial initial function defined in uart_16550.c */
+
+extern void up_earlyserialinit(void);
+extern void up_serialinit(void);
+
+#endif
+
+#include "qemu_rv32_memorymap.h"
+
+#endif /* __ARCH_RISCV_SRC_QEMU_RV32_CHIP_H */
---
+++ boards/risc-v/qemu-rv32/rv32-virt/README.txt
@@ -0,0 +1,31 @@
+1. Download and install toolchain
+
+  $ curl https://static.dev.sifive.com/dev-tools/riscv64-unknown-elf-gcc-8.3.0-2019.08.0-x86_64-linux-ubuntu14.tar.gz
+
+2. Build and install qemu
+
+  $ git clone https://github.com/qemu/qemu
+  $ cd qemu
+  $ ./configure --target-list=riscv32-softmmu
+  $ make
+  $ sudo make install
+
+3. Configure and build NuttX
+
+  $ mkdir ./nuttx; cd ./nuttx
+  $ git clone https://github.com/apache/incubator-nuttx.git
+  $ git clone https://github.com/apache/incubator-nuttx-apps.git
+  $ cd nuttx
+  $ make distclean
+  $ ./tools/configure.sh rv32-virt:nsh
+  $ make
+
+4. Run the nuttx with qemu
+
+  $ qemu-system-riscv32 -M virt -cpu rv32 -bios none -kernel nuttx -nographic
+
+5. TODO
+
+  Support FPU
+  Support RISC-V User mode
+  Support Network
