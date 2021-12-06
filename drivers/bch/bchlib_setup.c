6885f781d2c6ba19df430eab6bc13fdd12181f5b
Jiuzhu Dong <dongjiuzhu1@xiaomi.com>
2021-12-06 22:53:21 +0800

    bch: specify the alignment of bch buffer by CONFIG_BCH_BUFFER_ALIGNMENT
    Change-Id: I9c9de69a2590925ba0938c49ac572faf50d735a0
    Signed-off-by: Jiuzhu Dong <dongjiuzhu1@xiaomi.com>

--- drivers/bch/bchlib_setup.c
+++ drivers/bch/bchlib_setup.c
@@ -112,7 +112,7 @@ int bchlib_setup(const char *blkdev, bool readonly, FAR void **handle)
 
   /* Allocate the sector I/O buffer */
 
-  bch->buffer = (FAR uint8_t *)kmm_malloc(bch->sectsize);
+  bch->buffer = kmm_memalign(CONFIG_BCH_BUFFER_ALIGNMENT, bch->sectsize);
   if (!bch->buffer)
     {
       ferr("ERROR: Failed to allocate sector buffer\n");
