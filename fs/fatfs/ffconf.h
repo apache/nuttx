60b8ae17806fcb6d432af3c35f18ca0f0eafbcbb
Jiuzhu Dong <dongjiuzhu1@xiaomi.com>
2021-12-24 12:20:35 +0800

    fatfs: adjust code_page to 437 because all code page are too bigger
    VELAPLATFO-847
    
    Change-Id: I719cc1c8e19e0325ffdb5ac470f8638836144508
    Signed-off-by: Jiuzhu Dong <dongjiuzhu1@xiaomi.com>

--- fs/fatfs/ffconf.h
+++ fs/fatfs/ffconf.h
@@ -84,7 +84,7 @@
 / Locale and Namespace Configurations
 /---------------------------------------------------------------------------*/
 
-#define FF_CODE_PAGE	0
+#define FF_CODE_PAGE	437
 /* This option specifies the OEM code page to be used on the target system.
 /  Incorrect code page setting can cause a file open failure.
 /
