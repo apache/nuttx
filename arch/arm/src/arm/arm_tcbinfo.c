e269548f87cbd2b3c51c8dc862fc2375a657459f
Xiang Xiao <xiaoxiang@xiaomi.com>
2021-12-27 11:09:38 +0800

    sched/tcbinfo: Fix the compile warning
    Update tcbinfo struct
    
    armv8-m/arm_tcbinfo.c:109:3: warning: excess elements in struct initializer
      109 |   TCB_REG_OFF(REG_S31),
          |   ^~~~~~~~~~~
    armv8-m/arm_tcbinfo.c:109:3: note: (near initialization for 'g_tcbinfo')
    armv8-m/arm_tcbinfo.c:110:3: warning: excess elements in struct initializer
      110 |   0,
          |   ^
    armv8-m/arm_tcbinfo.c:110:3: note: (near initialization for 'g_tcbinfo')
    armv8-m/arm_tcbinfo.c:111:3: warning: excess elements in struct initializer
      111 |   TCB_REG_OFF(REG_FPSCR),
          |   ^~~~~~~~~~~
    armv8-m/arm_tcbinfo.c:111:3: note: (near initialization for 'g_tcbinfo')
    armv8-m/arm_tcbinfo.c:112:3: warning: excess elements in struct initializer
      112 |   0,
          |   ^
    armv8-m/arm_tcbinfo.c:112:3: note: (near initialization for 'g_tcbinfo')
    armv8-m/arm_tcbinfo.c:37:1: warning: missing braces around initializer [-Wmissing-braces]
       37 | {
    
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>
    Signed-off-by: zhuyanlin <zhuyanlin1@xiaomi.com>

--- arch/arm/src/arm/arm_tcbinfo.c
+++ arch/arm/src/arm/arm_tcbinfo.c
@@ -30,20 +30,11 @@
 #include <arch/irq.h>
 
 /****************************************************************************
- * Public Data
+ * Private Data
  ****************************************************************************/
 
-const struct tcbinfo_s g_tcbinfo =
+static const uint16_t g_reg_offs[] =
 {
-  TCB_PID_OFF,
-  TCB_STATE_OFF,
-  TCB_PRI_OFF,
-#if CONFIG_TASK_NAME_SIZE > 0
-  TCB_NAME_OFF,
-#endif
-
-  XCPTCONTEXT_REGS,
-
   TCB_REG_OFF(REG_R0),
   TCB_REG_OFF(REG_R1),
   TCB_REG_OFF(REG_R2),
@@ -61,6 +52,22 @@ const struct tcbinfo_s g_tcbinfo =
   TCB_REG_OFF(REG_R14),
   TCB_REG_OFF(REG_R15),
   TCB_REG_OFF(REG_CPSR),
+};
+
+/****************************************************************************
+ * Public Data
+ ****************************************************************************/
+
+const struct tcbinfo_s g_tcbinfo =
+{
+  TCB_PID_OFF,
+  TCB_STATE_OFF,
+  TCB_PRI_OFF,
+  TCB_NAME_OFF,
+  XCPTCONTEXT_REGS,
+  {
+    .p = g_reg_offs,
+  },
 };
 
 #endif
