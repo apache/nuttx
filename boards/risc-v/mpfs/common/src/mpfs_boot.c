e827e254bcf9d7ffafc4db087478f00b45354aca
Ville Juven <ville.juven@unikie.com>
2021-12-28 13:00:15 +0200

    Add support for a ROMFS image for MPFS
    The image must be placed into:
    boards/risc-v/mpfs/icicle/include/boot_romfsimg.h
    
    The image is mounted by mpfs_bringup, which is run by the application
    itself, or by board_late_initialize() in the case when
    CONFIG_BOARD_LATE_INITIALIZE is defined, e.g. with CONFIG_BUILD_KERNEL

--- boards/risc-v/mpfs/common/src/mpfs_boot.c
+++ boards/risc-v/mpfs/common/src/mpfs_boot.c
@@ -29,6 +29,8 @@
 #include <nuttx/board.h>
 #include <arch/board/board.h>
 
+#include "board_config.h"
+
 /****************************************************************************
  * Pre-processor Definitions
  ****************************************************************************/
@@ -56,3 +58,25 @@ void mpfs_boardinitialize(void)
 {
   board_autoled_initialize();
 }
+
+/****************************************************************************
+ * Name: board_late_initialize
+ *
+ * Description:
+ *   If CONFIG_BOARD_LATE_INITIALIZE is selected, then an additional
+ *   initialization call will be performed in the boot-up sequence to a
+ *   function called board_late_initialize(). board_late_initialize() will be
+ *   called immediately after up_initialize() is called and just before the
+ *   initial application is started.  This additional initialization phase
+ *   may be used, for example, to initialize board-specific device drivers.
+ *
+ ****************************************************************************/
+
+#ifdef CONFIG_BOARD_LATE_INITIALIZE
+void board_late_initialize(void)
+{
+  /* Perform board initialization */
+
+  mpfs_bringup();
+}
+#endif /* CONFIG_BOARD_LATE_INITIALIZE */
