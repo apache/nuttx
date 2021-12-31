3e85d81686f33920d302c8b6170f173dff76be7a
Xiang Xiao <xiaoxiang@xiaomi.com>
2021-12-31 16:22:11 +0800

    grahpics: Move VNC server to drivers/video
    and remove the empty VNC client
    
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- drivers/video/vnc/vnc_receiver.c
+++ drivers/video/vnc/vnc_receiver.c
@@ -1,5 +1,5 @@
 /****************************************************************************
- * graphics/vnc/server/vnc_receiver.c
+ * drivers/video/vnc/vnc_receiver.c
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
