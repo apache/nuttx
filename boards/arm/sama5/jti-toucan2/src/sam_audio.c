665e4ae481bbd7941c961389c3ac6228f191d803
TimJTi <56726697+TimJTi@users.noreply.github.com>
2022-01-06 13:34:18 +0000

    Add audio support for board

---
+++ boards/arm/sama5/jti-toucan2/src/sam_audio.c
@@ -0,0 +1,231 @@
+/****************************************************************************
+ * boards/arm/sama5/sama5/jti-toucan2/src/sam_audio.c
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <debug.h>
+#include <assert.h>
+#include <errno.h>
+
+#include <nuttx/audio/audio.h>
+#include <nuttx/audio/pcm.h>
+#include <nuttx/audio/tone.h>
+#include <nuttx/audio/audio_null.h>
+
+#include "jti-toucan2.h"
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Private Types
+ ****************************************************************************/
+
+/****************************************************************************
+ * Private Function Prototypes
+ ****************************************************************************/
+
+/****************************************************************************
+ * Private Data
+ ****************************************************************************/
+
+/****************************************************************************
+ * Private Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+#ifdef HAVE_AUDIO_CLASSD
+
+/****************************************************************************
+ * Name: sam_classd_setup
+ *
+ * Description:
+ *  Initialize CLASSD and register the CLASSD device
+ *
+ ****************************************************************************/ 
+
+int sam_audio_classd_initialize(int minor)
+{
+  FAR struct audio_lowerhalf_s *classd_audio;
+  FAR struct audio_lowerhalf_s *pcm;
+  FAR struct audio_lowerhalf_s *tone;
+  static bool initialized = false;
+  char devname[12];
+  int ret;
+
+  audinfo("minor %d\n", minor);
+  DEBUGASSERT(minor >= 0 && minor <= 25);  
+  if (!initialized)
+    {
+      /* Get a ClassD audio interface */
+
+      classd_audio = sama5_classd_initialize();
+      if (classd_audio == NULL)
+      {
+        auderr("ERROR:  Failed to get classd interface\n");
+        ret= -ENODEV;
+        goto errout;
+      }
+        /* Now we can embed the audio interface into a PCM decoder
+       * instance so that we will have a PCM front end for the CLASSD
+       * audio driver.
+       */
+
+      pcm = pcm_decode_initialize(classd_audio);
+      if (!pcm)
+        {
+          auderr("ERROR: Failed create the PCM decoder\n");
+          ret = -ENODEV;
+          goto errout_with_classd_audio;
+        }
+      /* Create a device name */
+
+      snprintf(devname, 12, "pcm%d",  minor);
+
+      /* Finally, we can register the PCM/NULL audio device. */
+
+      ret = audio_register(devname, pcm);
+      if (ret < 0)
+        {
+          auderr("ERROR: Failed to register /dev/%s device: %d\n",
+                 devname, ret);
+          goto errout_with_classd_pcm;
+        }        
+
+
+
+
+      /* Now we are initialized */      
+    }  
+      
+  return OK;
+  /* Error exits.  Unfortunately there is no mechanism in place now to
+   * recover resources from most errors on initialization failures.
+   */
+
+errout_with_classd_audio:
+errout_with_classd_pcm:
+errout:
+  return ret;  
+}
+#endif
+
+#ifdef HAVE_AUDIO_NULL
+/****************************************************************************
+ * Name: sam_audio_null_initialize
+ *
+ * Description:
+ *   Set up to use the NULL audio device for PCM unit-level testing.
+ *
+ * Input Parameters:
+ *   minor - The input device minor number
+ *
+ * Returned Value:
+ *   Zero is returned on success.  Otherwise, a negated errno value is
+ *   returned to indicate the nature of the failure.
+ *
+ ****************************************************************************/
+
+int sam_audio_null_initialize(int minor)
+{
+  FAR struct audio_lowerhalf_s *nullaudio;
+  FAR struct audio_lowerhalf_s *pcm;
+  static bool initialized = false;
+  char devname[12];
+  int ret;
+
+  audinfo("minor %d\n", minor);
+  DEBUGASSERT(minor >= 0 && minor <= 25);
+
+  /* Have we already initialized?
+   * Since we never uninitialize we must prevent multiple initializations.
+   * This is necessary, for example, when thetouchscreen example is used as
+   * a built-in application in NSH and can be called numerous times.
+   * It will attempt to initialize each time.
+   */
+
+  if (!initialized)
+    {
+      /* Get a null audio interface
+       */
+
+      nullaudio = audio_null_initialize();
+      if (!nullaudio)
+        {
+          auderr("ERROR: Failed to get the NULL audio interface\n");
+          ret = -ENODEV;
+          goto errout;
+        }
+
+      /* Now we can embed the null audio interface into a PCM decoder
+       * instance so that we will have a PCM front end for the NULL
+       * audio driver.
+       */
+
+      pcm = pcm_decode_initialize(nullaudio);
+      if (!pcm)
+        {
+          auderr("ERROR: Failed create the PCM decoder\n");
+          ret = -ENODEV;
+          goto errout_with_null_audio;
+        }
+
+      /* Create a device name */
+
+      snprintf(devname, 12, "pcm%d",  minor);
+
+      /* Finally, we can register the PCM/NULL audio device. */
+
+      ret = audio_register(devname, pcm);
+      if (ret < 0)
+        {
+          auderr("ERROR: Failed to register /dev/%s device: %d\n",
+                 devname, ret);
+          goto errout_with_null_pcm;
+        }
+
+      /* Now we are initialized */
+
+      initialized = true;
+    }
+
+  return OK;
+
+  /* Error exits.  Unfortunately there is no mechanism in place now to
+   * recover resources from most errors on initialization failures.
+   */
+
+errout_with_null_audio:
+errout_with_null_pcm:
+errout:
+  return ret;
+}
+
+#endif /* HAVE_AUDIO_NULL */
