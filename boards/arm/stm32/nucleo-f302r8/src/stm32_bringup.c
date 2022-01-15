3b1de327cdb1145184f0db5d7daa7b6990d3f4fa
raiden00pl <raiden00@railab.me>
2022-01-15 14:34:44 +0100

    boards/nucleo-f302r8: add CAN and SocketCAN examples

--- boards/arm/stm32/nucleo-f302r8/src/stm32_bringup.c
+++ boards/arm/stm32/nucleo-f302r8/src/stm32_bringup.c
@@ -164,6 +164,26 @@ int stm32_bringup(void)
     }
 #endif
 
+#ifdef CONFIG_STM32_CAN_CHARDRIVER
+  /* Initialize CAN and register the CAN driver. */
+
+  ret = stm32_can_setup();
+  if (ret < 0)
+    {
+      syslog(LOG_ERR, "ERROR: stm32_can_setup failed: %d\n", ret);
+    }
+#endif
+
+#ifdef CONFIG_STM32_CAN_SOCKET
+  /* Initialize CAN socket interface */
+
+  ret = stm32_cansock_setup();
+  if (ret < 0)
+    {
+      syslog(LOG_ERR, "ERROR: stm32_cansock_setup failed: %d\n", ret);
+    }
+#endif
+
   UNUSED(ret);
   return OK;
 }
---
+++ boards/arm/stm32/nucleo-f302r8/src/stm32_cansock.c
@@ -0,0 +1,57 @@
+/****************************************************************************
+ * boards/arm/stm32/nucleo-f302r8/src/stm32_cansock.c
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+#include <debug.h>
+
+#include "stm32_can.h"
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: stm32_cansock_setup
+ *
+ * Description:
+ *  Initialize CAN socket interface
+ *
+ ****************************************************************************/
+
+int stm32_cansock_setup(void)
+{
+  int ret;
+
+  /* Call stm32_caninitialize() to get an instance of the CAN interface */
+
+  ret = stm32_cansockinitialize(1);
+  if (ret < 0)
+    {
+      canerr("ERROR:  Failed to get CAN interface %d\n", ret);
+      return ret;
+    }
+
+  return OK;
+}
