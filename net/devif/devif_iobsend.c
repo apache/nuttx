2f02c8e5aa34bb546f09a0b28711f69000b18a67
zhanghongyu <zhanghongyu@xiaomi.com>
2022-01-20 21:02:37 +0800

    fix:Temporarily remove udp send large pkt assert
    Signed-off-by: zhanghongyu <zhanghongyu@xiaomi.com>

--- net/devif/devif_iobsend.c
+++ net/devif/devif_iobsend.c
@@ -55,7 +55,11 @@
 void devif_iob_send(FAR struct net_driver_s *dev, FAR struct iob_s *iob,
                     unsigned int len, unsigned int offset)
 {
-  DEBUGASSERT(dev && len > 0 && len < NETDEV_PKTSIZE(dev));
+  if (!(dev && len > 0 && len < NETDEV_PKTSIZE(dev)))
+    {
+      nerr("devif_iob_send error, %p, send len: %d\n", dev, len);
+      return;
+    }
 
   /* Copy the data from the I/O buffer chain to the device buffer */
 
