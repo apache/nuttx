c7423ee00bedbcdfcf1581682bbc1fac32810cf4
Xiang Xiao <xiaoxiang@xiaomi.com>
2022-01-31 23:03:02 +0800

    Revert "sched: Don't duplicate caller file handler when creating kernel thread"
    This reverts commit 512676cb06179e033bd55892020c89391b59812b.

--- sched/task/task_init.c
+++ sched/task/task_init.c
@@ -105,15 +105,7 @@ int nxtask_init(FAR struct task_tcb_s *tcb, const char *name, int priority,
 
   /* Associate file descriptors with the new task */
 
-  if (ttype == TCB_FLAG_TTYPE_KERNEL)
-    {
-      ret = group_setupidlefiles(tcb);
-    }
-  else
-    {
-      ret = group_setuptaskfiles(tcb);
-    }
-
+  ret = group_setuptaskfiles(tcb);
   if (ret < 0)
     {
       goto errout_with_group;
