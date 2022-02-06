76ce44e8378199fb77975c43b12df560d444480d
Xiang Xiao <xiaoxiang@xiaomi.com>
2022-02-07 01:31:01 +0800

    serial/pty: Decouple SUSv1 style from pseudo fs operation
    and always enable BSD style PTYs since this feature doesn't
    consume the additional code size
    
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- drivers/serial/pty.h
+++ drivers/serial/pty.h
@@ -26,6 +26,7 @@
  ****************************************************************************/
 
 #include <nuttx/config.h>
+#include <stdint.h>
 
 /****************************************************************************
  * Public Function Prototypes
@@ -52,31 +53,29 @@ extern "C"
 
 #ifdef CONFIG_PSEUDOTERM_SUSV1
 void ptmx_minor_free(uint8_t minor);
+#else
+#  define ptmx_minor_free(minor)
 #endif
 
 /****************************************************************************
- * Name: pty_register
+ * Name: pty_register2
  *
  * Description:
- *   Create and register PTY master and slave devices.  The master device
- *   will be registered at /dev/ptyN and slave at /dev/pts/N where N is
- *   the provided minor number.
- *
- *   The slave side of the interface is always locked initially.  The
- *   master must call unlockpt() before the slave device can be opened.
+ *   Create and register PTY master and slave devices.  The slave side of
+ *   the interface is always locked initially.  The master must call
+ *   unlockpt() before the slave device can be opened.
  *
  * Input Parameters:
  *   minor - The number that qualifies the naming of the created devices.
+ *   susv1 - select SUSv1 or BSD behaviour
  *
  * Returned Value:
- *   Zero (OK) is returned on success; a negated errno value is returned on
- *   any failure.
+ *   0 is returned on success; otherwise, the negative error code return
+ *   appropriately.
  *
  ****************************************************************************/
 
-#ifdef CONFIG_PSEUDOTERM_SUSV1
-int pty_register(int minor);
-#endif
+int pty_register2(int minor, bool susv1);
 
 #undef EXTERN
 #ifdef __cplusplus
