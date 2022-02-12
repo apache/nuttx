1b6559e7684965a4257024272e3379df0e6120c1
Xiang Xiao <xiaoxiang@xiaomi.com>
2022-02-13 04:47:40 +0800

    arch/sim: Always typedef xcpt_reg_t to unsigned long
    to simplify the code logic
    
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- arch/sim/include/types.h
+++ arch/sim/include/types.h
@@ -92,27 +92,6 @@ typedef __WCHAR_TYPE__     _wchar_t;
 typedef int                _wchar_t;
 #endif
 
-#if defined(CONFIG_HOST_X86_64) && !defined(CONFIG_SIM_M32)
-/* 64-bit build on 64-bit machine: A size is 8 bytes */
-
-#if defined(__SIZE_TYPE__)
-/* If __SIZE_TYPE__ is defined we define ssize_t based on size_t.
- * We simply change "unsigned" to "signed" for this single definition
- * to make sure ssize_t and size_t only differ by their signedness.
- */
-
-#define unsigned signed
-typedef __SIZE_TYPE__      _ssize_t;
-#undef unsigned
-typedef __SIZE_TYPE__      _size_t;
-#else
-typedef signed long long   _ssize_t;
-typedef unsigned long long _size_t;
-#endif
-
-#else
-/* 32-bit build on 32- or 64-bit machine: A size is 4 bytes */
-
 #if defined(__SIZE_TYPE__)
 /* If __SIZE_TYPE__ is defined we define ssize_t based on size_t.
  * We simply change "unsigned" to "signed" for this single definition
@@ -129,8 +108,6 @@ typedef unsigned long      _size_t;
 #else
 typedef signed int         _ssize_t;
 typedef unsigned int       _size_t;
-#endif
-
 #endif
 
 /* This is the size of the interrupt state save returned by
