6086f42bfb0dfe946ddd3393fd1c76fb7f73468a
Michael Jung <mijung@gmx.net>
2022-02-12 08:23:02 +0100

    b-u585i-iot02a: Board Support
    Support for the STMicroelectronics B-U585I-IOT02A development board. This
    is a proof-of-concept port that demonstrates running NuttX as the
    Non-Secure TrustZone domain companion to TrustedFirmware-M.
    
    Signed-off-by: Michael Jung <mijung@gmx.net>

---
+++ boards/arm/stm32u5/b-u585i-iot02a/src/stm32_clockconfig.c
@@ -0,0 +1,48 @@
+/****************************************************************************
+ * boards/arm/stm32u5/b-u585i-iot02a/src/stm32_clockconfig.c
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: stm32_board_clockconfig
+ *
+ * Description:
+ *   Currently the B-U585I-IOT02A board support is restricted to running
+ *   NuttX in the Non-Secure domain together with TrustedFirmware-M (TFM).
+ *   In this setup the clock configuration is done by TFM, not by NuttX.
+ *   Thus, the board's configuration sets
+ *   CONFIG_ARCH_BOARD_STM32L5_CUSTOM_CLOCKCONFIG to avoid the standard clock
+ *   config logic to run and instead do just nothing in this function.
+ *
+ ****************************************************************************/
+
+#if defined(CONFIG_ARCH_BOARD_STM32U5_CUSTOM_CLOCKCONFIG)
+void stm32_board_clockconfig(void)
+{
+}
+#endif
