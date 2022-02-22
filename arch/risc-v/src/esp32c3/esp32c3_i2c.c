43f57240e0396b07c6e2e666cc29d8911a025a2b
Xiang Xiao <xiaoxiang@xiaomi.com>
2022-02-23 01:35:23 +0800

    Replece clock_gettime(CLOCK_MONOTONIC) with clock_systime_timespec
    it's better to call the kernrel api insteaad user space api in kernel
    
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- arch/risc-v/src/esp32c3/esp32c3_i2c.c
+++ arch/risc-v/src/esp32c3/esp32c3_i2c.c
@@ -851,7 +851,7 @@ static int esp32c3_i2c_polling_waitdone(struct esp32c3_i2c_priv_s *priv)
    * forward and backwards.
    */
 
-  clock_gettime(CLOCK_MONOTONIC, &current_time);
+  clock_systime_timespec(&current_time);
 
   timeout.tv_sec  = current_time.tv_sec  + 10;
   timeout.tv_nsec = current_time.tv_nsec +  0;
@@ -892,7 +892,7 @@ static int esp32c3_i2c_polling_waitdone(struct esp32c3_i2c_priv_s *priv)
 
       /* Update current time */
 
-      clock_gettime(CLOCK_MONOTONIC, &current_time);
+      clock_systime_timespec(&current_time);
       current_us = TIMESPEC_TO_US(current_time.tv_sec, current_time.tv_nsec);
     }
 
