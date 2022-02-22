b49ee3d4ede1c59f8f1c5cb618423d79cc10e70c
Gustavo Henrique Nihei <gustavo.nihei@espressif.com>
2022-02-22 10:31:36 -0300

    xtensa/esp32s3: Add support for Main System Watchdog Timers
    Support for RTC Watchdog Timer is currently in place, but not yet
    functional due to not yet implemented RTC driver.
    
    Signed-off-by: Gustavo Henrique Nihei <gustavo.nihei@espressif.com>

--- arch/xtensa/src/esp32s3/hardware/esp32s3_rtccntl.h
+++ arch/xtensa/src/esp32s3/hardware/esp32s3_rtccntl.h
@@ -31,11 +31,48 @@
  * Pre-processor Definitions
  ****************************************************************************/
 
+/* Offset relative to each watchdog timer instance memory base */
+
+#define RWDT_CONFIG0_OFFSET         0x0090
+
+/* RWDT */
+
+#define RWDT_STAGE0_TIMEOUT_OFFSET  0x0094
+#define RWDT_STAGE1_TIMEOUT_OFFSET  0x0098
+#define RWDT_STAGE2_TIMEOUT_OFFSET  0x009c
+#define RWDT_STAGE3_TIMEOUT_OFFSET  0x00a0
+#define RWDT_FEED_OFFSET            0x00a4
+#define RWDT_WP_REG                 0x00a8
+#define RWDT_INT_ENA_REG_OFFSET     0x0040
+#define RWDT_INT_CLR_REG_OFFSET     0x004c
+
 /* The value that needs to be written to RTC_CNTL_WDT_WKEY to
  * write-enable the wdt registers
  */
 
 #define RTC_CNTL_WDT_WKEY_VALUE     0x50d83aa1
+
+/* The value that needs to be written to RTC_CNTL_SWD_WPROTECT_REG
+ * to write-enable the wdt registers
+ */
+
+#define RTC_CNTL_SWD_WKEY_VALUE     0x8f1d312a
+
+/* Possible values for RTC_CNTL_WDT_CPU_RESET_LENGTH
+ * and RTC_CNTL_WDT_SYS_RESET_LENGTH
+ */
+
+#define RTC_WDT_RESET_LENGTH_100_NS    0
+#define RTC_WDT_RESET_LENGTH_200_NS    1
+#define RTC_WDT_RESET_LENGTH_300_NS    2
+#define RTC_WDT_RESET_LENGTH_400_NS    3
+#define RTC_WDT_RESET_LENGTH_500_NS    4
+#define RTC_WDT_RESET_LENGTH_800_NS    5
+#define RTC_WDT_RESET_LENGTH_1600_NS   6
+#define RTC_WDT_RESET_LENGTH_3200_NS   7
+
+#define RTC_CNTL_TIME0_REG      RTC_CNTL_TIME_LOW0_REG
+#define RTC_CNTL_TIME1_REG      RTC_CNTL_TIME_HIGH0_REG
 
 /* RTC_CNTL_RTC_OPTIONS0_REG register
  * RTC common configure register
