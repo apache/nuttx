140dc248dbdffd63e78334377a2d7c8f0f6226d7
Gustavo Henrique Nihei <gustavo.nihei@espressif.com>
2022-03-07 10:33:57 -0300

    xtensa/esp32s2: Add support for Main System Watchdog Timers
    Support for RTC Watchdog Timer is currently in place, but not yet
    functional due to not yet implemented RTC driver.
    
    Signed-off-by: Gustavo Henrique Nihei <gustavo.nihei@espressif.com>

--- arch/xtensa/src/esp32s2/hardware/esp32s2_rtccntl.h
+++ arch/xtensa/src/esp32s2/hardware/esp32s2_rtccntl.h
@@ -31,11 +31,32 @@
  * Pre-processor Definitions
  ****************************************************************************/
 
+/* Offset relative to each watchdog timer instance memory base */
+
+#define RWDT_CONFIG0_OFFSET         0x0094
+
+/* RWDT */
+
+#define RWDT_STAGE0_TIMEOUT_OFFSET  0x0098
+#define RWDT_STAGE1_TIMEOUT_OFFSET  0x009c
+#define RWDT_STAGE2_TIMEOUT_OFFSET  0x00a0
+#define RWDT_STAGE3_TIMEOUT_OFFSET  0x00a4
+#define RWDT_FEED_OFFSET            0x00a8
+#define RWDT_WP_REG                 0x00ac
+#define RWDT_INT_ENA_REG_OFFSET     0x0040
+#define RWDT_INT_CLR_REG_OFFSET     0x004c
+
 /* The value that needs to be written to RTC_CNTL_WDT_WKEY to
  * write-enable the wdt registers
  */
 
-#define RTC_CNTL_WDT_WKEY_VALUE         0x50d83aa1
+#define RTC_CNTL_WDT_WKEY_VALUE     0x50d83aa1
+
+/* The value that needs to be written to RTC_CNTL_SWD_WPROTECT_REG
+ * to write-enable the wdt registers
+ */
+
+#define RTC_CNTL_SWD_WKEY_VALUE     0x8f1d312a
 
 #define DPORT_CPUPERIOD_SEL_80          0
 #define DPORT_CPUPERIOD_SEL_160         1
