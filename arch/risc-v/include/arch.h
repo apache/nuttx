57127b94296438da33365d527f7c005a1cace72c
Ville Juven <ville.juven@unikie.com>
2022-03-11 13:35:39 +0200

    RISC-V: Initial support for CONFIG_BUILD_KERNEL
    This implements initial support for kernel build (address environments,
    page allocator) for RISC-V.
    
    This is done a bit differently compared to the ARMV7 implementation:
    
    - Support implemented for Sv39 MMU, however the implementation should be
      extensible for other MMU types also.
    - Instead of preserving and moving the L1 references around, a canonical
      approach is used instead, where the page table base address register
      is switched upon context switch.
    - To preserve a bit of memory, only a single L1/L2 table is supported,
      this gives access to 1GiB of virtual memory for each process, which
      should be more than enough.
    
    Some things worth noting:
    - Assumes page pool is mapped with vaddr=paddr mappings
    - The CONFIG_ARCH_XXXX_VBASE and CONFIG_ARCH_XXXX_NPAGES values are
      ignored, with the exception of CONFIG_ARCH_DATA_VBASE which is used
      for ARCH_DATA_RESERVE
    - ARCH_DATA_RESERVE is placed at the beginning of the userspace task's
      address environment

--- arch/risc-v/include/arch.h
+++ arch/risc-v/include/arch.h
@@ -33,6 +33,7 @@
 
 #ifndef __ASSEMBLY__
 #  include <stdint.h>
+#  include <stddef.h>
 #endif
 
 /****************************************************************************
@@ -70,6 +71,18 @@
 #define STACK_ALIGNMENT     16
 #define STACK_FRAME_SIZE    __XSTR(STACK_ALIGNMENT)
 
+/* Provide the maximum amount of page table levels per MMU type */
+
+#ifdef CONFIG_ARCH_MMU_TYPE_SV39
+#  define ARCH_PGT_MAX_LEVELS (3)
+#endif
+
+/* Amount of static page tables allocated for an address environment */
+
+#ifdef CONFIG_ARCH_ADDRENV
+#  define ARCH_SPGTS          (ARCH_PGT_MAX_LEVELS - 1)
+#endif
+
 /****************************************************************************
  * Inline functions
  ****************************************************************************/
@@ -96,6 +109,60 @@ static inline uintptr_t up_getsp(void)
 /****************************************************************************
  * Public Types
  ****************************************************************************/
+
+#ifdef CONFIG_ARCH_ADDRENV
+#ifndef __ASSEMBLY__
+
+/* A task group must have its L1 table in memory always, and the rest can
+ * be dynamically committed to memory (and even swapped).
+ *
+ * In this implementation every level tables besides the final level N are
+ * kept in memory always, while the level N tables are dynamically allocated.
+ *
+ * The implications ? They depend on the MMU type.
+ *
+ * For Sv39 this means that:
+ * - A task can not have more than 1GB of memory allocated. This should be
+ *   plenty enough...
+ * - The minimum amount of memory needed for page tables per task is 12K,
+ *   which gives access to 2MB of memory. This is plenty for many tasks.
+ */
+
+struct group_addrenv_s
+{
+  /* Pointers to MAX_LEVELS-1 tables here, one of each are allocated for the
+   * task when it is created.
+   */
+
+  uintptr_t spgtables[ARCH_SPGTS];
+
+  /* For convenience store the text base here */
+
+  uintptr_t textvbase;
+
+  /* For convenience store the data base here */
+
+  uintptr_t datavbase;
+
+  /* For convenience store the heap base and initial size here */
+
+  uintptr_t heapvbase;
+  size_t    heapsize;
+
+  /* For convenience store the satp value here */
+
+  uintptr_t satp;
+};
+
+typedef struct group_addrenv_s group_addrenv_t;
+
+/* If an address environment needs to be saved, saving the satp register
+ * will suffice. The register width is architecture dependent
+ */
+
+typedef uintptr_t save_addrenv_t;
+#endif /* __ASSEMBLY__ */
+#endif /* CONFIG_ARCH_ADDRENV */
 
 /****************************************************************************
  * Public Function Prototypes
