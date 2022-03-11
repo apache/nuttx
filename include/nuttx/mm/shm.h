99c40b5b7f41ecd0b613dc09b45490501c74d352
Xiang Xiao <xiaoxiang@xiaomi.com>
2022-03-12 03:58:25 +0800

    mm/shm: Initialize shm_info_s at the definition place
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- include/nuttx/mm/shm.h
+++ include/nuttx/mm/shm.h
@@ -96,22 +96,6 @@ struct group_shm_s
  ****************************************************************************/
 
 /****************************************************************************
- * Name: shm_initialize
- *
- * Description:
- *   Perform one time, start-up initialization of the shared memory logic.
- *
- * Input Parameters:
- *   None
- *
- * Returned Value:
- *   None
- *
- ****************************************************************************/
-
-void shm_initialize(void);
-
-/****************************************************************************
  * Name: shm_group_initialize
  *
  * Description:
