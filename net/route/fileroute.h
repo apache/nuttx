56386b6cad10c435e2c8b3c71235ffe1246f0451
Xiang Xiao <xiaoxiang@xiaomi.com>
2022-03-12 12:39:09 +0800

    net/route: Remove net_init_fileroute
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- net/route/fileroute.h
+++ net/route/fileroute.h
@@ -47,25 +47,6 @@
 struct file; /* Forward reference */
 
 /****************************************************************************
- * Name: net_init_fileroute
- *
- * Description:
- *   Initialize the in-memory, RAM routing table
- *
- * Input Parameters:
- *   None
- *
- * Returned Value:
- *   None
- *
- * Assumptions:
- *   Called early in initialization so that no special protection is needed.
- *
- ****************************************************************************/
-
-void net_init_fileroute(void);
-
-/****************************************************************************
  * Name: net_openroute_ipv4/net_openroute_ipv6
  *
  * Description:
