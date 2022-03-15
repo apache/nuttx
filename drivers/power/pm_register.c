ae21df2ed3c560d4ea4ed1f1446fd1f2a396861a
zhuyanlin <zhuyanlin1@xiaomi.com>
2022-03-15 20:42:34 +0800

    power: unify lock states
    unify critical_section and nxsem with pm_lock/pm_unlock
    
    Signed-off-by: zhuyanlin <zhuyanlin1@xiaomi.com>

--- drivers/power/pm_register.c
+++ drivers/power/pm_register.c
@@ -56,20 +56,19 @@
 
 int pm_register(FAR struct pm_callback_s *callbacks)
 {
-  int ret;
+  irqstate_t flags;
 
   DEBUGASSERT(callbacks);
 
   /* Add the new entry to the end of the list of registered callbacks */
 
-  ret = pm_lock();
-  if (ret == OK)
-    {
-      dq_addlast(&callbacks->entry, &g_pmglobals.registry);
-      pm_unlock();
-    }
+  flags = pm_lock();
 
-  return ret;
+  dq_addlast(&callbacks->entry, &g_pmglobals.registry);
+
+  pm_unlock(flags);
+
+  return 0;
 }
 
 #endif /* CONFIG_PM */
