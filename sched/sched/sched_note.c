2cd82808e2f2e9062526d4597dc8ce38e4fc0248
zhuyanlin <zhuyanlin1@xiaomi.com>
2022-03-30 20:53:48 +0800

    sched_note: fix buffer size warning in coverity
    VELAPLATFO-2176
    
    Change-Id: I0476d114f0a4964ab62d7e4926c6cd71fb50cd86
    Signed-off-by: zhuyanlin <zhuyanlin1@xiaomi.com>

--- sched/sched/sched_note.c
+++ sched/sched/sched_note.c
@@ -483,7 +483,7 @@ void sched_note_start(FAR struct tcb_s *tcb)
   namelen = strlen(tcb->name);
 
   DEBUGASSERT(namelen <= CONFIG_TASK_NAME_SIZE);
-  strncpy(note.nsa_name, tcb->name, CONFIG_TASK_NAME_SIZE + 1);
+  strlcpy(note.nsa_name, tcb->name, CONFIG_TASK_NAME_SIZE + 1);
 
   length = SIZEOF_NOTE_START(namelen + 1);
 #else
