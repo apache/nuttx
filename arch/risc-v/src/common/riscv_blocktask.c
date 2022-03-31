e643b786842c4e681aeed4ecad5a2aba7dce2671
Huang Qi <huangqi3@xiaomi.com>
2022-03-31 17:04:04 +0800

    arch/risc-v: Remove unneeded group_addrenv call which handled by riscv_doirq
    Signed-off-by: Huang Qi <huangqi3@xiaomi.com>

--- arch/risc-v/src/common/riscv_blocktask.c
+++ arch/risc-v/src/common/riscv_blocktask.c
@@ -136,15 +136,6 @@ void up_block_task(struct tcb_s *tcb, tstate_t task_state)
 
           struct tcb_s *nexttcb = this_task();
 
-#ifdef CONFIG_ARCH_ADDRENV
-          /* Make sure that the address environment for the previously
-           * running task is closed down gracefully (data caches dump,
-           * MMU flushed) and set up the address environment for the new
-           * thread at the head of the ready-to-run list.
-           */
-
-          (void)group_addrenv(nexttcb);
-#endif
           /* Reset scheduler parameters */
 
           nxsched_resume_scheduler(nexttcb);
