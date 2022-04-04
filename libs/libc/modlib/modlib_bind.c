75b1358d8ebe6ea467401c5f73398b77f4779eb0
YAMAMOTO Takashi <yamamoto@midokura.com>
2022-04-04 13:21:17 +0900

    modlib_bind.c: Restore "Relocation address out of range" checks
    Restore the checks which got completely broken by the following change.
    ```
    commit 15142a8b106bee8db6ac744b963969feba5c8289
    Author: anjiahao <anjiahao@xiaomi.com>
    Date:   Fri Apr 1 16:20:03 2022 +0800
    
        modlib/modlib_bind:fix unsigned_compare with zero
    
        Signed-off-by: anjiahao <anjiahao@xiaomi.com>
    ```

--- libs/libc/modlib/modlib_bind.c
+++ libs/libc/modlib/modlib_bind.c
@@ -297,8 +297,7 @@ static int modlib_relocate(FAR struct module_s *modp,
 
       /* Calculate the relocation address. */
 
-      if (rel->r_offset ||
-          rel->r_offset + sizeof(uint32_t) > dstsec->sh_size)
+      if (rel->r_offset + sizeof(uint32_t) > dstsec->sh_size)
         {
           berr("ERROR: Section %d reloc %d: "
                "Relocation address out of range, "
@@ -485,8 +484,7 @@ static int modlib_relocateadd(FAR struct module_s *modp,
 
       /* Calculate the relocation address. */
 
-      if (rela->r_offset ||
-          rela->r_offset + sizeof(uint32_t) > dstsec->sh_size)
+      if (rela->r_offset + sizeof(uint32_t) > dstsec->sh_size)
         {
           berr("ERROR: Section %d reloc %d: "
                "Relocation address out of range, "
