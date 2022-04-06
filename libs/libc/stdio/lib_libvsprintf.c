e93100cb4d0ae49f19d0be83fc57b52d1086fee4
Jiuzhu Dong <dongjiuzhu1@xiaomi.com>
2022-04-06 17:34:43 +0800

    lib_lib_vsprintf/backtrace: fix the type issue
    using 't': For integer types, causes printf to expect
    a ptrdiff_t-sized integer argument.
    
    Signed-off-by: Jiuzhu Dong <dongjiuzhu1@xiaomi.com>

--- libs/libc/stdio/lib_libvsprintf.c
+++ libs/libc/stdio/lib_libvsprintf.c
@@ -1160,7 +1160,7 @@ static int vsprintf_internal(FAR struct lib_outstream_s *stream,
 
                           if (c == 'S')
                             {
-                              sprintf_internal(stream, "+%#jx/%#zx",
+                              sprintf_internal(stream, "+%#tx/%#zx",
                                                addr - symbol->sym_value,
                                                symbolsize);
                             }
