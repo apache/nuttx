d6c952c56f3be69c1b140b3900e8c1fc47616ffd
Abdelatif Guettouche <abdelatif.guettouche@espressif.com>
2022-04-13 12:45:02 +0200

    arch: Fix compile error when enabling CONFIG_DUMP_ON_EXIT
    "error: incompatible types when assigning to type 'struct filelist *' from type 'struct filelist'
       filelist = tcb->group->tg_filelist;"
    
    Signed-off-by: Abdelatif Guettouche <abdelatif.guettouche@espressif.com>

--- arch/misoc/src/lm32/lm32_exit.c
+++ arch/misoc/src/lm32/lm32_exit.c
@@ -64,7 +64,7 @@ static void _up_dumponexit(FAR struct tcb_s *tcb, FAR void *arg)
   sinfo("  TCB=%p name=%s pid=%d\n", tcb, tcb->name, tcb->pid);
   sinfo("    priority=%d state=%d\n", tcb->sched_priority, tcb->task_state);
 
-  filelist = tcb->group->tg_filelist;
+  filelist = &tcb->group->tg_filelist;
   for (i = 0; i < filelist->fl_rows; i++)
     {
       for (j = 0; j < CONFIG_NFILE_DESCRIPTORS_PER_BLOCK; j++)
