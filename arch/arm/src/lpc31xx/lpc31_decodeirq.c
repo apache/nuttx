dc961baaea17107109d94575e9bb5d9fb725b801
chao.an <anchao@xiaomi.com>
2022-04-14 18:07:14 +0800

    arm/armv7-[a|r]: move fpu save/restore to assembly handler
    Save/Restore FPU registers in C environment is dangerous practive,
    which cannot guarantee the compiler won't generate the assembly code
    with float point registers, especially in interrupt handling
    
    Signed-off-by: chao.an <anchao@xiaomi.com>

--- arch/arm/src/lpc31xx/lpc31_decodeirq.c
+++ arch/arm/src/lpc31xx/lpc31_decodeirq.c
@@ -89,23 +89,16 @@ uint32_t *arm_decodeirq(uint32_t *regs)
 
           irq_dispatch(irq, regs);
 
-#if defined(CONFIG_ARCH_FPU) || defined(CONFIG_ARCH_ADDRENV)
+#ifdef CONFIG_ARCH_ADDRENV
           /* Check for a context switch.  If a context switch occurred, then
            * CURRENT_REGS will have a different value than it did on entry.
-           * If an interrupt level context switch has occurred, then restore
-           * the floating point state and the establish the correct address
-           * environment before returning from the interrupt.
+           * If an interrupt level context switch has occurred, then
+           * establish the correct address environment before returning
+           * from the interrupt.
            */
 
           if (regs != CURRENT_REGS)
             {
-#ifdef CONFIG_ARCH_FPU
-              /* Restore floating point registers */
-
-              arm_restorefpu((uint32_t *)CURRENT_REGS);
-#endif
-
-#ifdef CONFIG_ARCH_ADDRENV
               /* Make sure that the address environment for the previously
                * running task is closed down gracefully (data caches dump,
                * MMU flushed) and set up the address environment for the new
@@ -113,7 +106,6 @@ uint32_t *arm_decodeirq(uint32_t *regs)
                */
 
               group_addrenv(NULL);
-#endif
             }
 #endif
 
