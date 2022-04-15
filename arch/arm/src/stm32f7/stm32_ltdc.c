3a015d56b0b0806626a3e6e7c213560444a7e886
okayserh <o.kayser.herold@gmail.com>
2022-04-15 13:06:45 +0200

    Fixed a compile error, presumably caused by C&P error.

--- arch/arm/src/stm32f7/stm32_ltdc.c
+++ arch/arm/src/stm32f7/stm32_ltdc.c
@@ -1283,9 +1283,11 @@ static void stm32_ltdc_periphconfig(void)
 
   reginfo("configured RCC_PLLSAI=%08x\n", getreg32(STM32_RCC_PLLSAICFGR));
 
-  /* Configure dedicated clock external */
+  /* Configure dedicated clock external.
+   * Division factor for LCD_CLK in DCKCFGR1
+   */
 
-  reginfo("configured RCC_DCKCFGR=%08x\n", getreg32(STM32_RCC_DCKCFGR));
+  reginfo("configured RCC_DCKCFGR1=%08x\n", getreg32(STM32_RCC_DCKCFGR1));
 
   /* Configure LTDC_SSCR */
 
