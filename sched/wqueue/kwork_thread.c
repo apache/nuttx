ebffcdc992876d5bab8ff4f191171b6b088b0082
Petro Karashchenko <petro.karashchenko@gmail.com>
2022-04-17 00:40:59 +0200

    semaphore: fix usage of NXSEM_INITIALIZER
    Signed-off-by: Petro Karashchenko <petro.karashchenko@gmail.com>

--- sched/wqueue/kwork_thread.c
+++ sched/wqueue/kwork_thread.c
@@ -80,7 +80,7 @@
 struct hp_wqueue_s g_hpwork =
 {
   {},
-  NXSEM_INITIALIZER(0, SEM_PRIO_NONE),
+  NXSEM_INITIALIZER(0, PRIOINHERIT_FLAGS_DISABLE),
 };
 
 #endif /* CONFIG_SCHED_HPWORK */
@@ -91,7 +91,7 @@ struct hp_wqueue_s g_hpwork =
 struct lp_wqueue_s g_lpwork =
 {
   {},
-  NXSEM_INITIALIZER(0, SEM_PRIO_NONE),
+  NXSEM_INITIALIZER(0, PRIOINHERIT_FLAGS_DISABLE),
 };
 
 #endif /* CONFIG_SCHED_LPWORK */
