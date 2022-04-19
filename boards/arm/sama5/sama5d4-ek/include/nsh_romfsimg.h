6a126ee733982f86e8141fd746404071a7638b25
Alin Jerpelea <alin.jerpelea@sony.com>
2022-04-19 08:50:53 +0000

    arch: arm: sama5: Add Apache license to files
    In the initial contribution those files were missing the license
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- boards/arm/sama5/sama5d4-ek/include/nsh_romfsimg.h
+++ boards/arm/sama5/sama5d4-ek/include/nsh_romfsimg.h
@@ -1,3 +1,23 @@
+/***************************************************************************
+ * boards/arm/sama5/sama5d4-ek/include/nsh_romfsimg.h
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ***************************************************************************/
+
 unsigned char romfs_img[] = {
   0x2d, 0x72, 0x6f, 0x6d, 0x31, 0x66, 0x73, 0x2d, 0x00, 0x00, 0x02, 0x00,
   0xe7, 0x04, 0x63, 0xbd, 0x4e, 0x53, 0x48, 0x49, 0x6e, 0x69, 0x74, 0x56,
