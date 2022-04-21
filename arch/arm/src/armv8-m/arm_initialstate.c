e567786701e47e86ba831f8f74aa4d8901d0b69b
chao.an <anchao@xiaomi.com>
2022-04-21 12:12:46 +0800

    arm/armv[7|8]m: compare of hardware fp registers should skip REG_FP_RESERVED
    Fix fpu test break
    
    Signed-off-by: chao.an <anchao@xiaomi.com>

--- arch/arm/src/armv8-m/arm_initialstate.c
+++ arch/arm/src/armv8-m/arm_initialstate.c
@@ -152,7 +152,6 @@ void up_initial_state(struct tcb_s *tcb)
 
 #ifdef CONFIG_ARCH_FPU
   xcp->regs[REG_FPSCR] |= ARMV8M_FPSCR_LTPSIZE_NONE;
-  xcp->regs[REG_FP_RESERVED] = 0;
 #endif /* CONFIG_ARCH_FPU */
 
   /* Enable or disable interrupts, based on user configuration */
