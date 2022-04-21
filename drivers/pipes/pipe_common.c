7a68d6ff66b3c931afefbb85cc32bdf89dc57b97
Xiang Xiao <xiaoxiang@xiaomi.com>
2022-04-21 08:05:36 +0800

    pipe: Increase buffer size by one byte to ompensate the full indicator
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- drivers/pipes/pipe_common.c
+++ drivers/pipes/pipe_common.c
@@ -144,7 +144,7 @@ FAR struct pipe_dev_s *pipecommon_allocdev(size_t bufsize)
       nxsem_set_protocol(&dev->d_rdsem, SEM_PRIO_NONE);
       nxsem_set_protocol(&dev->d_wrsem, SEM_PRIO_NONE);
 
-      dev->d_bufsize = bufsize;
+      dev->d_bufsize = bufsize + 1; /* +1 to compensate the full indicator */
     }
 
   return dev;
