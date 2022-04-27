725c1b48fab888dba15e525d70484c1202f528b3
Xu Xingliang <xuxingliang@xiaomi.com>
2022-04-27 18:14:26 +0800

    driver/cw2218: use single transaction to read reg
    CHAMPION-2373
    
    Signed-off-by: Xu Xingliang <xuxingliang@xiaomi.com>
    Change-Id: I0fba0aadeeea3e38b7c7a0228155a27f0da03211

--- drivers/power/cw2218.c
+++ drivers/power/cw2218.c
@@ -176,31 +176,26 @@ static const unsigned char g_config_profile_info[SIZE_OF_PROFILE] =
 static int cw2218_getreg8(FAR struct cw2218_dev_s *priv, uint8_t regaddr,
                           FAR uint8_t *regval, int num_char)
 {
-  struct i2c_config_s config;
-  int ret;
+  int err;
+  struct i2c_msg_s msg[2];
+  FAR struct i2c_master_s *dev = priv->i2c;
 
-  /* Set up the I2C configuration */
+  msg[0].addr = priv->addr;
+  msg[0].buffer = &regaddr;
+  msg[0].length = 1;
+  msg[0].flags = I2C_M_NOSTOP;
+  msg[0].frequency = priv->frequency;
 
-  config.frequency = priv->frequency;
-  config.address   = priv->addr;
-  config.addrlen   = 7;
+  msg[1].addr = priv->addr;
+  msg[1].buffer = regval;
+  msg[1].length = num_char;
+  msg[1].flags = I2C_M_READ;
+  msg[1].frequency = priv->frequency;
 
-  /* Write the register address */
-
-  ret = i2c_write(priv->i2c, &config, &regaddr, 1);
-  if (ret < 0)
+  if ((err = I2C_TRANSFER(dev, msg, 2)) < OK)
     {
-      baterr("ERROR: i2c_write failed: %d\n", ret);
-      return ret;
-    }
-
-  /* Restart and read 8-bit values from the register */
-
-  ret = i2c_read(priv->i2c, &config, regval, num_char);
-  if (ret < 0)
-    {
-      baterr("ERROR: i2c_read failed: %d\n", ret);
-      return ret;
+      baterr("ERROR: i2c_write failed: %d\n", err);
+      return err;
     }
 
   return OK;
