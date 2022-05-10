69daf438d6537a2d28c6e42e037450e805c2f38e
Ville Juven <ville.juven@unikie.com>
2022-05-10 12:04:42 +0300

    libc: Rename lib_Exit lib__Exit
    Naming collision with lib_exit on some systems

--- libs/libc/stdlib/lib__Exit.c
+++ libs/libc/stdlib/lib__Exit.c
@@ -1,5 +1,5 @@
 /****************************************************************************
- * libs/libc/stdlib/lib_Exit.c
+ * libs/libc/stdlib/lib__Exit.c
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
