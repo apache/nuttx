f7f8a21486bc78e667fd3b244b997aa999c06942
Xiang Xiao <xiaoxiang@xiaomi.com>
2022-05-11 22:21:25 +0800

    libs: Move aeabi_atexit from libxx/ to libc/machine/arm/
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

---
+++ libs/libc/machine/arm/aeabi_atexit.c
@@ -0,0 +1,54 @@
+/****************************************************************************
+ * libs/libc/machine/arm/aeabi_atexit.c
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/compiler.h>
+
+/****************************************************************************
+ * Public Function Prototypes
+ ****************************************************************************/
+
+int __cxa_atexit(void (*func)(void *), void *object, void *dso_handle);
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: __aeabi_atexit
+ *
+ * Description:
+ *   Registers static object destructors.  Normally atexit(f) should call
+ *   __aeabi_atexit (NULL, f, NULL).  But in the usage model here, static
+ *   constructors are initialized at power up and are never destroyed
+ *   because they have global scope and must persist for as long as the
+ *   embedded device is powered on.
+ *
+ ****************************************************************************/
+
+int weak_function __aeabi_atexit(void *object,
+                                 void (*func)(void *),
+                                 void *dso_handle)
+{
+  return __cxa_atexit(func, object, dso_handle);
+}
