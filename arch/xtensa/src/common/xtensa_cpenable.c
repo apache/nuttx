ad57791fe0eaee6686c36fa8e057a7b39ac851d2
zhuyanlin <zhuyanlin1@xiaomi.com>
2022-05-12 22:24:24 +0800

    arch:xtensa: remove struct xtensa_cpstate_s as no need used
    Signed-off-by: zhuyanlin <zhuyanlin1@xiaomi.com>

--- arch/xtensa/src/common/xtensa_cpenable.c
+++ arch/xtensa/src/common/xtensa_cpenable.c
@@ -45,7 +45,6 @@
  *   Enable a set of co-processors.
  *
  * Input Parameters:
- *   cpstate - A pointer to the Co-processor state save structure.
  *   cpset   - A bit set of co-processors to be enabled.  Matches bit layout
  *             of the CPENABLE register.  Bit 0-XCHAL_CP_NUM:  0 = no change
  *             1 = enable
@@ -55,7 +54,7 @@
  *
  ****************************************************************************/
 
-void xtensa_coproc_enable(struct xtensa_cpstate_s *cpstate, int cpset)
+void xtensa_coproc_enable(int cpset)
 {
   irqstate_t flags;
   uint32_t cpenable;
@@ -64,16 +63,6 @@ void xtensa_coproc_enable(struct xtensa_cpstate_s *cpstate, int cpset)
 
   flags = enter_critical_section();
 
-  /* Don't enable co-processors that may already be enabled
-   *
-   *          cpenable
-   *            0   1
-   *          --- ---
-   *  cpset 0 | 0   0
-   *        1 | 1   0
-   */
-
-  cpset ^= (cpset & cpstate->cpenable);
   if (cpset != 0)
     {
       /* Enable the co-processors */
@@ -81,9 +70,6 @@ void xtensa_coproc_enable(struct xtensa_cpstate_s *cpstate, int cpset)
       cpenable = xtensa_get_cpenable();
       cpenable |= cpset;
       xtensa_set_cpenable(cpenable);
-
-      cpstate->cpenable  = cpenable;
-      cpstate->cpstored &= ~cpset;
     }
 
   leave_critical_section(flags);
@@ -96,7 +82,6 @@ void xtensa_coproc_enable(struct xtensa_cpstate_s *cpstate, int cpset)
  *   Enable a set of co-processors.
  *
  * Input Parameters:
- *   cpstate - A pointer to the Co-processor state save structure.
  *   cpset   - A bit set of co-processors to be enabled.  Matches bit layout
  *             of the CPENABLE register.  Bit 0-XCHAL_CP_NUM:  0 = no change
  *             1 = disable
@@ -106,7 +91,7 @@ void xtensa_coproc_enable(struct xtensa_cpstate_s *cpstate, int cpset)
  *
  ****************************************************************************/
 
-void xtensa_coproc_disable(struct xtensa_cpstate_s *cpstate, int cpset)
+void xtensa_coproc_disable(int cpset)
 {
   irqstate_t flags;
   uint32_t cpenable;
@@ -115,16 +100,6 @@ void xtensa_coproc_disable(struct xtensa_cpstate_s *cpstate, int cpset)
 
   flags = enter_critical_section();
 
-  /* Don't disable co-processors that are already be disabled.
-   *
-   *          cpenable
-   *            0   1
-   *          --- ---
-   *  cpset 0 | 0   0
-   *        1 | 0   1
-   */
-
-  cpset &= cpstate->cpenable;
   if (cpset != 0)
     {
       /* Disable the co-processors */
@@ -132,9 +107,6 @@ void xtensa_coproc_disable(struct xtensa_cpstate_s *cpstate, int cpset)
       cpenable = xtensa_get_cpenable();
       cpenable &= ~cpset;
       xtensa_set_cpenable(cpenable);
-
-      cpstate->cpenable  = cpenable;
-      cpstate->cpstored &= ~cpset;
     }
 
   leave_critical_section(flags);
