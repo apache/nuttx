ab4641783c857659944b22702f7f1084a91144f2
Xiang Xiao <xiaoxiang@xiaomi.com>
2022-05-28 04:58:16 +0800

    sched/tls: Add task_init_info and task_uninit_info
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- sched/group/group_create.c
+++ sched/group/group_create.c
@@ -35,10 +35,10 @@
 #include <nuttx/lib/lib.h>
 #include <nuttx/semaphore.h>
 #include <nuttx/sched.h>
-#include <nuttx/tls.h>
 
 #include "sched/sched.h"
 #include "group/group.h"
+#include "tls/tls.h"
 
 /****************************************************************************
  * Pre-processor Definitions
@@ -185,9 +185,8 @@ int group_allocate(FAR struct task_tcb_s *tcb, uint8_t ttype)
 
   /* Alloc task info for group  */
 
-  group->tg_info = (FAR struct task_info_s *)
-    group_zalloc(group, sizeof(struct task_info_s));
-  if (!group->tg_info)
+  ret = task_init_info(group);
+  if (ret < 0)
     {
       goto errout_with_member;
     }
@@ -199,10 +198,6 @@ int group_allocate(FAR struct task_tcb_s *tcb, uint8_t ttype)
   /* Inherit the user identity from the parent task group */
 
   group_inherit_identity(group);
-
-  /* Initial user space semaphore */
-
-  nxsem_init(&group->tg_info->ta_sem, 0, 1);
 
   /* Initialize file descriptors for the TCB */
 
