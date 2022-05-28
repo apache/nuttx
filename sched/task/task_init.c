a3f58a649d843e493fa8386254bf607be5f9974b
Xiang Xiao <xiaoxiang@xiaomi.com>
2022-05-29 05:38:37 +0800

    sched/tls: Don't compensate the tls size to the stack size
    it isn't good to change the stack size passed by caller
    
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- sched/task/task_init.c
+++ sched/task/task_init.c
@@ -130,9 +130,7 @@ int nxtask_init(FAR struct task_tcb_s *tcb, const char *name, int priority,
     {
       /* Allocate the stack for the TCB */
 
-      ret = up_create_stack(&tcb->cmn,
-                            up_tls_size() + stack_size,
-                            ttype);
+      ret = up_create_stack(&tcb->cmn, stack_size, ttype);
     }
 
   if (ret < OK)
