ab4a3dca9d7b482ecd0e99aaee2373fec5e36a54
wangbowen6 <wangbowen6@xiaomi.com>
2022-06-01 16:00:11 +0800

    arm/tlsr82: support flash protection and voltage calibration.
    Signed-off-by: wangbowen6 <wangbowen6@xiaomi.com>

--- arch/arm/src/tlsr82/tlsr82_flash.h
+++ arch/arm/src/tlsr82/tlsr82_flash.h
@@ -37,6 +37,15 @@
  * Pre-processor Definitions
  ****************************************************************************/
 
+#define FLASH_VOLTAGE_1V6                    0x00
+#define FLASH_VOLTAGE_1V65                   0x01
+#define FLASH_VOLTAGE_1V7                    0x02
+#define FLASH_VOLTAGE_1V75                   0x03
+#define FLASH_VOLTAGE_1V8                    0x04
+#define FLASH_VOLTAGE_1V85                   0x05
+#define FLASH_VOLTAGE_1V9                    0x06
+#define FLASH_VOLTAGE_1V95                   0x07
+
 /****************************************************************************
  * Public Types
  ****************************************************************************/
@@ -64,7 +73,24 @@ void tlsr82_flash_erase_sector(uint32_t addr);
 void tlsr82_flash_read_data(uint32_t addr, uint8_t *buf, uint32_t len);
 void tlsr82_flash_write_data(uint32_t addr, const uint8_t *buf,
                              uint32_t len);
+uint8_t tlsr82_flash_read_status(uint8_t cmd);
+void tlsr82_flash_write_status(uint8_t type, uint16_t status);
+
+#ifdef CONFIG_TLSR82_FLASH_PROTECT
+void tlsr82_flash_protect(void);
+void tlsr82_flash_unprotect(void);
+#else
+#  define tlsr82_flash_protect()
+#  define tlsr82_flash_unprotect()
+#endif
+
 int tlsr82_flash_miduid_check(uint32_t *pmid, uint8_t *puid);
+
+#ifdef CONFIG_TLSR82_FLASH_CALIBRATE
+void tlsr82_flash_calibrate(uint32_t mid);
+#else
+#  define tlsr82_flash_calibrate(mid)
+#endif
 
 #undef EXTERN
 #if defined(__cplusplus)
