669fb83706c5ee8c93b78e685dba794d28786211
chao.an <anchao@xiaomi.com>
2022-06-16 14:55:36 +0800

    net/tcp(buffered): retransmit only one the earliest not acknowledged segment
    Retransmit only one the earliest not acknowledged segment
    (according to RFC 6298 (5.4)). The issue is the same as it was
    in tcp_send_unbuffered.c and tcp_sendfile.c.
    
    Signed-off-by: chao.an <anchao@xiaomi.com>

--- net/tcp/tcp_send_buffered.c
+++ net/tcp/tcp_send_buffered.c
@@ -323,7 +323,9 @@ static uint16_t psock_send_eventhandler(FAR struct net_driver_s *dev,
    */
 
   FAR struct tcp_conn_s *conn = pvpriv;
-  bool rexmit = false;
+#ifdef CONFIG_NET_TCP_FAST_RETRANSMIT
+  uint32_t rexmitno = 0;
+#endif
 
   /* Get the TCP connection pointer reliably from
    * the corresponding TCP socket.
@@ -501,12 +503,9 @@ static uint16_t psock_send_eventhandler(FAR struct net_driver_s *dev,
                 {
                   /* Do fast retransmit */
 
-                  rexmit = true;
-                }
-              else if ((TCP_WBNACK(wrb) > TCP_FAST_RETRANSMISSION_THRESH) &&
-                       TCP_WBNACK(wrb) == sq_count(&conn->unacked_q) - 1)
-                {
-                  /* Reset the duplicate ack counter */
+                  rexmitno = ackno;
+
+                  /* Reset counter */
 
                   TCP_WBNACK(wrb) = 0;
                 }
@@ -573,14 +572,77 @@ static uint16_t psock_send_eventhandler(FAR struct net_driver_s *dev,
       return flags;
     }
 
+#ifdef CONFIG_NET_TCP_FAST_RETRANSMIT
+  if (rexmitno != 0)
+    {
+      FAR struct tcp_wrbuffer_s *wrb;
+      FAR sq_entry_t *entry;
+      FAR sq_entry_t *next;
+      size_t sndlen;
+
+      /* According to RFC 6298 (5.4), retransmit the earliest segment
+       * that has not been acknowledged by the TCP receiver.
+       */
+
+      for (entry = sq_peek(&conn->unacked_q); entry; entry = next)
+        {
+          wrb = (FAR struct tcp_wrbuffer_s *)entry;
+          next = sq_next(entry);
+
+          if (rexmitno != TCP_WBSEQNO(wrb))
+            {
+              continue;
+            }
+
+          conn->rexmit_seq = rexmitno;
+
+          /* Reconstruct the length of the earliest segment to be
+           * retransmitted.
+           */
+
+          sndlen = TCP_WBPKTLEN(wrb);
+
+          if (sndlen > conn->mss)
+            {
+              sndlen = conn->mss;
+            }
+
+          /* As we are retransmitting, the sequence number is expected
+           * already set for this write buffer.
+           */
+
+          DEBUGASSERT(TCP_WBSEQNO(wrb) != (unsigned)-1);
+          conn->rexmit_seq = TCP_WBSEQNO(wrb);
+
+#ifdef NEED_IPDOMAIN_SUPPORT
+          /* If both IPv4 and IPv6 support are enabled, then we will need to
+           * select which one to use when generating the outgoing packet.
+           * If only one domain is selected, then the setup is already in
+           * place and we need do nothing.
+           */
+
+          send_ipselect(dev, conn);
+#endif
+          /* Then set-up to send that amount of data. (this won't actually
+           * happen until the polling cycle completes).
+           */
+
+          devif_iob_send(dev, TCP_WBIOB(wrb), sndlen, 0);
+
+          /* Reset the retransmission timer. */
+
+          tcp_update_retrantimer(conn, conn->rto);
+
+          /* Continue waiting */
+
+          return flags;
+        }
+    }
+#endif
+
   /* Check if we are being asked to retransmit data */
 
-  else if ((flags & TCP_REXMIT) != 0)
-    {
-      rexmit = true;
-    }
-
-  if (rexmit)
+  if ((flags & TCP_REXMIT) != 0)
     {
       FAR struct tcp_wrbuffer_s *wrb;
       FAR sq_entry_t *entry;
