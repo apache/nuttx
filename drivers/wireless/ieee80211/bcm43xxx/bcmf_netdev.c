31a9429a239719f3378fec9c548d2ced6d2c6dc2
chao.an <anchao@xiaomi.com>
2022-06-25 02:31:21 +0800

    wireless/bcm43xxx: correct the return value
    Signed-off-by: chao.an <anchao@xiaomi.com>

--- drivers/wireless/ieee80211/bcm43xxx/bcmf_netdev.c
+++ drivers/wireless/ieee80211/bcm43xxx/bcmf_netdev.c
@@ -647,7 +647,7 @@ static int bcmf_ifup(FAR struct net_driver_s *dev)
   FAR struct bcmf_dev_s *priv = (FAR struct bcmf_dev_s *)dev->d_private;
   irqstate_t flags;
   uint32_t out_len;
-  int ret;
+  int ret = OK;
 
   /* Disable the hardware interrupt */
 
@@ -726,7 +726,7 @@ errout_in_critical_section:
 
   leave_critical_section(flags);
 
-  return OK;
+  return ret;
 }
 
 /****************************************************************************
