7b4db3956385901538fcc7c28c2d1b0a0ba528e8
Jiuzhu Dong <dongjiuzhu1@xiaomi.com>
2022-06-27 16:45:44 +0800

    driver/syslog: fix messy code in ramlog/syslog.
    VELAPLATFO-3086
    
    Change-Id: Ia706b781a5ac3938b6364567540f8635e0f38172
    Signed-off-by: Jiuzhu Dong <dongjiuzhu1@xiaomi.com>

--- drivers/syslog/ramlog.c
+++ drivers/syslog/ramlog.c
@@ -38,6 +38,7 @@
 #include <assert.h>
 #include <debug.h>
 #include <ctype.h>
+#include <sys/boardctl.h>
 
 #include <nuttx/arch.h>
 #include <nuttx/kmalloc.h>
@@ -233,6 +234,10 @@ static void ramlog_pollnotify(FAR struct ramlog_dev_s *priv,
 static void ramlog_initbuf(void)
 {
   FAR struct ramlog_dev_s *priv = &g_sysdev;
+#ifdef CONFIG_BOARDCTL_RESET_CAUSE
+  struct boardioc_reset_cause_s cause;
+  int ret;
+#endif
   bool is_empty = true;
   char prev;
   char cur;
@@ -243,6 +248,17 @@ static void ramlog_initbuf(void)
     {
       return;
     }
+
+#ifdef CONFIG_BOARDCTL_RESET_CAUSE
+  memset(&cause, 0, sizeof(cause));
+  ret = boardctl(BOARDIOC_RESET_CAUSE, (uintptr_t)&cause);
+  if (ret >= 0 && !cause.cause && !cause.flag)
+    {
+      memset(priv->rl_buffer, 0, priv->rl_bufsize);
+      priv->rl_head = priv->rl_tail = 0;
+      return;
+    }
+#endif
 
   prev = priv->rl_buffer[priv->rl_bufsize - 1];
 
--- drivers/syslog/syslog_rpmsg.c
+++ drivers/syslog/syslog_rpmsg.c
@@ -28,6 +28,7 @@
 #include <ctype.h>
 #include <errno.h>
 #include <string.h>
+#include <sys/boardctl.h>
 
 #ifdef CONFIG_ARCH_LOWPUTC
 #include <nuttx/arch.h>
@@ -438,6 +439,10 @@ ssize_t syslog_rpmsg_write(FAR struct syslog_channel_s *channel,
 void syslog_rpmsg_init_early(FAR void *buffer, size_t size)
 {
   FAR struct syslog_rpmsg_s *priv = &g_syslog_rpmsg;
+#ifdef CONFIG_BOARDCTL_RESET_CAUSE
+  struct boardioc_reset_cause_s cause;
+  int ret;
+#endif
   char prev;
   char cur;
   size_t i;
@@ -449,6 +454,16 @@ void syslog_rpmsg_init_early(FAR void *buffer, size_t size)
 
   priv->buffer = buffer;
   priv->size   = size;
+
+#ifdef CONFIG_BOARDCTL_RESET_CAUSE
+  memset(&cause, 0, sizeof(cause));
+  ret = boardctl(BOARDIOC_RESET_CAUSE, (uintptr_t)&cause);
+  if (ret >= 0 && !cause.cause && !cause.flag)
+    {
+      memset(buffer, 0, size);
+      return;
+    }
+#endif
 
   prev = priv->buffer[size - 1];
 
