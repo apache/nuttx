7477b67853e1d8ce63ea3982c5568e2eff8b727a
chao.an <anchao@xiaomi.com>
2022-07-02 12:58:28 +0800

    wireless/bcm43xxx: discard auth event if netdev down
    Signed-off-by: chao.an <anchao@xiaomi.com>

--- drivers/wireless/ieee80211/bcm43xxx/bcmf_netdev.c
+++ drivers/wireless/ieee80211/bcm43xxx/bcmf_netdev.c
@@ -756,6 +756,10 @@ static int bcmf_ifdown(FAR struct net_driver_s *dev)
 
   if (priv->bc_bifup)
     {
+      /* Mark the device "down" */
+
+      priv->bc_bifup = false;
+
 #ifdef CONFIG_IEEE80211_BROADCOM_LOWPOWER
       if (!work_available(&priv->lp_work))
         {
@@ -765,10 +769,6 @@ static int bcmf_ifdown(FAR struct net_driver_s *dev)
 
       bcmf_wl_enable(priv, false);
       bcmf_wl_active(priv, false);
-
-      /* Mark the device "down" */
-
-      priv->bc_bifup = false;
     }
 
   leave_critical_section(flags);
