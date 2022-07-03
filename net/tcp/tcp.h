da9b0994ebaf7eee1f837e024506cce4acb37ba2
chao.an <anchao@xiaomi.com>
2022-07-04 00:58:44 +0800

    net/tcp: fix regression of invalid update the rexmit_seq in buffer mode
    fix regression of invalid update the rexmit_seq in buffer mode
    rexmit_seq should not be used instead of sndseq in fast retransmission,
    sndseq of retransmission in the packet does not need to be re-updated
    
    Signed-off-by: chao.an <anchao@xiaomi.com>

--- net/tcp/tcp.h
+++ net/tcp/tcp.h
@@ -172,7 +172,10 @@ struct tcp_conn_s
   uint8_t  rcvseq[4];     /* The sequence number that we expect to
                            * receive next */
   uint8_t  sndseq[4];     /* The sequence number that was last sent by us */
+#if !defined(CONFIG_NET_TCP_WRITE_BUFFERS) || \
+    defined(CONFIG_NET_SENDFILE)
   uint32_t rexmit_seq;    /* The sequence number to be retrasmitted */
+#endif
   uint8_t  crefs;         /* Reference counts on this instance */
 #if defined(CONFIG_NET_IPv4) && defined(CONFIG_NET_IPv6)
   uint8_t  domain;        /* IP domain: PF_INET or PF_INET6 */
