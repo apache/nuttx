2ff07778e44757aca3edb3db5a0a4bb49769fa72
Michal Lenc <michallenc@seznam.cz>
2022-07-14 22:54:13 +0200

    samv7: add RX DMA support to serial driver
    This commit adds RX DMA support to serial driver. The DMA is currently
    supported only for USART peripherals, not for UART. It uses two circular
    buffers which size can be setup by SAMV7_SERIAL_RXDMA_BUFFER option.
    
    The idle bus interrupt is enabled to ensures data are read even if the
    buffer is not yet full. The timeout can be setup by
    SAMV7_SERIAL_DMA_TIMEOUT option.
    
    This adds support only for RX transfers.
    
    Signed-off-by: Michal Lenc <michallenc@seznam.cz>

--- arch/arm/src/samv7/sam_serial.h
+++ arch/arm/src/samv7/sam_serial.h
@@ -44,8 +44,64 @@
 #  define SERIAL_HAVE_RS485 1
 #endif
 
+/* Is RX DMA used on the console UART? */
+
+#undef SERIAL_HAVE_CONSOLE_RXDMA
+#if defined(CONFIG_USART0_SERIAL_CONSOLE) && defined(CONFIG_USART0_RXDMA)
+#  define SERIAL_HAVE_CONSOLE_RXDMA
+#elif defined(CONFIG_USART1_SERIAL_CONSOLE) && defined(CONFIG_USART1_RXDMA)
+#  define SERIAL_HAVE_CONSOLE_RXDMA
+#elif defined(CONFIG_USART1_SERIAL_CONSOLE) && defined(CONFIG_USART2_RXDMA)
+#  define SERIAL_HAVE_CONSOLE_RXDMA
+#endif
+
+/* RX DMA ops */
+
+#undef SERIAL_HAVE_NODMA_OPS
+#if !defined(CONFIG_USART0_RXDMA) && defined(CONFIG_SAMV7_USART0)
+#  define SERIAL_HAVE_NODMA_OPS
+#elif !defined(CONFIG_USART1_RXDMA) && defined(CONFIG_SAMV7_USART1)
+#  define SERIAL_HAVE_NODMA_OPS
+#elif !defined(CONFIG_USART2_RXDMA) && defined(CONFIG_SAMV7_USART2)
+#  define SERIAL_HAVE_NODMA_OPS
+#elif defined(CONFIG_SAMV7_UART0)
+#  define SERIAL_HAVE_NODMA_OPS
+#elif defined(CONFIG_SAMV7_UART1)
+#  define SERIAL_HAVE_NODMA_OPS
+#elif defined(CONFIG_SAMV7_UART2)
+#  define SERIAL_HAVE_NODMA_OPS
+#elif defined(CONFIG_SAMV7_UART3)
+#  define SERIAL_HAVE_NODMA_OPS
+#elif defined(CONFIG_SAMV7_UART4)
+#  define SERIAL_HAVE_NODMA_OPS
+#endif
+
+#undef SERIAL_HAVE_RXDMA_OPS
+#if defined(CONFIG_USART0_RXDMA)
+#  define SERIAL_HAVE_RXDMA_OPS
+#elif defined(CONFIG_USART1_RXDMA)
+#  define SERIAL_HAVE_RXDMA_OPS
+#elif defined(CONFIG_USART2_RXDMA)
+#  define SERIAL_HAVE_RXDMA_OPS
+#endif
+
 /****************************************************************************
  * Public Function Prototypes
  ****************************************************************************/
+
+/****************************************************************************
+ * Name: sam_serial_dma_poll
+ *
+ * Description:
+ *   Checks receive DMA buffers for received bytes that have not accumulated
+ *   to the point where the DMA half/full interrupt has triggered.
+ *
+ *   This function should be called from a timer or other periodic context.
+ *
+ ****************************************************************************/
+
+#ifdef SERIAL_HAVE_RXDMA
+void sam_serial_dma_poll(void)
+#endif
 
 #endif /* __ARCH_ARM_SRC_SAMV7_SAM_SERIAL_H */
