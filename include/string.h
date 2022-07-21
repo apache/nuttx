ea82707e9715b56c3c1664467923d4e0952638a8
Karel Kočí <cynerd@email.cz>
2022-07-21 15:50:50 +0200

    libs/libc/string: add memmem
    This function is the GNU extension.

--- include/string.h
+++ include/string.h
@@ -89,6 +89,8 @@ int        memcmp(FAR const void *s1, FAR const void *s2, size_t n);
 FAR void  *memcpy(FAR void *dest, FAR const void *src, size_t n);
 FAR void  *memmove(FAR void *dest, FAR const void *src, size_t count);
 FAR void  *memset(FAR void *s, int c, size_t n);
+FAR void  *memmem(FAR const void *haystack, size_t haystacklen,
+                  FAR const void *needle, size_t needlelen);
 
 void explicit_bzero(FAR void *s, size_t n);
 
