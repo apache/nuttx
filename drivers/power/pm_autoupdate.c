04321f860c9ffade522df6f59c76b28312e9c36a
ligd <liguiding1@xiaomi.com>
2022-07-25 20:25:47 +0800

    pm: add lock for pm_auto_updatestate_cb()
    Change-Id: Iac6c7ed02fbecd4f9fd6bae25a738faf1628bd19
    Signed-off-by: ligd <liguiding1@xiaomi.com>

--- drivers/power/pm_autoupdate.c
+++ drivers/power/pm_autoupdate.c
@@ -42,9 +42,14 @@ static void pm_auto_updatestate_cb(FAR void *arg)
 {
   int domain = (uintptr_t)arg;
   enum pm_state_e newstate;
+  irqstate_t flags;
+
+  flags = pm_lock(domain);
 
   newstate = pm_checkstate(domain);
   pm_changestate(domain, newstate);
+
+  pm_unlock(domain, flags);
 }
 
 /****************************************************************************
