744cc3b401b8f24b5d82a26b31ec4fb304286a14
ligd <liguiding1@xiaomi.com>
2022-07-28 12:11:19 +0800

    backtrace: fix busy loop backtrace
    Situation:
    1. user call assert
    2. crash in assert
    3. crash happend, do backtrace
    4. backtrace interrupt stack successful
    5. busy loop backtrace user stack with CURRENT_REGS
    
    Fix:
    use rtcb->regs instead
    
    Signed-off-by: ligd <liguiding1@xiaomi.com>

--- arch/arm/src/common/arm_assert.c
+++ arch/arm/src/common/arm_assert.c
@@ -60,6 +60,12 @@
 #ifdef CONFIG_ARCH_STACKDUMP
 
 /****************************************************************************
+ * Private Data
+ ****************************************************************************/
+
+static uint8_t s_last_regs[XCPTCONTEXT_SIZE];
+
+/****************************************************************************
  * Private Functions
  ****************************************************************************/
 
@@ -397,7 +403,8 @@ static void arm_dumpstate(void)
     }
   else
     {
-      up_saveusercontext(rtcb->xcp.regs);
+      up_saveusercontext(s_last_regs);
+      rtcb->xcp.regs = (uint32_t *)s_last_regs;
     }
 
   /* Dump the registers */
