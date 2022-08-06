668967209eb6f16f3e9077aa42c99d7ef2fb6ac8
p-eaglelaw <p-laoyufeng1@xiaomi.com>
2022-08-06 11:00:13 +0800

    remove unused lib

--- arch/arm/src/phy62xx/bus_dev.h
+++ arch/arm/src/phy62xx/bus_dev.h
@@ -57,43 +57,50 @@ enum
 
 /* ----  Interrupt Number Definition  ---- */
 
+#if   defined ( __CC_ARM )
+#define M0_IRQ_BASE 0
+#elif defined ( __GNUC__ )
+#define M0_IRQ_BASE 16
+#endif
 typedef enum IRQn
 {
-    /* ----  Cortex-M0 Processor Exceptions Numbers  ---- */
+    /* -------------------  Cortex-M0 Processor Exceptions Numbers  ------------------- */
+    NonMaskableInt_IRQn           = -14+M0_IRQ_BASE,      /*  2 Non Maskable Interrupt */
+    HardFault_IRQn                = -13+M0_IRQ_BASE,      /*  3 HardFault Interrupt */
 
-    NonMaskableInt_IRQn           = -14,      /*  2 Non Maskable Interrupt */
-    HardFault_IRQn                = -13,      /*  3 HardFault Interrupt */
-    SVCall_IRQn                   =  -5,      /* 11 SV Call Interrupt */
-    PendSV_IRQn                   =  -2,      /* 14 Pend SV Interrupt */
-    SysTick_IRQn                  =  -1,      /* 15 System Tick Interrupt */
 
-    /* ----  PHY BUMBEE M0 Interrupt Numbers  ---- */
 
-    BB_IRQn                       =   4,      /* Base band Interrupt */
-    KSCAN_IRQn                    =   5,      /* Key scan Interrupt */
-    RTC_IRQn                      =   6,      /* RTC Timer Interrupt */
+    SVCall_IRQn                   =  -5+M0_IRQ_BASE,      /* 11 SV Call Interrupt */
 
-    WDT_IRQn                      =  10,      /* Watchdog Timer Interrupt */
-    UART0_IRQn                    =  11,      /* UART0 Interrupt */
-    I2C0_IRQn                     =  12,      /* I2C0 Interrupt */
-    I2C1_IRQn                     =  13,      /* I2C1 Interrupt */
-    SPI0_IRQn                     =  14,      /* SPI0 Interrupt */
-    SPI1_IRQn                     =  15,      /* SPI1 Interrupt */
-    GPIO_IRQn                     =  16,      /* GPIO Interrupt */
-    UART1_IRQn                    =  17,      /* UART1 Interrupt */
-    SPIF_IRQn                     =  18,      /* SPIF Interrupt */
-    DMAC_IRQn                     =  19,      /* DMAC Interrupt */
-    TIM1_IRQn                     =  20,      /* Timer1 Interrupt */
-    TIM2_IRQn                     =  21,      /* Timer2 Interrupt */
-    TIM3_IRQn                     =  22,      /* Timer3 Interrupt */
-    TIM4_IRQn                     =  23,      /* Timer4 Interrupt */
-    TIM5_IRQn                     =  24,      /* Timer5 Interrupt */
-    TIM6_IRQn                     =  25,      /* Timer6 Interrupt */
+    PendSV_IRQn                   =  -2+M0_IRQ_BASE,      /* 14 Pend SV Interrupt */
+    SysTick_IRQn                  =  -1+M0_IRQ_BASE,      /* 15 System Tick Interrupt */
 
-    AES_IRQn                      =  28,      /* AES Interrupt */
-    ADCC_IRQn                     =  29,      /* ADC Interrupt */
-    QDEC_IRQn                     =  30,      /* QDEC Interrupt */
-    RNG_IRQn                      =  31,      /* RNG Interrupt */
+    /* ----------------------  PHY BUMBEE M0 Interrupt Numbers  --------------------- */
+    BB_IRQn                       =   4+M0_IRQ_BASE,      /* Base band Interrupt */
+    KSCAN_IRQn                    =   5+M0_IRQ_BASE,      /* Key scan Interrupt */
+    RTC_IRQn                      =   6+M0_IRQ_BASE,      /* RTC Timer Interrupt */
+
+    WDT_IRQn                      =  10+M0_IRQ_BASE,      /* Watchdog Timer Interrupt */
+    UART0_IRQn                    =  11+M0_IRQ_BASE,      /* UART0 Interrupt */
+    I2C0_IRQn                     =  12+M0_IRQ_BASE,      /* I2C0 Interrupt */
+    I2C1_IRQn                     =  13+M0_IRQ_BASE,      /* I2C1 Interrupt */
+    SPI0_IRQn                     =  14+M0_IRQ_BASE,       /* SPI0 Interrupt */
+    SPI1_IRQn                     =  15+M0_IRQ_BASE,       /* SPI1 Interrupt */
+    GPIO_IRQn                     =  16+M0_IRQ_BASE,      /* GPIO Interrupt */
+    UART1_IRQn                    =  17+M0_IRQ_BASE,      /* UART1 Interrupt */
+    SPIF_IRQn                     =  18+M0_IRQ_BASE,      /* SPIF Interrupt */
+    DMAC_IRQn                     =  19+M0_IRQ_BASE,      /* DMAC Interrupt */
+    TIM1_IRQn                     =  20+M0_IRQ_BASE,      /* Timer1 Interrupt */
+    TIM2_IRQn                     =  21+M0_IRQ_BASE,      /* Timer2 Interrupt */
+    TIM3_IRQn                     =  22+M0_IRQ_BASE,      /* Timer3 Interrupt */
+    TIM4_IRQn                     =  23+M0_IRQ_BASE,      /* Timer4 Interrupt */
+    TIM5_IRQn                     =  24+M0_IRQ_BASE,      /* Timer5 Interrupt */
+    TIM6_IRQn                     =  25+M0_IRQ_BASE,      /* Timer6 Interrupt */
+
+    AES_IRQn                      =  28+M0_IRQ_BASE,      /* AES Interrupt */
+    ADCC_IRQn                     =  29+M0_IRQ_BASE,      /* ADC Interrupt */
+    QDEC_IRQn                     =  30+M0_IRQ_BASE,      /* QDEC Interrupt */
+    RNG_IRQn                      =  31+M0_IRQ_BASE      /* RNG Interrupt */
 } IRQn_Type;
 
 #ifdef __cplusplus
