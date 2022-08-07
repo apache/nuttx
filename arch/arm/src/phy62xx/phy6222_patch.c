631ae0032ec1d7705da9f51b66c100987396da28
p-eaglelaw <p-laoyufeng1@xiaomi.com>
2022-08-07 10:34:14 +0800

    fix warning

--- arch/arm/src/phy62xx/phy6222_patch.c
+++ arch/arm/src/phy62xx/phy6222_patch.c
@@ -1,6 +1,11 @@
 #include "types.h"
+#include "rom_sym_def.h"
 #include <arch/irq.h>
 #include "mcu_phy_bumbee.h"
+
+extern int clear_timer_int(AP_TIM_TypeDef* TIMx);
+extern void clear_timer(AP_TIM_TypeDef* TIMx);
+extern void LL_evt_schedule(void);
 
 #ifndef CONFIG_PHY6222_SDK
 void TIM1_IRQHandler1(void)
