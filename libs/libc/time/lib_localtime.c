9a55f42e486cf84b8d6b892aed7e66c7670b78d4
Jiuzhu Dong <dongjiuzhu1@xiaomi.com>
2022-08-12 21:33:46 +0800

    libc/time: Return failed when lock is already held to avoid deadlock
    VELAPLATFO-3492
    
    when enable LIBC_LOCALTIME and SYSLOG_TIMESTAMP_LOCALTIME
    and zoneinfo/posixrulesi isn't exist
    
    [ EMERG] [ap] backtrace:
    [ EMERG] [ap] [20] [<0xc1cc3c4>] arm_switchcontext+0xc/0x10
    [ EMERG] [ap] [20] [<0xc1ac67a>] sem_wait+0x5a/0xc0
    [ EMERG] [ap] [20] [<0xc1c4588>] tzset.part.0+0x8/0x160
    [ EMERG] [ap] [20] [<0xc1c58de>] localtime_r+0xc6/0x114
    [ EMERG] [ap] [20] [<0xc1a910a>] clock_gettime+0x16/0xa8
    [ EMERG] [ap] [20] [<0xc1b7622>] nx_vsyslog+0x9e/0xc0
    [ EMERG] [ap] [20] [<0xc4ba7e0>] romfs_stat+0xa4/0xf4
    [ EMERG] [ap] [20] [<0xc4b47bc>] inode_semtake+0x24/0x5c
    [ EMERG] [ap] [20] [<0xc1c0102>] bsearch+0x26/0x6c
    [ EMERG] [ap] [20] [<0xc1c265a>] vsyslog+0x16/0x24
    [ EMERG] [ap] [20] [<0xc1c2676>] syslog+0xe/0x1c
    [ EMERG] [ap] [20] [<0xc4bae12>] romfs_open+0xee/0x178  <--- err log
    [ EMERG] [ap] [20] [<0xc4b68ae>] file_vopen+0x116/0x144
    [ EMERG] [ap] [20] [<0xc4b6ac8>] open+0x20/0x60
    [ EMERG] [ap] [20] [<0xc1c3df4>] tzload+0x84/0x810
    [ EMERG] [ap] [20] [<0xc1c6ef4>] mm_zalloc+0x10/0x18
    [ EMERG] [ap] [20] [<0xc4bb426>] romfs_checkmount+0x22/0x58
    [ EMERG] [ap] [20] [<0xc4bad74>] romfs_open+0x50/0x178
    [ EMERG] [ap] [20] [<0xc1c392c>] tzparse+0x2c/0x470
    [ EMERG] [ap] [20] [<0xc1ac5da>] nxsem_wait_uninterruptible+0x8a/0xd0
    [ EMERG] [ap] [20] [<0xc1ac690>] sem_wait+0x70/0xc0
    [ EMERG] [ap] [20] [<0xc4b47bc>] inode_semtake+0x24/0x5c
    [ EMERG] [ap] [20] [<0xc4b481c>] inode_semgive+0x28/0x58
    [ EMERG] [ap] [20] [<0xc4b58b4>] file_close+0x18/0x44
    [ EMERG] [ap] [20] [<0xc4b4690>] close+0x80/0xb4
    [ EMERG] [ap] [20] [<0xc1c43f4>] tzload+0x684/0x810
    [ EMERG] [ap] [20] [<0xc1c4616>] tzset.part.0+0x96/0x160
    [ EMERG] [ap] [20] [<0xc1c58de>] localtime_r+0xc6/0x114
    [ EMERG] [ap] [20] [<0xc1a910a>] clock_gettime+0x16/0xa8
    [ EMERG] [ap] [20] [<0xc1ea692>] cmd_date+0x276/0x37c
    
    Change-Id: Ifafc94ccece185c4451ab9fdde0a4df22590bdd7
    Signed-off-by: Jiuzhu Dong <dongjiuzhu1@xiaomi.com>

--- libs/libc/time/lib_localtime.c
+++ libs/libc/time/lib_localtime.c
@@ -58,6 +58,7 @@
 #include <fcntl.h>
 #include <assert.h>
 #include <errno.h>
+#include <unistd.h>
 
 #include <nuttx/fs/fs.h>
 
@@ -301,6 +302,12 @@ struct rule_s
   int_fast32_t r_time;        /* transition time of rule */
 };
 
+struct rsem_s
+{
+  sem_t   lock;               /* Manages exclusive access to file operations */
+  pid_t   holder;             /* The current holder of the semaphore */
+};
+
 /****************************************************************************
  * Private Data
  ****************************************************************************/
@@ -324,8 +331,17 @@ static int g_lcl_isset;
 static int g_gmt_isset;
 static FAR struct state_s *g_lcl_ptr;
 static FAR struct state_s *g_gmt_ptr;
-static sem_t g_lcl_sem = SEM_INITIALIZER(1);
-static sem_t g_gmt_sem = SEM_INITIALIZER(1);
+static struct rsem_s g_lcl_sem =
+{
+  SEM_INITIALIZER(1),
+  INVALID_PROCESS_ID,
+};
+
+static struct rsem_s g_gmt_sem =
+{
+  SEM_INITIALIZER(1),
+  INVALID_PROCESS_ID,
+};
 
 /* Section 4.12.3 of X3.159-1989 requires that
  *    Except for the strftime function, these functions [asctime,
@@ -417,26 +433,41 @@ static int  tzparse(FAR const char *name, FAR struct state_s *sp,
  * Private Functions
  ****************************************************************************/
 
-static void tz_semtake(FAR sem_t *sem)
+static int tz_semtake(FAR struct rsem_s *sem)
 {
-  int errcode = 0;
-  int ret;
+  pid_t pid = gettid();
 
-  do
+  if (pid == sem->holder)
     {
-      ret = _SEM_WAIT(sem);
-      if (ret < 0)
-        {
-          errcode = _SEM_ERRNO(ret);
-          DEBUGASSERT(errcode == EINTR || errcode == ECANCELED);
-        }
+      return -EAGAIN;
     }
-  while (ret < 0 && errcode == EINTR);
+  else
+    {
+      int errcode = 0;
+      int ret;
+
+      do
+        {
+          ret = _SEM_WAIT(&sem->lock);
+          if (ret < 0)
+            {
+              errcode = _SEM_ERRNO(ret);
+              DEBUGASSERT(errcode == EINTR || errcode == ECANCELED);
+            }
+        }
+      while (ret < 0 && errcode == EINTR);
+
+      sem->holder = pid;
+    }
+
+  return 0;
 }
 
-static void tz_semgive(FAR sem_t *sem)
+static void tz_semgive(FAR struct rsem_s *sem)
 {
-  DEBUGVERIFY(_SEM_POST(sem));
+  DEBUGASSERT(sem->holder == gettid());
+  sem->holder = INVALID_PROCESS_ID;
+  DEBUGVERIFY(_SEM_POST(&sem->lock));
 }
 
 static int_fast32_t detzcode(FAR const char *codep)
@@ -1826,7 +1857,11 @@ static FAR struct tm *gmtsub(FAR const time_t *timep,
         }
 #endif
 
-      tz_semtake(&g_gmt_sem);
+      if (tz_semtake(&g_gmt_sem) < 0)
+        {
+          return NULL;
+        }
+
       if (!g_gmt_isset)
         {
           g_gmt_ptr = lib_malloc(sizeof *g_gmt_ptr);
@@ -2544,7 +2579,11 @@ void tzset(void)
     }
 #endif
 
-  tz_semtake(&g_lcl_sem);
+  if (tz_semtake(&g_lcl_sem) < 0)
+    {
+      return;
+    }
+
   name = getenv("TZ");
   if (name == NULL)
     {
