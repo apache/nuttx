699f0e4e2b41e88c626a8fd3174e935f5a410a1b
Cis van Mierlo <cis.van.mierlo@nxp.com>
2022-08-19 11:05:45 +0200

    S32K1xx: Fixed PM bug for clockconfig

--- arch/arm/src/s32k1xx/s32k1xx_pminitialize.c
+++ arch/arm/src/s32k1xx/s32k1xx_pminitialize.c
@@ -25,6 +25,7 @@
 #include <nuttx/config.h>
 #include <nuttx/power/pm.h>
 
+#include "s32k1xx_clockconfig.h"
 #include "arm_internal.h"
 
 #ifdef CONFIG_PM
@@ -56,6 +57,10 @@ void arm_pminitialize(void)
   /* Initialize the NuttX power management subsystem proper */
 
   pm_initialize();
+
+  /* Register clockconfig first to receive power management callbacks */
+
+  s32k1xx_clock_pm_register();
 }
 
 #endif /* CONFIG_PM */
