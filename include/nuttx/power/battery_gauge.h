86202cdcae3bdcd92a433e8269bd2332785bbd13
chentao9 <chentao9@xiaomi.com>
2022-08-23 19:06:58 +0800

    ref: charge: move charge driver to separate repository(1/2)
    CHAMPION-9279
    
    move charge driver to separate repository
    
    dependson:855709 partner_8249 partner_8348
    
    Signed-off-by: chentao9 <chentao9@xiaomi.com>
    Change-Id: I6de1e8a489fbec6e0a6bb36d5a57bf04768e567d

--- include/nuttx/power/battery_gauge.h
+++ include/nuttx/power/battery_gauge.h
@@ -251,41 +251,6 @@ FAR struct battery_gauge_dev_s *max1704x_initialize(
                                                 uint32_t frequency);
 #endif
 
-/****************************************************************************
- * Name: cw2218_initialize
- *
- * Description:
- *   Initialize the cw2218 battery driver and return an instance of the
- *   lower_half interface that may be used with battery_register();
- *
- *   This driver requires:
- *
- *   CONFIG_BATTERY - Upper half battery driver support
- *   CONFIG_I2C - I2C support
- *   CONFIG_CW2218 - And the driver must be explicitly selected.
- *
- * Input Parameters:
- *   i2c - An instance of the I2C interface to use to communicate with the bq
- *   addr - The I2C address of the cw2218 (Better be 0x55).
- *   frequency - The I2C frequency
- *
- * Returned Value:
- *   A pointer to the initializeed lower-half driver instance. A NULL
- *   pointer is returned on a failure to initialize the cw2218 lower half.
- *
- ****************************************************************************/
- #if defined(CONFIG_I2C) &&  defined(CONFIG_CW2218)
-
-struct i2c_master_s; /* Forward reference */
-
-FAR struct battery_gauge_dev_s *cw2218_initialize(
-                                                FAR struct i2c_master_s *i2c,
-                                                FAR struct ioexpander_dev_s *dev,
-                                                uint8_t addr,
-                                                uint32_t frequency,
-                                                int int_pin);
-#endif
-
 #undef EXTERN
 #ifdef __cplusplus
 }
