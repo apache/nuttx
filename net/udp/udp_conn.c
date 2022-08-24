77e4536cb11b78f910487c6c9c133fcb65f1999a
zhanghongyu <zhanghongyu@xiaomi.com>
2022-08-24 15:06:59 +0800

    udp: Ipv4/6 can be bound to the same port
    VELAPLATFO-2660
    
    Signed-off-by: zhanghongyu <zhanghongyu@xiaomi.com>
    Change-Id: Icf4317eecb1afc3ead69800fd04a5c10cbc5152e

--- net/udp/udp_conn.c
+++ net/udp/udp_conn.c
@@ -145,7 +145,7 @@ static FAR struct udp_conn_s *udp_find_conn(uint8_t domain,
       if (domain == PF_INET)
 #endif
         {
-          if (conn->lport == portno &&
+          if (conn->domain == PF_INET && conn->lport == portno &&
               (net_ipv4addr_cmp(conn->u.ipv4.laddr, ipaddr->ipv4.laddr) ||
                net_ipv4addr_cmp(conn->u.ipv4.laddr, INADDR_ANY)))
             {
@@ -159,7 +159,7 @@ static FAR struct udp_conn_s *udp_find_conn(uint8_t domain,
       else
 #endif
         {
-          if (conn->lport == portno &&
+          if (conn->domain == PF_INET6 && conn->lport == portno &&
               (net_ipv6addr_cmp(conn->u.ipv6.laddr, ipaddr->ipv6.laddr) ||
                net_ipv6addr_cmp(conn->u.ipv6.laddr, g_ipv6_unspecaddr)))
             {
