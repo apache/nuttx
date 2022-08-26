38af5befad69e56c4d8f5d1a0ac6537e0363420f
Xiang Xiao <xiaoxiang@xiaomi.com>
2022-08-26 14:54:42 +0800

    bt_bridge: Fix error: address argument to atomic operation must be a pointer to _Atomic type
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- drivers/wireless/bluetooth/bt_bridge.c
+++ drivers/wireless/bluetooth/bt_bridge.c
@@ -69,7 +69,7 @@ struct bt_bridge_s
 #ifdef CONFIG_BLUETOOTH_BRIDGE_BTSNOOP
   FAR struct snoop_s       *snoop;
 #endif /* CONFIG_BLUETOOTH_BRIDGE_BTSNOOP */
-  uint8_t                   refs;
+  atomic_uint               refs;
 };
 
 /****************************************************************************
