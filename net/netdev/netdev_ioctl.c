5a0d67abe88d587f000e7d91104e715f3093a5c1
Xiang Xiao <xiaoxiang@xiaomi.com>
2022-08-30 13:46:46 +0800

    fs: Run the default action of FIONBIO/FIOCLEX/FIONCLEX in success path
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- net/netdev/netdev_ioctl.c
+++ net/netdev/netdev_ioctl.c
@@ -1516,7 +1516,7 @@ static int netdev_rt_ioctl(FAR struct socket *psock, int cmd,
 static int netdev_file_ioctl(FAR struct socket *psock, int cmd,
                              unsigned long arg)
 {
-  int ret;
+  int ret = OK;
 
   switch (cmd)
     {
@@ -1545,8 +1545,6 @@ static int netdev_file_ioctl(FAR struct socket *psock, int cmd,
                  {
                    conn->s_flags &= ~_SF_NONBLOCK;
                  }
-
-               ret = -ENOTTY; /* let file_vioctl update f_oflags */
             }
           else
             {
