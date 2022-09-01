f5df800655b6a0b6743c3accfdd2d2749ccf49a6
Masayuki Ishikawa <masayuki.ishikawa@gmail.com>
2022-09-01 11:29:16 +0900

    Revert "net/usrsock: read from the closed remote should return EOF"
    This reverts commit 3a653c1d5e7f498ac1a1cda135c68ab3451c43ee.

--- net/usrsock/usrsock_recvmsg.c
+++ net/usrsock/usrsock_recvmsg.c
@@ -443,12 +443,6 @@ errout_unlock:
       *fromlen = outaddrlen;
     }
 
-  if (conn->flags & USRSOCK_EVENT_REMOTE_CLOSED &&
-      ret == -ENOTCONN)
-    {
-      ret = OK;
-    }
-
   return ret;
 }
 
