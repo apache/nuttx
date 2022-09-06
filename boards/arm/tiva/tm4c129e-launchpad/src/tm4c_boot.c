b04ae46ea62a4dba8ee5344ba89f6a80b709dd57
Nathan Hartman <59230071+hartmannathan@users.noreply.github.com>
2022-09-06 18:57:09 -0400

    boards/arm/tiva: Add tm4c129e-launchpad
    * boards/arm/tiva/tm4c129e-launchpad: New subdirectory providing
      board support for the Texas Instruments TM4C Crypto Connected
      LaunchPad, or more correctly the EK-TM4C129EXL.
    
    * Documentation/introduction/detailed_support.rst,
      Documentation/introduction/supported_platforms.rst,
      boards/README.txt: Document the additional board support.
    
    * boards/Kconfig:
      (ARCH_BOARD_TM4C129E_LAUNCHPAD): New config.
      (ARCH_BOARD): Add tm4c129e-launchpad.
      (Board-Specific Options): Source the board-specific Kconfig
       boards/arm/tiva/tm4c129e-launchpad/Kconfig when selected.

---
+++ boards/arm/tiva/tm4c129e-launchpad/src/tm4c_boot.c
@@ -0,0 +1,103 @@
+/****************************************************************************
+ * boards/arm/tiva/tm4c129e-launchpad/src/tm4c_boot.c
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+#include <debug.h>
+
+#include <nuttx/board.h>
+#include <arch/board/board.h>
+
+#include "tm4c129e-launchpad.h"
+#include "arm_internal.h"
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Private Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: tiva_boardinitialize
+ *
+ * Description:
+ *   All Tiva architectures must provide the following entry point.
+ *   This entry point is called early in the initialization -- after all
+ *   memory has been configured and mapped but before any devices have been
+ *   initialized.
+ *
+ ****************************************************************************/
+
+void tiva_boardinitialize(void)
+{
+  /* Configure SPI chip selects if
+   * 1) SSI is not disabled, and
+   * 2) the weak function
+   * tm4c_ssidev_initialize() has been brought into the link.
+   */
+
+  /* The DK-TM4C129X microSD CS and OLED are on SSI0 */
+
+#if defined(CONFIG_TIVA_SSI0) || defined(CONFIG_TIVA_SSI1)
+  if (tm4c_ssidev_initialize)
+    {
+      tm4c_ssidev_initialize();
+    }
+#endif
+
+  /* Configure on-board LEDs if LED support has been selected. */
+
+#ifdef CONFIG_ARCH_LEDS
+  tm4c_led_initialize();
+#endif
+}
+
+/****************************************************************************
+ * Name: board_late_initialize
+ *
+ * Description:
+ *   If CONFIG_BOARD_LATE_INITIALIZE is selected, then an additional
+ *   initialization call will be performed in the boot-up sequence to a
+ *   function called board_late_initialize().  board_late_initialize() will
+ *   be called immediately after up_initialize() is called and just before
+ *   the initial application is started.  This additional initialization
+ *   phase may be used, for example, to initialize board-specific device
+ *   drivers.
+ *
+ ****************************************************************************/
+
+#ifdef CONFIG_BOARD_LATE_INITIALIZE
+void board_late_initialize(void)
+{
+  /* Perform board initialization */
+
+  tm4c_bringup();
+}
+#endif /* CONFIG_BOARD_LATE_INITIALIZE */
