9d825be7519c91b656ece77b412cc930970fd486
ligd <liguiding1@xiaomi.com>
2022-09-16 23:29:37 +0800

    armv7a/r: add isb to cp15_invalidate_icache()
    VELAPLATFO-3938
    
    Incase invalidate_icache hasn't done
    
    Change-Id: I31045dfb75020a653c61d208089442ec5f383550
    Signed-off-by: ligd <liguiding1@xiaomi.com>

--- arch/arm/src/armv7-r/cp15_cacheops.h
+++ arch/arm/src/armv7-r/cp15_cacheops.h
@@ -346,6 +346,7 @@
 .macro cp15_invalidate_icache, tmp
   mov \tmp, #0
   mrc p15, 0, \tmp, c7, c5, 0 /* ICIALLU */
+  isb
 .endm
 
 /****************************************************************************
@@ -686,6 +687,7 @@ static inline void cp15_invalidate_btb_inner_sharable(void)
 static inline void cp15_invalidate_icache(void)
 {
   CP15_SET(ICIALLU, 0);
+  ARM_ISB();
 }
 
 /****************************************************************************
