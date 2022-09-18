ae083f7b9389e9fe75819c1bd1ebfd1f91703255
Xiang Xiao <xiaoxiang@xiaomi.com>
2022-09-18 17:25:05 +0800

    arm/rtl8720c: Remove up_trigger_irq since it is implemented in arm_trigger_irq.c
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- arch/arm/src/rtl8720c/ameba_nvic.c
+++ arch/arm/src/rtl8720c/ameba_nvic.c
@@ -260,45 +260,6 @@ void up_enable_irq(int irq)
 }
 
 /****************************************************************************
- * Name: up_trigger_irq
- *
- * Description:
- *   Trigger an IRQ by software.
- *
- ****************************************************************************/
-
-void up_trigger_irq(int irq)
-{
-  uint32_t pend_bit = 0;
-  DEBUGASSERT(irq >= NVIC_IRQ_NMI && irq < NR_IRQS);
-  if (irq >= NVIC_IRQ_FIRST)
-    {
-      putreg32(irq - NVIC_IRQ_FIRST, NVIC_STIR);
-    }
-
-  else
-    {
-      switch (irq)
-        {
-        case NVIC_IRQ_PENDSV:
-          pend_bit = NVIC_INTCTRL_PENDSVSET;
-          break;
-        case NVIC_IRQ_NMI:
-          pend_bit = NVIC_INTCTRL_NMIPENDSET;
-          break;
-        case NVIC_IRQ_SYSTICK:
-          pend_bit = NVIC_INTCTRL_PENDSTSET;
-          break;
-        }
-
-      if (pend_bit)
-        {
-          modifyreg32(NVIC_INTCTRL, 0, pend_bit);
-        }
-    }
-}
-
-/****************************************************************************
  * Name: up_prioritize_irq
  *
  * Description:
