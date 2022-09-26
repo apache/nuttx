f6817c6a45085b9ae97df92b527b07cd7ec50b0c
yinshengkai <yinshengkai@xiaomi.com>
2022-09-26 15:12:55 +0800

    tool: sync mainline mkallsyms.py
    VELAPLATFO-3848
    
    Signed-off-by: yinshengkai <yinshengkai@xiaomi.com>
    Change-Id: Ibb7fdd07902e300a3443fbbb97f64f1538694889

--- tools/mkallsyms.py
+++ tools/mkallsyms.py
@@ -19,83 +19,93 @@
 #
 ############################################################################
 
-import os
-import sys
 import errno
+import os
 import re
-import cxxfilt
+import sys
 
-from elftools import __version__
-from elftools.elf.elffile import ELFFile
-from elftools.elf.sections import (
-    NoteSection, SymbolTableSection, SymbolTableIndexSection
-)
+try:
+    import cxxfilt
+    from elftools import __version__
+    from elftools.elf.elffile import ELFFile
+    from elftools.elf.sections import SymbolTableSection
+
+except ModuleNotFoundError:
+    print("Please execute the following command to install dependencies:")
+    print("pip install pyelftools cxxfilt")
+
 
 class SymbolTables(object):
-
-    def __init__(self, file, output):
-        self.elffile = ELFFile(file)
+    def __init__(self, elffile, output):
+        try:
+            file = open(elffile, "rb")
+            self.elffile = ELFFile(file)
+        except FileNotFoundError:
+            self.elffile = None
         self.output = output
         self.symbol_list = []
 
     def symbol_filter(self, symbol):
-        if symbol['st_info']['type'] != 'STT_FUNC':
+        if symbol["st_info"]["type"] != "STT_FUNC":
             return None
-        if symbol['st_info']['bind'] == 'STB_WEAK':
+        if symbol["st_info"]["bind"] == "STB_WEAK":
             return None
-        if symbol['st_shndx'] == 'SHN_UNDEF':
+        if symbol["st_shndx"] == "SHN_UNDEF":
             return None
         return symbol
 
-    def print_symbol_tables(self, noconst=False):
-        if noconst == False:
-            noconst = 'const'
-        else:
-            noconst = ''
+    def print_symbol_tables(self, isnoconst=False):
+        noconst = "const"
+        if not isnoconst:
+            noconst = ""
+
         self.emitline("#include <nuttx/compiler.h>")
         self.emitline("#include <nuttx/symtab.h>\n")
-        self.emitline("%s int g_nallsyms = %d + 2;" %
-                      (noconst, len(self.symbol_list)))
-        self.emitline("%s struct symtab_s g_allsyms[%d + 2] =\n{" %
-                        (noconst, len(self.symbol_list)))
-        self.emitline("  { \"Unknown\", (FAR %s void *)0x00000000 }," % (noconst))
-        for symbol in self.symbol_list:
-            self.emitline("  { \"%s\", (FAR %s void *)%s }," %
-                          (symbol[1], noconst, hex(symbol[0])))
+        self.emitline("%s int g_nallsyms = %d + 2;" % (noconst, len(self.symbol_list)))
         self.emitline(
-            "  { \"Unknown\", (FAR %s void *)0xffffffff }\n};" % (noconst))
+            "%s struct symtab_s g_allsyms[%d + 2] =\n{"
+            % (noconst, len(self.symbol_list))
+        )
+        self.emitline('  { "Unknown", (FAR %s void *)0x00000000 },' % (noconst))
+        for symbol in self.symbol_list:
+            self.emitline(
+                '  { "%s", (FAR %s void *)%s },' % (symbol[1], noconst, hex(symbol[0]))
+            )
+        self.emitline('  { "Unknown", (FAR %s void *)0xffffffff }\n};' % (noconst))
 
     def get_symtable(self):
-        symbol_tables = [(idx, s) for idx, s in enumerate(self.elffile.iter_sections())
-                         if isinstance(s, SymbolTableSection)]
+        symbol_tables = [
+            (idx, s)
+            for idx, s in enumerate(self.elffile.iter_sections())
+            if isinstance(s, SymbolTableSection)
+        ]
 
         if not symbol_tables and self.elffile.num_sections() == 0:
-            self.emitline('')
+            self.emitline("")
             return
 
         for section_index, section in symbol_tables:
             if not isinstance(section, SymbolTableSection):
                 continue
 
-            if section['sh_entsize'] == 0 or section.name != '.symtab':
+            if section["sh_entsize"] == 0 or section.name != ".symtab":
                 continue
 
             return section
 
     def parse_symbol(self):
+        if self.elffile is None:
+            return
         symtable = self.get_symtable()
         for nsym, symbol in enumerate(symtable.iter_symbols()):
-            if self.symbol_filter(symbol) != None:
-                try:
-                    symbol_name = cxxfilt.demangle(symbol.name)
-                except:
-                    continue
-                func_name = re.sub(r'\(.*$', '', symbol_name)
-                self.symbol_list.append((symbol['st_value'], func_name))
+            if self.symbol_filter(symbol) is not None:
+                symbol_name = cxxfilt.demangle(symbol.name)
+                func_name = re.sub(r"\(.*$", "", symbol_name)
+                self.symbol_list.append((symbol["st_value"], func_name))
         self.symbol_list = sorted(self.symbol_list, key=lambda item: item[0])
 
-    def emitline(self, s=''):
-        self.output.write(str(s) + '\n')
+    def emitline(self, s=""):
+        self.output.write(str(s) + "\n")
 
 
 def usage():
@@ -109,11 +119,11 @@ def parse_args(argv):
     outfile = None
     elffile = None
 
-    if argc > index and argv[index] == '--version':
-        print('mkallsyms.py: based on pyelftools %s' % __version__)
-        os._exit(0)
+    if argc > index and argv[index] == "--version":
+        print("mkallsyms.py: based on pyelftools %s" % __version__)
+        os.exit(0)
 
-    if argc > index and argv[index] == 'noconst':
+    if argc > index and argv[index] == "noconst":
         noconst = True
         index += 1
     else:
@@ -124,20 +134,15 @@ def parse_args(argv):
         index += 1
 
     if argc > index:
-        outfile = open(argv[index], 'w')
+        outfile = open(argv[index], "w")
     else:
         outfile = sys.stdout
 
-    if os.path.exists(elffile) != True:
-        usage()
     return noconst, elffile, outfile
 
 
-if __name__ == '__main__':
-    index = 1
+if __name__ == "__main__":
     noconst, elffile, outfile = parse_args(sys.argv)
-
-    with open(elffile, 'rb') as file:
-        readelf = SymbolTables(file, outfile)
-        readelf.parse_symbol()
-        readelf.print_symbol_tables(noconst)
+    readelf = SymbolTables(elffile, outfile)
+    readelf.parse_symbol()
+    readelf.print_symbol_tables(noconst)
