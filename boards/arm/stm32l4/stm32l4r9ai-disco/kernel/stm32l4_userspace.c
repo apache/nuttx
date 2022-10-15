4b62c69eb3d5a58181205565344ca6b702b4b6ae
Xiang Xiao <xiaoxiang@xiaomi.com>
2022-10-16 01:53:23 +0800

    boards: Remove the duplicated prototype of CONFIG_INIT_ENTRYPOINT
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- boards/arm/stm32l4/stm32l4r9ai-disco/kernel/stm32l4_userspace.c
+++ boards/arm/stm32l4/stm32l4r9ai-disco/kernel/stm32l4_userspace.c
@@ -61,10 +61,6 @@ extern uint8_t _edata[];           /* End+1 of .data */
 extern uint8_t _sbss[];            /* Start of .bss */
 extern uint8_t _ebss[];            /* End+1 of .bss */
 
-/* This is the user space entry point */
-
-int CONFIG_INIT_ENTRYPOINT(int argc, char *argv[]);
-
 const struct userspace_s userspace locate_data(".userspace") =
 {
   /* General memory map */
