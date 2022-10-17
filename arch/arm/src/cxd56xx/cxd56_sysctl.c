xiaoxiang781216/incubator-nuttx

Replace nxsem API when used as a lock with nxmutex API
Signed-off-by: anjiahao <anjiahao@xiaomi.com>
Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

arch/arm/src/cxd56xx/cxd56_sysctl.c
@@ -27,6 +27,7 @@
 #include <nuttx/arch.h>
 #include <nuttx/fs/fs.h>
 #include <nuttx/fs/ioctl.h>
+#include <nuttx/mutex.h>
 #include <nuttx/semaphore.h>
 
 #include <stdio.h>
@@ -57,8 +58,6 @@
  ****************************************************************************/
 
 static int  sysctl_ioctl(struct file *filep, int cmd, unsigned long arg);
-static int  sysctl_semtake(sem_t *semid);
-static void sysctl_semgive(sem_t *semid);
 static int  sysctl_rxhandler(int cpuid, int protoid,
                              uint32_t pdata, uint32_t data,
                              void *userdata);
@@ -67,7 +66,7 @@ static int  sysctl_rxhandler(int cpuid, int protoid,
  * Private Data
  ****************************************************************************/
 
-static sem_t g_exc;
+static mutex_t g_exc;
 static sem_t g_sync;
 static int g_errcode = 0;
 
@@ -96,16 +95,6 @@ static int sysctl_ioctl(struct file *filep, int cmd, unsigned long arg)
   return ret;
 }
 
-static int sysctl_semtake(sem_t *semid)
-{
-  return nxsem_wait_uninterruptible(semid);
-}
-
-static void sysctl_semgive(sem_t *semid)
-{
-  nxsem_post(semid);
-}
-
 static int sysctl_rxhandler(int cpuid, int protoid,
                             uint32_t pdata, uint32_t data,
                             void *userdata)
@@ -115,8 +104,7 @@ static int sysctl_rxhandler(int cpuid, int protoid,
 
   g_errcode = (int)data;
 
-  sysctl_semgive(&g_sync);
-
+  nxsem_post(&g_sync);
   return OK;
 }
 
@@ -131,7 +119,7 @@ int cxd56_sysctlcmd(uint8_t id, uint32_t data)
 
   /* Get exclusive access */
 
-  ret = sysctl_semtake(&g_exc);
+  ret = nxmutex_lock(&g_exc);
   if (ret < 0)
     {
       return ret;
@@ -146,17 +134,17 @@ int cxd56_sysctlcmd(uint8_t id, uint32_t data)
   ret = cxd56_iccsend(CXD56_PROTO_SYSCTL, &msg, SYSCTL_TIMEOUT);
   if (ret < 0)
     {
-      sysctl_semgive(&g_exc);
+      nxmutex_unlock(&g_exc);
       _err("Timeout.\n");
       return ret;
     }
 
   /* Wait for reply message from system CPU */
 
-  ret = sysctl_semtake(&g_sync);
+  ret = nxsem_wait_uninterruptible(&g_sync);
   if (ret < 0)
     {
-      sysctl_semgive(&g_exc);
+      nxmutex_unlock(&g_exc);
       return ret;
     }
 
@@ -164,8 +152,7 @@ int cxd56_sysctlcmd(uint8_t id, uint32_t data)
 
   ret = g_errcode;
 
-  sysctl_semgive(&g_exc);
-
+  nxmutex_unlock(&g_exc);
   return ret;
 }
 
@@ -173,7 +160,7 @@ void cxd56_sysctlinitialize(void)
 {
   cxd56_iccinit(CXD56_PROTO_SYSCTL);
 
-  nxsem_init(&g_exc, 0, 1);
+  nxmutex_init(&g_exc);
   nxsem_init(&g_sync, 0, 0);
   nxsem_set_protocol(&g_sync, SEM_PRIO_NONE);
 

arch/arm/src/lpc54xx/lpc54_dma.c
@@ -32,6 +32,7 @@
 #include <debug.h>
 
 #include <nuttx/arch.h>
+#include <nuttx/mutex.h>
 
 #include "arm_internal.h"
 #include "hardware/lpc54_inputmux.h"
@@ -59,7 +60,7 @@ struct lpc54_dmach_s
 
 struct lpc54_dma_s
 {
-  sem_t exclsem;           /* For exclusive access to the DMA channel list */
+  mutex_t excllock;       /* For exclusive access to the DMA channel list */
 
   /* This is the state of each DMA channel */
 
@@ -233,7 +234,7 @@ void weak_function arm_dma_initialize(void)
 
   /* Initialize the DMA state structure */
 
-  nxsem_init(&g_dma.exclsem, 0, 1);
+  nxmutex_init(&g_dma.excllock);
 
   /* Set the SRAMBASE to the beginning a array of DMA descriptors, one for
    * each DMA channel.
@@ -295,7 +296,7 @@ int lpc54_dma_setup(int ch, uint32_t cfg, uint32_t xfrcfg, uint8_t trigsrc,
 
   /* Get exclusive access to the DMA data structures and interface */
 
-  ret = nxsem_wait(&g_dma.exclsem);
+  ret = nxmutex_lock(&g_dma.excllock);
   if (ret < 0)
     {
       return ret;
@@ -307,7 +308,7 @@ int lpc54_dma_setup(int ch, uint32_t cfg, uint32_t xfrcfg, uint8_t trigsrc,
   if (dmach->inuse)
     {
       ret = -EBUSY;
-      goto errout_with_exclsem;
+      goto errout_with_excllock;
     }
 
   dmach->inuse = true;
@@ -437,8 +438,8 @@ int lpc54_dma_setup(int ch, uint32_t cfg, uint32_t xfrcfg, uint8_t trigsrc,
   putreg32(xfrcfg, base + LPC54_DMA_XFERCFG_OFFSET);
   ret = OK;
 
-errout_with_exclsem:
-  nxsem_post(&g_dma.exclsem);
+errout_with_excllock:
+  nxmutex_unlock(&g_dma.exclock);
   return ret;
 }
 

arch/arm/src/s32k3xx/s32k3xx_lpspi.c
@@ -60,7 +60,7 @@
 
 #include <nuttx/irq.h>
 #include <nuttx/arch.h>
-#include <nuttx/semaphore.h>
+#include <nuttx/mutex.h>
 #include <nuttx/spi/spi.h>
 #include <nuttx/power/pm.h>
 
@@ -117,7 +117,7 @@ struct s32k3xx_lpspidev_s
 #ifdef CONFIG_S32K3XX_LPSPI_INTERRUPTS
   uint8_t spiirq;             /* SPI IRQ number */
 #endif
-  sem_t exclsem;              /* Held while chip is selected for mutual exclusion */
+  mutex_t excllock;           /* Held while chip is selected for mutual exclusion */
   uint32_t frequency;         /* Requested clock frequency */
   uint32_t actual;            /* Actual clock frequency */
   int8_t nbits;               /* Width of word in bits */
@@ -1052,11 +1052,11 @@ static int s32k3xx_lpspi_lock(struct spi_dev_s *dev, bool lock)
 
   if (lock)
     {
-      ret = nxsem_wait_uninterruptible(&priv->exclsem);
+      ret = nxmutex_lock(&priv->excllock);
     }
   else
     {
-      ret = nxsem_post(&priv->exclsem);
+      ret = nxmutex_unlock(&priv->excllock);
     }
 
   return ret;
@@ -2032,9 +2032,9 @@ static void s32k3xx_lpspi_bus_initialize(struct s32k3xx_lpspidev_s *priv)
 
   s32k3xx_lpspi_setmode((struct spi_dev_s *)priv, SPIDEV_MODE0);
 
-  /* Initialize the SPI semaphore that enforces mutually exclusive access */
+  /* Initialize the SPI mutex that enforces mutually exclusive access */
 
-  nxsem_init(&priv->exclsem, 0, 1);
+  nxmutex_init(&priv->excllock);
 
   /* Enable LPSPI */
 

drivers/mtd/mtd_nand.c
@@ -77,11 +77,6 @@
  * Private Function Prototypes
  ****************************************************************************/
 
-/* NAND locking */
-
-static int      nand_lock(FAR struct nand_dev_s *nand);
-#define         nand_unlock(n) nxsem_post(&(n)->exclsem)
-
 /* Bad block checking */
 
 #ifdef CONFIG_MTD_NAND_BLOCKCHECK
@@ -119,26 +114,6 @@ static int     nand_ioctl(struct mtd_dev_s *dev, int cmd,
 /****************************************************************************
  * Private Functions
  ****************************************************************************/
-
-/****************************************************************************
- * Name: nand_lock
- *
- * Description:
- *   Get exclusive access to the nand.
- *
- * Input Parameters:
- *   nand  - Pointer to a struct nand_dev_s instance.
- *   block - Number of block to check.
- *
- * Returned Value:
- *   OK on success; a negated errno value on failure.
- *
- ****************************************************************************/
-
-static int nand_lock(FAR struct nand_dev_s *nand)
-{
-  return nxsem_wait(&nand->exclsem);
-}
 
 /****************************************************************************
  * Name: nand_checkblock
@@ -548,7 +523,7 @@ static int nand_erase(struct mtd_dev_s *dev, off_t startblock,
 
   /* Lock access to the NAND until we complete the erase */
 
-  nand_lock(nand);
+  nxmutex_lock(&nand->excllock);
   while (blocksleft-- > 0)
     {
       /* Erase each sector */
@@ -558,14 +533,14 @@ static int nand_erase(struct mtd_dev_s *dev, off_t startblock,
         {
           ferr("ERROR: nand_eraseblock failed on block %ld: %d\n",
                (long)startblock, ret);
-          nand_unlock(nand);
+          nxmutex_unlock(&nand->excllock);
           return ret;
         }
 
       startblock++;
     }
 
-  nand_unlock(nand);
+  nxmutex_unlock(&nand->excllock);
   return (int)nblocks;
 }
 
@@ -614,7 +589,7 @@ static ssize_t nand_bread(struct mtd_dev_s *dev, off_t startpage,
 
   /* Lock access to the NAND until we complete the read */
 
-  nand_lock(nand);
+  nxmutex_lock(&nand->excllock);
 
   /* Then read every page from NAND */
 
@@ -657,11 +632,11 @@ static ssize_t nand_bread(struct mtd_dev_s *dev, off_t startpage,
       buffer += pagesize;
     }
 
-  nand_unlock(nand);
+  nxmutex_unlock(&nand->excllock);
   return npages;
 
 errout_with_lock:
-  nand_unlock(nand);
+  nxmutex_unlock(&nand->excllock);
   return ret;
 }
 
@@ -710,7 +685,7 @@ static ssize_t nand_bwrite(struct mtd_dev_s *dev, off_t startpage,
 
   /* Lock access to the NAND until we complete the write */
 
-  nand_lock(nand);
+  nxmutex_lock(&nand->excllock);
 
   /* Then write every page into NAND */
 
@@ -753,11 +728,11 @@ static ssize_t nand_bwrite(struct mtd_dev_s *dev, off_t startpage,
       buffer += pagesize;
     }
 
-  nand_unlock(nand);
+  nxmutex_unlock(&nand->excllock);
   return npages;
 
 errout_with_lock:
-  nand_unlock(nand);
+  nxmutex_unlock(&nand->excllock);
   return ret;
 }
 
@@ -967,7 +942,7 @@ FAR struct mtd_dev_s *nand_initialize(FAR struct nand_raw_s *raw)
   nand->mtd.ioctl  = nand_ioctl;
   nand->raw        = raw;
 
-  nxsem_init(&nand->exclsem, 0, 1);
+  nxmutex_init(&nand->excllock);
 
 #if defined(CONFIG_MTD_NAND_BLOCKCHECK) && defined(CONFIG_DEBUG_INFO) && \
     defined(CONFIG_DEBUG_FS)

drivers/wireless/ieee80211/bcm43xxx/bcmf_interface.c
@@ -68,7 +68,7 @@ void bcmf_interface_free_frame(FAR struct bcmf_dev_s  *priv,
 {
   FAR bcmf_interface_dev_t *ibus = (FAR bcmf_interface_dev_t *) priv->bus;
 
-  if (nxsem_wait_uninterruptible(&ibus->queue_mutex) < 0)
+  if (nxmutex_lock(&ibus->queue_mutex) < 0)
     {
       DEBUGPANIC();
     }
@@ -80,7 +80,7 @@ void bcmf_interface_free_frame(FAR struct bcmf_dev_s  *priv,
       ibus->tx_queue_count--;
     }
 
-  nxsem_post(&ibus->queue_mutex);
+  nxmutex_unlock(&ibus->queue_mutex);
 }
 
 /****************************************************************************
@@ -97,7 +97,7 @@ bcmf_interface_frame_t
 
   while (1)
     {
-      if (nxsem_wait_uninterruptible(&ibus->queue_mutex) < 0)
+      if (nxmutex_lock(&ibus->queue_mutex) < 0)
         {
           DEBUGPANIC();
         }
@@ -115,12 +115,12 @@ bcmf_interface_frame_t
                   ibus->tx_queue_count++;
                 }
 
-              nxsem_post(&ibus->queue_mutex);
+              nxmutex_unlock(&ibus->queue_mutex);
               break;
             }
         }
 
-      nxsem_post(&ibus->queue_mutex);
+      nxmutex_unlock(&ibus->queue_mutex);
 
       if (!block)
         {

graphics/nxterm/nxterm_resize.c
@@ -69,7 +69,7 @@ int nxterm_resize(NXTERM handle, FAR const struct nxgl_size_s *size)
 
   /* Get exclusive access to the state structure */
 
-  ret = nxterm_semwait(priv);
+  ret = nxmutex_lock(&priv->excllock);
   if (ret < 0)
     {
       return ret;
@@ -82,6 +82,6 @@ int nxterm_resize(NXTERM handle, FAR const struct nxgl_size_s *size)
   priv->wndo.wsize.w = size->w;
   priv->wndo.wsize.h = size->h;
 
-  nxterm_sempost(priv);
+  nxmutex_unlock(&priv->excllock);
   return true;
 }

wireless/ieee802154/mac802154_internal.h
@@ -35,6 +35,7 @@
 
 #include <nuttx/wdog.h>
 #include <nuttx/wqueue.h>
+#include <nuttx/mutex.h>
 #include <nuttx/semaphore.h>
 
 #include <nuttx/wireless/ieee802154/ieee802154_mac.h>
@@ -104,8 +105,8 @@ struct ieee802154_privmac_s
   FAR struct mac802154_maccb_s   *cb;       /* Head of a list of MAC callbacks */
   FAR struct mac802154_radiocb_s radiocb;   /* Interface to bind to radio */
 
-  sem_t exclsem;                            /* Support exclusive access */
-  uint8_t nclients;                         /* Number of notification clients */
+  mutex_t                        excllock;  /* Support exclusive access */
+  uint8_t                        nclients;  /* Number of notification clients */
 
   /* Only support a single command at any given time. As of now I see no
    * condition where you need to have more than one command frame
@@ -305,7 +306,7 @@ struct ieee802154_privmac_s
  ****************************************************************************/
 
 int  mac802154_txdesc_alloc(FAR struct ieee802154_privmac_s *priv,
-      FAR struct ieee802154_txdesc_s **txdesc, bool allow_interrupt);
+      FAR struct ieee802154_txdesc_s **txdesc);
 
 void mac802154_setupindirect(FAR struct ieee802154_privmac_s *priv,
       FAR struct ieee802154_txdesc_s *txdesc);
@@ -510,62 +511,12 @@ void mac802154_notify(FAR struct ieee802154_privmac_s *priv,
 
 /* General helpers **********************************************************/
 
-#define mac802154_givesem(s) nxsem_post(s)
-
-static inline int mac802154_takesem(sem_t *sem, bool allowinterrupt)
-{
-  if (allowinterrupt)
-    {
-      return nxsem_wait(sem);
-    }
-  else
-    {
-      return nxsem_wait_uninterruptible(sem);
-    }
-}
-
-#ifdef CONFIG_MAC802154_LOCK_VERBOSE
-#define mac802154_unlock(dev) \
-  mac802154_givesem(&dev->exclsem); \
-  wlinfo("MAC unlocked\n");
-#else
-#define mac802154_unlock(dev) \
-  mac802154_givesem(&dev->exclsem);
-#endif
-
-#define mac802154_lock(dev, allowinterrupt) \
-  mac802154_lockpriv(dev, allowinterrupt, __FUNCTION__)
-
-static inline int
-mac802154_lockpriv(FAR struct ieee802154_privmac_s *dev,
-                   bool allowinterrupt, FAR const char *funcname)
-{
-  int ret;
-
-#ifdef CONFIG_MAC802154_LOCK_VERBOSE
-  wlinfo("Locking MAC: %s\n", funcname);
-#endif
-  ret = mac802154_takesem(&dev->exclsem, allowinterrupt);
-  if (ret < 0)
-    {
-      wlwarn("Failed to lock MAC\n");
-    }
-  else
-    {
-#ifdef CONFIG_MAC802154_LOCK_VERBOSE
-      wlinfo("MAC locked\n");
-#endif
-    }
-
-  return ret;
-}
-
 static inline void
 mac802154_txdesc_free(FAR struct ieee802154_privmac_s *priv,
                       FAR struct ieee802154_txdesc_s *txdesc)
 {
   sq_addlast((FAR sq_entry_t *)txdesc, &priv->txdesc_queue);
-  mac802154_givesem(&priv->txdesc_sem);
+  nxsem_post(&priv->txdesc_sem);
 }
 
 /****************************************************************************

apache/nuttx

arch/arm/src/cxd56xx/cxd56_sysctl.c
@@ -27,6 +27,7 @@
 #include <nuttx/arch.h>
 #include <nuttx/fs/fs.h>
 #include <nuttx/fs/ioctl.h>
+#include <nuttx/mutex.h>
 #include <nuttx/semaphore.h>
 
 #include <stdio.h>
@@ -57,8 +58,6 @@
  ****************************************************************************/
 
 static int  sysctl_ioctl(struct file *filep, int cmd, unsigned long arg);
-static int  sysctl_semtake(sem_t *semid);
-static void sysctl_semgive(sem_t *semid);
 static int  sysctl_rxhandler(int cpuid, int protoid,
                              uint32_t pdata, uint32_t data,
                              void *userdata);
@@ -67,7 +66,7 @@ static int  sysctl_rxhandler(int cpuid, int protoid,
  * Private Data
  ****************************************************************************/
 
-static sem_t g_exc;
+static mutex_t g_lock;
 static sem_t g_sync;
 static int g_errcode = 0;
 
@@ -96,16 +95,6 @@ static int sysctl_ioctl(struct file *filep, int cmd, unsigned long arg)
   return ret;
 }
 
-static int sysctl_semtake(sem_t *semid)
-{
-  return nxsem_wait_uninterruptible(semid);
-}
-
-static void sysctl_semgive(sem_t *semid)
-{
-  nxsem_post(semid);
-}
-
 static int sysctl_rxhandler(int cpuid, int protoid,
                             uint32_t pdata, uint32_t data,
                             void *userdata)
@@ -115,8 +104,7 @@ static int sysctl_rxhandler(int cpuid, int protoid,
 
   g_errcode = (int)data;
 
-  sysctl_semgive(&g_sync);
-
+  nxsem_post(&g_sync);
   return OK;
 }
 
@@ -131,7 +119,7 @@ int cxd56_sysctlcmd(uint8_t id, uint32_t data)
 
   /* Get exclusive access */
 
-  ret = sysctl_semtake(&g_exc);
+  ret = nxmutex_lock(&g_lock);
   if (ret < 0)
     {
       return ret;
@@ -146,17 +134,17 @@ int cxd56_sysctlcmd(uint8_t id, uint32_t data)
   ret = cxd56_iccsend(CXD56_PROTO_SYSCTL, &msg, SYSCTL_TIMEOUT);
   if (ret < 0)
     {
-      sysctl_semgive(&g_exc);
+      nxmutex_unlock(&g_lock);
       _err("Timeout.\n");
       return ret;
     }
 
   /* Wait for reply message from system CPU */
 
-  ret = sysctl_semtake(&g_sync);
+  ret = nxsem_wait_uninterruptible(&g_sync);
   if (ret < 0)
     {
-      sysctl_semgive(&g_exc);
+      nxmutex_unlock(&g_lock);
       return ret;
     }
 
@@ -164,8 +152,7 @@ int cxd56_sysctlcmd(uint8_t id, uint32_t data)
 
   ret = g_errcode;
 
-  sysctl_semgive(&g_exc);
-
+  nxmutex_unlock(&g_lock);
   return ret;
 }
 
@@ -173,7 +160,7 @@ void cxd56_sysctlinitialize(void)
 {
   cxd56_iccinit(CXD56_PROTO_SYSCTL);
 
-  nxsem_init(&g_exc, 0, 1);
+  nxmutex_init(&g_lock);
   nxsem_init(&g_sync, 0, 0);
   nxsem_set_protocol(&g_sync, SEM_PRIO_NONE);
 

arch/arm/src/lpc54xx/lpc54_dma.c
@@ -32,6 +32,7 @@
 #include <debug.h>
 
 #include <nuttx/arch.h>
+#include <nuttx/mutex.h>
 
 #include "arm_internal.h"
 #include "hardware/lpc54_inputmux.h"
@@ -59,7 +60,7 @@ struct lpc54_dmach_s
 
 struct lpc54_dma_s
 {
-  sem_t exclsem;           /* For exclusive access to the DMA channel list */
+  mutex_t lock;           /* For exclusive access to the DMA channel list */
 
   /* This is the state of each DMA channel */
 
@@ -233,7 +234,7 @@ void weak_function arm_dma_initialize(void)
 
   /* Initialize the DMA state structure */
 
-  nxsem_init(&g_dma.exclsem, 0, 1);
+  nxmutex_init(&g_dma.lock);
 
   /* Set the SRAMBASE to the beginning a array of DMA descriptors, one for
    * each DMA channel.
@@ -295,7 +296,7 @@ int lpc54_dma_setup(int ch, uint32_t cfg, uint32_t xfrcfg, uint8_t trigsrc,
 
   /* Get exclusive access to the DMA data structures and interface */
 
-  ret = nxsem_wait(&g_dma.exclsem);
+  ret = nxmutex_lock(&g_dma.lock);
   if (ret < 0)
     {
       return ret;
@@ -307,7 +308,7 @@ int lpc54_dma_setup(int ch, uint32_t cfg, uint32_t xfrcfg, uint8_t trigsrc,
   if (dmach->inuse)
     {
       ret = -EBUSY;
-      goto errout_with_exclsem;
+      goto errout_with_excllock;
     }
 
   dmach->inuse = true;
@@ -437,8 +438,8 @@ int lpc54_dma_setup(int ch, uint32_t cfg, uint32_t xfrcfg, uint8_t trigsrc,
   putreg32(xfrcfg, base + LPC54_DMA_XFERCFG_OFFSET);
   ret = OK;
 
-errout_with_exclsem:
-  nxsem_post(&g_dma.exclsem);
+errout_with_excllock:
+  nxmutex_unlock(&g_dma.lock);
   return ret;
 }
 

arch/arm/src/s32k3xx/s32k3xx_lpspi.c
@@ -60,7 +60,7 @@
 
 #include <nuttx/irq.h>
 #include <nuttx/arch.h>
-#include <nuttx/semaphore.h>
+#include <nuttx/mutex.h>
 #include <nuttx/spi/spi.h>
 #include <nuttx/power/pm.h>
 
@@ -117,7 +117,7 @@ struct s32k3xx_lpspidev_s
 #ifdef CONFIG_S32K3XX_LPSPI_INTERRUPTS
   uint8_t spiirq;             /* SPI IRQ number */
 #endif
-  sem_t exclsem;              /* Held while chip is selected for mutual exclusion */
+  mutex_t lock;               /* Held while chip is selected for mutual exclusion */
   uint32_t frequency;         /* Requested clock frequency */
   uint32_t actual;            /* Actual clock frequency */
   int8_t nbits;               /* Width of word in bits */
@@ -1052,11 +1052,11 @@ static int s32k3xx_lpspi_lock(struct spi_dev_s *dev, bool lock)
 
   if (lock)
     {
-      ret = nxsem_wait_uninterruptible(&priv->exclsem);
+      ret = nxmutex_lock(&priv->lock);
     }
   else
     {
-      ret = nxsem_post(&priv->exclsem);
+      ret = nxmutex_unlock(&priv->lock);
     }
 
   return ret;
@@ -2032,9 +2032,9 @@ static void s32k3xx_lpspi_bus_initialize(struct s32k3xx_lpspidev_s *priv)
 
   s32k3xx_lpspi_setmode((struct spi_dev_s *)priv, SPIDEV_MODE0);
 
-  /* Initialize the SPI semaphore that enforces mutually exclusive access */
+  /* Initialize the SPI mutex that enforces mutually exclusive access */
 
-  nxsem_init(&priv->exclsem, 0, 1);
+  nxmutex_init(&priv->lock);
 
   /* Enable LPSPI */
 

drivers/mtd/mtd_nand.c
@@ -77,11 +77,6 @@
  * Private Function Prototypes
  ****************************************************************************/
 
-/* NAND locking */
-
-static int      nand_lock(FAR struct nand_dev_s *nand);
-#define         nand_unlock(n) nxsem_post(&(n)->exclsem)
-
 /* Bad block checking */
 
 #ifdef CONFIG_MTD_NAND_BLOCKCHECK
@@ -119,26 +114,6 @@ static int     nand_ioctl(struct mtd_dev_s *dev, int cmd,
 /****************************************************************************
  * Private Functions
  ****************************************************************************/
-
-/****************************************************************************
- * Name: nand_lock
- *
- * Description:
- *   Get exclusive access to the nand.
- *
- * Input Parameters:
- *   nand  - Pointer to a struct nand_dev_s instance.
- *   block - Number of block to check.
- *
- * Returned Value:
- *   OK on success; a negated errno value on failure.
- *
- ****************************************************************************/
-
-static int nand_lock(FAR struct nand_dev_s *nand)
-{
-  return nxsem_wait(&nand->exclsem);
-}
 
 /****************************************************************************
  * Name: nand_checkblock
@@ -548,7 +523,7 @@ static int nand_erase(struct mtd_dev_s *dev, off_t startblock,
 
   /* Lock access to the NAND until we complete the erase */
 
-  nand_lock(nand);
+  nxmutex_lock(&nand->lock);
   while (blocksleft-- > 0)
     {
       /* Erase each sector */
@@ -558,14 +533,14 @@ static int nand_erase(struct mtd_dev_s *dev, off_t startblock,
         {
           ferr("ERROR: nand_eraseblock failed on block %ld: %d\n",
                (long)startblock, ret);
-          nand_unlock(nand);
+          nxmutex_unlock(&nand->lock);
           return ret;
         }
 
       startblock++;
     }
 
-  nand_unlock(nand);
+  nxmutex_unlock(&nand->lock);
   return (int)nblocks;
 }
 
@@ -614,7 +589,7 @@ static ssize_t nand_bread(struct mtd_dev_s *dev, off_t startpage,
 
   /* Lock access to the NAND until we complete the read */
 
-  nand_lock(nand);
+  nxmutex_lock(&nand->lock);
 
   /* Then read every page from NAND */
 
@@ -657,11 +632,11 @@ static ssize_t nand_bread(struct mtd_dev_s *dev, off_t startpage,
       buffer += pagesize;
     }
 
-  nand_unlock(nand);
+  nxmutex_unlock(&nand->lock);
   return npages;
 
 errout_with_lock:
-  nand_unlock(nand);
+  nxmutex_unlock(&nand->lock);
   return ret;
 }
 
@@ -710,7 +685,7 @@ static ssize_t nand_bwrite(struct mtd_dev_s *dev, off_t startpage,
 
   /* Lock access to the NAND until we complete the write */
 
-  nand_lock(nand);
+  nxmutex_lock(&nand->lock);
 
   /* Then write every page into NAND */
 
@@ -753,11 +728,11 @@ static ssize_t nand_bwrite(struct mtd_dev_s *dev, off_t startpage,
       buffer += pagesize;
     }
 
-  nand_unlock(nand);
+  nxmutex_unlock(&nand->lock);
   return npages;
 
 errout_with_lock:
-  nand_unlock(nand);
+  nxmutex_unlock(&nand->lock);
   return ret;
 }
 
@@ -967,7 +942,7 @@ FAR struct mtd_dev_s *nand_initialize(FAR struct nand_raw_s *raw)
   nand->mtd.ioctl  = nand_ioctl;
   nand->raw        = raw;
 
-  nxsem_init(&nand->exclsem, 0, 1);
+  nxmutex_init(&nand->lock);
 
 #if defined(CONFIG_MTD_NAND_BLOCKCHECK) && defined(CONFIG_DEBUG_INFO) && \
     defined(CONFIG_DEBUG_FS)

drivers/wireless/ieee80211/bcm43xxx/bcmf_interface.c
@@ -68,7 +68,7 @@ void bcmf_interface_free_frame(FAR struct bcmf_dev_s  *priv,
 {
   FAR bcmf_interface_dev_t *ibus = (FAR bcmf_interface_dev_t *) priv->bus;
 
-  if (nxsem_wait_uninterruptible(&ibus->queue_mutex) < 0)
+  if (nxmutex_lock(&ibus->queue_lock) < 0)
     {
       DEBUGPANIC();
     }
@@ -80,7 +80,7 @@ void bcmf_interface_free_frame(FAR struct bcmf_dev_s  *priv,
       ibus->tx_queue_count--;
     }
 
-  nxsem_post(&ibus->queue_mutex);
+  nxmutex_unlock(&ibus->queue_lock);
 }
 
 /****************************************************************************
@@ -97,7 +97,7 @@ bcmf_interface_frame_t
 
   while (1)
     {
-      if (nxsem_wait_uninterruptible(&ibus->queue_mutex) < 0)
+      if (nxmutex_lock(&ibus->queue_lock) < 0)
         {
           DEBUGPANIC();
         }
@@ -115,12 +115,12 @@ bcmf_interface_frame_t
                   ibus->tx_queue_count++;
                 }
 
-              nxsem_post(&ibus->queue_mutex);
+              nxmutex_unlock(&ibus->queue_lock);
               break;
             }
         }
 
-      nxsem_post(&ibus->queue_mutex);
+      nxmutex_unlock(&ibus->queue_lock);
 
       if (!block)
         {

graphics/nxterm/nxterm_resize.c
@@ -69,7 +69,7 @@ int nxterm_resize(NXTERM handle, FAR const struct nxgl_size_s *size)
 
   /* Get exclusive access to the state structure */
 
-  ret = nxterm_semwait(priv);
+  ret = nxmutex_lock(&priv->lock);
   if (ret < 0)
     {
       return ret;
@@ -82,6 +82,6 @@ int nxterm_resize(NXTERM handle, FAR const struct nxgl_size_s *size)
   priv->wndo.wsize.w = size->w;
   priv->wndo.wsize.h = size->h;
 
-  nxterm_sempost(priv);
+  nxmutex_unlock(&priv->lock);
   return true;
 }

wireless/ieee802154/mac802154_internal.h
@@ -35,6 +35,7 @@
 
 #include <nuttx/wdog.h>
 #include <nuttx/wqueue.h>
+#include <nuttx/mutex.h>
 #include <nuttx/semaphore.h>
 
 #include <nuttx/wireless/ieee802154/ieee802154_mac.h>
@@ -104,8 +105,8 @@ struct ieee802154_privmac_s
   FAR struct mac802154_maccb_s   *cb;       /* Head of a list of MAC callbacks */
   FAR struct mac802154_radiocb_s radiocb;   /* Interface to bind to radio */
 
-  sem_t exclsem;                            /* Support exclusive access */
-  uint8_t nclients;                         /* Number of notification clients */
+  mutex_t                        lock;      /* Support exclusive access */
+  uint8_t                        nclients;  /* Number of notification clients */
 
   /* Only support a single command at any given time. As of now I see no
    * condition where you need to have more than one command frame
@@ -305,7 +306,7 @@ struct ieee802154_privmac_s
  ****************************************************************************/
 
 int  mac802154_txdesc_alloc(FAR struct ieee802154_privmac_s *priv,
-      FAR struct ieee802154_txdesc_s **txdesc, bool allow_interrupt);
+      FAR struct ieee802154_txdesc_s **txdesc);
 
 void mac802154_setupindirect(FAR struct ieee802154_privmac_s *priv,
       FAR struct ieee802154_txdesc_s *txdesc);
@@ -510,62 +511,12 @@ void mac802154_notify(FAR struct ieee802154_privmac_s *priv,
 
 /* General helpers **********************************************************/
 
-#define mac802154_givesem(s) nxsem_post(s)
-
-static inline int mac802154_takesem(sem_t *sem, bool allowinterrupt)
-{
-  if (allowinterrupt)
-    {
-      return nxsem_wait(sem);
-    }
-  else
-    {
-      return nxsem_wait_uninterruptible(sem);
-    }
-}
-
-#ifdef CONFIG_MAC802154_LOCK_VERBOSE
-#define mac802154_unlock(dev) \
-  mac802154_givesem(&dev->exclsem); \
-  wlinfo("MAC unlocked\n");
-#else
-#define mac802154_unlock(dev) \
-  mac802154_givesem(&dev->exclsem);
-#endif
-
-#define mac802154_lock(dev, allowinterrupt) \
-  mac802154_lockpriv(dev, allowinterrupt, __FUNCTION__)
-
-static inline int
-mac802154_lockpriv(FAR struct ieee802154_privmac_s *dev,
-                   bool allowinterrupt, FAR const char *funcname)
-{
-  int ret;
-
-#ifdef CONFIG_MAC802154_LOCK_VERBOSE
-  wlinfo("Locking MAC: %s\n", funcname);
-#endif
-  ret = mac802154_takesem(&dev->exclsem, allowinterrupt);
-  if (ret < 0)
-    {
-      wlwarn("Failed to lock MAC\n");
-    }
-  else
-    {
-#ifdef CONFIG_MAC802154_LOCK_VERBOSE
-      wlinfo("MAC locked\n");
-#endif
-    }
-
-  return ret;
-}
-
 static inline void
 mac802154_txdesc_free(FAR struct ieee802154_privmac_s *priv,
                       FAR struct ieee802154_txdesc_s *txdesc)
 {
   sq_addlast((FAR sq_entry_t *)txdesc, &priv->txdesc_queue);
-  mac802154_givesem(&priv->txdesc_sem);
+  nxsem_post(&priv->txdesc_sem);
 }
 
 /****************************************************************************