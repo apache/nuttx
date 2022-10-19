10a5c660f2815dd604b49e9323affc7e760ca0d0
SPRESENSE <41312067+SPRESENSE@users.noreply.github.com>
2022-10-19 19:55:48 +0900

    tools: Fix an issue that libboard.a is not exported
    If CONFIG_ARCH_BOARD_COMMON is enabled, libboard.a should be generated.
    Fix an issue it is not built with make export.

--- tools/FlatLibs.mk
+++ tools/FlatLibs.mk
@@ -132,6 +132,12 @@ ifeq ($(CONFIG_OPENAMP),y)
 NUTTXLIBS += staging$(DELIM)libopenamp$(LIBEXT)
 endif
 
+# Add libraries for board common support
+
+ifeq ($(CONFIG_ARCH_BOARD_COMMON),y)
+NUTTXLIBS += staging$(DELIM)libboard$(LIBEXT)
+endif
+
 # Export all libraries
 
 EXPORTLIBS = $(NUTTXLIBS)
