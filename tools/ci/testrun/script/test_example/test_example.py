fb10ec3404f381b7e877e85e3ce59a486f6422b5
nietingting <nietingting@xiaomi.com>
2022-10-21 18:04:29 +0800

    CI: add example to sim
    VELAPLATFO-1813
    
    Change-Id: I65b3bd92509ed4247a23eba5bc708cfaa81a2484
    Signed-off-by: nietingting <nietingting@xiaomi.com>

---
+++ tools/ci/testrun/script/test_example/test_example.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+# encoding: utf-8
+import pytest
+
+pytestmark = [pytest.mark.common, pytest.mark.qemu]
+
+
+def test_hello(p):
+    ret = p.sendCommand("hello", "Hello, World!!")
+    assert ret == 0
+
+
+def test_helloxx(p):
+    ret = p.sendCommand("helloxx", "Hello, World!!")
+    assert ret == 0
+
+
+def test_pipe(p):
+    p.sendCommand("umount /tmp")
+    ret = p.sendCommand("pipe", "redirect_reader: Returning success", 60)
+    assert ret == 0
+    p.sendCommand("\n")
+    p.sendCommand("mount -t tmpfs /tmp")
+
+
+def test_popen(p):
+    ret = p.sendCommand("popen", "Calling pclose()")
+    assert ret == 0
+
+
+def test_usrsocktest(p):
+    ret = p.sendCommand("usrsocktest", "HEAP AFTER TESTS", 60)
+    assert ret == 0
