e46bdeca1d41def67f6aec714412842f57b4f028
Xiang Xiao <xiaoxiang@xiaomi.com>
2022-10-29 02:36:10 +0800

    Fix Error: board/cxd56_leds.c:43:20: error: unused function 'led_clrbits' [-Werror,-Wunused-function] static inline void led_clrbits(unsigned int clrbits)                    ^ Error: board/cxd56_leds.c:66:20: error: unused function 'led_setbits' [-Werror,-Wunused-function] static inline void led_setbits(unsigned int setbits)
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- boards/arm/cxd56xx/spresense/src/cxd56_leds.c
+++ boards/arm/cxd56xx/spresense/src/cxd56_leds.c
@@ -40,6 +40,7 @@
  * Private Functions
  ****************************************************************************/
 
+#ifndef CONFIG_ARCH_LEDS_CPU_ACTIVITY
 static inline void led_clrbits(unsigned int clrbits)
 {
   if ((clrbits & BOARD_LED1_BIT) != 0)
@@ -85,6 +86,7 @@ static inline void led_setbits(unsigned int setbits)
       cxd56_gpio_write(GPIO_LED4, true);
     }
 }
+#endif
 
 /****************************************************************************
  * Public Functions
