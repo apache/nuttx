ce6b610c15c35aa9bd83b32b88c0f678a5e46107
Xiang Xiao <xiaoxiang@xiaomi.com>
2022-10-30 01:26:11 +0800

    Fix clang compiler warning in boards/arm/stm32/
    Error: board/stm32_ssd1289.c:290:33: error: shifting a negative signed value is undefined [-Werror,-Wshift-negative-value]
      putreg32(FSMC_BTR_ADDSET(5) | FSMC_BTR_ADDHLD(0) |
      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~
    /github/workspace/sources/nuttx/arch/arm/src/chip/hardware/stm32_fsmc.h:164:42: note: expanded from macro 'FSMC_BTR_ADDHLD'
                                             ^
    /github/workspace/sources/nuttx/arch/arm/src/common/arm_internal.h:135:54: note: expanded from macro 'putreg32'
                                                         ^
    Error: board/stm32_ssd1289.c:291:33: error: shifting a negative signed value is undefined [-Werror,-Wshift-negative-value]
               FSMC_BTR_DATAST(9) | FSMC_BTR_BUSTURN(0) |
               ~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~
    /github/workspace/sources/nuttx/arch/arm/src/chip/hardware/stm32_fsmc.h:172:42: note: expanded from macro 'FSMC_BTR_BUSTURN'
                                             ^
    /github/workspace/sources/nuttx/arch/arm/src/common/arm_internal.h:135:54: note: expanded from macro 'putreg32'
                                                         ^
    Error: board/stm32_ssd1289.c:292:12: error: shifting a negative signed value is undefined [-Werror,-Wshift-negative-value]
               FSMC_BTR_CLKDIV(0) | FSMC_BTR_DATLAT(0) |
               ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    /github/workspace/sources/nuttx/arch/arm/src/chip/hardware/stm32_fsmc.h:176:42: note: expanded from macro 'FSMC_BTR_CLKDIV'
                                             ^
    /github/workspace/sources/nuttx/arch/arm/src/common/arm_internal.h:135:54: note: expanded from macro 'putreg32'
                                                         ^
    Error: board/stm32_ssd1289.c:292:33: error: shifting a negative signed value is undefined [-Werror,-Wshift-negative-value]
               FSMC_BTR_CLKDIV(0) | FSMC_BTR_DATLAT(0) |
               ~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~
    /github/workspace/sources/nuttx/arch/arm/src/chip/hardware/stm32_fsmc.h:180:42: note: expanded from macro 'FSMC_BTR_DATLAT'
                                             ^
    /github/workspace/sources/nuttx/arch/arm/src/common/arm_internal.h:135:54: note: expanded from macro 'putreg32'
                                                         ^
    
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- boards/arm/stm32/hymini-stm32v/src/stm32_ssd1289.c
+++ boards/arm/stm32/hymini-stm32v/src/stm32_ssd1289.c
@@ -375,7 +375,7 @@ static void stm32_selectlcd(void)
   /* Bank1 NOR/SRAM timing register configuration */
 
   putreg32(FSMC_BTR_ADDSET(1)  | FSMC_BTR_ADDHLD(0) | FSMC_BTR_DATAST(2) |
-           FSMC_BTR_BUSTURN(0) | FSMC_BTR_CLKDIV(0) | FSMC_BTR_DATLAT(0) |
+           FSMC_BTR_BUSTURN(0) | FSMC_BTR_CLKDIV(1) | FSMC_BTR_DATLAT(2) |
            FSMC_BTR_ACCMODA,
            STM32_FSMC_BTR1);
 
