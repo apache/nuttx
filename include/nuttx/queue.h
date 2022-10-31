e0a3fdf98207346fe6db3c1f7bc428ead163f893
chao an <anchao@xiaomi.com>
2022-11-01 02:11:18 +0800

    sched/posix/timer: handle invaild timerid correctly
    TIMER_SETTIME(2)
    
    NAME
           timer_settime, timer_gettime - arm/disarm and fetch state of POSIX per-process timer
    
    SYNOPSIS
           #include <time.h>
    
           int timer_settime(timer_t timerid, int flags,
                             const struct itimerspec *new_value,
                             struct itimerspec *old_value);
           int timer_gettime(timer_t timerid, struct itimerspec *curr_value);
    ...
    ERRORS
    ...
           EINVAL timerid is invalid.
    
    Signed-off-by: chao an <anchao@xiaomi.com>

--- include/nuttx/queue.h
+++ include/nuttx/queue.h
@@ -156,7 +156,7 @@
   while (0)
 
 #define sq_for_every(q, p) \
-  for((p) = (q)->head; (p) != NULL; (p) = (p)->flink)
+  for ((p) = (q)->head; (p) != NULL; (p) = (p)->flink)
 
 #define sq_rem(p, q) \
   do \
