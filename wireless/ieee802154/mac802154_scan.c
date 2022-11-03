ac0eff5c2f511ebdce43e77aa04a74012d7e6792
chao an <anchao@xiaomi.com>
2022-11-03 18:26:00 +0800

    wireless/ieee802154: mac lock should in pairs
    Change-Id: Id635b1ee74c47a88bbc92a5875c3126a1bfd6afa
    Signed-off-by: chao an <anchao@xiaomi.com>

--- wireless/ieee802154/mac802154_scan.c
+++ wireless/ieee802154/mac802154_scan.c
@@ -345,6 +345,7 @@ static void mac802154_scantimeout(FAR void *arg)
           mac802154_scanfinish(priv, IEEE802154_STATUS_NO_BEACON);
         }
 
+      nxmutex_unlock(&priv->lock);
       return;
     }
 
