ddcebb22c936ff6b6d5559cc556e9794f764e563
Xiang Xiao <xiaoxiang@xiaomi.com>
2022-11-11 15:07:43 +0800

    Fix Error: chip/lpc43_tickless_rit.c:127:24: error: unused function 'lpc43_tl_get_mask' [-Werror,-Wunused-function]
    static inline uint32_t lpc43_tl_get_mask(void)
                           ^
    Error: chip/lpc43_tickless_rit.c:169:20: error: unused function 'lpc43_tl_get_enable' [-Werror,-Wunused-function]
    static inline bool lpc43_tl_get_enable(void)
    
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- arch/arm/src/lpc43xx/lpc43_tickless_rit.c
+++ arch/arm/src/lpc43xx/lpc43_tickless_rit.c
@@ -124,11 +124,6 @@ static inline void lpc43_tl_set_mask(uint32_t value)
     }
 }
 
-static inline uint32_t lpc43_tl_get_mask(void)
-{
-  return mask_cache;
-}
-
 static inline bool lpc43_tl_get_ctrl_bit(uint32_t bit)
 {
   return ((ctrl_cache & bit)?true:false);
@@ -164,11 +159,6 @@ static inline bool lpc43_tl_get_reset_on_match(void)
 static inline void lpc43_tl_set_enable(bool value)
 {
   lpc43_tl_set_ctrl_bit(RIT_CTRL_EN, value);
-}
-
-static inline bool lpc43_tl_get_enable(void)
-{
-  return lpc43_tl_get_ctrl_bit(RIT_CTRL_EN);
 }
 
 static inline void lpc43_tl_clear_interrupt(void)
