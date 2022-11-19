9d2ecdca8c68c17935c09b6539e4cfbca6f9a389
Xiang Xiao <xiaoxiang@xiaomi.com>
2022-11-19 14:59:34 +0800

    arch/arm64: Move group_addrenv to arm64_syscall_switch
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- arch/arm64/src/common/arm64_syscall.c
+++ arch/arm64/src/common/arm64_syscall.c
@@ -202,10 +202,20 @@ uint64_t *arm64_syscall_switch(uint64_t * regs)
         break;
     }
 
-  /* Restore the cpu lock */
-
   if ((uint64_t *)f_regs != ret_regs)
     {
+#ifdef CONFIG_ARCH_ADDRENV
+      /* Make sure that the address environment for the previously
+       * running task is closed down gracefully (data caches dump,
+       * MMU flushed) and set up the address environment for the new
+       * thread at the head of the ready-to-run list.
+       */
+
+      group_addrenv(NULL);
+#endif
+
+    /* Restore the cpu lock */
+
       restore_critical_section();
     }
 
