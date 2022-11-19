6f4e8d3d95576acb14e4a80fa0a7a79a07dff5f4
Xiang Xiao <xiaoxiang@xiaomi.com>
2022-11-19 13:01:43 +0800

    Fix error: more '%' conversions than data arguments
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- arch/arm/src/stm32/stm32_usbdev.c
+++ arch/arm/src/stm32/stm32_usbdev.c
@@ -2892,7 +2892,7 @@ static int stm32_epconfigure(struct usbdev_ep_s *ep,
   if (!ep || !desc)
     {
       usbtrace(TRACE_DEVERROR(STM32_TRACEERR_INVALIDPARMS), 0);
-      uerr("ERROR: ep=%p desc=%p\n");
+      uerr("ERROR: ep=%p desc=%p\n", ep, desc);
       return -EINVAL;
     }
 #endif
