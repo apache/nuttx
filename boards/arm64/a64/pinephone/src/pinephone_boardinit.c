c2d083e3c1d379b2fea68b4c1c6c875d4fc317bf
Lee Lup Yuen <luppy@appkaki.com>
2022-11-25 12:07:57 +0800

    Update boards/arm64/a64/pinephone/src/pinephone_boardinit.c
    Co-authored-by: hartmannathan <59230071+hartmannathan@users.noreply.github.com>

--- boards/arm64/a64/pinephone/src/pinephone_boardinit.c
+++ boards/arm64/a64/pinephone/src/pinephone_boardinit.c
@@ -89,7 +89,7 @@ void a64_board_initialize(void)
  *   If CONFIG_BOARD_LATE_INITIALIZE is selected, then an additional
  *   initialization call will be performed in the boot-up sequence to a
  *   function called board_late_initialize(). board_late_initialize() will be
- *   called immediately after up_intitialize() is called and just before the
+ *   called immediately after up_initialize() is called and just before the
  *   initial application is started.  This additional initialization phase
  *   may be used, for example, to initialize board-specific device drivers.
  *
