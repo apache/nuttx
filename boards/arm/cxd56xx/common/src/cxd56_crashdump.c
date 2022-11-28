apache/nuttx

commit 6d30726a1bce27a21dab7bf58163f014923e1bf3

Remove the unnecessary "return;" at the end of function
Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

boards/arm/cxd56xx/common/src/cxd56_crashdump.c
@@ -239,5 +239,4 @@ exit:
 #if defined(CONFIG_CXD56_RESET_ON_CRASH)
   board_reset_on_crash();
 #endif
-  return;
 }

arch/arm/src/cxd56xx/cxd56_backuplog.c
@@ -303,8 +303,6 @@ void up_backuplog_free(const char *name)
     }
 
   leave_critical_section(flags);
-
-  return;
 }
 
 /****************************************************************************

arch/arm/src/cxd56xx/cxd56_cisif.c
@@ -372,8 +372,6 @@ static void cisif_callback_for_intlev(uint8_t code)
   cisif_reg_write(CISIF_EXE_CMD, 1);
   cisif_reg_write(CISIF_YCC_DREAD_CONT, 0);
   cisif_reg_write(CISIF_JPG_DREAD_CONT, 0);
-
-  return;
 }
 
 /****************************************************************************

arch/arm/src/cxd56xx/cxd56_cpupause.c
@@ -463,7 +463,6 @@ void up_send_irqreq(int idx, int irq, int cpu)
   /* Finally unlock the spinlock to proceed the handler */
 
   spin_unlock(&g_cpu_wait[cpu]);
-  return;
 }
 
 #endif /* CONFIG_SMP */

arch/arm/src/cxd56xx/cxd56_rtc.c
@@ -318,8 +318,6 @@ static void cxd56_rtc_initialize(wdparm_t arg)
   /* Make it possible to use the RTC timer functions */
 
   g_rtc_enabled = true;
-
-  return;
 }
 
 /****************************************************************************

arch/arm/src/imxrt/imxrt_usbdev.c
@@ -2923,8 +2923,6 @@ void arm_usbinitialize(void)
   /* Reset/Re-initialize the USB hardware */
 
   imxrt_usbreset(priv);
-
-  return;
 }
 
 /****************************************************************************

arch/arm/src/lc823450/lc823450_dma.c
@@ -384,7 +384,6 @@ void arm_dma_initialize(void)
 
 void lc823450_dmaconfigure(uint8_t dmarequest, bool alternate)
 {
-  return;
 }
 
 /****************************************************************************
@@ -459,8 +458,6 @@ void lc823450_dmarequest(DMA_HANDLE handle, uint8_t dmarequest)
 
   putreg32(val, DMACCFG(dmach->chn));
   up_disable_clk(LC823450_CLOCK_DMA);
-
-  return;
 }
 
 /****************************************************************************
@@ -665,5 +662,4 @@ void lc823450_dmastop(DMA_HANDLE handle)
     }
 
   spin_unlock_irqrestore(NULL, flags);
-  return;
 }

arch/arm/src/lc823450/lc823450_irq.c
@@ -262,8 +262,6 @@ static void lc823450_extint_clr(int irq)
 
   regaddr = INTC_REG(EXTINTCLR_BASE, port);
   putreg32(1 << pin, regaddr);
-
-  return;
 }
 
 /****************************************************************************

commit d197d8c2c0099461fe629e5d354ec78ea10c2949

boards: cxd56xx: Fix the build error in cxd56_crashdump.c
Summary:
- This commit fixes the build error in cxd56_crashdump.c
  regressed by https://github.com/apache/nuttx/pull/7707

Impact:
- None

Tesing:
- Tested with spresense:wifi

Signed-off-by: Masayuki Ishikawa <Masayuki.Ishikawa@jp.sony.com>

boards/arm/cxd56xx/common/src/cxd56_crashdump.c
@@ -238,5 +238,7 @@ void board_crashdump(uintptr_t currentsp, void *tcb,
 exit:
 #if defined(CONFIG_CXD56_RESET_ON_CRASH)
   board_reset_on_crash();
+#else
+  return;
 #endif
 }