6120aa52f04384d51cd2414f355e6c7cd8a9878d
okayserh <o.kayser.herold@gmail.com>
2022-12-02 22:32:35 +0100

    Added initial USB support for stm32f746g discovery.

--- boards/arm/stm32f7/stm32f746g-disco/src/stm32_bringup.c
+++ boards/arm/stm32f7/stm32f746g-disco/src/stm32_bringup.c
@@ -104,6 +104,19 @@ int stm32_bringup(void)
     }
 #endif
 
+#ifdef HAVE_USBHOST
+  /* Initialize USB host operation.  stm32_usbhost_initialize() starts a
+   * thread will monitor for USB connection and disconnection events.
+   */
+
+  ret = stm32_usbhost_initialize();
+  if (ret != OK)
+    {
+      syslog(LOG_ERR, "ERROR: Failed to initialize USB host: %d\n", ret);
+      return ret;
+    }
+#endif
+
 #ifdef CONFIG_INPUT_FT5X06
   /* Initialize the touchscreen */
 
