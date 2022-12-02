apache/nuttx

commit 48311cc61f9bef0143992408bc1353a11e19ea41

Fix unaligned memory access when creating ICMP Port Unreachable messages
commit 3b69d09c80b4d21681475c43d9f165838bbf1a5c corrected the
unreachable handling for net/udp/icmp but introduced an unaligned store.
This splits the uint32_t data field into a two element uint16_t data
field to avoid the unaligned store.

net/icmpv6/icmpv6_reply.c
@@ -133,9 +133,10 @@ void icmpv6_reply(FAR struct net_driver_s *dev, int type, int code, int data)
 
   /* Initialize the ICMPv6 header */
 
-  icmpv6->type   = type;
-  icmpv6->code   = code;
-  icmpv6->data   = htonl(data);
+  icmpv6->type    = type;
+  icmpv6->code    = code;
+  icmpv6->data[0] = data >> 16;
+  icmpv6->data[1] = data & 0xffff;
 
   /* Calculate the ICMPv6 checksum over the ICMPv6 header and payload. */
 

include/nuttx/net/icmp.h
@@ -141,7 +141,7 @@ struct icmp_hdr_s
           uint16_t seqno;   /* "  " "" "   " "      " "  " "     " */
         };
 
-      uint32_t data;
+      uint16_t data[2];
     };
 };
 

include/nuttx/net/icmpv6.h
@@ -157,7 +157,7 @@ struct icmpv6_hdr_s
    * message type indicated by the Type and Code fields.
    */
 
-  uint32_t data;
+  uint16_t data[2];
 };
 
 /* The ICMPv6 and IPv6 headers */

net/icmp/icmp_reply.c
@@ -153,7 +153,8 @@ void icmp_reply(FAR struct net_driver_s *dev, int type, int code)
 
   icmp->type        = type;
   icmp->icode       = code;
-  icmp->data        = 0;
+  icmp->data[0]     = 0;
+  icmp->data[1]     = 0;
 
   /* Calculate the ICMP checksum. */
 

commit b41d9faa80a8beab9b6b5645cd7040b35fa83ce8

net/icmpv6: Fix icmpv6_reply function
Fix icmpv6_reply logic broken by commit 48311cc61f and 391b501639.

- 48311cc61f "Fix unaligned memory access when creating ICMP Port Unreachable messages"
  - It removed `htonl` function outside `data`, then the byte order may be wrong, so add `htons` back.
- 391b501639 "net: extract l3 header build code into new functions"
  - It mis-removed the `memmove`, and the icmpv6 has no payload copied after this commit.

Signed-off-by: Zhe Weng <wengzhe@xiaomi.com>

net/icmpv6/icmpv6_reply.c
@@ -112,6 +112,10 @@ void icmpv6_reply(FAR struct net_driver_s *dev, int type, int code, int data)
 
   dev->d_len = ipicmplen + datalen;
 
+  /* Copy fields from original packet */
+
+  memmove(icmpv6 + 1, ipv6, datalen);
+
   ipv6_build_header(IPv6BUF, dev->d_len - IPv6_HDRLEN, IP_PROTO_ICMP6,
                     dev->d_ipv6addr, ipv6->srcipaddr, 255);
 
@@ -119,8 +123,8 @@ void icmpv6_reply(FAR struct net_driver_s *dev, int type, int code, int data)
 
   icmpv6->type    = type;
   icmpv6->code    = code;
-  icmpv6->data[0] = data >> 16;
-  icmpv6->data[1] = data & 0xffff;
+  icmpv6->data[0] = htons(data >> 16);
+  icmpv6->data[1] = htons(data & 0xffff);
 
   /* Calculate the ICMPv6 checksum over the ICMPv6 header and payload. */
 