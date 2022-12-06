2719869ab2e6c9517e182445581494e7d9ac2ea3
田昕 <tianxin7@xiaomi.com>
2022-12-06 14:33:13 +0800

    Move _assert to kernel space.
    Signed-off-by: 田昕 <tianxin7@xiaomi.com>

--- sched/misc/assert.c
+++ sched/misc/assert.c
@@ -1,5 +1,5 @@
 /****************************************************************************
- * libs/libc/assert/lib_assert.c
+ * sched/misc/assert.c
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
