0bd57dda28218b0f007ad1a255ad8c4f2920b132
tangyusong1 <tangyusong1@xiaomi.com>
2022-12-07 17:38:11 +0800

    Adjust ioctl function of cellular
    VELAPLATFO-3447
    
    Add SIOCGCELLNETDEV
    Give a uint8_t[136] for cellular to save data
    
    Signed-off-by: tangyusong1 <tangyusong1@xiaomi.com>
    Change-Id: I6432682b34e5a7a67343ceb81e7fff8c0668b599

--- include/nuttx/fs/ioctl.h
+++ include/nuttx/fs/ioctl.h
@@ -93,6 +93,7 @@
 #define _PKTRADIOBASE   (0x3500) /* Packet radio ioctl commands */
 #define _LTEBASE        (0x3600) /* LTE device ioctl commands */
 #define _VIDIOCBASE     (0x3700) /* Video device ioctl commands */
+#define _CELLIOCBASE    (0x3800) /* Cellular device ioctl commands */
 #define _WLIOCBASE      (0x8b00) /* Wireless modules ioctl network commands */
 
 /* boardctl() commands share the same number space */
@@ -604,6 +605,11 @@
 
 #define _VIDIOCVALID(c) (_IOC_TYPE(c)==_VIDIOCBASE)
 #define _VIDIOC(nr)      _IOC(_VIDIOCBASE,nr)
+
+/* cellularctl() command definitions ****************************************/
+
+#define _CELLIOCVALID(c) (_IOC_TYPE(c)==_CELLIOCBASE)
+#define _CELLIOC(nr)     _IOC(_CELLIOCBASE,nr)
 
 /* Wireless driver network ioctl definitions ********************************/
 
