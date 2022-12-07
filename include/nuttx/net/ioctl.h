1c7a9d282cb5ebc05a84a82c615b7f27c55eb6ba
tangyusong1 <tangyusong1@xiaomi.com>
2022-12-07 17:22:02 +0800

    Add mtu set and interface name set in ioctl
    Follow LinuxOS:linux-5.10.152\include\uapi\linux\sockios.h
    Add SIOCSIFMTU and SIOCSIFNAME
    
    Signed-off-by: tangyusong1 <tangyusong1@xiaomi.com>

--- include/nuttx/net/ioctl.h
+++ include/nuttx/net/ioctl.h
@@ -47,6 +47,7 @@
 #define SIOCGIFNETMASK   _SIOC(0x0007)  /* Get network mask */
 #define SIOCSIFNETMASK   _SIOC(0x0008)  /* Set network mask */
 #define SIOCGIFMTU       _SIOC(0x0009)  /* Get MTU size */
+#define SIOCSIFMTU       _SIOC(0x0033)  /* Set MTU size */
 
 /* IPv6 interface control operations */
 
@@ -72,6 +73,7 @@
 
 #define SIOCGIFNAME      _SIOC(0x002A)  /* Get interface name string */
 #define SIOCGIFINDEX     _SIOC(0x002B)  /* Get index based name string */
+#define SIOCSIFNAME      _SIOC(0x0034)  /* Set interface name string*/
 
 /* Interface flags */
 
