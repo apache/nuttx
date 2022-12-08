d68694ff1dfc22126822db132237691f84be9cf2
Peter van der Perk <peter.vanderperk@nxp.com>
2022-12-08 15:07:26 +0100

    Sysview add prefix kconfig to add an option to decouple sched_note calls

--- include/nuttx/note/note_sysview.h
+++ include/nuttx/note/note_sysview.h
@@ -26,6 +26,17 @@
  ****************************************************************************/
 
 #include <nuttx/config.h>
+#include <nuttx/sched.h>
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+#ifdef CONFIG_SEGGER_SYSVIEW_PREFIX
+#  define PREFIX(fun) sysview ## _ ## fun
+#else
+#  define PREFIX(fun) fun
+#endif
 
 /****************************************************************************
  * Public Function Prototypes
@@ -47,6 +58,43 @@
 
 #ifdef CONFIG_SEGGER_SYSVIEW
 int sysview_initialize(void);
-#endif
+
+#  ifdef CONFIG_SEGGER_SYSVIEW_PREFIX
+
+void PREFIX(sched_note_start)(struct tcb_s *tcb);
+
+void PREFIX(sched_note_stop)(struct tcb_s *tcb);
+
+void PREFIX(sched_note_suspend)(struct tcb_s *tcb);
+
+void PREFIX(sched_note_resume)(struct tcb_s *tcb);
+
+#    ifdef CONFIG_SCHED_INSTRUMENTATION_IRQHANDLER
+void PREFIX(sched_note_irqhandler)(int irq, void *handler, bool enter);
+#    endif
+
+#    ifdef CONFIG_SCHED_INSTRUMENTATION_SYSCALL
+void PREFIX(sched_note_syscall_enter)(int nr);
+void PREFIX(sched_note_syscall_leave)(int nr, uintptr_t result);
+#    endif
+
+#    ifdef CONFIG_SCHED_INSTRUMENTATION_FILTER
+void PREFIX(sched_note_filter_mode)(struct note_filter_mode_s *oldm,
+                            struct note_filter_mode_s *newm);
+#    endif
+
+#    ifdef CONFIG_SCHED_INSTRUMENTATION_IRQHANDLER
+void PREFIX(sched_note_filter_irq)(struct note_filter_irq_s *oldf,
+                           struct note_filter_irq_s *newf);
+#    endif
+
+#    ifdef CONFIG_SCHED_INSTRUMENTATION_SYSCALL
+void PREFIX(sched_note_filter_syscall)(struct note_filter_syscall_s *oldf,
+                               struct note_filter_syscall_s *newf);
+#    endif
+
+#  endif /* CONFIG_SEGGER_SYSVIEW_PREFIX */
+
+#endif /* CONFIG_SEGGER_SYSVIEW */
 
 #endif /* __INCLUDE_NUTTX_NOTE_NOTE_SYSVIEW_H */
