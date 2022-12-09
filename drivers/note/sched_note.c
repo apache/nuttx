4223af1aa0fc3b9c029cf3456998fdc266b7beba
yinshengkai <yinshengkai@xiaomi.com>
2022-12-09 18:27:09 +0800

    sched: move sched_note.c to drivers/note
    VELAPLATFO-2150
    
    Signed-off-by: yinshengkai <yinshengkai@xiaomi.com>
    Change-Id: Ie86e3b04e8f860a4616c9cd8730bd5a9bfe100b4

--- drivers/note/sched_note.c
+++ drivers/note/sched_note.c
@@ -1,5 +1,5 @@
 /****************************************************************************
- * sched/sched/sched_note.c
+ * drivers/note/sched_note.c
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
