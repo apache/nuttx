3d2ffed8ae1117e2faf98e515d1e3b8ad1b96d83
zhangyuan21 <zhangyuan21@xiaomi.com>
2022-12-13 09:26:28 +0800

    Revert "pthread: Change the default name from <pthread> to <0xyyyyyyyy>"
    This reverts commit 092d23b25db6d83a850a8d3b57f2d54201bff898.
    
    Signed-off-by: zhangyuan21 <zhangyuan21@xiaomi.com>

--- sched/pthread/pthread_create.c
+++ sched/pthread/pthread_create.c
@@ -26,7 +26,6 @@
 
 #include <sys/types.h>
 #include <stdbool.h>
-#include <stdio.h>
 #include <string.h>
 #include <pthread.h>
 #include <sched.h>
@@ -58,6 +57,16 @@
  */
 
 const pthread_attr_t g_default_pthread_attr = PTHREAD_ATTR_INITIALIZER;
+
+/****************************************************************************
+ * Private Data
+ ****************************************************************************/
+
+#if CONFIG_TASK_NAME_SIZE > 0
+/* This is the name for name-less pthreads */
+
+static const char g_pthreadname[] = "<pthread>";
+#endif
 
 /****************************************************************************
  * Private Functions
@@ -92,8 +101,8 @@ static inline void pthread_tcb_setup(FAR struct pthread_tcb_s *ptcb,
 #if CONFIG_TASK_NAME_SIZE > 0
   /* Copy the pthread name into the TCB */
 
-  snprintf(ptcb->cmn.name, CONFIG_TASK_NAME_SIZE,
-           "pt-%p", ptcb->cmn.entry.pthread);
+  strncpy(ptcb->cmn.name, g_pthreadname, CONFIG_TASK_NAME_SIZE);
+  ptcb->cmn.name[CONFIG_TASK_NAME_SIZE] = '\0';
 #endif /* CONFIG_TASK_NAME_SIZE */
 
   /* For pthreads, args are strictly pass-by-value; that actual
