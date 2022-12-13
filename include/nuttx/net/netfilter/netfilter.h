a5bd90682957a60852474cac2cbb34a8a8cc5e9f
Zhe Weng <wengzhe@xiaomi.com>
2022-12-13 16:06:21 +0800

    net: Add netfilter compatible headers.
    Signed-off-by: Zhe Weng <wengzhe@xiaomi.com>

---
+++ include/nuttx/net/netfilter/netfilter.h
@@ -0,0 +1,59 @@
+/****************************************************************************
+ * include/nuttx/net/netfilter/netfilter.h
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+#ifndef __INCLUDE_NUTTX_NET_NETFILTER_NETFILTER_H
+#define __INCLUDE_NUTTX_NET_NETFILTER_NETFILTER_H
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <sys/types.h>
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+/* Responses from hook functions. */
+
+#define NF_DROP   0
+#define NF_ACCEPT 1
+#define NF_STOLEN 2
+#define NF_QUEUE  3
+#define NF_REPEAT 4
+#define NF_STOP   5 /* Deprecated, for userspace nf_queue compatibility. */
+#define NF_MAX_VERDICT NF_STOP
+
+/****************************************************************************
+ * Public Types
+ ****************************************************************************/
+
+enum nf_inet_hooks
+{
+  NF_INET_PRE_ROUTING,
+  NF_INET_LOCAL_IN,
+  NF_INET_FORWARD,
+  NF_INET_LOCAL_OUT,
+  NF_INET_POST_ROUTING,
+  NF_INET_NUMHOOKS,
+  NF_INET_INGRESS = NF_INET_NUMHOOKS,
+};
+
+#endif /* __INCLUDE_NUTTX_NET_NETFILTER_NETFILTER_H */
---
+++ include/nuttx/net/netfilter/nf_nat.h
@@ -0,0 +1,81 @@
+/****************************************************************************
+ * include/nuttx/net/netfilter/nf_nat.h
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+#ifndef __INCLUDE_NUTTX_NET_NETFILTER_NF_NAT_H
+#define __INCLUDE_NUTTX_NET_NETFILTER_NF_NAT_H
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <sys/types.h>
+
+/****************************************************************************
+ * Public Types
+ ****************************************************************************/
+
+union nf_conntrack_man_proto
+{
+  /* Add other protocols here. */
+
+  uint16_t all;
+
+  struct
+    {
+      uint16_t port;
+    } tcp;
+  struct
+    {
+      uint16_t port;
+    } udp;
+  struct
+    {
+      uint16_t id;
+    } icmp;
+  struct
+    {
+      uint16_t port;
+    } dccp;
+  struct
+    {
+      uint16_t port;
+    } sctp;
+  struct
+    {
+      uint16_t key; /* GRE key is 32bit, PPtP only uses 16bit */
+    } gre;
+};
+
+struct nf_nat_ipv4_range
+{
+  unsigned int                 flags;
+  uint32_t                     min_ip;
+  uint32_t                     max_ip;
+  union nf_conntrack_man_proto min;
+  union nf_conntrack_man_proto max;
+};
+
+struct nf_nat_ipv4_multi_range_compat
+{
+  unsigned int             rangesize;
+  struct nf_nat_ipv4_range range[1];
+};
+
+#endif /* __INCLUDE_NUTTX_NET_NETFILTER_NF_NAT_H */
