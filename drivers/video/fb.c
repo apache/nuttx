12b9a92da912d6da7cb878affb6e5bc11b494b5d
jihandong <jihandong@xiaomi.com>
2022-12-13 16:42:12 +0800

    driver/fb: support linux info
    VELAPLATFO-3723
    
    Change-Id: I4ffa14a73601c1890548e2773245abe7c79b28d5
    Signed-off-by: jihandong <jihandong@xiaomi.com>

--- drivers/video/fb.c
+++ drivers/video/fb.c
@@ -539,6 +539,136 @@ static int fb_ioctl(FAR struct file *filep, int cmd, unsigned long arg)
         }
         break;
 
+      case FBIOGET_VSCREENINFO:
+        {
+          struct fb_videoinfo_s vinfo;
+          struct fb_planeinfo_s pinfo;
+          FAR struct fb_var_screeninfo *varinfo =
+            (FAR struct fb_var_screeninfo *)((uintptr_t)arg);
+
+          DEBUGASSERT(varinfo != 0 && fb->vtable != NULL &&
+                      fb->vtable->getvideoinfo != NULL &&
+                      fb->vtable->getplaneinfo != NULL);
+          ret = fb->vtable->getvideoinfo(fb->vtable, &vinfo);
+          if (ret < 0)
+            {
+              break;
+            }
+
+          ret = fb->vtable->getplaneinfo(fb->vtable, fb->plane, &pinfo);
+          if (ret < 0)
+            {
+              break;
+            }
+
+          memset(varinfo, 0, sizeof(struct fb_var_screeninfo));
+          varinfo->xres           = vinfo.xres;
+          varinfo->yres           = vinfo.yres;
+          varinfo->xres_virtual   = pinfo.xres_virtual;
+          varinfo->yres_virtual   = pinfo.yres_virtual;
+          varinfo->xoffset        = pinfo.xoffset;
+          varinfo->yoffset        = pinfo.yoffset;
+          varinfo->bits_per_pixel = pinfo.bpp;
+          varinfo->grayscale      = FB_ISMONO(vinfo.fmt);
+          switch (vinfo.fmt)
+            {
+              case FB_FMT_Y1:
+                varinfo->red.offset    = 0;
+                varinfo->green.offset  = 0;
+                varinfo->blue.offset   = 0;
+                varinfo->red.length    = 1;
+                varinfo->green.length  = 1;
+                varinfo->blue.length   = 1;
+                break;
+
+              case FB_FMT_Y8:
+                varinfo->red.offset    = 0;
+                varinfo->green.offset  = 0;
+                varinfo->blue.offset   = 0;
+                varinfo->red.length    = 8;
+                varinfo->green.length  = 8;
+                varinfo->blue.length   = 8;
+                break;
+
+              case FB_FMT_RGB16_555:
+                varinfo->red.offset    = 10;
+                varinfo->green.offset  = 5;
+                varinfo->blue.offset   = 0;
+                varinfo->red.length    = 5;
+                varinfo->green.length  = 5;
+                varinfo->blue.length   = 5;
+                break;
+
+              case FB_FMT_RGB16_565:
+                varinfo->red.offset    = 11;
+                varinfo->green.offset  = 5;
+                varinfo->blue.offset   = 0;
+                varinfo->red.length    = 5;
+                varinfo->green.length  = 6;
+                varinfo->blue.length   = 5;
+                break;
+
+              case FB_FMT_RGB24:
+              case FB_FMT_RGB32:
+                varinfo->red.offset    = 16;
+                varinfo->green.offset  = 8;
+                varinfo->blue.offset   = 0;
+                varinfo->red.length    = 8;
+                varinfo->green.length  = 8;
+                varinfo->blue.length   = 8;
+                break;
+
+              case FB_FMT_RGBA32:
+                varinfo->red.offset    = 16;
+                varinfo->green.offset  = 8;
+                varinfo->blue.offset   = 0;
+                varinfo->transp.offset = 24;
+                varinfo->red.length    = 8;
+                varinfo->green.length  = 8;
+                varinfo->blue.length   = 8;
+                varinfo->transp.length = 8;
+                break;
+          }
+        }
+        break;
+
+      case FBIOGET_FSCREENINFO:
+        {
+          struct fb_videoinfo_s vinfo;
+          struct fb_planeinfo_s pinfo;
+          FAR struct fb_fix_screeninfo *fixinfo =
+            (FAR struct fb_fix_screeninfo *)((uintptr_t)arg);
+
+          DEBUGASSERT(fixinfo != 0 && fb->vtable != NULL &&
+                      fb->vtable->getvideoinfo != NULL &&
+                      fb->vtable->getplaneinfo != NULL);
+          ret = fb->vtable->getvideoinfo(fb->vtable, &vinfo);
+          if (ret < 0)
+            {
+              break;
+            }
+
+          ret = fb->vtable->getplaneinfo(fb->vtable, fb->plane, &pinfo);
+          if (ret < 0)
+            {
+              break;
+            }
+
+          memset(fixinfo, 0, sizeof(struct fb_fix_screeninfo));
+#ifdef CONFIG_FB_MODULEINFO
+          strlcpy(fixinfo->id, vinfo.moduleinfo, sizeof(fixinfo->id));
+#endif
+          fixinfo->smem_start  = (unsigned long)pinfo.fbmem;
+          fixinfo->smem_len    = pinfo.fblen;
+          fixinfo->type        = FB_ISYUVPLANAR(vinfo.fmt) ?
+                                 FB_TYPE_INTERLEAVED_PLANES :
+                                 FB_TYPE_PACKED_PIXELS;
+          fixinfo->visual      = FB_ISMONO(vinfo.fmt) ?
+                                 FB_VISUAL_MONO10 : FB_VISUAL_TRUECOLOR;
+          fixinfo->line_length = pinfo.stride;
+        }
+        break;
+
       default:
         gerr("ERROR: Unsupported IOCTL command: %d\n", cmd);
         ret = -ENOTTY;
--- include/nuttx/video/fb.h
+++ include/nuttx/video/fb.h
@@ -286,6 +286,180 @@
                                                * Argument: read-only struct
                                                *           fb_planeinfo_s* */
 
+/* Linux Support ************************************************************/
+
+#define FBIOGET_VSCREENINFO   _FBIOC(0x0017)  /* Get video variable info */
+                                              /* Argument: writable struct
+                                               *           fb_var_screeninfo */
+#define FBIOGET_FSCREENINFO   _FBIOC(0x0018)  /* Get video fix info */
+                                              /* Argument: writable struct
+                                               *           fb_fix_screeninfo */
+
+#define FB_TYPE_PACKED_PIXELS        0      /* Packed Pixels */
+#define FB_TYPE_PLANES               1      /* Non interleaved planes */
+#define FB_TYPE_INTERLEAVED_PLANES   2      /* Interleaved planes */
+#define FB_TYPE_TEXT                 3      /* Text/attributes */
+#define FB_TYPE_VGA_PLANES           4      /* EGA/VGA planes  */
+#define FB_TYPE_FOURCC               5      /* Type identified by a V4L2 FOURCC */
+
+#define FB_AUX_TEXT_MDA              0      /* Monochrome text */
+#define FB_AUX_TEXT_CGA              1      /* CGA/EGA/VGA Color text */
+#define FB_AUX_TEXT_S3_MMIO          2      /* S3 MMIO fasttext */
+#define FB_AUX_TEXT_MGA_STEP16       3      /* MGA Millenium I: text, attr, */
+                                            /* 14 reserved bytes */
+#define FB_AUX_TEXT_MGA_STEP8        4      /* other MGAs: text, attr, */
+                                            /* 6 reserved bytes */
+#define FB_AUX_TEXT_SVGA_GROUP       8      /* 8-15: SVGA tileblit */
+                                            /* compatible modes */
+#define FB_AUX_TEXT_SVGA_MASK        7      /* lower three bits says step */
+#define FB_AUX_TEXT_SVGA_STEP2       8      /* SVGA text mode: text, attr */
+#define FB_AUX_TEXT_SVGA_STEP4       9      /* SVGA text mode: text, attr, */
+                                            /* 2 reserved bytes */
+#define FB_AUX_TEXT_SVGA_STEP8       10     /* SVGA text mode: text, attr, */
+                                            /* 6 reserved bytes */
+#define FB_AUX_TEXT_SVGA_STEP16      11     /* SVGA text mode: text, attr, */
+                                            /* 14 reserved bytes */
+#define FB_AUX_TEXT_SVGA_LAST        15     /* reserved up to 15 */
+
+#define FB_AUX_VGA_PLANES_VGA4       0      /* 16 color planes (EGA/VGA) */
+#define FB_AUX_VGA_PLANES_CFB4       1      /* CFB4 in planes (VGA) */
+#define FB_AUX_VGA_PLANES_CFB8       2      /* CFB8 in planes (VGA) */
+
+#define FB_VISUAL_MONO01             0      /* Monochr. 1=Black 0=White */
+#define FB_VISUAL_MONO10             1      /* Monochr. 1=White 0=Black */
+#define FB_VISUAL_TRUECOLOR          2      /* True color */
+#define FB_VISUAL_PSEUDOCOLOR        3      /* Pseudo color (like atari) */
+#define FB_VISUAL_DIRECTCOLOR        4      /* Direct color */
+#define FB_VISUAL_STATIC_PSEUDOCOLOR 5      /* Pseudo color readonly */
+#define FB_VISUAL_FOURCC             6      /* Visual identified by */
+                                            /* a V4L2 FOURCC */
+
+#define FB_ACCEL_NONE                0      /* no hardware accelerator */
+#define FB_ACCEL_ATARIBLITT          1      /* Atari Blitter */
+#define FB_ACCEL_AMIGABLITT          2      /* Amiga Blitter */
+#define FB_ACCEL_S3_TRIO64           3      /* Cybervision64 (S3 Trio64) */
+#define FB_ACCEL_NCR_77C32BLT        4      /* RetinaZ3 (NCR 77C32BLT) */
+#define FB_ACCEL_S3_VIRGE            5      /* Cybervision64/3D (S3 ViRGE) */
+#define FB_ACCEL_ATI_MACH64GX        6      /* ATI Mach 64GX family */
+#define FB_ACCEL_DEC_TGA             7      /* DEC 21030 TGA */
+#define FB_ACCEL_ATI_MACH64CT        8      /* ATI Mach 64CT family */
+#define FB_ACCEL_ATI_MACH64VT        9      /* ATI Mach 64CT family VT class */
+#define FB_ACCEL_ATI_MACH64GT        10     /* ATI Mach 64CT family GT class */
+#define FB_ACCEL_SUN_CREATOR         11     /* Sun Creator/Creator3D */
+#define FB_ACCEL_SUN_CGSIX           12     /* Sun cg6 */
+#define FB_ACCEL_SUN_LEO             13     /* Sun leo/zx */
+#define FB_ACCEL_IMS_TWINTURBO       14     /* IMS Twin Turbo */
+#define FB_ACCEL_3DLABS_PERMEDIA2    15     /* 3Dlabs Permedia 2 */
+#define FB_ACCEL_MATROX_MGA2064W     16     /* Matrox MGA2064W (Millenium) */
+#define FB_ACCEL_MATROX_MGA1064SG    17     /* Matrox MGA1064SG (Mystique) */
+#define FB_ACCEL_MATROX_MGA2164W     18     /* Matrox MGA2164W (Millenium II) */
+#define FB_ACCEL_MATROX_MGA2164W_AGP 19     /* Matrox MGA2164W (Millenium II) */
+#define FB_ACCEL_MATROX_MGAG100      20     /* Matrox G100 (Productiva G100) */
+#define FB_ACCEL_MATROX_MGAG200      21     /* Matrox G200 (Myst, Mill, ...) */
+#define FB_ACCEL_SUN_CG14            22     /* Sun cgfourteen */
+#define FB_ACCEL_SUN_BWTWO           23     /* Sun bwtwo */
+#define FB_ACCEL_SUN_CGTHREE         24     /* Sun cgthree */
+#define FB_ACCEL_SUN_TCX             25     /* Sun tcx */
+#define FB_ACCEL_MATROX_MGAG400      26     /* Matrox G400 */
+#define FB_ACCEL_NV3                 27     /* nVidia RIVA 128 */
+#define FB_ACCEL_NV4                 28     /* nVidia RIVA TNT */
+#define FB_ACCEL_NV5                 29     /* nVidia RIVA TNT2 */
+#define FB_ACCEL_CT_6555x            30     /* C&T 6555x */
+#define FB_ACCEL_3DFX_BANSHEE        31     /* 3Dfx Banshee */
+#define FB_ACCEL_ATI_RAGE128         32     /* ATI Rage128 family */
+#define FB_ACCEL_IGS_CYBER2000       33     /* CyberPro 2000 */
+#define FB_ACCEL_IGS_CYBER2010       34     /* CyberPro 2010 */
+#define FB_ACCEL_IGS_CYBER5000       35     /* CyberPro 5000 */
+#define FB_ACCEL_SIS_GLAMOUR         36     /* SiS 300/630/540 */
+#define FB_ACCEL_3DLABS_PERMEDIA3    37     /* 3Dlabs Permedia 3 */
+#define FB_ACCEL_ATI_RADEON          38     /* ATI Radeon family */
+#define FB_ACCEL_I810                39     /* Intel 810/815 */
+#define FB_ACCEL_SIS_GLAMOUR_2       40     /* SiS 315, 650, 740 */
+#define FB_ACCEL_SIS_XABRE           41     /* SiS 330 ("Xabre") */
+#define FB_ACCEL_I830                42     /* Intel 830M/845G/85x/865G */
+#define FB_ACCEL_NV_10               43     /* nVidia Arch 10 */
+#define FB_ACCEL_NV_20               44     /* nVidia Arch 20 */
+#define FB_ACCEL_NV_30               45     /* nVidia Arch 30 */
+#define FB_ACCEL_NV_40               46     /* nVidia Arch 40 */
+#define FB_ACCEL_XGI_VOLARI_V        47     /* XGI Volari V3XT, V5, V8 */
+#define FB_ACCEL_XGI_VOLARI_Z        48     /* XGI Volari Z7 */
+#define FB_ACCEL_OMAP1610            49     /* TI OMAP16xx */
+#define FB_ACCEL_TRIDENT_TGUI        50     /* Trident TGUI */
+#define FB_ACCEL_TRIDENT_3DIMAGE     51     /* Trident 3DImage */
+#define FB_ACCEL_TRIDENT_BLADE3D     52     /* Trident Blade3D */
+#define FB_ACCEL_TRIDENT_BLADEXP     53     /* Trident BladeXP */
+#define FB_ACCEL_CIRRUS_ALPINE       53     /* Cirrus Logic 543x/544x/5480 */
+#define FB_ACCEL_NEOMAGIC_NM2070     90     /* NeoMagic NM2070 */
+#define FB_ACCEL_NEOMAGIC_NM2090     91     /* NeoMagic NM2090 */
+#define FB_ACCEL_NEOMAGIC_NM2093     92     /* NeoMagic NM2093 */
+#define FB_ACCEL_NEOMAGIC_NM2097     93     /* NeoMagic NM2097 */
+#define FB_ACCEL_NEOMAGIC_NM2160     94     /* NeoMagic NM2160 */
+#define FB_ACCEL_NEOMAGIC_NM2200     95     /* NeoMagic NM2200 */
+#define FB_ACCEL_NEOMAGIC_NM2230     96     /* NeoMagic NM2230 */
+#define FB_ACCEL_NEOMAGIC_NM2360     97     /* NeoMagic NM2360 */
+#define FB_ACCEL_NEOMAGIC_NM2380     98     /* NeoMagic NM2380 */
+#define FB_ACCEL_PXA3XX              99     /* PXA3xx */
+
+#define FB_ACCEL_SAVAGE4             0x80   /* S3 Savage4 */
+#define FB_ACCEL_SAVAGE3D            0x81   /* S3 Savage3D */
+#define FB_ACCEL_SAVAGE3D_MV         0x82   /* S3 Savage3D-MV */
+#define FB_ACCEL_SAVAGE2000          0x83   /* S3 Savage2000 */
+#define FB_ACCEL_SAVAGE_MX_MV        0x84   /* S3 Savage/MX-MV */
+#define FB_ACCEL_SAVAGE_MX           0x85   /* S3 Savage/MX */
+#define FB_ACCEL_SAVAGE_IX_MV        0x86   /* S3 Savage/IX-MV */
+#define FB_ACCEL_SAVAGE_IX           0x87   /* S3 Savage/IX */
+#define FB_ACCEL_PROSAVAGE_PM        0x88   /* S3 ProSavage PM133 */
+#define FB_ACCEL_PROSAVAGE_KM        0x89   /* S3 ProSavage KM133 */
+#define FB_ACCEL_S3TWISTER_P         0x8a   /* S3 Twister */
+#define FB_ACCEL_S3TWISTER_K         0x8b   /* S3 TwisterK */
+#define FB_ACCEL_SUPERSAVAGE         0x8c   /* S3 Supersavage */
+#define FB_ACCEL_PROSAVAGE_DDR       0x8d   /* S3 ProSavage DDR */
+#define FB_ACCEL_PROSAVAGE_DDRK      0x8e   /* S3 ProSavage DDR-K */
+
+#define FB_ACCEL_PUV3_UNIGFX         0xa0   /* PKUnity-v3 Unigfx */
+
+#define FB_NONSTD_HAM                1      /* Hold-And-Modify (HAM) */
+#define FB_NONSTD_REV_PIX_IN_B       2      /* Order of pixels in each byte */
+                                            /* is reversed */
+
+#define FB_ACTIVATE_NOW              0      /* Set values immediately (or vbl) */
+#define FB_ACTIVATE_NXTOPEN          1      /* Activate on next open */
+#define FB_ACTIVATE_TEST             2      /* Don't set, round up impossible */
+#define FB_ACTIVATE_MASK             15     /* values */
+#define FB_ACTIVATE_VBL              16     /* Activate values on next vbl  */
+#define FB_CHANGE_CMAP_VBL           32     /* Change colormap on vbl */
+#define FB_ACTIVATE_ALL              64     /* Change all VCs on this fb */
+#define FB_ACTIVATE_FORCE            128    /* Force apply even when no change */
+#define FB_ACTIVATE_INV_MODE         256    /* Invalidate videomode */
+
+#define FB_ACCELF_TEXT               1      /* (OBSOLETE) See fb_info.flags */
+                                            /* and vc_mode */
+
+#define FB_SYNC_HOR_HIGH_ACT         1      /* Horizontal sync high active */
+#define FB_SYNC_VERT_HIGH_ACT        2      /* Vertical sync high active */
+#define FB_SYNC_EXT                  4      /* External sync */
+#define FB_SYNC_COMP_HIGH_ACT        8      /* Composite sync high active */
+#define FB_SYNC_BROADCAST            16     /* Broadcast video timings */
+                                            /* vtotal = 144d/288n/576i => PAL */
+                                            /* vtotal = 121d/242n/484i => NTSC */
+#define FB_SYNC_ON_GREEN             32     /* Sync on green */
+
+#define FB_VMODE_NONINTERLACED       0      /* Non interlaced */
+#define FB_VMODE_INTERLACED          1      /* Interlaced */
+#define FB_VMODE_DOUBLE              2      /* Double scan */
+#define FB_VMODE_ODD_FLD_FIRST       4      /* Interlaced: top line first */
+#define FB_VMODE_MASK                255
+
+#define FB_VMODE_YWRAP               256    /* Ywrap instead of panning */
+#define FB_VMODE_SMOOTH_XPAN         512    /* Smooth xpan possible */
+                                            /* (internally used) */
+#define FB_VMODE_CONUPDATE           512    /* Don't update x/yoffset */
+
+#define FB_ROTATE_UR                 0
+#define FB_ROTATE_CW                 1
+#define FB_ROTATE_UD                 2
+#define FB_ROTATE_CCW                3
+
 /****************************************************************************
  * Public Types
  ****************************************************************************/
@@ -617,6 +791,84 @@ struct fb_vtable_s
   /* Pointer to framebuffer device private data. */
 
   FAR void *priv;
+};
+
+/* Linux Support ************************************************************/
+
+struct fb_fix_screeninfo
+{
+  char          id[16];       /* Identification string eg "TT Builtin" */
+  unsigned long smem_start;   /* Start of frame buffer mem */
+                              /* (physical address) */
+  uint32_t      smem_len;     /* Length of frame buffer mem */
+  uint32_t      type;         /* See FB_TYPE_* */
+  uint32_t      type_aux;     /* Interleave for interleaved Planes */
+  uint32_t      visual;       /* See FB_VISUAL_* */
+  uint16_t      xpanstep;     /* Zero if no hardware panning  */
+  uint16_t      ypanstep;     /* Zero if no hardware panning  */
+  uint16_t      ywrapstep;    /* Zero if no hardware ywrap    */
+  uint32_t      line_length;  /* Length of a line in bytes    */
+  unsigned long mmio_start;   /* Start of Memory Mapped I/O */
+                              /* (physical address) */
+  uint32_t      mmio_len;     /* Length of Memory Mapped I/O  */
+  uint32_t      accel;        /* Indicate to driver which */
+                              /* specific chip/card we have */
+  uint16_t      capabilities; /* See FB_CAP_* */
+  uint16_t      reserved[2];  /* Reserved for future compatibility */
+};
+
+/* Interpretation of offset for color fields: All offsets are from the right,
+ * inside a "pixel" value, which is exactly 'bits_per_pixel' wide (means: you
+ * can use the offset as right argument to <<). A pixel afterwards is a bit
+ * stream and is written to video memory as that unmodified.
+ *
+ * For pseudocolor: offset and length should be the same for all color
+ * components. Offset specifies the position of the least significant bit
+ * of the pallette index in a pixel value. Length indicates the number
+ * of available palette entries (i.e. # of entries = 1 << length).
+ */
+
+struct fb_bitfield
+{
+  uint32_t offset;    /* Beginning of bitfield */
+  uint32_t length;    /* Length of bitfield */
+  uint32_t msb_right; /* != 0 : Most significant bit is  right */
+};
+
+struct fb_var_screeninfo
+{
+  uint32_t xres;             /* Visible resolution */
+  uint32_t yres;
+  uint32_t xres_virtual;     /* Virtual resolution */
+  uint32_t yres_virtual;
+  uint32_t xoffset;          /* Offset from virtual to visible */
+  uint32_t yoffset;          /* Resolution */
+  uint32_t bits_per_pixel;   /* Guess what */
+  uint32_t grayscale;        /* 0 = color, 1 = grayscale, >1 = FOURCC */
+  struct fb_bitfield red;    /* Bitfield in fb mem if true color, */
+  struct fb_bitfield green;  /* else only length is significant */
+  struct fb_bitfield blue;
+  struct fb_bitfield transp; /* Transparency */
+  uint32_t nonstd;           /* != 0 Non standard pixel format */
+  uint32_t activate;         /* See FB_ACTIVATE_* */
+  uint32_t height;           /* Height of picture in mm */
+  uint32_t width;            /* Width of picture in mm */
+  uint32_t accel_flags;      /* (OBSOLETE) See fb_info.flags */
+
+  /* Timing: All values in pixclocks, except pixclock (of course) */
+
+  uint32_t pixclock;         /* Pixel clock in ps (pico seconds) */
+  uint32_t left_margin;      /* Time from sync to picture */
+  uint32_t right_margin;     /* Time from picture to sync */
+  uint32_t upper_margin;     /* Time from sync to picture */
+  uint32_t lower_margin;
+  uint32_t hsync_len;        /* Length of horizontal sync */
+  uint32_t vsync_len;        /* Length of vertical sync */
+  uint32_t sync;             /* See FB_SYNC_* */
+  uint32_t vmode;            /* See FB_VMODE_* */
+  uint32_t rotate;           /* Angle we rotate counter clockwise */
+  uint32_t colorspace;       /* Colorspace for FOURCC-based modes */
+  uint32_t reserved[4];      /* Reserved for future compatibility */
 };
 
 /****************************************************************************
