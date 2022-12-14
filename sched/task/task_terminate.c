6557f3a30a4fcb714dbca6c055d036e6667180ae
Ville Juven <ville.juven@unikie.com>
2022-12-14 09:37:22 +0200

    libc/stdio: Flush streams in userspace when process exits
    This fixes a long standing issue, where the kernel does flushing of file
    streams, although it should not handle such streams at all.

--- sched/task/task_terminate.c
+++ sched/task/task_terminate.c
@@ -51,8 +51,8 @@
  *   to determine if blocking is permitted or not.
  *
  *   This function is the final function called all task termination
- *   sequences.  nxtask_terminate() is called only from task_delete() (with
- *   nonblocking == false) and from nxtask_exit() (with nonblocking == true).
+ *   sequences.  nxtask_terminate() is called only from task_delete() and
+ *   from nxtask_exit().
  *
  *   The path through nxtask_exit() supports the final stops of the exit(),
  *   _exit(), and pthread_exit
@@ -60,18 +60,12 @@
  *   - pthread_exit().  Calls _exit()
  *   - exit(). Calls _exit()
  *   - _exit().  Calls nxtask_exit() making the currently running task
- *     non-running. nxtask_exit then calls nxtask_terminate() (with nonblocking
- *     == true) to terminate the non-running task.
- *
- *   NOTE: that the state of non-blocking is irrelevant when called through
- *   exit() and pthread_exit().  In those cases nxtask_exithook() has already
- *   been called with nonblocking == false;
+ *     non-running. nxtask_exit then calls nxtask_terminate() to terminate
+ *     the non-running task.
  *
  * Input Parameters:
  *   pid - The task ID of the task to delete.  A pid of zero
  *         signifies the calling task.
- *   nonblocking - True: The task is an unhealthy, partially torn down
- *         state and is not permitted to block.
  *
  * Returned Value:
  *   OK on success; or ERROR on failure
@@ -81,7 +75,7 @@
  *
  *******************************************************************************/
 
-int nxtask_terminate(pid_t pid, bool nonblocking)
+int nxtask_terminate(pid_t pid)
 {
   FAR struct tcb_s *dtcb;
   FAR dq_queue_t *tasklist;
@@ -159,18 +153,14 @@ int nxtask_terminate(pid_t pid, bool nonblocking)
 
   leave_critical_section(flags);
 
-  /* Perform common task termination logic (flushing streams, calling
-   * functions registered by at_exit/on_exit, etc.).  We need to do
+  /* Perform common task termination logic.  We need to do
    * this as early as possible so that higher level clean-up logic
    * can run in a healthy tasking environment.
-   *
-   * In the case where the task exits via exit(), nxtask_exithook()
-   * may be called twice.
    *
    * I suppose EXIT_SUCCESS is an appropriate return value???
    */
 
-  nxtask_exithook(dtcb, EXIT_SUCCESS, nonblocking);
+  nxtask_exithook(dtcb, EXIT_SUCCESS);
 
   /* Since all tasks pass through this function as the final step in their
    * exit sequence, this is an appropriate place to inform any instrumentation
