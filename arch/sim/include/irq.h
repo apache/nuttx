0dc8bf60ebb2911411e1015ce2c92f11ce3e971d
Xiang Xiao <xiaoxiang@xiaomi.com>
2022-12-17 04:10:06 +0800

    arch/sim: Disable interrupt in up_interrupt_context for SMP
    like other arch to fix the race condition
    
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- arch/sim/include/irq.h
+++ arch/sim/include/irq.h
@@ -146,7 +146,17 @@ static inline uintptr_t up_getsp(void)
 
 static inline bool up_interrupt_context(void)
 {
-  return CURRENT_REGS != NULL;
+#ifdef CONFIG_SMP
+  irqstate_t flags = up_irq_save();
+#endif
+
+  bool ret = CURRENT_REGS != NULL;
+
+#ifdef CONFIG_SMP
+  up_irq_restore(flags);
+#endif
+
+  return ret;
 }
 
 #undef EXTERN
