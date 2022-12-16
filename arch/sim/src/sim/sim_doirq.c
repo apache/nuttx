d6309f5a67793d08b8dd60f4f56945a587943429
Xiang Xiao <xiaoxiang@xiaomi.com>
2022-12-17 04:06:30 +0800

    arch/sim: Rename sim_interruptcontext.c to sim_doirq.c
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- arch/sim/src/sim/sim_doirq.c
+++ arch/sim/src/sim/sim_doirq.c
@@ -1,5 +1,5 @@
 /****************************************************************************
- * arch/sim/src/sim/sim_interruptcontext.c
+ * arch/sim/src/sim/sim_doirq.c
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
