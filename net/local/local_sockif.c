e9428518f1c8ac3e4b37be0f896d280c497e3ada
Xiang Xiao <xiaoxiang@xiaomi.com>
2022-12-18 15:18:56 +0800

    net/local: Support the abstract path
    https://man7.org/linux/man-pages/man7/unix.7.html
    
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- net/local/local_sockif.c
+++ net/local/local_sockif.c
@@ -379,15 +379,16 @@ static int local_getsockname(FAR struct socket *psock,
 
           *addrlen = sizeof(sa_family_t);
         }
-      else /* conn->lctype = LOCAL_TYPE_PATHNAME */
+      else /* conn->lc_type = LOCAL_TYPE_PATHNAME */
         {
           /* Get the full length of the socket name (incl. null terminator) */
 
-          int namelen = strlen(conn->lc_path) + 1;
+          size_t namelen = strlen(conn->lc_path) + 1 +
+                           (conn->lc_type == LOCAL_TYPE_ABSTRACT);
 
           /* Get the available length in the user-provided buffer. */
 
-          int pathlen = *addrlen - sizeof(sa_family_t);
+          size_t pathlen = *addrlen - sizeof(sa_family_t);
 
           /* Clip the socket name size so that if fits in the user buffer */
 
@@ -398,8 +399,15 @@ static int local_getsockname(FAR struct socket *psock,
 
           /* Copy the path into the user address structure */
 
-          strlcpy(unaddr->sun_path, conn->lc_path, namelen);
-          unaddr->sun_path[pathlen - 1] = '\0';
+          if (conn->lc_type == LOCAL_TYPE_ABSTRACT)
+            {
+              unaddr->sun_path[0] = '\0';
+              strlcpy(&unaddr->sun_path[1], conn->lc_path, namelen - 1);
+            }
+          else
+            {
+              strlcpy(unaddr->sun_path, conn->lc_path, namelen);
+            }
 
           *addrlen = sizeof(sa_family_t) + namelen;
         }
