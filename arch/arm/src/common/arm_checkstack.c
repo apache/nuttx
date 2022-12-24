b9d7d009435a2927610af3b483e11b3dee3f1ac2
Xiang Xiao <xiaoxiang@xiaomi.com>
2022-12-25 01:52:34 +0800

    arch: Remove the unused arch color function variant
    up_check_tcbstack_remain, up_check_stack, up_check_stack_remain and up_check_intstack_remain
    
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- arch/arm/src/common/arm_checkstack.c
+++ arch/arm/src/common/arm_checkstack.c
@@ -182,7 +182,7 @@ void arm_stack_color(void *stackbase, size_t nbytes)
 }
 
 /****************************************************************************
- * Name: up_check_stack and friends
+ * Name: up_check_tcbstack and friends
  *
  * Description:
  *   Determine (approximately) how much stack has been used be searching the
@@ -224,31 +224,11 @@ size_t up_check_tcbstack(struct tcb_s *tcb)
   return size;
 }
 
-ssize_t up_check_tcbstack_remain(struct tcb_s *tcb)
-{
-  return tcb->adj_stack_size - up_check_tcbstack(tcb);
-}
-
-size_t up_check_stack(void)
-{
-  return up_check_tcbstack(running_task());
-}
-
-ssize_t up_check_stack_remain(void)
-{
-  return up_check_tcbstack_remain(running_task());
-}
-
 #if CONFIG_ARCH_INTERRUPTSTACK > 3
 size_t up_check_intstack(void)
 {
   return arm_stack_check((void *)up_get_intstackbase(),
                          STACK_ALIGN_DOWN(CONFIG_ARCH_INTERRUPTSTACK));
-}
-
-size_t up_check_intstack_remain(void)
-{
-  return STACK_ALIGN_DOWN(CONFIG_ARCH_INTERRUPTSTACK) - up_check_intstack();
 }
 #endif
 
--- arch/xtensa/src/common/xtensa_checkstack.c
+++ arch/xtensa/src/common/xtensa_checkstack.c
@@ -141,7 +141,7 @@ size_t xtensa_stack_check(uintptr_t alloc, size_t size)
 }
 
 /****************************************************************************
- * Name: up_check_stack and friends
+ * Name: up_check_tcbstack and friends
  *
  * Description:
  *   Determine (approximately) how much stack has been used be searching the
@@ -162,30 +162,10 @@ size_t up_check_tcbstack(struct tcb_s *tcb)
                             tcb->adj_stack_size);
 }
 
-ssize_t up_check_tcbstack_remain(struct tcb_s *tcb)
-{
-  return tcb->adj_stack_size - up_check_tcbstack(tcb);
-}
-
-size_t up_check_stack(void)
-{
-  return up_check_tcbstack(running_task());
-}
-
-ssize_t up_check_stack_remain(void)
-{
-  return up_check_tcbstack_remain(running_task());
-}
-
 #if CONFIG_ARCH_INTERRUPTSTACK > 15
 size_t up_check_intstack(void)
 {
   return xtensa_stack_check(up_get_intstackbase(), INTSTACK_SIZE);
-}
-
-size_t up_check_intstack_remain(void)
-{
-  return INTSTACK_SIZE - up_check_intstack();
 }
 #endif
 
