a7b292653a14209db97807d8e783b8757214f4c1
yinshengkai <yinshengkai@xiaomi.com>
2022-12-27 19:48:46 +0800

    Revert f72f7ebee08215bce2443a84c2173c4031297e0a
    VELAPLATFO-2150
    
    Revert "Sysview add prefix kconfig to add an option to decouple sched_note calls"
    
    Signed-off-by: yinshengkai <yinshengkai@xiaomi.com>
    Change-Id: Ieee6db82e38a202b65212b7abda18ab1fb14cb70

--- include/nuttx/segger/sysview.h
+++ include/nuttx/segger/sysview.h
@@ -26,17 +26,6 @@
  ****************************************************************************/
 
 #include <nuttx/config.h>
-#include <nuttx/sched.h>
-
-/****************************************************************************
- * Pre-processor Definitions
- ****************************************************************************/
-
-#ifdef CONFIG_SEGGER_SYSVIEW_PREFIX
-#  define PREFIX(fun) sysview ## _ ## fun
-#else
-#  define PREFIX(fun) fun
-#endif
 
 /****************************************************************************
  * Public Function Prototypes
@@ -58,43 +47,6 @@
 
 #ifdef CONFIG_SEGGER_SYSVIEW
 int sysview_initialize(void);
-
-#  ifdef CONFIG_SEGGER_SYSVIEW_PREFIX
-
-void PREFIX(sched_note_start)(struct tcb_s *tcb);
-
-void PREFIX(sched_note_stop)(struct tcb_s *tcb);
-
-void PREFIX(sched_note_suspend)(struct tcb_s *tcb);
-
-void PREFIX(sched_note_resume)(struct tcb_s *tcb);
-
-#    ifdef CONFIG_SCHED_INSTRUMENTATION_IRQHANDLER
-void PREFIX(sched_note_irqhandler)(int irq, void *handler, bool enter);
-#    endif
-
-#    ifdef CONFIG_SCHED_INSTRUMENTATION_SYSCALL
-void PREFIX(sched_note_syscall_enter)(int nr);
-void PREFIX(sched_note_syscall_leave)(int nr, uintptr_t result);
-#    endif
-
-#    ifdef CONFIG_SCHED_INSTRUMENTATION_FILTER
-void PREFIX(sched_note_filter_mode)(struct note_filter_mode_s *oldm,
-                            struct note_filter_mode_s *newm);
-#    endif
-
-#    ifdef CONFIG_SCHED_INSTRUMENTATION_IRQHANDLER
-void PREFIX(sched_note_filter_irq)(struct note_filter_irq_s *oldf,
-                           struct note_filter_irq_s *newf);
-#    endif
-
-#    ifdef CONFIG_SCHED_INSTRUMENTATION_SYSCALL
-void PREFIX(sched_note_filter_syscall)(struct note_filter_syscall_s *oldf,
-                               struct note_filter_syscall_s *newf);
-#    endif
-
-#  endif /* CONFIG_SEGGER_SYSVIEW_PREFIX */
-
-#endif /* CONFIG_SEGGER_SYSVIEW */
+#endif
 
 #endif /* __INCLUDE_NUTTX_SEGGER_SYSVIEW_H */
