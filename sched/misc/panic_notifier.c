4cb6005fc480bcd5bdb383ca54e36a678c774a8b
Xiang Xiao <xiaoxiang@xiaomi.com>
2022-12-28 17:06:44 +0800

    sched/misc: Rename panic.c to panic_notifier.c
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- sched/misc/panic_notifier.c
+++ sched/misc/panic_notifier.c
@@ -1,5 +1,5 @@
 /****************************************************************************
- * sched/misc/panic.c
+ * sched/misc/panic_notifier.c
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
