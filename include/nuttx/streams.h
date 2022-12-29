8f05661a530ec6884f72318c4a43b8e26c9bfe50
dongjiuzhu1 <dongjiuzhu1@xiaomi.com>
2022-12-29 21:56:27 +0800

    syslog: A trailing newline is added if none is present.
    refs:
    https://www.manpagez.com/man/3/syslog/
    
    Signed-off-by: dongjiuzhu1 <dongjiuzhu1@xiaomi.com>

--- include/nuttx/streams.h
+++ include/nuttx/streams.h
@@ -230,6 +230,7 @@ struct lib_syslogstream_s
 #ifdef CONFIG_SYSLOG_BUFFER
   FAR struct iob_s *iob;
 #endif
+  int last_ch;
 };
 
 /* LZF compressed stream pipeline */
@@ -455,11 +456,7 @@ void lib_syslogstream_open(FAR struct lib_syslogstream_s *stream);
  *
  ****************************************************************************/
 
-#ifdef CONFIG_SYSLOG_BUFFER
 void lib_syslogstream_close(FAR struct lib_syslogstream_s *stream);
-#else
-#  define lib_syslogstream_close(s)
-#endif
 
 /****************************************************************************
  * Name: lib_lzfoutstream
