96a45e2c7562eed8b093347060f9a4942718446a
田昕 <tianxin7@xiaomi.com>
2022-12-29 15:38:13 +0800

    sched/misc:add linux-like reboot notifier list
    Signed-off-by: 田昕 <tianxin7@xiaomi.com>

---
+++ Documentation/reference/os/notifier.rst
@@ -0,0 +1,95 @@
+===========
+Notifier Chain
+===========
+
+NuttX provides a callback list mechanism called *Notifier Chain*.
+Notifier chain is essentially a list of callbacks used at certain times,
+such as system asserting, powering off and restarting.
+
+**Notifier chain** is very much like the Linux notifier chains, except
+for some implementation differences.
+
+Classes of Notifier Chain
+======================
+
+There are currently two different classes of notifier.
+
+Atomic notifier chains
+-------------------------------
+
+Atomic notifier chains: Chain callbacks run in interrupt/atomic context.
+In Nuttx, callouts are allowed to block(In Linux, callouts in atomic
+notifier chain are not allowed to block). One example of an Atomic notifier
+chain is turning off FPU when asserting.
+
+Blocking notifier chains:
+------------------------------
+
+Blocking notifier chains: Chain callbacks run in process context.
+Callouts are allowed to block. One example of a blocking notifier chain
+is when an orderly powering off is needed. 
+
+Common Notifier Chain Interfaces
+============================
+
+Notifier Block Types
+--------------------------
+
+-  ``struct notifier_block``. Defines one notifier callback entry.
+
+Notifier Chain Interfaces
+---------------------
+
+.. c:function:: void panic_notifier_chain_register(FAR struct notifier_block *nb)
+
+  Add notifier to the panic notifier chain.
+
+  The panic notifier chain is an atomic notifier chain. It will be called
+  when asserting.
+
+  :param nb: New entry in notifier chain.
+
+.. c:function:: void panic_notifier_chain_unregister(FAR struct notifier_block *nb)
+
+  Remove notifier from the panic notifier chain.
+
+  The panic notifier chain is an atomic notifier chain. It will be called
+  when asserting.
+
+  :param nh: Entry to remove from notifier chain.
+
+.. c:function:: void panic_notifier_call_chain(unsigned long action, FAR void *data)
+
+  Call functions in the panic notifier chain.
+
+  The panic notifier chain is an atomic notifier chain. It will be called
+  when asserting.
+
+  :param action: Value passed unmodified to notifier function.
+  :param data: Pointer passed unmodified to notifier function.
+
+.. c:function:: void register_reboot_notifier(FAR struct notifier_block *nb)
+
+  Add notifier to the reboot notifier chain.
+
+  The reboot notifier chain is an atomic notifier chain.
+
+  :param nb: New entry in notifier chain.
+
+.. c:function:: void unregister_reboot_notifier(FAR struct notifier_block *nb)
+
+  Remove notifier from the reboot notifier chain.
+
+  The reboot notifier chain is an atomic notifier chain.
+
+  :param nh: Entry to remove from notifier chain.
+
+.. c:function:: void reboot_notifier_call_chain(unsigned long action, FAR void *data)
+
+  Call functions in the reboot notifier chain.
+
+  The reboot notifier chain is an atomic notifier chain.
+
+  :param action: Value passed unmodified to notifier function.
+  :param data: Pointer passed unmodified to notifier function.
+
---
+++ sched/misc/reboot_notifier.c
@@ -0,0 +1,87 @@
+/****************************************************************************
+ * sched/misc/reboot_notifier.c
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/arch.h>
+#include <nuttx/notifier.h>
+
+#include <sys/types.h>
+
+/****************************************************************************
+ * Private Data
+ ****************************************************************************/
+
+static ATOMIC_NOTIFIER_HEAD(g_reboot_notifier_list);
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name:  register_reboot_notifier
+ *
+ * Description:
+ *   Add notifier to the reboot notifier chain
+ *
+ * Input Parameters:
+ *    nb - New entry in notifier chain
+ *
+ ****************************************************************************/
+
+void register_reboot_notifier(FAR struct notifier_block *nb)
+{
+  atomic_notifier_chain_register(&g_reboot_notifier_list, nb);
+}
+
+/****************************************************************************
+ * Name:  unregister_reboot_notifier
+ *
+ * Description:
+ *   Remove notifier from the reboot notifier chain
+ *
+ * Input Parameters:
+ *    nb - Entry to remove from notifier chain
+ *
+ ****************************************************************************/
+
+void unregister_reboot_notifier(FAR struct notifier_block *nb)
+{
+  atomic_notifier_chain_unregister(&g_reboot_notifier_list, nb);
+}
+
+/****************************************************************************
+ * Name:  reboot_notifier_call_chain
+ *
+ * Description:
+ *   Call functions in the reboot notifier chain.
+ *
+ * Input Parameters:
+ *    action - Value passed unmodified to notifier function
+ *    data   - Pointer passed unmodified to notifier function
+ *
+ ****************************************************************************/
+
+void reboot_notifier_call_chain(unsigned long action, FAR void *data)
+{
+  atomic_notifier_call_chain(&g_reboot_notifier_list, action, data);
+}
