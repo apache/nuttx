7c8a0f8e5efa4de815abf0dab0da2b27693dd1aa
zhangyuan21 <zhangyuan21@xiaomi.com>
2023-01-04 13:17:31 +0800

    backtrace: use CURRENT_REGS when in interrupt context
    VELAPLATFO-5395
    
    Change-Id: I4ee14fa8e19a7c55b019c031c6ad2a1b03e21524
    Signed-off-by: zhangyuan21 <zhangyuan21@xiaomi.com>

--- arch/arm/src/common/arm_backtrace_fp.c
+++ arch/arm/src/common/arm_backtrace_fp.c
@@ -140,8 +140,8 @@ int up_backtrace(struct tcb_s *tcb,
             {
               ret += backtrace(rtcb->stack_base_ptr,
                                rtcb->stack_base_ptr + rtcb->adj_stack_size,
-                               (void *)rtcb->xcp.regs[REG_FP],
-                               (void *)rtcb->xcp.regs[REG_PC],
+                               (void *)CURRENT_REGS[REG_FP],
+                               (void *)CURRENT_REGS[REG_PC],
                                &buffer[ret], size - ret, &skip);
             }
         }
