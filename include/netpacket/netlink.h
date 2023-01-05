18d8137dfd53e04fe8ca55fdd1d18e6d5ae0ba00
zhanghongyu <zhanghongyu@xiaomi.com>
2023-01-05 20:48:14 +0800

    netlink: add struct nlmsgerr definition
    Signed-off-by: zhanghongyu <zhanghongyu@xiaomi.com>

--- include/netpacket/netlink.h
+++ include/netpacket/netlink.h
@@ -420,6 +420,22 @@ struct nlmsghdr
                           /* Data follows */
 };
 
+struct nlmsgerr
+{
+  int   error;
+  struct nlmsghdr msg;
+  /**
+   * Followed by the message contents unless NETLINK_CAP_ACK was set
+   * or the ACK indicates success (error == 0)
+   * message length is aligned with NLMSG_ALIGN()
+   */
+
+  /**
+   * Followed by TLVs defined in enum nlmsgerr_attrs
+   * if NETLINK_EXT_ACK was set
+   */
+};
+
 /* NETLINK_ROUTE Message Structures *****************************************/
 
 /* RTM_NEWLINK, RTM_DELLINK, RTM_GETLINK
