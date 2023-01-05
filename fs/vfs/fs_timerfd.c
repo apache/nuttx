f92b431c98f47a49345e701e2c582cd9217e63c7
qinwei1 <qinwei1@xiaomi.com>
2023-01-05 20:33:35 +0800

    fs/vfs: add missed truncate/mmap callback at timerfd file_operation
    Signed-off-by: qinwei1 <qinwei1@xiaomi.com>

--- fs/vfs/fs_timerfd.c
+++ fs/vfs/fs_timerfd.c
@@ -111,6 +111,8 @@ static const struct file_operations g_timerfd_fops =
   NULL,          /* write */
   NULL,          /* seek */
   NULL,          /* ioctl */
+  NULL,          /* truncate */
+  NULL,          /* mmap */
 #ifdef CONFIG_TIMER_FD_POLL
   timerfd_poll   /* poll */
 #endif
