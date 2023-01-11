b96623f7b406ace4721cdf842d03c0b2d38e19af
chao an <anchao@xiaomi.com>
2023-01-11 13:57:05 +0800

    net/devif: reuse devif_send() for can/pkt/icmp stack
    Signed-off-by: chao an <anchao@xiaomi.com>

--- net/devif/devif.h
+++ net/devif/devif.h
@@ -483,48 +483,6 @@ void devif_iob_send(FAR struct net_driver_s *dev, FAR struct iob_s *buf,
 #endif
 
 /****************************************************************************
- * Name: devif_pkt_send
- *
- * Description:
- *   Called from socket logic in order to send a raw packet in response to
- *   an xmit or poll request from the network interface driver.
- *
- *   This is almost identical to calling devif_send() except that the data to
- *   be sent is copied into dev->d_buf (vs. dev->d_appdata), since there is
- *   no header on the data.
- *
- * Assumptions:
- *   This function must be called with the network locked.
- *
- ****************************************************************************/
-
-#if defined(CONFIG_NET_PKT)
-void devif_pkt_send(FAR struct net_driver_s *dev, FAR const void *buf,
-                    unsigned int len);
-#endif
-
-/****************************************************************************
- * Name: devif_can_send
- *
- * Description:
- *   Called from socket logic in order to send a raw packet in response to
- *   an xmit or poll request from the network interface driver.
- *
- *   This is almost identical to calling devif_send() except that the data to
- *   be sent is copied into dev->d_buf (vs. dev->d_appdata), since there is
- *   no header on the data.
- *
- * Assumptions:
- *   This function must be called with the network locked.
- *
- ****************************************************************************/
-
-#if defined(CONFIG_NET_CAN)
-void devif_can_send(FAR struct net_driver_s *dev, FAR const void *buf,
-                    unsigned int len);
-#endif
-
-/****************************************************************************
  * Name: devif_out
  *
  * Description:
