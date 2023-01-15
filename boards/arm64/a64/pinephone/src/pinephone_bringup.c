c07bd5d644d30181180c2d894a17955f2a0eb0b2
Lee Lup Yuen <luppy@appkaki.com>
2023-01-15 18:19:12 +0800

    arm64/pinephone: Add driver for PinePhone Touch Panel
    This PR adds the existing Goodix GT9XX Touch Panel Driver to the Bringup Function for PINE64 PinePhone.
    
    With this PR, LVGL Touchscreen Apps will respond to Touch Input on PinePhone.
    
    ### Modified Files
    
    `boards/arm64/a64/pinephone/src/pinephone_bringup.c`: Added GT9XX Touch Panel Driver to PinePhone Bringup Function
    
    `boards/arm64/a64/pinephone/src/Makefile`: Added `pinephone_touch.c` to the Makefile
    
    `include/nuttx/input/gt9xx.h`: Fixed a comment in GT9XX Touch Panel Driver
    
    ### New Files
    
    `boards/arm64/a64/pinephone/src/pinephone_touch.c`, `pinephone_touch.h`: Register GT9XX Touch Panel Driver on PinePhone
    
    `boards/arm64/a64/pinephone/configs/lvgl/defconfig`: Added PinePhone Board Config `lvgl` to support LVGL Touchscreen Apps
    
    ### Updated Documentation
    
    `Documentation/platforms/arm/a64/boards/pinephone/index.rst`: Added PinePhone Board Config `lvgl` for LVGL Touchscreen Apps

--- boards/arm64/a64/pinephone/src/pinephone_bringup.c
+++ boards/arm64/a64/pinephone/src/pinephone_bringup.c
@@ -23,14 +23,15 @@
  ****************************************************************************/
 
 #include <nuttx/config.h>
+#include <nuttx/kmalloc.h>
 #include <sys/types.h>
 #include <syslog.h>
+#include "a64_twi.h"
+#include "pinephone.h"
+#include "pinephone_pmic.h"
+
 #ifdef CONFIG_I2C
 #  include <nuttx/i2c/i2c_master.h>
-#endif
-#include <nuttx/kmalloc.h>
-#ifdef CONFIG_MPU60X0_I2C
-#  include <nuttx/sensors/mpu60x0.h>
 #endif
 
 #ifdef CONFIG_FS_PROCFS
@@ -46,9 +47,13 @@
 #  include "pinephone_display.h"
 #endif
 
-#include "a64_twi.h"
-#include "pinephone.h"
-#include "pinephone_pmic.h"
+#ifdef CONFIG_INPUT_GT9XX
+#  include "pinephone_touch.h"
+#endif
+
+#ifdef CONFIG_MPU60X0_I2C
+#  include <nuttx/sensors/mpu60x0.h>
+#endif
 
 /****************************************************************************
  * Public Functions
@@ -64,13 +69,20 @@
 
 int pinephone_bringup(void)
 {
-  int ret;
-#ifdef CONFIG_I2C
-  int i2c_bus;
-  struct i2c_master_s *i2c;
-#ifdef CONFIG_MPU60X0_I2C
-  struct mpu_config_s *mpu_config;
+  int ret = OK;
+
+#if defined(CONFIG_I2C) && defined(CONFIG_A64_TWI0)
+  const int i2c0_bus = 0;
+  struct i2c_master_s *i2c0 = NULL;
 #endif
+
+#if defined(CONFIG_I2C) && defined(CONFIG_A64_TWI1)
+  const int i2c1_bus = 1;
+  struct i2c_master_s *i2c1 = NULL;
+#endif
+
+#if defined(CONFIG_MPU60X0_I2C) && defined(CONFIG_A64_TWI1)
+  struct mpu_config_s *mpu_config = NULL;
 #endif
 
 #ifdef CONFIG_USERLED
@@ -107,25 +119,62 @@ int pinephone_bringup(void)
   pinephone_display_test_pattern();
 #endif
 
-#if defined(CONFIG_I2C) && defined(CONFIG_A64_TWI1)
-  i2c_bus = 1;
-  i2c = a64_i2cbus_initialize(i2c_bus);
-  if (i2c == NULL)
+#if defined(CONFIG_I2C) && defined(CONFIG_A64_TWI0)
+  /* Initialize TWI0 as I2C Bus 0 */
+
+  i2c0 = a64_i2cbus_initialize(i2c0_bus);
+  if (i2c0 == NULL)
     {
-      syslog(LOG_ERR, "ERROR: Failed to get I2C%d interface\n", i2c_bus);
+      syslog(LOG_ERR, "ERROR: Failed to get I2C%d interface\n", i2c0_bus);
     }
-  else
+#endif
+
+#if defined(CONFIG_I2C) && defined(CONFIG_A64_TWI1)
+  /* Initialize TWI1 as I2C Bus 1 */
+
+  i2c1 = a64_i2cbus_initialize(i2c1_bus);
+  if (i2c1 == NULL)
     {
-#if defined(CONFIG_SYSTEM_I2CTOOL)
-      ret = i2c_register(i2c, i2c_bus);
+      syslog(LOG_ERR, "ERROR: Failed to get I2C%d interface\n", i2c1_bus);
+    }
+#endif
+
+#if defined(CONFIG_SYSTEM_I2CTOOL) && defined(CONFIG_A64_TWI1)
+  /* Register I2C Driver for I2C Bus 1 at TWI1 */
+
+  if (i2c1 != NULL)
+    {
+      ret = i2c_register(i2c1, i2c1_bus);
       if (ret < 0)
         {
           syslog(LOG_ERR, "ERROR: Failed to register I2C%d driver: %d\n",
-                 i2c_bus, ret);
+                 i2c1_bus, ret);
         }
+    }
 #endif
 
-#ifdef CONFIG_MPU60X0_I2C
+#if defined(CONFIG_INPUT_GT9XX) && defined(CONFIG_A64_TWI0)
+  /* Register Touch Input Driver for GT9XX Touch Panel at TWI0 */
+
+  if (i2c0 != NULL)
+    {
+      /* Register Touch Input Driver at /dev/input0 */
+
+      ret = pinephone_touch_panel_register("/dev/input0", i2c0);
+      if (ret < 0)
+        {
+          syslog(LOG_ERR,
+                 "ERROR: Failed to register Touch Input GT9xx: %d\n",
+                 ret);
+        }
+    }
+#endif
+
+#if defined(CONFIG_MPU60X0_I2C) && defined(CONFIG_A64_TWI1)
+  /* Register IMU Driver for MPU-60X0 Accelerometer at TWI1 */
+
+  if (i2c1 != NULL)
+    {
       /* Init PMIC */
 
       ret = pinephone_pmic_init();
@@ -139,6 +188,8 @@ int pinephone_bringup(void)
 
       up_mdelay(15);
 
+      /* Register IMU Driver at /dev/imu0 */
+
       mpu_config = kmm_zalloc(sizeof(struct mpu_config_s));
       if (mpu_config == NULL)
         {
@@ -146,11 +197,10 @@ int pinephone_bringup(void)
         }
       else
         {
-          mpu_config->i2c = i2c;
+          mpu_config->i2c = i2c1;
           mpu_config->addr = 0x68;
           mpu60x0_register("/dev/imu0", mpu_config);
         }
-#endif
     }
 #endif
 
---
+++ boards/arm64/a64/pinephone/src/pinephone_touch.c
@@ -0,0 +1,245 @@
+/****************************************************************************
+ * boards/arm64/a64/pinephone/src/pinephone_touch.c
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+/* Reference:
+ * "NuttX RTOS for PinePhone: Touch Panel"
+ * https://lupyuen.github.io/articles/touch2
+ */
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <debug.h>
+
+#include <nuttx/arch.h>
+#include <nuttx/i2c/i2c_master.h>
+#include <nuttx/input/gt9xx.h>
+#include "arm64_arch.h"
+#include "arm64_gic.h"
+#include "a64_pio.h"
+#include "pinephone_touch.h"
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+/* Goodix GT917S Touch Panel Interrupt at PH4 */
+
+#define CTP_INT (PIO_EINT | PIO_PORT_PIOH | PIO_PIN4)
+
+/* I2C Address for Touch Panel */
+
+#define CTP_I2C_ADDR 0x5d
+
+/****************************************************************************
+ * Private Function Prototypes
+ ****************************************************************************/
+
+static int pinephone_gt9xx_irq_attach(const struct gt9xx_board_s *state,
+                                      xcpt_t isr,
+                                      FAR void *arg);
+static void pinephone_gt9xx_irq_enable(const struct gt9xx_board_s *state,
+                                       bool enable);
+static int pinephone_gt9xx_set_power(const struct gt9xx_board_s *state,
+                                     bool on);
+
+/****************************************************************************
+ * Private Data
+ ****************************************************************************/
+
+/* Callback for Board-Specific Operations */
+
+static const struct gt9xx_board_s g_pinephone_gt9xx =
+{
+  .irq_attach = pinephone_gt9xx_irq_attach,
+  .irq_enable = pinephone_gt9xx_irq_enable,
+  .set_power  = pinephone_gt9xx_set_power
+};
+
+/****************************************************************************
+ * Private Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: pinephone_gt9xx_irq_attach
+ *
+ * Description:
+ *   Attach the Interrupt Handler for Touch Panel.
+ *
+ * Input Parameters:
+ *   state - Callback for Board-Specific Operations
+ *   isr   - Interrupt Handler
+ *   arg   - Argument for Interrupt Handler
+ *
+ * Returned Value:
+ *   Zero (OK) on success; a negated errno value is returned on any failure.
+ *
+ ****************************************************************************/
+
+static int pinephone_gt9xx_irq_attach(const struct gt9xx_board_s *state,
+                                      xcpt_t isr,
+                                      FAR void *arg)
+{
+  int ret;
+
+  iinfo("\n");
+  DEBUGASSERT(state != NULL && isr != NULL && arg != NULL);
+
+  /* Attach the Interrupt Handler to Port PH */
+
+  ret = irq_attach(A64_IRQ_PH_EINT, isr, arg);
+  if (ret < 0)
+    {
+      ierr("Attach Interrupt Handler failed: %d\n", ret);
+      return ret;
+    }
+
+  /* Set Interrupt Priority in Generic Interrupt Controller v2 */
+
+  arm64_gic_irq_set_priority(A64_IRQ_PH_EINT, 0, IRQ_TYPE_EDGE);
+
+  /* Enable Interrupts for Port PH */
+
+  up_enable_irq(A64_IRQ_PH_EINT);
+
+  return OK;
+}
+
+/****************************************************************************
+ * Name: pinephone_gt9xx_irq_enable
+ *
+ * Description:
+ *   Enable or disable Interrupts for the Touch Panel.
+ *
+ * Input Parameters:
+ *   state  - Callback for Board-Specific Operations
+ *   enable - True to enable interrupts; False to disable interrupts
+ *
+ * Returned Value:
+ *   Zero (OK) on success; a negated errno value is returned on any failure.
+ *
+ ****************************************************************************/
+
+static void pinephone_gt9xx_irq_enable(const struct gt9xx_board_s *state,
+                                       bool enable)
+{
+  int ret;
+
+  iinfo("enable=%d\n", enable);
+  DEBUGASSERT(state != NULL);
+
+  if (enable)
+    {
+      /* Configure the Touch Panel Interrupt */
+
+      ret = a64_pio_config(CTP_INT);
+      if (ret < 0)
+        {
+          ierr("Configure Touch Panel Interrupt failed: %d\n", ret);
+          return;
+        }
+
+      /* Enable the Touch Panel Interrupt */
+
+      ret = a64_pio_irqenable(CTP_INT);
+      if (ret < 0)
+        {
+          ierr("Enable Touch Panel Interrupt failed: %d\n", ret);
+          return;
+        }
+    }
+  else
+    {
+      /* Disable the Touch Panel Interrupt */
+
+      ret = a64_pio_irqdisable(CTP_INT);
+      if (ret < 0)
+        {
+          ierr("Disable Touch Panel Interrupt failed: %d\n", ret);
+          return;
+        }
+    }
+}
+
+/****************************************************************************
+ * Name: pinephone_gt9xx_set_power
+ *
+ * Description:
+ *   Power on or off the Touch Panel.
+ *
+ * Input Parameters:
+ *   state - Callback for Board-Specific Operations
+ *   on    - True to power on; False to power off
+ *
+ * Returned Value:
+ *   Zero (OK) on success; a negated errno value is returned on any failure.
+ *
+ ****************************************************************************/
+
+static int pinephone_gt9xx_set_power(const struct gt9xx_board_s *state,
+                                     bool on)
+{
+  /* Assume that Touch Panel is already powered on by pinephone_pmic_init() */
+
+  iinfo("on=%d\n", on);
+  return OK;
+}
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: pinephone_touch_panel_register
+ *
+ * Description:
+ *   Register the driver for Goodix GT9XX Touch Panel.  Attach the
+ *   Interrupt Handler for the Touch Panel and disable Touch Interrupts.
+ *
+ * Input Parameters:
+ *   devpath - Device Path (e.g. "/dev/input0")
+ *   i2c     - I2C Bus
+ *
+ * Returned Value:
+ *   Zero (OK) on success; a negated errno value is returned on any failure.
+ *
+ ****************************************************************************/
+
+int pinephone_touch_panel_register(const char *devpath,
+                                   struct i2c_master_s *i2c)
+{
+  int ret;
+
+  iinfo("devpath=%s\n", devpath);
+  DEBUGASSERT(devpath != NULL && i2c != NULL);
+
+  ret = gt9xx_register(devpath, i2c, CTP_I2C_ADDR, &g_pinephone_gt9xx);
+  if (ret < 0)
+    {
+      ierr("Register Touch Input GT9xx failed: %d\n", ret);
+      return ret;
+    }
+
+  return OK;
+}
