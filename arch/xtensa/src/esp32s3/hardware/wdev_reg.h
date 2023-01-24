c659b96085a55ebac34d2e757c4c2423b74494d8
Alan Carvalho de Assis <acassis@gmail.com>
2023-01-24 17:46:26 -0300

    esp32s3: Add RNG driver
    Fix multiplier noticed by Tiago
    
    Co-author: Tiago Medicci <tiago.medicci@espressif.com>

---
+++ arch/xtensa/src/esp32s3/hardware/wdev_reg.h
@@ -0,0 +1,34 @@
+/****************************************************************************
+ * arch/xtensa/src/esp32s3/hardware/wdev_reg.h
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+#ifndef __ARCH_XTENSA_SRC_ESP32S3_HARDWARE_WDEV_REG_H
+#define __ARCH_XTENSA_SRC_ESP32S3_HARDWARE_WDEV_REG_H
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include "esp32s3_soc.h"
+
+/* Hardware random number generator register */
+
+#define WDEV_RND_REG 0x6003507C
+
+#endif /* __ARCH_XTENSA_SRC_ESP32S3_HARDWARE_WDEV_REG_H */
