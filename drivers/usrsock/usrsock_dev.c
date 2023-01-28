3815e2408893bf08c68e833bbca0d3a9ddfc6ac5
lilei19 <lilei19@xiaomi.com>
2023-01-28 23:02:52 +0800

    add holder for mutex
    Signed-off-by: lilei19 <lilei19@xiaomi.com>

--- drivers/usrsock/usrsock_dev.c
+++ drivers/usrsock/usrsock_dev.c
@@ -510,7 +510,7 @@ int usrsock_request(FAR struct iovec *iov, unsigned int iovcnt)
 
   /* Set outstanding request for daemon to handle. */
 
-  net_sem_wait_uninterruptible(&dev->devlock);
+  net_mutex_lock(&dev->devlock);
 
   if (usrsockdev_is_opened(dev))
     {
