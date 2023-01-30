e622730146c9377a55e512399323df6f39797358
dongjiuzhu1 <dongjiuzhu1@xiaomi.com>
2023-01-30 21:53:07 +0800

    drivers/pipes: fix write busy loop because POLLOUT always ready.
    the size of pipes buffer +1 as compensate the full indicator
    
    Signed-off-by: dongjiuzhu1 <dongjiuzhu1@xiaomi.com>

--- drivers/pipes/pipe_common.c
+++ drivers/pipes/pipe_common.c
@@ -699,7 +699,7 @@ int pipecommon_poll(FAR struct file *filep, FAR struct pollfd *fds,
 
       eventset = 0;
       if ((filep->f_oflags & O_WROK) &&
-          nbytes < (dev->d_bufsize - dev->d_polloutthrd))
+          nbytes < (dev->d_bufsize - 1 - dev->d_polloutthrd))
         {
           eventset |= POLLOUT;
         }
