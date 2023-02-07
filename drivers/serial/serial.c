0a8eb7cc29a3f36499c7fbfca96dade1628faa7e
Huang Qi <huangqi3@xiaomi.com>
2023-02-07 18:12:08 +0800

    driver/serial: Convert CR to LF in driver
    Enable the behavior by default for console,
    but configurable by termios.
    
    Binary size:
    
    Before:
       text    data     bss     dec     hex filename
     326460     409    8164  335033   51cb9 nuttx/nuttx
    
     After:
        text    data     bss     dec     hex filename
     326478     409    8164  335051   51ccb nuttx/nuttx
    
    Signed-off-by: Huang Qi <huangqi3@xiaomi.com>

--- drivers/serial/serial.c
+++ drivers/serial/serial.c
@@ -876,6 +876,11 @@ static ssize_t uart_read(FAR struct file *filep,
            * IUCLC - Not Posix
            * IXON/OXOFF - no xon/xoff flow control.
            */
+#else
+          if (dev->isconsole && ch == '\r')
+            {
+              ch = '\n';
+            }
 #endif
 
           /* Store the received character */
@@ -1525,7 +1530,6 @@ static int uart_ioctl(FAR struct file *filep, int cmd, unsigned long arg)
               dev->tc_iflag = termiosp->c_iflag;
               dev->tc_oflag = termiosp->c_oflag;
               dev->tc_lflag = termiosp->c_lflag;
-
               ret = 0;
             }
             break;
@@ -1761,6 +1765,10 @@ int uart_register(FAR const char *path, FAR uart_dev_t *dev)
       /* Enable \n -> \r\n translation for the console */
 
       dev->tc_oflag = OPOST | ONLCR;
+
+      /* Convert CR to LF by default for console */
+
+      dev->tc_iflag |= ICRNL;
     }
 #endif
 
