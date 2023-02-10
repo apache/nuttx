98a8bcf6f0e31026b4e8a15551b4ed684115a885
David Sidrane <David.Sidrane@NscDg.com>
2023-02-10 06:13:42 -0800

    mmcsd_sdio:Release CPU during wait for Write Completion

--- drivers/mmcsd/mmcsd_sdio.c
+++ drivers/mmcsd/mmcsd_sdio.c
@@ -1266,6 +1266,10 @@ static int mmcsd_transferready(FAR struct mmcsd_state_s *priv)
           goto errorout;
         }
 
+      /* Do not hog the CPU */
+
+      nxsig_usleep(1000);
+
       /* We are still in the programming state. Calculate the elapsed
        * time... we can't stay in this loop forever!
        */
