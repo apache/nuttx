c9b324ab2623c8136464ad94fe715f57bc9a2d57
zhangyuan21 <zhangyuan21@xiaomi.com>
2023-02-10 14:41:36 +0800

    sched/pthread: check pthread group after find joininfo
    VELAPLATFO-5865
    
    Need check tcb or group info after find joininfo,
    otherwise the wrong value will be returned.
    
    Change-Id: I612504eb5913bfe26662e5eaa0d0082a4fc3b308
    Signed-off-by: zhangyuan21 <zhangyuan21@xiaomi.com>

--- sched/pthread/pthread_findjoininfo.c
+++ sched/pthread/pthread_findjoininfo.c
@@ -141,7 +141,8 @@ FAR struct join_s *pthread_findjoininfo(FAR struct task_group_s *group,
       FAR struct tcb_s *tcb = nxsched_get_tcb((pthread_t)pid);
 
       if (tcb != NULL && (tcb->flags & TCB_FLAG_DETACHED) == 0 &&
-          (tcb->flags & TCB_FLAG_TTYPE_MASK) == TCB_FLAG_TTYPE_PTHREAD)
+          (tcb->flags & TCB_FLAG_TTYPE_MASK) == TCB_FLAG_TTYPE_PTHREAD &&
+          tcb->group == group)
         {
           pjoin = pthread_createjoininfo((FAR struct pthread_tcb_s *)tcb);
         }
