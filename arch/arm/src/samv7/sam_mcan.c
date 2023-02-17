apache/nuttx

commit d07792a3438309600f134e97b5373b380b666cff

Initialize global mutext/sem by NXMUTEX_INITIALIZER and SEM_INITIALIZER
Signed-off-by: anjiahao <anjiahao@xiaomi.com>
Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

arch/arm/src/samv7/sam_mcan.c
@@ -1077,8 +1077,18 @@ static struct sam_config_s g_mcan0const =
 
 /* MCAN0 variable driver state */
 
-static struct sam_mcan_s g_mcan0priv;
-static struct can_dev_s g_mcan0dev;
+static struct sam_mcan_s g_mcan0priv =
+{
+  .config           = &g_mcan1const,
+  .lock             = NXMUTEX_INITIALIZER,
+  .txfsem           = SEM_INITIALIZER(CONFIG_SAMV7_MCAN0_TXFIFOQ_SIZE),
+};
+
+static struct can_dev_s g_mcan0dev =
+{
+  .cd_ops           = &g_mcanops,
+  .cd_priv          = &g_mcan0priv,
+};
 
 #endif /* CONFIG_SAMV7_MCAN0 */
 
@@ -1164,8 +1174,18 @@ static struct sam_config_s g_mcan1const =
 
 /* MCAN0 variable driver state */
 
-static struct sam_mcan_s g_mcan1priv;
-static struct can_dev_s g_mcan1dev;
+static struct sam_mcan_s g_mcan1priv =
+{
+  .config           = &g_mcan1const,
+  .lock             = NXMUTEX_INITIALIZER,
+  .txfsem           = SEM_INITIALIZER(CONFIG_SAMV7_MCAN0_TXFIFOQ_SIZE),
+};
+
+static struct can_dev_s g_mcan1dev =
+{
+  .cd_ops           = &g_mcanops,
+  .cd_priv          = &g_mcan1priv,
+};
 
 #endif /* CONFIG_SAMV7_MCAN1 */
 
@@ -2354,14 +2374,9 @@ static void mcan_reset(struct can_dev_s *dev)
   mcan_putreg(priv, SAM_MCAN_IE_OFFSET, 0);
   mcan_putreg(priv, SAM_MCAN_TXBTIE_OFFSET, 0);
 
-  /* Make sure that all buffers are released.
-   *
-   * REVISIT: What if a thread is waiting for a buffer?  The following
-   * will not wake up any waiting threads.
-   */
+  /* Make sure that all buffers are released. */
 
-  nxsem_destroy(&priv->txfsem);
-  nxsem_init(&priv->txfsem, 0, config->ntxfifoq);
+  nxsem_reset(&priv->txfsem, config->ntxfifoq);
 
   /* Disable peripheral clocking to the MCAN controller */
 
@@ -4213,9 +4228,7 @@ static int mcan_hw_initialize(struct sam_mcan_s *priv)
 
 struct can_dev_s *sam_mcan_initialize(int port)
 {
-  struct can_dev_s *dev;
   struct sam_mcan_s *priv;
-  const struct sam_config_s *config;
   uint32_t regval;
 
   caninfo("MCAN%d\n", port);
@@ -4239,9 +4252,7 @@ struct can_dev_s *sam_mcan_initialize(int port)
     {
       /* Select the MCAN0 device structure */
 
-      dev    = &g_mcan0dev;
-      priv   = &g_mcan0priv;
-      config = &g_mcan0const;
+      priv = &g_mcan0priv;
 
       /* Configure MCAN0 Message RAM Base Address */
 
@@ -4258,9 +4269,7 @@ struct can_dev_s *sam_mcan_initialize(int port)
     {
       /* Select the MCAN1 device structure */
 
-      dev    = &g_mcan1dev;
-      priv   = &g_mcan1priv;
-      config = &g_mcan1const;
+      priv = &g_mcan1priv;
 
       /* Configure MCAN1 Message RAM Base Address */
 
@@ -4283,10 +4292,7 @@ struct can_dev_s *sam_mcan_initialize(int port)
     {
       /* Yes, then perform one time data initialization */
 
-      memset(priv, 0, sizeof(struct sam_mcan_s));
-      priv->config = config;
-
-      /* Get the revision of the chip (A or B ) */
+      /* Get the revision of the chip (A or B) */
 
       regval = getreg32(SAM_CHIPID_CIDR);
       priv->rev = regval & CHIPID_CIDR_VERSION_MASK;
@@ -4299,29 +4305,21 @@ struct can_dev_s *sam_mcan_initialize(int port)
         {
           /* Revision A */
 
-          priv->btp    = config->btp;
-          priv->fbtp   = config->fbtp;
+          priv->btp  = priv->config->btp;
+          priv->fbtp = priv->config->fbtp;
         }
       else if (priv->rev == 1)
         {
           /* Revision B */
 
-          priv->btp    = config->nbtp;
-          priv->fbtp   = config->dbtp;
+          priv->btp  = priv->config->nbtp;
+          priv->fbtp = priv->config->dbtp;
         }
       else
         {
           canerr("ERROR: Incorrect chip revision: %d\n", priv->rev);
           return NULL;
         }
-
-      /* Initialize mutex & semaphores */
-
-      nxmutex_init(&priv->lock);
-      nxsem_init(&priv->txfsem, 0, config->ntxfifoq);
-
-      dev->cd_ops  = &g_mcanops;
-      dev->cd_priv = (void *)priv;
 
       /* And put the hardware in the initial state */
 

commit 191abf22d3f75a75838c1a9c29e01bb08c80b287

samv7: fix build error in sam_mcan.c file
Commit d07792a caused a build error in sam_mcan.c file. This commit fixes
the build. The file now succesfully compiles and CAN works.

Signed-off-by: Michal Lenc <michallenc@seznam.cz>

arch/arm/src/samv7/sam_mcan.c
@@ -4220,6 +4220,7 @@ static int mcan_hw_initialize(struct sam_mcan_s *priv)
 struct can_dev_s *sam_mcan_initialize(int port)
 {
   struct sam_mcan_s *priv;
+  struct can_dev_s *dev;
   uint32_t regval;
 
   caninfo("MCAN%d\n", port);
@@ -4243,13 +4244,14 @@ struct can_dev_s *sam_mcan_initialize(int port)
     {
       /* Select the MCAN0 device structure */
 
-      priv = &g_mcan0priv;
+      dev = &g_mcan0dev;
+      priv = dev->cd_priv;
 
       /* Configure MCAN0 Message RAM Base Address */
 
       regval  = getreg32(SAM_MATRIX_CAN0);
       regval &= MATRIX_CAN0_RESERVED;
-      regval |= (uint32_t)config->msgram.stdfilters &
+      regval |= (uint32_t)priv->config->msgram.stdfilters &
                 MATRIX_CAN0_CAN0DMABA_MASK;
       putreg32(regval, SAM_MATRIX_CAN0);
     }
@@ -4260,13 +4262,14 @@ struct can_dev_s *sam_mcan_initialize(int port)
     {
       /* Select the MCAN1 device structure */
 
-      priv = &g_mcan1priv;
+      dev = &g_mcan1dev;
+      priv = dev->cd_priv;
 
       /* Configure MCAN1 Message RAM Base Address */
 
       regval  = getreg32(SAM_MATRIX_CCFG_SYSIO);
       regval &= ~MATRIX_CCFG_CAN1DMABA_MASK;
-      regval |= (uint32_t)config->msgram.stdfilters &
+      regval |= (uint32_t)priv->config->msgram.stdfilters &
                 MATRIX_CCFG_CAN1DMABA_MASK;
       putreg32(regval, SAM_MATRIX_CCFG_SYSIO);
     }