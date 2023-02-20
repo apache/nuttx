3b7352b6538ea77db53c2f9f2253de4bdf294363
chao an <anchao@xiaomi.com>
2023-02-20 14:44:27 +0800

    net/devif: fix null pointer reference found out by coverity
    Signed-off-by: chao an <anchao@xiaomi.com>

--- net/devif/devif_filesend.c
+++ net/devif/devif_filesend.c
@@ -150,7 +150,11 @@ int devif_file_send(FAR struct net_driver_s *dev, FAR struct file *file,
 errout:
   if (ret < 0)
     {
-      netdev_iob_release(dev);
+      if (dev != NULL)
+        {
+          netdev_iob_release(dev);
+        }
+
       nerr("ERROR: devif_iob_send error: %d\n", ret);
     }
 
