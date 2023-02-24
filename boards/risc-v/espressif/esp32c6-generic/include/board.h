c149e52791bdf5f69cfeed0f088037e6172d514f
Gustavo Henrique Nihei <gustavo.nihei@espressif.com>
2023-02-24 11:36:14 -0300

    risc-v/espressif: Add support for ESP32-C6
    Signed-off-by: Gustavo Henrique Nihei <gustavo.nihei@espressif.com>

---
+++ boards/risc-v/espressif/esp32c6-generic/include/board.h
@@ -0,0 +1,25 @@
+/****************************************************************************
+ * boards/risc-v/espressif/esp32c6-generic/include/board.h
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+#ifndef __BOARDS_RISCV_ESPRESSIF_ESP32C6_GENERIC_INCLUDE_BOARD_H
+#define __BOARDS_RISCV_ESPRESSIF_ESP32C6_GENERIC_INCLUDE_BOARD_H
+
+#endif /* __BOARDS_RISCV_ESPRESSIF_ESP32C6_GENERIC_INCLUDE_BOARD_H */
+
