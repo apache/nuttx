216683942808cc6ffdc98624586f54417cb15352
Xiang Xiao <xiaoxiang@xiaomi.com>
2023-02-25 18:57:19 +0800

    Fix error: '%s' directive output may be truncated
    writing up to 31 bytes into a region of size 27 [-Werror=format-truncation=]
    
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- drivers/ioexpander/gpio.c
+++ drivers/ioexpander/gpio.c
@@ -593,7 +593,7 @@ int gpio_pin_register(FAR struct gpio_dev_s *dev, int minor)
 int gpio_pin_register_byname(FAR struct gpio_dev_s *dev,
                              FAR const char *pinname)
 {
-  char devname[32];
+  char devname[64];
   int ret;
 
   DEBUGASSERT(dev != NULL && dev->gp_ops != NULL && pinname != NULL);
@@ -691,7 +691,7 @@ int gpio_pin_unregister(FAR struct gpio_dev_s *dev, int minor)
 int gpio_pin_unregister_byname(FAR struct gpio_dev_s *dev,
                                FAR const char *pinname)
 {
-  char devname[32];
+  char devname[64];
 
   snprintf(devname, sizeof(devname), "/dev/%s", pinname);
 
