93097be705b4b807e9f9bb693d74cbcf0a97bb37
Michal Lenc <michallenc@seznam.cz>
2023-02-27 16:27:44 +0100

    samv7: add support for PWM fault protection
    This commit adds configurable fault protection to SAMv7 PWM driver.
    The fault input can be used from peripherals as ADC or GPIO inputs.
    Inputs from GPIO have configurable polarity (high or low). The PWM output
    is automatically set to zero if fault input is active and restored
    if fault input is not actived.
    
    Signed-off-by: Michal Lenc <michallenc@seznam.cz>

--- arch/arm/src/samv7/sam_pwm.h
+++ arch/arm/src/samv7/sam_pwm.h
@@ -88,6 +88,154 @@
 
 #define PWM1_NCHANNELS (PWM1_CH0 + PWM1_CH1 + PWM1_CH2 + PWM1_CH3)
 
+/* Fault protection */
+
+#ifdef CONFIG_SAMV7_PWM0_PA9
+#define PWM0_PA9 1
+#ifdef CONFIG_SAMV7_PWM0_PA9_POL
+#define PWM0_PA9_POL 0
+#else
+#define PWM0_PA9_POL 1
+#endif
+#else
+#define PWM0_PA9 0
+#define PWM0_PA9_POL 0
+#endif
+
+#ifdef CONFIG_SAMV7_PWM0_PD8
+#define PWM0_PD8 (1 << 1)
+#ifdef CONFIG_SAMV7_PWM0_PD8_POL
+#define PWM0_PD8_POL 0
+#else
+#define PWM0_PD8_POL (1 << 1)
+#endif
+#else
+#define PWM0_PD8 0
+#define PWM0_PD8_POL 0
+#endif
+
+#ifdef CONFIG_SAMV7_PWM0_PD9
+#define PWM0_PD9 (1 << 2)
+#ifdef CONFIG_SAMV7_PWM0_PD9_POL
+#define PWM0_PD9_POL 0
+#else
+#define PWM0_PD9_POL (1 << 2)
+#endif
+#else
+#define PWM0_PD9 0
+#define PWM0_PD9_POL 0
+#endif
+
+#ifdef CONFIG_SAMV7_PWM0_PMC
+#define PWM0_PMC (1 << 3)
+#else
+#define PWM0_PMC 0
+#endif
+
+#ifdef CONFIG_SAMV7_PWM0_AFEC0
+#define PWM0_AFEC0 (1 << 4)
+#else
+#define PWM0_AFEC0 0
+#endif
+
+#ifdef CONFIG_SAMV7_PWM0_AFEC1
+#define PWM0_AFEC1 (1 << 5)
+#else
+#define PWM0_AFEC1 0
+#endif
+
+#ifdef CONFIG_SAMV7_PWM0_ACC
+#define PWM0_ACC (1 << 6)
+#else
+#define PWM0_ACC 0
+#endif
+
+#ifdef CONFIG_SAMV7_PWM0_TIM0
+#define PWM0_TIM0 (1 << 7)
+#else
+#define PWM0_TIM0 0
+#endif
+
+#define PWM0_FAULTS (PWM0_PA9 + PWM0_PD8 + PWM0_PD9 + PWM0_PMC + \
+                     PWM0_AFEC0 + PWM0_AFEC1 + PWM0_ACC + PWM0_TIM0)
+
+#define PWM0_POL    (PWM0_PA9_POL + PWM0_PD8_POL + PWM0_PD9_POL + \
+                     PWM0_PMC + PWM0_AFEC0 + PWM0_AFEC1 + PWM0_ACC + \
+                     PWM0_TIM0)
+
+#ifdef CONFIG_SAMV7_PWM1_PA21
+#define PWM1_PA21 1
+#ifdef CONFIG_SAMV7_PWM1_PA21_POL
+#define PWM1_PA21_POL 0
+#else
+#define PWM1_PA21_POL 1
+#endif
+#else
+#define PWM1_PA21 0
+#define PWM1_PA21_POL 0
+#endif
+
+#ifdef CONFIG_SAMV7_PWM1_PA26
+#define PWM1_PA26 (1 << 1)
+#ifdef CONFIG_SAMV7_PWM1_PA26_POL
+#define PWM1_PA26_POL 0
+#else
+#define PWM1_PA26_POL (1 << 1)
+#endif
+#else
+#define PWM1_PA26 0
+#define PWM1_PA26_POL 0
+#endif
+
+#ifdef CONFIG_SAMV7_PWM1_PA28
+#define PWM1_PA28 (1 << 2)
+#ifdef CONFIG_SAMV7_PWM1_PA28_POL
+#define PWM1_PA28_POL 0
+#else
+#define PWM1_PA28_POL (1 << 2)
+#endif
+#else
+#define PWM1_PA28 0
+#define PWM1_PA28_POL 0
+#endif
+
+#ifdef CONFIG_SAMV7_PWM1_PMC
+#define PWM1_PMC (1 << 3)
+#else
+#define PWM1_PMC 0
+#endif
+
+#ifdef CONFIG_SAMV7_PWM1_AFEC0
+#define PWM1_AFEC0 (1 << 4)
+#else
+#define PWM1_AFEC0 0
+#endif
+
+#ifdef CONFIG_SAMV7_PWM1_AFEC1
+#define PWM1_AFEC1 (1 << 5)
+#else
+#define PWM1_AFEC1 0
+#endif
+
+#ifdef CONFIG_SAMV7_PWM1_ACC
+#define PWM1_ACC (1 << 6)
+#else
+#define PWM1_ACC 0
+#endif
+
+#ifdef CONFIG_SAMV7_PWM1_TIM0
+#define PWM1_TIM0 (1 << 7)
+#else
+#define PWM1_TIM0 0
+#endif
+
+#define PWM1_FAULTS (PWM1_PA21 + PWM1_PA26 + PWM1_PA28 + PWM1_PMC + \
+                     PWM1_AFEC0 + PWM1_AFEC1 + PWM1_ACC + PWM1_TIM0)
+
+#define PWM1_POL    (PWM1_PA21_POL + PWM1_PA26_POL + PWM1_PA28_POL + \
+                     PWM1_PMC + PWM1_AFEC0 + PWM1_AFEC1 + PWM1_ACC + \
+                     PWM1_TIM0)
+
 /****************************************************************************
  * Public Function Prototypes
  ****************************************************************************/
