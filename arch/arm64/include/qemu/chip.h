0fd1de1ceaff0af1495ca3a9370dc102d146afc1
qinwei1 <qinwei1@xiaomi.com>
2023-02-28 21:31:23 +0800

    arm64: ARMv8-r(Cortex-R82) support(mpid fix)
    Summary
      Different ARM64 Core will use different Affn define, the mpidr_el1
    value is not CPU number, So we need to change CPU number to mpid
    and vice versa, the patch change the mpid define into platform
    
    Signed-off-by: qinwei1 <qinwei1@xiaomi.com>

--- arch/arm64/include/qemu/chip.h
+++ arch/arm64/include/qemu/chip.h
@@ -44,7 +44,7 @@
 
 #define CONFIG_GICD_BASE          0x8000000
 #define CONFIG_GICR_BASE          0x80a0000
-
+#define CONFIG_GICR_OFFSET        0x20000
 #else
 
 #error CONFIG_ARM_GIC_VERSION should be 2, 3 or 4
