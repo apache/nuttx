f539773ee7b9fdeb72585e3725c016fac619d296
raiden00pl <raiden00@railab.me>
2023-03-01 11:36:44 +0100

    boards: initial support for nrf5340-dk

---
+++ boards/arm/nrf53/nrf5340-dk/README.txt
@@ -0,0 +1,23 @@
+nRF5340-DK
+==========
+
+README for NuttX port to NRF5340 DK (PCA10095) boards.
+
+Tool Issues
+===========
+
+  OpenOCD
+  -------------
+  There is no support official support for Nordic Cortex M33 chips (nRF9160 or nRF5340).
+
+  Segger J-Link
+  -------------
+  To start the GDB servers for the application core and the network core, use these commands:
+
+    JLinkGDBServer -device nRF5340_xxAA_APP -autoconnect 1 -if SWD -speed 4000 -port 2331 -swoport 2332 -telnetport 2333
+    JLinkGDBServer -device nRF5340_xxAA_NET -autoconnect 1 -if SWD -speed 4000 -port 2334 -swoport 2335 -telnetport 2336
+
+  Then you can connect GDB to targets:
+
+    (gdb_app) target remote localhost:2331
+    (gdb_net) target remote localhost:2334
---
+++ boards/arm/nrf53/nrf5340-dk/src/nrf53_userleds.c
@@ -0,0 +1,148 @@
+/****************************************************************************
+ * boards/arm/nrf53/nrf5340-dk/src/nrf53_userleds.c
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <debug.h>
+
+#include <arch/board/board.h>
+
+#include "chip.h"
+#include "arm_internal.h"
+#include "nrf5340-dk.h"
+
+#ifndef CONFIG_ARCH_LEDS
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+/* LED definitions **********************************************************/
+
+/* If CONFIG_ARCH_LEDS is not defined, then the LEDs are completely under
+ * control of the application.  The following interfaces are then available
+ * for application control of the LEDs:
+ *
+ *  uint32_t board_userled_initialize(void);
+ *  void board_userled(int led, bool ledon);
+ *  void board_userled_all(uint32_t ledset);
+ */
+
+#define LED_ON 0
+#define LED_OFF 1
+
+/* This array maps an LED number to GPIO pin configuration */
+
+static const uint32_t g_ledcfg[BOARD_NLEDS] =
+{
+#if 0 < BOARD_NLEDS
+  GPIO_LED1,
+#endif
+#if 1 < BOARD_NLEDS
+  GPIO_LED2,
+#endif
+#if 2 < BOARD_NLEDS
+  GPIO_LED3,
+#endif
+#if 3 < BOARD_NLEDS
+  GPIO_LED4,
+#endif
+};
+
+/****************************************************************************
+ * Private Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: led_dumppins
+ ****************************************************************************/
+
+#ifdef LED_VERBOSE
+static void led_dumppins(const char *msg)
+{
+  nrf53_pin_dump(PINCONFIG_LED, msg);
+  nrf53_gpio_dump(GPIO_LED, msg);
+}
+#else
+#  define led_dumppins(m)
+#endif
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: board_userled_initialize
+ ****************************************************************************/
+
+uint32_t board_userled_initialize(void)
+{
+  int i;
+
+  /* Configure LED pin as a GPIO outputs */
+
+  led_dumppins("board_userled_initialize() Entry)");
+
+  /* Configure GPIO as an outputs */
+
+  for (i = 0; i < BOARD_NLEDS; i++)
+    {
+      nrf53_gpio_config(g_ledcfg[i]);
+    }
+
+  led_dumppins("board_userled_initialize() Exit");
+  return BOARD_NLEDS;
+}
+
+/****************************************************************************
+ * Name: board_userled
+ ****************************************************************************/
+
+void board_userled(int led, bool ledon)
+{
+  if ((unsigned)led < BOARD_NLEDS)
+    {
+      nrf53_gpio_write(g_ledcfg[led], ledon ? LED_ON : LED_OFF);
+    }
+}
+
+/****************************************************************************
+ * Name: board_userled_all
+ ****************************************************************************/
+
+void board_userled_all(uint32_t ledset)
+{
+  int i;
+
+  /* Configure LED1-8 GPIOs for output */
+
+  for (i = 0; i < BOARD_NLEDS; i++)
+    {
+      nrf53_gpio_write(g_ledcfg[i], (ledset & (1 << i)) ? LED_ON : LED_OFF);
+    }
+}
+
+#endif /* !CONFIG_ARCH_LEDS */
