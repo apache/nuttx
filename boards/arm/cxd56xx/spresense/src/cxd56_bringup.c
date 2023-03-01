aaae8b3f9be6ab8b7fdcb6cb4f414a9d2cea2710
SPRESENSE <41312067+SPRESENSE@users.noreply.github.com>
2023-03-01 14:30:28 +0900

    boards: spresense: Add eMMC initialization into bringup function
    Add eMMC initialization into bringup function.

--- boards/arm/cxd56xx/spresense/src/cxd56_bringup.c
+++ boards/arm/cxd56xx/spresense/src/cxd56_bringup.c
@@ -440,6 +440,16 @@ int cxd56_bringup(void)
   board_automount_initialize();
 #endif
 
+#if defined(CONFIG_CXD56_EMMC) && !defined(CONFIG_CXD56_EMMC_LATE_INITIALIZE)
+  /* Mount the eMMC block driver */
+
+  ret = board_emmc_initialize();
+  if (ret < 0)
+    {
+      _err("ERROR: Failed to initialize eMMC: %d\n", ret);
+    }
+#endif
+
 #ifdef CONFIG_CPUFREQ_RELEASE_LOCK
   /* Enable dynamic clock control and CPU clock down for power saving */
 
