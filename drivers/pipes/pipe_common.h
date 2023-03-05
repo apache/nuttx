4d6a8663fa0932a8c1b40639464f68af670f0808
Tiago Medicci Serrano <tiago.medicci@gmail.com>
2023-03-05 16:45:51 -0300

    drivers/pipe: make pipe and named pipe (mkfifo) POSIX-compliant
    According to https://pubs.opengroup.org/onlinepubs/9699919799/functions/open.html
    
    "
     When opening a FIFO with O_RDONLY or O_WRONLY set:
      * If O_NONBLOCK is set, an open() for reading-only shall return
        without delay. An open() for writing-only shall return an error
        if no process currently has the file open for reading.
    
      * If O_NONBLOCK is clear, an open() for reading-only shall block
        the calling thread until a thread opens the file for writing.
        An open() for writing-only shall block the calling thread until
        a thread opens the file for reading.
    "
    
    This commit has an equivalent on nuttx-apps: EXAMPLES_PIPE app
    was updated to be able to check pipes and named pipes behavior.

--- drivers/pipes/pipe_common.h
+++ drivers/pipes/pipe_common.h
@@ -115,8 +115,10 @@ typedef uint8_t pipe_ndx_t;   /*  8-bit index */
 struct pipe_dev_s
 {
   mutex_t    d_bflock;      /* Used to serialize access to d_buffer and indices */
-  sem_t      d_rdsem;       /* Empty buffer - Reader waits for data write */
-  sem_t      d_wrsem;       /* Full buffer - Writer waits for data read */
+  sem_t      d_rdsem;       /* Empty buffer - Reader waits for data write AND
+                             * block O_RDONLY open until there is at least one writer */
+  sem_t      d_wrsem;       /* Full buffer - Writer waits for data read AND
+                             * block O_WRONLY open until there is at least one reader */
   pipe_ndx_t d_wrndx;       /* Index in d_buffer to save next byte written */
   pipe_ndx_t d_rdndx;       /* Index in d_buffer to return the next byte read */
   pipe_ndx_t d_bufsize;     /* allocated size of d_buffer in bytes */
