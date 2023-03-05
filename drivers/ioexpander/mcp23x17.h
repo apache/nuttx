a3e50d5782dc2cc88a2a566bc085a898a60a1ee3
jturnsek <jernej.turnsek@gmail.com>
2023-03-05 18:38:24 +0100

    Bug fixes and improved interrupt support for mcp23x17 driver

--- drivers/ioexpander/mcp23x17.h
+++ drivers/ioexpander/mcp23x17.h
@@ -152,6 +152,10 @@ struct mcp23x17_dev_s
   struct ioexpander_dev_s dev;          /* Nested structure to allow casting
                                          * as public gpio expander.
                                          */
+#ifdef CONFIG_MCP23X17_MULTIPLE
+  FAR struct mcp23x17_dev_s *flink;     /* Supports a singly linked list of drivers */
+#endif
+
   FAR struct mcp23x17_config_s *config; /* Board configuration data */
   FAR struct i2c_master_s *i2c;         /* Saved I2C driver instance */
   mutex_t lock;                         /* Mutual exclusion */
