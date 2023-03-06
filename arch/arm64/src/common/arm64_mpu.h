da6244016dac25873f0d0e7fada8d3c78c88baed
zhangyuan21 <zhangyuan21@xiaomi.com>
2023-03-06 18:53:04 +0800

    arch/arm64: add cache enable and disable function
    Signed-off-by: zhangyuan21 <zhangyuan21@xiaomi.com>

--- arch/arm64/src/common/arm64_mpu.h
+++ arch/arm64/src/common/arm64_mpu.h
@@ -202,13 +202,23 @@
     .mair_idx = MPU_MAIR_INDEX_DEVICE,                    \
   }
 
-#define REGION_RAM_ATTR                                   \
-  {                                                       \
-    /* AP, XN, SH */                                      \
-    .rbar = NOT_EXEC | P_RW_U_NA_MSK | NON_SHAREABLE_MSK, \
-    /* Cache-ability */                                   \
-    .mair_idx = MPU_MAIR_INDEX_SRAM,                      \
-  }
+#ifdef CONFIG_SMP
+#  define REGION_RAM_ATTR                                        \
+    {                                                            \
+      /* AP, XN, SH */                                           \
+      .rbar = (NOT_EXEC | P_RW_U_NA_MSK | INNER_SHAREABLE_MSK) , \
+      /* Cache-ability */                                        \
+      .mair_idx = MPU_MAIR_INDEX_SRAM,                           \
+    }
+#else
+#  define REGION_RAM_ATTR                                   \
+    {                                                       \
+      /* AP, XN, SH */                                      \
+      .rbar = NOT_EXEC | P_RW_U_NA_MSK | NON_SHAREABLE_MSK, \
+      /* Cache-ability */                                   \
+      .mair_idx = MPU_MAIR_INDEX_SRAM,                      \
+    }
+#endif
 
 #define REGION_RAM_TEXT_ATTR                   \
   {                                            \
@@ -232,11 +242,11 @@ struct arm64_mpu_region_attr
 {
   /* Attributes belonging to PRBAR */
 
-  uint8_t rbar : 5;
+  uint8_t rbar;
 
   /* MAIR index for attribute indirection */
 
-  uint8_t mair_idx : 3;
+  uint8_t mair_idx;
 };
 
 /* Region definition data structure */
