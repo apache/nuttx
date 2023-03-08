apache/nuttx

commit 62661600b2bbc656a57881f26e3b8f8854d79b33

treewide: add DOWNLOAD variable as unification of curl call
This is a followup to the commit
03b164f59ce40a3f5677b0588af2aee8d9697bf6.

arch/risc-v/src/esp32c3/Bootloader.mk
@@ -168,8 +168,7 @@ BOOTLOADER_URL     = https://github.com/espressif/esp-nuttx-bootloader/releases/
 ifeq ($(CONFIG_ESP32C3_APP_FORMAT_MCUBOOT),y)
 
 bootloader:
-	$(Q) echo "Downloading Bootloader binaries"
-	$(Q) curl -L $(BOOTLOADER_URL)/mcuboot-esp32c3.bin -o $(TOPDIR)/mcuboot-esp32c3.bin
+	$(call DOWNLOAD,$(BOOTLOADER_URL),mcuboot-esp32c3.bin,$(TOPDIR)/mcuboot-esp32c3.bin)
 
 clean_bootloader:
 	$(call DELFILE,$(TOPDIR)/mcuboot-esp32c3.bin)
@@ -177,9 +176,8 @@ clean_bootloader:
 else ifeq ($(CONFIG_ESP32C3_APP_FORMAT_LEGACY),y)
 
 bootloader:
-	$(Q) echo "Downloading Bootloader binaries"
-	$(Q) curl -L $(BOOTLOADER_URL)/bootloader-esp32c3.bin -o $(TOPDIR)/bootloader-esp32c3.bin
-	$(Q) curl -L $(BOOTLOADER_URL)/partition-table-esp32c3.bin -o $(TOPDIR)/partition-table-esp32c3.bin
+	$(call DOWNLOAD,$(BOOTLOADER_URL),mcuboot-esp32c3.bin,$(TOPDIR)/mcuboot-esp32c3.bin)
+	$(call DOWNLOAD,$(BOOTLOADER_URL),partition-table-esp32c3.bin,$(TOPDIR)/partition-table-esp32c3.bin)
 
 clean_bootloader:
 	$(call DELFILE,$(TOPDIR)/bootloader-esp32c3.bin)

arch/xtensa/src/esp32/Bootloader.mk
@@ -177,8 +177,7 @@ BOOTLOADER_URL     = https://github.com/espressif/esp-nuttx-bootloader/releases/
 ifeq ($(CONFIG_ESP32_APP_FORMAT_MCUBOOT),y)
 
 bootloader:
-	$(Q) echo "Downloading Bootloader binaries"
-	$(Q) curl -L $(BOOTLOADER_URL)/mcuboot-esp32.bin -o $(TOPDIR)/mcuboot-esp32.bin
+	$(call DOWNLOAD,$(BOOTLOADER_URL),mcuboot-esp32.bin,$(TOPDIR)/mcuboot-esp32.bin)
 
 clean_bootloader:
 	$(call DELFILE,$(TOPDIR)/mcuboot-esp32.bin)
@@ -186,9 +185,8 @@ clean_bootloader:
 else ifeq ($(CONFIG_ESP32_APP_FORMAT_LEGACY),y)
 
 bootloader:
-	$(Q) echo "Downloading Bootloader binaries"
-	$(Q) curl -L $(BOOTLOADER_URL)/bootloader-esp32.bin -o $(TOPDIR)/bootloader-esp32.bin
-	$(Q) curl -L $(BOOTLOADER_URL)/partition-table-esp32.bin -o $(TOPDIR)/partition-table-esp32.bin
+	$(call DOWNLOAD,$(BOOTLOADER_URL),mcuboot-esp32.bin,$(TOPDIR)/mcuboot-esp32.bin)
+	$(call DOWNLOAD,$(BOOTLOADER_URL),partition-table-esp32.bin,$(TOPDIR)/partition-table-esp32.bin)
 
 clean_bootloader:
 	$(call DELFILE,$(TOPDIR)/bootloader-esp32.bin)

arch/xtensa/src/esp32s2/Bootloader.mk
@@ -166,8 +166,7 @@ BOOTLOADER_URL     = https://github.com/espressif/esp-nuttx-bootloader/releases/
 ifeq ($(CONFIG_ESP32S2_APP_FORMAT_MCUBOOT),y)
 
 bootloader:
-	$(Q) echo "Downloading Bootloader binaries"
-	$(Q) curl -L $(BOOTLOADER_URL)/mcuboot-esp32s2.bin -o $(TOPDIR)/mcuboot-esp32s2.bin
+	$(call DOWNLOAD,$(BOOTLOADER_URL),mcuboot-esp32s2.bin,$(TOPDIR)/mcuboot-esp32s2.bin)
 
 clean_bootloader:
 	$(call DELFILE,$(TOPDIR)/mcuboot-esp32s2.bin)
@@ -175,9 +174,8 @@ clean_bootloader:
 else ifeq ($(CONFIG_ESP32S2_APP_FORMAT_LEGACY),y)
 
 bootloader:
-	$(Q) echo "Downloading Bootloader binaries"
-	$(Q) curl -L $(BOOTLOADER_URL)/bootloader-esp32s2.bin -o $(TOPDIR)/bootloader-esp32s2.bin
-	$(Q) curl -L $(BOOTLOADER_URL)/partition-table-esp32s2.bin -o $(TOPDIR)/partition-table-esp32s2.bin
+	$(call DOWNLOAD,$(BOOTLOADER_URL),mcuboot-esp32s2.bin,$(TOPDIR)/mcuboot-esp32s2.bin)
+	$(call DOWNLOAD,$(BOOTLOADER_URL),partition-table-esp32s2.bin,$(TOPDIR)/partition-table-esp32s2.bin)
 
 clean_bootloader:
 	$(call DELFILE,$(TOPDIR)/bootloader-esp32s2.bin)

arch/xtensa/src/esp32s3/Bootloader.mk
@@ -110,8 +110,7 @@ BOOTLOADER_URL     = https://github.com/espressif/esp-nuttx-bootloader/releases/
 ifeq ($(CONFIG_ESP32S3_APP_FORMAT_MCUBOOT),y)
 
 bootloader:
-	$(Q) echo "Downloading Bootloader binaries"
-	$(Q) curl -L $(BOOTLOADER_URL)/mcuboot-esp32s3.bin -o $(TOPDIR)/mcuboot-esp32s3.bin
+	$(call DOWNLOAD,$(BOOTLOADER_URL),mcuboot-esp32s3.bin,$(TOPDIR)/mcuboot-esp32s3.bin)
 
 clean_bootloader:
 	$(call DELFILE,$(TOPDIR)/mcuboot-esp32s3.bin)
@@ -119,9 +118,8 @@ clean_bootloader:
 else ifeq ($(CONFIG_ESP32S3_APP_FORMAT_LEGACY),y)
 
 bootloader:
-	$(Q) echo "Downloading Bootloader binaries"
-	$(Q) curl -L $(BOOTLOADER_URL)/bootloader-esp32s3.bin -o $(TOPDIR)/bootloader-esp32s3.bin
-	$(Q) curl -L $(BOOTLOADER_URL)/partition-table-esp32s3.bin -o $(TOPDIR)/partition-table-esp32s3.bin
+	$(call DOWNLOAD,$(BOOTLOADER_URL),mcuboot-esp32s3.bin,$(TOPDIR)/mcuboot-esp32s3.bin)
+	$(call DOWNLOAD,$(BOOTLOADER_URL),partition-table-esp32s3.bin,$(TOPDIR)/partition-table-esp32s3.bin)
 
 clean_bootloader:
 	$(call DELFILE,$(TOPDIR)/bootloader-esp32s3.bin)

arch/arm/src/nrf52/Make.defs
@@ -120,8 +120,7 @@ NRFXLIB_TGZ     := $(NRFXLIB_REF).tar.gz
 NRFXLIB_URL     := https://github.com/nrfconnect/sdk-nrfxlib/archive
 
 $(NRFXLIB_TGZ):
-	$(Q) echo "Downloading: NRFXLIB"
-	$(Q) curl -L $(NRFXLIB_URL)/$(NRFXLIB_TGZ) -o chip/$(NRFXLIB_TGZ)
+	$(call DOWNLOAD,$(NRFXLIB_URL),$(NRFXLIB_TGZ),chip/$(NRFXLIB_TGZ))
 
 chip/$(NRFXLIB_UNPACK): $(NRFXLIB_TGZ)
 	$(Q) echo "Unpacking: NRXFLIB"

arch/arm/src/nrf53/Make.defs
@@ -51,8 +51,7 @@ NRFXLIB_TGZ     := $(NRFXLIB_REF).tar.gz
 NRFXLIB_URL     := https://github.com/nrfconnect/sdk-nrfxlib/archive
 
 $(NRFXLIB_TGZ):
-	$(Q) echo "Downloading: NRFXLIB"
-	$(Q) curl -L $(NRFXLIB_URL)/$(NRFXLIB_TGZ) -o chip/$(NRFXLIB_TGZ)
+	$(call DOWNLOAD,$(NRFXLIB_URL),$(NRFXLIB_TGZ),chip/$(NRFXLIB_TGZ))
 
 chip/$(NRFXLIB_UNPACK): $(NRFXLIB_TGZ)
 	$(Q) echo "Unpacking: NRXFLIB"

arch/risc-v/src/bl602/Make.defs
@@ -71,8 +71,7 @@ WIRELESS_DRV_ZIP     = v$(WIRELESS_DRV_VERSION).zip
 WIRELESS_DRV_URL     = https://github.com/bouffalolab/bl_blob/archive/refs/heads
 
 $(WIRELESS_DRV_ZIP):
-	$(Q) echo "Downloading: BL602 Wireless Drivers"
-	$(Q) curl -L $(WIRELESS_DRV_URL)/$(WIRELESS_DRV_ZIP) -o chip/$(WIRELESS_DRV_ZIP)
+	$(call DOWNLOAD,$(WIRELESS_DRV_URL),$(WIRELESS_DRV_ZIP),chip/$(WIRELESS_DRV_ZIP))
 
 chip/$(WIRELESS_DRV_UNPACK): $(WIRELESS_DRV_ZIP)
 	$(Q) echo "Unpacking: BL602 Wireless Drivers"

arch/risc-v/src/esp32c3/Make.defs
@@ -189,8 +189,7 @@ WIRELESS_DRV_ZIP    = $(WIRELESS_DRV_ID).zip
 WIRELESS_DRV_URL    = https://github.com/espressif/esp-wireless-drivers-3rdparty/archive
 
 $(WIRELESS_DRV_ZIP):
-	$(Q) echo "Downloading: ESP Wireless Drivers"
-	$(Q) curl -L $(WIRELESS_DRV_URL)/$(WIRELESS_DRV_ZIP) -o chip/$(WIRELESS_DRV_ZIP)
+	$(call DOWNLOAD,$(WIRELESS_DRV_URL),$(WIRELESS_DRV_ZIP),chip/$(WIRELESS_DRV_ZIP))
 
 chip/$(WIRELESS_DRV_UNPACK): $(WIRELESS_DRV_ZIP)
 	$(Q) echo "Unpacking: ESP Wireless Drivers"

commit abe88c965a742027326dba4122872c5d520ef75e

espressif: Fix download of IDFboot binaries
It was broken after #8759, which erroneously substituted with MCUboot

Signed-off-by: Gustavo Henrique Nihei <gustavo.nihei@espressif.com>

arch/risc-v/src/esp32c3/Bootloader.mk
@@ -176,7 +176,7 @@ clean_bootloader:
 else ifeq ($(CONFIG_ESP32C3_APP_FORMAT_LEGACY),y)
 
 bootloader:
-	$(call DOWNLOAD,$(BOOTLOADER_URL),mcuboot-esp32c3.bin,$(TOPDIR)/mcuboot-esp32c3.bin)
+	$(call DOWNLOAD,$(BOOTLOADER_URL),bootloader-esp32c3.bin,$(TOPDIR)/bootloader-esp32c3.bin)
 	$(call DOWNLOAD,$(BOOTLOADER_URL),partition-table-esp32c3.bin,$(TOPDIR)/partition-table-esp32c3.bin)
 
 clean_bootloader:

arch/xtensa/src/esp32/Bootloader.mk
@@ -185,7 +185,7 @@ clean_bootloader:
 else ifeq ($(CONFIG_ESP32_APP_FORMAT_LEGACY),y)
 
 bootloader:
-	$(call DOWNLOAD,$(BOOTLOADER_URL),mcuboot-esp32.bin,$(TOPDIR)/mcuboot-esp32.bin)
+	$(call DOWNLOAD,$(BOOTLOADER_URL),bootloader-esp32.bin,$(TOPDIR)/bootloader-esp32.bin)
 	$(call DOWNLOAD,$(BOOTLOADER_URL),partition-table-esp32.bin,$(TOPDIR)/partition-table-esp32.bin)
 
 clean_bootloader:

arch/xtensa/src/esp32s2/Bootloader.mk
@@ -174,7 +174,7 @@ clean_bootloader:
 else ifeq ($(CONFIG_ESP32S2_APP_FORMAT_LEGACY),y)
 
 bootloader:
-	$(call DOWNLOAD,$(BOOTLOADER_URL),mcuboot-esp32s2.bin,$(TOPDIR)/mcuboot-esp32s2.bin)
+	$(call DOWNLOAD,$(BOOTLOADER_URL),bootloader-esp32s2.bin,$(TOPDIR)/bootloader-esp32s2.bin)
 	$(call DOWNLOAD,$(BOOTLOADER_URL),partition-table-esp32s2.bin,$(TOPDIR)/partition-table-esp32s2.bin)
 
 clean_bootloader:

arch/xtensa/src/esp32s3/Bootloader.mk
@@ -118,7 +118,7 @@ clean_bootloader:
 else ifeq ($(CONFIG_ESP32S3_APP_FORMAT_LEGACY),y)
 
 bootloader:
-	$(call DOWNLOAD,$(BOOTLOADER_URL),mcuboot-esp32s3.bin,$(TOPDIR)/mcuboot-esp32s3.bin)
+	$(call DOWNLOAD,$(BOOTLOADER_URL),bootloader-esp32s3.bin,$(TOPDIR)/bootloader-esp32s3.bin)
 	$(call DOWNLOAD,$(BOOTLOADER_URL),partition-table-esp32s3.bin,$(TOPDIR)/partition-table-esp32s3.bin)
 
 clean_bootloader: