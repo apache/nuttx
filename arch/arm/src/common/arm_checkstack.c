12bb4f04f10de615b0d5245c20e8cc908be3e0fd
zhangyuan21 <zhangyuan21@xiaomi.com>
2023-03-09 21:03:20 +0800

    arch: fixed error in the calculation of nwords caused an out of bounds
    Signed-off-by: zhangyuan21 <zhangyuan21@xiaomi.com>

--- arch/arm/src/common/arm_checkstack.c
+++ arch/arm/src/common/arm_checkstack.c
@@ -172,7 +172,7 @@ void arm_stack_color(void *stackbase, size_t nbytes)
     }
 
   stkend = STACK_ALIGN_DOWN(stkend);
-  nwords = (stkend - (uintptr_t)stackbase) >> 2;
+  nwords = (stkend - (uintptr_t)stkptr) >> 2;
 
   /* Set the entire stack to the coloration value */
 
