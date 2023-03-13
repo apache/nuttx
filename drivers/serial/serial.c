apache/nuttx

commit 68384e9db42e254b2cf6720bc3380aebdd2b298c

drivers/serial: Echo input in driver layer
Signed-off-by: Huang Qi <huangqi3@xiaomi.com>

drivers/serial/serial.c
@@ -24,6 +24,7 @@
 
 #include <nuttx/config.h>
 
+#include <ctype.h>
 #include <sys/types.h>
 #include <stdint.h>
 #include <stdbool.h>
@@ -37,6 +38,7 @@
 #include <spawn.h>
 
 #include <nuttx/irq.h>
+#include <nuttx/ascii.h>
 #include <nuttx/arch.h>
 #include <nuttx/clock.h>
 #include <nuttx/sched.h>
@@ -887,6 +889,45 @@ static ssize_t uart_read(FAR struct file *filep,
 
           *buffer++ = ch;
           recvd++;
+
+          if (dev->isconsole
+#ifdef CONFIG_SERIAL_TERMIOS
+              || (dev->tc_iflag & ECHO)
+#endif
+             )
+            {
+              /* Check for the beginning of a VT100 escape sequence, 3 byte */
+
+              if (ch == ASCII_ESC)
+                {
+                  /* Mark that we should skip 2 more bytes */
+
+                  dev->escape = 2;
+                  continue;
+                }
+              else if (dev->escape == 2 && ch != ASCII_LBRACKET)
+                {
+                  /* It's not an <esc>[x 3 byte sequence, show it */
+
+                  dev->escape = 0;
+                }
+
+              /* Echo if the character is not a control byte */
+
+              if ((!iscntrl(ch & 0xff) || (ch == '\n')) && dev->escape == 0)
+                {
+                    {
+                      uart_putxmitchar(dev, ch, true);
+                    }
+                }
+
+              /* Skipping character count down */
+
+              if (dev->escape > 0)
+                {
+                  dev->escape--;
+                }
+            }
         }
 
 #ifdef CONFIG_DEV_SERIAL_FULLBLOCKS
@@ -1068,6 +1109,14 @@ static ssize_t uart_read(FAR struct file *filep,
               uart_enablerxint(dev);
             }
         }
+    }
+
+  if (recvd > 0)
+    {
+#ifdef CONFIG_SERIAL_TXDMA
+      uart_dmatxavail(dev);
+#endif
+      uart_enabletxint(dev);
     }
 
 #ifdef CONFIG_SERIAL_RXDMA
@@ -1770,7 +1819,11 @@ int uart_register(FAR const char *path, FAR uart_dev_t *dev)
 
       /* Convert CR to LF by default for console */
 
-      dev->tc_iflag |= ICRNL;
+      dev->tc_iflag |= ICRNL | ECHO;
+
+      /* Clear escape counter */
+
+      dev->escape = 0;
     }
 #endif
 

include/nuttx/serial/serial.h
@@ -271,6 +271,7 @@ struct uart_dev_s
   /* State data */
 
   uint8_t              open_count;   /* Number of times the device has been opened */
+  uint8_t              escape;       /* Number of the character to be escaped */
   volatile bool        xmitwaiting;  /* true: User waiting for space in xmit.buffer */
   volatile bool        recvwaiting;  /* true: User waiting for data in recv.buffer */
 #ifdef CONFIG_SERIAL_REMOVABLE

commit 9ffd3a1e5b4496c8b32311ad6d204f64f8ee8122

drivers/serial: Fix wrong ECHO flag
There is a rookie mistake introduced by https://github.com/apache/nuttx/pull/8691,
ECHO flag is a part of Local Mode (c_lflags) instead of c_iflags.

nuttx-apps should do the same change in nsh_login and termcurse_vt100.

Signed-off-by: Huang Qi <huangqi3@xiaomi.com>

drivers/serial/serial.c
@@ -893,7 +893,7 @@ static ssize_t uart_read(FAR struct file *filep,
 
           if (
 #ifdef CONFIG_SERIAL_TERMIOS
-              dev->tc_iflag & ECHO
+              dev->tc_lflag & ECHO
 #else
               dev->isconsole
 #endif
@@ -1822,9 +1822,9 @@ int uart_register(FAR const char *path, FAR uart_dev_t *dev)
 
   if (dev->isconsole)
     {
-      /* Enable signals by default */
+      /* Enable signals and echo by default */
 
-      dev->tc_lflag |= ISIG;
+      dev->tc_lflag |= ISIG | ECHO;
 
       /* Enable \n -> \r\n translation for the console */
 
@@ -1832,7 +1832,7 @@ int uart_register(FAR const char *path, FAR uart_dev_t *dev)
 
       /* Convert CR to LF by default for console */
 
-      dev->tc_iflag |= ICRNL | ECHO;
+      dev->tc_iflag |= ICRNL;
 
       /* Clear escape counter */
 