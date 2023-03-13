a5aa7886ba4f78c2428664c7ae8af30e36a9d4e5
raiden00pl <raiden00@railab.me>
2023-03-13 14:16:48 +0100

    boards/nrf5340-dk: add TIMER configuration

---
+++ boards/arm/nrf53/nrf5340-dk/src/nrf53_timer.c
@@ -0,0 +1,58 @@
+/****************************************************************************
+ * boards/arm/nrf53/nrf5340-dk/src/nrf53_timer.c
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+#include <nuttx/timers/timer.h>
+
+#include <debug.h>
+
+#include "nrf53_tim_lowerhalf.h"
+
+#include "nrf5340-dk.h"
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: nrf53_timer_driver_setup
+ *
+ * Description:
+ *   Configure the timer driver.
+ *
+ * Input Parameters:
+ *   devpath - The full path to the timer device.  This should be of the
+ *             form /dev/timer0
+ *   timer   - The timer's number.
+ *
+ * Returned Value:
+ *   Zero (OK) is returned on success; A negated errno value is returned
+ *   to indicate the nature of any failure.
+ *
+ ****************************************************************************/
+
+int nrf53_timer_driver_setup(const char *devpath, int timer)
+{
+  return nrf53_timer_initialize(devpath, timer);
+}
