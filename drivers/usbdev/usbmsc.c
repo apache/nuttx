7f028effc935a8636612a3c879f9c57e7173047c
raiden00pl <raiden00@railab.me>
2023-03-23 19:00:03 +0100

    usbmsc: do not send deferred response if we are part of the composite device
    EP0 is owned by the composite class so it shouldn't be directly accessed by usbmsc

--- drivers/usbdev/usbmsc.c
+++ drivers/usbdev/usbmsc.c
@@ -1219,6 +1219,7 @@ void usbmsc_rdcomplete(FAR struct usbdev_ep_s *ep,
 
 void usbmsc_deferredresponse(FAR struct usbmsc_dev_s *priv, bool failed)
 {
+#ifndef CONFIG_USBMSC_COMPOSITE
   FAR struct usbdev_s *dev;
   FAR struct usbdev_req_s *ctrlreq;
   int ret;
@@ -1260,6 +1261,7 @@ void usbmsc_deferredresponse(FAR struct usbmsc_dev_s *priv, bool failed)
       usbtrace(TRACE_CLSERROR(USBMSC_TRACEERR_DEFERREDRESPSTALLED), 0);
       EP_STALL(dev->ep0);
     }
+#endif
 }
 
 /****************************************************************************
