1f1c7700178da5e2cfd85015c6a3bca155849e78
hujun5 <hujun5@xiaomi.com>
2023-04-23 14:45:14 +0800

    system: pthread_barrierwait should be moved to kernel space
    The current implementation requires the use of enter_critical_section, so the source code needs to be moved to kernel space
    
    Signed-off-by: hujun5 <hujun5@xiaomi.com>

--- sched/pthread/pthread_barrierwait.c
+++ sched/pthread/pthread_barrierwait.c
@@ -1,5 +1,5 @@
 /****************************************************************************
- * libs/libc/pthread/pthread_barrierwait.c
+ * sched/pthread/pthread_barrierwait.c
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
