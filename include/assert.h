516bb0428627d06f28ad5a7e8c6cc3d700c45315
dongjiuzhu1 <dongjiuzhu1@xiaomi.com>
2023-04-26 23:16:18 +0800

    nuttx/bits.h: support GENMASK and GENMASK_ULL
    Ref to https://github.com/torvalds/linux/blob/master/include/linux/bits.h
    
    Signed-off-by: dongjiuzhu1 <dongjiuzhu1@xiaomi.com>

--- include/assert.h
+++ include/assert.h
@@ -132,6 +132,14 @@
 #  endif
 #endif
 
+/* Force a compilation error if condition is true, but also produce a
+ * result (of value 0 and type int), so the expression can be used
+ * e.g. in a structure initializer (or where-ever else comma expressions
+ * aren't permitted).
+ */
+
+#define BUILD_BUG_ON_ZERO(e) ((int)(sizeof(struct { int:(-!!(e)); })))
+
 /****************************************************************************
  * Included Files
  ****************************************************************************/
