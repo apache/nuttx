0f0f30a504b9d19e723976c662950c9b3ff40d55
Xiang Xiao <xiaoxiang@xiaomi.com>
2023-05-01 02:22:02 +0800

    mtd/nand: Implement MTDIOC_ERASESTATE ioctl
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- drivers/mtd/mtd_nand.c
+++ drivers/mtd/mtd_nand.c
@@ -801,6 +801,14 @@ static int nand_ioctl(struct mtd_dev_s *dev, int cmd, unsigned long arg)
         }
         break;
 
+      case MTDIOC_ERASESTATE:
+        {
+          FAR uint8_t *result = (FAR uint8_t *)arg;
+          *result = 0xff;
+          ret = OK;
+        }
+        break;
+
       default:
         ret = -ENOTTY; /* Bad command */
         break;
