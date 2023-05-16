040eb3c990adad935409cddd077976d7742ca043
Ville Juven <ville.juven@unikie.com>
2023-05-16 18:03:53 +0300

    riscv/swint: Give the full tcb to the context switch routine
    Why? The tcb can contain info that is needed by the context switch
    routine. One example is lazy-FPU handling; the integer registers can
    be stored into the stack, because they are always stored & restored.
    
    Lazy-FPU however needs a non-volatile location to store the FPU registers
    as the save feature will skip saving a clean FPU, but the restore must
    always restore the FPU registers if the thread uses FPU.

--- arch/risc-v/src/common/riscv_internal.h
+++ arch/risc-v/src/common/riscv_internal.h
@@ -29,6 +29,7 @@
 
 #ifndef __ASSEMBLY__
 #  include <nuttx/compiler.h>
+#  include <nuttx/sched.h>
 #  include <sys/types.h>
 #  include <stdint.h>
 #  include <syscall.h>
@@ -207,6 +208,18 @@ void riscv_fpuconfig(void);
 #  define riscv_fpuconfig()
 #endif
 
+/* Save / restore context of task */
+
+static inline void riscv_savecontext(struct tcb_s *tcb)
+{
+  tcb->xcp.regs = (uintptr_t *)CURRENT_REGS;
+}
+
+static inline void riscv_restorecontext(struct tcb_s *tcb)
+{
+  CURRENT_REGS = (uintptr_t *)tcb->xcp.regs;
+}
+
 /* RISC-V PMP Config ********************************************************/
 
 int riscv_config_pmp_region(uintptr_t region, uintptr_t attr,
@@ -304,19 +317,19 @@ void *riscv_perform_syscall(uintptr_t *regs);
 
 /* SYS call 1:
  *
- * void riscv_fullcontextrestore(uintptr_t *restoreregs) noreturn_function;
+ * void riscv_fullcontextrestore(struct tcb_s *next) noreturn_function;
  */
 
-#define riscv_fullcontextrestore(restoreregs) \
-  sys_call1(SYS_restore_context, (uintptr_t)restoreregs)
+#define riscv_fullcontextrestore(next) \
+  sys_call1(SYS_restore_context, (uintptr_t)next)
 
 /* SYS call 2:
  *
- * void riscv_switchcontext(uintptr_t *saveregs, uintptr_t *restoreregs);
+ * riscv_switchcontext(struct tcb_s *prev, struct tcb_s *next);
  */
 
-#define riscv_switchcontext(saveregs, restoreregs) \
-  sys_call2(SYS_switch_context, (uintptr_t)saveregs, (uintptr_t)restoreregs)
+#define riscv_switchcontext(prev, next) \
+  sys_call2(SYS_switch_context, (uintptr_t)prev, (uintptr_t)next)
 
 #ifdef CONFIG_BUILD_KERNEL
 /* SYS call 3:
