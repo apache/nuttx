7a0ef1d0cac4f2524d1660fa9739008823be483a
ligd <liguiding1@xiaomi.com>
2023-05-19 15:37:55 +0800

    secure: fix compile failed when no CONFIG_ARCH_HAVE_TRUSTZONE
    Signed-off-by: ligd <liguiding1@xiaomi.com>

--- include/nuttx/arch.h
+++ include/nuttx/arch.h
@@ -1552,17 +1552,19 @@ void up_trigger_irq(int irq, cpu_set_t cpuset);
 int up_prioritize_irq(int irq, int priority);
 #endif
 
-#ifdef CONFIG_ARCH_HAVE_TRUSTZONE
-
 /****************************************************************************
- * Name: up_set_secure_irq
+ * Name: up_secure_irq
  *
  * Description:
  *   Secure an IRQ
  *
  ****************************************************************************/
 
+#ifdef CONFIG_ARCH_HAVE_TRUSTZONE
 void up_secure_irq(int irq, bool secure);
+#else
+# define up_secure_irq(i, s)
+#endif
 
 /****************************************************************************
  * Name: up_secure_irq_all
@@ -1572,8 +1574,10 @@ void up_secure_irq(int irq, bool secure);
  *
  ****************************************************************************/
 
+#ifdef CONFIG_ARCH_HAVE_TRUSTZONE
 void up_secure_irq_all(bool secure);
-
+#else
+# define up_secure_irq_all(s)
 #endif
 
 /****************************************************************************
