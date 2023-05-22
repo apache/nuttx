04fa008018d01fb5769d14d62b610258f6a1ab7a
raiden00pl <raiden00@railab.me>
2023-05-22 14:15:10 +0200

    boards/nrf5340-dk: add usb example
    The board can act as an USB composite device. For now CDCACM and RNDIS are supported.

--- boards/arm/nrf53/nrf5340-dk/src/nrf53_bringup.c
+++ boards/arm/nrf53/nrf5340-dk/src/nrf53_bringup.c
@@ -41,6 +41,14 @@
 #  include <nuttx/input/buttons.h>
 #endif
 
+#ifdef CONFIG_USBMONITOR
+#  include <nuttx/usb/usbmonitor.h>
+#endif
+
+#ifdef CONFIG_RNDIS
+#  include <nuttx/usb/rndis.h>
+#endif
+
 #ifdef CONFIG_NRF53_SOFTDEVICE_CONTROLLER
 #  include "nrf53_sdc.h"
 #endif
@@ -254,6 +262,27 @@ int nrf53_bringup(void)
              "ERROR: Failed to initialize ADC driver: %d\n",
              ret);
     }
+#endif
+
+#ifdef CONFIG_USBMONITOR
+  /* Start the USB Monitor */
+
+  ret = usbmonitor_start();
+  if (ret != OK)
+    {
+      syslog(LOG_ERR, "ERROR: Failed to start USB monitor: %d\n", ret);
+    }
+#endif
+
+#if defined(CONFIG_RNDIS) && !defined(CONFIG_RNDIS_COMPOSITE)
+  uint8_t mac[6];
+  mac[0] = 0xa0; /* TODO */
+  mac[1] = (CONFIG_NETINIT_MACADDR_2 >> (8 * 0)) & 0xff;
+  mac[2] = (CONFIG_NETINIT_MACADDR_1 >> (8 * 3)) & 0xff;
+  mac[3] = (CONFIG_NETINIT_MACADDR_1 >> (8 * 2)) & 0xff;
+  mac[4] = (CONFIG_NETINIT_MACADDR_1 >> (8 * 1)) & 0xff;
+  mac[5] = (CONFIG_NETINIT_MACADDR_1 >> (8 * 0)) & 0xff;
+  usbdev_rndis_initialize(mac);
 #endif
 
   /* Initialize BLE */
