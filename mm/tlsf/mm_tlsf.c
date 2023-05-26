6572081e075f2b20faa56463a78f7e469faafced
anjiahao <anjiahao@xiaomi.com>
2023-05-26 10:42:50 +0800

    mempool:Allocate a chunk for multiple mempool avoid memory fragmentation
    Signed-off-by: anjiahao <anjiahao@xiaomi.com>

--- mm/tlsf/mm_tlsf.c
+++ mm/tlsf/mm_tlsf.c
@@ -844,10 +844,11 @@ FAR struct mm_heap_s *mm_initialize(FAR const char *name,
     }
 
   heap->mm_mpool = mempool_multiple_init(name, poolsize, MEMPOOL_NPOOLS,
-                                  (mempool_multiple_alloc_t)mempool_memalign,
-                                  (mempool_multiple_free_t)mm_free, heap,
-                                  CONFIG_MM_HEAP_MEMPOOL_EXPAND,
-                                  CONFIG_MM_HEAP_MEMPOOL_DICTIONARY_EXPAND);
+                              (mempool_multiple_alloc_t)mempool_memalign,
+                              (mempool_multiple_free_t)mm_free, heap,
+                              CONFIG_MM_HEAP_MEMPOOL_CHUNK_SIZE,
+                              CONFIG_MM_HEAP_MEMPOOL_EXPAND_SIZE,
+                              CONFIG_MM_HEAP_MEMPOOL_DICTIONARY_EXPAND_SIZE);
 #endif
 
   return heap;
