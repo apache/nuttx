fc97ce7dbd7dcae7b9b1b100b0826413ac837009
Fotis Panagiotopoulos <f.j.panag@gmail.com>
2023-05-30 00:26:19 +0300

    net: Fix task block when devif_send fails.
    When a task needs to send data, a callback is allocated and the
    transmission is happening in a worker task through devif_send.
    Synchronization between the two tasks (sender & worker) is
    achieved by a semaphore.
    
    If devif_send fails, this semaphore was never posted, leaving
    the sending task blocked indefinitely. This commit fixes this
    by checking the return code of netif_send, and posting this
    semaphore in case of failure.
    
    Polling then stops, and execution is resumed on the sending
    task.

--- net/devif/devif.h
+++ net/devif/devif.h
@@ -432,31 +432,40 @@ uint16_t devif_conn_event(FAR struct net_driver_s *dev, uint16_t flags,
 uint16_t devif_dev_event(FAR struct net_driver_s *dev, uint16_t flags);
 
 /****************************************************************************
- * Send data on the current connection.
+ * Name: devif_send
  *
- * This function is used to send out a single segment of TCP data.  Only
- * socket logic that have been invoked by the lower level for event
- * processing can send data.
+ * Description:
+ *   Send data on the current connection.
  *
- * The amount of data that actually is sent out after a call to this
- * function is determined by the maximum amount of data TCP allows. The
- * network will automatically crop the data so that only the appropriate
- * amount of data is sent. The mss field of the TCP connection structure
- * can be used to determine the amount of data that actually will be sent.
+ *   This function is used to send out a single segment of TCP data.  Only
+ *   socket logic that have been invoked by the lower level for event
+ *   processing can send data.
+ *
+ *   The amount of data that actually is sent out after a call to this
+ *   function is determined by the maximum amount of data TCP allows. The
+ *   network will automatically crop the data so that only the appropriate
+ *   amount of data is sent. The mss field of the TCP connection structure
+ *   can be used to determine the amount of data that actually will be sent.
  *
  * Note:  This function does not guarantee that the sent data will
  * arrive at the destination.  If the data is lost in the network, the
  * TCP socket layer will be invoked with the TCP_REXMIT flag set.  The
  * socket layer will then have to resend the data using this function.
  *
- * data A pointer to the data which is to be sent.
+ * Input Parameters:
+ *   dev - The network device state structure associated with the network
+ *     device that initiated the callback event.
+ *   buf - A pointer to the data which is to be sent.
+ *   len - The maximum amount of data bytes to be sent.
+ *   offset - Offset of data in buffer.
  *
- * len The maximum amount of data bytes to be sent.
+ * Returned Value:
+ *   The amount of data sent, or negated ERRNO in case of failure.
  *
  ****************************************************************************/
 
-void devif_send(FAR struct net_driver_s *dev, FAR const void *buf,
-                int len, unsigned int offset);
+int devif_send(FAR struct net_driver_s *dev, FAR const void *buf,
+               int len, unsigned int offset);
 
 /****************************************************************************
  * Name: devif_iob_send
@@ -475,9 +484,9 @@ void devif_send(FAR struct net_driver_s *dev, FAR const void *buf,
 
 #ifdef CONFIG_MM_IOB
 struct iob_s;
-void devif_iob_send(FAR struct net_driver_s *dev, FAR struct iob_s *buf,
-                    unsigned int len, unsigned int offset,
-                    unsigned int target_offset);
+int devif_iob_send(FAR struct net_driver_s *dev, FAR struct iob_s *buf,
+                   unsigned int len, unsigned int offset,
+                   unsigned int target_offset);
 #endif
 
 /****************************************************************************
