c946472842c83a908bcac24cd5356d844bd585c0
qinwei1 <qinwei1@xiaomi.com>
2023-05-30 14:36:36 +0800

    arm64: GoldFish Platform support
    Summary:
       Adding virtual evaluate platform GoldFish. Which is based on
    Android Goldfish Emulator, it's a ARM virt board but Android enhance
    it with more featue
       The patch set goldfish as a chip
    
    Signed-off-by: qinwei1 <qinwei1@xiaomi.com>

---
+++ arch/arm64/src/goldfish/goldfish_serial.h
@@ -0,0 +1,58 @@
+/****************************************************************************
+ * arch/arm64/src/goldfish/goldfish_serial.h
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+#ifndef __ARCH_ARM64_SRC_GOLDFISH_GOLDFISH_SERIAL_H
+#define __ARCH_ARM64_SRC_GOLDFISH_GOLDFISH_SERIAL_H
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Public Types
+ ****************************************************************************/
+
+/****************************************************************************
+ * Inline Functions
+ ****************************************************************************/
+
+#ifndef __ASSEMBLY__
+
+/****************************************************************************
+ * Public Data
+ ****************************************************************************/
+
+#ifdef CONFIG_ARCH_CHIP_GOLDFISH
+#define CONFIG_QEMU_UART_BASE      0x9000000
+#define CONFIG_QEMU_UART_IRQ       (GIC_SPI_INT_BASE + 1)
+#endif
+
+/****************************************************************************
+ * Public Function Prototypes
+ ****************************************************************************/
+
+#endif /* __ASSEMBLY__ */
+#endif /* __ARCH_ARM64_SRC_GOLDFISH_GOLDFISH_SERIAL_H */
