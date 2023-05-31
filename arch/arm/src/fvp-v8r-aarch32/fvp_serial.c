ac1b94efbec3e33452d86898990b6a573e407109
Alexander Merkle <alexander.merkle@lauterbach.com>
2023-05-31 15:00:31 +0200

    board: add CortexR52 FVP AEMv8R platform
    see board/arm/fvp-v8r-aarch32/fvp-armv8r-aarch32/scripts/readme.txt
    
    Port is highly based on fvp-v8r AARCH32 port.

---
+++ arch/arm/src/fvp-v8r-aarch32/fvp_serial.c
@@ -0,0 +1,75 @@
+/***************************************************************************
+ * arch/arm/src/fvp-v8r-aarch32/fvp_serial.c
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ***************************************************************************/
+
+/***************************************************************************
+ * Included Files
+ ***************************************************************************/
+
+#include <nuttx/config.h>
+#include <sys/types.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <string.h>
+#include <assert.h>
+#include <errno.h>
+#include <debug.h>
+
+#include <nuttx/serial/uart_pl011.h>
+
+#include "arm_internal.h"
+
+#ifdef USE_SERIALDRIVER
+
+/***************************************************************************
+ * Public Functions
+ ***************************************************************************/
+
+/***************************************************************************
+ * Name: arm_earlyserialinit
+ *
+ * Description:
+ *   see arm_internal.h
+ *
+ ***************************************************************************/
+
+void arm_earlyserialinit(void)
+{
+  /* Enable the console UART.  The other UARTs will be initialized if and
+   * when they are first opened.
+   */
+
+  pl011_earlyserialinit();
+}
+
+/***************************************************************************
+ * Name: arm_serialinit
+ *
+ * Description:
+ *   see arm_internal.h
+ *
+ ***************************************************************************/
+
+void arm_serialinit(void)
+{
+  pl011_serialinit();
+}
+
+#endif /* USE_SERIALDRIVER */
