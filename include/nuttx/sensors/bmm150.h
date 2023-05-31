8dfe3dca641d4f48b4bd7416171330b1d35955ed
raiden00pl <raiden00@railab.me>
2023-05-31 19:37:38 +0200

    sensors: add support for bmm150 magnetometer
    add support for Bosch BMM150 Magnetometer

---
+++ include/nuttx/sensors/bmm150.h
@@ -0,0 +1,80 @@
+/****************************************************************************
+ * include/nuttx/sensors/bmm150.h
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+#ifndef __INCLUDE_NUTTX_SENSORS_BMM150_H
+#define __INCLUDE_NUTTX_SENSORS_BMM150_H
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+#include <nuttx/i2c/i2c_master.h>
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Public Types
+ ****************************************************************************/
+
+struct bmm150_config_s
+{
+  FAR struct i2c_master_s *i2c;
+  uint8_t addr;
+};
+
+/****************************************************************************
+ * Public Function Prototypes
+ ****************************************************************************/
+
+#ifdef __cplusplus
+#define EXTERN extern "C"
+extern "C"
+{
+#else
+#define EXTERN extern
+#endif
+
+/****************************************************************************
+ * Name: bmm150_register
+ *
+ * Description:
+ *   Register the BMM150 character device as uorb
+ *
+ * Input Parameters:
+ *   devno  - device instance
+ *   config - device configuratio
+ *
+ * Returned Value:
+ *   Zero (OK) on success; a negated errno value on failure.
+ *
+ ****************************************************************************/
+
+int bmm150_register_uorb(int devno, FAR struct bmm150_config_s *config);
+
+#undef EXTERN
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __INCLUDE_NUTTX_SENSORS_BMM150_H */
