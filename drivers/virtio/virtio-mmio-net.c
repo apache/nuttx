e149a1278bd57a4a006d9bb658e9809d846ea6aa
Masayuki Ishikawa <masayuki.ishikawa@gmail.com>
2023-06-02 00:00:18 +0900

    dirivers: virtio: Fix virtnet_virtnet_txpoll() in virtio-mmio-net.c
    Summary:
    - I noticed that nfsmount always timed out.
    - This commit fixes this issue.
    
    Impact:
    - None
    
    Testing:
    - Tested wih qemu-7.1
    - NOTE: defconfigs will be updated later.
    
    Signed-off-by: Masayuki Ishikawa <Masayuki.Ishikawa@jp.sony.com>

--- drivers/virtio/virtio-mmio-net.c
+++ drivers/virtio/virtio-mmio-net.c
@@ -421,7 +421,7 @@ static int virtnet_transmit(FAR struct virtnet_driver_s *priv)
  *   dev - Reference to the NuttX driver state structure
  *
  * Returned Value:
- *   OK on success; a negated errno on failure
+ *   Always OK
  *
  * Assumptions:
  *   The network is locked.
@@ -443,9 +443,11 @@ static int virtnet_txpoll(FAR struct net_driver_s *dev)
 
   virtnet_reply(priv);
 
-  /* Stop the poll now because we only have one tx buffer (g_pktbuf) */
+  /* NOTE: Since virtnet_transmit() now waits for TX completion,
+   * this method should return OK to continue.
+   */
 
-  return -EBUSY;
+  return OK;
 }
 
 /****************************************************************************
