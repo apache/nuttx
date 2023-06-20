29ddfd44a0d2bc05e54361d44c573f32d9f35a59
dongjiuzhu1 <dongjiuzhu1@xiaomi.com>
2023-06-20 16:56:24 +0800

    driver/note: dump irq handler address to trace
    Signed-off-by: dongjiuzhu1 <dongjiuzhu1@xiaomi.com>

--- drivers/note/noteram_driver.c
+++ drivers/note/noteram_driver.c
@@ -887,8 +887,8 @@ static int noteram_dump_one(FAR uint8_t *p, FAR struct lib_outstream_s *s,
 
         nih = (FAR struct note_irqhandler_s *)p;
         ret += noteram_dump_header(s, note, ctx);
-        ret += lib_sprintf(s, "irq_handler_entry: irq=%u name=%d\n",
-                          nih->nih_irq, nih->nih_irq);
+        ret += lib_sprintf(s, "irq_handler_entry: irq=%u name=%pS\n",
+                           nih->nih_irq, (FAR void *)nih->nih_handler);
         cctx->intr_nest++;
       }
       break;
