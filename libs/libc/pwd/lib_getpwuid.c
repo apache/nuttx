9d1b8f0966d9fef6a2d16a86cd6d8355856e8c8a
Xiang Xiao <xiaoxiang@xiaomi.com>
2023-06-25 19:47:32 +0800

    libc/pwd: Correct the geos related comment and ddd ROOT_GEOS macro
    fix the minor issue in https://github.com/apache/nuttx/pull/7998
    
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- libs/libc/pwd/lib_getpwuid.c
+++ libs/libc/pwd/lib_getpwuid.c
@@ -70,7 +70,7 @@ FAR struct passwd *getpwuid(uid_t uid)
       return NULL;
     }
 
-  return getpwbuf(ROOT_UID, ROOT_GID, ROOT_NAME, ROOT_NAME, ROOT_DIR,
+  return getpwbuf(ROOT_UID, ROOT_GID, ROOT_NAME, ROOT_GEOCS, ROOT_DIR,
                   ROOT_SHELL);
 #endif
 }
