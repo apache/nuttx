149c82483a00e7e910bd01d676df24834c2217a7
Mingjie Shen <shen497@purdue.edu>
2023-06-24 23:19:49 -0400

    drivers/sensors/apds9960.c: Fix use after free
    Memory pointed by priv may be used (in line 1289) after it is
    freed in line 1283.
    Fix by adding the missing return statement in error handling.
    
    Signed-off-by: Mingjie Shen <shen497@purdue.edu>

--- drivers/sensors/apds9960.c
+++ drivers/sensors/apds9960.c
@@ -1281,6 +1281,7 @@ int apds9960_register(FAR const char *devpath,
     {
       snerr("ERROR: Failed to register driver: %d\n", ret);
       kmm_free(priv);
+      return ret;
     }
 
   /* Attach to the interrupt */
