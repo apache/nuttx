4d1b19c213b9233da6c845eba499ec867b7a8663
shipei <shipei@xiaomi.com>
2023-06-26 21:34:19 +0800

    audio:add virtio snd driver
    playback test:
    nxplayer
    device /dev/audio/pcm0p
    playraw /data/test.wav 2 16 44100
    stop
    
    record test:
    nxrecorder
    device /dev/audio/pcm0c
    recordraw /data/rec.pcm 2 16 16000
    stop
    
    Signed-off-by: shipei <shipei@xiaomi.com>

--- drivers/virtio/virtio.c
+++ drivers/virtio/virtio.c
@@ -32,6 +32,7 @@
 #include "virtio-net.h"
 #include "virtio-rng.h"
 #include "virtio-serial.h"
+#include "virtio-snd.h"
 
 /****************************************************************************
  * Private Types
@@ -145,7 +146,15 @@ void virtio_register_drivers(void)
   ret = virtio_register_serial_driver();
   if (ret < 0)
     {
-      vrterr("virtio_serial_driver_init failed, ret=%d\n", ret);
+      vrterr("virtio_register_serial_driver failed, ret=%d\n", ret);
+    }
+#endif
+
+#ifdef CONFIG_DRIVERS_VIRTIO_SOUND
+  ret = virtio_register_snd_driver();
+  if (ret < 0)
+    {
+      vrterr("virtio_register_snd_driver failed, ret=%d\n", ret);
     }
 #endif
 
