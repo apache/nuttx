a1f26ab24b192d18eb6aa5de31a8c146dc8441c7
SPRESENSE <41312067+SPRESENSE@users.noreply.github.com>
2023-06-27 11:23:59 +0900

    include: nuttx: Re-allocate SIOCDENYINETSOCK
    The SIOCDENYINETSOCK definition is duplicate with SIOCSIFMTU.
    So, re-allocate SIOCDENYINETSOCK command number.

--- include/nuttx/net/ioctl.h
+++ include/nuttx/net/ioctl.h
@@ -125,7 +125,7 @@
 
 /* Network socket control ***************************************************/
 
-#define SIOCDENYINETSOCK _SIOC(0x0033) /* Deny network socket. */
+#define SIOCDENYINETSOCK _SIOC(0x003C) /* Deny network socket. */
 
 /* Bridge calls *************************************************************/
 
