bde91a2abd716b58dd8b1e406935bbddd1c0b858
wangchen <wangchen41@xiaomi.com>
2023-06-30 17:24:07 +0800

    inet:fix libuvtest udp multicast error
    Signed-off-by: wangchen <wangchen41@xiaomi.com>

--- net/inet/ipv4_setsockopt.c
+++ net/inet/ipv4_setsockopt.c
@@ -38,6 +38,7 @@
 #include "igmp/igmp.h"
 #include "inet/inet.h"
 #include "socket/socket.h"
+#include "udp/udp.h"
 
 #if defined(CONFIG_NET_IPv4) && defined(CONFIG_NET_SOCKOPTS)
 
@@ -180,6 +181,7 @@ int ipv4_setsockopt(FAR struct socket *psock, int option,
                                        * outgoing multicast packets */
         {
           FAR struct socket_conn_s *conn;
+          int ttl;
 
           if (value == NULL || value_len == 0)
             {
