a4b6b158ce7f99ee8d306e2c8ace83a5002d4cfa
wangming9 <wangming9@xiaomi.com>
2023-06-30 14:26:02 +0800

    arch/armv6-m: By default, add REG_EXC_RETURN to xcptcontext.
    Signed-off-by: wangming9 <wangming9@xiaomi.com>

--- arch/arm/src/armv6-m/arm_initialstate.c
+++ arch/arm/src/armv6-m/arm_initialstate.c
@@ -129,7 +129,6 @@ void up_initial_state(struct tcb_s *tcb)
 #endif
 #endif /* CONFIG_PIC */
 
-#ifdef CONFIG_BUILD_PROTECTED
   /* All tasks start via a stub function in kernel space.  So all
    * tasks must start in privileged thread mode.  If CONFIG_BUILD_PROTECTED
    * is defined, then that stub function will switch to unprivileged
@@ -137,7 +136,6 @@ void up_initial_state(struct tcb_s *tcb)
    */
 
   xcp->regs[REG_EXC_RETURN] = EXC_RETURN_PRIVTHR;
-#endif
 
   /* Enable or disable interrupts, based on user configuration */
 
