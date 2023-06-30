745f443cbf34719551cd0bc6212658b40947448a
Masayuki Ishikawa <masayuki.ishikawa@gmail.com>
2023-06-30 13:12:52 +0900

    arch: arm64: Fix to access psci with hypervisor
    Summary:
    - I noticed that pcsi version can not be obtained with qemu + kvm.
    - Also, pci_detect hangs with qemu + hvf.
    - This commit fixes this issue by using hvc (hypervisor call).
    
    Impact:
    - None
    
    Testing:
    - Tested with qemu-7.1 + kvm (linux) and qemu-7.1 + hvf (macOS)
    
    Signed-off-by: Masayuki Ishikawa <Masayuki.Ishikawa@jp.sony.com>

--- arch/arm64/src/qemu/qemu_boot.c
+++ arch/arm64/src/qemu/qemu_boot.c
@@ -154,9 +154,10 @@ void arm64_chip_boot(void)
 
   arm64_mmu_init(true);
 
-#if defined(CONFIG_SMP) || defined(CONFIG_ARCH_HAVE_PSCI)
+#if defined(CONFIG_ARCH_CHIP_QEMU_WITH_HV)
+  arm64_psci_init("hvc");
+#elif defined(CONFIG_SMP) || defined(CONFIG_ARCH_HAVE_PSCI)
   arm64_psci_init("smc");
-
 #endif
 
   /* Perform board-specific device initialization. This would include
