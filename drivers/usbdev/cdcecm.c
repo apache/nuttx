ee8c94635d1fac49bd6f43bf03c87f1a82b81706
zhangyuan21 <zhangyuan21@xiaomi.com>
2023-07-06 14:48:51 +0800

    usbdev: set ep0 priv only not define CONFIG_USBDEV_COMPOSITE
    Signed-off-by: zhangyuan21 <zhangyuan21@xiaomi.com>

--- drivers/usbdev/cdcecm.c
+++ drivers/usbdev/cdcecm.c
@@ -1661,7 +1661,9 @@ static int cdcecm_bind(FAR struct usbdevclass_driver_s *driver,
 
   uinfo("\n");
 
+#ifndef CONFIG_CDCECM_COMPOSITE
   dev->ep0->priv = self;
+#endif
 
   /* Preallocate control request */
 
