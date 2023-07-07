b8d3e32bdfdc85841a3de4f4589ce972e9cb1987
Petro Karashchenko <petro.karashchenko@gmail.com>
2023-07-07 12:04:59 +0300

    sched/clock: move clock_getcpuclockid() and clock_getres() to libc
    Signed-off-by: Petro Karashchenko <petro.karashchenko@gmail.com>

--- include/sys/syscall_lookup.h
+++ include/sys/syscall_lookup.h
@@ -170,8 +170,6 @@ SYSCALL_LOOKUP(clock_nanosleep,            4)
  */
 
 SYSCALL_LOOKUP(clock,                      0)
-SYSCALL_LOOKUP(clock_getcpuclockid,        2)
-SYSCALL_LOOKUP(clock_getres,               2)
 SYSCALL_LOOKUP(clock_gettime,              2)
 SYSCALL_LOOKUP(clock_settime,              2)
 #ifdef CONFIG_CLOCK_TIMEKEEPING
