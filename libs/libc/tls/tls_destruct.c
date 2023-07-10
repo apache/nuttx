e26badd057df199efc0d75b0823e8f53843868a0
cuiziwei <cuiziwei@xiaomi.com>
2023-07-10 10:08:50 +0800

    nuttx/tls:Setting the candidtate index to null prevents dangling pointers.
    Signed-off-by: cuiziwei <cuiziwei@xiaomi.com>

--- libs/libc/tls/tls_destruct.c
+++ libs/libc/tls/tls_destruct.c
@@ -60,7 +60,7 @@ void tls_destruct(void)
   DEBUGASSERT(info != NULL);
   tlsset = info->ta_tlsset;
 
-  for (candidate = 0; candidate < CONFIG_TLS_NELEM; candidate++)
+  for (candidate = CONFIG_TLS_NELEM - 1; candidate >= 0; candidate--)
     {
       /* Is this candidate index available? */
 
@@ -68,6 +68,7 @@ void tls_destruct(void)
       if (tlsset & mask)
         {
           tls_elem_ptr = (FAR void *)tls->tl_elem[candidate];
+          tls->tl_elem[candidate] = 0;
           destructor = info->ta_tlsdtor[candidate];
           if (tls_elem_ptr && destructor)
             {
