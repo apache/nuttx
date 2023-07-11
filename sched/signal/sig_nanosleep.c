4c14280c1c2d3602aaca12dfaf037d099725fb93
guoshichao <guoshichao@xiaomi.com>
2023-07-11 20:56:40 +0800

    sched/signal/sig_nanosleep: fix the clock_nanosleep posix case
    1. make the clock_nanosleep can pass
    ltp/open_posix_testsuite/clock_nanosleep 13-1, 10-1, 9-1 cases
    2. the modification are referred to https://pubs.opengroup.org/onlinepubs/9699919799/functions/clock_nanosleep.html
    
    Signed-off-by: guoshichao <guoshichao@xiaomi.com>

--- sched/signal/sig_nanosleep.c
+++ sched/signal/sig_nanosleep.c
@@ -244,12 +244,13 @@ int nxsig_nanosleep(FAR const struct timespec *rqtp,
  *   actually slept). If the rmtp argument is NULL, the remaining time is not
  *   returned.
  *
- *   If clock_nanosleep() fails, it returns a value of -1 and sets errno to
- *   indicate the error. The clock_nanosleep() function will fail if:
+ *   If clock_nanosleep() fails, it returns a value of errno. The
+ *   clock_nanosleep() function will fail if:
  *
  *     EINTR - The clock_nanosleep() function was interrupted by a signal.
  *     EINVAL - The rqtp argument specified a nanosecond value less than
- *       zero or greater than or equal to 1000 million.
+ *       zero or greater than or equal to 1000 million. Or the clockid that
+ *       does not specify a known clock.
  *     ENOSYS - The clock_nanosleep() function is not supported by this
  *       implementation.
  *
@@ -264,6 +265,12 @@ int clock_nanosleep(clockid_t clockid, int flags,
   /* clock_nanosleep() is a cancellation point */
 
   enter_cancellation_point();
+
+  if (clockid < CLOCK_REALTIME || clockid > CLOCK_BOOTTIME)
+    {
+      leave_cancellation_point();
+      return EINVAL;
+    }
 
   /* Check if absolute time is selected */
 
@@ -286,7 +293,7 @@ int clock_nanosleep(clockid_t clockid, int flags,
 
           leave_critical_section(irqstate);
           leave_cancellation_point();
-          return ERROR;
+          return -ret;
         }
 
       clock_timespec_subtract(rqtp, &now, &reltime);
@@ -312,10 +319,9 @@ int clock_nanosleep(clockid_t clockid, int flags,
 
   if (ret < 0)
     {
-      /* If not set the errno variable and return -1 */
+      /* If not return the errno */
 
-      set_errno(-ret);
-      ret = ERROR;
+      ret = -ret;
     }
 
   leave_cancellation_point();
