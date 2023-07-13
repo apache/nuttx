8277229f53eae7bb37a0f6bbd6d16dc39cffb21d
chao an <anchao@xiaomi.com>
2023-07-13 22:55:12 +0800

    nxstyle: covert utf-8 to ascii
    libs/libc/stdio/lib_libgetstreams.c: C source, UTF-8 Unicode text
    ->
    libs/libc/stdio/lib_libgetstreams.c: C source, ASCII text
    
    Signed-off-by: chao an <anchao@xiaomi.com>

--- libs/libc/stdio/lib_libgetstreams.c
+++ libs/libc/stdio/lib_libgetstreams.c
@@ -62,7 +62,7 @@ FAR struct streamlist *lib_get_streams(void)
  * Name: lib_get_stream
  *
  * Description:
- *   Return a pointer to the file stream for this thread and given fd。
+ *   Return a pointer to the file stream for this thread and given fd.
  *   Note: only reserved fd number 0/1/2 is valid.
  *
  ****************************************************************************/
