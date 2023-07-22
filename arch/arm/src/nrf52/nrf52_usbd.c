3915d12b39349397f41b465fd6d591de0b6c9d3b
Brian Doherty <brian.r.doherty@gmail.com>
2023-07-22 15:38:02 -0500

    nrf52 USB init was waiting for an event which may not come if USB remains connected through boot. Changed to read status register instead.

--- arch/arm/src/nrf52/nrf52_usbd.c
+++ arch/arm/src/nrf52/nrf52_usbd.c
@@ -3052,9 +3052,10 @@ static void nrf52_hwinitialize(struct nrf52_usbdev_s *priv)
 {
   /* Wait for VBUS */
 
-  /* TODO: connect to POWER USB events */
 
-  while (getreg32(NRF52_POWER_EVENTS_USBDETECTED) == 0);
+  /* Wait for both power applied and ready */
+
+  while ((getreg32(NRF52_POWER_USBREGSTATUS) & NRF52_POWER_USBREGSTATUS_VBUSDETECT) == 0) ;
 
   /* Errata [187] USBD: USB cannot be enabled */
 
