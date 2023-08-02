1d858ec03feb27d4c8c72b07ff309fbb246723e2
raiden00pl <raiden00@railab.me>
2023-08-02 11:27:05 +0200

    stm32: initialize perf counter if sytemview enabled

--- arch/arm/src/stm32/stm32_start.c
+++ arch/arm/src/stm32/stm32_start.c
@@ -158,7 +158,7 @@ void __start(void)
 
   showprogress('C');
 
-#ifdef CONFIG_SCHED_IRQMONITOR
+#if defined(CONFIG_SCHED_IRQMONITOR) || defined(CONFIG_SEGGER_SYSVIEW)
   up_perf_init((void *)STM32_SYSCLK_FREQUENCY);
 #endif
 
