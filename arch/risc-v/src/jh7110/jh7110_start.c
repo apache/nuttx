856526adee25cdb9987ff800767a71ec9059d1ea
Lee Lup Yuen <luppy@appkaki.com>
2023-08-04 07:31:14 +0800

    arch/risc-v: Add support for StarFive JH7110 SoC
    This PR adds support for the StarFive JH7110 RISC-V SoC. This will be used by the upcoming port of NuttX for PINE64 Star64 SBC. [The source files are explained in the articles here](https://github.com/lupyuen/nuttx-star64)
    
    Modified Files in arch/risc-v:
    
    Kconfig: Added ARCH_CHIP_JH7110 for JH7110 SoC
    
    New Files in arch/risc-v:
    
    include/jh7110/chip.h: JH7110 Definitions
    
    include/jh7110/irq.h: Support 127 External Interrupts
    
    src/jh7110/chip.h: Interrupt Stack Macro
    
    src/jh7110/jh7110_allocateheap.c: Kernel Heap
    
    src/jh7110/jh7110_head.S: Linux Header and Boot Code
    
    src/jh7110/jh7110_irq.c: Configure Interrupts
    
    src/jh7110/jh7110_irq_dispatch.c: Dispatch Interrupts
    
    src/jh7110/jh7110_memorymap.h: Memory Map
    
    src/jh7110/jh7110_mm_init.c, jh7110_mm_init.h: Memory Mgmt
    
    src/jh7110/jh7110_pgalloc.c: Page Allocator
    
    src/jh7110/jh7110_start.c: Startup Code
    
    src/jh7110/jh7110_timerisr.c: Timer Interrupt
    
    src/jh7110/hardware/jh7110_memorymap.h: PLIC Base Address
    
    src/jh7110/hardware/jh7110_plic.h: PLIC Register Addresses
    
    src/jh7110/Kconfig: JH7110 Config
    
    src/jh7110/Make.defs: Makefile

---
+++ arch/risc-v/src/jh7110/jh7110_start.c
@@ -0,0 +1,168 @@
+/****************************************************************************
+ * arch/risc-v/src/jh7110/jh7110_start.c
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+#include <nuttx/init.h>
+#include <nuttx/arch.h>
+#include <nuttx/serial/uart_16550.h>
+#include <arch/board/board.h>
+
+#include "riscv_internal.h"
+#include "chip.h"
+#include "jh7110_mm_init.h"
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+#ifdef CONFIG_DEBUG_FEATURES
+#define showprogress(c) up_putc(c)
+#else
+#define showprogress(c)
+#endif
+
+/****************************************************************************
+ * Extern Function Declarations
+ ****************************************************************************/
+
+extern void __trap_vec(void);
+
+/****************************************************************************
+ * Public Data
+ ****************************************************************************/
+
+/* NOTE: g_idle_topstack needs to point the top of the idle stack
+ * for CPU0 and this value is used in up_initial_state()
+ */
+
+uintptr_t g_idle_topstack = JH7110_IDLESTACK_TOP;
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: jh7110_clear_bss
+ ****************************************************************************/
+
+void jh7110_clear_bss(void)
+{
+  uint32_t *dest;
+
+  /* Clear .bss.  We'll do this inline (vs. calling memset) just to be
+   * certain that there are no issues with the state of global variables.
+   */
+
+  for (dest = (uint32_t *)_sbss; dest < (uint32_t *)_ebss; )
+    {
+      *dest++ = 0;
+    }
+}
+
+/****************************************************************************
+ * Name: jh7110_start
+ ****************************************************************************/
+
+void jh7110_start_s(int mhartid)
+{
+  /* Configure FPU */
+
+  riscv_fpuconfig();
+
+  if (mhartid > 0)
+    {
+      goto cpux;
+    }
+
+  showprogress('A');
+
+#ifdef USE_EARLYSERIALINIT
+  riscv_earlyserialinit();
+#endif
+
+  showprogress('B');
+
+  /* Do board initialization */
+
+  showprogress('C');
+
+  /* Setup page tables for kernel and enable MMU */
+
+  jh7110_mm_init();
+
+  /* Call nx_start() */
+
+  nx_start();
+
+cpux:
+
+#ifdef CONFIG_SMP
+  riscv_cpu_boot(mhartid);
+#endif
+
+  while (true)
+    {
+      asm("WFI");
+    }
+}
+
+/****************************************************************************
+ * Name: jh7110_start
+ ****************************************************************************/
+
+void jh7110_start(int mhartid)
+{
+  DEBUGASSERT(mhartid == 0); /* Only Hart 0 supported for now */
+
+  if (0 == mhartid)
+    {
+      jh7110_clear_bss();
+
+      /* Initialize the per CPU areas */
+
+      riscv_percpu_add_hart(mhartid);
+    }
+
+  /* Disable MMU */
+
+  WRITE_CSR(satp, 0x0);
+
+  /* Set the trap vector for S-mode */
+
+  WRITE_CSR(stvec, (uintptr_t)__trap_vec);
+
+  /* Start S-mode */
+
+  jh7110_start_s(mhartid);
+}
+
+void riscv_earlyserialinit(void)
+{
+  u16550_earlyserialinit();
+}
+
+void riscv_serialinit(void)
+{
+  u16550_serialinit();
+}
