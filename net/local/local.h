d528d2c410462ba8c90a9f1953b7123b056b6a11
wangchen <wangchen41@xiaomi.com>
2023-08-03 11:28:28 +0800

    net/local/local_sockif.c:add local_getpeername function implementation
    this testcast
    TEST_IMPL(udp_send_unix) {
      /* Test that "uv_udp_send()" supports sending over
         a "sockaddr_un" address. */
      struct sockaddr_un addr;
      uv_udp_t handle;
      uv_udp_send_t req;
      uv_loop_t* loop;
      uv_buf_t buf = uv_buf_init("PING", 4);
      int fd;
      int r;
    
      loop = uv_default_loop();
    
      memset(&addr, 0, sizeof addr);
      addr.sun_family = AF_UNIX;
      ASSERT(strlen(TEST_PIPENAME) < sizeof(addr.sun_path));
      memcpy(addr.sun_path, TEST_PIPENAME, strlen(TEST_PIPENAME));
    
      fd = socket(AF_UNIX, SOCK_STREAM, 0);
      ASSERT(fd >= 0);
    
      unlink(TEST_PIPENAME);
      ASSERT(0 == bind(fd, (const struct sockaddr*)&addr, sizeof addr));
      ASSERT(0 == listen(fd, 1));
    
      r = uv_udp_init(loop, &handle);
      ASSERT(r == 0);
      r = uv_udp_open(&handle, fd);
      ASSERT(r == 0);
      uv_run(loop, UV_RUN_DEFAULT);
    
      r = uv_udp_send(&req,
                      &handle,
                      &buf,
                      1,
                      (const struct sockaddr*) &addr,
                      NULL);
      ASSERT(r == 0);
    
      uv_close((uv_handle_t*)&handle, NULL);
      uv_run(loop, UV_RUN_DEFAULT);
      close(fd);
      unlink(TEST_PIPENAME);
    
      MAKE_VALGRIND_HAPPY();
      return 0;
    }
    
    Signed-off-by: wangchen <wangchen41@xiaomi.com>

--- net/local/local.h
+++ net/local/local.h
@@ -122,9 +122,9 @@ struct local_conn_s
   char lc_path[UNIX_PATH_MAX];   /* Path assigned by bind() */
   int32_t lc_instance_id;        /* Connection instance ID for stream
                                   * server<->client connection pair */
-#ifdef CONFIG_NET_LOCAL_SCM
   FAR struct local_conn_s *
                         lc_peer; /* Peer connection instance */
+#ifdef CONFIG_NET_LOCAL_SCM
   uint16_t lc_cfpcount;          /* Control file pointer counter */
   FAR struct file *
      lc_cfps[LOCAL_NCONTROLFDS]; /* Socket message control filep */
