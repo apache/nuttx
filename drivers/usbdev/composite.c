a6d8460a58c3641d618d6cb6cd3daf4cce6fa3d2
zhangyuan21 <zhangyuan21@xiaomi.com>
2023-08-04 14:19:57 +0800

    composite: return ok when host send a duplicate setconfig message
    Signed-off-by: zhangyuan21 <zhangyuan21@xiaomi.com>

--- drivers/usbdev/composite.c
+++ drivers/usbdev/composite.c
@@ -579,9 +579,17 @@ static int composite_setup(FAR struct usbdevclass_driver_s *driver,
 
         case USB_REQ_SETCONFIGURATION:
           {
-            if (ctrl->type == 0 && value != priv->config)
+            if (ctrl->type == 0)
               {
                 int i;
+
+                if (priv->config == value)
+                  {
+                    /* Already configured -- Do nothing */
+
+                    ret = OK;
+                    break;
+                  }
 
                 /* Save the configuration and inform the constituent
                  * classes
