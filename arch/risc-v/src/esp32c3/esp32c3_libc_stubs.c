2c346c4c89885a99ae020a2588d2704f64646290
Petro Karashchenko <petro.karashchenko@gmail.com>
2023-08-08 01:37:16 +0300

    arch/esp32: use kernel internal API for libc stubs
    Signed-off-by: Petro Karashchenko <petro.karashchenko@gmail.com>

--- arch/risc-v/src/esp32c3/esp32c3_libc_stubs.c
+++ arch/risc-v/src/esp32c3/esp32c3_libc_stubs.c
@@ -31,9 +31,11 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <signal.h>
 
+#include <nuttx/signal.h>
 #include <nuttx/mutex.h>
+#include <nuttx/lib/lib.h>
+
 #include "rom/esp32c3_libc_stubs.h"
 
 /****************************************************************************
@@ -59,12 +61,12 @@ struct _reent;
 
 int _close_r(struct _reent *r, int fd)
 {
-  return close(fd);
+  return nx_close(fd);
 }
 
 int _fstat_r(struct _reent *r, int fd, struct stat *statbuf)
 {
-  return fstat(fd, statbuf);
+  return nx_fstat(fd, statbuf);
 }
 
 int _getpid_r(struct _reent *r)
@@ -74,11 +76,10 @@ int _getpid_r(struct _reent *r)
 
 int _kill_r(struct _reent *r, int pid, int sig)
 {
-  return kill(pid, sig);
+  return nxsig_kill(pid, sig);
 }
 
-int _link_r(struct _reent *r, const char *oldpath,
-                      const char *newpath)
+int _link_r(struct _reent *r, const char *oldpath, const char *newpath)
 {
   /* TODO */
 
@@ -87,22 +88,20 @@ int _link_r(struct _reent *r, const char *oldpath,
 
 int _lseek_r(struct _reent *r, int fd, int offset, int whence)
 {
-  return lseek(fd, offset, whence);
+  return nx_seek(fd, offset, whence);
 }
 
-int _open_r(struct _reent *r, const char *pathname,
-                      int flags, int mode)
+int _open_r(struct _reent *r, const char *pathname, int flags, int mode)
 {
-  return open(pathname, flags, mode);
+  return nx_open(pathname, flags, mode);
 }
 
 int _read_r(struct _reent *r, int fd, void *buf, int count)
 {
-  return read(fd, buf, count);
+  return nx_read(fd, buf, count);
 }
 
-int _rename_r(struct _reent *r, const char *oldpath,
-                        const char *newpath)
+int _rename_r(struct _reent *r, const char *oldpath, const char *newpath)
 {
   return rename(oldpath, newpath);
 }
@@ -112,13 +111,12 @@ void *_sbrk_r(struct _reent *r, ptrdiff_t increment)
   /* TODO: sbrk is only supported on Kernel mode */
 
   errno = -ENOMEM;
-  return (void *) -1;
+  return (void *)-1;
 }
 
-int _stat_r(struct _reent *r, const char *pathname,
-                      struct stat *statbuf)
+int _stat_r(struct _reent *r, const char *pathname, struct stat *statbuf)
 {
-  return stat(pathname, statbuf);
+  return nx_stat(pathname, statbuf, 1);
 }
 
 clock_t _times_r(struct _reent *r, struct tms *buf)
@@ -128,12 +126,12 @@ clock_t _times_r(struct _reent *r, struct tms *buf)
 
 int _unlink_r(struct _reent *r, const char *pathname)
 {
-  return unlink(pathname);
+  return nx_unlink(pathname);
 }
 
 int _write_r(struct _reent *r, int fd, const void *buf, int count)
 {
-  return write(fd, buf, count);
+  return nx_write(fd, buf, count);
 }
 
 int _gettimeofday_r(struct _reent *r, struct timeval *tv, void *tz)
@@ -143,22 +141,22 @@ int _gettimeofday_r(struct _reent *r, struct timeval *tv, void *tz)
 
 void *_malloc_r(struct _reent *r, size_t size)
 {
-  return malloc(size);
+  return lib_malloc(size);
 }
 
 void *_realloc_r(struct _reent *r, void *ptr, size_t size)
 {
-  return realloc(ptr, size);
+  return lib_realloc(ptr, size);
 }
 
 void *_calloc_r(struct _reent *r, size_t nmemb, size_t size)
 {
-  return calloc(nmemb, size);
+  return lib_calloc(nmemb, size);
 }
 
 void _free_r(struct _reent *r, void *ptr)
 {
-  free(ptr);
+  lib_free(ptr);
 }
 
 void _abort(void)
@@ -237,7 +235,7 @@ struct _reent *__getreent(void)
 {
   /* TODO */
 
-  return (struct _reent *) NULL;
+  return (struct _reent *)NULL;
 }
 
 int _system_r(struct _reent *r, const char *command)
@@ -253,42 +251,42 @@ int _system_r(struct _reent *r, const char *command)
 
 static const struct syscall_stub_table g_stub_table =
 {
-    .__getreent = &__getreent,
-    ._malloc_r = &_malloc_r,
-    ._free_r = &_free_r,
-    ._realloc_r = &_realloc_r,
-    ._calloc_r = &_calloc_r,
-    ._abort = NULL,
-    ._system_r = &_system_r,
-    ._rename_r = &_rename_r,
-    ._times_r = &_times_r,
-    ._gettimeofday_r = &_gettimeofday_r,
-    ._raise_r = &_raise_r,
-    ._unlink_r = &_unlink_r,
-    ._link_r = &_link_r,
-    ._stat_r = &_stat_r,
-    ._fstat_r = &_fstat_r,
-    ._sbrk_r = &_sbrk_r,
-    ._getpid_r = &_getpid_r,
-    ._kill_r = &_kill_r,
-    ._exit_r = NULL,
-    ._close_r = &_close_r,
-    ._open_r = &_open_r,
-    ._write_r = &_write_r,
-    ._lseek_r = &_lseek_r,
-    ._read_r = &_read_r,
-    ._lock_init = &_lock_init,
-    ._lock_init_recursive = &_lock_init_recursive,
-    ._lock_close = &_lock_close,
-    ._lock_close_recursive = &_lock_close_recursive,
-    ._lock_acquire = &_lock_acquire,
-    ._lock_acquire_recursive = &_lock_acquire_recursive,
-    ._lock_try_acquire = &_lock_try_acquire,
-    ._lock_try_acquire_recursive = &_lock_try_acquire_recursive,
-    ._lock_release = &_lock_release,
-    ._lock_release_recursive = &_lock_release_recursive,
-    ._printf_float = NULL,
-    ._scanf_float = NULL,
+  .__getreent = &__getreent,
+  ._malloc_r = &_malloc_r,
+  ._free_r = &_free_r,
+  ._realloc_r = &_realloc_r,
+  ._calloc_r = &_calloc_r,
+  ._abort = &_abort,
+  ._system_r = &_system_r,
+  ._rename_r = &_rename_r,
+  ._times_r = &_times_r,
+  ._gettimeofday_r = &_gettimeofday_r,
+  ._raise_r = &_raise_r,
+  ._unlink_r = &_unlink_r,
+  ._link_r = &_link_r,
+  ._stat_r = &_stat_r,
+  ._fstat_r = &_fstat_r,
+  ._sbrk_r = &_sbrk_r,
+  ._getpid_r = &_getpid_r,
+  ._kill_r = &_kill_r,
+  ._exit_r = NULL,
+  ._close_r = &_close_r,
+  ._open_r = &_open_r,
+  ._write_r = &_write_r,
+  ._lseek_r = &_lseek_r,
+  ._read_r = &_read_r,
+  ._lock_init = &_lock_init,
+  ._lock_init_recursive = &_lock_init_recursive,
+  ._lock_close = &_lock_close,
+  ._lock_close_recursive = &_lock_close_recursive,
+  ._lock_acquire = &_lock_acquire,
+  ._lock_acquire_recursive = &_lock_acquire_recursive,
+  ._lock_try_acquire = &_lock_try_acquire,
+  ._lock_try_acquire_recursive = &_lock_try_acquire_recursive,
+  ._lock_release = &_lock_release,
+  ._lock_release_recursive = &_lock_release_recursive,
+  ._printf_float = NULL,
+  ._scanf_float = NULL
 };
 
 /****************************************************************************
