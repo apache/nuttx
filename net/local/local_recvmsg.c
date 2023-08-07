6bbf721a042f8b3f7c33f5686d0fdd1d1b9d1d76
liqinhui <liqinhui@xiaomi.com>
2023-08-07 16:21:45 +0800

    net/local: Fix the error return length when read the bigger packet.
    Verifiction:
      Write a bigger packet and read the packet using a smaller buffer.
      The return length of reading should be the length of smaller buffer.
    
    Signed-off-by: liqinhui <liqinhui@xiaomi.com>

--- net/local/local_recvmsg.c
+++ net/local/local_recvmsg.c
@@ -398,7 +398,6 @@ psock_dgram_recvfrom(FAR struct socket *psock, FAR void *buf, size_t len,
 
           DEBUGASSERT(tmplen <= remaining);
           remaining -= tmplen;
-          readlen += tmplen;
         }
       while (remaining > 0);
     }
