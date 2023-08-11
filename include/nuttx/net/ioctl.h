9d3abe8b712e46d254326c094c012acb7c47afff
xucheng5 <xucheng5@xiaomi.com>
2023-08-11 11:14:13 +0800

    socket can : support ioctl cmd SIOCCANRECOVERY
    send CMD to restart controller in state bus-off
    
    Signed-off-by: xucheng5 <xucheng5@xiaomi.com>

--- include/nuttx/net/ioctl.h
+++ include/nuttx/net/ioctl.h
@@ -121,6 +121,7 @@
 #define SIOCDCANEXTFILTER  _SIOC(0x002F)  /* Delete hardware-level exteneded ID filter */
 #define SIOCACANSTDFILTER  _SIOC(0x0030)  /* Add hardware-level standard ID filter */
 #define SIOCDCANSTDFILTER  _SIOC(0x0031)  /* Delete hardware-level standard ID filter */
+#define SIOCCANRECOVERY    _SIOC(0x0032)  /* Recovery can, work only when bus-off state */
 
 /* Network socket control ***************************************************/
 
