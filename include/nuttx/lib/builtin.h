903e87a7bdb2ac9644ef059437e69919b9972a57
fangxinyong <fangxinyong@xiaomi.com>
2023-08-12 07:39:45 +0800

    builtin: support uid/gid config for binfs app
    Implement I_SUID/I_SGID feature for binfs in the POSIX compliant way.
    If set-user-ID bit is set in the file permissions, then the effective
    user ID of process shall be set to UID of the new process image file.
    
    test case:
    hello example emulates to set uid and file set-user-ID bit, and call
    geteuid and getegid API.
    UID  = 2000
    GID  = 3000
    MODE = 06555
    
    nsh> ls -l /bin/hello
     -r-sr-sr-x    2000    3000       0 hello
    nsh> hello
    geteuid:2000
    getegid:3000
    
    Signed-off-by: fangxinyong <fangxinyong@xiaomi.com>

--- include/nuttx/lib/builtin.h
+++ include/nuttx/lib/builtin.h
@@ -40,6 +40,11 @@ struct builtin_s
   int         priority;     /* Use: SCHED_PRIORITY_DEFAULT */
   int         stacksize;    /* Desired stack size */
   main_t      main;         /* Entry point: main(int argc, char *argv[]) */
+#ifdef CONFIG_SCHED_USER_IDENTITY
+  uid_t       uid;          /* File owner user identity */
+  gid_t       gid;          /* File owner group identity */
+  int         mode;         /* File mode added to */
+#endif
 };
 
 /****************************************************************************
@@ -170,6 +175,64 @@ FAR const char *builtin_getname(int index);
  ****************************************************************************/
 
 FAR const struct builtin_s *builtin_for_index(int index);
+
+#ifdef CONFIG_SCHED_USER_IDENTITY
+
+/****************************************************************************
+ * Name: builtin_getuid
+ *
+ * Description:
+ *   Returns file uid of the application at 'index' in the table
+ *   of built-in applications.
+ *
+ * Input Parameters:
+ *   index - From 0 and on ...
+ *
+ * Returned Value:
+ *   Returns valid uid for app if index is valid.
+ *   Otherwise 0 is returned.
+ *
+ ****************************************************************************/
+
+uid_t builtin_getuid(int index);
+
+/****************************************************************************
+ * Name: builtin_getgid
+ *
+ * Description:
+ *   Returns file gid of the application at 'index' in the table
+ *   of built-in applications.
+ *
+ * Input Parameters:
+ *   index - From 0 and on ...
+ *
+ * Returned Value:
+ *   Returns valid gid for app if index is valid.
+ *   Otherwise 0 is returned.
+ *
+ ****************************************************************************/
+
+gid_t builtin_getgid(int index);
+
+/****************************************************************************
+ * Name: builtin_getmode
+ *
+ * Description:
+ *   Returns file mode of the application at 'index' in the table
+ *   of built-in applications.
+ *
+ * Input Parameters:
+ *   index - From 0 and on ...
+ *
+ * Returned Value:
+ *   Returns valid mode for app if index is valid.
+ *   Otherwise 0 is returned.
+ *
+ ****************************************************************************/
+
+int builtin_getmode(int index);
+
+#endif
 
 #undef EXTERN
 #if defined(__cplusplus)
