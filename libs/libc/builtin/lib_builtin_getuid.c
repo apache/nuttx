f8853ed13b0d61f0696e9aa43ad2eddeb8a53116
fangxinyong <fangxinyong@xiaomi.com>
2023-08-12 07:39:45 +0800

    builtin: support uid/gid config for binfs app
    Implement I_SUID/I_SGID feature for binfs in the POSIX compliant way.
    If set-user-ID bit is set in the file permissions, then the effective
    user ID of process shall be set to UID of the new process image file.
    
    test case:
    hello example emulates to set uid and file set-user-ID bit, and call
    geteuid and getegid API.
    UID  = 2000
    GID  = 3000
    MODE = 06555
    
    nsh> ls -l /bin/hello
     -r-sr-sr-x    2000    3000       0 hello
    nsh> hello
    geteuid:2000
    getegid:3000
    
    Signed-off-by: fangxinyong <fangxinyong@xiaomi.com>

---
+++ libs/libc/builtin/lib_builtin_getuid.c
@@ -0,0 +1,63 @@
+/****************************************************************************
+ * libs/libc/builtin/lib_builtin_getuid.c
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+#include <nuttx/lib/builtin.h>
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: builtin_getuid
+ *
+ * Description:
+ *   Returns file uid of the application at 'index' in the table
+ *   of built-in applications.
+ *
+ * Input Parameters:
+ *   index - From 0 and on ...
+ *
+ * Returned Value:
+ *   Returns valid uid for app if index is valid.
+ *   Otherwise 0 is returned.
+ *
+ ****************************************************************************/
+
+uid_t builtin_getuid(int index)
+{
+  FAR const struct builtin_s *builtin;
+
+  builtin = builtin_for_index(index);
+
+  if (builtin != NULL)
+    {
+      return builtin->uid;
+    }
+
+  /* Return user identity 'root' with a uid value of 0. */
+
+  return 0;
+}
