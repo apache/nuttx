b8a41a99723cffb9a6d16285e07e92021ee63b69
guoshichao <guoshichao@xiaomi.com>
2023-08-15 15:53:04 +0800

    arch/arm/qemu_idle: add qemu_idle implementation
    the qemu_idle implement up_idle() method, inside the up_idle()
    method will invoke sm_switch_nsec() function to implement
    boot the ap core from tee core
    
    Signed-off-by: guoshichao <guoshichao@xiaomi.com>

---
+++ arch/arm/src/qemu/qemu_idle.c
@@ -0,0 +1,61 @@
+/****************************************************************************
+ * arch/arm/src/qemu/qemu_idle.c
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/arch.h>
+#ifdef CONFIG_ARCH_TRUSTZONE_SECURE
+#include "sm.h"
+#endif
+
+/****************************************************************************
+ * Private Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: up_idle
+ *
+ * Description:
+ *   up_idle() is the logic that will be executed when their is no other
+ *   ready-to-run task.  This is processor idle time and will continue until
+ *   some interrupt occurs to cause a context switch from the idle task.
+ *
+ *   Processing in this state may be processor-specific. e.g., this is where
+ *   power management operations might be performed.
+ *
+ ****************************************************************************/
+
+void up_idle(void)
+{
+#ifdef CONFIG_ARCH_TRUSTZONE_SECURE
+  sm_switch_nsec();
+#else
+
+  /* Sleep until an interrupt occurs to save power */
+
+  asm("WFI");
+#endif
+}
