5fe40614a7135a3f5a1496f5e19733caaeff36bb
liaoao <liaoao@xiaomi.com>
2023-08-17 18:30:59 +0800

    qemu:fdt:add device tree support for goldfish arm/arm64
    Signed-off-by: liaoao <liaoao@xiaomi.com>

--- arch/arm/src/goldfish/goldfish_boot.c
+++ arch/arm/src/goldfish/goldfish_boot.c
@@ -29,6 +29,10 @@
 #include "goldfish_irq.h"
 #include "goldfish_memorymap.h"
 
+#ifdef CONFIG_DEVICE_TREE
+#  include <nuttx/fdt.h>
+#endif
+
 /****************************************************************************
  * Public Functions
  ****************************************************************************/
@@ -46,6 +50,10 @@ void arm_boot(void)
   /* Set the page table for section */
 
   goldfish_setupmappings();
+
+#ifdef CONFIG_DEVICE_TREE
+  fdt_register((FAR const char *)0x40000000);
+#endif
 
 #ifdef USE_EARLYSERIALINIT
   /* Perform early serial initialization if we are going to use the serial
