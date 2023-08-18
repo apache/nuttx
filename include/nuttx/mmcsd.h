b0c9cde1970180ceebb39e915490ca8d0ff16aba
wanggang26 <wanggang26@xiaomi.com>
2023-08-18 18:54:33 +0800

    drivers/mmcsd: Add RPMB ioctl
    Signed-off-by: wanggang26 <wanggang26@xiaomi.com>

--- include/nuttx/mmcsd.h
+++ include/nuttx/mmcsd.h
@@ -49,6 +49,7 @@
 #define MMC_RPMB_READ_CNT       0x02
 #define MMC_RPMB_WRITE          0x03
 #define MMC_RPMB_READ           0x04
+#define MMC_RPMB_READ_RESP      0x05
 
 /****************************************************************************
  * Public Types
