4e4db63a4baed4bc3d829fe62fc01c06eef01b85
yinshengkai <yinshengkai@xiaomi.com>
2023-08-21 11:33:01 +0800

    drivers/serial: unmodify head and tail pointers
    Signed-off-by: yinshengkai <yinshengkai@xiaomi.com>

--- drivers/serial/serial_dma.c
+++ drivers/serial/serial_dma.c
@@ -188,14 +188,6 @@ void uart_recvchars_dma(FAR uart_dev_t *dev)
   bool is_full;
   int nexthead;
 
-  /* If RX buffer is empty move tail and head to zero position */
-
-  if (rxbuf->head == rxbuf->tail)
-    {
-      rxbuf->head = 0;
-      rxbuf->tail = 0;
-    }
-
   /* Get the next head index and check if there is room to adding another
    * byte to the buffer.
    */
