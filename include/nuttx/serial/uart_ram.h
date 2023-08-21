64b216dd8fa53637ebb74b09ba95e175d2640c7e
yinshengkai <yinshengkai@xiaomi.com>
2023-08-21 11:33:01 +0800

    drivers/serial: add ram uart driver
    It uses the memory block as the serial communication medium, which can communicate between different processes or different CPUs
    
    Using the following configuration, the cross-core communication rate of 200MHz cortex-M55 is about 461KB/s
    RAM_UART_BUFSIZE=1024
    RAM_UART_POLLING_INTERVAL=100
    
    Signed-off-by: yinshengkai <yinshengkai@xiaomi.com>

---
+++ include/nuttx/serial/uart_ram.h
@@ -0,0 +1,82 @@
+/****************************************************************************
+ * include/nuttx/serial/uart_ram.h
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+#ifndef __INCLUDE_NUTTX_SERIAL_UART_RAM_H
+#define __INCLUDE_NUTTX_SERIAL_UART_RAM_H
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+#ifdef CONFIG_RAM_UART
+
+#include <stdatomic.h>
+#include <stdbool.h>
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Type Declarations
+ ****************************************************************************/
+
+struct uart_rambuf_s
+{
+    char buffer[CONFIG_RAM_UART_BUFSIZE];
+    atomic_uint wroff;
+    atomic_uint rdoff;
+};
+
+/****************************************************************************
+ * Public Data
+ ****************************************************************************/
+
+/****************************************************************************
+ * Public Function Prototypes
+ ****************************************************************************/
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/****************************************************************************
+ * Name: uart_ram_register
+ ****************************************************************************/
+
+int uart_ram_register(FAR const char *devname,
+                      FAR struct uart_rambuf_s buffer[2], bool slave);
+
+/****************************************************************************
+ * Name: ram_serialinit
+ ****************************************************************************/
+
+void ram_serialinit(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
+#endif
