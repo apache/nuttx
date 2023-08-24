1b195907d18873edc2e2bcc260089fd8bdc5faf4
cuiziwei <cuiziwei@xiaomi.com>
2023-08-24 17:12:13 +0800

    nuttx/sim:add up_irq_enbale function.
    Signed-off-by: cuiziwei <cuiziwei@xiaomi.com>

--- arch/sim/include/irq.h
+++ arch/sim/include/irq.h
@@ -118,6 +118,7 @@ int up_cpu_index(void);
 irqstate_t up_irq_flags(void);
 irqstate_t up_irq_save(void);
 void up_irq_restore(irqstate_t flags);
+void up_irq_enable(void);
 
 /****************************************************************************
  * Inline functions
