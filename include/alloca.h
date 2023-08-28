b0b85f65da0ba96358240eeb1ad75646c288290b
chao an <anchao@xiaomi.com>
2023-08-28 11:38:29 +0800

    libs/alloca: add alloca implement for MSVC
    Reference:
    https://learn.microsoft.com/en-us/cpp/c-runtime-library/reference/alloca?view=msvc-170
    
    Signed-off-by: chao an <anchao@xiaomi.com>

--- include/alloca.h
+++ include/alloca.h
@@ -32,6 +32,11 @@
 #  undef  __alloca
 #  define alloca(size)   __alloca(size)
 #  define __alloca(size) __builtin_alloca(size)
+#elif defined(_MSC_VER)
+#  undef  alloca
+#  undef  __alloca
+#  define alloca(size)   _alloca(size)
+#  define __alloca(size) _alloca(size)
 #endif
 
 #endif /* __INCLUDE_ALLOCA_H */
