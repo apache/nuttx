b446a002dbcc18bf82e96893f6cc7d97b19c5e0f
wangchen <wangchen41@xiaomi.com>
2023-08-29 18:02:32 +0800

    net:add customizable default max & min port add customizable default max & min port
    Signed-off-by: wangchen <wangchen41@xiaomi.com>

--- net/nat/ipv4_nat_entry.c
+++ net/nat/ipv4_nat_entry.c
@@ -41,15 +41,6 @@
 #if defined(CONFIG_NET_NAT) && defined(CONFIG_NET_IPv4)
 
 /****************************************************************************
- * Pre-processor Definitions
- ****************************************************************************/
-
-/* TODO: Why we limit to 32000 in net stack? */
-
-#define NAT_PORT_REASSIGN_MAX 32000
-#define NAT_PORT_REASSIGN_MIN 4096
-
-/****************************************************************************
  * Private Data
  ****************************************************************************/
 
@@ -119,9 +110,9 @@ static uint16_t ipv4_nat_select_port_without_stack(
   uint16_t hport = NTOHS(portno);
   while (ipv4_nat_port_inuse(protocol, ip, portno))
     {
-      if (++hport >= NAT_PORT_REASSIGN_MAX)
+      if (++hport >= CONFIG_NET_DEFAULT_MAX_PORT)
         {
-          hport = NAT_PORT_REASSIGN_MIN;
+          hport = CONFIG_NET_DEFAULT_MIN_PORT;
         }
 
       portno = HTONS(hport);
@@ -209,9 +200,9 @@ static uint16_t ipv4_nat_select_port(FAR struct net_driver_s *dev,
           while (icmp_findconn(dev, id) ||
                  ipv4_nat_port_inuse(IP_PROTO_ICMP, dev->d_ipaddr, id))
             {
-              if (++hid >= NAT_PORT_REASSIGN_MAX)
+              if (++hid >= CONFIG_NET_DEFAULT_MAX_PORT)
                 {
-                  hid = NAT_PORT_REASSIGN_MIN;
+                  hid = CONFIG_NET_DEFAULT_MIN_PORT;
                 }
 
               id = HTONS(hid);
