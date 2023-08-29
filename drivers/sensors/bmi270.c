60364d7c72d80aa9a91211aeafd0bd312e3b1dd4
SPRESENSE <41312067+SPRESENSE@users.noreply.github.com>
2023-08-29 19:55:23 +0900

    drivers/sensors/bmi270: Enable to select config memory
    Allow to transfer the BMI270 configuration data directly, and
    provide an option to use the heap memory if it cannot transfer
    directly.

--- drivers/sensors/bmi270.c
+++ drivers/sensors/bmi270.c
@@ -1258,9 +1258,11 @@ static int bmi270_init_seq(FAR struct bmi270_dev_s *priv)
 
   bmi270_putreg8(priv, BMI270_INIT_CTRL, 0);
 
+#ifdef CONFIG_SENSORS_BMI270_LOAD_FROM_HEAP
+
   /* Copy configuration to RAM */
 
-  tmp = malloc(sizeof(g_bmi270_config_file));
+  tmp = kmm_malloc(sizeof(g_bmi270_config_file));
   if (tmp == NULL)
     {
       snerr("Failed to allocate memory for configuration file\n");
@@ -1269,11 +1271,23 @@ static int bmi270_init_seq(FAR struct bmi270_dev_s *priv)
 
   memcpy(tmp, g_bmi270_config_file, sizeof(g_bmi270_config_file));
 
+#else
+
+  /* Transfer directly from const data memory */
+
+  tmp = (FAR uint8_t *)&g_bmi270_config_file;
+
+#endif
+
   /* Load configuration - start with byte 0 */
 
   bmi270_putregs(priv, BMI270_INIT_DATA,
                  tmp,
                  sizeof(g_bmi270_config_file));
+
+#ifdef CONFIG_SENSORS_BMI270_LOAD_FROM_HEAP
+  kmm_free(tmp);
+#endif
 
   /* Complete config load INIT_CTRL=0x01 */
 
