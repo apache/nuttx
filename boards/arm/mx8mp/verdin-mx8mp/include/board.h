56873a4f87b1a43ce1cc4b06b46f30cc16cf9c1c
Philippe Leduc <philippe.leduc@wandercraft.eu>
2023-08-31 17:17:01 +0200

    Add GPIO Add userleds

--- boards/arm/mx8mp/verdin-mx8mp/include/board.h
+++ boards/arm/mx8mp/verdin-mx8mp/include/board.h
@@ -36,8 +36,47 @@
  * Pre-processor Definitions
  ****************************************************************************/
 
-#define IOMUX_CONSOLE_UART_RX IOMUXC_UART4_RXD_UART4_RX, 0, IOMUXC_SW_PAD_CTL_PAD_PUE_MASK | IOMUXC_SW_PAD_CTL_PAD_PE_MASK
-#define IOMUX_CONSOLE_UART_TX IOMUXC_UART4_TXD_UART4_TX, 0, IOMUXC_SW_PAD_CTL_PAD_PUE_MASK | IOMUXC_SW_PAD_CTL_PAD_PE_MASK
+/* Console UART IOMUX configuration */
+
+#define IOMUX_CONSOLE_UART_RX IOMUXC_UART4_RXD_UART4_RX, 0, UART_PAD_CTRL
+#define IOMUX_CONSOLE_UART_TX IOMUXC_UART4_TXD_UART4_TX, 0, UART_PAD_CTRL
+
+/* LED definitions **********************************************************/
+
+/* The Freedom K66F has a single RGB LED driven by the K66F as follows:
+
+/* LED index values for use with board_userled() */
+
+#define BOARD_LED_1       0
+#define BOARD_LED_2       1
+#define BOARD_LED_3       2
+#define BOARD_LED_4       3
+#define BOARD_NLEDS       4
+
+/* LED bits for use with board_userled_all() */
+
+#define BOARD_LED_1_BIT   (1 << BOARD_LED_1)
+#define BOARD_LED_2_BIT   (1 << BOARD_LED_2)
+#define BOARD_LED_3_BIT   (1 << BOARD_LED_3)
+#define BOARD_LED_4_BIT   (1 << BOARD_LED_4)
+
+/* If CONFIG_ARCH_LEDs is defined, then NuttX will control the LED on board.
+ * The following definitions describe how NuttX controls
+ * the LEDs:
+ *
+ *   SYMBOL                Meaning                      LED
+ *   -------------------  ----------------------------  --------------------
+ */
+
+#define LED_STARTED       1 /* NuttX has been started    None  */
+#define LED_HEAPALLOCATE  2 /* Heap has been allocated   ON(1) */
+#define LED_IRQSENABLED   0 /* Interrupts enabled        ON(1) */
+#define LED_STACKCREATED  3 /* Idle stack created        ON(2) */
+#define LED_INIRQ         0 /* In an interrupt          (no change)  */
+#define LED_SIGNAL        0 /* In a signal handler      (no change)  */
+#define LED_ASSERTION     0 /* An assertion failed      (no change)  */
+#define LED_PANIC         5 /* The system has crashed    FLASH(3)    */
+#undef  LED_IDLE          4 /* idle loop                 FLASH(4)    */
 
 /****************************************************************************
  * Public Data
