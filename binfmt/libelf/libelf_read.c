2d5f4a88890bf280f71db46838a0c47d00c16b0a
Masayuki Ishikawa <masayuki.ishikawa@gmail.com>
2023-09-04 14:34:04 +0900

    Revert "binfmt: Support arch copy section by self for dynamic code loading"
    This reverts commit 8a13da322deb2d5372aabf824b0c910a4e7fcf33.

--- binfmt/libelf/libelf_read.c
+++ binfmt/libelf/libelf_read.c
@@ -98,23 +98,9 @@ static inline void elf_dumpreaddata(FAR char *buffer, size_t buflen)
 int elf_read(FAR struct elf_loadinfo_s *loadinfo, FAR uint8_t *buffer,
              size_t readsize, off_t offset)
 {
-  size_t  nsize = readsize;   /* Bytes to read from the object file */
-  ssize_t nbytes;             /* Number of bytes read */
-  off_t   rpos;               /* Position returned by lseek */
-  int     ret = OK;           /* Return value */
-
-#ifdef CONFIG_ARCH_USE_COPY_SECTION
-  FAR uint8_t *dest = buffer; /* Destination address - `buffer` */
-
-  /* Redirect `buffer` to temporary allocated memory */
-
-  buffer = kmm_malloc(readsize);
-  if (buffer == NULL)
-    {
-      berr("ERROR: Failed to allocate memory\n");
-      return -ENOMEM;
-    }
-#endif
+  size_t  nsize = readsize;
+  ssize_t nbytes;      /* Number of bytes read */
+  off_t   rpos;        /* Position returned by lseek */
 
   binfo("Read %zu bytes from offset %" PRIdOFF "\n", readsize, offset);
 
@@ -129,8 +115,7 @@ int elf_read(FAR struct elf_loadinfo_s *loadinfo, FAR uint8_t *buffer,
         {
           berr("Failed to seek to position %" PRIdOFF ": %" PRIdOFF "\n",
                offset, rpos);
-          ret = rpos;
-          goto errout;
+          return rpos;
         }
 
       /* Read the file data at offset into the user buffer */
@@ -145,15 +130,13 @@ int elf_read(FAR struct elf_loadinfo_s *loadinfo, FAR uint8_t *buffer,
             {
               berr("Read from offset %" PRIdOFF " failed: %zd\n",
                    offset, nbytes);
-              ret = nbytes;
-              goto errout;
+              return nbytes;
             }
         }
       else if (nbytes == 0)
         {
           berr("Unexpected end of file\n");
-          ret = -ENODATA;
-          goto errout;
+          return -ENODATA;
         }
       else
         {
@@ -162,25 +145,6 @@ int elf_read(FAR struct elf_loadinfo_s *loadinfo, FAR uint8_t *buffer,
         }
     }
 
-#ifdef CONFIG_ARCH_USE_COPY_SECTION
-  /* Copy the requested data from temporary memory to destination */
-
-  ret = up_copy_section(dest, buffer, nsize);
-  if (ret < 0)
-    {
-      berr("ERROR: Failed to copy section at offset %"PRIdOFF"\n", offset);
-      goto errout;
-    }
-#endif
-
   elf_dumpreaddata(buffer, nsize);
-
-errout:
-#ifdef CONFIG_ARCH_USE_COPY_SECTION
-  /* Free the temporary memory */
-
-  kmm_free(buffer);
-#endif
-
-  return ret;
+  return OK;
 }
