14ce8947a9d92b819cc98103bda8eeec850d1d80
hujun5 <hujun5@xiaomi.com>
2023-09-07 09:02:04 +0800

    sam_hsmci: rm sched_[un]lock
    Signed-off-by: hujun5 <hujun5@xiaomi.com>

--- arch/arm/src/samv7/sam_hsmci.c
+++ arch/arm/src/samv7/sam_hsmci.c
@@ -2331,7 +2331,6 @@ static int sam_sendsetup(struct sdio_dev_s *dev,
    * in order to avoid a TX data underrun.
    */
 
-  sched_lock();
   flags = enter_critical_section();
 
   src       = (const uint32_t *)buffer;
@@ -2348,7 +2347,6 @@ static int sam_sendsetup(struct sdio_dev_s *dev,
 
           mcerr("ERROR: sr %08" PRIx32 "\n", sr);
           leave_critical_section(flags);
-          sched_unlock();
           return -EIO;
         }
       else if ((sr & HSMCI_INT_TXRDY) != 0)
@@ -2406,7 +2404,6 @@ static int sam_sendsetup(struct sdio_dev_s *dev,
     }
 
   leave_critical_section(flags);
-  sched_unlock();
   return OK;
 }
 
