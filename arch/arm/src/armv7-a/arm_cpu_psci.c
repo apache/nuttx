0778acf3d1c34b23bb3337a6c7cc3c28dffafada
wangming9 <wangming9@xiaomi.com>
2023-09-13 17:47:33 +0800

    arm/psci: Fixed arm psci related compilation errors
    Summary
    include/arch/syscall.h: Assembler messages:
    include/arch/syscall.h:133: Error: bad instruction `struct arm_smccc_res'
    include/arch/syscall.h:134: Error: junk at end of line, first unrecognized character is `{'
    include/arch/syscall.h:135: Error: bad instruction `unsigned long a0'
    include/arch/syscall.h:136: Error: bad instruction `unsigned long a1'
    include/arch/syscall.h:137: Error: bad instruction `unsigned long a2'
    include/arch/syscall.h:138: Error: bad instruction `unsigned long a3'
    include/arch/syscall.h:139: Error: bad instruction `unsigned long a4'
    include/arch/syscall.h:140: Error: bad instruction `unsigned long a5'
    include/arch/syscall.h:141: Error: bad instruction `unsigned long a6'
    include/arch/syscall.h:142: Error: bad instruction `unsigned long a7'
    include/arch/syscall.h:143: Error: junk at end of line, first unrecognized character is `}'
    include/arch/syscall.h:145: Error: bad instruction `typedef struct arm_smccc_res
    
    Signed-off-by: wangming9 <wangming9@xiaomi.com>

--- arch/arm/src/armv7-a/arm_cpu_psci.c
+++ arch/arm/src/armv7-a/arm_cpu_psci.c
@@ -1,5 +1,5 @@
 /***************************************************************************
- * arch/arm/src/common/arm_cpu_psci.c
+ * arch/arm/src/armv7-a/arm_cpu_psci.c
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
