4ed48da792d69f1debee7cd36bc555a936ad216b
Petro Karashchenko <petro.karashchenko@gmail.com>
2023-09-15 22:12:37 +0300

    esp32: use ESP32 specific macro for register values extraction
    Signed-off-by: Petro Karashchenko <petro.karashchenko@gmail.com>

--- arch/risc-v/src/esp32c3/esp32c3_serial.c
+++ arch/risc-v/src/esp32c3/esp32c3_serial.c
@@ -663,9 +663,9 @@ static bool esp32c3_txempty(struct uart_dev_s *dev)
   struct esp32c3_uart_s *priv = dev->priv;
 
   reg = getreg32(UART_INT_RAW_REG(priv->id));
-  reg = reg & UART_TXFIFO_EMPTY_INT_RAW_M;
+  reg = REG_MASK(reg, UART_TX_DONE_INT_RAW);
 
-  return (reg > 0) ? true : false;
+  return reg > 0;
 }
 
 /****************************************************************************
