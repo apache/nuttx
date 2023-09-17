2e9ee0e9b08b16228d02fb20232191eff3027746
guoshichao <guoshichao@xiaomi.com>
2023-09-17 11:18:04 +0800

    libs/libc/wchar: add wcsspn implementation
    Signed-off-by: guoshichao <guoshichao@xiaomi.com>

---
+++ libs/libc/wchar/lib_wcsspn.c
@@ -0,0 +1,58 @@
+/****************************************************************************
+ * libs/libc/wchar/lib_wcsspn.c
+ *
+ * Copyright © 2005-2014 Rich Felker, et al.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <wchar.h>
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name:  wcsspn
+ *
+ * Description:
+ *    Get span of character set in wide string
+ *
+ * Input Parameters:
+ *   s - the wide string to be scanned
+ *   c - wide string containing the characters to match
+ *
+ * Returned Value:
+ *   The length of the initial portion of "s" which consists only of wide
+ *   characters that are part of "c"
+ *
+ ****************************************************************************/
+
+size_t wcsspn(FAR const wchar_t *s, FAR const wchar_t *c)
+{
+  FAR const wchar_t *a;
+  for (a = s; *s && wcschr(c, *s); s++);
+  return s - a;
+}
