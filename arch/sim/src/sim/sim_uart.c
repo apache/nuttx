LingaoM/incubator-nuttx

arch/sim: add sim uart_ram support
VELAPLATFO-10757

Change-Id: Ib7b868b9fd6e9a50adced72f935c2a9455df91f3
Signed-off-by: yinshengkai <yinshengkai@xiaomi.com>

arch/sim/src/sim/sim_uart.c
@@ -25,7 +25,9 @@
 #include <nuttx/config.h>
 #include <nuttx/serial/serial.h>
 #include <nuttx/fs/ioctl.h>
+#include <nuttx/serial/uart_ram.h>
 #include <nuttx/wqueue.h>
+#include <string.h>
 #include <sys/types.h>
 #include <fcntl.h>
 #include <errno.h>
@@ -663,6 +665,21 @@ static bool tty_txempty(struct uart_dev_s *dev)
 }
 #endif
 
+#ifdef CONFIG_SIM_RAM_UART
+static int sim_uartram_register(FAR const char *devname, bool slave)
+{
+  char name[NAME_MAX];
+  FAR struct uart_rambuf_s *shmem;
+
+  strlcpy(name, strrchr(devname, '/') + 1, NAME_MAX);
+  shmem = host_allocshmem(name, sizeof(struct uart_rambuf_s) * 2, !slave);
+  DEBUGASSERT(shmem);
+
+  memset(shmem, 0, sizeof(struct uart_rambuf_s) * 2);
+  return uart_ram_register(devname, shmem, slave);
+}
+#endif
+
 /****************************************************************************
  * Public Functions
  ****************************************************************************/
@@ -673,6 +690,30 @@ static bool tty_txempty(struct uart_dev_s *dev)
 
 void sim_uartinit(void)
 {
+#ifdef CONFIG_SIM_RAM_UART0
+#  ifdef CONFIG_SIM_RAM_UART0_SLAVE
+  sim_uartram_register("/dev/tty0", true);
+#  else
+  sim_uartram_register("/dev/tty0", false);
+#  endif
+#endif
+
+#ifdef CONFIG_SIM_RAM_UART1
+#  ifdef CONFIG_SIM_RAM_UART1_SLAVE
+  sim_uartram_register("/dev/tty1", true);
+#  else
+  sim_uartram_register("/dev/tty1", false);
+#  endif
+#endif
+
+#ifdef CONFIG_SIM_RAM_UART2
+#  ifdef CONFIG_SIM_RAM_UART2_SLAVE
+  sim_uartram_register("/dev/tty2", true);
+#  else
+  sim_uartram_register("/dev/tty2", false);
+#  endif
+#endif
+
 #ifdef USE_DEVCONSOLE
   /* Start the simulated UART device */
 

apache/nuttx

arch/sim/src/sim/sim_uart.c
@@ -25,7 +25,9 @@
 #include <nuttx/config.h>
 #include <nuttx/serial/serial.h>
 #include <nuttx/fs/ioctl.h>
+#include <nuttx/serial/uart_ram.h>
 #include <nuttx/wqueue.h>
+#include <string.h>
 #include <sys/types.h>
 #include <fcntl.h>
 #include <errno.h>
@@ -666,6 +668,21 @@ static bool tty_txempty(struct uart_dev_s *dev)
 }
 #endif
 
+#ifdef CONFIG_SIM_RAM_UART
+static int sim_uartram_register(FAR const char *devname, bool slave)
+{
+  char name[NAME_MAX];
+  FAR struct uart_rambuf_s *shmem;
+
+  strlcpy(name, strrchr(devname, '/') + 1, NAME_MAX);
+  shmem = host_allocshmem(name, sizeof(struct uart_rambuf_s) * 2, !slave);
+  DEBUGASSERT(shmem);
+
+  memset(shmem, 0, sizeof(struct uart_rambuf_s) * 2);
+  return uart_ram_register(devname, shmem, slave);
+}
+#endif
+
 /****************************************************************************
  * Public Functions
  ****************************************************************************/
@@ -676,6 +693,30 @@ static bool tty_txempty(struct uart_dev_s *dev)
 
 void sim_uartinit(void)
 {
+#ifdef CONFIG_SIM_RAM_UART0
+#  ifdef CONFIG_SIM_RAM_UART0_SLAVE
+  sim_uartram_register("/dev/ttyVS0", true);
+#  else
+  sim_uartram_register("/dev/ttyVS0", false);
+#  endif
+#endif
+
+#ifdef CONFIG_SIM_RAM_UART1
+#  ifdef CONFIG_SIM_RAM_UART1_SLAVE
+  sim_uartram_register("/dev/ttyVS1", true);
+#  else
+  sim_uartram_register("/dev/ttyVS1", false);
+#  endif
+#endif
+
+#ifdef CONFIG_SIM_RAM_UART2
+#  ifdef CONFIG_SIM_RAM_UART2_SLAVE
+  sim_uartram_register("/dev/ttyVS2", true);
+#  else
+  sim_uartram_register("/dev/ttyVS2", false);
+#  endif
+#endif
+
 #ifdef USE_DEVCONSOLE
   /* Start the simulated UART device */
 