c45c43c7af78f0e1e78a95cb539e802035cd31e7
Theo <theo.martin@wandercraft.eu>
2023-09-22 15:34:30 +0200

    Add activation of SPI0 on arduino due

---
+++ boards/arm/sam34/arduino-due/src/sam_spidev.c
@@ -0,0 +1,46 @@
+/****************************************************************************
+ * boards/arm/sam34/arduino-due/src/sam_spidev.c
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <debug.h>
+#include <arch/board/board.h>
+
+#include "sam_spi.h"
+#include "hardware/sam3x_pinmap.h"
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+void sam_spi0select(uint32_t devid, bool selected)
+{
+  spiinfo("devid: %08x CS: %s\n",
+          (unsigned int)devid, selected ? "assert" : "de-assert");
+  sam_gpiowrite(GPIO_SPI0_CS, !selected);
+}
+
+uint8_t sam_spi0status(struct spi_dev_s *dev, uint32_t devid)
+{
+  spiinfo("Returning nothing\n");
+  return 0;
+}
