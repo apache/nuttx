c690348597ac8177c119708bd7a9b8f7e15ab25f
raiden00pl <raiden00@railab.me>
2023-09-28 09:54:28 +0200

    arch/{all stm32 | all nordic | at32}: simplify the enable condition for up_perf_init

--- arch/arm/src/nrf52/nrf52_start.c
+++ arch/arm/src/nrf52/nrf52_start.c
@@ -203,7 +203,7 @@ void __start(void)
   nrf52_enable_profile(true);
 #endif
 
-#if defined(CONFIG_SCHED_IRQMONITOR) || defined(CONFIG_SEGGER_SYSVIEW)
+#ifdef CONFIG_ARCH_PERF_EVENTS
   up_perf_init((void *)BOARD_SYSTICK_CLOCK);
 #endif
 
