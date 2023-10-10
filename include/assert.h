64176214a0e8bafb2bbda091a6865cf757c2a7f7
guoshichao <guoshichao@xiaomi.com>
2023-10-10 19:49:49 +0800

    assert: add compile_assert macro
    Signed-off-by: guoshichao <guoshichao@xiaomi.com>

--- include/assert.h
+++ include/assert.h
@@ -152,6 +152,8 @@
 #  endif
 #endif
 
+#define COMPILE_TIME_ASSERT(x) static_assert(x, "compile time assert failed")
+
 /* Force a compilation error if condition is true, but also produce a
  * result (of value 0 and type int), so the expression can be used
  * e.g. in a structure initializer (or where-ever else comma expressions
