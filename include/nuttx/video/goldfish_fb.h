2a92748c421fe8dcb6eae0d34fee2630c54a92c8
jianglianfang <jianglianfang@xiaomi.com>
2023-10-10 16:28:56 +0800

    drivers/goldfish_fb: optimize goldfish fb register
    The Goldfish FB register should be optimized by considering the need to pass in parameters for base and irq.
    
    Signed-off-by: jianglianfang <jianglianfang@xiaomi.com>

---
+++ include/nuttx/video/goldfish_fb.h
@@ -0,0 +1,63 @@
+/****************************************************************************
+ * include/nuttx/video/goldfish_fb.h
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+#ifndef __INCLUDE_NUTTX_VIDEO_GOLDFISH_FB_H
+#define __INCLUDE_NUTTX_VIDEO_GOLDFISH_FB_H
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Public Types
+ ****************************************************************************/
+
+/****************************************************************************
+ * Public Data
+ ****************************************************************************/
+
+#ifdef __cplusplus
+#define EXTERN extern "C"
+extern "C"
+{
+#else
+#define EXTERN extern
+#endif
+
+/****************************************************************************
+ * Public Function Prototypes
+ ****************************************************************************/
+
+#ifdef CONFIG_GOLDFISH_FB
+int goldfish_fb_register(int display, FAR void *regs, int irq);
+#endif
+
+#undef EXTERN
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __INCLUDE_NUTTX_VIDEO_GOLDFISH_FB_H */
