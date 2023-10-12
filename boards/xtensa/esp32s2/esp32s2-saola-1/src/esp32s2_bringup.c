7cb0d4270451ed82da312e1677a1290378df1425
Eren Terzioglu <eren.terzioglu@espressif.com>
2023-10-12 18:45:28 +0300

    xtensa/esp32s2: Add SPI slave support

--- boards/xtensa/esp32s2/esp32s2-saola-1/src/esp32s2_bringup.c
+++ boards/xtensa/esp32s2/esp32s2-saola-1/src/esp32s2_bringup.c
@@ -74,6 +74,16 @@
 #  include "esp32s2_max6675.h"
 #endif
 
+#ifdef CONFIG_SPI_DRIVER
+#  include "esp32s2_spi.h"
+#  include "esp32s2_board_spidev.h"
+#endif
+
+#ifdef CONFIG_SPI_SLAVE_DRIVER
+#  include "esp32s2_spi.h"
+#  include "esp32s2_board_spislavedev.h"
+#endif
+
 #include "esp32s2-saola-1.h"
 
 /****************************************************************************
@@ -159,6 +169,33 @@ int esp32s2_bringup(void)
     {
       syslog(LOG_ERR, "Failed to initialize GPIO Driver: %d\n", ret);
       return ret;
+    }
+#endif
+
+#ifdef CONFIG_ESP32S2_SPI2
+# ifdef CONFIG_SPI_DRIVER
+  ret = board_spidev_initialize(ESP32S2_SPI2);
+  if (ret < 0)
+    {
+      syslog(LOG_ERR, "Failed to initialize SPI%d driver: %d\n",
+             ESP32S2_SPI2, ret);
+    }
+# elif defined(CONFIG_SPI_SLAVE_DRIVER) && defined(CONFIG_ESP32S2_SPI2_SLAVE)
+  ret = board_spislavedev_initialize(ESP32S2_SPI2);
+  if (ret < 0)
+    {
+      syslog(LOG_ERR, "Failed to initialize SPI%d Slave driver: %d\n",
+              ESP32S2_SPI2, ret);
+    }
+# endif
+#endif
+
+#if defined(CONFIG_SPI_SLAVE_DRIVER) && defined(CONFIG_ESP32S2_SPI3_SLAVE)
+  ret = board_spislavedev_initialize(ESP32S2_SPI3);
+  if (ret < 0)
+    {
+      syslog(LOG_ERR, "Failed to initialize SPI%d Slave driver: %d\n",
+              ESP32S2_SPI3, ret);
     }
 #endif
 
