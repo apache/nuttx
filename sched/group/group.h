4f998c2d862858d5ab1cd53320dbd7323bc15677
Xiang Xiao <xiaoxiang@xiaomi.com>
2023-10-15 14:28:39 +0800

    stdio: Initialize stdin, stdout and stderr directly
    and then remove group_setupstreams
    
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- sched/group/group.h
+++ sched/group/group.h
@@ -121,8 +121,5 @@ void group_remove_children(FAR struct task_group_s *group);
 
 int  group_setupidlefiles(FAR struct task_tcb_s *tcb);
 int  group_setuptaskfiles(FAR struct task_tcb_s *tcb);
-#ifdef CONFIG_FILE_STREAM
-int  group_setupstreams(FAR struct task_tcb_s *tcb);
-#endif
 
 #endif /* __SCHED_GROUP_GROUP_H */
