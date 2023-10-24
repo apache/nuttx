3348b472167167fb33d203ddac38e4b4d5caf199
Michal Lenc <michallenc@seznam.cz>
2023-10-24 11:50:10 +0200

    ci: add fmemopen test and merge fopencookie and fmemopen to stdio test
    CI test for fmemopen() function was added. The test was merged with
    fopencookie interface test and merged into single test_stdio.
    
    Signed-off-by: Michal Lenc <michallenc@seznam.cz>

--- tools/ci/testrun/script/test_os/test_os.py
+++ tools/ci/testrun/script/test_os/test_os.py
@@ -39,10 +39,12 @@ def test_getprime(p):
     assert ret == 0
 
 
-def test_fopencookie(p):
+def test_stdio(p):
     if p.board in do_not_support:
         pytest.skip("unsupported at {}".format(p.board))
     ret = p.sendCommand("fopencookie_test", "fopencokie tests were succesfull.")
+    assert ret == 0
+    ret = p.sendCommand("fmemopen_test", "FAILED: 0")
     assert ret == 0
 
 
