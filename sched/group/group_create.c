87d6084f3164174b352a0889bea55298882b84aa
chao an <anchao@xiaomi.com>
2023-10-24 22:27:33 +0800

    sched: fix the minor style issue
    and remove the unused return value
    
    Signed-off-by: chao an <anchao@xiaomi.com>

--- sched/group/group_create.c
+++ sched/group/group_create.c
@@ -129,7 +129,7 @@ static inline void group_inherit_identity(FAR struct task_group_s *group)
 int group_allocate(FAR struct task_tcb_s *tcb, uint8_t ttype)
 {
   FAR struct task_group_s *group;
-  int ret = -ENOMEM;
+  int ret;
 
   DEBUGASSERT(tcb && !tcb->cmn.group);
 
@@ -158,6 +158,7 @@ int group_allocate(FAR struct task_tcb_s *tcb, uint8_t ttype)
   group->tg_members = kmm_malloc(GROUP_INITIAL_MEMBERS * sizeof(pid_t));
   if (!group->tg_members)
     {
+      ret = -ENOMEM;
       goto errout_with_group;
     }
 
