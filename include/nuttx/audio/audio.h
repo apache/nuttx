ca7230cf74f2cdd8642d56d96da4cf53d5e21a71
jinxiuxu <jinxiuxu@xiaomi.com>
2023-10-25 09:41:17 +0800

    audio: add amr format support
    Signed-off-by: jinxiuxu <jinxiuxu@xiaomi.com>

--- include/nuttx/audio/audio.h
+++ include/nuttx/audio/audio.h
@@ -158,6 +158,7 @@
 #define AUDIO_FMT_AAC               0x0d
 #define AUDIO_FMT_MSBC              0x0e
 #define AUDIO_FMT_CVSD              0x0f
+#define AUDIO_FMT_AMR               0x10
 
 /* Audio Sub-Format Types ***************************************************/
 
