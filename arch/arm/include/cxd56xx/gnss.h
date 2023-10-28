0a011585c4cde00cbcb59d21e118fbfc9a6d5440
SPRESENSE <41312067+SPRESENSE@users.noreply.github.com>
2023-10-28 19:19:38 +0900

    arch: cxd56xx: Update gnss header files
    - Add some ioctl commands to get version, sleep, wakeup and reset.
    - Add new data structures for CXD5610 GNSS Add-on board.

--- arch/arm/include/cxd56xx/gnss.h
+++ arch/arm/include/cxd56xx/gnss.h
@@ -547,10 +547,46 @@ extern "C"
 
 #define CXD56_GNSS_IOCTL_GET_1PPS_OUTPUT 53
 
+/**
+ * Get the firmware version
+ *
+ * param[in] arg
+ * string array of CXD56_GNSS_VERSION_MAXLEN
+ */
+
+#define CXD56_GNSS_IOCTL_GET_VERSION 54
+
+/**
+ * Sleep the firmware
+ *
+ * param[in] arg
+ * CXD56_GNSS_SLEEP(0) or CXD56_GNSS_DEEPSLEEP(1)
+ */
+
+#define CXD56_GNSS_IOCTL_SLEEP 55
+
+/**
+ * Wake up the firmware
+ *
+ * param arg
+ * Parameter is Unnecessary. Set Zero.
+ */
+
+#define CXD56_GNSS_IOCTL_WAKEUP 56
+
+/**
+ * Reset the firmware
+ *
+ * param arg
+ * Parameter is Unnecessary. Set Zero.
+ */
+
+#define CXD56_GNSS_IOCTL_RESET 57
+
 /* check macros for GNSS commands */
 
 #define CXD56_GNSS_IOCTL_INVAL 0
-#define CXD56_GNSS_IOCTL_MAX   54
+#define CXD56_GNSS_IOCTL_MAX   58
 
 /* Same value to GD Start mode CXD56_GNSS_STMOD_XXXX for fw_gd_start */
 
