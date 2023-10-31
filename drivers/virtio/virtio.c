89c49c53a755cf5fc91036940e9fccb042d6a233
Bowen Wang <wangbowen6@xiaomi.com>
2023-10-31 21:15:09 +0800

    virtio-mmio/pci: add alloc_buf/free_buf for mmio and pci transport layer
    And remove the virtio_alloc/free_buf implementation in virtio.c, because
    these two apis has been moved to the OpenAMP
    
    Signed-off-by: Bowen Wang <wangbowen6@xiaomi.com>

--- drivers/virtio/virtio.c
+++ drivers/virtio/virtio.c
@@ -73,48 +73,6 @@ static struct virtio_bus_s g_virtio_bus =
  ****************************************************************************/
 
 /****************************************************************************
- * Name: virtio_alloc_buf
- ****************************************************************************/
-
-FAR void *virtio_alloc_buf(FAR struct virtio_device *vdev,
-                           size_t size, size_t align)
-{
-  if (align == 0)
-    {
-      return kmm_malloc(size);
-    }
-  else
-    {
-      return kmm_memalign(align, size);
-    }
-}
-
-/****************************************************************************
- * Name: virtio_zalloc_buf
- ****************************************************************************/
-
-FAR void *virtio_zalloc_buf(FAR struct virtio_device *vdev,
-                            size_t size, size_t align)
-{
-  FAR void *ptr = virtio_alloc_buf(vdev, size, align);
-  if (ptr != NULL)
-    {
-      memset(ptr, 0, size);
-    }
-
-  return ptr;
-}
-
-/****************************************************************************
- * Name: virtio_mmio_free_buf
- ****************************************************************************/
-
-void virtio_free_buf(FAR struct virtio_device *vdev, FAR void *buf)
-{
-  kmm_free(buf);
-}
-
-/****************************************************************************
  * Name: virtio_register_drivers
  ****************************************************************************/
 
