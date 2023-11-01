f1fc53d7dac29ba20bebc0b227828e460673131f
dulibo1 <dulibo1@xiaomi.com>
2023-11-01 17:01:15 +0800

    gpio:add feature for gpio wakeup
    1.add pin type to cfg gpio as wake up source
    2.add IOEXPANDER_OPTION_WAKEUPCFG for set wake up option
    
    Signed-off-by: dulibo1 <dulibo1@xiaomi.com>
    Signed-off-by: buxiasen <buxiasen@xiaomi.com>

--- include/nuttx/ioexpander/ioexpander.h
+++ include/nuttx/ioexpander/ioexpander.h
@@ -77,6 +77,10 @@
                                           * structure to expander driver.
                                           */
 
+#define IOEXPANDER_OPTION_WAKEUPCFG   5  /* Configure interrupt for a pin to wake up the Soc */
+#define IOEXPANDER_WAKEUP_DISABLE     0  /* Do not cfg the pin as wake up source */
+#define IOEXPANDER_WAKEUP_ENABLE      1  /* Cfg the pin as wake up source */
+
 /* Access macros ************************************************************/
 
 /****************************************************************************
