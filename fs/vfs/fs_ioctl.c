033a91328a1932e5562471ac0234f16aec500eb0
wanggang26 <wanggang26@xiaomi.com>
2023-11-04 20:11:19 +0800

    vfs:fix a type mismatch issue and a typo
    Signed-off-by: wanggang26 <wanggang26@xiaomi.com>

--- fs/vfs/fs_ioctl.c
+++ fs/vfs/fs_ioctl.c
@@ -181,7 +181,7 @@ static int file_vioctl(FAR struct file *filep, int req, va_list ap)
                                         (unsigned long)(uintptr_t)&geo);
             if (ret >= 0)
               {
-                *(FAR blksize_t *)(uintptr_t)arg = geo.geo_nsectors;
+                *(FAR blkcnt_t *)(uintptr_t)arg = geo.geo_nsectors;
               }
           }
         break;
