6db011c695f10fff56becedd514808b88d53bbf6
chenrun1 <chenrun1@xiaomi.com>
2023-11-21 20:51:46 +0800

    Revert "libc/lib_bzero:Add bzero prototype."

--- include/strings.h
+++ include/strings.h
@@ -49,6 +49,10 @@
 #define bcopy(b1,b2,len) memmove(b2,b1,len)
 #endif
 
+#ifndef bzero /* See mm/README.txt */
+#define bzero(s,n)       memset(s,0,n)
+#endif
+
 #define strcasecmp_l(s1, s2, l)     strcasecmp(s1, s2)
 #define strncasecmp_l(s1, s2, n, l) strncasecmp(s1, s2, n)
 
@@ -90,8 +94,6 @@ FAR char *rindex(FAR const char *s, int c);
 
 int strcasecmp(FAR const char *, FAR const char *);
 int strncasecmp(FAR const char *, FAR const char *, size_t);
-
-void bzero(FAR void *s, size_t n);
 
 #if CONFIG_FORTIFY_SOURCE > 0
 fortify_function(bzero) void bzero(FAR void *s, size_t n)
