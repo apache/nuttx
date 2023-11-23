d8d75a5301e16f8b5c972730019e4ff44f830e3c
liqinhui <liqinhui@xiaomi.com>
2023-11-23 19:20:13 +0800

    net/icmpv6:Optimize the process of obtaining the IPv6 address through RA.
    - When receiving Router Advertisement Message, if Managed Address
      Configuration flag is set, we should ignore the Autonomous
      Address-configuration flag, and obtain the IPv6 address via the
      stateful process.
    
    Signed-off-by: liqinhui <liqinhui@xiaomi.com>

--- net/icmpv6/icmpv6_input.c
+++ net/icmpv6/icmpv6_input.c
@@ -441,20 +441,22 @@ void icmpv6_input(FAR struct net_driver_s *dev, unsigned int iplen)
                     FAR struct icmpv6_prefixinfo_s *prefixopt =
                                       (FAR struct icmpv6_prefixinfo_s *)opt;
 
-                    /* Is the "A" flag set? */
+                    /* if "M" flag isn't set, and the "A" flag is set.
+                     * Set the new network addresses.
+                     */
 
-                    if ((prefixopt->flags & ICMPv6_PRFX_FLAG_A) != 0)
+                    if ((adv->flags & ICMPv6_RADV_FLAG_M) == 0 &&
+                        (prefixopt->flags & ICMPv6_PRFX_FLAG_A) != 0)
                       {
-                        /* Yes.. Set the new network addresses. */
-
-                        icmpv6_setaddresses(dev, ipv6->srcipaddr,
+                         icmpv6_setaddresses(dev, ipv6->srcipaddr,
                                     prefixopt->prefix, prefixopt->preflen);
-
-                        /* Notify any waiting threads */
-
-                        icmpv6_rnotify(dev);
-                        prefix = true;
                       }
+
+                      /* Notify any waiting threads */
+
+                      icmpv6_rnotify(dev, (adv->flags & ICMPv6_RADV_FLAG_M) ?
+                                          -EADDRNOTAVAIL : OK);
+                      prefix = true;
                   }
                   break;
 
