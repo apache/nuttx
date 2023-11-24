c33e78e3ba8781049174b228309ee06c1141ed96
yinshengkai <yinshengkai@xiaomi.com>
2023-11-24 15:56:37 +0800

    gote: implement asynchronous printf formatting
    Do not format immediately when calling sched_note_printf, but delay formatting until dump trace.
    After turning on SYSTEM_NOTE, similar asynchronous syslog functions can be achieved.
    
    Signed-off-by: yinshengkai <yinshengkai@xiaomi.com>

--- include/nuttx/note/note_driver.h
+++ include/nuttx/note/note_driver.h
@@ -99,14 +99,10 @@ struct note_driver_ops_s
                     size_t curused);
 #endif
 #ifdef CONFIG_SCHED_INSTRUMENTATION_DUMP
-  CODE void (*string)(FAR struct note_driver_s *drv, uintptr_t ip,
-                      FAR const char *buf);
   CODE void (*event)(FAR struct note_driver_s *drv, uintptr_t ip,
                      uint8_t event, FAR const void *buf, size_t len);
   CODE void (*vprintf)(FAR struct note_driver_s *drv, uintptr_t ip,
                        FAR const char *fmt, va_list va) printf_like(3, 0);
-  CODE void (*vbprintf)(FAR struct note_driver_s *drv, uintptr_t ip,
-                        FAR const char *fmt, va_list va) printf_like(3, 0);
 #endif
 };
 
