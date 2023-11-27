aaff268d49669e5b6fbf61fe07b2f266d3579ed5
raiden00pl <raiden00@railab.me>
2023-11-27 10:24:53 +0100

    Documentation: port warnings_filter Sphinx extension from Zephyr
    ignore known warnings:
    
    /home/raiden00/git/RTOS/nuttx/nuttx/Documentation/reference/os/netdev.rst:61: WARNING: Duplicate C declaration, also defined at reference/os/netdev:39.
    Declaration is '.. c:struct:: net_driver_s'.
    /home/raiden00/git/RTOS/nuttx/nuttx/Documentation/reference/user/07_signals.rst:180: WARNING: Duplicate C declaration, also defined at reference/user/structures:112.
    Declaration is '.. c:struct:: sigaction'.

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -31,9 +31,12 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 # import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
 
+import sys
+
+# Add the '_extensions' directory to sys.path, to enable finding Sphinx
+# extensions within.
+sys.path.insert(0, "_extensions")
 
 # -- Project information -----------------------------------------------------
 
@@ -55,6 +58,7 @@ extensions = [
     "sphinx.ext.todo",
     "sphinx_tabs.tabs",
     "sphinx_copybutton",
+    "warnings_filter",
 ]
 
 source_suffix = [".rst", ".md"]
@@ -124,3 +128,7 @@ linkcheck_ignore = [
 latex_engine = "lualatex"
 
 copybutton_exclude = ".linenos, .gp, .go"
+
+# -- Options for warnings_filter ------------------------------------------
+
+warnings_filter_config = "known-warnings.txt"
