57c4f9f12d6646d7b7a122f2a59314c565eeac82
yintao <yintao@xiaomi.com>
2023-11-29 00:18:47 +0800

    sim/sim_rptun: Support master/slave notifies opposite side when recovery
    When in a multi-core structure, as the intermediate core,
    remote is both the master and slave;When the remote exception or
    restart occurs, it needs to notify the slave and reestablish the connection
    
    Signed-off-by: yintao <yintao@xiaomi.com>

--- arch/sim/src/sim/win/sim_hostmemory.c
+++ arch/sim/src/sim/win/sim_hostmemory.c
@@ -56,7 +56,7 @@ void host_freeheap(void *mem)
   _aligned_free(mem);
 }
 
-void *host_allocshmem(const char *name, size_t size, int master)
+void *host_allocshmem(const char *name, size_t size)
 {
   HANDLE handle;
   void *mem;
