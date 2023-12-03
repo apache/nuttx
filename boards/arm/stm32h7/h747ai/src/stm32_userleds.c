65eff966c28875f75ff0b68f037331a210261310
Roy Ratcliffe <roy@ratcliffe.me>
2023-12-03 08:33:02 +0000

    ci: rewrap long-line comment

--- boards/arm/stm32h7/h747ai/src/stm32_userleds.c
+++ boards/arm/stm32h7/h747ai/src/stm32_userleds.c
@@ -107,9 +107,8 @@ void board_userled(int led, bool ledon)
  *
  * Description:
  *   If CONFIG_ARCH_LEDS is defined, then NuttX will control the on-board
- *   LEDs. If CONFIG_ARCH_LEDS is not defined, then the board_userled_all() is
- *   available to control the LED from user application logic. NOTE: since
- *   there is only a single LED on-board, this is function is not very useful.
+ *   LEDs.  If CONFIG_ARCH_LEDS is not defined, then the board_userled_all()
+ *   is available to control the LED from user application logic.
  *
  ****************************************************************************/
 
