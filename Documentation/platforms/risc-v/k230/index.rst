6a1cdb7defa9c2deef6f614ffbc71558ba7bd582
Yanfeng Liu <yfliu2008@qq.com>
2023-12-13 10:59:21 +0800

    risc-v: Initial support for CanMV-k230 board and K230 chip
    The code is mainly derived from the NuttX qemu-rv/rv-virt codebase.
    
    Major changes:
    
    - boards/Kconfig:       add new BOARD_K230_CANMV
    - arch/risc-v/Kconfig:  add new CHIP_K230 chip and ARCH_RV_MMIO_BITS
    - arch/risc-v/src/common/riscv_mtimer.c: use ARCH_RV_MMIO_BITS to
                            select MMIO access width
    
    New additions:
    
    - arch/risc-v/include/k230/: k230 SoC definitions
    - arch/risc-v/src/k230/:     k230 SoC sources
    - boards/risc-v/k230/canmv230/:  CanMV-K230 board sources and configs
    - Documentation/platforms/risc-v/k230/: simple doc
    
    Note that only FLAT build works for canmv230 now.
    
    This PR has changes in RiscV common layer thus may affect other RiscV ports
    It changes the mtime/mtimecmp access control from using config ARCH_RV64 to
    newly intorduced config ARCH_RV_MMIO_BITS.
    
    Original design uses ARCH_RV64 to select 64bit MMIO in riscv_mtimer.c, this
    can't cope with the situation with K230 --- it has ARCH_RV64 but only can do
    32bit MMIO. So a new ARCH_RV_MMIO_BITS config has been introduced. Its value
    depicts the MMIO width in bits. The MMIO_BITS defaults to 32/64 for RV32/
    RV64 respectively. This allows the macro to replace current use of ARCH_RV64
    in riscv_mtimer.c.
    
    The new MMIO_BITS config is a derived one, and for RiscV chips with
    equal CPU and MMIO widths there is no need to explicitly set it as the
    default rule will do that. Only chips with different CPU and MMIO widths
    need set it in Kconfig.
    
    So by design this change should be safe but RiscV ports should be checked.
    
    "ostest" verification has been done for:
    
    - canmv230/nsh
    - rv-vivt/nsh
    - rv-virt/nsh64
    
    configuration generation and manual check of derived RV_MMIO_BITS has been
    done for:
    
    - star64/nsh
    - arty_a7/nsh
    - bl602evb/nsh
    
    Signed-off-by: Yanfeng Liu <yfliu2008@qq.com>

---
+++ Documentation/platforms/risc-v/k230/index.rst
@@ -0,0 +1,12 @@
+=============
+Kendryte K230
+=============
+
+Supported Boards
+================
+
+.. toctree::
+   :glob:
+   :maxdepth: 1
+
+   boards/*/*
---
+++ arch/risc-v/src/k230/k230_start.c
@@ -0,0 +1,239 @@
+/****************************************************************************
+ * arch/risc-v/src/k230/k230_start.c
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+#include <nuttx/init.h>
+#include <nuttx/arch.h>
+#include <nuttx/serial/uart_16550.h>
+#include <arch/board/board.h>
+
+#include "riscv_internal.h"
+#include "chip.h"
+
+#ifdef CONFIG_BUILD_KERNEL
+#  include "k230_mm_init.h"
+#endif
+
+#ifdef CONFIG_DEVICE_TREE
+#  include <nuttx/fdt.h>
+#endif
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+#ifdef CONFIG_DEBUG_FEATURES
+#define showprogress(c) up_putc(c)
+#else
+#define showprogress(c)
+#endif
+
+#if defined (CONFIG_BUILD_KERNEL) && !defined (CONFIG_ARCH_USE_S_MODE)
+#  error "Target requires kernel in S-mode, enable CONFIG_ARCH_USE_S_MODE"
+#endif
+
+/****************************************************************************
+ * Extern Function Declarations
+ ****************************************************************************/
+
+#ifdef CONFIG_BUILD_KERNEL
+extern void __trap_vec(void);
+extern void __trap_vec_m(void);
+extern void up_mtimer_initialize(void);
+#endif
+
+/****************************************************************************
+ * Name: k230_clear_bss
+ ****************************************************************************/
+
+void k230_clear_bss(void)
+{
+  uint32_t *dest;
+
+  /* Clear .bss.  We'll do this inline (vs. calling memset) just to be
+   * certain that there are no issues with the state of global variables.
+   */
+
+  for (dest = (uint32_t *)_sbss; dest < (uint32_t *)_ebss; )
+    {
+      *dest++ = 0;
+    }
+}
+
+/****************************************************************************
+ * Public Data
+ ****************************************************************************/
+
+/* NOTE: g_idle_topstack needs to point the top of the idle stack
+ * for CPU0 and this value is used in up_initial_state()
+ */
+
+uintptr_t g_idle_topstack = K230_IDLESTACK_TOP;
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: k230_start
+ ****************************************************************************/
+
+#ifdef CONFIG_BUILD_KERNEL
+void k230_start_s(int mhartid, const char *dtb)
+#else
+void k230_start(int mhartid, const char *dtb)
+#endif
+{
+  /* Configure FPU */
+
+  riscv_fpuconfig();
+
+  if (mhartid > 0)
+    {
+      goto cpux;
+    }
+
+#ifndef CONFIG_BUILD_KERNEL
+  k230_clear_bss();
+#endif
+
+#ifdef CONFIG_DEVICE_TREE
+  fdt_register(dtb);
+#endif
+
+  showprogress('A');
+
+#ifdef USE_EARLYSERIALINIT
+  riscv_earlyserialinit();
+#endif
+
+  showprogress('B');
+
+  /* Do board initialization */
+
+#ifdef CONFIG_BUILD_KERNEL
+  /* Setup page tables for kernel and enable MMU */
+
+  k230_mm_init();
+#endif
+
+  showprogress('C');
+
+  /* Call nx_start() */
+
+  nx_start();
+
+cpux:
+
+#ifdef CONFIG_SMP
+  riscv_cpu_boot(mhartid);
+#endif
+
+  while (true)
+    {
+      asm("WFI");
+    }
+}
+
+#ifdef CONFIG_BUILD_KERNEL
+
+/****************************************************************************
+ * Name: k230_start
+ ****************************************************************************/
+
+void k230_start(int mhartid, const char *dtb)
+{
+  /* NOTE: still in M-mode */
+
+  if (0 == mhartid)
+    {
+      k230_clear_bss();
+
+      /* Initialize the per CPU areas */
+
+      riscv_percpu_add_hart(mhartid);
+    }
+
+  /* Disable MMU and enable PMP */
+
+  WRITE_CSR(satp, 0x0);
+  WRITE_CSR(pmpaddr0, 0x3fffffffffffffull);
+  WRITE_CSR(pmpcfg0, 0xf);
+
+  /* Set exception and interrupt delegation for S-mode */
+
+  WRITE_CSR(medeleg, 0xffff);
+  WRITE_CSR(mideleg, 0xffff);
+
+  /* Allow to write satp from S-mode */
+
+  CLEAR_CSR(mstatus, MSTATUS_TVM);
+
+  /* Set mstatus to S-mode and enable SUM */
+
+  CLEAR_CSR(mstatus, ~MSTATUS_MPP_MASK);
+  SET_CSR(mstatus, MSTATUS_MPPS | SSTATUS_SUM);
+
+  /* Set the trap vector for S-mode */
+
+  WRITE_CSR(stvec, (uintptr_t)__trap_vec);
+
+  /* Set the trap vector for M-mode */
+
+  WRITE_CSR(mtvec, (uintptr_t)__trap_vec_m);
+
+  if (0 == mhartid)
+    {
+      /* Only the primary CPU needs to initialize mtimer
+       * before entering to S-mode
+       */
+
+      up_mtimer_initialize();
+    }
+
+  /* Set mepc to the entry */
+
+  WRITE_CSR(mepc, (uintptr_t)k230_start_s);
+
+  /* Set a0 to mhartid and a1 to dtb explicitly and enter to S-mode */
+
+  asm volatile (
+      "mv a0, %0 \n"
+      "mv a1, %1 \n"
+      "mret \n"
+      :: "r" (mhartid), "r" (dtb)
+  );
+}
+#endif
+
+void riscv_earlyserialinit(void)
+{
+  u16550_earlyserialinit();
+}
+
+void riscv_serialinit(void)
+{
+  u16550_serialinit();
+}
