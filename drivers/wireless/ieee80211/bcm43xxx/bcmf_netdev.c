cfc0189b7d71a8b6819ecf0f8a50352a8f59e521
Zhe Weng <wengzhe@xiaomi.com>
2023-12-13 12:39:43 +0800

    netdev/ipv6: Move `xxx_ipv6multicast` from arch to common code
    The `xxx_ipv6multicast` function in each driver is not adapted to
    multiple IPv6 addresses yet, and they're redundant, so try to take them
    into common code.
    
    Change:
    1. Add MAC `g_ipv6_ethallnodes` and `g_ipv6_ethallrouters` in
       `icmpv6_devinit` and call them in `netdev_register`
    2. Add multicast MAC for Neighbor Solicitation when adding any IPv6
       address, and remove them when IPv6 address is removed
    3. Select `NET_MCASTGROUP` when `NET_ICMPv6` because now we need
       `d_addmac` when we have ICMPv6
    
    Note:
    We want modules outside net stack to call functions like
    `netdev_ipv6_add` and never touch the related MAC address, so these MAC
    functions are added as internal functions to `net/netdev/netdev.h`
    
    Signed-off-by: Zhe Weng <wengzhe@xiaomi.com>

--- drivers/wireless/ieee80211/bcm43xxx/bcmf_netdev.c
+++ drivers/wireless/ieee80211/bcm43xxx/bcmf_netdev.c
@@ -123,9 +123,6 @@ static int  bcmf_addmac(FAR struct net_driver_s *dev,
 static int  bcmf_rmmac(FAR struct net_driver_s *dev,
                        FAR const uint8_t *mac);
 #endif
-#ifdef CONFIG_NET_ICMPv6
-static void bcmf_ipv6multicast(FAR struct bcmf_dev_s *priv);
-#endif
 #endif
 #ifdef CONFIG_NETDEV_IOCTL
 static int  bcmf_ioctl(FAR struct net_driver_s *dev, int cmd,
@@ -636,14 +633,6 @@ static int bcmf_ifup(FAR struct net_driver_s *dev)
                                CONFIG_IEEE80211_BROADCOM_DEFAULT_COUNTRY);
     }
 
-  /* Instantiate MAC address from priv->bc_dev.d_mac.ether.ether_addr_octet */
-
-#ifdef CONFIG_NET_ICMPv6
-  /* Set up IPv6 multicast address filtering */
-
-  bcmf_ipv6multicast(priv);
-#endif
-
   /* Enable the hardware interrupt */
 
   priv->bc_bifup = true;
@@ -880,6 +869,7 @@ static int bcmf_addmac(FAR struct net_driver_s *dev, FAR const uint8_t *mac)
 
   /* Add the MAC address to the hardware multicast routing table */
 
+  UNUSED(priv);
   return OK;
 }
 #endif
@@ -909,81 +899,10 @@ static int bcmf_rmmac(FAR struct net_driver_s *dev, FAR const uint8_t *mac)
 
   /* Add the MAC address to the hardware multicast routing table */
 
+  UNUSED(priv);
   return OK;
 }
 #endif
-
-/****************************************************************************
- * Name: bcmf_ipv6multicast
- *
- * Description:
- *   Configure the IPv6 multicast MAC address.
- *
- * Input Parameters:
- *   priv - A reference to the private driver state structure
- *
- * Returned Value:
- *   OK on success; Negated errno on failure.
- *
- * Assumptions:
- *
- ****************************************************************************/
-
-#ifdef CONFIG_NET_ICMPv6
-static void bcmf_ipv6multicast(FAR struct bcmf_dev_s *priv)
-{
-  FAR struct net_driver_s *dev;
-  uint16_t tmp16;
-  uint8_t mac[6];
-
-  /* For ICMPv6, we need to add the IPv6 multicast address
-   *
-   * For IPv6 multicast addresses, the Ethernet MAC is derived by
-   * the four low-order octets OR'ed with the MAC 33:33:00:00:00:00,
-   * so for example the IPv6 address FF02:DEAD:BEEF::1:3 would map
-   * to the Ethernet MAC address 33:33:00:01:00:03.
-   *
-   * NOTES:  This appears correct for the ICMPv6 Router Solicitation
-   * Message, but the ICMPv6 Neighbor Solicitation message seems to
-   * use 33:33:ff:01:00:03.
-   */
-
-  mac[0] = 0x33;
-  mac[1] = 0x33;
-
-  dev    = &priv->bc_dev;
-  tmp16  = dev->d_ipv6addr[6];
-  mac[2] = 0xff;
-  mac[3] = tmp16 >> 8;
-
-  tmp16  = dev->d_ipv6addr[7];
-  mac[4] = tmp16 & 0xff;
-  mac[5] = tmp16 >> 8;
-
-  ninfo("IPv6 Multicast: %02x:%02x:%02x:%02x:%02x:%02x\n",
-        mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
-
-  bcmf_addmac(dev, mac);
-
-#ifdef CONFIG_NET_ICMPv6_AUTOCONF
-  /* Add the IPv6 all link-local nodes Ethernet address.  This is the
-   * address that we expect to receive ICMPv6 Router Advertisement
-   * packets.
-   */
-
-  bcmf_addmac(dev, g_ipv6_ethallnodes.ether_addr_octet);
-#endif /* CONFIG_NET_ICMPv6_AUTOCONF */
-
-#ifdef CONFIG_NET_ICMPv6_ROUTER
-  /* Add the IPv6 all link-local routers Ethernet address.  This is the
-   * address that we expect to receive ICMPv6 Router Solicitation
-   * packets.
-   */
-
-  bcmf_addmac(dev, g_ipv6_ethallrouters.ether_addr_octet);
-#endif /* CONFIG_NET_ICMPv6_ROUTER */
-}
-#endif /* CONFIG_NET_ICMPv6 */
 
 /****************************************************************************
  * Name: bcmf_ioctl
