ce5fafa74501db8a876abd087a884578a7d78c7a
hujun5 <hujun5@xiaomi.com>
2023-12-20 20:09:51 +0800

    irq: inline restore_critical_section
    reason:
    In the SMP, when a context switch occurs, restore_critical_section is executed.
    In order to reduce the time taken for context switching,
    we inline the restore_critical_section function.
    Given that restore_critical_section is small in size
    and is called from only one location, inlining it does not increase the size of the image.
    
    Signed-off-by: hujun5 <hujun5@xiaomi.com>

--- include/nuttx/irq.h
+++ include/nuttx/irq.h
@@ -312,7 +312,19 @@ void leave_critical_section(irqstate_t flags) noinstrument_function;
  ****************************************************************************/
 
 #ifdef CONFIG_SMP
-void restore_critical_section(void);
+#  define restore_critical_section() \
+   do { \
+       FAR struct tcb_s *tcb; \
+       int me = this_cpu(); \
+       tcb = current_task(me); \
+       if (tcb->irqcount <= 0) \
+         {\
+           if ((g_cpu_irqset & (1 << me)) != 0) \
+             { \
+               cpu_irqlock_clear(); \
+             } \
+         } \
+    } while (0)
 #else
 #  define restore_critical_section()
 #endif
