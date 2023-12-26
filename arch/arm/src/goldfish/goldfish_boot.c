48187078701abf4bb977398d43c37a28dd2c40c6
ligd <liguiding1@xiaomi.com>
2023-12-26 15:44:36 +0800

    glodfish: add SMP boot support
    Signed-off-by: ligd <liguiding1@xiaomi.com>

--- arch/arm/src/goldfish/goldfish_boot.c
+++ arch/arm/src/goldfish/goldfish_boot.c
@@ -25,9 +25,12 @@
 #include <nuttx/config.h>
 
 #include "arm_internal.h"
+#include "arm_cpu_psci.h"
 
 #include "goldfish_irq.h"
 #include "goldfish_memorymap.h"
+#include "smp.h"
+#include "gic.h"
 
 #ifdef CONFIG_DEVICE_TREE
 #  include <nuttx/fdt.h>
@@ -57,12 +60,12 @@ void arm_boot(void)
 
   arm_fpuconfig();
 
-#ifdef CONFIG_DEVICE_TREE
-  fdt_register((const char *)0x40000000);
+#ifdef CONFIG_ARCH_HAVE_PSCI
+  arm_psci_init("smc");
 #endif
 
-#if defined(CONFIG_ARCH_HAVE_PSCI)
-  arm_psci_init("smc");
+#ifdef CONFIG_DEVICE_TREE
+  fdt_register((const char *)0x40000000);
 #endif
 
 #ifdef USE_EARLYSERIALINIT
@@ -73,3 +76,16 @@ void arm_boot(void)
   arm_earlyserialinit();
 #endif
 }
+
+#if defined(CONFIG_ARCH_HAVE_PSCI) && defined(CONFIG_SMP)
+int up_cpu_start(int cpu)
+{
+#ifdef CONFIG_SCHED_INSTRUMENTATION
+  /* Notify of the start event */
+
+  sched_note_cpu_start(this_task_inirq(), cpu);
+#endif
+
+  return psci_cpu_on(cpu, (uintptr_t)__start);
+}
+#endif
