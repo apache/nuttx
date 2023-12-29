675f7754940160fc0ce73bcbfa5b757b64594fd4
Yanfeng Liu <yfliu2008@qq.com>
2023-12-29 14:35:25 +0800

    log messages of binfmt are very much.
    this is to adjust levels of a few of them based on debugging
    experiences. These non-critical logs were marked as errors. So we
    likely can adjust them as warnings so that real errors can
    stand out easily.
    
    Signed-off-by: Yanfeng Liu <yfliu2008@qq.com>

--- binfmt/libelf/libelf_bind.c
+++ binfmt/libelf/libelf_bind.c
@@ -496,7 +496,7 @@ static int elf_relocateadd(FAR struct elf_loadinfo_s *loadinfo, int relidx,
 
               if (ret == -ESRCH)
                 {
-                  berr("Section %d reloc %d: "
+                  bwarn("Section %d reloc %d: "
                        "Undefined symbol[%d] has no name: %d\n",
                        relidx, i, symidx, ret);
                 }
