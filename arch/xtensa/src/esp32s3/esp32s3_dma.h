bdd02cc624efe3fd2a9a078535ec21c848b2fcca
chenwen@espressif.com <chenwen@espressif.com>
2024-01-05 10:54:25 +0800

    xtensa/esp32s3: Add APIs to release DMA channel resources
    Signed-off-by: chenwen@espressif.com <chenwen@espressif.com>

--- arch/xtensa/src/esp32s3/esp32s3_dma.h
+++ arch/xtensa/src/esp32s3/esp32s3_dma.h
@@ -146,6 +146,22 @@ int32_t esp32s3_dma_request(enum esp32s3_dma_periph_e periph,
                             bool burst_en);
 
 /****************************************************************************
+ * Name: esp32s3_dma_release
+ *
+ * Description:
+ *   Release DMA channel from peripheral.
+ *
+ * Input Parameters:
+ *   chan - Peripheral for which the DMA channel request was made
+ *
+ * Returned Value:
+ *   None.
+ *
+ ****************************************************************************/
+
+void esp32s3_dma_release(int chan);
+
+/****************************************************************************
  * Name: esp32s3_dma_setup
  *
  * Description:
@@ -271,6 +287,22 @@ void esp32s3_dma_set_ext_memblk(int chan, bool tx,
  ****************************************************************************/
 
 void esp32s3_dma_init(void);
+
+/****************************************************************************
+ * Name: esp32s3_dma_deinit
+ *
+ * Description:
+ *   Deinitialize DMA driver.
+ *
+ * Input Parameters:
+ *   None
+ *
+ * Returned Value:
+ *   None.
+ *
+ ****************************************************************************/
+
+void esp32s3_dma_deinit(void);
 
 #ifdef __cplusplus
 }
