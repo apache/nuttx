7c89f943c08102849d0cb25917f8100793bec04e
chao an <anchao@lixiang.com>
2024-01-15 14:22:07 +0800

    armv8-r/perf: enable perf count only ARCH_PERF_EVENTS is enabled
    Signed-off-by: chao an <anchao@lixiang.com>

--- arch/arm/src/armv8-r/arm_perf.c
+++ arch/arm/src/armv8-r/arm_perf.c
@@ -28,6 +28,8 @@
 #include "arm_internal.h"
 #include "sctlr.h"
 
+#ifdef CONFIG_ARCH_PERF_EVENTS
+
 /****************************************************************************
  * Private Data
  ****************************************************************************/
@@ -85,3 +87,4 @@ void up_perf_convert(unsigned long elapsed, struct timespec *ts)
   left        = elapsed - ts->tv_sec * g_cpu_freq;
   ts->tv_nsec = NSEC_PER_SEC * (uint64_t)left / g_cpu_freq;
 }
+#endif /* CONFIG_ARCH_PERF_EVENTS */
