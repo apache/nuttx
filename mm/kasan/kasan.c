b2924cfd7de5e82ef3e23feb7905e9df20cfe7f2
anjiahao <anjiahao@xiaomi.com>
2024-01-16 16:47:04 +0800

    kasan:fix bug if kasan_mem_to_shadow mod size is 0 just return
    Signed-off-by: anjiahao <anjiahao@xiaomi.com>

--- mm/kasan/kasan.c
+++ mm/kasan/kasan.c
@@ -149,7 +149,7 @@ static FAR uintptr_t *kasan_mem_to_shadow(FAR const void *ptr, size_t size,
   size_t mod;
   size_t i;
 
-  if (g_region_init != KASAN_INIT_VALUE || size == 0)
+  if (g_region_init != KASAN_INIT_VALUE)
     {
       return NULL;
     }
@@ -168,6 +168,11 @@ static FAR uintptr_t *kasan_mem_to_shadow(FAR const void *ptr, size_t size,
         }
 
       mod = size % KASAN_SHADOW_SCALE;
+      if (mod == 0)
+        {
+          return NULL;
+        }
+
       addr += mul * KASAN_SHADOW_SCALE;
       size = mod;
     }
