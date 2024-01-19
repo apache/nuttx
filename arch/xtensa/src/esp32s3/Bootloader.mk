0859f1cb91107e4885873cdf61ae8e68530b6e18
Tiago Medicci Serrano <tiago.medicci@espressif.com>
2024-01-19 10:52:42 -0300

    esp32s3: Improve selection of the bootloader being used on Kconfig
    - A pre-built IDF bootloader is used by default;
    - `ESP32S3_PARTITION_TABLE` requires the IDF bootloader to be built
    from sources.
    - Native MCUboot also can be used to boot the device. It will be
    built from sources and depends on !ESP32S3_PARTITION_TABLE.

--- arch/xtensa/src/esp32s3/Bootloader.mk
+++ arch/xtensa/src/esp32s3/Bootloader.mk
@@ -131,16 +131,6 @@ else ifeq ($(CONFIG_ESP32S3_BOOTLOADER_DOWNLOAD_PREBUILT),y)
 BOOTLOADER_VERSION = latest
 BOOTLOADER_URL     = https://github.com/espressif/esp-nuttx-bootloader/releases/download/$(BOOTLOADER_VERSION)
 
-ifeq ($(CONFIG_ESP32S3_APP_FORMAT_MCUBOOT),y)
-
-bootloader:
-	$(call DOWNLOAD,$(BOOTLOADER_URL),mcuboot-esp32s3.bin,$(TOPDIR)/mcuboot-esp32s3.bin)
-
-clean_bootloader:
-	$(call DELFILE,$(TOPDIR)/mcuboot-esp32s3.bin)
-
-else ifeq ($(CONFIG_ESP32S3_APP_FORMAT_LEGACY),y)
-
 bootloader:
 	$(call DOWNLOAD,$(BOOTLOADER_URL),bootloader-esp32s3.bin,$(TOPDIR)/bootloader-esp32s3.bin)
 	$(call DOWNLOAD,$(BOOTLOADER_URL),partition-table-esp32s3.bin,$(TOPDIR)/partition-table-esp32s3.bin)
@@ -148,7 +138,5 @@ bootloader:
 clean_bootloader:
 	$(call DELFILE,$(TOPDIR)/bootloader-esp32s3.bin)
 	$(call DELFILE,$(TOPDIR)/partition-table-esp32s3.bin)
-
-endif
 
 endif
