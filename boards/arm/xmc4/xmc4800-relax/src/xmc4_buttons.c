26ffb1f568cd2f9d8815c385746931642e773abb
ThomasNS <thomas.narayana-swamy@wandercraft.eu>
2024-01-25 18:40:11 +0100

    add xmc4800-relax to nuttx

---
+++ boards/arm/xmc4/xmc4800-relax/src/xmc4_buttons.c
@@ -0,0 +1,64 @@
+/****************************************************************************
+ * boards/arm/xmc4/xmc4800-relax/src/xmc4_buttons.c
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+#include <arch/board/board.h>
+#include "xmc4800-relax.h"
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: board_button_initialize
+ ****************************************************************************/
+
+uint32_t board_button_initialize(void)
+{
+#warning Missing logic
+  return 0;
+}
+
+/****************************************************************************
+ * Name: board_buttons
+ ****************************************************************************/
+
+uint32_t board_buttons(void)
+{
+#warning Missing logic
+  return 0;
+}
+
+/****************************************************************************
+ * Name: board_button_irq
+ ****************************************************************************/
+
+#ifdef CONFIG_ARCH_IRQBUTTONS
+int board_button_irq(int id, xcpt_t irqhandler, void *arg)
+{
+#warning Missing logic
+  return -ENOSYS;
+}
+#endif /* CONFIG_ARCH_IRQBUTTONS */
