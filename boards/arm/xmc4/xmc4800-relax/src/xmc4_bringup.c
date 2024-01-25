13c310f2cb59366e6a9927f13156964a3708f112
ThomasNS <thomas.narayana-swamy@wandercraft.eu>
2024-01-25 18:40:11 +0100

    add xmc4800-relax to nuttx

---
+++ boards/arm/xmc4/xmc4800-relax/src/xmc4_bringup.c
@@ -0,0 +1,81 @@
+/****************************************************************************
+ * boards/arm/xmc4/xmc4800-relax/src/xmc4_bringup.c
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+#include <debug.h>
+#include <errno.h>
+#include <sys/types.h>
+
+#ifdef CONFIG_XMC4_USCI_SPI
+#  include <nuttx/spi/spi_transfer.h>
+#endif
+#ifdef CONFIG_USERLED
+#  include <nuttx/leds/userled.h>
+#endif
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: xmc4_bringup
+ *
+ * Description:
+ *   Bring up board features
+ *
+ ****************************************************************************/
+
+int xmc4_bringup(void)
+{
+  int ret = OK;
+
+#ifdef CONFIG_XMC4_USCI_SPI
+  struct spi_dev_s *spi;
+  spi = xmc4_spibus_initialize(4);
+
+  if (!spi)
+    {
+      return -ENODEV;
+    }
+
+  ret = spi_register(spi, 0);
+  if (ret < 0)
+    {
+      snerr("ERROR: Failed to register driver: %d\n", ret);
+    }
+#endif
+
+#ifdef CONFIG_USERLED
+  /* Register the LED driver */
+
+  ret = userled_lower_initialize("/dev/userleds");
+  if (ret < 0)
+    {
+      syslog(LOG_ERR, "ERROR: userled_lower_initialize() failed: %d\n", ret);
+    }
+#endif
+
+  return ret;
+}
---
+++ boards/arm/xmc4/xmc4800-relax/src/xmc4_userleds.c
@@ -0,0 +1,87 @@
+/****************************************************************************
+ * boards/arm/xmc4/xmc4800-relax/src/xmc4_userleds.c
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+#include <debug.h>
+
+#include <arch/board/board.h>
+
+#include "xmc4_gpio.h"
+#include "xmc4800-relax.h"
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: board_userled_initialize
+ ****************************************************************************/
+
+uint32_t board_userled_initialize(void)
+{
+  /* Configure LED1-2 GPIOs for output */
+
+  xmc4_gpio_config(GPIO_LED1);
+  xmc4_gpio_config(GPIO_LED2);
+  return BOARD_NLEDS;
+}
+
+/****************************************************************************
+ * Name: board_userled
+ ****************************************************************************/
+
+void board_userled(int led, bool ledon)
+{
+  gpioconfig_t ledcfg;
+
+  if (led == BOARD_LED0)
+    {
+      ledcfg = GPIO_LED1;
+    }
+  else if (led == BOARD_LED1)
+    {
+      ledcfg = GPIO_LED2;
+    }
+  else
+    {
+      return;
+    }
+
+  xmc4_gpio_write(ledcfg, ledon);
+}
+
+/****************************************************************************
+ * Name: board_userled_all
+ ****************************************************************************/
+
+void board_userled_all(uint32_t ledset)
+{
+  bool ledon;
+
+  ledon = ((ledset & BOARD_LED0_BIT) != 0);
+  xmc4_gpio_write(GPIO_LED1, ledon);
+
+  ledon = ((ledset & BOARD_LED1_BIT) != 0);
+  xmc4_gpio_write(GPIO_LED2, ledon);
+}
