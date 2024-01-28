5f831610944a6bd09f94d032838b3147c6802dd3
raiden00pl <raiden00@railab.me>
2024-01-28 15:44:40 +0100

    mrf24j40: always use SPI MODE(0,0)
    Other SPI modes are not supported. From doc:
    
      "The MRF24J40 supports SPI (mode 0,0) which requires
      that SCK idles in a low state."

--- drivers/wireless/ieee802154/mrf24j40/mrf24j40.h
+++ drivers/wireless/ieee802154/mrf24j40/mrf24j40.h
@@ -76,6 +76,8 @@
 
 #define MRF24J40_SYMBOL_DURATION_PS 16000000
 
+#define MRF24J40_SPIMODE SPIDEV_MODE0
+
 /* Clock configuration macros */
 
 #define MRF24J40_BEACONINTERVAL_NSEC(beaconorder) \
@@ -88,10 +90,6 @@
 
 #ifndef CONFIG_SCHED_HPWORK
 #  error High priority work queue required in this driver
-#endif
-
-#ifndef CONFIG_IEEE802154_MRF24J40_SPIMODE
-#  define CONFIG_IEEE802154_MRF24J40_SPIMODE SPIDEV_MODE0
 #endif
 
 #ifndef CONFIG_IEEE802154_MRF24J40_FREQUENCY
@@ -174,7 +172,7 @@ static inline void mrf24j40_spi_lock(FAR struct spi_dev_s *spi)
 {
   SPI_LOCK(spi, 1);
   SPI_SETBITS(spi, 8);
-  SPI_SETMODE(spi, CONFIG_IEEE802154_MRF24J40_SPIMODE);
+  SPI_SETMODE(spi, MRF24J40_SPIMODE);
   SPI_SETFREQUENCY(spi, CONFIG_IEEE802154_MRF24J40_FREQUENCY);
 }
 
