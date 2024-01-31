8122ed4e1ae4400b848039691d8a29ea4afdf90d
Ville Juven <ville.juven@unikie.com>
2024-01-31 15:56:23 +0200

    fs/shmfs: Fix CONFIG_FS_SHM -> CONFIG_FS_SHMFS
    The macro was wrong

--- fs/vfs/fs_stat.c
+++ fs/vfs/fs_stat.c
@@ -278,7 +278,7 @@ int inode_stat(FAR struct inode *inode, FAR struct stat *buf, int resolve)
     }
   else
 #endif
-#if defined(CONFIG_FS_SHM)
+#if defined(CONFIG_FS_SHMFS)
   /* Check for shared memory */
 
   if (INODE_IS_SHM(inode))
