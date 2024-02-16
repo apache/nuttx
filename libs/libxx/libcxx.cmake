4e53b77dce6bff0c4c2abc80a366eec1ca9d9601
Petro Karashchenko <petro.karashchenko@gmail.com>
2024-02-16 14:22:07 +0100

    libs/libxx: add patch to remove mach/mach_time.h inclusion
    Inclusion of mach/mach_time.h by libcxx/src/chrono.cpp breaks
    the build on latest MACOS dues to https://forums.developer.apple.com/forums/thread/746737
    The interface from mach/mach_time.h is not referenced in chrono.cpp
    so it is safe to remove it
    
    Signed-off-by: Petro Karashchenko <petro.karashchenko@gmail.com>

--- libs/libxx/libcxx.cmake
+++ libs/libxx/libcxx.cmake
@@ -43,7 +43,9 @@ if(NOT EXISTS ${CMAKE_CURRENT_LIST_DIR}/libcxx)
       patch -p1 -d ${CMAKE_CURRENT_LIST_DIR}/libcxx <
       ${CMAKE_CURRENT_LIST_DIR}/0001_fix_stdatomic_h_miss_typedef.patch && patch
       -p3 -d ${CMAKE_CURRENT_LIST_DIR}/libcxx <
-      ${CMAKE_CURRENT_LIST_DIR}/mbstate_t.patch
+      ${CMAKE_CURRENT_LIST_DIR}/mbstate_t.patch && patch -p1 -d
+      ${CMAKE_CURRENT_LIST_DIR}/libcxx <
+      ${CMAKE_CURRENT_LIST_DIR}/0001-libcxx-remove-mach-time-h.patch
     DOWNLOAD_NO_PROGRESS true
     TIMEOUT 30)
 
