4b5b7649f676eddb69ad1a9088c41b3fa5f2e50e
simonatoaca <simona.alexandra2000@gmail.com>
2024-02-19 09:58:10 +0200

    esp32-sparrow-kit: Add I2S support for the board's microphone
    The board's microphone uses 24-bit i2s and this commit also fixes
    the segmentation fault caused by the audio buffer overflow.
    
    arch/xtensa/src/esp32/esp32_i2s.c: Fix bug regarding 24-bit audio and add AUDIOIOC_STOP to ioctl
    drivers/audio/audio_i2s.c: Report number of channels on AUDIOIOC_GETCAPS
    in boards/xtensa/esp32/esp32-sparrow-kit:
    	/configs/nsh/defconfig: Add I2S configs
    	/src/esp32-sparrow-kit.h: Add the signature of esp32_i2sdev_initialize()
    	/src/esp32_bringup.c: Add call to esp32_i2sdev_initialize()
    
    Signed-off-by: simonatoaca <simona.alexandra2000@gmail.com>

--- boards/xtensa/esp32/esp32-sparrow-kit/src/esp32_bringup.c
+++ boards/xtensa/esp32/esp32-sparrow-kit/src/esp32_bringup.c
@@ -80,6 +80,10 @@
 #  include "esp32_board_i2c.h"
 #endif
 
+#ifdef CONFIG_ESP32_I2S
+#  include "esp32_i2s.h"
+#endif
+
 #ifdef CONFIG_SENSORS_BMP180
 #  include "esp32_bmp180.h"
 #endif
@@ -342,6 +346,23 @@ int esp32_bringup(void)
 #endif
 
 #endif
+
+#ifdef CONFIG_ESP32_I2S
+
+#ifdef CONFIG_ESP32_I2S0
+
+  /* Configure I2S generic audio on I2S0 */
+
+  ret = board_i2sdev_initialize(ESP32_I2S0);
+  if (ret < 0)
+    {
+      syslog(LOG_ERR, "Failed to initialize I2S%d driver: %d\n",
+             CONFIG_ESP32_I2S0, ret);
+    }
+
+#endif  /* CONFIG_ESP32_I2S0 */
+
+#endif /* CONFIG_ESP32_I2S */
 
 #ifdef CONFIG_SENSORS_BMP180
   /* Try to register BMP180 device in I2C0 */
