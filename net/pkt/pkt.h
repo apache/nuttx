9a23ebdbc0a844bd19cbf433918829c5e4ad067b
Zhe Weng <wengzhe@xiaomi.com>
2024-02-19 11:46:04 +0800

    net/pkt: Support binding to devices other than ETH
    We're using the `sll_ifindex` inside `struct sockaddr_ll` to bind
    device, so we don't need to translate it into mac address, we can just
    match the index, which also let us bind to different type of devices
    other than Ethernet.
    
    Ref: Linux also uses `ifindex` to find related device and note it down without mac addresses.
    https://man7.org/linux/man-pages/man7/packet.7.html
    https://github.com/torvalds/linux/blob/v6.7/net/packet/af_packet.c#L3328
    https://github.com/torvalds/linux/blob/v6.7/net/packet/af_packet.c#L3264-L3265
    
    Signed-off-by: Zhe Weng <wengzhe@xiaomi.com>

--- net/pkt/pkt.h
+++ net/pkt/pkt.h
@@ -60,9 +60,7 @@ struct pkt_conn_s
 
   /* Pkt socket-specific content follows */
 
-  uint8_t    lmac[6];  /* The local Ethernet address in network byte order */
   uint8_t    ifindex;
-  uint16_t   proto;
   uint8_t    crefs;    /* Reference counts on this instance */
 
   /* Read-ahead buffering.
@@ -97,7 +95,6 @@ EXTERN const struct sock_intf_s g_pkt_sockif;
  ****************************************************************************/
 
 struct net_driver_s; /* Forward reference */
-struct eth_hdr_s;    /* Forward reference */
 struct socket;       /* Forward reference */
 
 /****************************************************************************
@@ -137,15 +134,15 @@ void pkt_free(FAR struct pkt_conn_s *conn);
  * Name: pkt_active()
  *
  * Description:
- *   Find a connection structure that is the appropriate
- *   connection to be used with the provided Ethernet header
+ *   Find a connection structure that is the appropriate connection to be
+ *   used with the provided network device
  *
  * Assumptions:
  *   This function is called from network logic at with the network locked.
  *
  ****************************************************************************/
 
-FAR struct pkt_conn_s *pkt_active(FAR struct eth_hdr_s *buf);
+FAR struct pkt_conn_s *pkt_active(FAR struct net_driver_s *dev);
 
 /****************************************************************************
  * Name: pkt_nextconn()
