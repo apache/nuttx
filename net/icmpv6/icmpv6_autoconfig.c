0168b0569624d5a72a36c3df5d687f3471d716c6
meijian <meijian@xiaomi.com>
2024-03-06 10:45:54 +0800

    [icmpv6] fix resource leak of icmpv6
    Signed-off-by: meijian <meijian@xiaomi.com>

--- net/icmpv6/icmpv6_autoconfig.c
+++ net/icmpv6/icmpv6_autoconfig.c
@@ -392,6 +392,12 @@ got_lladdr:
       ret = icmpv6_send_message(dev, false);
       if (ret < 0)
         {
+          /* Remove our wait structure from the list (we may no longer be
+           *  at the head of the list).
+           */
+
+          icmpv6_rwait_cancel(&notify);
+
           nerr("ERROR: Failed send router solicitation: %d\n", ret);
           break;
         }
