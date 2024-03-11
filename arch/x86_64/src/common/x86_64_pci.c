0659b333b43bd9557d70eaac0d253a781303b811
p-szafonimateusz <p-szafonimateusz@xiaomi.com>
2024-03-11 17:14:52 +0100

    arch/x86_64/intel64: add MSI/MSI-X support
    Add MSI and MSI-X support for intel64
    
    Signed-off-by: p-szafonimateusz <p-szafonimateusz@xiaomi.com>

--- arch/x86_64/src/common/x86_64_pci.c
+++ arch/x86_64/src/common/x86_64_pci.c
@@ -27,6 +27,8 @@
 #include <assert.h>
 #include <debug.h>
 
+#include <arch/irq.h>
+
 #include <nuttx/pci/pci.h>
 
 #include "x86_64_internal.h"
@@ -38,6 +40,9 @@
 #define PCI_CFG_ADDR      0xcf8
 #define PCI_DATA_ADDR     0xcfc
 #define PCI_CFG_EN        (1 << 31)
+
+#define X86_64_MAR_DEST    0xfee00000
+#define X86_64_MDR_TYPE    0x4000
 
 /****************************************************************************
  * Private Functions Definitions
@@ -54,17 +59,31 @@ static int x86_64_pci_read_io(struct pci_bus_s *bus, uintptr_t addr,
 static int x86_64_pci_write_io(struct pci_bus_s *bus, uintptr_t addr,
                                int size, uint32_t val);
 
+static int x86_64_pci_get_irq(struct pci_bus_s *bus, uint8_t line);
+
+static int x86_64_pci_alloc_irq(struct pci_bus_s *bus,
+                                int *irq, int num);
+static void x86_64_pci_release_irq(struct pci_bus_s *bus,
+                                   int *irq, int num);
+static int x86_64_pci_connect_irq(struct pci_bus_s *bus,
+                                  int *irq, int num,
+                                  uintptr_t *mar, uint32_t *mdr);
+
 /****************************************************************************
  * Private Data
  ****************************************************************************/
 
 static const struct pci_ops_s g_x86_64_pci_ops =
 {
-  .write    = x86_64_pci_write,
-  .read     = x86_64_pci_read,
-  .map      = x86_64_pci_map,
-  .read_io  = x86_64_pci_read_io,
-  .write_io = x86_64_pci_write_io,
+  .write       = x86_64_pci_write,
+  .read        = x86_64_pci_read,
+  .map         = x86_64_pci_map,
+  .read_io     = x86_64_pci_read_io,
+  .write_io    = x86_64_pci_write_io,
+  .get_irq     = x86_64_pci_get_irq,
+  .alloc_irq   = x86_64_pci_alloc_irq,
+  .release_irq = x86_64_pci_release_irq,
+  .connect_irq = x86_64_pci_connect_irq,
 };
 
 static struct pci_controller_s g_x86_64_pci =
@@ -279,6 +298,126 @@ static uintptr_t x86_64_pci_map(struct pci_bus_s *bus, uintptr_t start,
                  X86_PAGE_PRESENT | X86_PAGE_NOCACHE | X86_PAGE_GLOBAL);
 
   return ret < 0 ? 0 : start;
+}
+
+/****************************************************************************
+ * Name: x86_64_pci_get_irq
+ *
+ * Description:
+ *  Get interrupt number associated with a given INTx line.
+ *
+ * Input Parameters:
+ *   bus  - Bus that PCI device resides
+ *   line - activated PCI legacy interrupt line
+ *
+ * Returned Value:
+ *   Return interrupt number associated with a given INTx
+ *
+ ****************************************************************************/
+
+static int x86_64_pci_get_irq(struct pci_bus_s *bus, uint8_t line)
+{
+  UNUSED(bus);
+
+  return IRQ0 + line;
+}
+
+/****************************************************************************
+ * Name: x86_64_pci_alloc_irq
+ *
+ * Description:
+ *  Allocate interrupts for MSI/MSI-X vector.
+ *
+ * Input Parameters:
+ *   bus - Bus that PCI device resides
+ *   irq - allocated vectors array
+ *   num - number of vectors to allocate
+ *
+ * Returned Value:
+ *   >0: success, return number of allocated vectors,
+ *   <0: A negative value errno
+ *
+ ****************************************************************************/
+
+static int x86_64_pci_alloc_irq(struct pci_bus_s *bus, int *irq, int num)
+{
+  int tmp = 0;
+  int i   = 0;
+
+  /* Try to get irq */
+
+  tmp = up_alloc_irq_msi(&num);
+  if (tmp < 0)
+    {
+      return tmp;
+    }
+
+  /* Copy allocated interrupts */
+
+  for (i = 0; i < num; i++)
+    {
+      irq[i] = tmp++;
+    }
+
+  return num;
+}
+
+/****************************************************************************
+ * Name: x86_64_pci_release_irq
+ *
+ * Description:
+ *  Allocate interrupts for MSI/MSI-X vector.
+ *
+ * Input Parameters:
+ *   bus - Bus that PCI device resides
+ *   irq - vectors array to release
+ *   num - number of vectors in array
+ *
+ * Returned Value:
+ *   None
+ *
+ ****************************************************************************/
+
+static void x86_64_pci_release_irq(struct pci_bus_s *bus, int *irq, int num)
+{
+  up_release_irq_msi(irq, num);
+}
+
+/****************************************************************************
+ * Name: x86_64_pci_connect_irq
+ *
+ * Description:
+ *  Connect interrupt for MSI/MSI-X.
+ *
+ * Input Parameters:
+ *   bus - Bus that PCI device resides
+ *   irq - vectors array
+ *   num - number of vectors in array
+ *   mar - returned value for Message Address Register
+ *   mdr - returned value for Message Data Register
+ *
+ * Returned Value:
+ *   >0: success, 0: A positive value errno
+ *
+ ****************************************************************************/
+
+static int x86_64_pci_connect_irq(struct pci_bus_s *bus, int *irq, int num,
+                                  uintptr_t *mar, uint32_t *mdr)
+{
+  UNUSED(num);
+
+  if (mar != NULL)
+    {
+      *mar = X86_64_MAR_DEST |
+        (up_apic_cpu_id() << PCI_MSI_DATA_CPUID_SHIFT);
+    }
+
+  if (mdr != NULL)
+    {
+      *mdr = X86_64_MDR_TYPE | irq[0];
+    }
+
+  return OK;
 }
 
 /****************************************************************************
