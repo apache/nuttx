17cb6ed63d7b3d4ab72f3882039835ddb903782b
Xiang Xiao <xiaoxiang@xiaomi.com>
2024-03-12 13:48:07 +0800

    sched/instrument: rename stack_record.c to stack_monitor.c
    to align with gprof naming convention.
    
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- sched/instrument/instrument.c
+++ sched/instrument/instrument.c
@@ -31,7 +31,7 @@
  ****************************************************************************/
 
 #if CONFIG_SCHED_STACK_RECORD > 0
-extern struct instrument_s g_stack_record;
+extern struct instrument_s g_stack_monitor;
 #endif
 
 /****************************************************************************
@@ -55,6 +55,6 @@ extern struct instrument_s g_stack_record;
 void instrument_initialize(void)
 {
 #if CONFIG_SCHED_STACK_RECORD > 0
-  instrument_register(&g_stack_record);
+  instrument_register(&g_stack_monitor);
 #endif
 }
