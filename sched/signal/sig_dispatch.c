822967fe24f15c9349e138b299e4d31d9fc35cbb
chao an <anchao@lixiang.com>
2024-03-14 17:35:30 +0800

    sched/signal: move unblock signal info to stack
    struct tcb_s will reduce by 24 bytes
    
    Signed-off-by: chao an <anchao@lixiang.com>

--- sched/signal/sig_dispatch.c
+++ sched/signal/sig_dispatch.c
@@ -398,7 +398,7 @@ int nxsig_tcbdispatch(FAR struct tcb_s *stcb, siginfo_t *info)
           (masked == 0 ||
            nxsig_ismember(&stcb->sigwaitmask, info->si_signo)))
         {
-          memcpy(&stcb->sigunbinfo, info, sizeof(siginfo_t));
+          memcpy(stcb->sigunbinfo, info, sizeof(siginfo_t));
           sigemptyset(&stcb->sigwaitmask);
 
           if (WDOG_ISACTIVE(&stcb->waitdog))
@@ -461,7 +461,7 @@ int nxsig_tcbdispatch(FAR struct tcb_s *stcb, siginfo_t *info)
 
       if (stcb->task_state == TSTATE_WAIT_SIG)
         {
-          memcpy(&stcb->sigunbinfo, info, sizeof(siginfo_t));
+          memcpy(stcb->sigunbinfo, info, sizeof(siginfo_t));
           sigemptyset(&stcb->sigwaitmask);
 
           if (WDOG_ISACTIVE(&stcb->waitdog))
