877e46299023297a4c38374746770d23d9c6241b
wangyongrong <wangyongrong@xiaomi.com>
2024-03-25 11:11:33 +0800

    vitrio-pci.c: add virtio-pci transport support for Nuttx
    1. only support pci modern device;
    2. need the pci controller support MSI/MSI-X;
    It has been verified based on virtio-rng and virtio-net.
    
    Signed-off-by: Yongrong Wang <wangyongrong@xiaomi.com>
    Signed-off-by: Bowen Wang <wangbowen6@xiaomi.com>

--- drivers/pci/pci_drivers.c
+++ drivers/pci/pci_drivers.c
@@ -29,6 +29,7 @@
 #include <nuttx/pci/pci_qemu_test.h>
 #include <nuttx/rptun/rptun_ivshmem.h>
 #include <nuttx/rpmsg/rpmsg_virtio_ivshmem.h>
+#include <nuttx/virtio/virtio-pci.h>
 
 #include "pci_drivers.h"
 
@@ -100,6 +101,16 @@ int pci_register_drivers(void)
   if (ret < 0)
     {
       pcierr("pci_register_qemu_edu_driver failed, ret=%d\n", ret);
+    }
+#endif
+
+  /* Initialization virtio pci driver */
+
+#ifdef CONFIG_DRIVERS_VIRTIO_PCI
+  ret = register_virtio_pci_driver();
+  if (ret < 0)
+    {
+      pcierr("register_virtio_pci_driver failed, ret=%d\n", ret);
     }
 #endif
 
