b1da7a2b0f08141f136dd579a56300b01f151435
Gao Jiawei <gaojiawei@xiaomi.com>
2024-03-27 09:25:11 +0800

    sched/wdog: remove unreachable branch
    Signed-off-by: Gao Jiawei <gaojiawei@xiaomi.com>

--- sched/wdog/wd_start.c
+++ sched/wdog/wd_start.c
@@ -218,10 +218,6 @@ int wd_start(FAR struct wdog_s *wdog, sclock_t delay,
     {
       delay = 1;
     }
-  else if (++delay <= 0)
-    {
-      delay--;
-    }
 
 #ifdef CONFIG_SCHED_TICKLESS
   /* Cancel the interval timer that drives the timing events.  This will
