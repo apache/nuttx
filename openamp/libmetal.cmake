a2d941ef68249c055ffb1750fdfaf810a5ff2cf9
Yanfeng Liu <yfliu2008@qq.com>
2024-03-28 10:54:13 +0800

    openamp/libmetal.cmake: sync with libmetal.defs
    This adds missing patches so that to be in sync with libmetal.defs
    
    Signed-off-by: Yanfeng Liu <yfliu2008@qq.com>

--- openamp/libmetal.cmake
+++ openamp/libmetal.cmake
@@ -36,6 +36,10 @@ if(NOT EXISTS ${CMAKE_CURRENT_LIST_DIR}/libmetal)
     PATCH_COMMAND
       patch -p0 -d ${CMAKE_CURRENT_LIST_DIR} <
       ${CMAKE_CURRENT_LIST_DIR}/0001-libmetal-add-metal_list_for_each_safe-support.patch
+      && patch -p0 -d ${CMAKE_CURRENT_LIST_DIR} <
+      ${CMAKE_CURRENT_LIST_DIR}/0002-libmetal-nuttx-io.c-align-access-when-read-write-siz.patch
+      && patch -p0 -d ${CMAKE_CURRENT_LIST_DIR} <
+      ${CMAKE_CURRENT_LIST_DIR}/0003-libmetal-nuttx-io.c-Fix-void-pointer-arithmetic-in-a.patch
     DOWNLOAD_NO_PROGRESS true
     TIMEOUT 30)
 
