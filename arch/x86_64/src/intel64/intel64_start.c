30226901c00c426d74f792feb731b835e6f87281
p-szafonimateusz <p-szafonimateusz@xiaomi.com>
2024-04-03 11:52:47 +0200

    arch/x86_64: add simple ACPI parser
    add simple ACPI parser for intel64.
    
    For now RSDP signature can be found in BIOS legacy region or can be provided by multiboot2
    
    Signed-off-by: p-szafonimateusz <p-szafonimateusz@xiaomi.com>

--- arch/x86_64/src/intel64/intel64_start.c
+++ arch/x86_64/src/intel64/intel64_start.c
@@ -29,6 +29,8 @@
 #include <arch/board/board.h>
 #include <arch/multiboot2.h>
 
+#include <arch/acpi.h>
+
 #include "x86_64_internal.h"
 
 #include "intel64_lowsetup.h"
@@ -41,6 +43,7 @@
 
 uint32_t g_mb_magic __attribute__((section(".loader.bss")));
 uint32_t g_mb_info_struct __attribute__((section(".loader.bss")));
+uintptr_t g_acpi_rsdp = 0;
 
 /****************************************************************************
  * Private Functions
@@ -75,6 +78,22 @@ static void x86_64_mb2_config(void)
         {
           case MULTIBOOT_TAG_TYPE_EFI64:
             {
+              break;
+            }
+
+          case MULTIBOOT_TAG_TYPE_ACPI_OLD:
+            {
+              struct multiboot_tag_old_acpi *acpi
+                  = (struct multiboot_tag_old_acpi *)tag;
+              g_acpi_rsdp = (uintptr_t)acpi->rsdp;
+              break;
+            }
+
+          case MULTIBOOT_TAG_TYPE_ACPI_NEW:
+            {
+              struct multiboot_tag_new_acpi *acpi =
+                (struct multiboot_tag_new_acpi *)tag;
+              g_acpi_rsdp = (uintptr_t)acpi->rsdp;
               break;
             }
 
@@ -135,6 +154,12 @@ void __nxstart(void)
   /* Low-level, pre-OS initialization */
 
   intel64_lowsetup();
+
+#ifdef CONFIG_ARCH_X86_64_ACPI
+  /* Initialize ACPI */
+
+  acpi_init(g_acpi_rsdp);
+#endif
 
   /* perform board-specific initializations */
 
