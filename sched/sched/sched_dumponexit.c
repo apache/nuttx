55e3cf7e2d9e12a1655a47f9b3fb1f74b3ef591e
Xiang Xiao <xiaoxiang@xiaomi.com>
2024-04-17 13:34:45 +0800

    sched: Move dump.c to sched_dumponexit.c
    The dump.c is a part of sched module, so move it to sched directory.
    
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- sched/sched/sched_dumponexit.c
+++ sched/sched/sched_dumponexit.c
@@ -1,5 +1,5 @@
 /****************************************************************************
- * sched/misc/dump.c
+ * sched/sched/sched_dumponexit.c
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
