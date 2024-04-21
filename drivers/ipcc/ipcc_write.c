a5aea615370743d9575e01ce3f73dd956b16be45
Xiang Xiao <xiaoxiang@xiaomi.com>
2024-04-21 23:47:54 +0800

    driver: Post sempahore only when it's value is smaller than one
    to avoid waking up the waiting thread redundantly.
    
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- drivers/ipcc/ipcc_write.c
+++ drivers/ipcc/ipcc_write.c
@@ -90,11 +90,10 @@ void ipcc_txfree_notify(FAR struct ipcc_driver_s *priv)
 
   /* Notify all blocked writers that data is available to write */
 
-  do
+  while (nxsem_get_value(&priv->txsem, &semval) >= 0 && semval <= 0)
     {
       nxsem_post(&priv->txsem);
     }
-  while (nxsem_get_value(&priv->txsem, &semval) == 0 && semval <= 0);
 }
 
 /****************************************************************************
