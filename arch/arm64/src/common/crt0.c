dd1612c30599199f20ab3807c7049c2d32111245
Masayuki Ishikawa <masayuki.ishikawa@gmail.com>
2024-04-24 18:44:05 +0900

    Revert "tools/export: fix names for app linker script and program entry."
    This reverts commit 5c3fc2796b50a9e8c6a60dbd8560f38755bc0c3d.

--- arch/arm64/src/common/crt0.c
+++ arch/arm64/src/common/crt0.c
@@ -91,7 +91,7 @@ static void sig_trampoline(void)
  ****************************************************************************/
 
 /****************************************************************************
- * Name: __start
+ * Name: _start
  *
  * Description:
  *   This function is the low level entry point into the main thread of
@@ -110,7 +110,7 @@ static void sig_trampoline(void)
  *
  ****************************************************************************/
 
-void __start(int argc, char *argv[])
+void _start(int argc, char *argv[])
 {
   int ret;
 
--- tools/mkexport.sh
+++ tools/mkexport.sh
@@ -182,15 +182,10 @@ cp "${TOPDIR}/tools/incdir.c" "${EXPORTDIR}/tools/."
 
 # Copy the board specific linker if found, or use the default when not.
 
-APPLD=gnu-elf.ld
-if [ -f "${BOARDDIR}/scripts/${APPLD}" ]; then
-  cp -f "${BOARDDIR}/scripts/${APPLD}" "${EXPORTDIR}/scripts/."
+if [ -f "${BOARDDIR}/scripts/gnu-elf.ld" ]; then
+  cp -f "${BOARDDIR}/scripts/gnu-elf.ld" "${EXPORTDIR}/scripts/."
 else
-  cp -f "${TOPDIR}/binfmt/libelf/${APPLD}" "${EXPORTDIR}/scripts/."
-fi
-
-if [ "${NUTTX_BUILD}" = "kernel" ]; then
-  LDNAME=${APPLD}
+  cp -f "${TOPDIR}/binfmt/libelf/gnu-elf.ld" "${EXPORTDIR}/scripts/."
 fi
 
 # Copy the board config script
@@ -265,7 +260,6 @@ echo "LDELFFLAGS       = ${LDELFFLAGS}" >>"${EXPORTDIR}/scripts/Make.defs"
 echo "NUTTX_ARCH       = ${NUTTX_ARCH}" >>"${EXPORTDIR}/scripts/Make.defs"
 echo "NUTTX_ARCH_CHIP  = ${NUTTX_ARCH_CHIP}" >>"${EXPORTDIR}/scripts/Make.defs"
 echo "NUTTX_BOARD      = ${NUTTX_BOARD}" >>"${EXPORTDIR}/scripts/Make.defs"
-echo "NUTTX_BUILD      = ${NUTTX_BUILD}" >>"${EXPORTDIR}/scripts/Make.defs"
 
 echo "set(ARCHCFLAGS          \"${ARCHCFLAGS}\")"       > "${EXPORTDIR}/scripts/target.cmake"
 echo "set(ARCHCPUFLAGS        \"${ARCHCPUFLAGS}\")"     >>"${EXPORTDIR}/scripts/target.cmake"
@@ -295,7 +289,6 @@ echo "set(LDELFFLAGS          \"${LDELFFLAGS}\")"       >>"${EXPORTDIR}/scripts/
 echo "set(NUTTX_ARCH          \"${NUTTX_ARCH}\")"       >>"${EXPORTDIR}/scripts/target.cmake"
 echo "set(NUTTX_ARCH_CHIP     \"${NUTTX_ARCH_CHIP}\")"  >>"${EXPORTDIR}/scripts/target.cmake"
 echo "set(NUTTX_BOARD         \"${NUTTX_BOARD}\")"      >>"${EXPORTDIR}/scripts/target.cmake"
-echo "set(NUTTX_BUILD         \"${NUTTX_BUILD}\")"      >>"${EXPORTDIR}/scripts/target.cmake"
 
 
 # Additional compilation options when the kernel is built
