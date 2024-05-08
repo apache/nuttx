c978f2fe4c7f29eba1dacb3065572030a7a0578c
dongjiuzhu1 <dongjiuzhu1@xiaomi.com>
2024-05-08 19:49:11 +0800

    drivers/mtd/bch: fix size_t overflow when offset > 4GB
    Signed-off-by: dongjiuzhu1 <dongjiuzhu1@xiaomi.com>

--- include/nuttx/drivers/drivers.h
+++ include/nuttx/drivers/drivers.h
@@ -231,7 +231,7 @@ int bchlib_teardown(FAR void *handle);
  *
  ****************************************************************************/
 
-ssize_t bchlib_read(FAR void *handle, FAR char *buffer, size_t offset,
+ssize_t bchlib_read(FAR void *handle, FAR char *buffer, off_t offset,
                     size_t len);
 
 /****************************************************************************
@@ -243,7 +243,7 @@ ssize_t bchlib_read(FAR void *handle, FAR char *buffer, size_t offset,
  *
  ****************************************************************************/
 
-ssize_t bchlib_write(FAR void *handle, FAR const char *buffer, size_t offset,
+ssize_t bchlib_write(FAR void *handle, FAR const char *buffer, off_t offset,
                      size_t len);
 
 /****************************************************************************
