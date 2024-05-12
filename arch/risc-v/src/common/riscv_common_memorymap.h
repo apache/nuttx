apache/nuttx

commit bc022f8cd841b6b9f6507b7aca6abbab931345b3

arch/risc-v: remove g_cpux_idlestack
As all the board allocate idle stack from _ebss. The idle stack
layout is fix and can be computed directly by using _ebss. There
is no need to use g_cpux_idlestack array anymore, remove it.

Signed-off-by: Inochi Amaoto <inochiama@outlook.com>

arch/risc-v/src/common/riscv_common_memorymap.h
@@ -67,7 +67,8 @@ EXTERN uintptr_t g_idle_topstack;
 
 /* Address of per-cpu idle stack base */
 
-EXTERN const uint8_t *g_cpux_idlestack[CONFIG_SMP_NCPUS];
+#define g_cpux_idlestack(cpuid) \
+   (g_idle_topstack - SMP_STACK_SIZE * ((cpuid) + 1))
 
 /* Address of the saved user stack pointer */
 

arch/risc-v/src/bl602/bl602_start.c
@@ -148,10 +148,6 @@ void bfl_main(void)
 
   asm volatile("csrw mtvec, %0" ::"r"((uintptr_t)exception_common + 2));
 
-  /* Setup base stack */
-
-  riscv_set_basestack((uintptr_t)_ebss, SMP_STACK_SIZE);
-
   /* Configure the UART so we can get debug output */
 
   bl602_lowsetup();

arch/risc-v/src/bl808/bl808_start.c
@@ -267,10 +267,6 @@ void bl808_start(int mhartid)
 
       bl808_clear_bss();
 
-      /* Setup base stack */
-
-      riscv_set_basestack(BL808_IDLESTACK_BASE, SMP_STACK_SIZE);
-
       /* Copy the RAM Disk */
 
       bl808_copy_ramdisk();

arch/risc-v/src/c906/c906_start.c
@@ -79,10 +79,6 @@ void __c906_start(uint32_t mhartid)
       *dest++ = 0;
     }
 
-  /* Setup base stack */
-
-  riscv_set_basestack(C906_IDLESTACK_BASE, SMP_STACK_SIZE);
-
   /* Move the initialized data section from his temporary holding spot in
    * FLASH into the correct place in SRAM.  The correct place in SRAM is
    * give by _sdata and _edata.  The temporary location is in FLASH at the

arch/risc-v/src/common/espressif/esp_start.c
@@ -453,10 +453,6 @@ void __esp_start(void)
       *dest++ = 0;
     }
 
-  /* Setup base stack */
-
-  riscv_set_basestack((uintptr_t)_ebss, SMP_STACK_SIZE);
-
   /* Setup the syscall table needed by the ROM code */
 
   esp_setup_syscall_table();

arch/risc-v/src/common/riscv_cpuidlestack.c
@@ -50,8 +50,6 @@
 uintptr_t g_idle_topstack = (uintptr_t)_ebss +
                                        SMP_STACK_SIZE * CONFIG_SMP_NCPUS;
 
-const uint8_t *g_cpux_idlestack[CONFIG_SMP_NCPUS];
-
 /****************************************************************************
  * Public Functions
  ****************************************************************************/
@@ -109,7 +107,7 @@ int up_cpu_idlestack(int cpu, struct tcb_s *tcb, size_t stack_size)
 
   /* Get the top of the stack */
 
-  stack_alloc = (uintptr_t)g_cpux_idlestack[cpu];
+  stack_alloc = (uintptr_t)g_cpux_idlestack(cpu);
   DEBUGASSERT(stack_alloc != 0 && STACK_ISALIGNED(stack_alloc));
 
   tcb->adj_stack_size  = SMP_STACK_SIZE;

arch/risc-v/src/common/riscv_initialstate.c
@@ -97,7 +97,7 @@ void up_initial_state(struct tcb_s *tcb)
 
   if (tcb->pid == IDLE_PROCESS_ID)
     {
-      tcb->stack_alloc_ptr = (void *)g_cpux_idlestack[riscv_mhartid()];
+      tcb->stack_alloc_ptr = (void *)g_cpux_idlestack(riscv_mhartid());
       tcb->stack_base_ptr  = tcb->stack_alloc_ptr;
       tcb->adj_stack_size  = SMP_STACK_SIZE;
 

arch/risc-v/src/common/riscv_internal.h
@@ -331,18 +331,6 @@ int riscv_check_pmp_access(uintptr_t attr, uintptr_t base, uintptr_t size);
 int riscv_configured_pmp_regions(void);
 int riscv_next_free_pmp_region(void);
 
-/* RISC-V Memorymap Config **************************************************/
-
-static inline void riscv_set_basestack(uintptr_t base, uintptr_t size)
-{
-  unsigned int i;
-
-  for (i = 0; i < CONFIG_SMP_NCPUS; i++)
-    {
-      g_cpux_idlestack[i] = (const uint8_t *)(base + size * i);
-    }
-}
-
 /* RISC-V SBI wrappers ******************************************************/
 
 #ifdef CONFIG_ARCH_USE_S_MODE

commit 9c22a77078c369e2eb7e281c3c35d2e330572174

arch/risc-v: fix idle stack assign order
The bc022f8cd8 introduces a static way to calculate idle stack
address for risc-v platform. However, it uses the reverse order
to access idle stack, which breaks boards with smp configuration.

Correct the idle stack order of g_cpux_idlestack.

Fixes: bc022f8cd8 ("arch/risc-v: unify idle stack calculation")
Signed-off-by: Inochi Amaoto <inochiama@outlook.com>

arch/risc-v/src/common/riscv_common_memorymap.h
@@ -68,7 +68,7 @@ EXTERN uintptr_t g_idle_topstack;
 /* Address of per-cpu idle stack base */
 
 #define g_cpux_idlestack(cpuid) \
-   (g_idle_topstack - SMP_STACK_SIZE * ((cpuid) + 1))
+   (g_idle_topstack - SMP_STACK_SIZE * (CONFIG_SMP_NCPUS - (cpuid)))
 
 /* Address of the saved user stack pointer */
 