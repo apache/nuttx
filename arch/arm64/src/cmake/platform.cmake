d1789d84e8e1d94237cf7661d5ae818823f6e564
simbit18 <101105604+simbit18@users.noreply.github.com>
2024-05-13 16:17:37 +0200

    fix nxstyle
    fix Relative file path does not match actual file.

--- arch/arm64/src/cmake/platform.cmake
+++ arch/arm64/src/cmake/platform.cmake
@@ -1,5 +1,5 @@
 # ##############################################################################
-# ./arch/arm64/src/cmake/platform.cmake
+# arch/arm64/src/cmake/platform.cmake
 #
 # Licensed to the Apache Software Foundation (ASF) under one or more contributor
 # license agreements.  See the NOTICE file distributed with this work for
--- arch/risc-v/src/cmake/platform.cmake
+++ arch/risc-v/src/cmake/platform.cmake
@@ -1,5 +1,5 @@
 # ##############################################################################
-# ./arch/risc-v/src/cmake/platform.cmake
+# arch/risc-v/src/cmake/platform.cmake
 #
 # Licensed to the Apache Software Foundation (ASF) under one or more contributor
 # license agreements.  See the NOTICE file distributed with this work for
