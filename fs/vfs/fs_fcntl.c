74da742557a126e3206ecaeb709b07828bd69348
chenrun1 <chenrun1@xiaomi.com>
2024-05-15 14:31:28 +0800

    filelock:Move filelock call to ioctl
    Forward the implementation originally placed in fcntl to ioctl. in order
    to achieve cross-core.
    
    Signed-off-by: chenrun1 <chenrun1@xiaomi.com>

--- fs/vfs/fs_fcntl.c
+++ fs/vfs/fs_fcntl.c
@@ -35,7 +35,6 @@
 #include <nuttx/fs/fs.h>
 
 #include "inode/inode.h"
-#include "lock.h"
 
 /****************************************************************************
  * Private Functions
@@ -197,12 +196,8 @@ static int file_vfcntl(FAR struct file *filep, int cmd, va_list ap)
          */
 
         {
-          FAR struct flock *flock = va_arg(ap, FAR struct flock *);
-          ret = file_ioctl(filep, FIOC_GETLK, flock);
-          if (ret < 0)
-            {
-              ret = file_getlk(filep, flock);
-            }
+          ret = file_ioctl(filep, FIOC_GETLK,
+                           va_arg(ap, FAR struct flock *));
         }
 
         break;
@@ -218,12 +213,8 @@ static int file_vfcntl(FAR struct file *filep, int cmd, va_list ap)
          */
 
         {
-          FAR struct flock *flock = va_arg(ap, FAR struct flock *);
-          ret = file_ioctl(filep, FIOC_SETLK, flock);
-          if (ret < 0)
-            {
-              ret = file_setlk(filep, flock, true);
-            }
+          ret = file_ioctl(filep, FIOC_SETLK,
+                           va_arg(ap, FAR struct flock *));
         }
 
         break;
@@ -238,12 +229,8 @@ static int file_vfcntl(FAR struct file *filep, int cmd, va_list ap)
          */
 
         {
-          FAR struct flock *flock = va_arg(ap, FAR struct flock *);
-          ret = file_ioctl(filep, FIOC_SETLKW, flock);
-          if (ret < 0)
-            {
-              ret = file_setlk(filep, flock, false);
-            }
+          ret = file_ioctl(filep, FIOC_SETLKW,
+                           va_arg(ap, FAR struct flock *));
         }
 
         break;
