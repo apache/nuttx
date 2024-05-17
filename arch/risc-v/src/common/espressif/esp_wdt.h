aa0bb55529bdcbbb62c3cfbbb61a51ad95a9d220
Eren Terzioglu <eren.terzioglu@espressif.com>
2024-05-17 14:37:33 +0300

    esp32[c3|c6|h2]: Add RWDT support

--- arch/risc-v/src/common/espressif/esp_wdt.h
+++ arch/risc-v/src/common/espressif/esp_wdt.h
@@ -28,6 +28,18 @@
 #include <nuttx/config.h>
 
 /****************************************************************************
+ * Public Types
+ ****************************************************************************/
+
+/* Instances of Watchdog Timer  */
+
+enum esp_wdt_inst_e
+{
+  ESP_WDT_MWDT = 0,  /* Main System Watchdog Timer (MWDT) of Timer Group 0 */
+  ESP_WDT_RWDT       /* RTC Watchdog Timer (RWDT) */
+};
+
+/****************************************************************************
  * Public Function Prototypes
  ****************************************************************************/
 
@@ -38,7 +50,9 @@
  *   Initialize the watchdog timer.
  *
  * Input Parameters:
- *   None.
+ *   devpath - The full path to the watchdog.  This should
+ *             be of the form /dev/watchdogX
+ *   wdt_id  - A Watchdog Timer instance to be initialized.
  *
  * Returned Values:
  *   Zero (OK) is returned on success; a negated errno value is returned on
@@ -46,6 +60,6 @@
  *
  ****************************************************************************/
 
-int esp_wdt_initialize(void);
+int esp_wdt_initialize(const char *devpath, enum esp_wdt_inst_e wdt_id);
 
 #endif /* __ARCH_RISCV_SRC_COMMON_ESPRESSIF_ESP_WDT_H */
