apache/nuttx

commit b10d6be17a63d96d287d970ae180b33d34d93202

net:Add check for address binding
Signed-off-by: wangchen <wangchen41@xiaomi.com>

net/udp/udp_conn.c
@@ -806,6 +806,7 @@ int udp_bind(FAR struct udp_conn_s *conn, FAR const struct sockaddr *addr)
 {
   uint16_t portno;
   int ret;
+  FAR struct net_driver_s *dev;
 
 #if defined(CONFIG_NET_IPv4) && defined(CONFIG_NET_IPv6)
   if (conn->domain != addr->sa_family)
@@ -823,6 +824,29 @@ int udp_bind(FAR struct udp_conn_s *conn, FAR const struct sockaddr *addr)
     {
       FAR const struct sockaddr_in *inaddr =
         (FAR const struct sockaddr_in *)addr;
+
+      if (!net_ipv4addr_cmp(inaddr->sin_addr.s_addr, INADDR_ANY) &&
+        !net_ipv4addr_cmp(inaddr->sin_addr.s_addr, HTONL(INADDR_LOOPBACK)) &&
+        !net_ipv4addr_cmp(inaddr->sin_addr.s_addr, INADDR_BROADCAST) &&
+        !IN_MULTICAST(NTOHL(inaddr->sin_addr.s_addr)))
+        {
+          ret = -EADDRNOTAVAIL;
+
+          for (dev = g_netdevices; dev; dev = dev->flink)
+            {
+              if (net_ipv4addr_cmp(inaddr->sin_addr.s_addr, dev->d_ipaddr))
+                {
+                  ret = 0;
+                  break;
+                }
+            }
+
+          if (ret == -EADDRNOTAVAIL)
+            {
+              net_unlock();
+              return ret;
+            }
+        }
 
       /* Get the port number that we are binding to */
 
@@ -844,6 +868,33 @@ int udp_bind(FAR struct udp_conn_s *conn, FAR const struct sockaddr *addr)
     {
       FAR const struct sockaddr_in6 *inaddr =
         (FAR const struct sockaddr_in6 *)addr;
+
+      if (!net_ipv6addr_cmp(inaddr->sin6_addr.in6_u.u6_addr16,
+                        g_ipv6_unspecaddr) &&
+      !net_ipv6addr_cmp(inaddr->sin6_addr.in6_u.u6_addr16,
+                        g_ipv6_loopback) &&
+      !net_ipv6addr_cmp(inaddr->sin6_addr.in6_u.u6_addr16,
+                        g_ipv6_allnodes) &&
+      !net_ipv6addr_cmp(inaddr->sin6_addr.in6_u.u6_addr16, g_ipv6_allnodes))
+        {
+          ret = -EADDRNOTAVAIL;
+
+          for (dev = g_netdevices; dev; dev = dev->flink)
+            {
+              if (net_ipv6addr_cmp(inaddr->sin6_addr.in6_u.u6_addr16,
+                                  dev->d_ipv6addr))
+                {
+                  ret = 0;
+                  break;
+                }
+            }
+
+          if (ret == -EADDRNOTAVAIL)
+            {
+              net_unlock();
+              return ret;
+            }
+        }
 
       /* Get the port number that we are binding to */
 

net/inet/inet.h
@@ -71,6 +71,7 @@ extern "C"
 
 #ifdef CONFIG_NET_IPv6
 EXTERN const net_ipv6addr_t g_ipv6_unspecaddr;       /* An address of all zeroes */
+EXTERN const net_ipv6addr_t g_ipv6_loopback;         /* An address of loopback */
 EXTERN const net_ipv6addr_t g_ipv6_allnodes;         /* All link local nodes */
 
 #if defined(CONFIG_NET_ICMPv6_AUTOCONF) || defined(CONFIG_NET_ICMPv6_ROUTER) || \

net/inet/inet_globals.c
@@ -47,6 +47,12 @@ const net_ipv6addr_t g_ipv6_unspecaddr =  /* An address of all zeroes */
   0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000
 };
 
+const net_ipv6addr_t g_ipv6_loopback =    /* An address of loopback */
+{
+  0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,
+  HTONS(0x0001)
+};
+
 /* IPv6 Multi-cast IP addresses.  See RFC 2375 */
 
 const net_ipv6addr_t g_ipv6_allnodes =    /* All link local nodes */

net/tcp/tcp_conn.c
@@ -69,6 +69,7 @@
 #include "arp/arp.h"
 #include "icmpv6/icmpv6.h"
 #include "nat/nat.h"
+#include "netdev/netdev.h"
 
 /****************************************************************************
  * Private Data
@@ -320,6 +321,7 @@ static inline int tcp_ipv4_bind(FAR struct tcp_conn_s *conn,
 {
   int port;
   int ret;
+  FAR struct net_driver_s *dev;
 
   /* Verify or select a local port and address */
 
@@ -329,6 +331,29 @@ static inline int tcp_ipv4_bind(FAR struct tcp_conn_s *conn,
     {
       net_unlock();
       return -EINVAL;
+    }
+
+  if (!net_ipv4addr_cmp(addr->sin_addr.s_addr, INADDR_ANY) &&
+    !net_ipv4addr_cmp(addr->sin_addr.s_addr, HTONL(INADDR_LOOPBACK)) &&
+    !net_ipv4addr_cmp(addr->sin_addr.s_addr, INADDR_BROADCAST) &&
+    !IN_MULTICAST(NTOHL(addr->sin_addr.s_addr)))
+    {
+      ret = -EADDRNOTAVAIL;
+
+      for (dev = g_netdevices; dev; dev = dev->flink)
+        {
+          if (net_ipv4addr_cmp(addr->sin_addr.s_addr, dev->d_ipaddr))
+            {
+              ret = 0;
+              break;
+            }
+        }
+
+      if (ret == -EADDRNOTAVAIL)
+        {
+          net_unlock();
+          return ret;
+        }
     }
 
   /* Verify or select a local port (network byte order) */
@@ -391,6 +416,7 @@ static inline int tcp_ipv6_bind(FAR struct tcp_conn_s *conn,
 {
   int port;
   int ret;
+  FAR struct net_driver_s *dev;
 
   /* Verify or select a local port and address */
 
@@ -400,6 +426,33 @@ static inline int tcp_ipv6_bind(FAR struct tcp_conn_s *conn,
     {
       net_unlock();
       return -EINVAL;
+    }
+
+  if (!net_ipv6addr_cmp(addr->sin6_addr.in6_u.u6_addr16,
+                        g_ipv6_unspecaddr) &&
+      !net_ipv6addr_cmp(addr->sin6_addr.in6_u.u6_addr16,
+                        g_ipv6_loopback) &&
+      !net_ipv6addr_cmp(addr->sin6_addr.in6_u.u6_addr16,
+                        g_ipv6_allnodes) &&
+      !net_ipv6addr_cmp(addr->sin6_addr.in6_u.u6_addr16, g_ipv6_allnodes))
+    {
+      ret = -EADDRNOTAVAIL;
+
+      for (dev = g_netdevices; dev; dev = dev->flink)
+        {
+          if (net_ipv6addr_cmp(addr->sin6_addr.in6_u.u6_addr16,
+                              dev->d_ipv6addr))
+            {
+              ret = 0;
+              break;
+            }
+        }
+
+      if (ret == -EADDRNOTAVAIL)
+        {
+          net_unlock();
+          return ret;
+        }
     }
 
   /* Verify or select a local port (network byte order) */

commit 9e26c5bc80651822c2e5bae869eb4e96800cf3d6

net/udp: fix net_[un]lock() in udp_bind
net_unlock() called without a previous call to net_lock().

Error introduced by b10d6be17a6

net/udp/udp_conn.c
@@ -816,6 +816,10 @@ int udp_bind(FAR struct udp_conn_s *conn, FAR const struct sockaddr *addr)
     }
 #endif
 
+  /* Interrupts must be disabled while access the UDP connection list */
+
+  net_lock();
+
 #ifdef CONFIG_NET_IPv4
 #ifdef CONFIG_NET_IPv6
   if (conn->domain == PF_INET)
@@ -928,10 +932,6 @@ int udp_bind(FAR struct udp_conn_s *conn, FAR const struct sockaddr *addr)
     }
   else
     {
-      /* Interrupts must be disabled while access the UDP connection list */
-
-      net_lock();
-
       /* Is any other UDP connection already bound to this address
        * and port ?
        */
@@ -958,10 +958,9 @@ int udp_bind(FAR struct udp_conn_s *conn, FAR const struct sockaddr *addr)
         {
           ret         = -EADDRINUSE;
         }
-
-      net_unlock();
     }
 
+  net_unlock();
   return ret;
 }
 