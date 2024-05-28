081ec954c9250f081bebc1191795f0df0660ac1a
simbit18 <101105604+simbit18@users.noreply.github.com>
2024-05-28 12:14:21 +0200

    tools/ci/platforms: linux.sh and ubuntu.sh fix mkdir: wamrc: File exists
    mkdir wamrc -> mkdir -p wamrc

--- tools/ci/platforms/linux.sh
+++ tools/ci/platforms/linux.sh
@@ -308,7 +308,7 @@ wasi_sdk() {
     wasibasefile=wasi-sdk-19.0-linux
     wasmbasefile=wamrc-1.1.2-x86_64-ubuntu-20.04
     cd "${NUTTXTOOLS}"
-    mkdir wamrc
+    mkdir -p wamrc
 
     # Download the latest WASI-enabled WebAssembly C/C++ toolchain prebuilt by WASM
     wget --quiet https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-19/${wasibasefile}.tar.gz
