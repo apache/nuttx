1b31c0db6e0743efd24f154ad023fe4974366f6d
raiden00pl <raiden00@railab.me>
2024-06-01 09:53:33 +0200

    sched/task/task_exithook.c: fix gcc14 error
    umm_memdump() should be always declared otherwise we get error when DEBUG_MM=n:
    
    sched/task/task_exithook.c:468:7: error: implicit declaration of function ‘umm_memdump’; did you mean ‘mm_memdump’? [-Wimplicit-function-declaration]

--- include/nuttx/mm/mm.h
+++ include/nuttx/mm/mm.h
@@ -375,6 +375,10 @@ struct mallinfo_task kmm_mallinfo_task(FAR const struct malltask *task);
 void mm_memdump(FAR struct mm_heap_s *heap,
                 FAR const struct mm_memdump_s *dump);
 
+/* Functions contained in umm_memdump.c *************************************/
+
+void umm_memdump(FAR const struct mm_memdump_s *dump);
+
 #ifdef CONFIG_DEBUG_MM
 /* Functions contained in mm_checkcorruption.c ******************************/
 
@@ -383,10 +387,6 @@ void mm_checkcorruption(FAR struct mm_heap_s *heap);
 /* Functions contained in umm_checkcorruption.c *****************************/
 
 FAR void umm_checkcorruption(void);
-
-/* Functions contained in umm_memdump.c *************************************/
-
-void umm_memdump(FAR const struct mm_memdump_s *dump);
 
 /* Functions contained in kmm_checkcorruption.c *****************************/
 
