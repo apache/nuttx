02f1d732a9c4c5abdfe16f64d8096e58ca72780e
qinwei1 <qinwei1@xiaomi.com>
2024-06-03 17:22:59 +0800

    arch/arm/src/mps: implement Protected Build for mps2-an500
    Summary
     1. add Protected build Support for ARM MPS AN500
     2. refine mps Memory layout configure and enable MPU support
    Note
     1. ostest for an547:nsh
     2. ostest for an500:nsh and an500:knsh
    
    Signed-off-by: qinwei1 <qinwei1@xiaomi.com>
    Signed-off-by: ligd <liguiding1@xiaomi.com>

--- arch/arm/src/mps/mps_start.c
+++ arch/arm/src/mps/mps_start.c
@@ -25,34 +25,63 @@
 #include <nuttx/config.h>
 #include <nuttx/init.h>
 
+#include <nuttx/cache.h>
+#include <nuttx/init.h>
+#include <arch/board/board.h>
+
 #include "arm_internal.h"
+#include "barriers.h"
+#include "nvic.h"
 #include "mps_irq.h"
+#include "mps_userspace.h"
 #include "mpu.h"
 
 /****************************************************************************
- * Public Functions
- ****************************************************************************/
-
-#define HEAP_BASE      ((uintptr_t)_ebss + CONFIG_IDLETHREAD_STACKSIZE)
-
-/****************************************************************************
- * Public Data
- ****************************************************************************/
-
-/* g_idle_topstack: _sbss is the start of the BSS region as defined by the
- * linker script. _ebss lies at the end of the BSS region. The idle task
- * stack starts at the end of BSS and is of size CONFIG_IDLETHREAD_STACKSIZE.
- * The IDLE thread is the thread that the system boots on and, eventually,
- * becomes the IDLE, do nothing task that runs only when there is nothing
- * else to run.  The heap continues from there until the end of memory.
- * g_idle_topstack is a read-only variable the provides this computed
- * address.
- */
-
-const uintptr_t g_idle_topstack = HEAP_BASE;
-
-/****************************************************************************
  * Private Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: mps_tcmenable
+ *
+ * Description:
+ *   Enable/disable tightly coupled memories.  Size of tightly coupled
+ *   memory regions is controlled by GPNVM Bits 7-8.
+ *
+ ****************************************************************************/
+
+static inline void mps_tcmenable(void)
+{
+  uint32_t regval;
+
+  ARM_DSB();
+  ARM_ISB();
+
+  /* Enabled/disabled ITCM */
+
+#ifdef CONFIG_ARMV7M_ITCM
+  regval  = NVIC_TCMCR_EN | NVIC_TCMCR_RMW | NVIC_TCMCR_RETEN;
+#else
+  regval  = getreg32(NVIC_ITCMCR);
+  regval &= ~NVIC_TCMCR_EN;
+#endif
+  putreg32(regval, NVIC_ITCMCR);
+
+  /* Enabled/disabled DTCM */
+
+#ifdef CONFIG_ARMV7M_DTCM
+  regval  = NVIC_TCMCR_EN | NVIC_TCMCR_RMW | NVIC_TCMCR_RETEN;
+#else
+  regval  = getreg32(NVIC_DTCMCR);
+  regval &= ~NVIC_TCMCR_EN;
+#endif
+  putreg32(regval, NVIC_DTCMCR);
+
+  ARM_DSB();
+  ARM_ISB();
+}
+
+/****************************************************************************
+ * Public Functions
  ****************************************************************************/
 
 /****************************************************************************
@@ -71,6 +100,9 @@ void __start(void)
   /* If enabled reset the MPU */
 
   mpu_early_reset();
+#ifdef CONFIG_ARM_MPU
+  mpu_showtype();
+#endif
   arm_fpuconfig();
 
   /* Set bss to zero */
@@ -89,9 +121,49 @@ void __start(void)
       *dest++ = *src++;
     }
 
+  /* Perform early serial initialization */
+
 #ifdef USE_EARLYSERIALINIT
   arm_earlyserialinit();
 #endif
 
+  /* Enable/disable tightly coupled memories */
+
+  mps_tcmenable();
+
+#ifdef CONFIG_ARMV7M_DCACHE
+  /* Memory barrier */
+
+  ARM_DMB();
+
+#endif
+
+#ifdef CONFIG_BUILD_PROTECTED
+  /* For the case of the separate user-/kernel-space build, perform whatever
+   * platform specific initialization of the user memory is required.
+   * Normally this just means initializing the user space .data and .bss
+   * segments.
+   */
+
+  mps_userspace();
+#endif
+
+#ifdef CONFIG_ARM_MPU
+  /* Then enable the MPU */
+
+  mpu_control(true, false, true);
+#endif
+
+  /* Enable I- and D-Caches */
+
+  up_enable_icache();
+  up_enable_dcache();
+
+  /* Then start NuttX */
+
   nx_start();
+
+  /* Shouldn't get here */
+
+  for (; ; );
 }
