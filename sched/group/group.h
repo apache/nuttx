9d17145fc3b491a018dfebf72dd1d9c6dd09e8a3
chao an <anchao@lixiang.com>
2024-06-06 15:03:40 +0800

    sched/pthread: simplify pthread_create() branch logic
    remove redundant branch logic
    
    Signed-off-by: chao an <anchao@lixiang.com>

--- sched/group/group.h
+++ sched/group/group.h
@@ -59,8 +59,8 @@ void task_initialize(void);
 int  group_initialize(FAR struct task_tcb_s *tcb, uint8_t ttype);
 void group_postinitialize(FAR struct task_tcb_s *tcb);
 #ifndef CONFIG_DISABLE_PTHREAD
-int  group_bind(FAR struct pthread_tcb_s *tcb);
-int  group_join(FAR struct pthread_tcb_s *tcb);
+void group_bind(FAR struct pthread_tcb_s *tcb);
+void group_join(FAR struct pthread_tcb_s *tcb);
 #endif
 void group_leave(FAR struct tcb_s *tcb);
 void group_drop(FAR struct task_group_s *group);
