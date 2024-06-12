f9cd4bae548ec86cdf4f8e364f6a0d5c039c3660
Samuel Martinez <samymtz66@gmail.com>
2024-06-12 14:01:37 -0400

    Led and buttons examples
    Description:
    Test LED examples on freedom-k66f, nucleo-f401re and blackpill-f411ce boards.
    Update linker file for bluepill-f103c6.

--- boards/arm/stm32/nucleo-f4x1re/src/stm32_bringup.c
+++ boards/arm/stm32/nucleo-f4x1re/src/stm32_bringup.c
@@ -41,6 +41,10 @@
 #  include <nuttx/leds/userled.h>
 #endif
 
+#ifdef CONFIG_INPUT_BUTTONS
+#  include <nuttx/input/buttons.h>
+#endif
+
 #include "nucleo-f4x1re.h"
 
 #include <nuttx/board.h>
@@ -90,6 +94,16 @@ int stm32_bringup(void)
     {
       syslog(LOG_ERR, "ERROR: userled_lower_initialize() failed: %d\n", ret);
       return ret;
+    }
+#endif
+
+#ifdef CONFIG_INPUT_BUTTONS
+  /* Register the BUTTON driver */
+
+  ret = btn_lower_initialize("/dev/buttons");
+  if (ret < 0)
+    {
+      syslog(LOG_ERR, "ERROR: btn_lower_initialize() failed: %d\n", ret);
     }
 #endif
 
