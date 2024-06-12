04eeef06a7e9f84460b1c10e43ba61f1f930f485
chao an <anchao@lixiang.com>
2024-06-12 17:51:18 +0800

    arch/tricore: update the function prototype as mainline
    Signed-off-by: chao an <anchao@lixiang.com>

--- arch/tricore/src/common/tricore_checkstack.c
+++ arch/tricore/src/common/tricore_checkstack.c
@@ -142,7 +142,7 @@ size_t up_check_tcbstack(struct tcb_s *tcb)
 }
 
 #if CONFIG_ARCH_INTERRUPTSTACK > 15
-size_t up_check_intstack(void)
+size_t up_check_intstack(int cpu)
 {
   return tricore_stack_check((uintptr_t)g_intstackalloc,
                            (CONFIG_ARCH_INTERRUPTSTACK & ~15));
