ca41d63c5001d4b63633350bda9b9181b1f0552d
p-szafonimateusz <p-szafonimateusz@xiaomi.com>
2024-06-17 11:28:16 +0200

    x86_64: add ELF support
    add arch_elf64.c for x86_64, ported from sim/x86
    
    Signed-off-by: p-szafonimateusz <p-szafonimateusz@xiaomi.com>

---
+++ arch/x86_64/include/elf.h
@@ -0,0 +1,45 @@
+/****************************************************************************
+ * arch/x86_64/include/elf.h
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+#ifndef __ARCH_X86_64_INCLUDE_ELF_H
+#define __ARCH_X86_64_INCLUDE_ELF_H
+
+/****************************************************************************
+ * Pre-processor Prototypes
+ ****************************************************************************/
+
+#define R_X86_64_NONE   0
+#define R_X86_64_64     1
+#define R_X86_64_PC32   2
+#define R_X86_64_PLT32  4
+#define R_X86_64_32     10
+#define R_X86_64_32S    11
+#define R_X86_64_PC64   24
+
+/* 4.3.1 ELF Identification.  Should have:
+ *
+ * e_machine         = EM_X86_64
+ */
+
+#define EM_ARCH      EM_X86_64
+
+#define EF_FLAG      0
+
+#endif /* __ARCH_X86_64_INCLUDE_ELF_H */
