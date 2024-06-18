dd2cc73a0b4a966340ff65125ab26e7d69308f7d
ouyangxiangzhen <ouyangxiangzhen@xiaomi.com>
2024-06-18 20:25:50 +0800

    sched/timer: Fix timer accuracy problems
    This commit fixed timer accuracy problems when repetitive timer is set.
    
    Signed-off-by: ouyangxiangzhen <ouyangxiangzhen@xiaomi.com>

--- sched/timer/timer_create.c
+++ sched/timer/timer_create.c
@@ -183,6 +183,7 @@ int timer_create(clockid_t clockid, FAR struct sigevent *evp,
   ret->pt_crefs = 1;
   ret->pt_owner = nxsched_getpid();
   ret->pt_delay = 0;
+  ret->pt_expected = 0;
 
   /* Was a struct sigevent provided? */
 
