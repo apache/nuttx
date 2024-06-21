6c9b468dde89e785880b364d1f4f4041860102d1
xuxingliang <xuxingliang@xiaomi.com>
2024-06-21 10:57:27 +0800

    arch/arm64: fix build warnings
    common/arm64_mpu.c:355:13: error: format '%llX' expects argument of type 'long long unsigned int', but argument 4 has type 'long unsigned int' [-Werror=format=]
      355 |       _info("MPU-%d, 0x%08llX-0x%08llX SH=%llX AP=%llX XN=%llX\n", i,
          |             ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    common/arm64_mpu.c:355:29: note: format string is defined here
      355 |       _info("MPU-%d, 0x%08llX-0x%08llX SH=%llX AP=%llX XN=%llX\n", i,
          |                        ~~~~~^
          |                             |
          |                             long long unsigned int
          |                        %08lX
    common/arm64_mpu.c:355:13: error: format '%llX' expects argument of type 'long long unsigned int', but argument 5 has type 'long unsigned int' [-Werror=format=]
      355 |       _info("MPU-%d, 0x%08llX-0x%08llX SH=%llX AP=%llX XN=%llX\n", i,
          |             ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    common/arm64_mpu.c:355:38: note: format string is defined here
      355 |       _info("MPU-%d, 0x%08llX-0x%08llX SH=%llX AP=%llX XN=%llX\n", i,
    
    Signed-off-by: xuxingliang <xuxingliang@xiaomi.com>

--- arch/arm64/src/common/arm64_mpu.c
+++ arch/arm64/src/common/arm64_mpu.c
@@ -352,7 +352,7 @@ void mpu_dump_region(void)
       write_sysreg(i, prselr_el1);
       prlar = read_sysreg(prlar_el1);
       prbar = read_sysreg(prbar_el1);
-      _info("MPU-%d, 0x%08X-0x%08X SH=%X AP=%X XN=%X\n", i,
+      _info("MPU-%d, 0x%08llX-0x%08llX SH=%llX AP=%llX XN=%llX\n", i,
             prbar & MPU_RBAR_BASE_MSK, prlar & MPU_RLAR_LIMIT_MSK,
             prbar & MPU_RBAR_SH_MSK, prbar & MPU_RBAR_AP_MSK,
             prbar & MPU_RBAR_XN_MSK);
