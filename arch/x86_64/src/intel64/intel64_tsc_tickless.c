d0779e0eef132528dbb4fbf232090c845d6541fd
ouyangxiangzhen <ouyangxiangzhen@xiaomi.com>
2024-06-28 16:48:24 +0800

    arch/x86_64: Fix up_timer compilation error
    This commit fix up_timer compilation error.
    
    Signed-off-by: ouyangxiangzhen <ouyangxiangzhen@xiaomi.com>

--- arch/x86_64/src/intel64/intel64_tsc_tickless.c
+++ arch/x86_64/src/intel64/intel64_tsc_tickless.c
@@ -76,7 +76,12 @@
 
 extern unsigned long g_x86_64_timer_freq;
 
+#ifndef CONFIG_SCHED_TICKLESS_ALARM
+static uint64_t g_goal_time;
+#else
 static struct timespec g_goal_time_ts;
+#endif
+
 static uint64_t g_last_stop_time;
 static uint64_t g_start_tsc;
 static uint32_t g_timer_active;
@@ -336,7 +341,7 @@ void up_timer_expire(void)
   g_timer_active = 0;
 
   up_mask_tmr();
-  sched_timer_expiration();
+  nxsched_timer_expiration();
 }
 
 #else /* CONFIG_SCHED_TICKLESS_ALARM */
