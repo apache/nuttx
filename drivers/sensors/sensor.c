ffac2a314b9113ac6b2f197f479c50a74271cd7b
wangjianyu3 <wangjianyu3@xiaomi.com>
2024-06-28 20:47:52 +0800

    driver/sensors: Support GNSS Geofence
    Signed-off-by: wangjianyu3 <wangjianyu3@xiaomi.com>

--- drivers/sensors/sensor.c
+++ drivers/sensors/sensor.c
@@ -195,6 +195,7 @@ static const struct sensor_meta_s g_sensor_meta[] =
   {sizeof(struct sensor_gnss_satellite),      "gnss_satellite"},
   {sizeof(struct sensor_gnss_measurement),    "gnss_measurement"},
   {sizeof(struct sensor_gnss_clock),          "gnss_clock"},
+  {sizeof(struct sensor_gnss_geofence_event), "gnss_geofence"},
 };
 
 static const struct file_operations g_sensor_fops =
