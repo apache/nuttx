ab51eae836019f7ceb0b56d084ad3ff001eb2470
chao an <anchao@lixiang.com>
2024-06-29 09:52:21 +0800

    libmetal/atomic: enable 64-bit atomic by toolchain builtin flags
    Fix compile error:
    arm-none-eabi-ld: (remoteproc_virtio.o): in function `metal_io_read':
    metal/io.h:252: undefined reference to `__atomic_load_8'
    arm-none-eabi-ld: (remoteproc_virtio.o): in function `metal_io_write':
    metal/io.h:290: undefined reference to `__atomic_store_8'
    
    Not all 32-bit architectures support 64bit atomic, gcc/clang
    toolchains have built-in properties to indicate whether support atomic64:
    
    | $ arm-none-eabi-gcc -march=armv7e-m  -dM -E - < /dev/null | grep SYNC
    | #define __GCC_HAVE_SYNC_COMPARE_AND_SWAP_1 1
    | #define __GCC_HAVE_SYNC_COMPARE_AND_SWAP_2 1
    | #define __GCC_HAVE_SYNC_COMPARE_AND_SWAP_4 1
    
    Signed-off-by: chao an <anchao@lixiang.com>

--- openamp/libmetal.cmake
+++ openamp/libmetal.cmake
@@ -40,6 +40,8 @@ if(NOT EXISTS ${CMAKE_CURRENT_LIST_DIR}/libmetal)
       ${CMAKE_CURRENT_LIST_DIR}/0002-libmetal-nuttx-io.c-align-access-when-read-write-siz.patch
       && patch -p0 -d ${CMAKE_CURRENT_LIST_DIR} <
       ${CMAKE_CURRENT_LIST_DIR}/0003-libmetal-nuttx-io.c-Fix-void-pointer-arithmetic-in-a.patch
+      && patch -p0 -d ${CMAKE_CURRENT_LIST_DIR} <
+      ${CMAKE_CURRENT_LIST_DIR}/0004-libmetal-atomic-enable-64-bit-atomic-by-toolchain-bu.patch
     DOWNLOAD_NO_PROGRESS true
     TIMEOUT 30)
 
