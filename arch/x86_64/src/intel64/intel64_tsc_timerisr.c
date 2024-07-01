0bdf7f62ddbbbae94c79c012aa14f0dad641a469
p-szafonimateusz <p-szafonimateusz@xiaomi.com>
2024-07-01 18:20:27 +0200

    arch/intel64/irq.h: rename rdtsc macros
    rename _rdtsc macro to rdtsc to avoid conflict with external code
    rename rdtsc macro to rdtscp to be the same as asm instruction used
    
    Signed-off-by: p-szafonimateusz <p-szafonimateusz@xiaomi.com>

--- arch/x86_64/src/intel64/intel64_tsc_timerisr.c
+++ arch/x86_64/src/intel64/intel64_tsc_timerisr.c
@@ -73,7 +73,7 @@ void apic_timer_set(unsigned long timeout_ns)
     (unsigned long long)timeout_ns * g_x86_64_timer_freq / NS_PER_SEC;
 
 #ifdef CONFIG_ARCH_INTEL64_TSC_DEADLINE
-    write_msr(MSR_IA32_TSC_DEADLINE, rdtsc() + ticks);
+    write_msr(MSR_IA32_TSC_DEADLINE, rdtscp() + ticks);
 #else
     write_msr(MSR_X2APIC_TMICT, ticks);
 #endif
