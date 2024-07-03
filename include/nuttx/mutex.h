c7b815e79544b9ce7ba679650d9424b397f57f7a
yinshengkai <yinshengkai@xiaomi.com>
2024-07-03 15:21:57 +0800

    mutex: add nxrmutex_is_recursive api
    Signed-off-by: yinshengkai <yinshengkai@xiaomi.com>

--- include/nuttx/mutex.h
+++ include/nuttx/mutex.h
@@ -393,6 +393,22 @@ int nxrmutex_destroy(FAR rmutex_t *rmutex);
 bool nxrmutex_is_hold(FAR rmutex_t *rmutex);
 
 /****************************************************************************
+ * Name: nxrmutex_is_recursive
+ *
+ * Description:
+ *   This function check whether the recursive mutex is recursive
+ *
+ * Parameters:
+ *   rmutex - Recursive mutex descriptor.
+ *
+ * Return Value:
+ *  If rmutex has returned to True recursively, otherwise returns false.
+ *
+ ****************************************************************************/
+
+bool nxrmutex_is_recursive(FAR rmutex_t *rmutex);
+
+/****************************************************************************
  * Name: nxrmutex_get_holder
  *
  * Description:
