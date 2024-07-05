85a47709b5db423bcab9129abaf7b145a77e56cd
hujun5 <hujun5@xiaomi.com>
2024-07-05 20:53:49 +0800

    arm64: g_current_regs is only used to determine if we are in irq, with other functionalities removed.
    Signed-off-by: hujun5 <hujun5@xiaomi.com>

--- arch/arm64/src/common/arm64_cpupause.c
+++ arch/arm64/src/common/arm64_cpupause.c
@@ -116,11 +116,7 @@ int up_cpu_paused_save(void)
   sched_note_cpu_paused(tcb);
 #endif
 
-  /* Save the current context at current_regs into the TCB at the head
-   * of the assigned task list for this CPU.
-   */
-
-  arm64_savestate(tcb->xcp.regs);
+  UNUSED(tcb);
 
   return OK;
 }
@@ -210,11 +206,7 @@ int up_cpu_paused_restore(void)
 
   nxsched_resume_scheduler(tcb);
 
-  /* Then switch contexts.  Any necessary address environment changes
-   * will be made when the interrupt returns.
-   */
-
-  arm64_restorestate(tcb->xcp.regs);
+  UNUSED(tcb);
 
   return OK;
 }
