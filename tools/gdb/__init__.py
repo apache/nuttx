c7557618aa51bf259d4ac076d21515dc7f88f8f7
Gao Jiawei <gaojiawei@xiaomi.com>
2024-07-09 11:39:15 +0800

    enable stack trace printing on excpetion
    Signed-off-by: Gao Jiawei <gaojiawei@xiaomi.com>

--- tools/gdb/__init__.py
+++ tools/gdb/__init__.py
@@ -36,6 +36,8 @@ py_files.remove(os.path.abspath(__file__))
 
 gdb.execute("set pagination off")
 gdb.write("set pagination off\n")
+gdb.execute("set python print-stack full")
+gdb.write("set python print-stack full\n")
 for py_file in py_files:
     gdb.execute("source %s" % py_file)
     gdb.write("source %s\n" % py_file)
