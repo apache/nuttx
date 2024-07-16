85e8536fbe7f7544b26eaaedc4daa26641be49f0
zouboan <zouboan@hotmail.com>
2024-07-16 21:24:01 +0800

    zcu111: add support for board led

--- boards/arm64/zynq-mpsoc/zcu111/include/board.h
+++ boards/arm64/zynq-mpsoc/zcu111/include/board.h
@@ -31,4 +31,46 @@
  * Pre-processor Definitions
  ****************************************************************************/
 
+/* LED definitions **********************************************************/
+
+/* The ZCU111 PL has only one MIO LEDs, Others LED connected to PL.
+ *
+ * If CONFIG_ARCH_LEDS is not defined, then the user can control the LEDs in
+ * any way.  The following definitions are used to access individual LEDs.
+ */
+
+/* LED index values for use with board_userled() */
+
+#define BOARD_LED1                     0
+#define BOARD_NLEDS                    1
+
+/* LED bits for use with board_userled_all() */
+
+#define BOARD_LED1_BIT                 (1 << BOARD_LED1)
+
+/* If CONFIG_ARCH_LEDs is defined, then NuttX will control the LED on board
+ * The following definitions describe how NuttX controls the LED:
+ *
+ *   SYMBOL              Meaning                  LED1 state
+ *   ------------------  -----------------------  ----------
+ *   LED_STARTED         NuttX has been started   OFF
+ *   LED_HEAPALLOCATE    Heap has been allocated  OFF
+ *   LED_IRQSENABLED     Interrupts enabled       OFF
+ *   LED_STACKCREATED    Idle stack created       ON
+ *   LED_INIRQ           In an interrupt          No change
+ *   LED_SIGNAL          In a signal handler      No change
+ *   LED_ASSERTION       An assertion failed      No change
+ *   LED_PANIC           The system has crashed   Blinking
+ *   LED_IDLE            STM32 is is sleep mode   Not used
+ */
+
+#define LED_STARTED      0
+#define LED_HEAPALLOCATE 0
+#define LED_IRQSENABLED  0
+#define LED_STACKCREATED 1
+#define LED_INIRQ        2
+#define LED_SIGNAL       2
+#define LED_ASSERTION    2
+#define LED_PANIC        1
+
 #endif /* __BOARDS_ARM64_ZYNQ_MPSOC_ZCU111_INCLUDE_BOARD_H */
