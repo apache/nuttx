41e976d11992c1f965fe8bfa7296b716f63e9544
hujun5 <hujun5@xiaomi.com>
2024-07-19 11:13:38 +0800

    libs/libc/queue: inline queue list to improve performance add a config CONFIG_LIBC_INLINE_QUEUE to inline the queue list
    test:
    We can use qemu for testing.
    compiling
    make distclean -j20; ./tools/configure.sh -l qemu-armv8a:nsh_smp ;make -j20
    running
    qemu-system-aarch64 -cpu cortex-a53 -smp 4 -nographic -machine virt,virtualization=on,gic-version=3 -net none -chardev stdio,id=con,mux=on -serial chardev:con -mon chardev=con,mode=readline -kernel ./nuttx
    
    Signed-off-by: hujun5 <hujun5@xiaomi.com>

--- include/nuttx/queue.h
+++ include/nuttx/queue.h
@@ -31,6 +31,14 @@
  * Pre-processor Definitions
  ****************************************************************************/
 
+#ifdef CONFIG_LIBC_INLINE_QUEUE
+#  ifndef STATIC_INLINE
+#    define STATIC_INLINE static inline_function
+#  endif
+#else
+#  define STATIC_INLINE
+#endif
+
 #define sq_init(q) \
   do \
     { \
@@ -343,24 +351,241 @@ extern "C"
 
 /* Add nodes to queues */
 
+#ifndef CONFIG_LIBC_INLINE_QUEUE
 void sq_addafter(FAR sq_entry_t *prev, FAR sq_entry_t *node,
                  FAR sq_queue_t *queue);
+#else
+STATIC_INLINE void sq_addafter(FAR sq_entry_t *prev, FAR sq_entry_t *node,
+                               FAR sq_queue_t *queue)
+{
+  if (!queue->head || prev == queue->tail)
+    {
+      sq_addlast(node, queue);
+    }
+  else
+    {
+      node->flink = prev->flink;
+      prev->flink = node;
+    }
+}
+#endif
+
+#ifndef CONFIG_LIBC_INLINE_QUEUE
 void dq_addafter(FAR dq_entry_t *prev, FAR dq_entry_t *node,
                  FAR dq_queue_t *queue);
+#else
+STATIC_INLINE void dq_addafter(FAR dq_entry_t *prev, FAR dq_entry_t *node,
+                               FAR dq_queue_t *queue)
+{
+  if (!queue->head || prev == queue->tail)
+    {
+      dq_addlast(node, queue);
+    }
+  else
+    {
+      FAR dq_entry_t *next = prev->flink;
+      node->blink = prev;
+      node->flink = next;
+      next->blink = node;
+      prev->flink = node;
+    }
+}
+#endif
 
 /* Remove nodes from queues */
 
+#ifndef CONFIG_LIBC_INLINE_QUEUE
 FAR sq_entry_t *sq_remafter(FAR sq_entry_t *node, FAR sq_queue_t *queue);
+#else
+STATIC_INLINE FAR sq_entry_t *sq_remafter(FAR sq_entry_t *node,
+                                          FAR sq_queue_t *queue)
+{
+  FAR sq_entry_t *ret = node->flink;
+
+  if (queue->head && ret)
+    {
+      if (queue->tail == ret)
+        {
+          queue->tail = node;
+          node->flink = NULL;
+        }
+      else
+        {
+          node->flink = ret->flink;
+        }
+
+      ret->flink = NULL;
+    }
+
+  return ret;
+}
+#endif
+
+#ifndef CONFIG_LIBC_INLINE_QUEUE
 FAR dq_entry_t *dq_remafter(FAR dq_entry_t *node, FAR dq_queue_t *queue);
+#else
+STATIC_INLINE FAR dq_entry_t *dq_remafter(FAR dq_entry_t *node,
+                                          FAR dq_queue_t *queue)
+{
+  FAR dq_entry_t *ret = node->flink;
+
+  if (queue->head != NULL && ret != NULL)
+    {
+      dq_rem(ret, queue);
+    }
+
+  return ret;
+}
+#endif
+
+#ifndef CONFIG_LIBC_INLINE_QUEUE
 FAR sq_entry_t *sq_remlast(FAR sq_queue_t *queue);
+#else
+STATIC_INLINE FAR sq_entry_t *sq_remlast(FAR sq_queue_t *queue)
+{
+  FAR sq_entry_t *ret = queue->tail;
+
+  if (ret)
+    {
+      if (queue->head == queue->tail)
+        {
+          queue->head = NULL;
+          queue->tail = NULL;
+        }
+      else
+        {
+          FAR sq_entry_t *prev;
+          for (prev = queue->head;
+              prev && prev->flink != ret;
+              prev = prev->flink);
+
+          if (prev)
+            {
+              prev->flink = NULL;
+              queue->tail = prev;
+            }
+        }
+
+      ret->flink = NULL;
+    }
+
+  return ret;
+}
+#endif
+
+#ifndef CONFIG_LIBC_INLINE_QUEUE
 FAR dq_entry_t *dq_remlast(FAR dq_queue_t *queue);
+#else
+STATIC_INLINE FAR dq_entry_t *dq_remlast(FAR dq_queue_t *queue)
+{
+  FAR dq_entry_t *ret = queue->tail;
+
+  if (ret)
+    {
+      FAR dq_entry_t *prev = ret->blink;
+      if (!prev)
+        {
+          queue->head = NULL;
+          queue->tail = NULL;
+        }
+      else
+        {
+          queue->tail = prev;
+          prev->flink = NULL;
+        }
+
+      ret->flink = NULL;
+      ret->blink = NULL;
+    }
+
+  return ret;
+}
+#endif
+
+#ifndef CONFIG_LIBC_INLINE_QUEUE
 FAR sq_entry_t *sq_remfirst(FAR sq_queue_t *queue);
+#else
+STATIC_INLINE FAR sq_entry_t *sq_remfirst(FAR sq_queue_t *queue)
+{
+  FAR sq_entry_t *ret = queue->head;
+
+  if (ret)
+    {
+      queue->head = ret->flink;
+      if (!queue->head)
+        {
+          queue->tail = NULL;
+        }
+
+      ret->flink = NULL;
+    }
+
+  return ret;
+}
+#endif
+
+#ifndef CONFIG_LIBC_INLINE_QUEUE
 FAR dq_entry_t *dq_remfirst(FAR dq_queue_t *queue);
+#else
+STATIC_INLINE FAR dq_entry_t *dq_remfirst(FAR dq_queue_t *queue)
+{
+  FAR dq_entry_t *ret = queue->head;
+
+  if (ret)
+    {
+      FAR dq_entry_t *next = ret->flink;
+      if (!next)
+        {
+          queue->head = NULL;
+          queue->tail = NULL;
+        }
+      else
+        {
+          queue->head = next;
+          next->blink = NULL;
+        }
+
+      ret->flink = NULL;
+      ret->blink = NULL;
+    }
+
+  return ret;
+}
+#endif
 
 /* Count nodes in queues */
 
+#ifndef CONFIG_LIBC_INLINE_QUEUE
 size_t sq_count(FAR sq_queue_t *queue);
+#else
+STATIC_INLINE size_t sq_count(FAR sq_queue_t *queue)
+{
+  FAR sq_entry_t *node;
+  size_t count;
+
+  for (node = queue->head, count = 0;
+       node != NULL;
+       node = node->flink, count++);
+
+  return count;
+}
+#endif
+
+#ifndef CONFIG_LIBC_INLINE_QUEUE
 size_t dq_count(FAR dq_queue_t *queue);
+#else
+STATIC_INLINE size_t dq_count(FAR dq_queue_t *queue)
+{
+  FAR dq_entry_t *node;
+  size_t count;
+
+  for (node = queue->head, count = 0;
+       node != NULL;
+       node = node->flink, count++);
+
+  return count;
+}
+#endif
 
 #undef EXTERN
 #ifdef __cplusplus
