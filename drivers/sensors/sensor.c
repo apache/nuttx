607b8effe9296d7fe3fea7b7bd43d0e3df53b1b8
wangjianyu3 <wangjianyu3@xiaomi.com>
2024-07-22 16:24:56 +0800

    driver/sensors: Let gnss geofence follow the existing naming rules
    ORB_ID(sensor_gnss_geofence_event)
    
    Signed-off-by: wangjianyu3 <wangjianyu3@xiaomi.com>

--- drivers/sensors/sensor.c
+++ drivers/sensors/sensor.c
@@ -195,7 +195,7 @@ static const struct sensor_meta_s g_sensor_meta[] =
   {sizeof(struct sensor_gnss_satellite),      "gnss_satellite"},
   {sizeof(struct sensor_gnss_measurement),    "gnss_measurement"},
   {sizeof(struct sensor_gnss_clock),          "gnss_clock"},
-  {sizeof(struct sensor_gnss_geofence_event), "gnss_geofence"},
+  {sizeof(struct sensor_gnss_geofence_event), "gnss_geofence_event"},
 };
 
 static const struct file_operations g_sensor_fops =
