5956aace736ece77e4dff6888254dde003acba58
wangxiaoxin <wangxiaoxin@xiaomi.com>
2024-08-05 10:03:49 +0800

    Modify SIOCGCANSTATE/ SIOCSCANSTATE ioctl num
    Notes:
    Duplicate with other ioctl numbers
    
    Signed-off-by: wangxiaoxin <wangxiaoxin@xiaomi.com>

--- include/nuttx/net/ioctl.h
+++ include/nuttx/net/ioctl.h
@@ -125,8 +125,8 @@
 #define SIOCACANSTDFILTER  _SIOC(0x0030)  /* Add hardware-level standard ID filter */
 #define SIOCDCANSTDFILTER  _SIOC(0x0031)  /* Delete hardware-level standard ID filter */
 #define SIOCCANRECOVERY    _SIOC(0x0032)  /* Recovery can, work only when bus-off state */
-#define SIOCGCANSTATE      _SIOC(0x003E)  /* Get state from a CAN/LIN controller */
-#define SIOCSCANSTATE      _SIOC(0x003F)  /* Set the LIN/CAN controller state */
+#define SIOCGCANSTATE      _SIOC(0x0041)  /* Get state from a CAN/LIN controller */
+#define SIOCSCANSTATE      _SIOC(0x0042)  /* Set the LIN/CAN controller state */
 
 /* Network socket control ***************************************************/
 
