b5968e7b1bf7fa7f81bc8ad89ae7fd8a3980a289
ligd <liguiding1@xiaomi.com>
2024-08-07 21:04:52 +0800

    wdog: add new API wd_start_realtime() support
    Signed-off-by: ligd <liguiding1@xiaomi.com>

--- sched/clock/clock_abstime2ticks.c
+++ sched/clock/clock_abstime2ticks.c
@@ -104,3 +104,38 @@ int clock_abstime2ticks(clockid_t clockid,
 
   return OK;
 }
+
+/****************************************************************************
+ * Name: clock_realtime2absticks
+ *
+ * Description:
+ *   Convert real time to monotonic ticks.
+ *
+ * Input Parameters:
+ *   mono - Return the converted time here.
+ *   abstime - Convert this absolute time to ticks
+ *
+ * Returned Value:
+ *   OK (0) on success; a negated errno value on failure.
+ *
+ * Assumptions:
+ *   Interrupts should be disabled so that the time is not changing during
+ *   the calculation
+ *
+ ****************************************************************************/
+
+int clock_realtime2absticks(FAR const struct timespec *reltime,
+                            FAR clock_t *absticks)
+{
+#ifndef CONFIG_CLOCK_TIMEKEEPING
+  struct timespec mono;
+
+  clock_timespec_subtract(reltime, &g_basetime, &mono);
+
+  *absticks = clock_time2ticks(&mono);
+
+  return OK;
+#else
+  return -ENOSYS;
+#endif
+}
