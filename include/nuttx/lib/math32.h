120224fc045997e75699972aff87db8774d73521
ligd <liguiding1@xiaomi.com>
2024-08-08 15:31:23 +0800

    math32.h: remove typeof depends
    Signed-off-by: ligd <liguiding1@xiaomi.com>

--- include/nuttx/lib/math32.h
+++ include/nuttx/lib/math32.h
@@ -328,12 +328,12 @@ extern "C"
       })
 
 #  define div_const(n, base) \
-    ((sizeof(typeof(n)) == sizeof(uint64_t)) ? div64_const(n, base) : ((n) / (base)))
+    ((sizeof(n) == sizeof(uint64_t)) ? div64_const(n, base) : ((n) / (base)))
 #  define div_const_roundup(n, base) \
-    ((sizeof(typeof(n)) == sizeof(uint64_t)) ? div64_const((n) + (base) - 1, base) : \
+    ((sizeof(n) == sizeof(uint64_t)) ? div64_const((n) + (base) - 1, base) : \
      (((n) + (base) - 1) / (base)))
 #  define div_const_roundnearest(n, base) \
-    ((sizeof(typeof(n)) == sizeof(uint64_t)) ? div64_const((n) + ((base) / 2), base) : \
+    ((sizeof(n) == sizeof(uint64_t)) ? div64_const((n) + ((base) / 2), base) : \
      (((n) + ((base) / 2)) / (base)))
 #else
 #  define div_const(n, base) ((n) / (base))
