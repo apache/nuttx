90e2395d6c78b7e99f51587747b7db7acab23903
wangchen <wangchen41@xiaomi.com>
2024-08-13 19:49:33 +0800

    netlink:add tls cleanup protection to protect waitsem in netlink_get_response
    Signed-off-by: wangchen <wangchen41@xiaomi.com>

--- net/netlink/netlink.h
+++ net/netlink/netlink.h
@@ -385,7 +385,7 @@ int netlink_notifier_setup(worker_t worker, FAR struct netlink_conn_s *conn,
  *
  ****************************************************************************/
 
-void netlink_notifier_teardown(FAR struct netlink_conn_s *conn);
+void netlink_notifier_teardown(FAR void *conn);
 
 /****************************************************************************
  * Name: netlink_notifier_signal
