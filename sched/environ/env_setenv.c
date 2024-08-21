6b21bebcd1347d5fae87cb2cd1adf2fc86d1d049
pengyinjie <pengyinjie@xiaomi.com>
2024-08-21 17:56:23 +0800

    [env]:Fixed spacing and typo issues in code comment descriptions
    [Desc]: as title
    
    Signed-off-by: pengyinjie <pengyinjie@xiaomi.com>

--- sched/environ/env_setenv.c
+++ sched/environ/env_setenv.c
@@ -47,7 +47,7 @@
  *
  * Description:
  *   The setenv() function adds the variable name to the environment with the
- *   specified 'value' if the varialbe 'name" does not exist. If the 'name'
+ *   specified 'value' if the variable 'name" does not exist. If the 'name'
  *   does exist in the environment, then its value is changed to 'value' if
  *   'overwrite' is non-zero; if 'overwrite' is zero, then the value of name
  *   unaltered.
