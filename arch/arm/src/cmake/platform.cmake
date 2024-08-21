1661a66843b3e2a4f0ee0c574c2e3e6b496c69cf
simbit18 <101105604+simbit18@users.noreply.github.com>
2024-08-21 14:57:09 +0200

    [cmake]: added initial support for MSYS2
    Currently concerns only arm.
    
    tools/ci/testlist/msys2.dat:
    At the moment I only added the board nucleo-l152re:nsh
    
    .github/workflows/build.yml:
    Enabled cmake for msys2

--- arch/arm/src/cmake/platform.cmake
+++ arch/arm/src/cmake/platform.cmake
@@ -87,7 +87,12 @@ if(NOT EXISTS ${extra_library} AND CONFIG_ARCH_TOOLCHAIN_CLANG)
     OUTPUT_VARIABLE extra_library)
 endif()
 
-list(APPEND EXTRA_LIB ${extra_library})
+if(CMAKE_HOST_SYSTEM_NAME MATCHES "MSYS|CYGWIN|Windows")
+  cmake_path(GET extra_library FILENAME extra_filename_library)
+  list(APPEND EXTRA_LIB -l:${extra_filename_library})
+else()
+  list(APPEND EXTRA_LIB ${extra_library})
+endif()
 
 if(NOT CONFIG_LIBM)
   execute_process(
@@ -95,7 +100,13 @@ if(NOT CONFIG_LIBM)
             --print-file-name=libm.a
     OUTPUT_STRIP_TRAILING_WHITESPACE
     OUTPUT_VARIABLE extra_library)
-  list(APPEND EXTRA_LIB ${extra_library})
+
+  if(CMAKE_HOST_SYSTEM_NAME MATCHES "MSYS|CYGWIN|Windows")
+    cmake_path(GET extra_library FILENAME extra_filename_library)
+    list(APPEND EXTRA_LIB -l:${extra_filename_library})
+  else()
+    list(APPEND EXTRA_LIB ${extra_library})
+  endif()
 endif()
 
 if(CONFIG_LIBSUPCXX)
@@ -104,7 +115,12 @@ if(CONFIG_LIBSUPCXX)
             --print-file-name=libsupc++.a
     OUTPUT_STRIP_TRAILING_WHITESPACE
     OUTPUT_VARIABLE extra_library)
-  list(APPEND EXTRA_LIB ${extra_library})
+  if(CMAKE_HOST_SYSTEM_NAME MATCHES "MSYS|CYGWIN|Windows")
+    cmake_path(GET extra_library FILENAME extra_filename_library)
+    list(APPEND EXTRA_LIB -l:${extra_filename_library})
+  else()
+    list(APPEND EXTRA_LIB ${extra_library})
+  endif()
 endif()
 
 if(CONFIG_ARCH_COVERAGE)
@@ -113,7 +129,12 @@ if(CONFIG_ARCH_COVERAGE)
             --print-file-name=libgcov.a
     OUTPUT_STRIP_TRAILING_WHITESPACE
     OUTPUT_VARIABLE extra_library)
-  list(APPEND EXTRA_LIB ${extra_library})
+  if(CMAKE_HOST_SYSTEM_NAME MATCHES "MSYS|CYGWIN|Windows")
+    cmake_path(GET extra_library FILENAME extra_filename_library)
+    list(APPEND EXTRA_LIB -l:${extra_filename_library})
+  else()
+    list(APPEND EXTRA_LIB ${extra_library})
+  endif()
 endif()
 
 nuttx_add_extra_library(${EXTRA_LIB})
