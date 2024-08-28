7b91a0b17fb6a32f3e66f1ab9df352fc65685917
yangsong8 <yangsong8@xiaomi.com>
2024-08-28 11:11:48 +0800

    cdcacm: align cdcacm_epconfigure function parameters
    Signed-off-by: yangsong8 <yangsong8@xiaomi.com>

--- drivers/usbdev/cdcacm.c
+++ drivers/usbdev/cdcacm.c
@@ -976,7 +976,7 @@ static int cdcacm_setconfig(FAR struct cdcacm_dev_s *priv, uint8_t config)
   /* Configure the IN interrupt endpoint */
 
   ret = cdcacm_epconfigure(priv->epintin, CDCACM_EPINTIN, false,
-                            &priv->devinfo, priv->usbdev->speed);
+                           &priv->devinfo, priv->usbdev->speed);
 
   if (ret < 0)
     {
@@ -989,7 +989,7 @@ static int cdcacm_setconfig(FAR struct cdcacm_dev_s *priv, uint8_t config)
   /* Configure the IN bulk endpoint */
 
   ret = cdcacm_epconfigure(priv->epbulkin, CDCACM_EPBULKIN, false,
-                            &priv->devinfo, priv->usbdev->speed);
+                           &priv->devinfo, priv->usbdev->speed);
 
   if (ret < 0)
     {
@@ -1002,7 +1002,7 @@ static int cdcacm_setconfig(FAR struct cdcacm_dev_s *priv, uint8_t config)
   /* Configure the OUT bulk endpoint */
 
   ret = cdcacm_epconfigure(priv->epbulkout, CDCACM_EPBULKOUT, true,
-                            &priv->devinfo, priv->usbdev->speed);
+                           &priv->devinfo, priv->usbdev->speed);
 
   if (ret < 0)
     {
