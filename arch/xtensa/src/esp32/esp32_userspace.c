027a40a7a38ec8ef70d9de56806ec2863f638aa3
YAMAMOTO Takashi <yamamoto@midokura.com>
2024-08-30 00:15:26 +0900

    esp32: fix seemingly wrong calculations
    DPORT_APP_CACHE_MASK_DRAM1 is already a mask.

--- arch/xtensa/src/esp32/esp32_userspace.c
+++ arch/xtensa/src/esp32/esp32_userspace.c
@@ -224,7 +224,7 @@ static noinline_function IRAM_ATTR void configure_sram_mmu(void)
 
 #ifdef CONFIG_SMP
   regval  = getreg32(DPORT_APP_CACHE_CTRL1_REG);
-  regval &= ~(1 << DPORT_APP_CACHE_MASK_DRAM1);
+  regval &= ~DPORT_APP_CACHE_MASK_DRAM1;
   putreg32(regval, DPORT_APP_CACHE_CTRL1_REG);
   ASSERT(cache_sram_mmu_set(1, PIDCTRL_PID_KERNEL, SOC_EXTRAM_DATA_LOW, 0,
                             32, 128) == 0);
