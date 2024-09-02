5cf2d13b4484642525d33a607cccd5b5c77b1ea5
chao an <anchao@lixiang.com>
2024-09-02 13:06:25 +0800

    global/variables: add g_ prefix to some global variables
    Signed-off-by: chao an <anchao@lixiang.com>

--- libs/libc/time/lib_gmtime.c
+++ libs/libc/time/lib_gmtime.c
@@ -31,6 +31,12 @@
 #include <nuttx/clock.h>
 
 /****************************************************************************
+ * Private Data
+ ****************************************************************************/
+
+static struct tm g_gmtime;
+
+/****************************************************************************
  * Public Functions
  ****************************************************************************/
 
@@ -44,8 +50,7 @@
 
 FAR struct tm *gmtime(FAR const time_t *timep)
 {
-  static struct tm tm;
-  return gmtime_r(timep, &tm);
+  return gmtime_r(timep, &g_gmtime);
 }
 
 FAR struct tm *localtime(FAR const time_t *timep)
