fc08e6d32c4a12cef9e6e5064083e3f4872b8c08
Alin Jerpelea <alin.jerpelea@sony.com>
2024-09-11 13:45:11 +0200

    sched: migrate to SPDX identifier
    Most tools used for compliance and SBOM generation use SPDX identifiers
    This change brings us a step closer to an easy SBOM generation.
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- sched/task/task_spawnparms.c
+++ sched/task/task_spawnparms.c
@@ -1,6 +1,8 @@
 /****************************************************************************
  * sched/task/task_spawnparms.c
  *
+ * SPDX-License-Identifier: Apache-2.0
+ *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.  The
