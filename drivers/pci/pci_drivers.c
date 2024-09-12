1e212981b9d4a3eab9399f8c111fd26f3ca0fb95
p-szafonimateusz <p-szafonimateusz@xiaomi.com>
2024-09-12 12:31:22 +0200

    drivers/net: add support for Intel I225 network card
    add support for Intel I225 network card
    
    Signed-off-by: p-szafonimateusz <p-szafonimateusz@xiaomi.com>

--- drivers/pci/pci_drivers.c
+++ drivers/pci/pci_drivers.c
@@ -31,6 +31,7 @@
 #include <nuttx/rpmsg/rpmsg_virtio_ivshmem.h>
 #include <nuttx/virtio/virtio-pci.h>
 #include <nuttx/net/e1000.h>
+#include <nuttx/net/igc.h>
 
 #include "pci_drivers.h"
 
@@ -122,6 +123,16 @@ int pci_register_drivers(void)
   if (ret < 0)
     {
       pcierr("pci_e1000_init failed, ret=%d\n", ret);
+    }
+#endif
+
+  /* Initialization igc driver */
+
+#ifdef CONFIG_NET_IGC
+  ret = pci_igc_init();
+  if (ret < 0)
+    {
+      pcierr("pci_igc_init failed, ret=%d\n", ret);
     }
 #endif
 
