2fea6de3075290f93affcab741a51553b4ebd6eb
Alin Jerpelea <alin.jerpelea@sony.com>
2024-09-12 16:44:55 +0200

    mm: migrate to SPDX identifier
    Most tools used for compliance and SBOM generation use SPDX identifiers
    This change brings us a step closer to an easy SBOM generation.
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- mm/kmm_heap/kmm_extend.c
+++ mm/kmm_heap/kmm_extend.c
@@ -1,6 +1,8 @@
 /****************************************************************************
  * mm/kmm_heap/kmm_extend.c
  *
+ * SPDX-License-Identifier: Apache-2.0
+ *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.  The
