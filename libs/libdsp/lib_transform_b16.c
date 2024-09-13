3ac9a6e17950c2f93dc1feb1b024aec8a5e28098
Alin Jerpelea <alin.jerpelea@sony.com>
2024-09-13 10:19:20 +0200

    libs/libdsp: migrate to SPDX identifier
    Most tools used for compliance and SBOM generation use SPDX identifiers
    This change brings us a step closer to an easy SBOM generation.
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- libs/libdsp/lib_transform_b16.c
+++ libs/libdsp/lib_transform_b16.c
@@ -1,6 +1,8 @@
 /****************************************************************************
  * libs/libdsp/lib_transform_b16.c
  *
+ * SPDX-License-Identifier: Apache-2.0
+ *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.  The
