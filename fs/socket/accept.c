f0b5d2c7eca584c4b378dce038c4ca190d14f3ad
chenrun1 <chenrun1@xiaomi.com>
2024-09-20 15:12:12 +0800

    accept4:move function from net to fs
    Summary:
      Implementation in accept4 is special, the requested newsock is saved as filep->priv. This will cause sock_file_close to use fs_heap_free filep->priv during close. When fs_heap is configured, the released memory will not be on fs_heap, causing a crash.
    
    Signed-off-by: chenrun1 <chenrun1@xiaomi.com>

--- fs/socket/accept.c
+++ fs/socket/accept.c
@@ -1,7 +1,5 @@
 /****************************************************************************
- * net/socket/accept.c
- *
- * SPDX-License-Identifier: Apache-2.0
+ * fs/socket/accept.c
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -36,28 +34,28 @@
 #include <fcntl.h>
 
 #include <nuttx/cancelpt.h>
+#include <nuttx/net/net.h>
 #include <nuttx/fs/fs.h>
-#include <nuttx/kmalloc.h>
 #include <arch/irq.h>
 
-#include "socket/socket.h"
+#include "fs_heap.h"
 
 /****************************************************************************
  * Public Functions
  ****************************************************************************/
 
 /****************************************************************************
- * Name: psock_accept
+ * Name: accept4
  *
  * Description:
- *   The psock_accept function is used with connection-based socket types
+ *   The accept4 function is used with connection-based socket types
  *   (SOCK_STREAM, SOCK_SEQPACKET and SOCK_RDM). It extracts the first
  *   connection request on the queue of pending connections, creates a new
  *   connected socket with mostly the same properties as 'sockfd', and
  *   allocates a new socket descriptor for the socket, which is returned. The
  *   newly created socket is no longer in the listening state. The original
  *   socket 'sockfd' is unaffected by this call.  Per file descriptor flags
- *   are not inherited across an psock_accept.
+ *   are not inherited across an accept.
  *
  *   The 'sockfd' argument is a socket descriptor that has been created with
  *   socket(), bound to a local address with bind(), and is listening for
@@ -69,26 +67,28 @@
  *   actual length of the address returned.
  *
  *   If no pending connections are present on the queue, and the socket is
- *   not marked as non-blocking, psock_accept blocks the caller until a
- *   connection is present. If the socket is marked non-blocking and no
- *   pending connections are present on the queue, psock_accept returns
- *   EAGAIN.
+ *   not marked as non-blocking, accept blocks the caller until a connection
+ *   is present. If the socket is marked non-blocking and no pending
+ *   connections are present on the queue, accept returns EAGAIN.
  *
  * Input Parameters:
- *   psock    Reference to the listening socket structure
+ *   sockfd   The listening socket descriptor
  *   addr     Receives the address of the connecting client
- *   addrlen  Input: allocated size of 'addr', Return: returned size
- *            of 'addr'
- *   newsock  Location to return the accepted socket information.
+ *   addrlen  Input: allocated size of 'addr',
+ *            Return: returned size of 'addr'
  *   flags    The flags used for initialization
  *
  * Returned Value:
- *  Returns 0 (OK) on success.  On failure, it returns a negated errno value
- *  to indicate the nature of the error.
+ *  Returns -1 on error. If it succeeds, it returns a non-negative integer
+ *  that is a descriptor for the accepted socket.
  *
  * EAGAIN or EWOULDBLOCK
  *   The socket is marked non-blocking and no connections are present to
  *   be accepted.
+ * EBADF
+ *   The descriptor is invalid.
+ * ENOTSOCK
+ *  The descriptor references a file, not a socket.
  * EOPNOTSUPP
  *   The referenced socket is not of type SOCK_STREAM.
  * EINTR
@@ -103,7 +103,8 @@
  * ENFILE
  *   The system maximum for file descriptors has been reached.
  * EFAULT
- *   The addr parameter is not in a writable part of the user address space.
+ *   The addr parameter is not in a writable part of the user address
+ *   space.
  * ENOBUFS or ENOMEM
  *   Not enough free memory.
  * EPROTO
@@ -113,59 +114,90 @@
  *
  ****************************************************************************/
 
-int psock_accept(FAR struct socket *psock, FAR struct sockaddr *addr,
-                 FAR socklen_t *addrlen, FAR struct socket *newsock,
-                 int flags)
+int accept4(int sockfd, FAR struct sockaddr *addr, FAR socklen_t *addrlen,
+            int flags)
 {
-  FAR struct socket_conn_s *conn;
+  FAR struct socket *psock = NULL;
+  FAR struct socket *newsock;
+  FAR struct file *filep;
+  int oflags = O_RDWR;
+  int errcode;
+  int newfd;
   int ret;
 
-  DEBUGASSERT(psock != NULL && psock->s_conn != NULL && newsock != NULL);
+  /* accept4() is a cancellation point */
 
-  /* May sure that the socket has been opened with socket() */
+  enter_cancellation_point();
 
-  if (psock == NULL || psock->s_conn == NULL)
+  if (flags & ~(SOCK_NONBLOCK | SOCK_CLOEXEC))
     {
-      nerr("ERROR: Socket invalid or not opened\n");
-      return -EINVAL;
+      errcode = EINVAL;
+      goto errout;
     }
 
-  /* Is the socket listening for a connection? */
+  /* Get the underlying socket structure */
 
-  conn = psock->s_conn;
-  if (!_SS_ISLISTENING(conn->s_flags))
+  ret = sockfd_socket(sockfd, &filep, &psock);
+
+  /* Verify that the sockfd corresponds to valid, allocated socket */
+
+  if (ret < 0)
     {
-      nerr("ERROR: Socket is not listening for a connection.\n");
-      return -EINVAL;
+      errcode = -ret;
+      goto errout;
     }
 
-  /* Let the address family's accept() method handle the operation */
-
-  DEBUGASSERT(psock->s_sockif != NULL);
-  if (psock->s_sockif->si_accept == NULL)
+  newsock = fs_heap_zalloc(sizeof(*newsock));
+  if (newsock == NULL)
     {
-      return -EOPNOTSUPP;
+      errcode = ENOMEM;
+      goto errout_with_filep;
     }
 
-  net_lock();
-  ret = psock->s_sockif->si_accept(psock, addr, addrlen, newsock, flags);
-  if (ret >= 0)
+  ret = psock_accept(psock, addr, addrlen, newsock, flags);
+  if (ret < 0)
     {
-      /* Mark the new socket as connected. */
-
-      conn = newsock->s_conn;
-      conn->s_flags |= _SF_CONNECTED;
-      conn->s_flags &= ~_SF_CLOSED;
-      if (flags & SOCK_NONBLOCK)
-        {
-          conn->s_flags |= _SF_NONBLOCK;
-        }
-    }
-  else
-    {
-      nerr("ERROR: si_accept failed: %d\n", ret);
+      errcode = -ret;
+      goto errout_with_alloc;
     }
 
-  net_unlock();
-  return ret;
+  /* Allocate a socket descriptor for the new connection now (so that it
+   * cannot fail later)
+   */
+
+  if (flags & SOCK_CLOEXEC)
+    {
+      oflags |= O_CLOEXEC;
+    }
+
+  if (flags & SOCK_NONBLOCK)
+    {
+      oflags |= O_NONBLOCK;
+    }
+
+  newfd = sockfd_allocate(newsock, oflags);
+  if (newfd < 0)
+    {
+      errcode = ENFILE;
+      goto errout_with_psock;
+    }
+
+  fs_putfilep(filep);
+  leave_cancellation_point();
+  return newfd;
+
+errout_with_psock:
+  psock_close(newsock);
+
+errout_with_alloc:
+  fs_heap_free(newsock);
+
+errout_with_filep:
+  fs_putfilep(filep);
+
+errout:
+  leave_cancellation_point();
+
+  set_errno(errcode);
+  return ERROR;
 }
