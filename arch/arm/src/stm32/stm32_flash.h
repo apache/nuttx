df47241a2bdb43d013571246253c78f934b8783f
Kyle Wilson <kwilson@2g-eng.com>
2024-10-07 08:28:30 -0500

    STM32G4 Flash Driver
    Added a flash driver for the STM32G4 series. The primary change here is
    the addition of stm32g4xxx_flash.c. This file uses the STM32L4 flash
    driver as a template. The primary difference is the accounting for dual
    banks with different page sizes.
    
    Fixed error while building b-g474e-dpow1/buckboost. It was possible (technically) to have page be used uninitialzied. Changing the if statement to default to using a flash_page_size == 2048 fixes this issue.

--- arch/arm/src/stm32/stm32_flash.h
+++ arch/arm/src/stm32/stm32_flash.h
@@ -35,6 +35,28 @@
  * Public Function Prototypes
  ****************************************************************************/
 
+int stm32_flash_lock(void);
+int stm32_flash_unlock(void);
+
+/****************************************************************************
+ * Name: stm32_flash_user_optbytes
+ *
+ * Description:
+ *   Modify the contents of the user option bytes (USR OPT) on the flash.
+ *   This does not set OBL_LAUNCH so new options take effect only after
+ *   next power reset.
+ *
+ * Input Parameters:
+ *   clrbits - Bits in the option bytes to be cleared
+ *   setbits - Bits in the option bytes to be set
+ *
+ * Returned Value:
+ *   Option bytes after operation is completed
+ *
+ ****************************************************************************/
+
+uint32_t stm32_flash_users_optbytes(uint32_t clrbits, uint32_t setbits);
+
 /****************************************************************************
  * Name: stm32_eeprom_size
  *
