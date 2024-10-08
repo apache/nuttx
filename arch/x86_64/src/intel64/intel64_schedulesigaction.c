apache/nuttx

commit f12996c8517b1511149defbd59778ffbae1909d7

sched: replace sync pause with async pause for nxsig_process
Signed-off-by: hujun5 <hujun5@xiaomi.com>

arch/x86_64/src/intel64/intel64_schedulesigaction.c
@@ -157,6 +157,7 @@ void up_schedule_sigaction(struct tcb_s *tcb)
        */
 
       tcb->xcp.regs[REG_RIP]    = (uint64_t)x86_64_sigdeliver;
+      tcb->xcp.regs[REG_RIP]    = tcb->xcp.regs[REG_RIP] - 8;
       tcb->xcp.regs[REG_RFLAGS] = 0;
     }
 }
@@ -202,76 +203,30 @@ void up_schedule_sigaction(struct tcb_s *tcb)
 
       else
         {
-          /* If we signaling a task running on the other CPU, we have
-           * to PAUSE the other CPU.
+          /* tcb is running on the same CPU */
+
+          /* Save the return lr and cpsr and one scratch register.
+           * These will be restored by the signal trampoline after
+           * the signals have been delivered.
            */
 
-          if (cpu != me && tcb->task_state == TSTATE_TASK_RUNNING)
-            {
-              /* Pause the CPU */
+          tcb->xcp.saved_rip         = up_current_regs()[REG_RIP];
+          tcb->xcp.saved_rsp         = up_current_regs()[REG_RSP];
+          tcb->xcp.saved_rflags      = up_current_regs()[REG_RFLAGS];
 
-              up_cpu_pause(cpu);
-
-              /* Now tcb on the other CPU can be accessed safely */
-
-              /* Copy tcb->xcp.regs to tcp.xcp.saved. These will be
-               * restored by the signal trampoline after the signal has
-               * been delivered.
-               */
-
-              tcb->xcp.saved_rip        = tcb->xcp.regs[REG_RIP];
-              tcb->xcp.saved_rsp        = tcb->xcp.regs[REG_RSP];
-              tcb->xcp.saved_rflags     = tcb->xcp.regs[REG_RFLAGS];
-
-              /* Then set up to vector to the trampoline with interrupts
-               * disabled
-               */
-
-              tcb->xcp.regs[REG_RIP]    = (uint64_t)x86_64_sigdeliver;
-              tcb->xcp.regs[REG_RSP]    = tcb->xcp.regs[REG_RSP] - 8;
-              tcb->xcp.regs[REG_RFLAGS] = 0;
-            }
-          else
-            {
-              /* tcb is running on the same CPU */
-
-              /* Save the return lr and cpsr and one scratch register.
-               * These will be restored by the signal trampoline after
-               * the signals have been delivered.
-               */
-
-              tcb->xcp.saved_rip         = up_current_regs()[REG_RIP];
-              tcb->xcp.saved_rsp         = up_current_regs()[REG_RSP];
-              tcb->xcp.saved_rflags      = up_current_regs()[REG_RFLAGS];
-
-              /* Then set up to vector to the trampoline with interrupts
-               * disabled
-               */
-
-              up_current_regs()[REG_RIP]    = (uint64_t)x86_64_sigdeliver;
-              up_current_regs()[REG_RSP]    = up_current_regs()[REG_RSP] - 8;
-              up_current_regs()[REG_RFLAGS] = 0;
-
-              /* And make sure that the saved context in the TCB
-               * is the same as the interrupt return context.
-               */
-
-              x86_64_savestate(tcb->xcp.regs);
-            }
-
-          /* NOTE: If the task runs on another CPU(cpu), adjusting
-           * global IRQ controls will be done in the pause handler
-           * on the CPU(cpu) by taking a critical section.
-           * If the task is scheduled on this CPU(me), do nothing
-           * because this CPU already took a critical section
+          /* Then set up to vector to the trampoline with interrupts
+           * disabled
            */
 
-          /* RESUME the other CPU if it was PAUSED */
+          up_current_regs()[REG_RIP]    = (uint64_t)x86_64_sigdeliver;
+          up_current_regs()[REG_RIP]    = up_current_regs()[REG_RIP] - 8;
+          up_current_regs()[REG_RFLAGS] = 0;
 
-          if (cpu != me && tcb->task_state == TSTATE_TASK_RUNNING)
-            {
-              up_cpu_resume(cpu);
-            }
+          /* And make sure that the saved context in the TCB
+           * is the same as the interrupt return context.
+           */
+
+          x86_64_savestate(tcb->xcp.regs);
         }
     }
 

arch/arm/src/armv6-m/arm_schedulesigaction.c
@@ -98,30 +98,6 @@ void up_schedule_sigaction(struct tcb_s *tcb)
     }
   else
     {
-      /* CASE 2:  The task that needs to receive the signal is running.
-       * This could happen if the task is running on another CPU OR if
-       * we are in an interrupt handler and the task is running on this
-       * CPU.  In the former case, we will have to PAUSE the other CPU
-       * first.  But in either case, we will have to modify the return
-       * state as well as the state in the TCB.
-       */
-
-      /* If we signaling a task running on the other CPU, we have
-       * to PAUSE the other CPU.
-       */
-
-#ifdef CONFIG_SMP
-      int cpu = tcb->cpu;
-      int me  = this_cpu();
-
-      if (cpu != me && tcb->task_state == TSTATE_TASK_RUNNING)
-        {
-          /* Pause the CPU */
-
-          up_cpu_pause(cpu);
-        }
-#endif
-
       /* Save the return PC, CPSR and either the BASEPRI or PRIMASK
        * registers (and perhaps also the LR).  These will be restored
        * by the signal trampoline after the signal has been delivered.
@@ -156,15 +132,6 @@ void up_schedule_sigaction(struct tcb_s *tcb)
       tcb->xcp.regs[REG_LR]         = EXC_RETURN_THREAD;
       tcb->xcp.regs[REG_EXC_RETURN] = EXC_RETURN_THREAD;
       tcb->xcp.regs[REG_CONTROL]    = getcontrol() & ~CONTROL_NPRIV;
-#endif
-
-#ifdef CONFIG_SMP
-      /* RESUME the other CPU if it was PAUSED */
-
-      if (cpu != me && tcb->task_state == TSTATE_TASK_RUNNING)
-        {
-          up_cpu_resume(cpu);
-        }
 #endif
     }
 }

arch/arm/src/armv7-a/arm_schedulesigaction.c
@@ -97,30 +97,6 @@ void up_schedule_sigaction(struct tcb_s *tcb)
     }
   else
     {
-      /* CASE 2:  The task that needs to receive the signal is running.
-       * This could happen if the task is running on another CPU OR if
-       * we are in an interrupt handler and the task is running on this
-       * CPU.  In the former case, we will have to PAUSE the other CPU
-       * first.  But in either case, we will have to modify the return
-       * state as well as the state in the TCB.
-       */
-
-      /* If we signaling a task running on the other CPU, we have
-       * to PAUSE the other CPU.
-       */
-
-#ifdef CONFIG_SMP
-      int cpu = tcb->cpu;
-      int me  = this_cpu();
-
-      if (cpu != me && tcb->task_state == TSTATE_TASK_RUNNING)
-        {
-          /* Pause the CPU */
-
-          up_cpu_pause(cpu);
-        }
-#endif
-
       /* Save the return lr and cpsr and one scratch register.  These
        * will be restored by the signal trampoline after the signals
        * have been delivered.
@@ -151,15 +127,6 @@ void up_schedule_sigaction(struct tcb_s *tcb)
       tcb->xcp.regs[REG_CPSR]  = (PSR_MODE_SYS | PSR_I_BIT | PSR_F_BIT);
 #ifdef CONFIG_ARM_THUMB
       tcb->xcp.regs[REG_CPSR] |= PSR_T_BIT;
-#endif
-
-#ifdef CONFIG_SMP
-      /* RESUME the other CPU if it was PAUSED */
-
-      if (cpu != me && tcb->task_state == TSTATE_TASK_RUNNING)
-        {
-          up_cpu_resume(cpu);
-        }
 #endif
     }
 }

arch/arm/src/armv7-m/arm_schedulesigaction.c
@@ -99,30 +99,6 @@ void up_schedule_sigaction(struct tcb_s *tcb)
     }
   else
     {
-      /* CASE 2:  The task that needs to receive the signal is running.
-       * This could happen if the task is running on another CPU OR if
-       * we are in an interrupt handler and the task is running on this
-       * CPU.  In the former case, we will have to PAUSE the other CPU
-       * first.  But in either case, we will have to modify the return
-       * state as well as the state in the TCB.
-       */
-
-      /* If we signaling a task running on the other CPU, we have
-       * to PAUSE the other CPU.
-       */
-
-#ifdef CONFIG_SMP
-      int cpu = tcb->cpu;
-      int me  = this_cpu();
-
-      if (cpu != me && tcb->task_state == TSTATE_TASK_RUNNING)
-        {
-          /* Pause the CPU */
-
-          up_cpu_pause(cpu);
-        }
-#endif
-
       /* Save the return PC, CPSR and either the BASEPRI or PRIMASK
        * registers (and perhaps also the LR).  These will be restored
        * by the signal trampoline after the signal has been delivered.
@@ -161,15 +137,6 @@ void up_schedule_sigaction(struct tcb_s *tcb)
       tcb->xcp.regs[REG_LR]         = EXC_RETURN_THREAD;
       tcb->xcp.regs[REG_EXC_RETURN] = EXC_RETURN_THREAD;
       tcb->xcp.regs[REG_CONTROL]    = getcontrol() & ~CONTROL_NPRIV;
-#endif
-
-#ifdef CONFIG_SMP
-      /* RESUME the other CPU if it was PAUSED */
-
-      if (cpu != me && tcb->task_state == TSTATE_TASK_RUNNING)
-        {
-          up_cpu_resume(cpu);
-        }
 #endif
     }
 }

arch/arm/src/armv7-r/arm_schedulesigaction.c
@@ -95,22 +95,6 @@ void up_schedule_sigaction(struct tcb_s *tcb)
     }
   else
     {
-      /* If we signaling a task running on the other CPU, we have
-       * to PAUSE the other CPU.
-       */
-
-#ifdef CONFIG_SMP
-      int cpu = tcb->cpu;
-      int me  = this_cpu();
-
-      if (cpu != me && tcb->task_state == TSTATE_TASK_RUNNING)
-        {
-          /* Pause the CPU */
-
-          up_cpu_pause(cpu);
-        }
-#endif
-
       /* Save the return lr and cpsr and one scratch register.  These
        * will be restored by the signal trampoline after the signals
        * have been delivered.
@@ -141,15 +125,6 @@ void up_schedule_sigaction(struct tcb_s *tcb)
       tcb->xcp.regs[REG_CPSR]  = (PSR_MODE_SYS | PSR_I_BIT | PSR_F_BIT);
 #ifdef CONFIG_ARM_THUMB
       tcb->xcp.regs[REG_CPSR] |= PSR_T_BIT;
-#endif
-
-#ifdef CONFIG_SMP
-      /* RESUME the other CPU if it was PAUSED */
-
-      if (cpu != me && tcb->task_state == TSTATE_TASK_RUNNING)
-        {
-          up_cpu_resume(cpu);
-        }
 #endif
     }
 }

arch/arm/src/armv8-m/arm_schedulesigaction.c
@@ -99,30 +99,6 @@ void up_schedule_sigaction(struct tcb_s *tcb)
     }
   else
     {
-      /* CASE 2:  The task that needs to receive the signal is running.
-       * This could happen if the task is running on another CPU OR if
-       * we are in an interrupt handler and the task is running on this
-       * CPU.  In the former case, we will have to PAUSE the other CPU
-       * first.  But in either case, we will have to modify the return
-       * state as well as the state in the TCB.
-       */
-
-      /* If we signaling a task running on the other CPU, we have
-       * to PAUSE the other CPU.
-       */
-
-#ifdef CONFIG_SMP
-      int cpu = tcb->cpu;
-      int me  = this_cpu();
-
-      if (cpu != me && tcb->task_state == TSTATE_TASK_RUNNING)
-        {
-          /* Pause the CPU */
-
-          up_cpu_pause(cpu);
-        }
-#endif
-
       /* Save the return PC, CPSR and either the BASEPRI or PRIMASK
        * registers (and perhaps also the LR).  These will be restored
        * by the signal trampoline after the signal has been delivered.
@@ -161,15 +137,6 @@ void up_schedule_sigaction(struct tcb_s *tcb)
       tcb->xcp.regs[REG_LR]         = EXC_RETURN_THREAD;
       tcb->xcp.regs[REG_EXC_RETURN] = EXC_RETURN_THREAD;
       tcb->xcp.regs[REG_CONTROL]    = getcontrol() & ~CONTROL_NPRIV;
-#endif
-
-#ifdef CONFIG_SMP
-      /* RESUME the other CPU if it was PAUSED */
-
-      if (cpu != me && tcb->task_state == TSTATE_TASK_RUNNING)
-        {
-          up_cpu_resume(cpu);
-        }
 #endif
     }
 }

arch/arm/src/armv8-r/arm_schedulesigaction.c
@@ -95,22 +95,6 @@ void up_schedule_sigaction(struct tcb_s *tcb)
     }
   else
     {
-      /* If we signaling a task running on the other CPU, we have
-       * to PAUSE the other CPU.
-       */
-
-#ifdef CONFIG_SMP
-      int cpu = tcb->cpu;
-      int me  = this_cpu();
-
-      if (cpu != me && tcb->task_state == TSTATE_TASK_RUNNING)
-        {
-          /* Pause the CPU */
-
-          up_cpu_pause(cpu);
-        }
-#endif
-
       /* Save the return lr and cpsr and one scratch register.  These
        * will be restored by the signal trampoline after the signals
        * have been delivered.
@@ -141,15 +125,6 @@ void up_schedule_sigaction(struct tcb_s *tcb)
       tcb->xcp.regs[REG_CPSR]  = (PSR_MODE_SYS | PSR_I_BIT | PSR_F_BIT);
 #ifdef CONFIG_ARM_THUMB
       tcb->xcp.regs[REG_CPSR] |= PSR_T_BIT;
-#endif
-
-#ifdef CONFIG_SMP
-      /* RESUME the other CPU if it was PAUSED */
-
-      if (cpu != me && tcb->task_state == TSTATE_TASK_RUNNING)
-        {
-          up_cpu_resume(cpu);
-        }
 #endif
     }
 }

arch/arm64/src/common/arm64_schedulesigaction.c
@@ -132,18 +132,6 @@ void up_schedule_sigaction(struct tcb_s *tcb)
     }
   else
     {
-#ifdef CONFIG_SMP
-      int cpu = tcb->cpu;
-      int me  = this_cpu();
-
-      if (cpu != me && tcb->task_state == TSTATE_TASK_RUNNING)
-        {
-          /* Pause the CPU */
-
-          up_cpu_pause(cpu);
-        }
-#endif
-
       /* Save the return lr and cpsr and one scratch register.  These
        * will be restored by the signal trampoline after the signals
        * have been delivered.
@@ -154,14 +142,5 @@ void up_schedule_sigaction(struct tcb_s *tcb)
       /* create signal process context */
 
       arm64_init_signal_process(tcb, NULL);
-
-#ifdef CONFIG_SMP
-      /* RESUME the other CPU if it was PAUSED */
-
-      if (cpu != me && tcb->task_state == TSTATE_TASK_RUNNING)
-        {
-          up_cpu_resume(cpu);
-        }
-#endif
     }
 }

commit ac1ab8ec0db51e82a9c84674ada14843f4a60dfd

x86_64: fix regression
This commit fixes the regression from https://github.com/apache/nuttx/pull/13768

Signed-off-by: hujun5 <hujun5@xiaomi.com>

arch/x86_64/src/intel64/intel64_schedulesigaction.c
@@ -157,7 +157,7 @@ void up_schedule_sigaction(struct tcb_s *tcb)
        */
 
       tcb->xcp.regs[REG_RIP]    = (uint64_t)x86_64_sigdeliver;
-      tcb->xcp.regs[REG_RIP]    = tcb->xcp.regs[REG_RIP] - 8;
+      tcb->xcp.regs[REG_RSP]    = tcb->xcp.regs[REG_RSP] - 8;
       tcb->xcp.regs[REG_RFLAGS] = 0;
     }
 }
@@ -219,7 +219,7 @@ void up_schedule_sigaction(struct tcb_s *tcb)
            */
 
           up_current_regs()[REG_RIP]    = (uint64_t)x86_64_sigdeliver;
-          up_current_regs()[REG_RIP]    = up_current_regs()[REG_RIP] - 8;
+          up_current_regs()[REG_RSP]    = up_current_regs()[REG_RSP] - 8;
           up_current_regs()[REG_RFLAGS] = 0;
 
           /* And make sure that the saved context in the TCB