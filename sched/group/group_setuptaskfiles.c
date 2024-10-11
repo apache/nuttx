a041ebbaef20fdb6279be6f1aae265549efabd29
dongjiuzhu1 <dongjiuzhu1@xiaomi.com>
2024-10-11 14:05:00 +0800

    Revert "sched/group/setuptask_file: duplicate idle task fd for kernel thread"
    let's using shared group for kthreads, see pull/12320
    
    This reverts commit 4e24eec7b607e4a6a7eca0fae9c58f02dc72e002.

--- sched/group/group_setuptaskfiles.c
+++ sched/group/group_setuptaskfiles.c
@@ -66,7 +66,7 @@ int group_setuptaskfiles(FAR struct task_tcb_s *tcb,
   FAR struct task_group_s *group = tcb->cmn.group;
   int ret = OK;
 #ifndef CONFIG_FDCLONE_DISABLE
-  FAR struct tcb_s *rtcb;
+  FAR struct tcb_s *rtcb = this_task();
 #endif
 
   sched_trace_begin();
@@ -77,20 +77,7 @@ int group_setuptaskfiles(FAR struct task_tcb_s *tcb,
 #endif
 
 #ifndef CONFIG_FDCLONE_DISABLE
-
-  /* The The file descriptors of kernel threads should be clean and
-   * should not be generated based on user threads. Instead, an idle
-   * thread should be chosen.
-   */
-
-  if ((tcb->cmn.flags & TCB_FLAG_TTYPE_MASK) == TCB_FLAG_TTYPE_KERNEL)
-    {
-      rtcb = &g_idletcb[this_cpu()];
-    }
-  else
-    {
-      rtcb = this_task();
-    }
+  DEBUGASSERT(rtcb->group);
 
   /* Duplicate the parent task's file descriptors */
 
