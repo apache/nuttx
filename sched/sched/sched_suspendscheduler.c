apache/nuttx

commit 0b3859521a6d7e0b67388c44c72aa8feb5a2b83b

arch: fix the sched parameter update when exiting
Signed-off-by: buxiasen <buxiasen@xiaomi.com>

sched/sched/sched_suspendscheduler.c
@@ -61,6 +61,13 @@
 
 void nxsched_suspend_scheduler(FAR struct tcb_s *tcb)
 {
+  /* Handle the task exiting case */
+
+  if (tcb != NULL)
+    {
+      return;
+    }
+
 #ifdef CONFIG_SCHED_SPORADIC
   /* Perform sporadic schedule operations */
 

arch/arm/src/arm/arm_syscall.c
@@ -152,6 +152,12 @@ uint32_t *arm_syscall(uint32_t *regs)
 
       cpu = this_cpu();
       tcb = current_task(cpu);
+
+      /* Update scheduler parameters */
+
+      nxsched_suspend_scheduler(g_running_tasks[cpu]);
+      nxsched_resume_scheduler(tcb);
+
       g_running_tasks[cpu] = tcb;
 
       /* Restore the cpu lock */

arch/arm/src/common/arm_exit.c
@@ -66,11 +66,9 @@ void up_exit(int status)
 
   tcb = this_task();
 
-  /* Adjusts time slice for SCHED_RR & SCHED_SPORADIC cases
-   * NOTE: the API also adjusts the global IRQ control for SMP
-   */
+  /* Scheduler parameters will update inside syscall */
 
-  nxsched_resume_scheduler(tcb);
+  g_running_tasks[this_cpu()] = NULL;
 
   /* Then switch contexts */
 

arch/arm64/src/common/arm64_exit.c
@@ -70,11 +70,9 @@ void up_exit(int status)
 
   tcb = this_task();
 
-  /* Adjusts time slice for SCHED_RR & SCHED_SPORADIC cases
-   * NOTE: the API also adjusts the global IRQ control for SMP
-   */
+  /* Scheduler parameters will update inside syscall */
 
-  nxsched_resume_scheduler(tcb);
+  g_running_tasks[this_cpu()] = NULL;
 
   /* Then switch contexts */
 

arch/avr/src/common/avr_exit.c
@@ -68,6 +68,7 @@ void up_exit(int status)
   /* Adjusts time slice for SCHED_RR & SCHED_SPORADIC cases */
 
   nxsched_resume_scheduler(tcb);
+  g_running_tasks[this_cpu()] = tcb;
 
 #ifdef CONFIG_ARCH_ADDRENV
   /* Make sure that the address environment for the previously running

arch/hc/src/common/hc_exit.c
@@ -67,6 +67,7 @@ void up_exit(int status)
   /* Adjusts time slice for SCHED_RR & SCHED_SPORADIC cases */
 
   nxsched_resume_scheduler(tcb);
+  g_running_tasks[this_cpu()] = tcb;
 
 #ifdef CONFIG_ARCH_ADDRENV
   /* Make sure that the address environment for the previously running

arch/mips/src/common/mips_exit.c
@@ -66,11 +66,9 @@ void up_exit(int status)
 
   tcb = this_task();
 
-  /* Adjusts time slice for SCHED_RR & SCHED_SPORADIC cases
-   * NOTE: the API also adjusts the global IRQ control for SMP
-   */
+  /* Scheduler parameters will update inside syscall */
 
-  nxsched_resume_scheduler(tcb);
+  g_running_tasks[this_cpu()] = NULL;
 
   /* Then switch contexts */
 

arch/misoc/src/lm32/lm32_exit.c
@@ -68,7 +68,7 @@ void up_exit(int status)
 
   /* Adjusts time slice for RR & SPORADIC cases */
 
-  nxsched_resume_scheduler(tcb);
+  g_running_tasks[this_cpu()] = NULL;
 
   /* Then switch contexts */
 

commit 80d5f4ff14264a243c5d9d1f186e99b07bc3f39b

sched: fix nxsched_suspend_scheduler regression
This commit fixes the regression from https://github.com/apache/nuttx/pull/13877

Signed-off-by: hujun5 <hujun5@xiaomi.com>

sched/sched/sched_suspendscheduler.c
@@ -63,7 +63,7 @@ void nxsched_suspend_scheduler(FAR struct tcb_s *tcb)
 {
   /* Handle the task exiting case */
 
-  if (tcb != NULL)
+  if (tcb == NULL)
     {
       return;
     }