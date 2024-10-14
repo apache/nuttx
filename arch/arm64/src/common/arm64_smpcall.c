d77cb8af7019b348bb9c82daf0e131151d21b1ae
hujun5 <hujun5@xiaomi.com>
2024-10-14 15:38:06 +0800

    sched: fix nxsched_process_delivered did not call hook
    Signed-off-by: hujun5 <hujun5@xiaomi.com>

--- arch/arm64/src/common/arm64_smpcall.c
+++ arch/arm64/src/common/arm64_smpcall.c
@@ -67,9 +67,16 @@
 
 int arm64_smp_sched_handler(int irq, void *context, void *arg)
 {
+  struct tcb_s *tcb;
   int cpu = this_cpu();
 
+  tcb = current_task(cpu);
+  nxsched_suspend_scheduler(tcb);
   nxsched_process_delivered(cpu);
+  tcb = current_task(cpu);
+  nxsched_resume_scheduler(tcb);
+
+  UNUSED(tcb);
 
   return OK;
 }
