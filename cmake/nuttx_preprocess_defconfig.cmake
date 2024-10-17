a33c63cfd470eeefa69313f08764a7d42935f025
xuxin19 <xuxin19@xiaomi.com>
2024-10-17 22:45:46 +0800

    cmake(feat):support CMake defconfig #include parse
    Signed-off-by: xuxin19 <xuxin19@xiaomi.com>

---
+++ cmake/nuttx_preprocess_defconfig.cmake
@@ -0,0 +1,39 @@
+# ##############################################################################
+# cmake/nuttx_preprocess_defconfig.cmake
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more contributor
+# license agreements.  See the NOTICE file distributed with this work for
+# additional information regarding copyright ownership.  The ASF licenses this
+# file to you under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License.  You may obtain a copy of
+# the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+# License for the specific language governing permissions and limitations under
+# the License.
+#
+# ##############################################################################
+
+# fetch defconfig content
+file(READ "${NUTTX_DEFCONFIG}" FILE_CONTENTS)
+string(FIND "${FILE_CONTENTS}" "#include" INCLUDE_FOUND)
+
+# Should we preprocess defconfig?
+if(INCLUDE_FOUND GREATER -1)
+  file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/preprocess_defconfig)
+  get_filename_component(DEFCONFIG_PATH "${NUTTX_DEFCONFIG}" DIRECTORY)
+  execute_process(
+    COMMAND
+      ${NUTTX_DIR}/tools/process_config.sh -I
+      ${DEFCONFIG_PATH}/../../common/configs -I ${DEFCONFIG_PATH}/../common -I
+      ${DEFCONFIG_PATH} -o ${CMAKE_BINARY_DIR}/preprocess_defconfig/defconfig
+      ${NUTTX_DEFCONFIG}
+    RESULT_VARIABLE PREPROCESS_DEFCONFIG_RESULT)
+  set(NUTTX_DEFCONFIG ${CMAKE_BINARY_DIR}/preprocess_defconfig/defconfig)
+endif()
