apache/nuttx

commit eae57cb0e6c2677dcf42e95959a8dd9946889602

sched: replace sync pause with async pause for nxtask_terminate
reason:
In the kernel, we are planning to remove all occurrences of up_cpu_pause as one of the steps to
simplify the implementation of critical sections. The goal is to enable spin_lock_irqsave to encapsulate critical sections,
thereby facilitating the replacement of critical sections(big lock) with smaller spin_lock_irqsave(small lock)

Signed-off-by: hujun5 <hujun5@xiaomi.com>

sched/task/task_exit.c
@@ -135,15 +135,6 @@ int nxtask_exit(void)
 
   rtcb->task_state = TSTATE_TASK_READYTORUN;
 
-  /* Move the TCB to the specified blocked task list and delete it.  Calling
-   * nxtask_terminate with non-blocking true will suppress atexit() and
-   * on-exit() calls and will cause buffered I/O to fail to be flushed.  The
-   * former is required _exit() behavior; the latter is optional _exit()
-   * behavior.
-   */
-
-  nxsched_add_blocked(dtcb, TSTATE_TASK_INACTIVE);
-
 #ifdef CONFIG_SMP
   /* NOTE:
    * During nxtask_terminate(), enter_critical_section() will be called
@@ -155,7 +146,8 @@ int nxtask_exit(void)
   rtcb->irqcount++;
 #endif
 
-  ret = nxtask_terminate(dtcb->pid);
+  dtcb->task_state = TSTATE_TASK_INACTIVE;
+  ret = nxsched_release_tcb(dtcb, dtcb->flags & TCB_FLAG_TTYPE_MASK);
 
 #ifdef CONFIG_SMP
   rtcb->irqcount--;

arch/arm/src/common/arm_exit.c
@@ -56,12 +56,6 @@ void up_exit(int status)
 {
   struct tcb_s *tcb = this_task();
 
-  /* Make sure that we are in a critical section with local interrupts.
-   * The IRQ state will be restored when the next task is started.
-   */
-
-  enter_critical_section();
-
   /* Destroy the task at the head of the ready to run list. */
 
   nxtask_exit();

arch/arm64/src/common/arm64_exit.c
@@ -60,12 +60,6 @@ void up_exit(int status)
   struct tcb_s *tcb = this_task();
   UNUSED(status);
 
-  /* Make sure that we are in a critical section with local interrupts.
-   * The IRQ state will be restored when the next task is started.
-   */
-
-  enter_critical_section();
-
   /* Destroy the task at the head of the ready to run list. */
 
   nxtask_exit();

arch/avr/src/common/avr_exit.c
@@ -55,12 +55,6 @@ void up_exit(int status)
 {
   struct tcb_s *tcb = this_task();
 
-  /* Make sure that we are in a critical section with local interrupts.
-   * The IRQ state will be restored when the next task is started.
-   */
-
-  enter_critical_section();
-
   /* Destroy the task at the head of the ready to run list. */
 
   nxtask_exit();

arch/hc/src/common/hc_exit.c
@@ -54,12 +54,6 @@ void up_exit(int status)
 {
   struct tcb_s *tcb = this_task();
 
-  /* Make sure that we are in a critical section with local interrupts.
-   * The IRQ state will be restored when the next task is started.
-   */
-
-  enter_critical_section();
-
   /* Destroy the task at the head of the ready to run list. */
 
   nxtask_exit();

arch/mips/src/common/mips_exit.c
@@ -56,12 +56,6 @@ void up_exit(int status)
 {
   struct tcb_s *tcb = this_task();
 
-  /* Make sure that we are in a critical section with local interrupts.
-   * The IRQ state will be restored when the next task is started.
-   */
-
-  enter_critical_section();
-
   /* Destroy the task at the head of the ready to run list. */
 
   nxtask_exit();

arch/misoc/src/lm32/lm32_exit.c
@@ -56,12 +56,6 @@ void up_exit(int status)
 {
   struct tcb_s *tcb = this_task();
 
-  /* Make sure that we are in a critical section with local interrupts.
-   * The IRQ state will be restored when the next task is started.
-   */
-
-  enter_critical_section();
-
   /* Destroy the task at the head of the ready to run list. */
 
   nxtask_exit();

arch/misoc/src/minerva/minerva_exit.c
@@ -56,12 +56,6 @@ void up_exit(int status)
 {
   struct tcb_s *tcb = this_task();
 
-  /* Make sure that we are in a critical section with local interrupts.  The
-   * IRQ state will be restored when the next task is started.
-   */
-
-  enter_critical_section();
-
   /* Destroy the task at the head of the ready to run list. */
 
   nxtask_exit();

commit c5c7375e80a3e559af92e17f2e7d5db1c51266e7

task_exit.c: Add missing sched_note_stop()
A regression from #13728 ; sched_note_stop() is never called for tasks
that exit normally via exit().

sched/task/task_exit.c
@@ -29,6 +29,8 @@
 #include <sched.h>
 #include <debug.h>
 
+#include <nuttx/sched_note.h>
+
 #include "sched/sched.h"
 
 #ifdef CONFIG_SMP
@@ -141,6 +143,7 @@ int nxtask_exit(void)
 #endif
 
   dtcb->task_state = TSTATE_TASK_INACTIVE;
+  sched_note_stop(dtcb);
   ret = nxsched_release_tcb(dtcb, dtcb->flags & TCB_FLAG_TTYPE_MASK);
 
 #ifdef CONFIG_SMP