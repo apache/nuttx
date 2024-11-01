edabf7e5f17c746bd1a5c42da78d5da5803c2bdb
hujun5 <hujun5@xiaomi.com>
2024-11-01 18:16:51 +0800

    fix some arch miss update g_running_tasks
    Signed-off-by: hujun5 <hujun5@xiaomi.com>

--- arch/x86/src/common/x86_switchcontext.c
+++ arch/x86/src/common/x86_switchcontext.c
@@ -102,6 +102,10 @@ void up_switch_context(struct tcb_s *tcb, struct tcb_s *rtcb)
 
       nxsched_resume_scheduler(tcb);
 
+      /* Record the new "running" task */
+
+      g_running_tasks[this_cpu()] = tcb;
+
       /* Then switch contexts */
 
       x86_fullcontextrestore(tcb->xcp.regs);
