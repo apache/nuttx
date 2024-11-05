5234ae1fa88febfa01e4fd6fae72d6ba09d4f113
Justin Morrison <justinmorrison@cmail.carleton.ca>
2024-11-04 23:29:59 -0500

    Initial Commit. Able to set configuration settings for the RN2483 device. Implemented with the rn2483_radio_set_cmd function which uses an enum to represent each cmd and sends a radio set <message> to set the desired values of the corresponding cmd. void *data is the generic pointer which will be set to different data types, so appropriate pointer casting was used. All inputs to the radio_set_cmd function are presumed to have already been checked, which is why there are no checks in the function itself. A dummy file_read call is a temporary fix to get rid of an annoying bug where the 'sys get ver' value would be the first values in the read buffer after startup. Logging is used mostly for debugging at this point through syslog.

--- drivers/wireless/lpwan/rn2483/rn2483.c
+++ drivers/wireless/lpwan/rn2483/rn2483.c
@@ -39,11 +39,14 @@
 #include <time.h>
 #include <unistd.h>
 
+#include <syslog.h>
+
 #include <nuttx/kmalloc.h>
 #include <nuttx/mutex.h>
 #include <nuttx/semaphore.h>
 
 #include <nuttx/wireless/lpwan/rn2483.h>
+#include <nuttx/wireless/lpwan/sx127x.h>
 
 /****************************************************************************
  * Pre-processor Definitions
@@ -54,6 +57,12 @@
 #ifndef CONFIG_LPWAN_RN2483_FREQ
 #define CONFIG_LPWAN_RN2483_FREQ 433050000
 #endif /* CONFIG_LPWAN_RN2483_FREQ */
+
+#if (CONFIG_LPWAN_RN2483_FREQ < 433000000 ||                                           \
+     (CONFIG_LPWAN_RN2483_FREQ > 434800000 && CONFIG_LPWAN_RN2483_FREQ < 863000000) || \
+     CONFIG_LPWAN_RN2483_FREQ > 870000000)
+#error "RN2483 frequency has to be between 433000000 and 434800000, or between 863000000 and 870000000, in Hz"
+#endif /* Bandwidth value check */
 
 /* Sync word */
 
@@ -73,7 +82,7 @@
 #define CONFIG_LPWAN_RN2483_BW 500
 #endif /* CONFIG_LPWAN_RN2483_BW */
 
-#if (CONFIG_LPWAN_RN2483_BW != 125 && CONFIG_LPWAN_RN2483_BW != 250 &&       \
+#if (CONFIG_LPWAN_RN2483_BW != 125 && CONFIG_LPWAN_RN2483_BW != 250 && \
      CONFIG_LPWAN_RN2483_BW != 500)
 #error "RN2483 bandwidth can only be one of 125, 250 and 500kHz"
 #endif /* Bandwidth value check */
@@ -161,6 +170,28 @@ struct rn2483_dev_s
   mutex_t devlock;
 };
 
+/*Represents the set commands for the RN2483 device*/
+typedef enum
+{
+  RN2483_SET_CR,
+  RN2483_SET_MOD,
+  RN2483_SET_FREQ,
+  RN2483_SET_BW,
+  RN2483_SET_PRLEN,
+  RN2483_SET_CRC,
+  RN2483_SET_IQI,
+  RN2483_SET_PWR,
+  RN2483_SET_SF,
+  RN2483_SET_SYNC,
+} rn2483_set_params;
+
+/*Represents the ioctl commands for the RN2483 device*/
+typedef enum
+{
+  RN2483_IOCTL_SET_FREQ,
+
+} rn2483_ioctl_cmd;
+
 /****************************************************************************
  * Private Function Prototypes
  ****************************************************************************/
@@ -175,6 +206,7 @@ static ssize_t rn2483_read(FAR struct file *filep, FAR char *buffer,
                            size_t buflen);
 static int rn2483_ioctl(FAR struct file *filep, int cmd, unsigned long arg);
 static int rn2483_unlink(FAR struct inode *inode);
+static int rn2483_radio_set_cmd(FAR struct rn2483_dev_s *priv, rn2483_set_params option, void *data);
 
 /****************************************************************************
  * Private Data
@@ -216,6 +248,23 @@ static const char *CODING_RATES[] = {
     [RN2483_CR_4_8] = "4/8",
 };
 
+/* String representation of set options */
+static const char *RN2483_SET_OPTIONS[] = {
+    [RN2483_SET_CR] = "cr",
+    [RN2483_SET_MOD] = "mod",
+    [RN2483_SET_FREQ] = "freq",
+    [RN2483_SET_BW] = "bw",
+    [RN2483_SET_PRLEN] = "prlen",
+    [RN2483_SET_CRC] = "crc",
+    [RN2483_SET_IQI] = "iqi",
+    [RN2483_SET_PWR] = "pwr",
+    [RN2483_SET_SF] = "sf",
+    [RN2483_SET_SYNC] = "sync",
+};
+
+#define SUCCESS "SUCCESS"
+#define FAILURE "FAILURE"
+
 /****************************************************************************
  * Private Functions
  ****************************************************************************/
@@ -237,9 +286,9 @@ static int rn2483_open(FAR struct file *filep)
 
   err = nxmutex_lock(&priv->devlock);
   if (err < 0)
-    {
-      return err;
-    }
+  {
+    return err;
+  }
 
   /* Increment the count of open references on the driver */
 
@@ -268,9 +317,9 @@ static int rn2483_close(FAR struct file *filep)
 
   err = nxmutex_lock(&priv->devlock);
   if (err < 0)
-    {
-      return err;
-    }
+  {
+    return err;
+  }
 
   /* Decrement the count of open references on the driver */
 
@@ -282,12 +331,12 @@ static int rn2483_close(FAR struct file *filep)
    */
 
   if (priv->crefs <= 0 && priv->unlinked)
-    {
-      file_close(&priv->uart);
-      nxmutex_destroy(&priv->devlock);
-      kmm_free(priv);
-      return 0;
-    }
+  {
+    file_close(&priv->uart);
+    nxmutex_destroy(&priv->devlock);
+    kmm_free(priv);
+    return 0;
+  }
 
   nxmutex_unlock(&priv->devlock);
   return 0;
@@ -305,6 +354,8 @@ static int rn2483_close(FAR struct file *filep)
 static ssize_t rn2483_read(FAR struct file *filep, FAR char *buffer,
                            size_t buflen)
 {
+  syslog(LOG_INFO, "Enter read\n");
+
   FAR struct inode *inode = filep->f_inode;
   FAR struct rn2483_dev_s *priv = inode->i_private;
   ssize_t length = 0;
@@ -313,26 +364,26 @@ static ssize_t rn2483_read(FAR struct file *filep, FAR char *buffer,
   /* If file position is non-zero, then we're at the end of file. */
 
   if (filep->f_pos > 0)
-    {
-      return 0;
-    }
+  {
+    return 0;
+  }
 
   /* Get exclusive access */
 
   err = nxmutex_lock(&priv->devlock);
   if (err < 0)
-    {
-      return err;
-    }
+  {
+    return err;
+  }
 
 #ifndef CONFIG_DISABLE_PSEUDOFS_OPERATIONS
   if (priv->unlinked)
-    {
-      /* Do not allow operations on unlinked drivers. */
+  {
+    /* Do not allow operations on unlinked drivers. */
 
-      nxmutex_unlock(&priv->devlock);
-      return 0;
-    }
+    nxmutex_unlock(&priv->devlock);
+    return 0;
+  }
 #endif
 
   // TODO actually receive
@@ -341,11 +392,11 @@ static ssize_t rn2483_read(FAR struct file *filep, FAR char *buffer,
   char command[] = "sys get ver\r\n";
   length = file_write(&priv->uart, command, sizeof(command));
   if (length < 0)
-    {
-      // TODO: log error
-      nxmutex_unlock(&priv->devlock);
-      return length;
-    }
+  {
+    // TODO: log error
+    nxmutex_unlock(&priv->devlock);
+    return length;
+  }
 
   // Read back the version string
   length = file_read(&priv->uart, buffer, buflen);
@@ -364,8 +415,18 @@ static ssize_t rn2483_read(FAR struct file *filep, FAR char *buffer,
 
 static ssize_t rn2483_write(FAR struct file *filep, FAR const char *buffer,
                             size_t buflen)
-{
-  // TODO actually transmit
+{ // // TODO actually transmit
+
+  FAR struct inode *inode = filep->f_inode;
+  FAR struct rn2483_dev_s *priv = inode->i_private;
+  int err;
+
+  err = nxmutex_lock(&priv->devlock);
+  if (err < 0)
+  {
+    return err;
+  }
+
   return -ENOSYS;
 }
 
@@ -381,28 +442,40 @@ static int rn2483_ioctl(FAR struct file *filep, int cmd, unsigned long arg)
 
   err = nxmutex_lock(&priv->devlock);
   if (err < 0)
-    {
-      return err;
-    }
+  {
+    return err;
+  }
 
 #ifndef CONFIG_DISABLE_PSEUDOFS_OPERATIONS
   if (priv->unlinked)
-    {
-      /* Do not allow operations on unlinked drivers. */
+  {
+    /* Do not allow operations on unlinked drivers. */
 
-      nxmutex_unlock(&priv->devlock);
-      return -ENODEV;
-    }
+    nxmutex_unlock(&priv->devlock);
+    return -ENODEV;
+  }
 #endif
 
   /* Handle command */
   switch (cmd)
+  {
+    // TODO: real commands
+
+  case RN2483_IOCTL_SET_FREQ:
+  {
+    FAR uint32_t *ptr = (FAR uint32_t *)((uintptr_t)arg);
+    err = rn2483_radio_set_cmd(priv, RN2483_SET_FREQ, ptr);
+    if (err < 0)
     {
-      // TODO: real commands
-    default:
-      err = -EINVAL;
-      break;
+      return err;
     }
+    break;
+  }
+
+  default:
+    err = -EINVAL;
+    break;
+  }
 
   nxmutex_unlock(&priv->devlock);
   return err;
@@ -423,18 +496,18 @@ static int rn2483_unlink(FAR struct inode *inode)
 
   err = nxmutex_lock(&priv->devlock);
   if (err < 0)
-    {
-      return err;
-    }
+  {
+    return err;
+  }
 
   /* Are there open references to the driver data structure? */
 
   if (priv->crefs <= 0)
-    {
-      nxmutex_destroy(&priv->devlock);
-      kmm_free(priv);
-      return 0;
-    }
+  {
+    nxmutex_destroy(&priv->devlock);
+    kmm_free(priv);
+    return 0;
+  }
 
   /* No. Just mark the driver as unlinked and free the resources when
    * the last client closes their reference to the driver.
@@ -445,6 +518,227 @@ static int rn2483_unlink(FAR struct inode *inode)
   return OK;
 }
 #endif
+
+/****************************************************************************
+ * Name: rn2483_radio_set_cmd
+ *
+ * Description:
+ *     Transmit set commands to radio board.
+ ****************************************************************************/
+
+static int rn2483_radio_set_cmd(FAR struct rn2483_dev_s *priv, rn2483_set_params cmd, void *data)
+{
+  syslog(LOG_INFO, "SET: %s", RN2483_SET_OPTIONS[cmd]);
+
+  char write_buffer[50];
+  ssize_t written;
+
+  switch (cmd)
+  {
+  case RN2483_SET_CR:
+  {
+    written = snprintf(write_buffer, sizeof(write_buffer), "radio set cr %s\r\n", CODING_RATES[*(enum rn2483_cr_e *)data]);
+    syslog(LOG_INFO, "%s", write_buffer);
+    // syslog(LOG_INFO, "Write Length: %d\n", written);
+    break;
+  }
+  case RN2483_SET_MOD:
+  {
+    written = snprintf(write_buffer, sizeof(write_buffer), "radio set mod %s\r\n", MODULATIONS[*(enum rn2483_mod_e *)data]);
+    syslog(LOG_INFO, "%s", write_buffer);
+    // syslog(LOG_INFO, "Write Length: %d\n", written);
+    break;
+  }
+  case RN2483_SET_FREQ:
+  {
+    written = snprintf(write_buffer, sizeof(write_buffer), "radio set freq %lu\r\n", *(uint32_t *)data);
+    syslog(LOG_INFO, "%s", write_buffer);
+    // syslog(LOG_INFO, "Write Length: %d\n", written);
+    break;
+  }
+  case RN2483_SET_BW:
+  {
+    written = snprintf(write_buffer, sizeof(write_buffer), "radio set bw %hu\r\n", *(uint16_t *)data);
+    syslog(LOG_INFO, "%s", write_buffer);
+    // syslog(LOG_INFO, "Write Length: %d\n", written);
+    break;
+  }
+  case RN2483_SET_PRLEN:
+  {
+    written = snprintf(write_buffer, sizeof(write_buffer), "radio set prlen %hu\r\n", *(uint16_t *)data);
+    syslog(LOG_INFO, "%s", write_buffer);
+    // syslog(LOG_INFO, "Write Length: %d\n", written);
+    break;
+  }
+  case RN2483_SET_CRC:
+  {
+    char *crc = (*(bool *)data) ? "on" : "off";
+    written = snprintf(write_buffer, sizeof(write_buffer), "radio set crc %s\r\n", crc);
+    syslog(LOG_INFO, "%s", write_buffer);
+    // syslog(LOG_INFO, "Write Length: %d\n", written);
+    break;
+  }
+  case RN2483_SET_IQI:
+  {
+    char *iqi = (*(bool *)data) ? "on" : "off";
+    written = snprintf(write_buffer, sizeof(write_buffer), "radio set iqi %s\r\n", iqi);
+    syslog(LOG_INFO, "%s", write_buffer);
+    // syslog(LOG_INFO, "Write Length: %d\n", written);
+    break;
+  }
+  case RN2483_SET_PWR:
+  {
+    written = snprintf(write_buffer, sizeof(write_buffer), "radio set pwr %d\r\n", *(int8_t *)data);
+    syslog(LOG_INFO, "%s", write_buffer);
+    // syslog(LOG_INFO, "Write Length: %d\n", written);
+    break;
+  }
+  case RN2483_SET_SF:
+  {
+    written = snprintf(write_buffer, sizeof(write_buffer), "radio set sf sf%d\r\n", *(uint8_t *)data);
+    syslog(LOG_INFO, "%s", write_buffer);
+    // syslog(LOG_INFO, "Write Length: %d\n", written);
+    break;
+  }
+  case RN2483_SET_SYNC:
+  {
+    written = snprintf(write_buffer, sizeof(write_buffer), "radio set sync %d\r\n", *(uint8_t *)data);
+    syslog(LOG_INFO, "%s", write_buffer);
+    // syslog(LOG_INFO, "Write Length: %d\n", written);
+    break;
+  }
+  default:
+  {
+    // Not a valid set command
+    syslog(LOG_INFO, "INVALID COMMAND GIVEN %d", cmd);
+    return -EINVAL;
+  }
+  }
+
+  ssize_t write_length = file_write(&priv->uart, write_buffer, written);
+  if (write_length < 0)
+  {
+    return write_length;
+  }
+
+  // Read
+  int buf_len = 50;
+  char read_buffer[buf_len];
+  size_t read = 0;
+  ssize_t fread;
+  char last = '\0';
+
+  // Read data into buffer until \r\n terminating sequence reached
+  while (read < buf_len)
+  {
+    fread = file_read(&priv->uart, &read_buffer[read], 1);
+    if (last == '\r' && read_buffer[read] == '\n')
+    {
+      break;
+    }
+    last = read_buffer[read];
+
+    if (fread < 0)
+      return fread;
+
+    read++;
+  }
+
+  // syslog(LOG_INFO, "%s", read_buffer);
+
+  if (strstr(read_buffer, "ok") != NULL)
+  {
+    syslog(LOG_INFO, "%s\n\n", SUCCESS);
+    return F_OK;
+  }
+  else if (strstr(read_buffer, "invalid_param") != NULL)
+  {
+    syslog(LOG_INFO, "%s\n\n", FAILURE);
+    return -EINVAL;
+  }
+  else
+  {
+    syslog(LOG_INFO, "INVALID VALUE (Not 'ok' or 'invalid_param')\n\n", FAILURE);
+    return -EINVAL;
+  }
+}
+
+int rn2483_set_config(FAR struct rn2483_dev_s *priv)
+{
+  int err;
+
+  /*  Set freq (Frequency in Hz)*/
+  err = rn2483_radio_set_cmd(priv, RN2483_SET_FREQ, &priv->config.freq);
+  if (err < 0)
+  {
+    return err;
+  }
+
+  /*  Set cr*/
+  err = rn2483_radio_set_cmd(priv, RN2483_SET_CR, &priv->config.cr);
+  if (err < 0)
+  {
+    return err;
+  }
+
+  /*  Set bw (Bandwidth in kHz)*/
+  err = rn2483_radio_set_cmd(priv, RN2483_SET_BW, &priv->config.bw);
+  if (err < 0)
+  {
+    return err;
+  }
+
+  /*  Set prlen (Preamble length)*/
+  err = rn2483_radio_set_cmd(priv, RN2483_SET_PRLEN, &priv->config.prlen);
+  if (err < 0)
+  {
+    return err;
+  }
+
+  /*  Set iqi (IQ invert, true for enabled)*/
+  err = rn2483_radio_set_cmd(priv, RN2483_SET_IQI, &priv->config.iqi);
+  if (err < 0)
+  {
+    return err;
+  }
+
+  /*  Set crc (Cyclic redundancy check, true for enabled)*/
+  err = rn2483_radio_set_cmd(priv, RN2483_SET_CRC, &priv->config.crc);
+  if (err < 0)
+  {
+    return err;
+  }
+
+  /*  Set pwr (Transmit output power)*/
+  err = rn2483_radio_set_cmd(priv, RN2483_SET_PWR, &priv->config.pwr);
+  if (err < 0)
+  {
+    return err;
+  }
+
+  /*  Set sf (Spread Factor)*/
+  err = rn2483_radio_set_cmd(priv, RN2483_SET_SF, &priv->config.sf);
+  if (err < 0)
+  {
+    return err;
+  }
+
+  /*  Set sync (Synchronization word)*/
+  err = rn2483_radio_set_cmd(priv, RN2483_SET_SYNC, &priv->config.sync);
+  if (err < 0)
+  {
+    return err;
+  }
+
+  /*  Set mod (Modulation: FSK or LoRa)*/
+  err = rn2483_radio_set_cmd(priv, RN2483_SET_MOD, &priv->config.mod);
+  if (err < 0)
+  {
+    return err;
+  }
+
+  return F_OK;
+}
 
 /****************************************************************************
  * Public Functions
@@ -460,6 +754,7 @@ static int rn2483_unlink(FAR struct inode *inode)
 
 int rn2483_register(FAR const char *devpath, FAR const char *uartpath)
 {
+  syslog(LOG_INFO, "Enter Register\n\n");
 
   FAR struct rn2483_dev_s *priv = NULL;
   int err = 0;
@@ -470,34 +765,34 @@ int rn2483_register(FAR const char *devpath, FAR const char *uartpath)
 
   priv = kmm_zalloc(sizeof(struct rn2483_dev_s));
   if (priv == NULL)
-    {
-      snerr("ERROR: Failed to allocate instance.\n");
-      return -ENOMEM;
-    }
+  {
+    snerr("ERROR: Failed to allocate instance.\n");
+    return -ENOMEM;
+  }
 
   /* Initialize mutex */
 
   err = nxmutex_init(&priv->devlock);
   if (err < 0)
-    {
-      // TODO: what logging macro needs to be used
-      // snerr("ERROR: Failed to register SHT4X driver: %d\n", err);
-      kmm_free(priv);
-      return err;
-    }
+  {
+    // TODO: what logging macro needs to be used
+    // snerr("ERROR: Failed to register SHT4X driver: %d\n", err);
+    kmm_free(priv);
+    return err;
+  }
 
   /* Get access to underlying UART interface */
 
   err = file_open(&priv->uart, uartpath, O_RDWR | O_CLOEXEC);
   if (err < 0)
-    {
-      // TODO log error
-      nxmutex_destroy(&priv->devlock);
-      kmm_free(priv);
-      return err;
-    }
+  {
+    // TODO log error
+    nxmutex_destroy(&priv->devlock);
+    kmm_free(priv);
+    return err;
+  }
 
-    /* Set configuration options. */
+  /* Set configuration options. */
 
 #if CONFIG_LPWAN_RN2483_CR == 5
   priv->config.cr = RN2483_CR_4_5;
@@ -515,8 +810,10 @@ int rn2483_register(FAR const char *devpath, FAR const char *uartpath)
   priv->config.mod = RN2483_MOD_FSK;
 #endif
 
-  // TODO: error check this value
   priv->config.freq = CONFIG_LPWAN_RN2483_FREQ;
+
+  // Error check of frequency value (Not sure if this is what you want Matteo)
+  assert((priv->config.freq >= 433000000 && priv->config.freq <= 434800000) || (priv->config.freq >= 863000000 && priv->config.freq <= 870000000));
 
   priv->config.bw = CONFIG_LPWAN_RN2483_BW;
   priv->config.prlen = CONFIG_LPWAN_RN2483_PRLEN;
@@ -526,19 +823,40 @@ int rn2483_register(FAR const char *devpath, FAR const char *uartpath)
   priv->config.sf = CONFIG_LPWAN_RN2483_SPREAD;
   priv->config.sync = CONFIG_LPWAN_RN2483_SYNC;
 
-  // TODO actually set configuration parameters on the radio via commands
+  // Dummy read to get rid of version string
+  int buf_len = 50;
+  char read_buffer[buf_len];
+  size_t read = 0;
+  ssize_t fread;
+  char last = '\0';
+
+  while (read < buf_len)
+  {
+    fread = file_read(&priv->uart, &read_buffer[read], 1);
+    if (last == '\r' && read_buffer[read] == '\n')
+    {
+      break;
+    }
+    last = read_buffer[read];
+
+    if (fread < 0)
+      return fread;
+    read++;
+  }
+
+  // Set configuration parameters on the radio via commands
+  rn2483_set_config(priv);
 
   /* Register the character driver */
-
   err = register_driver(devpath, &g_rn2483fops, 0666, priv);
   if (err < 0)
-    {
-      // TODO: what logging macro?
-      // snerr("ERROR: Failed to register RN2483 driver: %d\n", err);
-      file_close(&priv->uart);
-      nxmutex_destroy(&priv->devlock);
-      kmm_free(priv);
-    }
+  {
+    // TODO: what logging macro?
+    // snerr("ERROR: Failed to register RN2483 driver: %d\n", err);
+    file_close(&priv->uart);
+    nxmutex_destroy(&priv->devlock);
+    kmm_free(priv);
+  }
 
   return err;
 }
