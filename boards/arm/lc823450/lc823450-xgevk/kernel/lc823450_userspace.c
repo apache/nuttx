29e67b0fc1aa861cac2fc62d08e1a4f7f542aa61
Alin Jerpelea <alin.jerpelea@sony.com>
2024-11-06 14:23:04 +0100

    boards/arm/lc823450: migrate to SPDX identifier
    Most tools used for compliance and SBOM generation use SPDX identifiers
    This change brings us a step closer to an easy SBOM generation.
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- boards/arm/lc823450/lc823450-xgevk/kernel/lc823450_userspace.c
+++ boards/arm/lc823450/lc823450-xgevk/kernel/lc823450_userspace.c
@@ -1,6 +1,8 @@
 /****************************************************************************
  * boards/arm/lc823450/lc823450-xgevk/kernel/lc823450_userspace.c
  *
+ * SPDX-License-Identifier: Apache-2.0
+ *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.  The
