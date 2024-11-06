f14b9ed30228dcf5e14ee0be31603c4a7bc25354
Alin Jerpelea <alin.jerpelea@sony.com>
2024-11-06 15:24:12 +0100

    boards/arm/max326xx: migrate to SPDX identifier
    Most tools used for compliance and SBOM generation use SPDX identifiers
    This change brings us a step closer to an easy SBOM generation.
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- boards/arm/max326xx/max32660-evsys/src/max326_userleds.c
+++ boards/arm/max326xx/max32660-evsys/src/max326_userleds.c
@@ -1,6 +1,8 @@
 /****************************************************************************
  * boards/arm/max326xx/max32660-evsys/src/max326_userleds.c
  *
+ * SPDX-License-Identifier: Apache-2.0
+ *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.  The
