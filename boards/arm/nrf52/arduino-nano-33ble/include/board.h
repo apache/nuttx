ce70661c52b2f287b75fb3fe748ba0bf18fdd41d
Alin Jerpelea <alin.jerpelea@sony.com>
2024-11-06 15:51:02 +0100

    boards/arm/nrf52: migrate to SPDX identifier
    Most tools used for compliance and SBOM generation use SPDX identifiers
    This change brings us a step closer to an easy SBOM generation.
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- boards/arm/nrf52/arduino-nano-33ble/include/board.h
+++ boards/arm/nrf52/arduino-nano-33ble/include/board.h
@@ -1,6 +1,8 @@
 /****************************************************************************
  * boards/arm/nrf52/arduino-nano-33ble/include/board.h
  *
+ * SPDX-License-Identifier: Apache-2.0
+ *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.  The
