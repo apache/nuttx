2f5278b882bc22b5c7a9f468eee87ffcea218c36
Alin Jerpelea <alin.jerpelea@sony.com>
2024-11-06 15:11:40 +0100

    boards/arm/lpc31xx: migrate to SPDX identifier
    Most tools used for compliance and SBOM generation use SPDX identifiers
    This change brings us a step closer to an easy SBOM generation.
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- boards/arm/lpc31xx/olimex-lpc-h3131/src/lpc31_spi.c
+++ boards/arm/lpc31xx/olimex-lpc-h3131/src/lpc31_spi.c
@@ -1,6 +1,8 @@
 /****************************************************************************
  * boards/arm/lpc31xx/olimex-lpc-h3131/src/lpc31_spi.c
  *
+ * SPDX-License-Identifier: Apache-2.0
+ *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.  The
