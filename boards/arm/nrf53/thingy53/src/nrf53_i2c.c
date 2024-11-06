6802dcd9dee148ae40f22d8c7a7968a95aa97a67
Alin Jerpelea <alin.jerpelea@sony.com>
2024-11-06 15:59:37 +0100

    boards/arm/nrf53: migrate to SPDX identifier
    Most tools used for compliance and SBOM generation use SPDX identifiers
    This change brings us a step closer to an easy SBOM generation.
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- boards/arm/nrf53/thingy53/src/nrf53_i2c.c
+++ boards/arm/nrf53/thingy53/src/nrf53_i2c.c
@@ -1,6 +1,8 @@
 /****************************************************************************
  * boards/arm/nrf53/thingy53/src/nrf53_i2c.c
  *
+ * SPDX-License-Identifier: Apache-2.0
+ *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.  The
