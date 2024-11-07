4fe21362d28325f5fc72e11dd752f0e3b0c33ca8
Alin Jerpelea <alin.jerpelea@sony.com>
2024-11-07 10:57:21 +0100

    boards/arm/samv7: migrate to SPDX identifier
    Most tools used for compliance and SBOM generation use SPDX identifiers
    This change brings us a step closer to an easy SBOM generation.
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- boards/arm/samv7/common/include/sam_automount.h
+++ boards/arm/samv7/common/include/sam_automount.h
@@ -1,6 +1,8 @@
 /****************************************************************************
  * boards/arm/samv7/common/include/sam_automount.h
  *
+ * SPDX-License-Identifier: Apache-2.0
+ *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.  The
