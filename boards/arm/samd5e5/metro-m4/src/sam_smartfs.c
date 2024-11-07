cf861d93c85e274b43454752879cee35a10285dd
Alin Jerpelea <alin.jerpelea@sony.com>
2024-11-07 10:49:43 +0100

    boards/arm/samd5e5: migrate to SPDX identifier
    Most tools used for compliance and SBOM generation use SPDX identifiers
    This change brings us a step closer to an easy SBOM generation.
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- boards/arm/samd5e5/metro-m4/src/sam_smartfs.c
+++ boards/arm/samd5e5/metro-m4/src/sam_smartfs.c
@@ -1,6 +1,8 @@
 /****************************************************************************
  * boards/arm/samd5e5/metro-m4/src/sam_smartfs.c
  *
+ * SPDX-License-Identifier: Apache-2.0
+ *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.  The
--- boards/arm/samd5e5/same54-xplained-pro/src/sam_phyinit.c
+++ boards/arm/samd5e5/same54-xplained-pro/src/sam_phyinit.c
@@ -1,6 +1,8 @@
 /****************************************************************************
  * boards/arm/samd5e5/same54-xplained-pro/src/sam_phyinit.c
  *
+ * SPDX-License-Identifier: Apache-2.0
+ *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.  The
