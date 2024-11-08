5a90a565c1450cddd2914ab4a56efa75898b53d7
Alin Jerpelea <alin.jerpelea@sony.com>
2024-11-08 15:01:36 +0100

    boards/arm/tms570: migrate to SPDX identifier
    Most tools used for compliance and SBOM generation use SPDX identifiers
    This change brings us a step closer to an easy SBOM generation.
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- boards/arm/tms570/launchxl-tms57004/src/tms570_userleds.c
+++ boards/arm/tms570/launchxl-tms57004/src/tms570_userleds.c
@@ -1,6 +1,8 @@
 /****************************************************************************
  * boards/arm/tms570/launchxl-tms57004/src/tms570_userleds.c
  *
+ * SPDX-License-Identifier: Apache-2.0
+ *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.  The
