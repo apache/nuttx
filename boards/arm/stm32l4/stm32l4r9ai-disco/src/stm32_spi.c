290948e4a962f4caf7198644a6d12e62ca20d144
Alin Jerpelea <alin.jerpelea@sony.com>
2024-11-08 14:26:34 +0100

    boards/arm/stm32l4: migrate to SPDX identifier
    Most tools used for compliance and SBOM generation use SPDX identifiers
    This change brings us a step closer to an easy SBOM generation.
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- boards/arm/stm32l4/stm32l4r9ai-disco/src/stm32_spi.c
+++ boards/arm/stm32l4/stm32l4r9ai-disco/src/stm32_spi.c
@@ -1,6 +1,8 @@
 /****************************************************************************
  * boards/arm/stm32l4/stm32l4r9ai-disco/src/stm32_spi.c
  *
+ * SPDX-License-Identifier: Apache-2.0
+ *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.  The
