defd1cbaba3274c1a7224c879a88e8ce528c4f28
Alin Jerpelea <alin.jerpelea@sony.com>
2024-11-12 09:10:19 +0100

    boards/renesas/rx65n: migrate to SPDX identifier
    Most tools used for compliance and SBOM generation use SPDX identifiers
    This change brings us a step closer to an easy SBOM generation.
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- boards/renesas/rx65n/rx65n-grrose/src/rx65n_appinit.c
+++ boards/renesas/rx65n/rx65n-grrose/src/rx65n_appinit.c
@@ -1,6 +1,8 @@
 /****************************************************************************
  * boards/renesas/rx65n/rx65n-grrose/src/rx65n_appinit.c
  *
+ * SPDX-License-Identifier: Apache-2.0
+ *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.  The
