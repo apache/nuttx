0c5be55aee9d14b1ea6fc3282360b3673db78f06
Alin Jerpelea <alin.jerpelea@sony.com>
2024-11-12 09:34:01 +0100

    boards/risk-v/esp32c6: migrate to SPDX identifier
    Most tools used for compliance and SBOM generation use SPDX identifiers
    This change brings us a step closer to an easy SBOM generation.
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- boards/risc-v/esp32c6/esp32c6-devkitc/src/esp32c6_buttons.c
+++ boards/risc-v/esp32c6/esp32c6-devkitc/src/esp32c6_buttons.c
@@ -1,6 +1,8 @@
 /****************************************************************************
  * boards/risc-v/esp32c6/esp32c6-devkitc/src/esp32c6_buttons.c
  *
+ * SPDX-License-Identifier: Apache-2.0
+ *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.  The
