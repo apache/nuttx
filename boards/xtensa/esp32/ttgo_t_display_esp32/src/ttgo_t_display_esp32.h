84f2fe05c121e4667edd4d8885922906ec079846
Alin Jerpelea <alin.jerpelea@sony.com>
2024-11-12 12:59:02 +0100

    boards/xtensa/esp32: migrate to SPDX identifier
    Most tools used for compliance and SBOM generation use SPDX identifiers
    This change brings us a step closer to an easy SBOM generation.
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- boards/xtensa/esp32/ttgo_t_display_esp32/src/ttgo_t_display_esp32.h
+++ boards/xtensa/esp32/ttgo_t_display_esp32/src/ttgo_t_display_esp32.h
@@ -1,6 +1,8 @@
 /****************************************************************************
  * boards/xtensa/esp32/ttgo_t_display_esp32/src/ttgo_t_display_esp32.h
  *
+ * SPDX-License-Identifier: Apache-2.0
+ *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.  The
