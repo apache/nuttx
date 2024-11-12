90f876bbacb5ed7b07ea190e51094b04d09c4008
Alin Jerpelea <alin.jerpelea@sony.com>
2024-11-12 09:49:13 +0100

    boards/risk-v/litex: migrate to SPDX identifier
    Most tools used for compliance and SBOM generation use SPDX identifiers
    This change brings us a step closer to an easy SBOM generation.
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- boards/risc-v/litex/arty_a7/src/litex_appinit.c
+++ boards/risc-v/litex/arty_a7/src/litex_appinit.c
@@ -1,6 +1,8 @@
 /****************************************************************************
  * boards/risc-v/litex/arty_a7/src/litex_appinit.c
  *
+ * SPDX-License-Identifier: Apache-2.0
+ *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.  The
