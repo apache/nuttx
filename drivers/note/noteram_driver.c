50de39acd5481838c17fb13df3d8c2559f153900
Xiang Xiao <xiaoxiang@xiaomi.com>
2024-11-16 23:04:40 +0800

    note/noteram_driver: Remove the unused variable j
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- drivers/note/noteram_driver.c
+++ drivers/note/noteram_driver.c
@@ -1013,7 +1013,6 @@ static int noteram_dump_one(FAR uint8_t *p, FAR struct lib_outstream_s *s,
       {
         FAR struct note_syscall_enter_s *nsc;
         int i;
-        int j;
         uintptr_t arg;
 
         nsc = (FAR struct note_syscall_enter_s *)p;
@@ -1027,7 +1026,7 @@ static int noteram_dump_one(FAR uint8_t *p, FAR struct lib_outstream_s *s,
         ret += lib_sprintf(s, "sys_%s(",
                            g_funcnames[nsc->nsc_nr - CONFIG_SYS_RESERVED]);
 
-        for (i = j = 0; i < nsc->nsc_argc; i++)
+        for (i = 0; i < nsc->nsc_argc; i++)
           {
             arg = nsc->nsc_args[i];
             if (i == 0)
