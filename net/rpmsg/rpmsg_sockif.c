3fa55fb72e41437aface7faa7a5621dd7e4302c5
yintao <yintao@xiaomi.com>
2024-11-26 11:41:41 +0800

    net/rpmsg: support POLLHUP when unbind
    rpmsg_socket_ns_unbind should notify pollhup when reconnect
    Otherwise, uv_poll can't UV_DISCONNECT
    eg:
    if (revent & POLL_ERROR || revent & POLL_DISCONNECT)
               xxxx();
    
    Signed-off-by: yintao <yintao@xiaomi.com>

--- net/rpmsg/rpmsg_sockif.c
+++ net/rpmsg/rpmsg_sockif.c
@@ -479,7 +479,7 @@ static void rpmsg_socket_ns_unbind(FAR struct rpmsg_endpoint *ept)
   conn->unbind = true;
   rpmsg_socket_post(&conn->sendsem);
   rpmsg_socket_post(&conn->recvsem);
-  rpmsg_socket_poll_notify(conn, POLLIN | POLLOUT);
+  rpmsg_socket_poll_notify(conn, POLLIN | POLLOUT | POLLHUP | POLLERR);
 
   nxmutex_unlock(&conn->recvlock);
 }
