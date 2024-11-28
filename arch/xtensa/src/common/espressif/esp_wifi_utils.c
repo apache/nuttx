ea7ad079287d353581ccb873b6a6804588fe8791
Alin Jerpelea <alin.jerpelea@sony.com>
2024-11-28 12:13:32 +0100

    arch/xtensa: migrate to SPDX identifier
    Most tools used for compliance and SBOM generation use SPDX identifiers
    This change brings us a step closer to an easy SBOM generation.
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- arch/xtensa/src/common/espressif/esp_wifi_utils.c
+++ arch/xtensa/src/common/espressif/esp_wifi_utils.c
@@ -1,6 +1,8 @@
 /****************************************************************************
  * arch/xtensa/src/common/espressif/esp_wifi_utils.c
  *
+ * SPDX-License-Identifier: Apache-2.0
+ *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.  The
