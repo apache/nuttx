b6a32301ee97e58580a0212210a88c23f0979ec9
Alin Jerpelea <alin.jerpelea@sony.com>
2024-11-28 12:37:54 +0100

    arch/x86: migrate to SPDX identifier
    Most tools used for compliance and SBOM generation use SPDX identifiers
    This change brings us a step closer to an easy SBOM generation.
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- arch/x86/src/common/x86_udelay.c
+++ arch/x86/src/common/x86_udelay.c
@@ -1,6 +1,8 @@
 /****************************************************************************
  * arch/x86/src/common/x86_udelay.c
  *
+ * SPDX-License-Identifier: Apache-2.0
+ *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.  The
