3dde10adaa14e123652cac9c6700296a30a002ab
Alin Jerpelea <alin.jerpelea@sony.com>
2024-11-29 14:22:21 +0100

    arch/risk-v: migrate to SPDX identifier
    Most tools used for compliance and SBOM generation use SPDX identifiers
    This change brings us a step closer to an easy SBOM generation.
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- arch/risc-v/src/k210/k210_userspace.c
+++ arch/risc-v/src/k210/k210_userspace.c
@@ -1,6 +1,8 @@
 /****************************************************************************
  * arch/risc-v/src/k210/k210_userspace.c
  *
+ * SPDX-License-Identifier: Apache-2.0
+ *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.  The
