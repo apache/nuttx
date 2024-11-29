b322651d08d7df7c5e0ef786d3d63a7f2b853f2e
buxiasen <buxiasen@xiaomi.com>
2024-11-29 12:17:47 +0800

    coredump: add BOARD_CRASHDUMP_CUSTOM support
    For only board specific crashdump and no syslog/blk/mtd coredump
    
    Signed-off-by: buxiasen <buxiasen@xiaomi.com>

--- include/nuttx/board.h
+++ include/nuttx/board.h
@@ -820,7 +820,7 @@ int board_button_irq(int id, xcpt_t irqhandler, FAR void *arg);
  *
  ****************************************************************************/
 
-#ifdef CONFIG_BOARD_CRASHDUMP
+#ifdef CONFIG_BOARD_CRASHDUMP_CUSTOM
 struct tcb_s;
 void board_crashdump(uintptr_t sp, FAR struct tcb_s *tcb,
                      FAR const char *filename, int lineno,
