7693764f3463292135ed8e3231e7f699912d3e43
Alin Jerpelea <alin.jerpelea@sony.com>
2024-12-02 09:54:29 +0100

    arch/misoc: migrate to SPDX identifier
    Most tools used for compliance and SBOM generation use SPDX identifiers
    This change brings us a step closer to an easy SBOM generation.
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- arch/misoc/include/lm32/irq.h
+++ arch/misoc/include/lm32/irq.h
@@ -1,6 +1,8 @@
 /****************************************************************************
  * arch/misoc/include/lm32/irq.h
  *
+ * SPDX-License-Identifier: Apache-2.0
+ *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.  The
