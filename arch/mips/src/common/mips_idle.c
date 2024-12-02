bf02782568efc85b95ff212b64e70870db625fc5
Alin Jerpelea <alin.jerpelea@sony.com>
2024-12-02 10:36:37 +0100

    arch/misps: migrate to SPDX identifier
    Most tools used for compliance and SBOM generation use SPDX identifiers
    This change brings us a step closer to an easy SBOM generation.
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- arch/mips/src/common/mips_idle.c
+++ arch/mips/src/common/mips_idle.c
@@ -1,6 +1,8 @@
 /****************************************************************************
  * arch/mips/src/common/mips_idle.c
  *
+ * SPDX-License-Identifier: Apache-2.0
+ *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.  The
