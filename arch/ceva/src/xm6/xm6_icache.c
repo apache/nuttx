6e268f3c4586355ba1618abd55966e4c6bb1ec7a
Alin Jerpelea <alin.jerpelea@sony.com>
2024-12-02 10:53:57 +0100

    arch/ceva: migrate to SPDX identifier
    Most tools used for compliance and SBOM generation use SPDX identifiers
    This change brings us a step closer to an easy SBOM generation.
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- arch/ceva/src/xm6/xm6_icache.c
+++ arch/ceva/src/xm6/xm6_icache.c
@@ -1,6 +1,8 @@
 /****************************************************************************
  * arch/ceva/src/xm6/xm6_icache.c
  *
+ * SPDX-License-Identifier: Apache-2.0
+ *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.  The
