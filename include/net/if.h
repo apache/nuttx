631d80729d90b143e3a095ba18838df7a8d45aff
wangxiaoxin <wangxiaoxin@xiaomi.com>
2024-12-03 21:23:13 +0800

    Change can's state to can_st_type
    VELAPLATFO-48879
    
    Change-Id: Id27ce52097f19ff2fe8911da233867a65a08ec85
    Signed-off-by: wangxiaoxin <wangxiaoxin@xiaomi.com>

--- include/net/if.h
+++ include/net/if.h
@@ -211,7 +211,7 @@ enum can_ioctl_state_e
 struct can_ioctl_state_s
 {
   uintptr_t priv;             /* This is private data. */
-  enum can_ioctl_state_e state;
+  enum can_ioctl_state_e can_st_type;
 };
 
 /* There are two forms of the I/F request structure.
