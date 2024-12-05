1cfab89e65e1ec5cd24b22219c476612131367d6
Kyle Wilson <kwilson@2g-eng.com>
2024-12-05 13:08:56 -0600

    Added incomplete octospi hardware defintions.
    Register addresses are defined. Bit defintions are still needed.
    
    Bitmaps for OCTOSPI peripheral register
    
    Incremental updates to qspi files
    
    Redefined QSPI/OCTOSPI registers for STM32H5
    
    Fixed register definitions for the STM32H5. However, more work still needs to be done. Some bits that shared registers on the STM32H7 have different register locations on the H5. This still needs to be accounted for.
    
    Fixed more differences vs STM32H7 qspi
    
    Added ifdef for including stm32_dma.h in stm32_qspi.c. Added stm32_qspi.c to Make.defs.
    
    Register fixes. SPI activity but not able to format device yet.
    
    Fixed DCYC mask
    
    Set HCLK frequency to correct value. Undid ccrconfig debug.
    
    Tested Interrupt Mode (single SPI). Added alternate bytes to meminfo and cmdinfo structures. Updated Kconfig variables for STM32H5.
    
    Fixed base register in hardware/stm32_qspi.h. Updated qspi_dumpregs.
    
    The base register was previously set to STM32_QUADSPI_BASE, changed to the correctly named STM32_OCTOSPI1_BASE. However, these defines for the OCTOSPI registers are not even used. Instead qspi_putreg and qspi_getreg utilize the priv->base value to access OCTOSPI registers.
    
    Removed altbytes code, left as before. Moved QSPI clock selection to stm32h5xx_rcc.c
    
    Changed STM32H5_QUADSPI to STM32H5_QSPI1
    
    Added hook to define QSPI_CLK_FREQUENCY as STM32_QSPI_FREQUENCY from board.h
    
    Removed changes to nuttx qspi.h
    
    style fixes

---
+++ arch/arm/src/stm32h5/stm32_qspi.h
@@ -0,0 +1,129 @@
+/****************************************************************************
+ * arch/arm/src/stm32h5/stm32_qspi.h
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+#ifndef __ARCH_ARM_SRC_STM32H5_STM32_QSPI_H
+#define __ARCH_ARM_SRC_STM32H5_STM32_QSPI_H
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+#include <nuttx/spi/qspi.h>
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "chip.h"
+
+#ifdef CONFIG_STM32H5_QSPI1
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Public Types
+ ****************************************************************************/
+
+/****************************************************************************
+ * Inline Functions
+ ****************************************************************************/
+
+#ifndef __ASSEMBLY__
+
+/****************************************************************************
+ * Public Data
+ ****************************************************************************/
+
+#undef EXTERN
+#if defined(__cplusplus)
+#define EXTERN extern "C"
+extern "C"
+{
+#else
+#define EXTERN extern
+#endif
+
+/****************************************************************************
+ * Public Function Prototypes
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: stm32l4_qspi_initialize
+ *
+ * Description:
+ *   Initialize the selected QSPI port in master mode
+ *
+ * Input Parameters:
+ *   intf - Interface number(must be zero)
+ *
+ * Returned Value:
+ *   Valid SPI device structure reference on success; a NULL on failure
+ *
+ ****************************************************************************/
+
+struct qspi_dev_s;
+struct qspi_dev_s *stm32_qspi_initialize(int intf);
+
+/****************************************************************************
+ * Name: stm32l4_qspi_enter_memorymapped
+ *
+ * Description:
+ *   Put the QSPI device into memory mapped mode
+ *
+ * Input Parameters:
+ *   dev - QSPI device
+ *   meminfo - parameters like for a memory transfer used for reading
+ *   lpto - number of cycles to wait to automatically de-assert CS
+ *
+ * Returned Value:
+ *   None
+ *
+ ****************************************************************************/
+
+void stm32_qspi_enter_memorymapped(struct qspi_dev_s *dev,
+                                     const struct qspi_meminfo_s *meminfo,
+                                     uint32_t lpto);
+
+/****************************************************************************
+ * Name: stm32l4_qspi_exit_memorymapped
+ *
+ * Description:
+ *   Take the QSPI device out of memory mapped mode
+ *
+ * Input Parameters:
+ *   dev - QSPI device
+ *
+ * Returned Value:
+ *   None
+ *
+ ****************************************************************************/
+
+void stm32_qspi_exit_memorymapped(struct qspi_dev_s *dev);
+
+#undef EXTERN
+#if defined(__cplusplus)
+}
+#endif
+
+#endif /* __ASSEMBLY__ */
+#endif /* CONFIG_STM32H5_QSPI */
+#endif /* __ARCH_ARM_SRC_STM32H5_STM32_QSPI_H */
