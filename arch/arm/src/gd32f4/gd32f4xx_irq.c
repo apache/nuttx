16b63ed837beaaf9cc3de248d9a0e8536d7c3fcf
hujun5 <hujun5@xiaomi.com>
2024-12-06 21:32:44 +0800

    armv7/8m: fix regresion from https://github.com/apache/nuttx/pull/14881
    reason:
    svc call may trigger hardfault
    
    Background
        The origin of this issue is our desire to eliminate the function of storing
    "regs" in g_current_regs and instead utilize (*running_task)->xcp.regs for storage.
    The benefits of this approach include faster storage speed and
    avoiding multiple accesses to g_current_regs during context switching,
    thus ensuring that whether returning from an interrupt or an exception,
    we consistently use this_task()->xcp.regs
    
    Issue Encountered
        However, when storing registers, we must ensure that (running_task)->xcp.regs is invalid
    so that it can be safely overwritten.
    According to the existing logic, the only scenario where (running_task)->xcp.regs
    is valid is during restore_context. We must accurately identify this scenario.
    Initially, we used the condition (running_task)==NULL for this purpose, but we deemed
    this approach unsatisfactory as it did not align well with the actual logic.
    (running_task) should not be NULL. Consequently, we adopted other arch-specific methods for judgment,
    but due to special logic in some arch, the judgment was not accurate, leading to this issue.
    
    Solution:
        For armv6-m, we haven't found a more suitable solution, so we are sticking with (*running_task)==NULL.
        For armv7-m/armv8-m, by removing support for primask, we can achieve accurate judgment.
    
        PRIMASK is a design in armv6-m, that's why arm introduce BASEPRI from armv7-m.
    It's wrong to provide this option for armv7-m/armv8-m arch.
    
    Signed-off-by: hujun5 <hujun5@xiaomi.com>

--- arch/arm/src/gd32f4/gd32f4xx_irq.c
+++ arch/arm/src/gd32f4/gd32f4xx_irq.c
@@ -184,7 +184,6 @@ static int gd32_reserved(int irq, void *context, void *arg)
  *
  ****************************************************************************/
 
-#ifdef CONFIG_ARMV7M_USEBASEPRI
 static inline void gd32_prioritize_syscall(int priority)
 {
   uint32_t regval;
@@ -196,7 +195,6 @@ static inline void gd32_prioritize_syscall(int priority)
   regval |= (priority << NVIC_SYSH_PRIORITY_PR11_SHIFT);
   putreg32(regval, NVIC_SYSH8_11_PRIORITY);
 }
-#endif
 
 /****************************************************************************
  * Name: gd32_irqinfo
@@ -332,9 +330,8 @@ void up_irqinitialize(void)
 #ifdef CONFIG_ARCH_IRQPRIO
   /* up_prioritize_irq(GD32_IRQ_PENDSV, NVIC_SYSH_PRIORITY_MIN); */
 #endif
-#ifdef CONFIG_ARMV7M_USEBASEPRI
+
   gd32_prioritize_syscall(NVIC_SYSH_SVCALL_PRIORITY);
-#endif
 
   /* If the MPU is enabled, then attach and enable the Memory Management
    * Fault handler.
--- arch/arm/src/stm32h7/stm32_irq.c
+++ arch/arm/src/stm32h7/stm32_irq.c
@@ -205,7 +205,6 @@ static int stm32_reserved(int irq, void *context, void *arg)
  *
  ****************************************************************************/
 
-#ifdef CONFIG_ARMV7M_USEBASEPRI
 static inline void stm32_prioritize_syscall(int priority)
 {
   uint32_t regval;
@@ -217,7 +216,6 @@ static inline void stm32_prioritize_syscall(int priority)
   regval |= (priority << NVIC_SYSH_PRIORITY_PR11_SHIFT);
   putreg32(regval, NVIC_SYSH8_11_PRIORITY);
 }
-#endif
 
 /****************************************************************************
  * Name: stm32_irqinfo
@@ -374,9 +372,6 @@ static int stm32_irqinfo(int irq, uintptr_t *regaddr, uint32_t *bit,
 void up_irqinitialize(void)
 {
   uintptr_t regaddr;
-#if defined(CONFIG_DEBUG_SYMBOLS) && !defined(CONFIG_ARMV7M_USEBASEPRI)
-  uint32_t regval;
-#endif
   int nintlines;
   int i;
 
@@ -451,9 +446,8 @@ void up_irqinitialize(void)
 #ifdef CONFIG_ARCH_IRQPRIO
   /* up_prioritize_irq(STM32_IRQ_PENDSV, NVIC_SYSH_PRIORITY_MIN); */
 #endif
-#ifdef CONFIG_ARMV7M_USEBASEPRI
+
   stm32_prioritize_syscall(NVIC_SYSH_SVCALL_PRIORITY);
-#endif
 
   /* If the MPU is enabled, then attach and enable the Memory Management
    * Fault handler.
@@ -480,17 +474,6 @@ void up_irqinitialize(void)
 #endif
 
   stm32_dumpnvic("initial", NR_IRQS);
-
-  /* If a debugger is connected, try to prevent it from catching hardfaults.
-   * If CONFIG_ARMV7M_USEBASEPRI, no hardfaults are expected in normal
-   * operation.
-   */
-
-#if defined(CONFIG_DEBUG_SYMBOLS) && !defined(CONFIG_ARMV7M_USEBASEPRI)
-  regval  = getreg32(NVIC_DEMCR);
-  regval &= ~NVIC_DEMCR_VCHARDERR;
-  putreg32(regval, NVIC_DEMCR);
-#endif
 
 #ifndef CONFIG_SUPPRESS_INTERRUPTS
   /* Initialize logic to support a second level of interrupt decoding for
