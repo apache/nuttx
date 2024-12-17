60636c46660c0baa0311b87fc2281452eed1c687
chao an <anchao@lixiang.com>
2024-12-17 15:40:15 +0800

    drivers/pci: fix typo addres -> address
    fix typo addres -> address
    
    Signed-off-by: chao an <anchao@lixiang.com>

--- arch/arm/src/nrf91/nrf91_modem.h
+++ arch/arm/src/nrf91/nrf91_modem.h
@@ -55,19 +55,19 @@
 #define NRF91_SHMEM_TRACE_SIZE (CONFIG_NRF91_MODEM_SHMEM_TRACE_SIZE)
 
 #if !(NRF91_SHMEM_CTRL_BASE % 4 == 0)
-#  error SHMEM base addres must be word-aligned (4 bytes)
+#  error SHMEM base address must be word-aligned (4 bytes)
 #endif
 
 #if !(NRF91_SHMEM_TX_BASE % 4 == 0)
-#  error SHMEM base addres must be word-aligned (4 bytes)
+#  error SHMEM base address must be word-aligned (4 bytes)
 #endif
 
 #if !(NRF91_SHMEM_RX_BASE % 4 == 0)
-#  error SHMEM base addres must be word-aligned (4 bytes)
+#  error SHMEM base address must be word-aligned (4 bytes)
 #endif
 
 #if !(NRF91_SHMEM_TRACE_BASE % 4 == 0)
-#  error SHMEM base addres must be word-aligned (4 bytes)
+#  error SHMEM base address must be word-aligned (4 bytes)
 #endif
 
 /****************************************************************************
