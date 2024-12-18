5c1becab2435864add7ebfc0eb1d33c1e1ecaa53
Alin Jerpelea <alin.jerpelea@sony.com>
2024-12-18 04:51:02 +0100

    arch/arm: migrate to SPDX identifier
    Most tools used for compliance and SBOM generation use SPDX identifiers
    This change brings us a step closer to an easy SBOM generation.
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- arch/arm/src/lpc43xx/hardware/lpc43_spifi.h
+++ arch/arm/src/lpc43xx/hardware/lpc43_spifi.h
@@ -1,6 +1,8 @@
 /****************************************************************************
  * arch/arm/src/lpc43xx/hardware/lpc43_spifi.h
  *
+ * SPDX-License-Identifier: Apache-2.0
+ *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.  The
@@ -15,31 +17,6 @@
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
  * License for the specific language governing permissions and limitations
  * under the License.
- *
- ****************************************************************************/
-
-/* NOTE:  The SPIFI ROM interface is not defined in the LPC43xx user manual.
- * Some information in this file drivers from the NXP header file
- * spifi_rom_api.h.  I do not believe that any copyright restrictions apply.
- * But just to be certain:
- *
- *   Copyright(C) 2011, NXP Semiconductor
- *   All rights reserved.
- *
- * Software that is described herein is for illustrative purposes only which
- * provides customers with programming information regarding the products.
- * This software is supplied "AS IS" without any warranties. NXP
- * Semiconductors assumes no responsibility or liability for the use of the
- * software, conveys no license or title under any patent, copyright, or
- * mask work right to the product. NXP Semiconductors reserves the right to
- * make changes in the software without notification. NXP Semiconductors
- * also make no representation or warranty that such application will be
- * suitable for the specified use without further testing or modification.
- * Permission to use, copy, modify, and distribute this software and its
- * documentation is hereby granted, under NXP Semiconductors' relevant
- * copyright in the software, without fee, provided that it is used in
- * conjunction with NXP Semiconductors microcontrollers.  This copyright,
- * permission, and disclaimer notice must appear in all copies of this code.
  *
  ****************************************************************************/
 
