0e162eeb7a01f649fef5394b2bb7e382d96dacf4
Alan Carvalho de Assis <acassis@gmail.com>
2024-12-19 13:11:52 -0300

    drivers/sensors: Add Velocity sensor type to UORB
    This patch adds support for velocity measurement sensors.
    I plan to use it as a generic velocity type to be used for
    Renesas FS3000 (Air Velocity Flow).
    
    Signed-off-by: Alan C. Assis <acassis@gmail.com>

--- include/nuttx/uorb.h
+++ include/nuttx/uorb.h
@@ -467,9 +467,18 @@
 
 #define SENSOR_TYPE_GNSS_GEOFENCE                   52
 
-/* The total number of sensor */
+/* Velocity Sensor
+ * A sensor of this type measures the velocity as it is moving.
+ * The default unit velocity is meter by seconds m/s (SI).
+ */
 
-#define SENSOR_TYPE_COUNT                           53
+#define SENSOR_TYPE_VELOCITY                        53
+
+/* The total number of sensor
+ * please increase it if you added a new sensor type!
+ */
+
+#define SENSOR_TYPE_COUNT                           54
 
 /* The additional sensor open flags */
 
@@ -745,6 +754,12 @@ struct sensor_force         /* Type: Force */
   uint64_t timestamp;       /* Unit is microseconds */
   float force;              /* Force value, units is N */
   int32_t event;            /* Force event */
+};
+
+struct sensor_velocity      /* Type: Velocity */
+{
+  uint64_t timestamp;       /* Unit is microseconds */
+  float velocity;           /* Velocity value, units is m/s (SI) */
 };
 
 struct sensor_hall          /* Type: HALL */
