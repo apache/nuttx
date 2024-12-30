9b9869158aceb2e1de486c573e43168b7c582623
Lup Yuen Lee <luppy@appkaki.com>
2024-12-30 21:23:33 +0800

    arm/imxrt: Remove extra lock from imxrt_edma
    An extra `lock` appears in imxrt_edma due to 2 conflicting PRs:
    - https://github.com/apache/nuttx/pull/15352
    - https://github.com/apache/nuttx/pull/15353
    
    This PR removes the extra `lock` in imxrt_edma.

--- arch/arm/src/imxrt/imxrt_edma.c
+++ arch/arm/src/imxrt/imxrt_edma.c
@@ -145,7 +145,6 @@ struct imxrt_edma_s
   /* This array describes each DMA channel */
 
   struct imxrt_dmach_s dmach[IMXRT_EDMA_NCHANNELS];
-  spinlock_t lock;                /* Spinlock */
 };
 
 /****************************************************************************
