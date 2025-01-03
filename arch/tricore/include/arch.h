6274de5508cfcc3c3518fa9f473abc31930658cf
buxiasen <buxiasen@xiaomi.com>
2025-01-03 15:09:57 +0800

    arch/tricore: move tricore_csa2addr from internal.h to arch.h
    Make up_getusrsp possible move to irq as depends on tricore_csa2addr
    
    Signed-off-by: buxiasen <buxiasen@xiaomi.com>

--- arch/tricore/include/arch.h
+++ arch/tricore/include/arch.h
@@ -42,6 +42,13 @@
  * Pre-processor Definitions
  ****************************************************************************/
 
+/* Address <--> Context Save Areas */
+
+#define tricore_csa2addr(csa) ((uintptr_t *)((((csa) & 0x000F0000) << 12) \
+                                             | (((csa) & 0x0000FFFF) << 6)))
+#define tricore_addr2csa(addr) ((uintptr_t)(((((uintptr_t)(addr)) & 0xF0000000) >> 12) \
+                                            | (((uintptr_t)(addr) & 0x003FFFC0) >> 6)))
+
 /****************************************************************************
  * Public Types
  ****************************************************************************/
