aef24f5f3c46cd8d2e899b861e3db78a8e4ce272
zhangshoukui <zhangshoukui@xiaomi.com>
2025-01-07 10:17:58 +0800

    rename lib_pathbuffer to lib_tempbuffer
    Signed-off-by: zhangshoukui <zhangshoukui@xiaomi.com>

--- include/nuttx/lib/lib.h
+++ include/nuttx/lib/lib.h
@@ -120,15 +120,18 @@ FAR struct file_struct *lib_get_stream(int fd);
 
 unsigned long nrand(unsigned long limit);
 
-/* Functions defined in lib_pathbuffer.c ************************************/
+/* Functions defined in lib_tempbuffer.c ************************************/
 
-#ifdef CONFIG_LIBC_PATHBUFFER
-FAR char *lib_get_pathbuffer(void);
-void lib_put_pathbuffer(FAR char *buffer);
+#ifdef CONFIG_LIBC_TEMPBUFFER
+FAR char *lib_get_tempbuffer(size_t nbytes);
+void lib_put_tempbuffer(FAR char *buffer);
 #else
-#  define lib_get_pathbuffer() alloca(PATH_MAX)
-#  define lib_put_pathbuffer(b)
+#  define lib_get_tempbuffer(n) alloca(n)
+#  define lib_put_tempbuffer(b)
 #endif
+
+#define lib_get_pathbuffer() lib_get_tempbuffer(PATH_MAX)
+#define lib_put_pathbuffer(b) lib_put_tempbuffer(b)
 
 /* Functions defined in lib_realpath.c **************************************/
 
