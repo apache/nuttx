bde17e5cf3a5a286669565d70be3def3d18b8051
Huang Qi <huangqi3@xiaomi.com>
2025-01-14 17:52:58 +0800

    arch/risc-v: Decouple ARCH_RV_CPUID_MAP and up_cpu_index()
    Summary:
    - Separated CPU index functionality from CPU ID mapping configuration
    - Moved CPU ID mapping functions to new riscv_cpuidmap.c file
    - Made up_cpu_index() implementation dependent on ARCH_USE_S_MODE
    - Updated build system to handle new file organization
    
    Impact:
    - Improves code organization by separating concerns between basic CPU index
    functionality and advanced CPU ID mapping features
    - Makes CPU index functionality available independently of CPU ID mapping
    - Reduces conditional compilation complexity in header files
    - Better aligns with RISC-V architecture modes (M-mode vs S-mode)
    
    Testing:
    GitHub CI and local testing
    
    Signed-off-by: Huang Qi <huangqi3@xiaomi.com>

--- arch/risc-v/src/common/riscv_cpuidmap.c
+++ arch/risc-v/src/common/riscv_cpuidmap.c
@@ -1,5 +1,5 @@
 /****************************************************************************
- * arch/risc-v/src/common/riscv_cpuindex.c
+ * arch/risc-v/src/common/riscv_cpuidmap.c
  *
  * SPDX-License-Identifier: Apache-2.0
  *
@@ -36,14 +36,51 @@
  ****************************************************************************/
 
 /****************************************************************************
- * Name: up_cpu_index
+ * Name: up_this_cpu
  *
  * Description:
- *   Return the real core number regardless CONFIG_SMP setting
+ *   Return the logical core number. Default implementation is 1:1 mapping,
+ *   i.e. physical=logical.
  *
  ****************************************************************************/
 
-int up_cpu_index(void)
+int up_this_cpu(void)
 {
+  return riscv_hartid_to_cpuid((int)riscv_mhartid());
+}
+
+/****************************************************************************
+ * Name: riscv_hartid_to_cpuid
+ *
+ * Description:
+ *   Convert physical core number to logical core number. Default
+ *   implementation is 1:1 mapping, i.e. physical=logical.
+ *
+ ****************************************************************************/
+
+int weak_function riscv_hartid_to_cpuid(int hart)
+{
+#ifdef CONFIG_SMP
+  return hart - CONFIG_ARCH_RV_HARTID_BASE;
+#else
+  return 0;
+#endif
+}
+
+/****************************************************************************
+ * Name: riscv_cpuid_to_hartid
+ *
+ * Description:
+ *   Convert logical core number to physical core number. Default
+ *   implementation is 1:1 mapping, i.e. physical=logical.
+ *
+ ****************************************************************************/
+
+int weak_function riscv_cpuid_to_hartid(int cpu)
+{
+#ifdef CONFIG_SMP
+  return cpu + CONFIG_ARCH_RV_HARTID_BASE;
+#else
   return (int)riscv_mhartid();
+#endif
 }
