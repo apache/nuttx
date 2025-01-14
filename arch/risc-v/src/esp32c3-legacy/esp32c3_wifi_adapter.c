3ba03c6d795c3c57618aac688e29d7987fef433b
YAMAMOTO Takashi <yamamoto@midokura.com>
2025-01-14 15:48:24 +0900

    esp32c3-legacy: apply wifi deadlock fix from esp32s3
    cf. https://github.com/apache/nuttx/pull/15445

--- arch/risc-v/src/esp32c3-legacy/esp32c3_wifi_adapter.c
+++ arch/risc-v/src/esp32c3-legacy/esp32c3_wifi_adapter.c
@@ -2257,6 +2257,13 @@ static void esp_evt_work_cb(void *arg)
           break;
         }
 
+      /* Some of the following logic (eg. esp32c3_wlan_sta_set_linkstatus)
+       * can take net_lock(). To maintain the consistent locking order,
+       * we take net_lock() here before taking esp_wifi_lock. Note that
+       * net_lock() is a recursive lock.
+       */
+
+      net_lock();
       esp_wifi_lock(true);
 
       switch (evt_adpt->id)
@@ -2397,6 +2404,7 @@ static void esp_evt_work_cb(void *arg)
         }
 
       esp_wifi_lock(false);
+      net_unlock();
 
       kmm_free(evt_adpt);
     }
