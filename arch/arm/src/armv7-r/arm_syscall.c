84a80f8bed7b9fbc5f406d58990cd54516d74e6a
hujun5 <hujun5@xiaomi.com>
2025-01-14 19:12:28 +0800

    armv7a/r: fix regression from https://github.com/apache/nuttx/pull/14596
    reason:
    In kernel mode, sighand_test() may failed.
    because nxsig_unmask_pendingsignal may change tcb->xcp.regs,and we should update return value(regs).
    
    Signed-off-by: hujun5 <hujun5@xiaomi.com>

--- arch/arm/src/armv7-r/arm_syscall.c
+++ arch/arm/src/armv7-r/arm_syscall.c
@@ -258,6 +258,7 @@ uint32_t *arm_syscall(uint32_t *regs)
 
           rtcb->flags          &= ~TCB_FLAG_SYSCALL;
           nxsig_unmask_pendingsignal();
+          regs                  = tcb->xcp.regs;
         }
         break;
 #endif
