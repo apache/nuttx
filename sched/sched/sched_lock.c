914ae532e68bf4ca75c758d6f541a1d2fcdce8c3
hujun5 <hujun5@xiaomi.com>
2025-01-19 15:52:00 +0800

    sched: remove csection and reduce the interrupt disabling time in sched_[un]lock
    reason:
    1 Accelerated the implementation of sched_lock, remove enter_critical_section in sched_lock and
    only enter_critical_section when task scheduling is required.
    2 we add sched_lock_wo_note/sched_unlock_wo_note and it does not perform instrumentation logic
    
    Signed-off-by: hujun5 <hujun5@xiaomi.com>

--- sched/sched/sched_lock.c
+++ sched/sched/sched_lock.c
@@ -64,112 +64,36 @@
  *
  ****************************************************************************/
 
-#ifdef CONFIG_SMP
-
-int sched_lock(void)
+void sched_lock(void)
 {
-  FAR struct tcb_s *rtcb;
+  /* sched_lock() should have no effect if called from the interrupt level. */
 
-  /* If the CPU supports suppression of interprocessor interrupts, then
-   * simple disabling interrupts will provide sufficient protection for
-   * the following operation.
-   */
-
-  rtcb = this_task();
-
-  /* Check for some special cases:  (1) rtcb may be NULL only during early
-   * boot-up phases, and (2) sched_lock() should have no effect if called
-   * from the interrupt level.
-   */
-
-  if (rtcb != NULL && !up_interrupt_context())
+  if (!up_interrupt_context())
     {
-      irqstate_t flags;
+      FAR struct tcb_s *rtcb = this_task();
 
       /* Catch attempts to increment the lockcount beyond the range of the
        * integer type.
        */
 
-      DEBUGASSERT(rtcb->lockcount < MAX_LOCK_COUNT);
+      DEBUGASSERT(rtcb == NULL || rtcb->lockcount < MAX_LOCK_COUNT);
 
-      flags = enter_critical_section();
-
-      /* A counter is used to support locking.  This allows nested lock
-       * operations on this thread
-       */
-
-      rtcb->lockcount++;
-
-      /* Check if we just acquired the lock */
-
-      if (rtcb->lockcount == 1)
-        {
-          /* Note that we have pre-emption locked */
-
-#if CONFIG_SCHED_CRITMONITOR_MAXTIME_PREEMPTION >= 0
-          nxsched_critmon_preemption(rtcb, true, return_address(0));
-#endif
-#ifdef CONFIG_SCHED_INSTRUMENTATION_PREEMPTION
-          sched_note_preemption(rtcb, true);
-#endif
-        }
-
-      /* Move any tasks in the ready-to-run list to the pending task list
-       * where they will not be available to run until the scheduler is
-       * unlocked and nxsched_merge_pending() is called.
-       */
-
-      nxsched_merge_prioritized(list_readytorun(),
-                                list_pendingtasks(),
-                                TSTATE_TASK_PENDING);
-
-      leave_critical_section(flags);
-    }
-
-  return OK;
-}
-
-#else /* CONFIG_SMP */
-
-int sched_lock(void)
-{
-  FAR struct tcb_s *rtcb = this_task();
-
-  /* Check for some special cases:  (1) rtcb may be NULL only during early
-   * boot-up phases, and (2) sched_lock() should have no effect if called
-   * from the interrupt level.
-   */
-
-  if (rtcb != NULL && !up_interrupt_context())
-    {
-      /* Catch attempts to increment the lockcount beyond the range of the
-       * integer type.
-       */
-
-      DEBUGASSERT(rtcb->lockcount < MAX_LOCK_COUNT);
-
-      /* A counter is used to support locking.  This allows nested lock
+      /* A counter is used to support locking. This allows nested lock
        * operations on this thread (on any CPU)
        */
 
-      rtcb->lockcount++;
-
-      /* Check if we just acquired the lock */
-
-      if (rtcb->lockcount == 1)
+      if (rtcb != NULL && rtcb->lockcount++ == 0)
         {
+#if (CONFIG_SCHED_CRITMONITOR_MAXTIME_PREEMPTION >= 0) || \
+    defined(CONFIG_SCHED_INSTRUMENTATION_PREEMPTION)
+          irqstate_t flags = enter_critical_section_wo_note();
+
           /* Note that we have pre-emption locked */
 
-#if CONFIG_SCHED_CRITMONITOR_MAXTIME_PREEMPTION >= 0
           nxsched_critmon_preemption(rtcb, true, return_address(0));
-#endif
-#ifdef CONFIG_SCHED_INSTRUMENTATION_PREEMPTION
           sched_note_preemption(rtcb, true);
+          leave_critical_section_wo_note(flags);
 #endif
         }
     }
-
-  return OK;
 }
-
-#endif /* CONFIG_SMP */
