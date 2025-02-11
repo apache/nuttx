586f9f3e7b925d41b01ef4c4242e6b8d807900b7
Alan Carvalho de Assis <acassis@gmail.com>
2025-02-11 11:10:08 -0300

    nucleo-l432kc: Add support to bringup
    Many STM32L4 boards are missing stm32_bringup.c. That confusion
    was created when stm32_appinit.c was created. It introduced a new
    way to do the board initialization without depending on NSH arch
    specific initialization.
    
    Signed-off-by: Alan C. Assis <acassis@gmail.com>

--- boards/arm/stm32l4/nucleo-l432kc/src/stm32_uid.c
+++ boards/arm/stm32l4/nucleo-l432kc/src/stm32_uid.c
@@ -1,5 +1,5 @@
 /****************************************************************************
- * boards/arm/stm32l4/nucleo-l432kc/src/stm32_appinit.c
+ * boards/arm/stm32l4/nucleo-l432kc/src/stm32_uid.c
  *
  * SPDX-License-Identifier: Apache-2.0
  *
@@ -27,8 +27,18 @@
 #include <nuttx/config.h>
 
 #include <sys/types.h>
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <debug.h>
+#include <errno.h>
+
+#include <nuttx/arch.h>
 #include <nuttx/board.h>
 
+#include <arch/board/board.h>
+
+#include "stm32l4_uid.h"
 #include "nucleo-l432kc.h"
 
 /****************************************************************************
@@ -39,38 +49,13 @@
  * Public Functions
  ****************************************************************************/
 
-/****************************************************************************
- * Name: board_app_initialize
- *
- * Description:
- *   Perform application specific initialization.  This function is never
- *   called directly from application code, but only indirectly via the
- *   (non-standard) boardctl() interface using the command BOARDIOC_INIT.
- *
- * Input Parameters:
- *   arg - The boardctl() argument is passed to the board_app_initialize()
- *         implementation without modification.  The argument has no
- *         meaning to NuttX; the meaning of the argument is a contract
- *         between the board-specific initialization logic and the
- *         matching application logic.  The value could be such things as a
- *         mode enumeration value, a set of DIP switch switch settings, a
- *         pointer to configuration data read from a file or serial FLASH,
- *         or whatever you would like to do with it.  Every implementation
- *         should accept zero/NULL as a default configuration.
- *
- * Returned Value:
- *   Zero (OK) is returned on success; a negated errno value is returned on
- *   any failure to indicate the nature of the failure.
- *
- ****************************************************************************/
-
-int board_app_initialize(uintptr_t arg)
+int board_uniqueid(uint8_t *uniqueid)
 {
-  /* Did we already initialize via board_late_initialize()? */
+  if (uniqueid == NULL)
+    {
+      return -EINVAL;
+    }
 
-#ifndef CONFIG_BOARD_LATE_INITIALIZE
-  return stm32_bringup();
-#else
+  stm32l4_get_uniqueid(uniqueid);
   return OK;
-#endif
 }
