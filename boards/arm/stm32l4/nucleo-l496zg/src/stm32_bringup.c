e2a85dd2db38a4fdbcfca9b502f3d405ed7a37e7
Alan Carvalho de Assis <acassis@gmail.com>
2025-02-11 12:17:41 -0300

    nucleo-l496zg: Add support to bringup
    Many STM32L4 boards are missing stm32_bringup.c. That confusion
    was created when stm32_appinit.c was created. It introduced a new
    way to do the board initialization without depending on NSH arch
    specific initialization.
    
    Signed-off-by: Alan C. Assis <acassis@gmail.com>

---
+++ boards/arm/stm32l4/nucleo-l496zg/src/stm32_bringup.c
@@ -0,0 +1,197 @@
+/****************************************************************************
+ * boards/arm/stm32l4/nucleo-l496zg/src/stm32_bringup.c
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+#include <sys/types.h>
+#include <debug.h>
+#include <syslog.h>
+
+#include "nucleo-144.h"
+#include <nuttx/fs/fs.h>
+#include <nuttx/leds/userled.h>
+
+#include "stm32l4_i2c.h"
+
+/****************************************************************************
+ * Private Data
+ ****************************************************************************/
+
+#if defined(CONFIG_STM32L4_I2C1)
+struct i2c_master_s *i2c1;
+#endif
+#if defined(CONFIG_STM32L4_I2C2)
+struct i2c_master_s *i2c2;
+#endif
+#if defined(CONFIG_STM32L4_I2C3)
+struct i2c_master_s *i2c3;
+#endif
+#if defined(CONFIG_STM32L4_I2C4)
+struct i2c_master_s *i2c4;
+#endif
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: stm32_bringup
+ *
+ * Description:
+ *   Perform architecture-specific initialization
+ *
+ *   CONFIG_BOARD_LATE_INITIALIZE=y :
+ *     Called from board_late_initialize().
+ *
+ *   CONFIG_BOARD_LATE_INITIALIZE=n && CONFIG_BOARDCTL=y :
+ *     Called from the NSH library
+ *
+ ****************************************************************************/
+
+int stm32_bringup(void)
+{
+  int ret;
+
+#ifdef CONFIG_FS_PROCFS
+  /* Mount the procfs file system */
+
+  ret = nx_mount(NULL, STM32_PROCFS_MOUNTPOINT, "procfs", 0, NULL);
+  if (ret < 0)
+    {
+      syslog(LOG_ERR, "ERROR: Failed to mount procfs at %s: %d\n",
+             STM32_PROCFS_MOUNTPOINT, ret);
+    }
+#endif
+
+#if !defined(CONFIG_ARCH_LEDS) && defined(CONFIG_USERLED_LOWER)
+  /* Register the LED driver */
+
+  ret = userled_lower_initialize(LED_DRIVER_PATH);
+  if (ret < 0)
+    {
+      syslog(LOG_ERR, "ERROR: userled_lower_initialize() failed: %d\n", ret);
+    }
+#endif
+
+#ifdef CONFIG_ADC
+  /* Initialize ADC and register the ADC driver. */
+
+  ret = stm32_adc_setup();
+  if (ret < 0)
+    {
+      syslog(LOG_ERR, "ERROR: stm32_adc_setup failed: %d\n", ret);
+    }
+
+#ifdef CONFIG_STM32L4_DFSDM
+  /* Initialize DFSDM and register its filters as additional ADC devices. */
+
+  ret = stm32_dfsdm_setup();
+  if (ret < 0)
+    {
+      syslog(LOG_ERR, "ERROR: stm32_dfsdm_setup failed: %d\n", ret);
+    }
+
+#endif
+#endif /* CONFIG_ADC */
+
+#ifdef CONFIG_DAC
+  /* Initialize DAC and register the DAC driver. */
+
+  ret = stm32_dac_setup();
+  if (ret < 0)
+    {
+      syslog(LOG_ERR, "ERROR: stm32_dac_setup failed: %d\n", ret);
+    }
+#endif
+
+#if defined(CONFIG_FAT_DMAMEMORY)
+  if (stm32_dma_alloc_init() < 0)
+    {
+      syslog(LOG_ERR, "DMA alloc FAILED");
+    }
+#endif
+
+#if defined(CONFIG_NUCLEO_SPI_TEST)
+  /* Create SPI interfaces */
+
+  ret = stm32_spidev_bus_test();
+  if (ret != OK)
+    {
+      syslog(LOG_ERR, "ERROR: Failed to initialize SPI interfaces: %d\n",
+             ret);
+      return ret;
+    }
+#endif
+
+#if defined(CONFIG_MMCSD)
+  /* Configure SDIO */
+
+  /* Initialize the SDIO block driver */
+
+  ret = stm32l4_sdio_initialize();
+  if (ret != OK)
+    {
+      ferr("ERROR: Failed to initialize MMC/SD driver: %d\n", ret);
+      return ret;
+    }
+#endif
+
+#if defined(CONFIG_I2C)
+  /* Configure I2C */
+
+  /* REVISIT: this is ugly! */
+
+#if defined(CONFIG_STM32L4_I2C1)
+  i2c1 = stm32l4_i2cbus_initialize(1);
+#endif
+#if defined(CONFIG_STM32L4_I2C2)
+  i2c2 = stm32l4_i2cbus_initialize(2);
+#endif
+#if defined(CONFIG_STM32L4_I2C3)
+  i2c3 = stm32l4_i2cbus_initialize(3);
+#endif
+#if defined(CONFIG_STM32L4_I2C4)
+  i2c4 = stm32l4_i2cbus_initialize(4);
+#endif
+#ifdef CONFIG_I2C_DRIVER
+#if defined(CONFIG_STM32L4_I2C1)
+  i2c_register(i2c1, 1);
+#endif
+#if defined(CONFIG_STM32L4_I2C2)
+  i2c_register(i2c2, 2);
+#endif
+#if defined(CONFIG_STM32L4_I2C3)
+  i2c_register(i2c3, 3);
+#endif
+#if defined(CONFIG_STM32L4_I2C4)
+  i2c_register(i2c4, 4);
+#endif
+#endif
+#endif /* CONFIG_I2C */
+
+  UNUSED(ret);
+  return OK;
+}
