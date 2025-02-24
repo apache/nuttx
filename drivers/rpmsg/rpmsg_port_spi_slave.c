fa947f95ee4d1f7affdc618e0c5bcd7e70aa7724
Xiang Xiao <xiaoxiang@xiaomi.com>
2025-02-24 14:58:07 +0800

    drivers/rpmsg_port_spi/slave: add RPMSG_PORT_SPI_ prefix to BYTES2WORDS
    Code format optimization
    
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- drivers/rpmsg/rpmsg_port_spi_slave.c
+++ drivers/rpmsg/rpmsg_port_spi_slave.c
@@ -45,15 +45,14 @@
  ****************************************************************************/
 
 #ifdef CONFIG_RPMSG_PORT_SPI_CRC
-#  define rpmsg_port_spi_crc16(hdr) crc16ibm((FAR uint8_t *)&(hdr)->cmd, \
-                                             (hdr)->len - sizeof((hdr)->crc))
+#  define rpmsg_port_spi_crc16(hdr)     crc16ibm((FAR uint8_t *)&(hdr)->cmd, \
+                                                 (hdr)->len - sizeof((hdr)->crc))
 #else
-#  define rpmsg_port_spi_crc16(hdr) 0
+#  define rpmsg_port_spi_crc16(hdr)     0
 #endif
 
-#define RPMSG_PORT_SPI_CMD_TIMEOUT  1000
-
-#define BYTES2WORDS(s,b)            ((b) / ((s)->nbits >> 3))
+#define RPMSG_PORT_SPI_CMD_TIMEOUT      1000
+#define RPMSG_PORT_SPI_BYTES2WORDS(s,b) ((b) / ((s)->nbits >> 3))
 
 /****************************************************************************
  * Private Types
@@ -312,7 +311,7 @@ static void rpmsg_port_spi_exchange(FAR struct rpmsg_port_spi_s *rpspi)
 
   rpmsg_port_spi_pm_action(rpspi, true);
   SPIS_CTRLR_ENQUEUE(rpspi->spictrlr, txhdr,
-                     BYTES2WORDS(rpspi, rpspi->cmdhdr->len));
+                     RPMSG_PORT_SPI_BYTES2WORDS(rpspi, rpspi->cmdhdr->len));
   IOEXP_WRITEPIN(rpspi->ioe, rpspi->sreq, 1);
 
   rpspi->rxavail = txhdr->avail;
@@ -400,7 +399,7 @@ static size_t rpmsg_port_spi_slave_getdata(FAR struct spi_slave_dev_s *dev,
     container_of(dev, struct rpmsg_port_spi_s, spislv);
 
   *data = rpspi->rxhdr;
-  return BYTES2WORDS(rpspi, rpspi->cmdhdr->len);
+  return RPMSG_PORT_SPI_BYTES2WORDS(rpspi, rpspi->cmdhdr->len);
 }
 
 /****************************************************************************
