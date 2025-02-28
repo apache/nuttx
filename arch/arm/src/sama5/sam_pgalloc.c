a98c8451ac568c2afca0273d70e0c2d457abf64f
buxiasen <buxiasen@xiaomi.com>
2025-02-28 12:55:03 +0800

    sama5/memory: align va&pa method to up_addrenv_x
    Align prototype and make more easy to understand.
    
    Signed-off-by: buxiasen <buxiasen@xiaomi.com>

--- arch/arm/src/sama5/sam_pgalloc.c
+++ arch/arm/src/sama5/sam_pgalloc.c
@@ -37,8 +37,6 @@
 #include "chip.h"
 #include "mmu.h"
 
-#include "sam_pgalloc.h"
-
 #ifdef CONFIG_MM_PGALLOC
 
 /****************************************************************************
@@ -101,40 +99,5 @@ void up_allocate_pgheap(void **heap_start, size_t *heap_size)
                              CONFIG_SAMA5_DDRCS_PGHEAP_OFFSET);
   *heap_size  = CONFIG_SAMA5_DDRCS_PGHEAP_SIZE;
 }
-
-/****************************************************************************
- * Name: sam_virtpgaddr
- *
- * Description:
- *   Check if the physical address lies in the page pool and, if so
- *   get the mapping to the virtual address in the user data area.
- *
- ****************************************************************************/
-
-#ifndef CONFIG_ARCH_PGPOOL_MAPPING
-uintptr_t sam_virtpgaddr(uintptr_t paddr)
-{
-  uintptr_t poolstart;
-  uintptr_t poolend;
-
-  /* REVISIT: Not implemented correctly.  The reverse lookup from physical
-   * to virtual.  This will return a kernel accessible virtual address, but
-   * not an address usable by the user code.
-   *
-   * The correct solutions is complex and, perhaps, will never be needed.
-   */
-
-  poolstart = ((uintptr_t)SAM_DDRCS_PSECTION +
-                CONFIG_SAMA5_DDRCS_PGHEAP_OFFSET);
-  poolend   = poolstart + CONFIG_SAMA5_DDRCS_PGHEAP_SIZE;
-
-  if (paddr >= poolstart && paddr < poolend)
-    {
-      return paddr - SAM_DDRCS_PSECTION + SAM_DDRCS_VSECTION;
-    }
-
-  return 0;
-}
-#endif /* !CONFIG_ARCH_PGPOOL_MAPPING */
 
 #endif /* CONFIG_MM_PGALLOC */
