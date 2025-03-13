9d80d6bb4f613cd4f9a1d2add513f1426edaf945
Lup Yuen Lee <luppy@appkaki.com>
2025-03-13 10:08:45 +0800

    arch/arm64/a527: Add support for Allwinner A527 SoC
    This PR adds support for the Allwinner A527 Arm64 SoC.
    This will be used by the upcoming port of NuttX for
    PINE64 Yuzuki Avaota-A1 SBC.
    
    Most of the code was derived from NuttX for
    QEMU Arm64 Kernel Build qemu-armv8a:knsh.
    The modified code is explained here:
    https://lupyuen.github.io/articles/avaota#appendix-port-nuttx-to-avaota-a1
    
    Modified Files in arch/arm64:
    
    Kconfig: Added ARCH_CHIP_SUNXI for Allwinner 64-bit SoCs.
    Added ARCH_CHIP_SUNXI_A527 for A527 SoC.
    
    New Files in arch/arm64:
    
    include/a527/chip.h: A527 Definitions
    include/a527/irq.h: External Interrupts
    src/a527/chip.h: Memory Map
    src/a527/a527_boot.c, a527_boot.h: Startup Code
    src/a527/a527_initialize.c: Power Management
    src/a527/a527_lowputc.S: UART Output
    src/a527/a527_serial.c: Serial Driver
    src/a527/a527_textheap.c: Text Heap
    src/a527/a527_timer.c: A527 Timer
    src/a527/Kconfig: A527 Config
    src/a527/Make.defs, CMakeLists.txt: Makefiles
    
    Signed-off-by: Lup Yuen Lee <luppy@appkaki.com>

---
+++ arch/arm64/include/a527/irq.h
@@ -0,0 +1,42 @@
+/****************************************************************************
+ * arch/arm64/include/a527/irq.h
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+/* This file should never be included directly but, rather,
+ * only indirectly through nuttx/irq.h
+ */
+
+#ifndef __ARCH_ARM64_INCLUDE_A527_IRQ_H
+#define __ARCH_ARM64_INCLUDE_A527_IRQ_H
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+/* Total number of interrupts */
+
+#define NR_IRQS            220
+
+/* Map CPU MPID to Core ID */
+
+#define MPID_TO_CORE(mpid) (((mpid) >> MPIDR_AFF0_SHIFT) & MPIDR_AFFLVL_MASK)
+
+#endif /* __ARCH_ARM64_INCLUDE_A527_IRQ_H */
