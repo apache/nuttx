9e0852c6a4ee44c8d1fa2e9c8121257cdcf24533
gaohedong <gaohedong@xiaomi.com>
2025-03-15 22:24:05 +0800

    net/pkt: delete the read-back mechanism in pkt stack
    When app send a pkt-packet, then it will read back it and case some problem. Connection should not read back the message sent by itself.
    
    Signed-off-by: gaohedong <gaohedong@xiaomi.com>

--- net/pkt/pkt.h
+++ net/pkt/pkt.h
@@ -74,6 +74,8 @@ struct pkt_conn_s
    */
 
   struct iob_queue_s readahead;   /* Read-ahead buffering */
+
+  FAR struct iob_s  *pendiob;     /* The iob currently being sent */
 };
 
 /****************************************************************************
