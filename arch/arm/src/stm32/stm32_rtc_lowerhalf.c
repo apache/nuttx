9e07b4a924be9e68b4ad79e4258c92fe783c40d4
lpxiao <lp.xiao@hotmail.com>
2025-03-18 10:08:44 +0800

    arch/arm/stm32: fix stm32f1xx alarm support Summary: stm32: support querying a previously set RTC alarm In the STM32F1xx series
    Impact:
    When using the stm32f1xx series MCU, the alarm function can be used, but when we enabled the CONFIG_RTC_ALARM configuration, an error was reported due to the inability to find the specific implementation of rdalarm.
    
    Testing:
    In our own projects, we have done alarm verification using the STM32F103RE..

--- arch/arm/src/stm32/stm32_rtc_lowerhalf.c
+++ arch/arm/src/stm32/stm32_rtc_lowerhalf.c
@@ -745,9 +745,16 @@ static int stm32_rdalarm(struct rtc_lowerhalf_s *lower,
   irqstate_t flags;
 
   DEBUGASSERT(lower != NULL && alarminfo != NULL && alarminfo->time != NULL);
+#if defined(CONFIG_STM32_STM32F4XXX) || defined(CONFIG_STM32_STM32L15XX)
   DEBUGASSERT(alarminfo->id == RTC_ALARMA || alarminfo->id == RTC_ALARMB);
 
   if (alarminfo->id == RTC_ALARMA || alarminfo->id == RTC_ALARMB)
+#else
+  DEBUGASSERT(alarminfo->id >= 0 && alarminfo->id < CONFIG_RTC_NALARMS);
+
+  if (alarminfo != NULL && alarminfo->id >= 0 &&
+      alarminfo->id < CONFIG_RTC_NALARMS)
+#endif
     {
       /* Disable pre-emption while we do this so that we don't have to worry
        * about being suspended and working on an old time.
