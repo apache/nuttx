3dd303889309699b32a1420afa142fbc48905a17
Laczen JMS <laczenjms@gmail.com>
2025-03-28 14:00:00 +0100

    boards/extensa/esp32: use wlan config to start wlan
    Use wlan configuration option to enable wlan support for:
    - esp32-2432S028
    - esp32-audio-kit
    - esp32-devkitc
    - esp32-ethernet-kit
    - esp32-lyrat
    - esp32-pico-kit
    - esp32-sparrow-kit
    - esp32-wrover-kit
    - lilygo_tbeam_lora_gps
    - ttgo_eink5_v2
    - ttgo_lora_esp32
    - ttgo_t_display_esp32
    - esp32s2-saola-1
    - esp32s3-box
    - esp32s3-devkit
    - esp32s3-eye
    - esp32s3-korvo-2
    - esp32s3-lcd-ev
    - lckfb-szpi-esp32s3
    
    Signed-off-by: Laczen JMS <laczenjms@gmail.com>

--- boards/xtensa/esp32/esp32-pico-kit/src/esp32_bringup.c
+++ boards/xtensa/esp32/esp32-pico-kit/src/esp32_bringup.c
@@ -67,7 +67,7 @@
 #  include "esp32_ble.h"
 #endif
 
-#ifdef CONFIG_ESPRESSIF_WIFI
+#ifdef CONFIG_ESPRESSIF_WLAN
 #  include "esp32_board_wlan.h"
 #endif
 
@@ -245,11 +245,11 @@ int esp32_bringup(void)
     }
 #endif
 
-#ifdef CONFIG_ESPRESSIF_WIFI
+#ifdef CONFIG_ESPRESSIF_WLAN
   ret = board_wlan_init();
   if (ret < 0)
     {
-      syslog(LOG_ERR, "ERROR: Failed to initialize wireless subsystem=%d\n",
+      syslog(LOG_ERR, "ERROR: Failed to initialize wlan subsystem=%d\n",
              ret);
     }
 #endif
