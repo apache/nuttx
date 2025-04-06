921ffc731979726c26d75397111ec6e86a7cdcee
chao an <anchao.archer@bytedance.com>
2025-04-06 22:21:03 +0800

    libs/libc/crc16: move crc16 table to rodata
    move crc16 table from data to rodata
    
    Signed-off-by: chao an <anchao.archer@bytedance.com>

--- libs/libc/misc/lib_crc16.c
+++ libs/libc/misc/lib_crc16.c
@@ -49,7 +49,7 @@
 
 /* crc16_tab calculated by Mark G. Mendel, Network Systems Corporation */
 
-static uint16_t crc16_tab[256] =
+static const uint16_t crc16_tab[256] =
 {
   0x0000,  0x1021,  0x2042,  0x3063,  0x4084,  0x50a5,  0x60c6,  0x70e7,
   0x8108,  0x9129,  0xa14a,  0xb16b,  0xc18c,  0xd1ad,  0xe1ce,  0xf1ef,
