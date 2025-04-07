59da455fea95ec1de2a8ad7695fb75db8e0e78fd
chao an <anchao.archer@bytedance.com>
2025-04-07 12:26:49 +0800

    libs/libc/crc16: Separate implementation of crc16xmodem from crc16
    keep default crc16 catalogue for CRC-16/XMODEM
    
    Mapping crc16 implement to crc16xmodem
    
    crc16     -> crc16xmodem
    crc16part -> crc16xmodempart
    
    - CRC-16/ACORN, CRC-16/LTE, CRC-16/V-41-MSB, XMODEM, ZMODEM
    poly: 0x1021 initial seed: 0x0000, xor output: 0x0000
    
    : width=16
    : poly=0x1021
    : init=0x0000
    : refin=false
    : refout=false
    : xorout=0x0000
    : check=0x31c3
    : residue=0x0000
    : name="CRC-16/XMODEM"
    
    https://reveng.sourceforge.io/crc-catalogue/16.htm#crc.cat.crc-16-xmodem
    
    Signed-off-by: chao an <anchao.archer@bytedance.com>

--- include/nuttx/crc16.h
+++ include/nuttx/crc16.h
@@ -56,6 +56,9 @@ extern "C"
  * Description:
  *   Continue CRC calculation on a part of the buffer.
  *
+ *   The default polynomial is 0x1021 (x^16 + x^12 + x^5 + 1)
+ *   See crc16xmodempart()
+ *
  ****************************************************************************/
 
 uint16_t crc16part(FAR const uint8_t *src, size_t len, uint16_t crc16val);
@@ -65,6 +68,9 @@ uint16_t crc16part(FAR const uint8_t *src, size_t len, uint16_t crc16val);
  *
  * Description:
  *   Return a 16-bit CRC of the contents of the 'src' buffer, length 'len'
+ *
+ *   The default polynomial is 0x1021 (x^16 + x^12 + x^5 + 1)
+ *   See crc16xmodem()
  *
  ****************************************************************************/
 
@@ -157,6 +163,47 @@ uint16_t crc16ibmpart(FAR const uint8_t *src, size_t len,
  ****************************************************************************/
 
 uint16_t crc16ibm(FAR const uint8_t *src, size_t len);
+
+/****************************************************************************
+ * Name: crc16xmodempart
+ *
+ * Description:
+ *   Continue CRC calculation on a part of the buffer.
+ *
+ *   Return a 16-bit CRC-XMODEM of the contents of the 'src' buffer, length
+ *   'len', using the polynomial 0x1021 (x^16 + x^12 + x^5 + 1).
+ *
+ *   The following checksums can, among others, be calculated by this
+ *   function, depending on the value provided for the initial seed and
+ *   the value the final calculated CRC is XORed with:
+ *
+ *   - Alias: CRC-16/ACORN, CRC-16/LTE, CRC-16/V-41-MSB, XMODEM, ZMODEM
+ *   https://reveng.sourceforge.io/crc-catalogue/16.htm#crc.cat.crc-16-xmodem
+ *   poly: 0x1021 initial seed: 0x0000, xor output: 0x0000
+ *
+ ****************************************************************************/
+
+uint16_t crc16xmodempart(FAR const uint8_t *src, size_t len,
+                         uint16_t crc16val);
+
+/****************************************************************************
+ * Name: crc16xmodem
+ *
+ * Description:
+ *   Return a 16-bit CRC-XMODEM of the contents of the 'src' buffer, length
+ *   'len', using the polynomial 0x1021 (x^16 + x^12 + x^5 + 1).
+ *
+ *   The following checksums can, among others, be calculated by this
+ *   function, depending on the value provided for the initial seed and
+ *   the value the final calculated CRC is XORed with:
+ *
+ *   - Alias: CRC-16/ACORN, CRC-16/LTE, CRC-16/V-41-MSB, XMODEM, ZMODEM
+ *   https://reveng.sourceforge.io/crc-catalogue/16.htm#crc.cat.crc-16-xmodem
+ *   poly: 0x1021 initial seed: 0x0000, xor output: 0x0000
+ *
+ ****************************************************************************/
+
+uint16_t crc16xmodem(FAR const uint8_t *src, size_t len);
 
 #undef EXTERN
 #ifdef __cplusplus
