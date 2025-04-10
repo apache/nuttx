d3a4c7e667cf7b63806a9948cabac81b4317c211
Jukka Laitinen <jukka.laitinen@tii.ae>
2025-04-10 13:41:46 +0300

    Enable mutex fast path for sem_wait and nxsem_wait also when priority inheritance is enabled
    This enables the mutex fast path for nxsem_wait, nxsem_trywait and nxsem_post also when
    the priority inheritance is enabled.

--- libs/libc/semaphore/sem_post.c
+++ libs/libc/semaphore/sem_post.c
@@ -129,8 +129,8 @@ int nxsem_post(FAR sem_t *sem)
 #ifndef CONFIG_LIBC_ARCH_ATOMIC
 
   if ((sem->flags & SEM_TYPE_MUTEX)
-#  if defined(CONFIG_PRIORITY_PROTECT) || defined(CONFIG_PRIORITY_INHERITANCE)
-      && (sem->flags & SEM_PRIO_MASK) == SEM_PRIO_NONE
+#  if defined(CONFIG_PRIORITY_PROTECT)
+      && (sem->flags & SEM_PRIO_MASK) != SEM_PRIO_PROTECT
 #  endif
       )
     {
