apache/nuttx

commit 31605b6335a8411833b410bf89ef10bd4fe502b0

bluetooth: call bt_driver_register common interface
Signed-off-by: chengkai <chengkai@xiaomi.com>

arch/arm/src/nrf53/nrf53_sdc.c
@@ -38,9 +38,6 @@
 #include <arch/nrf53/nrf5340_irq_cpunet.h>
 #include <nuttx/wqueue.h>
 
-#if defined(CONFIG_UART_BTH4)
-#  include <nuttx/serial/uart_bth4.h>
-#endif
 #ifdef CONFIG_BLUETOOTH_RPMSG_SERVER
 #  include <nuttx/wireless/bluetooth/bt_rpmsghci.h>
 #endif
@@ -910,28 +907,13 @@ int nrf53_sdc_initialize(void)
       return ret;
     }
 
-#ifdef CONFIG_UART_BTH4
-  /* Register UART BT H4 device */
-
-  ret = uart_bth4_register(CONFIG_NRF53_BLE_TTY_NAME, &g_bt_driver);
+#ifdef CONFIG_DRIVERS_BLUETOOTH
+  ret = bt_driver_register(&g_bt_driver);
   if (ret < 0)
     {
-      wlerr("bt_bth4_register error: %d\n", ret);
+      wlerr("bt_driver_register error: %d\n", ret);
       return ret;
     }
-#elif defined(CONFIG_NET_BLUETOOTH)
-  /* Register network device */
-
-  ret = bt_netdev_register(&g_bt_driver);
-  if (ret < 0)
-    {
-      wlerr("bt_netdev_register error: %d\n", ret);
-      return ret;
-    }
-#elif defined(CONFIG_BLUETOOTH_RPMSG_SERVER)
-  /* Do nothing here */
-#else
-#  error
 #endif
 
   return ret;

arch/arm/src/nrf52/Kconfig
@@ -899,11 +899,6 @@ config NRF52_SDC_DLE
 	bool "Support Data Length Extension (DLE)"
 	default y
 
-config NRF52_BLE_TTY_NAME
-	string "BLE TTY device name"
-	default "/dev/ttyHCI0"
-	depends on UART_BTH4
-
 config NRF52_SDC_FICR_STATIC_ADDR
 	bool "Configure factory generated static random address"
 	default n

arch/arm/src/nrf52/nrf52_sdc.c
@@ -38,10 +38,6 @@
 #include <arch/nrf52/nrf52_irq.h>
 #include <nuttx/wqueue.h>
 
-#if defined(CONFIG_UART_BTH4)
-#  include <nuttx/serial/uart_bth4.h>
-#endif
-
 #include "arm_internal.h"
 #include "ram_vectors.h"
 
@@ -918,26 +914,13 @@ int nrf52_sdc_initialize(void)
       return ret;
     }
 
-#ifdef CONFIG_UART_BTH4
-  /* Register UART BT H4 device */
-
-  ret = uart_bth4_register(CONFIG_NRF52_BLE_TTY_NAME, &g_bt_driver);
+#ifdef CONFIG_DRIVERS_BLUETOOTH
+  ret = bt_driver_register(&g_bt_driver);
   if (ret < 0)
     {
-      wlerr("bt_bth4_register error: %d\n", ret);
+      wlerr("bt_driver_register error: %d\n", ret);
       return ret;
     }
-#elif defined(CONFIG_NET_BLUETOOTH)
-  /* Register network device */
-
-  ret = bt_netdev_register(&g_bt_driver);
-  if (ret < 0)
-    {
-      wlerr("bt_netdev_register error: %d\n", ret);
-      return ret;
-    }
-#else
-#  error
 #endif
 
   return ret;

arch/arm/src/nrf53/Kconfig
@@ -792,11 +792,6 @@ config NRF53_SDC_DLE
 	bool "Support Data Length Extension (DLE)"
 	default y
 
-config NRF53_BLE_TTY_NAME
-	string "BLE TTY device name"
-	default "/dev/ttyHCI0"
-	depends on UART_BTH4
-
 config NRF53_SDC_FICR_STATIC_ADDR
 	bool "Configure factory generated static random address"
 	default n

arch/arm/src/stm32wb/Kconfig
@@ -1182,11 +1182,6 @@ config STM32WB_BLE_LE_CODED_PHY
 	default STM32WB_STM32WB15 || STM32WB_STM32WB35 || STM32WB_STM32WB55
 	depends on STM32WB_STM32WB15 || STM32WB_STM32WB35 || STM32WB_STM32WB55
 
-config STM32WB_BLE_TTY_NAME
-	string "BLE TTY device name"
-	default "/dev/ttyHCI0"
-	depends on UART_BTH4
-
 config STM32WB_BLE_FICR_STATIC_ADDR
 	bool "Configure factory generated static random address"
 	default n

arch/arm/src/stm32wb/stm32wb_blehci.c
@@ -33,10 +33,6 @@
 #include <nuttx/mutex.h>
 #include <nuttx/wqueue.h>
 
-#if defined(CONFIG_UART_BTH4)
-#  include <nuttx/serial/uart_bth4.h>
-#endif
-
 #include "stm32wb_ipcc.h"
 #include "stm32wb_mbox.h"
 #include "stm32wb_mbox_shci.h"
@@ -336,22 +332,11 @@ static int stm32wb_blehci_driverinitialize(void)
 {
   int ret = 0;
 
-#ifdef CONFIG_UART_BTH4
-  /* Register UART BT H4 device */
-
-  ret = uart_bth4_register(CONFIG_STM32WB_BLE_TTY_NAME, &g_blehci_driver);
+#ifdef CONFIG_DRIVERS_BLUETOOTH
+  ret = bt_driver_register(&g_blehci_driver);
   if (ret < 0)
     {
-      wlerr("bt_bth4_register error: %d\n", ret);
-      return ret;
-    }
-#elif defined(CONFIG_NET_BLUETOOTH)
-  /* Register network device */
-
-  ret = bt_netdev_register(&g_blehci_driver);
-  if (ret < 0)
-    {
-      wlerr("bt_netdev_register error: %d\n", ret);
+      wlerr("bt_driver_register error: %d\n", ret);
       return ret;
     }
 #endif

arch/sim/src/sim/sim_hcisocket.c
@@ -40,8 +40,6 @@
 #include <nuttx/net/bluetooth.h>
 #include <nuttx/wireless/bluetooth/bt_driver.h>
 #include <nuttx/wireless/bluetooth/bt_uart.h>
-#include <nuttx/wireless/bluetooth/bt_bridge.h>
-#include <nuttx/serial/uart_bth4.h>
 
 #include "sim_internal.h"
 #include "sim_hosthcisocket.h"
@@ -256,27 +254,6 @@ static void bthcisock_free(struct bthcisock_s *dev)
   kmm_free(dev);
 }
 
-static int bthcisock_driver_register(struct bt_driver_s *drv, int id,
-                                     bool bt)
-{
-#ifdef CONFIG_UART_BTH4
-  char name[32];
-
-  if (bt)
-    {
-      snprintf(name, sizeof(name), "/dev/ttyBT%d", id);
-    }
-  else
-    {
-      snprintf(name, sizeof(name), "/dev/ttyBLE%d", id);
-    }
-
-  return uart_bth4_register(name, drv);
-#else
-  return bt_netdev_register(drv);
-#endif
-}
-
 /****************************************************************************
  * Name: sim_bthcisock_work
  *
@@ -320,10 +297,6 @@ static void sim_bthcisock_work(void *arg)
 int sim_bthcisock_register(int dev_id)
 {
   struct bthcisock_s *dev;
-#if defined(CONFIG_BLUETOOTH_BRIDGE)
-  struct bt_driver_s *btdrv;
-  struct bt_driver_s *bledrv;
-#endif
   int ret;
 
   dev = bthcisock_alloc(dev_id);
@@ -332,35 +305,17 @@ int sim_bthcisock_register(int dev_id)
       return -ENOMEM;
     }
 
-#if defined(CONFIG_BLUETOOTH_BRIDGE)
-  ret = bt_bridge_register(&dev->drv, &btdrv, &bledrv);
+#ifdef CONFIG_DRIVERS_BLUETOOTH
+  ret = bt_driver_register_with_id(&dev->drv, dev_id);
   if (ret < 0)
     {
-      goto end;
-    }
-
-  ret = bthcisock_driver_register(btdrv, dev_id, true);
-  if (ret < 0)
-    {
-      goto end;
-    }
-
-  ret = bthcisock_driver_register(bledrv, dev_id, false);
-  if (ret < 0)
-    {
-      goto end;
-    }
-
-end:
-#else
-  ret = bthcisock_driver_register(&dev->drv, dev_id, true);
-#endif
-
-  if (ret < 0)
-    {
+      wlerr("bt_driver_register error: %d\n", ret);
       bthcisock_free(dev);
       return ret;
     }
+#else
+# error "Please select CONFIG_DRIVERS_BLUETOOTH"
+#endif
 
   return work_queue(HPWORK, &dev->worker, sim_bthcisock_work, dev, 0);
 }

arch/xtensa/src/esp32/Kconfig
@@ -2175,11 +2175,6 @@ config ESP32_BLE_PKTBUF_NUM
 	int "BLE netcard packet buffer number per netcard"
 	default 16
 
-config ESP32_BLE_TTY_NAME
-	string "BLE TTY device name"
-	default "/dev/ttyHCI0"
-	depends on UART_BTH4
-
 config ESP32_BLE_TASK_STACK_SIZE
 	int "Controller task stack size"
 	default 4096

commit 6738a96be883b2bc4c8e479a5e94c681263c9c2e

arch/nrf53/nrf53_sdc.c: fix broken RPMSG HCI server
BT device should not be registered when RPMGS HCI server is enabled.
Broken server since 31605b6335a8411833b410bf89ef10bd4fe502b0.

Signed-off-by: raiden00pl <raiden00@railab.me>

arch/arm/src/nrf53/nrf53_sdc.c
@@ -909,12 +909,14 @@ int nrf53_sdc_initialize(void)
       return ret;
     }
 
+#ifndef CONFIG_BLUETOOTH_RPMSG_SERVER
   ret = bt_driver_register(&g_bt_driver);
   if (ret < 0)
     {
       wlerr("bt_driver_register error: %d\n", ret);
       return ret;
     }
+#endif
 
   return ret;
 }