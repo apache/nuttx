6e5c809428db99ad7717bcca9239c44ae517c6db
Eren Terzioglu <eren.terzioglu@espressif.com>
2025-05-19 17:20:07 +0200

    boards/risc-v: Add board layer SHA accelerator support for esp32[-c3|-c6|-h2]
    Add board layer SHA accelerator support for risc-v based Espressif devices
    
    Signed-off-by: Eren Terzioglu <eren.terzioglu@espressif.com>

--- boards/risc-v/esp32h2/esp32h2-devkit/src/esp32h2_bringup.c
+++ boards/risc-v/esp32h2/esp32h2-devkit/src/esp32h2_bringup.c
@@ -115,6 +115,10 @@
 #  include "espressif/esp_sdm.h"
 #endif
 
+#ifdef CONFIG_ESPRESSIF_SHA_ACCELERATOR
+#  include "espressif/esp_sha.h"
+#endif
+
 #include "esp32h2-devkit.h"
 
 /****************************************************************************
@@ -175,6 +179,16 @@ int esp_bringup(void)
   if (ret < 0)
     {
       syslog(LOG_ERR, "ERROR: Failed to init EFUSE: %d\n", ret);
+    }
+#endif
+
+#if defined(CONFIG_ESPRESSIF_SHA_ACCELERATOR) && \
+    !defined(CONFIG_CRYPTO_CRYPTODEV_HARDWARE)
+  ret = esp_sha_init();
+  if (ret < 0)
+    {
+      syslog(LOG_ERR,
+             "ERROR: Failed to initialize SHA: %d\n", ret);
     }
 #endif
 
