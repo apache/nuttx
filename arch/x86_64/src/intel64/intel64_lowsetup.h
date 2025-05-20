c9f9e1e35f6e75c262556f9cc6eabad59ebcec7c
ouyangxiangzhen <ouyangxiangzhen@xiaomi.com>
2025-05-20 17:04:40 +0800

    arch/intel64: add timer initialization for the secondary CPUs.
    This commit added timer initialization for the secondary CPUs.
    
    Signed-off-by: ouyangxiangzhen <ouyangxiangzhen@xiaomi.com>

--- arch/x86_64/src/intel64/intel64_lowsetup.h
+++ arch/x86_64/src/intel64/intel64_lowsetup.h
@@ -57,6 +57,16 @@ extern "C"
 
 void intel64_lowsetup(void);
 
+/****************************************************************************
+ * Name: intel64_timer_secondary_init
+ *
+ * Description:
+ *   Called for the secondary CPUs to initialize the timer subsystem.
+ *
+ ****************************************************************************/
+
+void intel64_timer_secondary_init(void);
+
 #undef EXTERN
 #if defined(__cplusplus)
 }
