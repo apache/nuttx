31b7d53a77ff12d46313dc104a5214d00a0aeb71
Eren Terzioglu <eren.terzioglu@espressif.com>
2025-05-20 17:09:18 +0200

    boards/xtensa: Add board layer SHA accelerator support for esp32[-s2|-s3]
    Add board layer SHA accelerator support for Xtensa based Espressif devices
    
    Signed-off-by: Eren Terzioglu <eren.terzioglu@espressif.com>

--- boards/xtensa/esp32s2/esp32s2-saola-1/src/esp32s2_bringup.c
+++ boards/xtensa/esp32s2/esp32s2-saola-1/src/esp32s2_bringup.c
@@ -117,6 +117,10 @@
 #  include "espressif/esp_sdm.h"
 #endif
 
+#ifdef CONFIG_ESPRESSIF_SHA_ACCELERATOR
+#  include "espressif/esp_sha.h"
+#endif
+
 #ifdef CONFIG_MMCSD_SPI
 #  include "esp32s2_board_sdmmc.h"
 #endif
@@ -171,6 +175,16 @@ int esp32s2_bringup(void)
   if (ret < 0)
     {
       syslog(LOG_ERR, "ERROR: Failed to init EFUSE: %d\n", ret);
+    }
+#endif
+
+#if defined(CONFIG_ESPRESSIF_SHA_ACCELERATOR) && \
+    !defined(CONFIG_CRYPTO_CRYPTODEV_HARDWARE)
+  ret = esp_sha_init();
+  if (ret < 0)
+    {
+      syslog(LOG_ERR,
+             "ERROR: Failed to initialize SHA: %d\n", ret);
     }
 #endif
 
