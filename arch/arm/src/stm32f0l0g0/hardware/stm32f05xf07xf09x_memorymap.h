fedd7500b37a10d8f1e041c1c7f0bac1a4316d91
raiden00pl <raiden00@railab.me>
2025-06-09 10:02:19 +0200

    arch/arm/src/stm32f0l0g0: UID support for F0, L0 and C0
    add UID support for F0, L0 and C0
    
    Signed-off-by: raiden00pl <raiden00@railab.me>

--- arch/arm/src/stm32f0l0g0/hardware/stm32f05xf07xf09x_memorymap.h
+++ arch/arm/src/stm32f0l0g0/hardware/stm32f05xf07xf09x_memorymap.h
@@ -141,4 +141,8 @@
 #define STM32_SCS_BASE       0xe000e000
 #define STM32_DEBUGMCU_BASE  0xe0042000
 
+/* System Memory Addresses **************************************************/
+
+#define STM32_SYSMEM_UID     0x1ffff7ac     /* The 96-bit unique device identifier */
+
 #endif /* __ARCH_ARM_SRC_STM32F0L0G0_HARDWARE_STM32F05XF07XF09X_MEMORYMAP_H */
