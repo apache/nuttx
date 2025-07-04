0d7258f8b713494c85db27de014b0e2a050bc735
Jukka Laitinen <jukka.laitinen@tii.ae>
2025-07-04 09:59:10 +0300

    sched: Remove pending list for CONFIG_SMP
    This fixes several places, where the sched erroneously checks if the scheduling
    is locked on current cpu/task, when it should check if the scheduling is locked
    on the target cpu/task.
    
    The original code randomly caused a task to be added to the pending list, and never
    taken out from there, leading to system halt.
    
    For SMP, there is no need for the pending list. Each CPU has got it's own
    running list (assigned tasks list), and pending tasks can just be kept in
    the unassigned (readytorun) list.
    
    In addition, the smp scheduling is changed in a way that every CPU just picks
    up the tasks from the ready-to-run list themselves. Which task to pick is
    not tried to be dictated by another CPU.
    
    This also allows using up_send_smp_sched for asynchronously
    - re-prioritizing a running task
    - triggering round robin scheduling switch
    
    Iow, no separate smp call mechanism is needed for those and the code can be simplified.
    
    Signed-off-by: Jukka Laitinen <jukka.laitinen@tii.ae>

--- sched/sched/sched_mergepending.c
+++ sched/sched/sched_mergepending.c
@@ -32,18 +32,8 @@
 
 #include <nuttx/queue.h>
 
-#ifdef CONFIG_SMP
-#  include <nuttx/spinlock.h>
-#endif
-
 #include "irq/irq.h"
 #include "sched/sched.h"
-
-/****************************************************************************
- * Pre-processor Definitions
- ****************************************************************************/
-
-#define ALL_CPUS ((cpu_set_t)-1)
 
 /****************************************************************************
  * Public Functions
@@ -71,7 +61,6 @@
  *
  ****************************************************************************/
 
-#ifndef CONFIG_SMP
 bool nxsched_merge_pending(void)
 {
   FAR struct tcb_s *ptcb;
@@ -161,125 +150,3 @@ bool nxsched_merge_pending(void)
 
   return ret;
 }
-#endif /* !CONFIG_SMP */
-
-/****************************************************************************
- * Name: nxsched_merge_pending
- *
- * Description:
- *   This function merges the prioritized g_pendingtasks list into the
- *   prioritized ready-to-run task list.
- *
- * Input Parameters:
- *   None
- *
- * Returned Value:
- *   true if the head of the ready-to-run task list has changed indicating
- *     a context switch is needed.
- *
- * Assumptions:
- * - The caller has established a critical section before calling this
- *   function.
- * - The caller handles the condition that occurs if the head of the
- *   ready-to-run task list is changed.
- *
- ****************************************************************************/
-
-#ifdef CONFIG_SMP
-bool nxsched_merge_pending(void)
-{
-  FAR struct tcb_s *rtcb;
-  FAR struct tcb_s *ptcb;
-  FAR struct tcb_s *tcb;
-  bool ret = false;
-  int cpu;
-
-  /* Remove and process every TCB in the g_pendingtasks list.
-   *
-   * Do nothing if (1) pre-emption is still disabled (by any CPU), or (2) if
-   * some CPU other than this one is in a critical section.
-   */
-
-  if (!nxsched_islocked_tcb(this_task()))
-    {
-      /* Find the CPU that is executing the lowest priority task */
-
-      ptcb = (FAR struct tcb_s *)dq_peek(list_pendingtasks());
-      if (ptcb == NULL)
-        {
-          /* The pending task list is empty */
-
-          return false;
-        }
-
-      cpu  = nxsched_select_cpu(ALL_CPUS); /* REVISIT:  Maybe ptcb->affinity */
-      rtcb = current_task(cpu);
-
-      /* Loop while there is a higher priority task in the pending task list
-       * than in the lowest executing task.
-       *
-       * Normally, this loop should execute no more than CONFIG_SMP_NCPUS
-       * times.  That number could be larger, however, if the CPU affinity
-       * sets do not include all CPUs. In that case, the excess TCBs will
-       * end up in the g_readytorun list.
-       */
-
-      while (ptcb->sched_priority > rtcb->sched_priority)
-        {
-          /* Remove the task from the pending task list */
-
-          tcb = (FAR struct tcb_s *)dq_remfirst(list_pendingtasks());
-
-          /* Add the pending task to the correct ready-to-run list. */
-
-          ret |= nxsched_add_readytorun(tcb);
-
-          /* This operation could cause the scheduler to become locked.
-           * Check if that happened.
-           */
-
-          if (nxsched_islocked_tcb(this_task()))
-            {
-              /* Yes.. then we may have incorrectly placed some TCBs in the
-               * g_readytorun list (unlikely, but possible).  We will have to
-               * move them back to the pending task list.
-               */
-
-              nxsched_merge_prioritized(list_readytorun(),
-                                        list_pendingtasks(),
-                                        TSTATE_TASK_PENDING);
-
-              /* And return with the scheduler locked and tasks in the
-               * pending task list.
-               */
-
-              goto errout;
-            }
-
-          /* Set up for the next time through the loop */
-
-          ptcb = (FAR struct tcb_s *)dq_peek(list_pendingtasks());
-          if (ptcb == NULL)
-            {
-              /* The pending task list is empty */
-
-              goto errout;
-            }
-
-          cpu  = nxsched_select_cpu(ALL_CPUS); /* REVISIT:  Maybe ptcb->affinity */
-          rtcb = current_task(cpu);
-        }
-
-      /* No more pending tasks can be made running.  Move any remaining
-       * tasks in the pending task list to the ready-to-run task list.
-       */
-
-      nxsched_merge_prioritized(list_pendingtasks(),
-                                list_readytorun(),
-                                TSTATE_TASK_READYTORUN);
-    }
-
-errout:
-  return ret;
-}
-#endif /* CONFIG_SMP */
