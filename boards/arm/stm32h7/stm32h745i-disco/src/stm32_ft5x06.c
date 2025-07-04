5671765acdebd95487b28159276bb345d536056e
wangjianyu3 <wangjianyu3@xiaomi.com>
2025-07-05 01:28:17 +0800

    boards: Update configs and params about FT5X06
    Add touchscreen swap configuration for boards and enable it for defconfig that enables `FT5X06_SWAPXY`.
    Refresh configurations to delete the deprecated `FT5X06_NPOLLWAITERS` and `FT5X06_SWAPXY`.
    
    Signed-off-by: wangjianyu3 <wangjianyu3@xiaomi.com>

--- boards/arm/stm32h7/stm32h745i-disco/src/stm32_ft5x06.c
+++ boards/arm/stm32h7/stm32h745i-disco/src/stm32_ft5x06.c
@@ -104,7 +104,13 @@ static const struct ft5x06_config_s g_ft5x06_config =
   .clear     = stm32_ft5x06_clear,
 #endif
   .wakeup    = stm32_ft5x06_wakeup,
-  .nreset    = stm32_ft5x06_nreset
+  .nreset    = stm32_ft5x06_nreset,
+  .lower     =
+    {
+#ifdef CONFIG_ARCH_BOARD_STM32H745I_DISCO_TOUCHSCREEN_SWAPXY
+      .flags = TOUCH_FLAG_SWAPXY,
+#endif
+    },
 };
 
 #ifndef CONFIG_FT5X06_POLLMODE
