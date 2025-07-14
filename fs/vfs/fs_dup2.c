apache/nuttx

commit b8e30b54ec9c3092cb44394cdb6a4958dd6d3b42

fs/vfs: Separate file descriptors from file descriptions
This patch is a rework of the NuttX file descriptor implementation. The
goal is two-fold:

1. Improve POSIX compliance. The old implementation tied file description
to inode only, not the file struct. POSIX however dictates otherwise.
2. Fix a bug with descriptor duplication (dup2() and dup3()). There is
an existing race condition with this POSIX API that currently results
in a kernel side crash.

The crash occurs when a partially open / closed file descriptor is
duplicated. The reason for the crash is that even if the descriptor is
closed, the file might still be in use by the kernel (due to e.g. ongoing
write to file). The open file data is changed by file_dup3() and this
causes a crash in the device / drivers themselves as they lose access to
the inode and private data.

The fix is done by separating struct file into file and file descriptor
structs. The file struct can live on even if the descriptor is closed,
fixing the crash. This also fixes the POSIX issue, as two descriptors
can now point to the same file.

Signed-off-by: Ville Juven <ville.juven@unikie.com>
Signed-off-by: dongjiuzhu1 <dongjiuzhu1@xiaomi.com>

fs/vfs/fs_dup2.c
@@ -42,13 +42,13 @@
  ****************************************************************************/
 
 /****************************************************************************
- * Name: file_dup3
+ * Name: file_dup2
  *
  * Description:
  *   Assign an inode to a specific files structure.  This is the heart of
- *   dup3.
+ *   dup2.
  *
- *   Equivalent to the non-standard dup3() function except that it
+ *   Equivalent to the non-standard dup2() function except that it
  *   accepts struct file instances instead of file descriptors and it does
  *   not set the errno variable.
  *
@@ -58,7 +58,7 @@
  *
  ****************************************************************************/
 
-int file_dup3(FAR struct file *filep1, FAR struct file *filep2, int flags)
+int file_dup2(FAR struct file *filep1, FAR struct file *filep2)
 {
   FAR struct inode *inode;
   int ret;
@@ -66,11 +66,6 @@ int file_dup3(FAR struct file *filep1, FAR struct file *filep2, int flags)
   if (filep1 == NULL || filep1->f_inode == NULL || filep2 == NULL)
     {
       return -EBADF;
-    }
-
-  if (flags != 0 && flags != O_CLOEXEC)
-    {
-      return -EINVAL;
     }
 
   if (filep1 == filep2)
@@ -83,27 +78,13 @@ int file_dup3(FAR struct file *filep1, FAR struct file *filep2, int flags)
   inode = filep1->f_inode;
   inode_addref(inode);
 
-  /* If there is already an inode contained in the new file structure,
-   * close the file and release the inode.
-   * But we need keep the filep2->f_inode, in case of realloced by others.
-   */
+  /* Close the second file */
 
-  ret = file_close_without_clear(filep2);
+  ret = file_close(filep2);
   if (ret < 0)
     {
       inode_release(inode);
       return ret;
-    }
-
-  /* The two filep don't share flags (the close-on-exec flag). */
-
-  if (flags == O_CLOEXEC)
-    {
-      filep2->f_oflags = filep1->f_oflags | O_CLOEXEC;
-    }
-  else
-    {
-      filep2->f_oflags = filep1->f_oflags & ~O_CLOEXEC;
     }
 
   filep2->f_priv  = NULL;
@@ -159,34 +140,10 @@ int file_dup3(FAR struct file *filep1, FAR struct file *filep2, int flags)
       if (ret < 0)
         {
           inode_release(inode);
-          return ret;
         }
     }
 
-  FS_ADD_BACKTRACE(filep2);
-  return OK;
-}
-
-/****************************************************************************
- * Name: file_dup2
- *
- * Description:
- *   Assign an inode to a specific files structure.  This is the heart of
- *   dup2.
- *
- *   Equivalent to the non-standard dup2() function except that it
- *   accepts struct file instances instead of file descriptors and it does
- *   not set the errno variable.
- *
- * Returned Value:
- *   Zero (OK) is returned on success; a negated errno value is return on
- *   any failure.
- *
- ****************************************************************************/
-
-int file_dup2(FAR struct file *filep1, FAR struct file *filep2)
-{
-  return file_dup3(filep1, filep2, 0);
+  return ret;
 }
 
 /****************************************************************************
@@ -209,7 +166,7 @@ int file_dup2(FAR struct file *filep1, FAR struct file *filep2)
 
 int nx_dup2(int fd1, int fd2)
 {
-  return nx_dup2_from_tcb(this_task(), fd1, fd2);
+  return fdlist_dup2(nxsched_get_fdlist_from_tcb(this_task()), fd1, fd2);
 }
 
 /****************************************************************************

crypto/cryptodev.c
@@ -1028,8 +1028,7 @@ static int cryptoioctl(FAR struct file *filep, int cmd, unsigned long arg)
         TAILQ_INIT(&fcr->csessions);
         TAILQ_INIT(&fcr->crpk_ret);
 
-        fd = file_allocate(&g_cryptoinode, 0,
-                           0, fcr, 0, true);
+        fd = file_allocate_from_inode(&g_cryptoinode, 0, 0, fcr, 0);
         if (fd < 0)
           {
             kmm_free(fcr);

drivers/misc/optee.c
@@ -1038,7 +1038,8 @@ optee_ioctl_shm_register(FAR struct optee_priv_data *priv,
       return ret;
     }
 
-  ret = file_allocate(&g_optee_shm_inode, O_CLOEXEC, 0, shm, 0, true);
+  ret = file_allocate_from_inode(&g_optee_shm_inode, O_CLOEXEC,
+                                 0, shm, 0, true);
   if (ret < 0)
     {
       optee_shm_free(shm);

fs/inode/inode.h
@@ -72,21 +72,21 @@
   while (0)
 
 #if CONFIG_FS_BACKTRACE > 0
-#  define FS_ADD_BACKTRACE(filep) \
+#  define FS_ADD_BACKTRACE(fd) \
      do \
        { \
           int n = sched_backtrace(_SCHED_GETTID(), \
-                                  (filep)->f_backtrace, \
+                                  (fd)->f_backtrace, \
                                   CONFIG_FS_BACKTRACE, \
                                   CONFIG_FS_BACKTRACE_SKIP); \
           if (n < CONFIG_FS_BACKTRACE) \
             { \
-              (filep)->f_backtrace[n] = NULL; \
+              (fd)->f_backtrace[n] = NULL; \
             } \
        } \
      while (0)
 #else
-#  define FS_ADD_BACKTRACE(filep)
+#  define FS_ADD_BACKTRACE(fd)
 #endif
 
 /****************************************************************************

fs/mqueue/mq_open.c
@@ -264,7 +264,6 @@ static int file_mq_vopen(FAR struct file *mq, FAR const char *mq_name,
 
       /* Associate the inode with a file structure */
 
-      memset(mq, 0, sizeof(*mq));
       mq->f_oflags = oflags;
       mq->f_inode  = inode;
 
@@ -308,7 +307,6 @@ static int file_mq_vopen(FAR struct file *mq, FAR const char *mq_name,
 
       /* Associate the inode with a file structure */
 
-      memset(mq, 0, sizeof(*mq));
       mq->f_oflags = oflags;
       mq->f_inode  = inode;
 
@@ -347,28 +345,26 @@ errout:
 
 static mqd_t nxmq_vopen(FAR const char *mq_name, int oflags, va_list ap)
 {
-  struct file mq;
+  FAR struct file *mq;
   int created;
   int ret;
+  int fd;
 
-  ret = file_mq_vopen(&mq, mq_name, oflags, getumask(), ap, &created);
+  fd = file_allocate(oflags, 0, &mq);
+  if (fd < 0)
+    {
+      return fd;
+    }
+
+  ret = file_mq_vopen(mq, mq_name, oflags, getumask(), ap, &created);
+  file_put(mq);
   if (ret < 0)
     {
+      nx_close(fd);
       return ret;
     }
 
-  ret = file_allocate(mq.f_inode, mq.f_oflags,
-                      mq.f_pos, mq.f_priv, 0, false);
-  if (ret < 0)
-    {
-      file_mq_close(&mq);
-      if (created)
-        {
-          file_mq_unlink(mq_name);
-        }
-    }
-
-  return ret;
+  return fd;
 }
 
 /****************************************************************************

fs/notify/inotify.c
@@ -1025,7 +1025,7 @@ static inline void notify_queue_filep_event(FAR struct file *filep,
 
 static void notify_free_entry(FAR ENTRY *entry)
 {
-  /* Key is alloced by lib_malloc, value is alloced by fs_heap_malloc */
+  /* Key is allocated by lib_malloc, value is allocated by fs_heap_malloc */
 
   fs_heap_free(entry->key);
   fs_heap_free(entry->data);
@@ -1255,8 +1255,8 @@ int inotify_init1(int flags)
       goto exit_set_errno;
     }
 
-  ret = file_allocate(&g_inotify_inode, O_RDOK | flags,
-                      0, dev, 0, true);
+  ret = file_allocate_from_inode(&g_inotify_inode, O_RDOK | flags,
+                                 0, dev, 0);
   if (ret < 0)
     {
       ferr("Failed to allocate inotify fd\n");

fs/procfs/fs_procfsproc.c
@@ -1252,7 +1252,6 @@ static ssize_t proc_groupfd(FAR struct proc_file_s *procfile,
                             size_t buflen, off_t offset)
 {
   FAR struct task_group_s *group = tcb->group;
-  FAR struct file *filep;
   FAR char *path;
   size_t remaining;
   size_t linesize;
@@ -1263,7 +1262,7 @@ static ssize_t proc_groupfd(FAR struct proc_file_s *procfile,
 
   DEBUGASSERT(group != NULL);
 
-  count = files_countlist(&group->tg_filelist);
+  count = fdlist_count(&group->tg_fdlist);
   if (count == 0)
     {
       return 0;
@@ -1303,11 +1302,12 @@ static ssize_t proc_groupfd(FAR struct proc_file_s *procfile,
 
   for (i = 0; i < count; i++)
     {
-      filep = files_fget(&group->tg_filelist, i);
+      FAR struct file *filep;
+      FAR struct fd *fdp;
 
       /* Is there an inode associated with the file descriptor? */
 
-      if (filep == NULL)
+      if (fdlist_get2(&group->tg_fdlist, i, &filep, &fdp) < 0)
         {
           continue;
         }
@@ -1327,7 +1327,7 @@ static ssize_t proc_groupfd(FAR struct proc_file_s *procfile,
 #if CONFIG_FS_BACKTRACE > 0
           linesize += backtrace_format(procfile->line + linesize,
                                        STATUS_LINELEN - linesize,
-                                       filep->f_backtrace,
+                                       fdp->f_backtrace,
                                        CONFIG_FS_BACKTRACE);
 #endif
           procfile->line[linesize - 2] = '\n';

commit 900c1a231acbbaa3cb5560a3d7f49904315ad249

fs/vfs: fix bug about lost dup oflags in dup2
this issue is caused by commit: b8e30b54ec9c3092cb44394cdb6a4958dd6d3b42

Signed-off-by: dongjiuzhu1 <dongjiuzhu1@xiaomi.com>

fs/vfs/fs_dup2.c
@@ -87,9 +87,10 @@ int file_dup2(FAR struct file *filep1, FAR struct file *filep2)
       return ret;
     }
 
-  filep2->f_priv  = NULL;
-  filep2->f_pos   = filep1->f_pos;
-  filep2->f_inode = inode;
+  filep2->f_oflags = filep1->f_oflags;
+  filep2->f_priv   = NULL;
+  filep2->f_pos    = filep1->f_pos;
+  filep2->f_inode  = inode;
 
   /* Call the open method on the file, driver, mountpoint so that it
    * can maintain the correct open counts.