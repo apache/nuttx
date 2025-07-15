983c8325727937044f9db2c00396bbbe43f0dc4e
simbit18 <101105604+simbit18@users.noreply.github.com>
2025-07-15 18:40:13 +0200

    ci/platforms/linux.sh: bump Xtensa ESP32|S2|S3 toolchain
    Bump Xtensa esp-14.2.0_20241119

--- tools/ci/platforms/linux.sh
+++ tools/ci/platforms/linux.sh
@@ -251,55 +251,21 @@ sparc_gcc_toolchain() {
   command sparc-gaisler-elf-gcc --version
 }
 
-xtensa_esp32_gcc_toolchain() {
-  add_path "${NUTTXTOOLS}"/xtensa-esp32-elf/bin
+xtensa_esp_gcc_toolchain() {
+  add_path "${NUTTXTOOLS}"/xtensa-esp-elf/bin
 
-  if [ ! -f "${NUTTXTOOLS}/xtensa-esp32-elf/bin/xtensa-esp32-elf-gcc" ]; then
+  if [ ! -f "${NUTTXTOOLS}/xtensa-esp-elf/bin/xtensa-esp32-elf-gcc" ]; then
     local basefile
-    basefile=xtensa-esp32-elf-12.2.0_20230208-x86_64-linux-gnu
+    basefile=xtensa-esp-elf-14.2.0_20241119-x86_64-linux-gnu
     cd "${NUTTXTOOLS}"
-    # Download the latest ESP32 GCC toolchain prebuilt by Espressif
-    curl -O -L -s https://github.com/espressif/crosstool-NG/releases/download/esp-12.2.0_20230208/${basefile}.tar.xz
+    # Download the latest ESP32, ESP32-S2 and ESP32-S3 GCC toolchain prebuilt by Espressif
+    curl -O -L -s https://github.com/espressif/crosstool-NG/releases/download/esp-14.2.0_20241119/${basefile}.tar.xz
     xz -d ${basefile}.tar.xz
     tar xf ${basefile}.tar
     rm ${basefile}.tar
   fi
 
   command xtensa-esp32-elf-gcc --version
-}
-
-xtensa_esp32s2_gcc_toolchain() {
-  add_path "${NUTTXTOOLS}"/xtensa-esp32s2-elf/bin
-
-  if [ ! -f "${NUTTXTOOLS}/xtensa-esp32s2-elf/bin/xtensa-esp32s2-elf-gcc" ]; then
-    local basefile
-    basefile=xtensa-esp32s2-elf-12.2.0_20230208-x86_64-linux-gnu
-    cd "${NUTTXTOOLS}"
-    # Download the latest ESP32 S2 GCC toolchain prebuilt by Espressif
-    curl -O -L -s https://github.com/espressif/crosstool-NG/releases/download/esp-12.2.0_20230208/${basefile}.tar.xz
-    xz -d ${basefile}.tar.xz
-    tar xf ${basefile}.tar
-    rm ${basefile}.tar
-  fi
-
-  command xtensa-esp32s2-elf-gcc --version
-}
-
-xtensa_esp32s3_gcc_toolchain() {
-  add_path "${NUTTXTOOLS}"/xtensa-esp32s3-elf/bin
-
-  if [ ! -f "${NUTTXTOOLS}/xtensa-esp32s3-elf/bin/xtensa-esp32s3-elf-gcc" ]; then
-    local basefile
-    basefile=xtensa-esp32s3-elf-12.2.0_20230208-x86_64-linux-gnu
-    cd "${NUTTXTOOLS}"
-    # Download the latest ESP32 S3 GCC toolchain prebuilt by Espressif
-    curl -O -L -s https://github.com/espressif/crosstool-NG/releases/download/esp-12.2.0_20230208/${basefile}.tar.xz
-    xz -d ${basefile}.tar.xz
-    tar xf ${basefile}.tar
-    rm ${basefile}.tar
-  fi
-
-  command xtensa-esp32s3-elf-gcc --version
 }
 
 wasi_sdk() {
@@ -368,7 +334,7 @@ install_build_tools() {
   mkdir -p "${NUTTXTOOLS}"
   echo "#!/usr/bin/env sh" > "${NUTTXTOOLS}"/env.sh
 
-  install="arm_clang_toolchain arm_gcc_toolchain arm64_gcc_toolchain bloaty kconfig_frontends mips_gcc_toolchain python_tools riscv_gcc_toolchain rx_gcc_toolchain sparc_gcc_toolchain xtensa_esp32_gcc_toolchain xtensa_esp32s2_gcc_toolchain xtensa_esp32s3_gcc_toolchain util_linux wasi_sdk"
+  install="arm_clang_toolchain arm_gcc_toolchain arm64_gcc_toolchain bloaty kconfig_frontends mips_gcc_toolchain python_tools riscv_gcc_toolchain rx_gcc_toolchain sparc_gcc_toolchain xtensa_esp_gcc_toolchain util_linux wasi_sdk"
 
   oldpath=$(cd . && pwd -P)
   for func in ${install}; do
