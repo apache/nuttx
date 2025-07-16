5b3c5ac6674003566868352060634632f2188b7d
simbit18 <101105604+simbit18@users.noreply.github.com>
2025-07-16 12:50:21 +0200

    ci/platforms/darwin.sh: bump Xtensa ESP32|S2|S3 toolchain
    Bump Xtensa esp-14.2.0_20241119

--- tools/ci/platforms/darwin.sh
+++ tools/ci/platforms/darwin.sh
@@ -309,55 +309,21 @@ zig() {
   command zig cc --version
 }
 
-xtensa_esp32_gcc_toolchain() {
-  add_path "${NUTTXTOOLS}"/xtensa-esp32-elf/bin
+xtensa_esp_gcc_toolchain() {
+  add_path "${NUTTXTOOLS}"/xtensa-esp-elf/bin
 
-  if [ ! -f "${NUTTXTOOLS}/xtensa-esp32-elf/bin/xtensa-esp32-elf-gcc" ]; then
+  if [ ! -f "${NUTTXTOOLS}/xtensa-esp-elf/bin/xtensa-esp32-elf-gcc" ]; then
     local basefile
-    basefile=xtensa-esp32-elf-12.2.0_20230208-x86_64-apple-darwin
+    basefile=xtensa-esp-elf-14.2.0_20241119-x86_64-apple-darwin
     cd "${NUTTXTOOLS}"
-    # Download the latest ESP32 GCC toolchain prebuilt by Espressif
-    curl -O -L -s https://github.com/espressif/crosstool-NG/releases/download/esp-12.2.0_20230208/${basefile}.tar.xz
+    # Download the latest ESP32, ESP32-S2 and ESP32-S3 GCC toolchain prebuilt by Espressif
+    curl -O -L -s https://github.com/espressif/crosstool-NG/releases/download/esp-14.2.0_20241119/${basefile}.tar.xz
     xz -d ${basefile}.tar.xz
     tar xf ${basefile}.tar
     rm ${basefile}.tar
   fi
 
   command xtensa-esp32-elf-gcc --version
-}
-
-xtensa_esp32s2_gcc_toolchain() {
-  add_path "${NUTTXTOOLS}"/xtensa-esp32s2-elf/bin
-
-  if [ ! -f "${NUTTXTOOLS}/xtensa-esp32s2-elf/bin/xtensa-esp32s2-elf-gcc" ]; then
-    local basefile
-    basefile=xtensa-esp32s2-elf-12.2.0_20230208-x86_64-apple-darwin
-    cd "${NUTTXTOOLS}"
-    # Download the latest ESP32 S2 GCC toolchain prebuilt by Espressif
-    curl -O -L -s https://github.com/espressif/crosstool-NG/releases/download/esp-12.2.0_20230208/${basefile}.tar.xz
-    xz -d ${basefile}.tar.xz
-    tar xf ${basefile}.tar
-    rm ${basefile}.tar
-  fi
-
-  command xtensa-esp32s2-elf-gcc --version
-}
-
-xtensa_esp32s3_gcc_toolchain() {
-  add_path "${NUTTXTOOLS}"/xtensa-esp32s3-elf/bin
-
-  if [ ! -f "${NUTTXTOOLS}/xtensa-esp32s3-elf/bin/xtensa-esp32s3-elf-gcc" ]; then
-    local basefile
-    basefile=xtensa-esp32s3-elf-12.2.0_20230208-x86_64-apple-darwin
-    cd "${NUTTXTOOLS}"
-    # Download the latest ESP32 S3 GCC toolchain prebuilt by Espressif
-    curl -O -L -s https://github.com/espressif/crosstool-NG/releases/download/esp-12.2.0_20230208/${basefile}.tar.xz
-    xz -d ${basefile}.tar.xz
-    tar xf ${basefile}.tar
-    rm ${basefile}.tar
-  fi
-
-  command xtensa-esp32s3-elf-gcc --version
 }
 
 u_boot_tools() {
@@ -436,7 +402,7 @@ install_build_tools() {
   mkdir -p "${NUTTXTOOLS}"
   echo "#!/usr/bin/env sh" > "${NUTTXTOOLS}"/env.sh
 
-  install="ninja_brew autoconf_brew arm_clang_toolchain arm_gcc_toolchain arm64_gcc_toolchain avr_gcc_toolchain binutils bloaty elf_toolchain gen_romfs gperf kconfig_frontends mips_gcc_toolchain python_tools riscv_gcc_toolchain rust dlang zig xtensa_esp32_gcc_toolchain xtensa_esp32s2_gcc_toolchain xtensa_esp32s3_gcc_toolchain u_boot_tools util_linux wasi_sdk c_cache"
+  install="ninja_brew autoconf_brew arm_clang_toolchain arm_gcc_toolchain arm64_gcc_toolchain avr_gcc_toolchain binutils bloaty elf_toolchain gen_romfs gperf kconfig_frontends mips_gcc_toolchain python_tools riscv_gcc_toolchain rust dlang zig xtensa_esp_gcc_toolchain u_boot_tools util_linux wasi_sdk c_cache"
 
   mkdir -p "${NUTTXTOOLS}"/homebrew
   export HOMEBREW_CACHE=${NUTTXTOOLS}/homebrew
