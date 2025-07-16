b3caa997ff9028a1e40ea1861db701181bfe974b
Theodore Karatapanis <tkaratapanis@census-labs.com>
2025-07-16 13:36:09 +0300

    drivers/misc/optee: Expanded RPC support.
    This commit expands RPC support for the OP-TEE driver using 2 files:
    1) drivers/misc/optee_rpc.c
       * Add support for RPCs that can be handled directly by the kernel.
       * Can delegate RPC handling to optee_supplicant.c for RPCs that
         need userspace interaction.
    2) drivers/misc/optee_supplicant.c
       * Enable communication between the userspace TEE supplicant and the
         kernel driver.
    
    Additional changes were needed to the following files:
    1) drivers/misc/optee.c
       * Add ioctls used SOLELY by the userspace TEE supplicant.
       * Register /dev/teepriv0 if the supplicant is enabled in Kconfig
       * Add OPTEE_ROLE_CA and OPTEE_ROLE_SUPPLICANT conditionals to
         differentiate paths, between a normal Client Application (CA)
         and the TEE supplicant.
       * Change some functions from static to "public" to reuse them
         in other C files.
       * Adjust optee_to/from_msg_param() to work with RPCs.
    
    2) drivers/misc/optee_smc.c
       * Call the RPC handler from optee_rpc.c
    
    3) drivers/misc/optee_msg.h
       * Add definition needed for RPCs
    
    4) drivers/misc/tee.h
       * Add ioctl definitions
       * Add TEE_SHM_SUPP flag, checked when unregistering supplicant
         memory.
    
    5) Documentation/guides/optee.rs
       * Add documentation for RPCs and the supplicant.
    
    6) drivers/misc/{CMakeLists.txt, Make.defs}
       * Account for the new files.
    
    7) drivers/misc/Kconfig
       * Add DEV_OPTEE_SUPPLICANT option to enable/disable the supplicant
         driver.

---
+++ drivers/misc/optee_supplicant.h
@@ -0,0 +1,73 @@
+/****************************************************************************
+ * drivers/misc/optee_supplicant.h
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+#ifndef __DRIVERS_MISC_OPTEE_SUPPLICANT_H
+#define __DRIVERS_MISC_OPTEE_SUPPLICANT_H
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <sys/types.h>
+#include "optee.h"
+
+/****************************************************************************
+ * Public Functions Prototypes
+ ****************************************************************************/
+
+#undef EXTERN
+#if defined(__cplusplus)
+#define EXTERN extern "C"
+extern "C"
+{
+#else
+#define EXTERN extern
+#endif
+
+void optee_supplicant_init(void);
+void optee_supplicant_uninit(void);
+bool optee_supplicant_running(void);
+
+FAR struct idr_s *optee_supplicant_get_shm_idr(void);
+FAR struct idr_s *optee_supplicant_init_shm_idr(void);
+
+uint32_t optee_supplicant_request(uint32_t func, size_t num_params,
+                                  FAR struct tee_ioctl_param *param);
+
+uint32_t optee_supplicant_cmd_free(int32_t shm_id);
+int32_t optee_supplicant_cmd_alloc(FAR struct optee_priv_data *priv,
+                                   size_t sz, FAR struct optee_shm **shm);
+
+void optee_supplicant_cmd(FAR struct optee_priv_data *priv,
+                          FAR struct optee_msg_arg *arg);
+
+int optee_supplicant_send(uint32_t ret, uint32_t num_params,
+                          FAR struct tee_ioctl_param *param);
+
+int optee_supplicant_recv(FAR uint32_t *func, FAR uint32_t *num_params,
+                          FAR struct tee_ioctl_param *params);
+
+#undef EXTERN
+#if defined(__cplusplus)
+}
+#endif
+#endif /* __DRIVERS_MISC_OPTEE_SUPPLICANT_H */
