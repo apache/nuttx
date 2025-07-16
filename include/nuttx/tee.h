9e1f99e7a61fc27b9cfe99ba9b83bd5bc50878da
Theodore Karatapanis <tkaratapanis@census-labs.com>
2025-07-16 12:56:42 +0300

    drivers/misc/optee: Add mmap shm op + remove memfd
    The previous approach with memfd has 3 problems:
    1) The close operation on the memfd isn't tied with optee_shm_close,
       therefore close(fd) doesn't free the optee_shm struct allocated
       by the kernel.
    
    2) The kernel unnecessarily maps the file descriptor to its memory,
       however only userspace should need to do that.
    
    3) Since the kernel doesn't need to map the file descriptor we
       don't need to unmap it.
    
    To use anonymous mapping, the prototype of map_anonymous() was
    moved from fs/mmap/fs_anonmap.h to include/nuttx/fs/fs.h. Since
    fs_anonmap.h didn't contain any other information it is deleted.
    
    A type from fs/mmap/fs_rammap.h was moved to the public :
    include/nuttx/fs/fs.h as well.
    
    Signed-off-by: Theodore Karatapanis <tkaratapanis@census-labs.com>

--- include/nuttx/tee.h
+++ include/nuttx/tee.h
@@ -382,6 +382,7 @@ struct tee_iocl_supp_send_arg
 
 #define TEE_SHM_ALLOC    (1 << 0) /* Kernel-malloced and must freed */
 #define TEE_SHM_REGISTER (1 << 1) /* Registered with TEE OS */
+#define TEE_SHM_USER_MAP (1 << 2) /* Will be used by userspace after mmap */
 
 /* struct tee_ioctl_shm_register_data - Shared memory register argument
  * addr:      [in] Start address of shared memory to register
