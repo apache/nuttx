9526dae27dc4645833b65eba2fc188bfe42531a9
chao an <anchao.archer@bytedance.com>
2025-07-19 15:50:50 +0800

    xtensa/isa: fix build break if compiler without ISA - XCHAL_HAVE_THREADPTR support
    common/xtensa_context.S: Assembler messages:
    common/xtensa_context.S:134: Error: invalid register number (231) for 'rur' instruction
    common/xtensa_context.S:283: Error: invalid register number (231) for 'wur' instruction
    clang-10: error: Xtensa-as command failed with exit code 1 (use -v to see invocation)
    make[1]: *** [Makefile:143: xtensa_context.o] Error 1
    make[1]: *** Waiting for unfinished jobs....
    
    Signed-off-by: chao an <anchao.archer@bytedance.com>

--- arch/xtensa/src/common/xtensa_saveusercontext.c
+++ arch/xtensa/src/common/xtensa_saveusercontext.c
@@ -79,8 +79,10 @@ int up_saveusercontext(void *saveregs)
           "   s32i a14, %0, (4 * " STRINGIFY(REG_A14) ")\n"
           "   s32i a15, %0, (4 * " STRINGIFY(REG_A15) ")\n"
 #endif
+#ifdef CONFIG_SCHED_THREAD_LOCAL
           "   rur  a2, THREADPTR\n"
           "   s32i a2, %0, (4 * " STRINGIFY(THREADPTR) ")\n"
+#endif
           "   rsr  a2, SAR\n"
           "   s32i a2, %0, (4 * " STRINGIFY(REG_SAR) ")\n"
 #if XCHAL_HAVE_S32C1I != 0
