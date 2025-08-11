43bcf93cbfd1c11b5e8d5e4b48422babbd856508
pangzhen1 <pangzhen1@xiaomi.com>
2025-08-11 19:22:08 +0800

    sched/irq: Avoid same static function name in different files
    In irq_attach_thread.c and irq_attach_wqueue.c, there are static functions named irq_default_handler with the same name, which can be easily misunderstood. Therefore, they have been renamed to different functions.
    
    Signed-off-by: pangzhen1 <pangzhen1@xiaomi.com>

--- sched/irq/irq_attach_thread.c
+++ sched/irq/irq_attach_thread.c
@@ -67,7 +67,7 @@ static pid_t g_irq_thread_pid[NR_IRQS];
  * Useful for oneshot interrupts.
  */
 
-static int irq_default_handler(int irq, FAR void *regs, FAR void *arg)
+static int irq_thread_default_handler(int irq, FAR void *regs, FAR void *arg)
 {
   FAR struct irq_thread_info_s *info = arg;
   int ret = IRQ_WAKE_THREAD;
@@ -101,7 +101,7 @@ static int isr_thread_main(int argc, FAR char *argv[])
 
   nxsem_init(&sem, 0, 0);
 
-  irq_attach(irq, irq_default_handler, &info);
+  irq_attach(irq, irq_thread_default_handler, &info);
 
 #if !defined(CONFIG_ARCH_NOINTC)
   up_enable_irq(irq);
