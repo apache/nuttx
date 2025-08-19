0dbbce3a8fef199b024fb15fa96462f6d6c5141e
Jukka Laitinen <jukka.laitinen@tii.ae>
2025-08-19 13:05:39 +0300

    sched: Remove unnecessary affinity restriction & restoration over smp calls
    When the task has TCB_FLAG_CPU_LOCKED it is locked to the CPU regardless
    of the affinity. There is no need to switch the affinity back and forth.
    
    Signed-off-by: Jukka Laitinen <jukka.laitinen@tii.ae>

--- sched/task/task_restart.c
+++ sched/task/task_restart.c
@@ -48,7 +48,6 @@
 struct restart_arg_s
 {
   pid_t pid;
-  cpu_set_t saved_affinity;
   bool need_restore;
 };
 
@@ -78,7 +77,6 @@ static int restart_handler(FAR void *cookie)
 
   if (arg->need_restore)
     {
-      tcb->affinity = arg->saved_affinity;
       tcb->flags &= ~TCB_FLAG_CPU_LOCKED;
     }
 
@@ -237,12 +235,8 @@ static int nxtask_restart(pid_t pid)
       else
         {
           arg.pid = tcb->pid;
-          arg.saved_affinity = tcb->affinity;
           arg.need_restore = true;
-
           tcb->flags |= TCB_FLAG_CPU_LOCKED;
-          CPU_ZERO(&tcb->affinity);
-          CPU_SET(tcb->cpu, &tcb->affinity);
         }
 
       nxsched_smp_call_single(tcb->cpu, restart_handler, &arg);
