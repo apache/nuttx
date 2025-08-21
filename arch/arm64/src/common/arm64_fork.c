4429e3d3ea38b4a5bc83f3607e247adb9128e717
wangmingrong1 <wangmingrong1@xiaomi.com>
2025-08-21 14:56:10 +0800

    arch/arm64: Delete the default save for SCTLR
    Currently, the SCTLR register is only used to switch the thread MTE state and has no other uses. Because saving this register is special, it will take a long time after testing, so the default saving behavior is deleted.
    
    Signed-off-by: wangmingrong1 <wangmingrong1@xiaomi.com>

--- arch/arm64/src/common/arm64_fork.c
+++ arch/arm64/src/common/arm64_fork.c
@@ -225,9 +225,8 @@ pid_t arm64_fork(const struct fork_s *context)
 
   child->xcp.regs[REG_ELR]    = (uint64_t)context->lr;
 
-  child->xcp.regs[REG_SCTLR_EL1]  = read_sysreg(sctlr_el1);
 #ifdef CONFIG_ARM64_MTE
-  child->xcp.regs[REG_SCTLR_EL1] |= SCTLR_TCF1_BIT;
+  child->xcp.regs[REG_SCTLR_EL1] = read_sysreg(sctlr_el1) | SCTLR_TCF1_BIT;
 #endif
 
   child->xcp.regs[REG_EXE_DEPTH] = 0;
