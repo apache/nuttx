21e7b42b5b9086fcb84ad5cab6422f34a27639e4
wangjianyu3 <wangjianyu3@xiaomi.com>
2025-08-28 10:31:20 +0800

    sched/signal: Old signal action need save sa_user
    For example, set sigaction after create signalfd,
    the sa_sigaction was restored but sa_user not,
    causing signalfd_action() get the wild private data.
    
    Signed-off-by: wangjianyu3 <wangjianyu3@xiaomi.com>

--- sched/signal/sig_action.c
+++ sched/signal/sig_action.c
@@ -294,6 +294,7 @@ int nxsig_action(int signo, FAR const struct sigaction *act,
           oact->sa_handler = sigact->act.sa_handler;
           oact->sa_mask    = sigact->act.sa_mask;
           oact->sa_flags   = sigact->act.sa_flags;
+          oact->sa_user    = sigact->act.sa_user;
         }
       else
         {
