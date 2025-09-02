apache/nuttx

commit 93911d52a8db7a8ec283d2b8e386a00a0ccdde09

tools/checkpatch.sh: check format for all commits in patch
If more than one commit is present in the patch, the commit format
must be checked separately for each commit in patch, otherwise not
all errors are detected.

Signed-off-by: raiden00pl <raiden00@railab.me>

tools/checkpatch.sh
@@ -334,15 +334,23 @@ check_msg() {
   fi
 
   if (( $num_lines < $min_num_lines && $signedoffby_found == 1 )); then
-      echo "Missing git commit message."
-      fail=1
+    echo "Missing git commit message"
+    fail=1
   fi
 }
 
 check_commit() {
   if [ $message != 0 ]; then
-    msg=`git show -s --format=%B $1`
-    check_msg <<< "$msg"
+    # check each commit format separately if this is a series of commits
+    if [[ $1 =~  HEAD ]]; then
+      for commit in $(git rev-list --no-merges $1); do
+        msg=`git show -s --format=%B $commit`
+        check_msg <<< "$msg"
+      done
+    else
+      msg=`git show -s --format=%B $1`
+      check_msg <<< "$msg"
+    fi
   fi
   diffs=`git diff $1`
   check_ranges <<< "$diffs"

commit d6bfded92d9c464f80ccb6aec1ce19f2dff6afdf

tools/checkpatch.sh: fix check for HEAD commit
fix checkpath.sh usage for HEAD commit:

  ./tools/checkpatch.sh -c -u -m -g HEAD

regression after 93911d52a8db7a8ec283d2b8e386a00a0ccdde09

Signed-off-by: raiden00pl <raiden00@railab.me>

tools/checkpatch.sh
@@ -342,7 +342,7 @@ check_msg() {
 check_commit() {
   if [ $message != 0 ]; then
     # check each commit format separately if this is a series of commits
-    if [[ $1 =~  HEAD ]]; then
+    if [[ $1 =~  ..HEAD ]]; then
       for commit in $(git rev-list --no-merges $1); do
         msg=`git show -s --format=%B $commit`
         check_msg <<< "$msg"