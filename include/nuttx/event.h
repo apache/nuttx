c0d4e689742a13eb9644cccab807fb591319e2f3
wangchengdong <wangchengdong@lixiang.com>
2025-09-07 17:07:18 +0800

    sched/event: add nxevent_getmask api
      In certain cases, when the event-wait thread is awakened,
      it needs to know the specific mask bit that triggered it,
      so that it can branch to different paths accordingly.
      This patch introduces the nxevent_getmask function to address this requirement.
    
    Signed-off-by: Chengdong Wang <wangchengdong@lixiang.com>

--- include/nuttx/event.h
+++ include/nuttx/event.h
@@ -310,6 +310,27 @@ nxevent_mask_t nxevent_trywait(FAR nxevent_t *event, nxevent_mask_t events,
                                nxevent_flags_t eflags);
 
 /****************************************************************************
+ * Name: nxevent_getmask
+ *
+ * Description:
+ *   Get the event mask of the given event object.
+ *
+ * Input Parameters:
+ *   event - Address of the event object
+ *
+ * Returned Value:
+ *   Returns the event mask value of the event object.
+ *
+ * Notes:
+ *   - This is an internal OS interface and must not be invoked directly
+ *     by user applications.
+ *   - This function is safe to call from an interrupt handler.
+ *
+ ****************************************************************************/
+
+nxevent_mask_t nxevent_getmask(FAR nxevent_t *event);
+
+/****************************************************************************
  * Name: nxevent_open
  *
  * Description:
