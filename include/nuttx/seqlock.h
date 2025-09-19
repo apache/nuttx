aa2c836952097ab60cb9248fa6d81bf293e90df5
hujun5 <hujun5@xiaomi.com>
2025-09-19 20:17:04 +0800

    seqlock: add SMP_WMB to seqlock
    This commit added SMP_WMB to seqlock.
    
    Signed-off-by: hujun5 <hujun5@xiaomi.com>

--- include/nuttx/seqlock.h
+++ include/nuttx/seqlock.h
@@ -161,6 +161,7 @@ irqstate_t write_seqlock_irqsave(FAR seqcount_t *s)
   irqstate_t flags = spin_lock_irqsave(&s->lock);
 
   s->sequence++;
+  SMP_WMB();
   return flags;
 }
 
@@ -183,6 +184,7 @@ irqstate_t write_seqlock_irqsave(FAR seqcount_t *s)
 static inline_function
 void write_sequnlock_irqrestore(seqcount_t *s, irqstate_t flags)
 {
+  SMP_WMB();
   s->sequence++;
   spin_unlock_irqrestore(&s->lock, flags);
 }
