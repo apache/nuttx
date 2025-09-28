f3fcdf3035a7212cccb574567421626a6a773a8e
tengshuangshuang <tengshuangshuang@xiaomi.com>
2025-09-28 15:31:13 +0800

    aio:fix fsync crash
    avoid wild pointer for the first time
    
    Signed-off-by: tengshuangshuang <tengshuangshuang@xiaomi.com>

--- fs/aio/aio_fsync.c
+++ fs/aio/aio_fsync.c
@@ -207,6 +207,10 @@ int aio_fsync(int op, FAR struct aiocb *aiocbp)
   sigwork_init(&aiocbp->aio_sigwork);
   aiocbp->aio_result = -EINPROGRESS;
 
+  /* Initialize list_node using aiocbp for the first time */
+
+  list_initialize(&aiocbp->lio_link);
+
   /* Create a container for the AIO control block.  This may cause us to
    * block if there are insufficient resources to satisfy the request.
    */
