4dc185f766f5ba368e758899fb6647af1453331d
raiden00pl <raiden00@railab.me>
2025-10-11 13:08:58 +0200

    mm/: remove reference to non-existent readme
    remove reference to non-existent readme in mm/
    
    Pointing to the documentation page doesn't make sense in this case,
    because it doesn't explain the use of `#undef XXX` for this case anyway.
    
    Signed-off-by: raiden00pl <raiden00@railab.me>

--- mm/umm_heap/umm_malloc_size.c
+++ mm/umm_heap/umm_malloc_size.c
@@ -36,7 +36,7 @@
  * Public Functions
  ****************************************************************************/
 
-#undef malloc_size /* See mm/README.txt */
+#undef malloc_size
 size_t malloc_size(FAR void *mem)
 {
   return mm_malloc_size(USR_HEAP, mem);
