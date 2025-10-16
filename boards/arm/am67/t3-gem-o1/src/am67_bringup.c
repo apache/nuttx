f6acd6ad9f239bc7ecd232a8942b85cdfa073edf
Erkan Vatan <evatan@t3gemstone.org>
2025-10-16 16:52:39 +0300

    boards/arm/am67: Add support for t3-gem-o1 board.
    This commit introduces basic support for the T3 Gemstone O1 (t3-gem-o1)
    development board, including board configuration, linker scripts, and
    drivers for NSH. Currently only UART console is supported.
    All necessary files and configurations are added to enable building and
    running NuttX on this TI AM67-based board.
    
    Co-authored-by: Emre Cecanpunar <emreleno@gmail.com>
    Co-authored-by: Abdullah Türkmen <abdullahturkmen@protonmail.com>
    Co-authored-by: Muhammet Onur Bayraktar <mobayraktar@t3gemstone.org>
    Co-authored-by: Bayram Akay <bakay@t3gemstone.org>
    Signed-off-by: Erkan Vatan <evatan@t3gemstone.org>

---
+++ boards/arm/am67/t3-gem-o1/src/am67_bringup.c
@@ -0,0 +1,66 @@
+/****************************************************************************
+ * boards/arm/am67/t3-gem-o1/src/am67_bringup.c
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+#include <nuttx/fs/fs.h>
+#include <debug.h>
+
+#include "t3-gem-o1.h"
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: am67_bringup
+ *
+ * Description:
+ *   Perform architecture-specific initialization.
+ *
+ *   CONFIG_BOARD_LATE_INITIALIZE=y :
+ *     Called from board_late_initialize().
+ *
+ *   CONFIG_BOARD_LATE_INITIALIZE=n && CONFIG_BOARDCTL=y :
+ *     Called from the NSH library.
+ *
+ ****************************************************************************/
+
+int am67_bringup(void)
+{
+  int ret = OK;
+
+#ifdef CONFIG_FS_PROCFS
+  /* Mount the procfs file system */
+
+  ret = nx_mount(NULL, "/proc", "procfs", 0, NULL);
+  if (ret < 0)
+    {
+      syslog(LOG_ERR, "ERROR: Failed to mount procfs at /proc: %d\n", ret);
+    }
+#endif
+
+  return ret;
+}
