5dfef930110c747933d3512bdf53882d4b0ec244
wangchengdong <wangchengdong@lixiang.com>
2025-10-23 09:00:56 +0800

    sched/event: Replace semaphore with direct scheduler operations in event
        The current event implementation uses semaphores for wait and post
        operations. Since semaphores are relatively heavy-weight and intended
        for resource-based synchronization, this is suboptimal.
    
        So this patch replaced the semaphore-based mechanism with direct
        scheduler operations to improve performance and reduce memory footprint.
    
        This patch also introduce a new task state TSTATE_WAIT_EVENT to indicate
        the task is waiting for a event.
    
    Signed-off-by: Chengdong Wang wangchengdong@lixiang.com

--- include/nuttx/event.h
+++ include/nuttx/event.h
@@ -46,6 +46,7 @@
 #define NXEVENT_WAIT_ALL     (1 << 0) /* Bit 0: Wait ALL */
 #define NXEVENT_WAIT_RESET   (1 << 1) /* Bit 1: Reset events before wait */
 #define NXEVENT_WAIT_NOCLEAR (1 << 2) /* Bit 2: Do not clear events after wait */
+#define NXEVENT_WAIT_TIMEOUT (1 << 3) /* Bit 3: Event wait is timeout */
 
 /* Event Post Flags */
 
@@ -66,7 +67,7 @@ struct nxevent_wait_s
   struct list_node        node;    /* Wait node of current task */
   nxevent_mask_t          expect;  /* Expect events of wait task */
   nxevent_flags_t         eflags;  /* Event flags of wait task */
-  sem_t                   sem;     /* Wait sem of current task */
+  FAR struct tcb_s        *wtcb;   /* The wait task */
 };
 
 struct nxevent_s
