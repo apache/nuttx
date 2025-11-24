5b18435f96fec9ba064791ac981b321aacc5eaca
zhangyu117 <zhangyu117@xiaomi.com>
2025-11-24 11:05:45 +0800

    arch/tricore: Adjust the related structures of arch and chip.
    support tc4evb board, so need adjust some Kconfig
    
    Signed-off-by: zhangyu117 <zhangyu117@xiaomi.com>

--- arch/tricore/src/common/tricore_mpu.c
+++ arch/tricore/src/common/tricore_mpu.c
@@ -66,12 +66,14 @@ static Ifx_CPU_DPRE get_dpre_value(unsigned int set)
       case 5:
         dpre_value.U = __mfcr(CPU_DPRE_5);
         break;
+#ifndef CONFIG_ARCH_CHIP_TC3XX
       case 6:
         dpre_value.U = __mfcr(CPU_DPRE_6);
         break;
       case 7:
         dpre_value.U = __mfcr(CPU_DPRE_7);
         break;
+#endif
       default:
         dpre_value.U = 0;
         break;
@@ -102,12 +104,14 @@ static void set_dpre_value(unsigned int set, Ifx_CPU_DPRE dpre_value)
       case 5:
         __mtcr(CPU_DPRE_5, dpre_value.U);
         break;
+#ifndef CONFIG_ARCH_CHIP_TC3XX
       case 6:
         __mtcr(CPU_DPRE_6, dpre_value.U);
         break;
       case 7:
         __mtcr(CPU_DPRE_7, dpre_value.U);
         break;
+#endif
       default:
         break;
     }
@@ -139,12 +143,14 @@ static Ifx_CPU_DPWE get_dpwe_value(unsigned int set)
       case 5:
         dpwe_value.U = __mfcr(CPU_DPWE_5);
         break;
+#ifndef CONFIG_ARCH_CHIP_TC3XX
       case 6:
         dpwe_value.U = __mfcr(CPU_DPWE_6);
         break;
       case 7:
         dpwe_value.U = __mfcr(CPU_DPWE_7);
         break;
+#endif
       default:
         dpwe_value.U = 0;
         break;
@@ -176,11 +182,13 @@ static void set_dpwe_value(unsigned int set, Ifx_CPU_DPWE dpwe_value)
         __mtcr(CPU_DPWE_5, dpwe_value.U);
         break;
       case 6:
+#ifndef CONFIG_ARCH_CHIP_TC3XX
         __mtcr(CPU_DPWE_6, dpwe_value.U);
         break;
       case 7:
         __mtcr(CPU_DPWE_7, dpwe_value.U);
         break;
+#endif
       default:
         break;
     }
@@ -212,12 +220,14 @@ static Ifx_CPU_CPXE get_cpxe_value(unsigned int set)
       case 5:
         cpxe_value.U = __mfcr(CPU_CPXE_5);
         break;
+#ifndef CONFIG_ARCH_CHIP_TC3XX
       case 6:
         cpxe_value.U = __mfcr(CPU_CPXE_6);
         break;
       case 7:
         cpxe_value.U = __mfcr(CPU_CPXE_7);
         break;
+#endif
       default:
         cpxe_value.U = 0;
         break;
@@ -248,12 +258,14 @@ static void set_cpxe_value(unsigned int set, Ifx_CPU_CPXE cpxe_value)
       case 5:
         __mtcr(CPU_CPXE_5, cpxe_value.U);
         break;
+#ifndef CONFIG_ARCH_CHIP_TC3XX
       case 6:
         __mtcr(CPU_CPXE_6, cpxe_value.U);
         break;
       case 7:
         __mtcr(CPU_CPXE_7, cpxe_value.U);
         break;
+#endif
       default:
         break;
     }
@@ -339,6 +351,7 @@ static int get_dpr_addrass_value(unsigned int region,
         dpr_l->U = __mfcr(CPU_DPR17_L);
         dpr_u->U = __mfcr(CPU_DPR17_U);
         break;
+#ifndef CONFIG_ARCH_CHIP_TC3XX
       case 18:
         dpr_l->U = __mfcr(CPU_DPR18_L);
         dpr_u->U = __mfcr(CPU_DPR18_U);
@@ -363,6 +376,7 @@ static int get_dpr_addrass_value(unsigned int region,
         dpr_l->U = __mfcr(CPU_DPR23_L);
         dpr_u->U = __mfcr(CPU_DPR23_U);
         break;
+#endif
       default:
         return -EINVAL;
     }
@@ -448,6 +462,7 @@ static void set_dpr_address_value(unsigned int region,
         __mtcr(CPU_DPR17_L, dpr_l.U);
         __mtcr(CPU_DPR17_U, dpr_u.U);
         break;
+#ifndef CONFIG_ARCH_CHIP_TC3XX
       case 18:
         __mtcr(CPU_DPR18_L, dpr_l.U);
         __mtcr(CPU_DPR18_U, dpr_u.U);
@@ -472,6 +487,7 @@ static void set_dpr_address_value(unsigned int region,
         __mtcr(CPU_DPR23_L, dpr_l.U);
         __mtcr(CPU_DPR23_U, dpr_u.U);
         break;
+#endif
       default:
         break;
     }
@@ -525,6 +541,7 @@ static int get_cpr_address_region(unsigned int region,
         cpr_l->U = __mfcr(CPU_CPR9_L);
         cpr_u->U = __mfcr(CPU_CPR9_U);
         break;
+#ifndef CONFIG_ARCH_CHIP_TC3XX
       case 10:
         cpr_l->U = __mfcr(CPU_CPR10_L);
         cpr_u->U = __mfcr(CPU_CPR10_U);
@@ -549,6 +566,7 @@ static int get_cpr_address_region(unsigned int region,
         cpr_l->U = __mfcr(CPU_CPR15_L);
         cpr_u->U = __mfcr(CPU_CPR15_U);
         break;
+#endif
       default:
         return -EINVAL;
     }
@@ -602,6 +620,7 @@ static void set_cpr_address_value(unsigned int region,
         __mtcr(CPU_CPR9_L, cpr_l.U);
         __mtcr(CPU_CPR9_U, cpr_u.U);
         break;
+#ifndef CONFIG_ARCH_CHIP_TC3XX
       case 10:
         __mtcr(CPU_CPR10_L, cpr_l.U);
         __mtcr(CPU_CPR10_U, cpr_u.U);
@@ -626,6 +645,7 @@ static void set_cpr_address_value(unsigned int region,
         __mtcr(CPU_CPR15_L, cpr_l.U);
         __mtcr(CPU_CPR15_U, cpr_u.U);
         break;
+#endif
     }
 
   UP_ISB();
