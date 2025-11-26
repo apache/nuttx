56b7d0ed426dd085897f90182a2139b444a3cd19
simbit18 <101105604+simbit18@users.noreply.github.com>
2025-11-26 11:21:19 +0100

    ci/platforms: necessary changes for macos-15-intel  and  macos-14 Apple Silicon
    macos-15-intel  and  macos-14 Apple Silicon
    - bloaty()
       aligned with the latest version
    
    macos-15-intel
     - avr_gcc_toolchain()
         Latest version not available for Intel,
         so it needs to be built from source, which takes over an hour.
         The latest version prebuilt for Intel is 9.4.0
         Added an workaround for installation.
    
    Signed-off-by: simbit18 <simbit18@gmail.com>

--- tools/ci/platforms/darwin_arm64.sh
+++ tools/ci/platforms/darwin_arm64.sh
@@ -130,7 +130,7 @@ bloaty() {
     cd "${NUTTXTOOLS}"/bloaty-src
     # Due to issues with latest MacOS versions use pinned commit.
     # https://github.com/google/bloaty/pull/326
-    git checkout 52948c107c8f81045e7f9223ec02706b19cfa882
+    # git checkout 52948c107c8f81045e7f9223ec02706b19cfa882
     mkdir -p "${NUTTXTOOLS}"/bloaty
     cmake -B build/bloaty -GNinja -D BLOATY_PREFER_SYSTEM_CAPSTONE=NO -D CMAKE_INSTALL_PREFIX="${NUTTXTOOLS}"/bloaty
     cmake --build build/bloaty
