apache/nuttx

commit c61d7c7e8de4406cf98246438caea8973f4dc297

nuttx/can: add message alignment
This adds ability for read and write operations to work with messages
aligned to configured number of bytes. This has few different use
cases.

The alignment is specified as unsigned integer and can be changed with
ioctl command CANIOC_SET_MSGALIGN. The current value can be queried by
CANIOC_GET_MSGALIGN command.

The default value for the message alignment is 1. This will provide
behavior consistent with current one. Thus messages are placed to the
buffer right after data of the previous one. The same applies for
writes.

The special alignment value 0 disables read and write of multiple frames. Thus
read will always return at most one message and write will always write
only one message even if larger buffer size is provided.

Another use case is if message alignment is set to exactly message
representation size (`sizeof(struct can_msg_s)`). This allows writing
and reading arrays of messages.

Other values provide even more advanced and specialized use cases, such
as optimizations if architecture has to emulate some non-aligned
accesses, there alignment of for example 4 bytes could provide
performance boost.

The original motivation behind this is was compatibility with socket
CAN. It is easier to port applications to NuttX's CAN driver if only one
frame is provided at the time. This solution was suggested by Pavel Pisa
<pisa@fel.cvut.cz> as a more versatile variant of plain boolean
disabling the multiple frame retrieval.

Signed-off-by: Karel Kočí <kkoci@elektroline.cz>

drivers/can/can.c
@@ -27,6 +27,7 @@
 #include <nuttx/config.h>
 
 #include <sys/types.h>
+#include <sys/param.h>
 #include <inttypes.h>
 #include <stdint.h>
 #include <stdbool.h>
@@ -193,6 +194,7 @@ static FAR struct can_reader_s *init_can_reader(FAR struct file *filep)
   DEBUGASSERT(reader != NULL);
 
   nxsem_init(&reader->fifo.rx_sem, 0, 0);
+  reader->msgalign = 1;
   filep->f_priv = reader;
 
   return reader;
@@ -380,6 +382,7 @@ static ssize_t can_read(FAR struct file *filep, FAR char *buffer,
 {
   FAR struct can_reader_s *reader;
   FAR struct can_rxfifo_s *fifo;
+  unsigned int             msgalign;
   irqstate_t               flags;
   int                      ret = 0;
 
@@ -395,6 +398,7 @@ static ssize_t can_read(FAR struct file *filep, FAR char *buffer,
       DEBUGASSERT(filep->f_priv != NULL);
       reader = (FAR struct can_reader_s *)filep->f_priv;
       fifo = &reader->fifo;
+      msgalign = reader->msgalign;
 
       /* Interrupts must be disabled while accessing the cd_recv FIFO */
 
@@ -482,6 +486,13 @@ static ssize_t can_read(FAR struct file *filep, FAR char *buffer,
           memcpy(&buffer[ret], msg, msglen);
           ret += msglen;
 
+          if (msgalign > 1)
+            {
+              ret = powerof2(msgalign)
+                ? roundup2(ret, msgalign)
+                : roundup(ret, msgalign);
+            }
+
           /* Increment the head of the circular message buffer */
 
           if (++fifo->rx_head >= CONFIG_CAN_RXFIFOSIZE)
@@ -489,7 +500,7 @@ static ssize_t can_read(FAR struct file *filep, FAR char *buffer,
               fifo->rx_head = 0;
             }
         }
-      while (fifo->rx_head != fifo->rx_tail);
+      while (fifo->rx_head != fifo->rx_tail && msgalign != 0);
 
       if (fifo->rx_head != fifo->rx_tail)
         {
@@ -505,7 +516,9 @@ return_with_irqdisabled:
       leave_critical_section(flags);
     }
 
-  return ret;
+  /* ret can be more than buflen due to roundup, so return at most buflen */
+
+  return ret ? MIN(ret, buflen) : -EMSGSIZE;
 }
 
 /****************************************************************************
@@ -595,9 +608,11 @@ static int can_xmit(FAR struct can_dev_s *dev)
 static ssize_t can_write(FAR struct file *filep, FAR const char *buffer,
                          size_t buflen)
 {
-  FAR struct inode         *inode   = filep->f_inode;
-  FAR struct can_dev_s     *dev     = inode->i_private;
-  FAR struct can_txcache_s *sender  = &dev->cd_sender;
+  FAR struct inode         *inode    = filep->f_inode;
+  FAR struct can_dev_s     *dev      = inode->i_private;
+  FAR struct can_txcache_s *sender   = &dev->cd_sender;
+  FAR struct can_reader_s  *reader   = filep->f_priv;
+  unsigned int              msgalign = reader->msgalign;
   FAR struct can_msg_s     *msg;
   bool                      inactive;
   ssize_t                   nsent   = 0;
@@ -702,6 +717,18 @@ static ssize_t can_write(FAR struct file *filep, FAR const char *buffer,
       /* Increment the number of bytes that were sent */
 
       nsent += msglen;
+
+      if (msgalign > 1)
+        {
+          nsent = powerof2(msgalign)
+            ? roundup2(nsent, msgalign)
+            : roundup(nsent, msgalign);
+        }
+
+      if (msgalign == 0)
+        {
+          break;
+        }
     }
 
   /* We get here after all messages have been added to the sender.  Check if
@@ -713,9 +740,11 @@ static ssize_t can_write(FAR struct file *filep, FAR const char *buffer,
       can_xmit(dev);
     }
 
-  /* Return the number of bytes that were sent */
+  /* Return the number of bytes that were sent, but at most buflen as nsent
+   * can be more due to roundup.
+   */
 
-  ret = nsent;
+  ret = MIN(nsent, buflen);
 
 return_with_irqdisabled:
   leave_critical_section(flags);
@@ -961,6 +990,22 @@ static int can_ioctl(FAR struct file *filep, int cmd, unsigned long arg)
               canerr("dev->cd_transv->cts_ops is NULL!");
               ret = -ENOTTY;
             }
+        }
+        break;
+
+      /* Set message alignment for read and write operations */
+
+      case CANIOC_SET_MSGALIGN:
+        {
+          reader->msgalign = *(FAR unsigned int *)arg;
+        }
+        break;
+
+      /* Get message alignment for read and write operations */
+
+      case CANIOC_GET_MSGALIGN:
+        {
+          *(FAR unsigned int *)arg = reader->msgalign;
         }
         break;
 

include/nuttx/can/can.h
@@ -305,6 +305,31 @@
  *                   is returned with the errno variable set to indicate the
  *                   nature of the error.
  *   Dependencies:   None
+ *
+ * CANIOC_SET_MSGALIGN
+ *   Description:    Set messages alignment. Read and written messages can be
+ *                   configured to be aligned to multiple of given bytes by
+ *                   this. The default value is 1. The alignment affects both
+ *                   read and write operation. The value 0 has a special
+ *                   meaning where both write will always write only a single
+ *                   message and read will always provide only a single
+ *                   message.
+ *
+ *   Argument:       A pointer to an unsigned int type with alignment value.
+ *   returned Value: Zero (OK) is returned on success.  Otherwise -1 (ERROR)
+ *                   is returned with the errno variable set to indicate the
+ *                   nature of the error.
+ *   Dependencies:   None
+ *
+ * CANIOC_GET_MSGALIGN
+ *   Description:    Get messages alignment. See CANIOC_SET_MSGALIGN for
+ *                   explanation.
+ *
+ *   Argument:       A pointer to an unsigned int type for alignment value.
+ *   returned Value: Zero (OK) is returned on success.  Otherwise -1 (ERROR)
+ *                   is returned with the errno variable set to indicate the
+ *                   nature of the error.
+ *   Dependencies:   None
  */
 
 #define CANIOC_RTR                _CANIOC(1)
@@ -326,9 +351,11 @@
 #define CANIOC_GET_STATE          _CANIOC(17)
 #define CANIOC_SET_TRANSVSTATE    _CANIOC(18)
 #define CANIOC_GET_TRANSVSTATE    _CANIOC(19)
+#define CANIOC_SET_MSGALIGN       _CANIOC(20)
+#define CANIOC_GET_MSGALIGN       _CANIOC(21)
 
 #define CAN_FIRST                 0x0001         /* First common command */
-#define CAN_NCMDS                 19             /* 20 common commands   */
+#define CAN_NCMDS                 21             /* 21 common commands   */
 
 /* User defined ioctl commands are also supported. These will be forwarded
  * by the upper-half CAN driver to the lower-half CAN driver via the
@@ -796,13 +823,14 @@ struct can_ops_s
  *
  *   The elements of 'cd_ops', and 'cd_priv'
  *
- * The common logic will initialize all semaphores.
+ * The common logic will initialize all semaphores and set 'msgalign' to '1'.
  */
 
 struct can_reader_s
 {
   struct list_node     list;
   struct can_rxfifo_s  fifo;             /* Describes receive FIFO */
+  unsigned int         msgalign;
   FAR struct pollfd   *cd_fds;
 };
 

commit 20e1a386cd64ef11c52f90f925aa959f84dbdf49

drivers/can/can.c: fix broken O_NONBLOCK
O_NONBLOCK open mode was broken since
https://github.com/apache/nuttx/pull/17360

MIN() comapres signed value (int) with unsigned value (size_t) which causes
an unexpected return value when ret is negative

Signed-off-by: raiden00pl <raiden00@railab.me>

drivers/can/can.c
@@ -518,7 +518,7 @@ return_with_irqdisabled:
 
   /* ret can be more than buflen due to roundup, so return at most buflen */
 
-  return ret ? MIN(ret, buflen) : -EMSGSIZE;
+  return ret ? MIN(ret, (ssize_t)buflen) : -EMSGSIZE;
 }
 
 /****************************************************************************
@@ -744,7 +744,7 @@ static ssize_t can_write(FAR struct file *filep, FAR const char *buffer,
    * can be more due to roundup.
    */
 
-  ret = MIN(nsent, buflen);
+  ret = MIN(nsent, (ssize_t)buflen);
 
 return_with_irqdisabled:
   leave_critical_section(flags);