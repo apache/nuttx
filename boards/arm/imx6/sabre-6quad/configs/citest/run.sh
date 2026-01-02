0721dfd1719ec689442bccccf93c7a846c388359
p-szafonimateusz <p-szafonimateusz@xiaomi.com>
2026-01-02 13:57:34 +0100

    boards/sabre-6quad/citest: migrate to NTFC
    migrate sabre-6quad/citest to NTFC test cases
    
    Signed-off-by: p-szafonimateusz <p-szafonimateusz@xiaomi.com>

--- boards/arm/imx6/sabre-6quad/configs/citest/run.sh
+++ boards/arm/imx6/sabre-6quad/configs/citest/run.sh
@@ -1,1 +1,55 @@
-../../../../../../tools/ci/cirun.sh
+#!/usr/bin/env bash
+############################################################################
+# boards/arm/imx6/sabre-6quad/configs/citest/run.sh
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.  The
+# ASF licenses this file to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance with the
+# License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+############################################################################
+
+set -o xtrace
+
+# start from nuttx dir
+olddir=$(pwd)
+nuttdir=${CURRENTCONFDIR}/../../../../../../
+cd ${nuttdir}
+
+# enable venv
+source ${NTFCDIR}/venv/bin/activate
+
+# run NTFC
+confpath=${CURRENTCONFDIR}/config.yaml
+jsonconf=${CURRENTCONFDIR}/session.json
+testpath=${NTFCDIR}/external/nuttx-testing
+python3 -m ntfc test --testpath=${testpath} --confpath=${confpath} --jsonconf=${jsonconf}
+
+ret="$?"
+echo $ret
+
+# disable venv
+deactivate
+
+# export test results
+artifacts=${ARTIFACTCONFDIR}/ntfc
+mkdir -p ${artifacts}
+mv pytest.debug.log ${artifacts}
+mv result ${artifacts}
+
+# restore old dir
+cd ${olddir}
+
+exit $ret
