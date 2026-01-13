7a9888bcdf3d7b25a21c1bfc36197dd2475a7cc6
Bartosz Wawrzynek <bartosz.wawrzynek@glamox.com>
2026-01-13 08:15:27 +0100

    boards/riscv/: Change _einit section
    This commit will change _einit section for esp32 mcu's for proper cxx initialization.
    
    Signed-off-by: Bartosz <bartol2205@gmail.com>

--- libs/libc/misc/lib_cxx_initialize.c
+++ libs/libc/misc/lib_cxx_initialize.c
@@ -42,35 +42,6 @@
 extern void macho_call_saved_init_funcs(void);
 #endif
 
-/* ESP32 platforms have linker scripts that place non-constructor data
- * (soc_reserved_memory_region, esp_system_init_fn_array) between _sinit
- * and _einit. This requires explicit handling to avoid calling invalid
- * function pointers.
- *
- * ESP32 RISC-V chips: Have __init_priority_array and __init_array
- * ESP32 Xtensa chips: Have only __init_array (using .ctors)
- */
-
-#if defined(CONFIG_ARCH_CHIP_ESP32) || defined(CONFIG_ARCH_CHIP_ESP32S2) ||    \
-    defined(CONFIG_ARCH_CHIP_ESP32S3)
-
-/* Xtensa ESP32 chips: Only __init_array exists */
-
-extern initializer_t __init_array_start[];
-extern initializer_t __init_array_end[];
-#define USE_ESP32_XTENSA_INIT_ARRAYS 1
-#elif defined(CONFIG_ARCH_CHIP_ESP32C3) ||                                     \
-    defined(CONFIG_ARCH_CHIP_ESP32C6) || defined(CONFIG_ARCH_CHIP_ESP32H2)
-
-/* RISC-V ESP32 chips: Both __init_priority_array and __init_array exist */
-
-extern initializer_t __init_priority_array_start[];
-extern initializer_t __init_priority_array_end[];
-extern initializer_t __init_array_start[];
-extern initializer_t __init_array_end[];
-#define USE_ESP32_RISCV_INIT_ARRAYS 1
-#endif
-
 /****************************************************************************
  * Public Functions
  ****************************************************************************/
@@ -91,99 +62,36 @@ extern initializer_t __init_array_end[];
  *
  ****************************************************************************/
 
-void lib_cxx_initialize(void)
-{
+void lib_cxx_initialize(void) {
 #ifdef CONFIG_HAVE_CXXINITIALIZE
   static int inited = 0;
 
-  if (inited == 0)
-    {
+  if (inited == 0) {
 #if defined(CONFIG_ARCH_SIM) && defined(CONFIG_HOST_MACOS)
-      macho_call_saved_init_funcs();
-#elif defined(USE_ESP32_XTENSA_INIT_ARRAYS)
-      initializer_t *initp;
-
-      /* ESP32 Xtensa platforms: Only iterate __init_array, skipping
-       * non-constructor data after it.
-       */
-
-      sinfo("__init_array: %p to %p\n", __init_array_start,
-            __init_array_end);
-
-      for (initp = __init_array_start; initp < __init_array_end; initp++)
-        {
-          initializer_t initializer = *initp;
-          sinfo("initp: %p initializer: %p\n", initp, initializer);
-
-          if (initializer)
-            {
-              sinfo("Calling %p\n", initializer);
-              initializer();
-            }
-        }
-#elif defined(USE_ESP32_RISCV_INIT_ARRAYS)
-      initializer_t *initp;
-
-      /* ESP32 RISC-V platforms: Iterate through priority array first,
-       * then regular array, skipping the non-constructor data sections
-       * in between.
-       */
-
-      sinfo("__init_priority_array: %p to %p\n",
-            __init_priority_array_start, __init_priority_array_end);
-
-      for (initp = __init_priority_array_start;
-           initp < __init_priority_array_end; initp++)
-        {
-          initializer_t initializer = *initp;
-          sinfo("priority initp: %p initializer: %p\n", initp, initializer);
-
-          if (initializer)
-            {
-              sinfo("Calling %p\n", initializer);
-              initializer();
-            }
-        }
-
-      sinfo("__init_array: %p to %p\n", __init_array_start,
-            __init_array_end);
-
-      for (initp = __init_array_start; initp < __init_array_end; initp++)
-        {
-          initializer_t initializer = *initp;
-          sinfo("initp: %p initializer: %p\n", initp, initializer);
-
-          if (initializer)
-            {
-              sinfo("Calling %p\n", initializer);
-              initializer();
-            }
-        }
+    macho_call_saved_init_funcs();
 #else
-      initializer_t *initp;
+    initializer_t *initp;
 
-      sinfo("_sinit: %p _einit: %p\n", _sinit, _einit);
+    sinfo("_sinit: %p _einit: %p\n", _sinit, _einit);
 
-      /* Visit each entry in the initialization table */
+    /* Visit each entry in the initialization table */
 
-      for (initp = _sinit; initp < _einit; initp++)
-        {
-          initializer_t initializer = *initp;
-          sinfo("initp: %p initializer: %p\n", initp, initializer);
+    for (initp = _sinit; initp < _einit; initp++) {
+      initializer_t initializer = *initp;
+      sinfo("initp: %p initializer: %p\n", initp, initializer);
 
-          /* Make sure that the address is non-NULL. Some toolchains
-           * may put NULL values or counts in the initialization table.
-           */
+      /* Make sure that the address is non-NULL. Some toolchains
+       * may put NULL values or counts in the initialization table.
+       */
 
-          if (initializer)
-            {
-              sinfo("Calling %p\n", initializer);
-              initializer();
-            }
-        }
+      if (initializer) {
+        sinfo("Calling %p\n", initializer);
+        initializer();
+      }
+    }
 #endif
 
-      inited = 1;
-    }
+    inited = 1;
+  }
 #endif
 }
