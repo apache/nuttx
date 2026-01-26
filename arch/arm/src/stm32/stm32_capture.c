d57899847aa1537d36dd4a3fb0a5e3c626c7b9d6
Alexey Matveev <tippet@yandex.ru>
2026-01-26 19:32:11 +0300

    arch/stm32: Add counter reset function
    Added stm32_cap_rstcounter() function to reset the capture counter
    by generating an update event. The function is integrated into the
    capture operations structure and exposed via STM32_CAP_RSTCOUNTER
    macro. Also added TI3 and TI4 input mappings for extended capture
    channel support.
    
    Signed-off-by: Alexey Matveev <tippet@yandex.ru>

--- arch/arm/src/stm32/stm32_capture.c
+++ arch/arm/src/stm32/stm32_capture.c
@@ -1110,8 +1110,8 @@ static int stm32_cap_setchannel(struct stm32_cap_dev_s *dev,
   switch (cfg & STM32_CAP_EDGE_MASK)
     {
       case STM32_CAP_EDGE_DISABLED:
-        regval = 0;
         ccer_en_bit = 0;
+        regval = 0;
         break;
 
       case STM32_CAP_EDGE_RISING:
@@ -1234,6 +1234,14 @@ static uint32_t stm32_cap_getcapture(struct stm32_cap_dev_s *dev,
   return stm32_getreg16(priv, offset);
 }
 
+static uint32_t stm32_cap_rstcounter(struct stm32_cap_dev_s *dev)
+{
+  const struct stm32_cap_priv_s *priv = (const struct stm32_cap_priv_s *)dev;
+
+  stm32_modifyreg16(priv, STM32_BTIM_EGR_OFFSET, 0, BTIM_EGR_UG);
+  return OK;
+}
+
 /****************************************************************************
  * Advanced Functions
  ****************************************************************************/
@@ -1253,7 +1261,8 @@ struct stm32_cap_ops_s stm32_cap_ops =
   .setisr       = &stm32_cap_setisr,
   .enableint    = &stm32_cap_enableint,
   .ackflags     = &stm32_cap_ackflags,
-  .getflags     = &stm32_cap_getflags
+  .getflags     = &stm32_cap_getflags,
+  .rstcounter   = &stm32_cap_rstcounter,
 };
 
 #ifdef CONFIG_STM32_TIM1_CAP
