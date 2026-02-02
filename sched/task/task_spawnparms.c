a60fd4cf975cb84da05aa1bc8fab5a42c80bbbcf
fangxinyong <fangxinyong@xiaomi.com>
2026-02-02 16:20:42 +0800

    sched/spawn: Fix MISRA C 2012 Rule 10.4 violations
    Fix violations of MISRA C:2012 Rule 10.4 (essential type operand of
    unsigned and signed) in posix_spawn attribute handling code.
    
    Changed all POSIX_SPAWN_* flag macro definitions to use unsigned literals
    (1u instead of 1) to ensure consistent unsigned arithmetic when performing
    bitwise operations. This eliminates mixed signed/unsigned operand violations
    in the following flags:
    
    - POSIX_SPAWN_RESETIDS
    - POSIX_SPAWN_SETPGROUP
    - POSIX_SPAWN_SETSCHEDPARAM
    - POSIX_SPAWN_SETSCHEDULER
    - POSIX_SPAWN_SETSIGDEF
    - POSIX_SPAWN_SETSIGMASK
    - POSIX_SPAWN_SETSID
    
    Updated all flag checking comparisons in spawn_execattrs() to compare
    against 0u instead of 0 for consistency.
    
    Signed-off-by: fangxinyong <fangxinyong@xiaomi.com>

--- sched/task/task_spawnparms.c
+++ sched/task/task_spawnparms.c
@@ -158,7 +158,7 @@ int spawn_execattrs(pid_t pid, FAR const posix_spawnattr_t *attr)
   /* Firstly, set the signal mask if requested to do so */
 
 #ifndef CONFIG_DISABLE_ALL_SIGNALS
-  if ((attr->flags & POSIX_SPAWN_SETSIGMASK) != 0)
+  if ((attr->flags & POSIX_SPAWN_SETSIGMASK) != 0u)
     {
       FAR struct tcb_s *tcb = nxsched_get_tcb(pid);
       if (tcb)
@@ -172,7 +172,7 @@ int spawn_execattrs(pid_t pid, FAR const posix_spawnattr_t *attr)
    * to set the priority of the of the new task.
    */
 
-  if ((attr->flags & POSIX_SPAWN_SETSCHEDPARAM) != 0)
+  if ((attr->flags & POSIX_SPAWN_SETSCHEDPARAM) != 0u)
     {
 #ifdef CONFIG_SCHED_SPORADIC
       /* Get the current sporadic scheduling parameters.  Those will not be
@@ -192,7 +192,7 @@ int spawn_execattrs(pid_t pid, FAR const posix_spawnattr_t *attr)
            * then we will call nxsched_set_scheduler() below.
            */
 
-          if ((attr->flags & POSIX_SPAWN_SETSCHEDULER) == 0)
+          if ((attr->flags & POSIX_SPAWN_SETSCHEDULER) == 0u)
             {
               sinfo("Setting priority=%d for pid=%d\n",
                     param.sched_priority, pid);
@@ -207,7 +207,7 @@ int spawn_execattrs(pid_t pid, FAR const posix_spawnattr_t *attr)
    * preparation for the nxsched_set_scheduler() call below.
    */
 
-  else if ((attr->flags & POSIX_SPAWN_SETSCHEDULER) != 0)
+  else if ((attr->flags & POSIX_SPAWN_SETSCHEDULER) != 0u)
     {
       ret = nxsched_get_param(0, &param);
     }
@@ -216,7 +216,7 @@ int spawn_execattrs(pid_t pid, FAR const posix_spawnattr_t *attr)
    * setting determined above.
    */
 
-  if (ret == OK && (attr->flags & POSIX_SPAWN_SETSCHEDULER) != 0)
+  if (ret == OK && (attr->flags & POSIX_SPAWN_SETSCHEDULER) != 0u)
     {
       sinfo("Setting policy=%d priority=%d for pid=%d\n",
             attr->policy, param.sched_priority, pid);
