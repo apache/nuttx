5730b7b8d13ab92e18ea9db0e1a957d7b5666b19
Matteo Golin <matteo.golin@gmail.com>
2026-02-18 23:39:17 -0500

    boards/xtensa: Replace board_app_initialize
    Replaced board_app_initialize logic with board_late_initialize.
    
    Signed-off-by: Matteo Golin <matteo.golin@gmail.com>

--- boards/xtensa/esp32s3/common/src/esp32s3_usbmsc.c
+++ boards/xtensa/esp32s3/common/src/esp32s3_usbmsc.c
@@ -53,10 +53,5 @@
 
 int board_usbmsc_initialize(int port)
 {
-  /* If system/usbmsc is built as an NSH command, then SD slot should
-   * already have been initialized in board_app_initialize()
-   * In this case, there is nothing further to be done here.
-   */
-
   return OK;
 }
