3c9692d8b40f6aa79fd4036d6023c40fe31c4ed4
Matteo Golin <matteo.golin@gmail.com>
2026-02-18 22:48:56 -0500

    boards/x86_64: Replace board_app_initialize
    Replaced board_app_initialize logic with board_late_initialize.
    
    Signed-off-by: Matteo Golin <matteo.golin@gmail.com>

--- boards/x86_64/qemu/qemu-intel64/src/qemu_boardinit.c
+++ boards/x86_64/qemu/qemu-intel64/src/qemu_boardinit.c
@@ -1,5 +1,5 @@
 /****************************************************************************
- * boards/x86_64/qemu/qemu-intel64/src/qemu_appinit.c
+ * boards/x86_64/qemu/qemu-intel64/src/qemu_boardinit.c
  *
  * SPDX-License-Identifier: Apache-2.0
  *
@@ -55,44 +55,6 @@
  ****************************************************************************/
 
 /****************************************************************************
- * Name: board_app_initialize
- *
- * Description:
- *   Perform application specific initialization.  This function is never
- *   called directly from application code, but only indirectly via the
- *   (non-standard) boardctl() interface using the command BOARDIOC_INIT.
- *
- * Input Parameters:
- *   arg - The boardctl() argument is passed to the board_app_initialize()
- *         implementation without modification.  The argument has no
- *         meaning to NuttX; the meaning of the argument is a contract
- *         between the board-specific initialization logic and the
- *         matching application logic.  The value could be such things as a
- *         mode enumeration value, a set of DIP switch switch settings, a
- *         pointer to configuration data read from a file or serial FLASH,
- *         or whatever you would like to do with it.  Every implementation
- *         should accept zero/NULL as a default configuration.
- *
- * Returned Value:
- *   Zero (OK) is returned on success; a negated errno value is returned on
- *   any failure to indicate the nature of the failure.
- *
- ****************************************************************************/
-
-int board_app_initialize(uintptr_t arg)
-{
-#ifdef CONFIG_BOARD_LATE_INITIALIZE
-  /* Board initialization already performed by board_late_initialize() */
-
-  return OK;
-#else
-  /* Perform board-specific initialization */
-
-  return qemu_bringup();
-#endif
-}
-
-/****************************************************************************
  * Name: board_late_initialize
  *
  * Description:
@@ -111,6 +73,7 @@ int board_app_initialize(uintptr_t arg)
  *
  ****************************************************************************/
 
+#ifdef CONFIG_BOARD_LATE_INITIALIZE
 void board_late_initialize(void)
 {
   int ret = OK;
@@ -137,3 +100,4 @@ void board_late_initialize(void)
         }
     }
 }
+#endif
