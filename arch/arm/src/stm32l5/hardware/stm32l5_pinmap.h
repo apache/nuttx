7f70e9a6b27d33b15bcf6250ea94b7f007c71641
raiden00pl <raiden00@railab.me>
2026-04-23 19:45:55 +0200

    !arch/arm/src/stm32l5: remove STM32L5_USE_LEGACY_PINMAP
    BREAKING CHANGE: remove STM32L5_USE_LEGACY_PINMAP and all references
    
    Signed-off-by: raiden00pl <raiden00@railab.me>

--- arch/arm/src/stm32l5/hardware/stm32l5_pinmap.h
+++ arch/arm/src/stm32l5/hardware/stm32l5_pinmap.h
@@ -31,11 +31,7 @@
 #include "chip.h"
 
 #if defined(CONFIG_STM32L5_STM32L562XX)
-#  if defined(CONFIG_STM32L5_USE_LEGACY_PINMAP)
-#    include "hardware/stm32l562xx_pinmap_legacy.h"
-#  else
-#    include "hardware/stm32l562xx_pinmap.h"
-#  endif
+#  include "hardware/stm32l562xx_pinmap.h"
 #else
 #  error "Unsupported STM32 L5 pin map"
 #endif
