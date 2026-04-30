37c89a2f9f1cef5f744ec4b97eb5835b2920ea64
Arjav Patel <arjav1528@gmail.com>
2026-05-01 02:19:01 +0530

    arch/xtensa/espressif: Add CMake support for ESP32 Xtensa chips.
    Add a shared Espressif Xtensa CMake layer and chip-level CMake entry points
    for ESP32, ESP32-S2, and ESP32-S3 to mirror existing Make-based bring-up.
    This enables CMake builds to resolve common Espressif arch and HAL wiring.
    
    Signed-off-by: Arjav Patel <arjav1528@gmail.com>

---
+++ arch/xtensa/src/esp32s2/hal_esp32s2.cmake
@@ -0,0 +1,8 @@
+# ##############################################################################
+# arch/xtensa/src/esp32s2/hal_esp32s2.cmake
+#
+# SPDX-License-Identifier: Apache-2.0
+# ##############################################################################
+
+set(CHIP_SERIES esp32s2)
+include(${NUTTX_ARCH_ABS_DIR}/src/common/espressif/hal_xtensa_espressif.cmake)
