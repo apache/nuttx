52922288ef159c0d1d03811e39144add5f3498de
raiden00pl <raiden00@railab.me>
2026-05-25 22:44:13 +0200

    !arch/stm32: move stm32g4 to its own directory
    BREAKING CHANGE:
    
    Part of splitting the legacy stm32 super-directory; relocates
    the stm32g4 sources, headers and boards into arch/arm/src/stm32g4,
    arch/arm/include/stm32g4 and boards/arm/stm32g4.
    
    Signed-off-by: raiden00pl <raiden00@railab.me>

---
+++ arch/arm/src/stm32g4/hardware/stm32_memorymap.h
@@ -0,0 +1,17 @@
+/****************************************************************************
+ * arch/arm/src/stm32g4/hardware/stm32_memorymap.h
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ ****************************************************************************/
+
+#ifndef __ARCH_ARM_SRC_STM32G4_HARDWARE_STM32_MEMORYMAP_H
+#define __ARCH_ARM_SRC_STM32G4_HARDWARE_STM32_MEMORYMAP_H
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include "hardware/stm32g4xxxx_memorymap.h"
+
+#endif /* __ARCH_ARM_SRC_STM32G4_HARDWARE_STM32_MEMORYMAP_H */
---
+++ arch/arm/src/stm32g4/hardware/stm32_pinmap.h
@@ -0,0 +1,17 @@
+/****************************************************************************
+ * arch/arm/src/stm32g4/hardware/stm32_pinmap.h
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ ****************************************************************************/
+
+#ifndef __ARCH_ARM_SRC_STM32G4_HARDWARE_STM32_PINMAP_H
+#define __ARCH_ARM_SRC_STM32G4_HARDWARE_STM32_PINMAP_H
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include "hardware/stm32g4xxxx_pinmap.h"
+
+#endif /* __ARCH_ARM_SRC_STM32G4_HARDWARE_STM32_PINMAP_H */
