6d6bcbfcbaa3c0073551ddc9b90fed2aaf78c178
raiden00pl <raiden00@railab.me>
2026-05-25 22:43:32 +0200

    !arch/stm32f0l0g0: move stm32g0 to its own directory
    BREAKING CHANGE:
    
    Part of splitting the legacy stm32 super-directory; relocates
    the stm32g0 sources, headers and boards into arch/arm/src/stm32g0,
    arch/arm/include/stm32g0 and boards/arm/stm32g0.
    
    Signed-off-by: raiden00pl <raiden00@railab.me>

---
+++ arch/arm/include/stm32g0/chip.h
@@ -0,0 +1,166 @@
+/****************************************************************************
+ * arch/arm/include/stm32g0/chip.h
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+#ifndef __ARCH_ARM_INCLUDE_STM32G0_CHIP_H
+#define __ARCH_ARM_INCLUDE_STM32G0_CHIP_H
+
+#define ARMV6M_PERIPHERAL_INTERRUPTS 32
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+/****************************************************************************
+ * Pre-processor Prototypes
+ ****************************************************************************/
+
+/* Get customizations for each supported chip */
+
+#if defined(CONFIG_ARCH_CHIP_STM32G070KB) || defined(CONFIG_ARCH_CHIP_STM32G070CB) || \
+      defined(CONFIG_ARCH_CHIP_STM32G070RB)
+
+#  define STM32_FLASH_SIZE     (128 * 1024) /* 128Kb */
+#  define STM32_SRAM_SIZE      (32 * 1024)  /* 32Kb */
+
+#  define STM32_NATIM           1  /* One advanced timer TIM1 */
+#  define STM32_NGTIM16         5  /* 16-bit general up/down timers TIM3,
+                                    * TIM14-17 */
+#  define STM32_NGTIM32         0  /* No 32-bit general up/down timers */
+#  define STM32_NBTIM           2  /* Two basic timers: TIM6, TIM7 */
+#  define STM32_NSPI            2  /* Two SPI modules SPI1-2 */
+#  define STM32_NI2S            1  /* One I2S module (SPI or I2S) */
+#  define STM32_NI2C            2  /* Two I2C (1 with SMBus/PMBus) */
+#  define STM32_NDMA            1  /* One DMA1, 7-channels */
+#  define STM32_NUSART          4  /* Four USART modules, USART1-4 */
+#  define STM32_NCAN            0  /* No CAN controllers */
+#  define STM32_NLCD            0  /* No LCD */
+#  define STM32_NUSBDEV         0  /* No USB full-speed device controller */
+#  define STM32_NUSBOTG         0  /* No USB OTG */
+#  define STM32_NCEC            0  /* One HDMI-CEC controller */
+#  define STM32_NADC            1  /* (1) ADC1, 16-channels */
+
+#  define STM32_NDAC            0  /* No DAC */
+#  define STM32_NCOMP           0  /* No Analog Comparators */
+#  define STM32_NCRC            1  /* No CRC module */
+#  define STM32_NRNG            0  /* No Random number generator (RNG) */
+#  define STM32_NCAP            0  /* No Capacitive sensing channels */
+#  define STM32_NPORTS          6  /* Six GPIO ports, GPIOA-F */
+
+#elif defined(CONFIG_ARCH_CHIP_STM32G071EB)   || defined(CONFIG_ARCH_CHIP_STM32G071G8)   || \
+      defined(CONFIG_ARCH_CHIP_STM32G071GB)   || defined(CONFIG_ARCH_CHIP_STM32G071G8XN) || \
+      defined(CONFIG_ARCH_CHIP_STM32G071GBXN) || defined(CONFIG_ARCH_CHIP_STM32G071K8)   || \
+      defined(CONFIG_ARCH_CHIP_STM32G071KB)   || defined(CONFIG_ARCH_CHIP_STM32G071K8XN) || \
+      defined(CONFIG_ARCH_CHIP_STM32G071KBXN) || defined(CONFIG_ARCH_CHIP_STM32G071C8)   || \
+      defined(CONFIG_ARCH_CHIP_STM32G071CB)   || defined(CONFIG_ARCH_CHIP_STM32G071R8)   || \
+      defined(CONFIG_ARCH_CHIP_STM32G071RB)
+
+#  define STM32_NATIM           1  /* One advanced timer TIM1 */
+#  define STM32_NGTIM16         4  /* 16-bit general up/down timers TIM2-3
+                                    * (with DMA) and TIM21-22 without DMA */
+#  define STM32_NGTIM32         0  /* No 32-bit general up/down timers */
+#  define STM32_NBTIM           2  /* Two basic timers: TIM6, TIM7 with DMA */
+                                   /* Two LPTIMER */
+#  define STM32_NSPI            2  /* Two SPI modules SPI1-2 */
+#  define STM32_NI2C            2  /* Two I2C (2 with SMBus/PMBus) */
+#  define STM32_NDMA            1  /* One DMA1, 7-channels */
+#  define STM32_NUSART          4  /* Four USART modules, USART1-4 */
+                                   /* One LPUART */
+#  define STM32_NCAN            0  /* No CAN controllers */
+#  define STM32_NLCD            0  /* No LCD */
+#  define STM32_NUSBDEV         0  /* No USB full-speed device controller */
+#  define STM32_NUSBOTG         0  /* No USB OTG */
+#  define STM32_NCEC            1  /* One HDMI-CEC controller */
+#  define STM32_NADC            1  /* (1) ADC1, 12-channels */
+
+#  define STM32_NDAC            2  /* Two DAC channels */
+#  define STM32_NCOMP           2  /* Two Analog Comparators */
+#  define STM32_NCRC            0  /* No CRC module */
+#  define STM32_NRNG            0  /* No Random number generator (RNG) */
+#  define STM32_NCAP            0  /* No Capacitive sensing channels */
+#  define STM32_NPORTS          6  /* Six GPIO ports, GPIOA-F */
+
+#elif defined(CONFIG_ARCH_CHIP_STM32G0B1KB) || defined(CONFIG_ARCH_CHIP_STM32G0B1CB) || \
+      defined(CONFIG_ARCH_CHIP_STM32G0B1RB) || defined(CONFIG_ARCH_CHIP_STM32G0B1MB) || \
+      defined(CONFIG_ARCH_CHIP_STM32G0B1VB) || defined(CONFIG_ARCH_CHIP_STM32G0B1KC) || \
+      defined(CONFIG_ARCH_CHIP_STM32G0B1CC) || defined(CONFIG_ARCH_CHIP_STM32G0B1RC) || \
+      defined(CONFIG_ARCH_CHIP_STM32G0B1MC) || defined(CONFIG_ARCH_CHIP_STM32G0B1VC) || \
+      defined(CONFIG_ARCH_CHIP_STM32G0B1KE) || defined(CONFIG_ARCH_CHIP_STM32G0B1CE) || \
+      defined(CONFIG_ARCH_CHIP_STM32G0B1RE) || defined(CONFIG_ARCH_CHIP_STM32G0B1NE) || \
+      defined(CONFIG_ARCH_CHIP_STM32G0B1ME) || defined(CONFIG_ARCH_CHIP_STM32G0B1VE)
+
+#  define STM32_NATIM           1  /* One advanced timer TIM1 */
+#  define STM32_NGTIM16         6  /* 16-bit general purpose timers */
+#  define STM32_NGTIM32         1  /* TIM2 */
+#  define STM32_NBTIM           2  /* Two basic timers: TIM6, TIM7 with DMA */
+                                   /* One LPTIMER */
+#  define STM32_NSPI            3  /* Two SPI modules SPI1-2 */
+#  define STM32_NI2C            3  /* Two I2C (2 with SMBus/PMBus) */
+#  define STM32_NDMA            2  /* DMA1 7-channels, DMA2 5-channels DMA1 */
+#  define STM32_NUSART          6  /* Six USART modules, USART1-6 */
+                                   /* Two LPUART */
+#  define STM32_NCAN            1  /* One FDCAN controller */
+#  define STM32_NLCD            0  /* No LCD */
+#  define STM32_NUSBDEV         1  /* One USB full-speed controller */
+#  define STM32_NUSBOTG         0  /* No USB OTG */
+#  define STM32_NCEC            1  /* One HDMI-CEC controller */
+#  define STM32_NADC            1  /* (1) ADC1, 12-channels */
+
+#  define STM32_NDAC            2  /* Two DAC channels */
+#  define STM32_NCOMP           3  /* Three Analog Comparators */
+#  define STM32_NCRC            0  /* No CRC module */
+#  define STM32_NRNG            1  /* One Random number generator (RNG) */
+#  define STM32_NCAP            0  /* No Capacitive sensing channels */
+#  define STM32_NPORTS          6  /* Six GPIO ports, GPIOA-F */
+
+/* STM32L EnergyLite Line ***************************************************/
+
+/* STM32L073XX  - With LCD
+ * STM32L072XX  - No LCD
+ * STM32L071XX  - Access line, no LCD
+ *
+ * STM32L0XXX8 - 64KB FLASH, 20KB SRAM, 3KB EEPROM
+ * STM32L0XXXB - 128KB FLASH, 20KB SRAM, 6KB EEPROM
+ * STM32L0XXXZ - 192KB FLASH, 20KB SRAM, 3KB EEPROM
+ *
+ * STM32L0XXCX - 48-pins
+ * STM32L0XXRX - 64-pins
+ * STM32L0XXVX - 100-pins
+ */
+
+#endif
+
+/* NVIC priority levels *****************************************************/
+
+/* Each priority field holds a priority value, 0-31. The lower the value,
+ * the greater the priority of the corresponding interrupt.  The processor
+ * implements only bits[7:6] of each field, bits[5:0] read as zero and
+ * ignore writes.
+ */
+
+#define NVIC_SYSH_PRIORITY_MIN     0xc0 /* All bits[7:6] set is minimum priority */
+#define NVIC_SYSH_PRIORITY_DEFAULT 0x80 /* Midpoint is the default */
+#define NVIC_SYSH_PRIORITY_MAX     0x00 /* Zero is maximum priority */
+#define NVIC_SYSH_PRIORITY_STEP    0x40 /* Two bits of interrupt priority used */
+
+#endif /* __ARCH_ARM_INCLUDE_STM32G0_CHIP_H */
---
+++ boards/arm/stm32g0/nucleo-g070rb/include/board.h
@@ -0,0 +1,246 @@
+/****************************************************************************
+ * boards/arm/stm32g0/nucleo-g070rb/include/board.h
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+#ifndef __BOARDS_ARM_STM32F0L0G0_NUCLEO_G070RB_INCLUDE_BOARD_H
+#define __BOARDS_ARM_STM32F0L0G0_NUCLEO_G070RB_INCLUDE_BOARD_H
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+#ifndef __ASSEMBLY__
+#  include <stdint.h>
+#  include <stdbool.h>
+#endif
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+/* Clocking *****************************************************************/
+
+/* HSI - Internal 16 MHz RC Oscillator
+ * LSI - 32 KHz RC
+ * HSE - 8 MHz from MCO output of ST-LINK (disabled by default)
+ * LSE - 32.768 kHz
+ */
+
+#define STM32_BOARD_XTAL        8000000ul          /* 8MHz */
+
+#define STM32_HSI_FREQUENCY     16000000ul         /* 16MHz */
+#define STM32_LSI_FREQUENCY     32000              /* Between 30kHz and 60kHz */
+#define STM32_HSE_FREQUENCY     STM32_BOARD_XTAL
+#define STM32_LSE_FREQUENCY     32768              /* X2 on board */
+
+/* Main PLL Configuration.
+ *
+ * PLL_VCO = (PLL_SOURCE_FREQUENCY / PLL_M) * PLL_N
+ * SYSCLK  = PLLCLK = PLLR = PLL_VCO / PLL_DIV_R
+ * PLLP    = PLL_VCO / PLL_DIV_P
+ *
+ * Subject to:
+ *
+ * PLL_SOURCE_FREQUENCY is one of {STM32_HSE_FREQUENCY, STM32_HSI_FREQUENCY}
+ *
+ *       1 <= PLL_DIV_M <= 8
+ *       8 <= PLL_DIV_N <= 86
+ *       2 <= PLL_DIV_P <= 32
+ *       2 <= PLL_DIV_R <= 8
+ *   4 MHz <= PLL_SOURCE_FREQUENCY <= 48 MHz
+ *  96 MHz <= PLL_VCO <= 344MHz
+ */
+
+/* Considering:
+ * - PLL_SOURCE_FREQUENCY = STM32_HSI_FREQUENCY = 16,000,000
+ * - PLL_DIV_M = 1
+ * - PLL_DIV_N = 8
+ * - PLL_DIV_R = 2
+ * - PLL_DIV_P = 2
+ *
+ *   PLL_VCO = (16,000,000 / 1) * 8 = 128 MHz
+ *   PLLP    =    (PLL_VCO / 2)     = 64 MHz
+ *   PLLR    =    (PLL_VCO / 2)     = 64 MHz
+ */
+
+#define STM32_PLLCFG_PLLSRC     RCC_PLLCFG_PLLSRC_HSI
+#define STM32_PLLCFG_PLLCFG     (RCC_PLLCFG_PLLPEN | \
+                                 RCC_PLLCFG_PLLREN)
+
+#define STM32_PLLCFG_PLLM       RCC_PLLCFG_PLLM(1)
+#define STM32_PLLCFG_PLLN       RCC_PLLCFG_PLLN(8)
+#define STM32_PLLCFG_PLLP       RCC_PLLCFG_PLLP(2)
+#define STM32_PLLCFG_PLLR       RCC_PLLCFG_PLLR(2)
+
+#define STM32_VCO_FREQUENCY     ((STM32_HSI_FREQUENCY / 1) * 8)
+#define STM32_PLLP_FREQUENCY    (STM32_VCO_FREQUENCY / 2)
+#define STM32_PLLR_FREQUENCY    (STM32_VCO_FREQUENCY / 2)
+
+/* Use the PLL and set the SYSCLK source to be the PLLR (64 MHz) */
+
+#define STM32_SYSCLK_SW         RCC_CFGR_SW_PLL
+#define STM32_SYSCLK_SWS        RCC_CFGR_SWS_PLL
+#define STM32_SYSCLK_FREQUENCY  (STM32_PLLR_FREQUENCY)
+
+/* AHB clock (HCLK) is SYSCLK (64 MHz) */
+
+#define STM32_RCC_CFGR_HPRE     RCC_CFGR_HPRE_SYSCLK
+#define STM32_HCLK_FREQUENCY    STM32_SYSCLK_FREQUENCY
+
+/* APB1 clock (PCLK1) is HCLK (64 MHz) */
+
+#define STM32_RCC_CFGR_PPRE1    RCC_CFGR_PPRE1_HCLK
+#define STM32_PCLK1_FREQUENCY   STM32_HCLK_FREQUENCY
+
+/* Timer clock frequencies */
+
+/* Timers driven from APB1. Frequency = PCLK1 */
+
+#define STM32_APB1_TIM3_CLKIN   (STM32_PCLK1_FREQUENCY)
+#define STM32_APB1_TIM6_CLKIN   (STM32_PCLK1_FREQUENCY)
+#define STM32_APB1_TIM7_CLKIN   (STM32_PCLK1_FREQUENCY)
+
+/* Timers driven from APB2 is equal to PCLK1 */
+
+#define STM32_APB2_TIM1_CLKIN   (STM32_PCLK1_FREQUENCY)
+#define STM32_APB2_TIM14_CLKIN  (STM32_PCLK1_FREQUENCY)
+#define STM32_APB2_TIM15_CLKIN  (STM32_PCLK1_FREQUENCY)
+#define STM32_APB2_TIM16_CLKIN  (STM32_PCLK1_FREQUENCY)
+#define STM32_APB2_TIM17_CLKIN  (STM32_PCLK1_FREQUENCY)
+
+/* LED definitions **********************************************************/
+
+/* Nucleo G070RB board has three LEDs. Two of these are controlled by
+ * logic on the board and are not available for software control:
+ *
+ * LD1 COM:  LD1 default status is red.  LD1 turns to green to indicate that
+ *           communications are in progress between the PC and the
+ *           ST-LINK/V2-1.
+ * LD3 5V_PWR:  green LED indicates that the board is powered by a 5V source.
+ *
+ * And one can be controlled by software:
+ *
+ * User LD4: green LED is a user LED connected to STM32 I/O PA5.
+ *
+ * If CONFIG_ARCH_LEDS is not defined, then the user can control the LED in
+ * any way.  The following definition is used to access the LED.
+ */
+
+/* LED index values for use with board_userled() */
+
+#define BOARD_LED1       0 /* User LD4 */
+#define BOARD_NLEDS      1
+
+/* LED bits for use with board_userled_all() */
+
+#define BOARD_LED1_BIT   (1 << BOARD_LED1)
+
+/* If CONFIG_ARCH_LEDs is defined, then NuttX will control the LED on the
+ * board. The following definitions describe how NuttX controls
+ * the LED:
+ *
+ *   SYMBOL              Meaning                  LED1 state
+ *   ------------------  -----------------------  ----------
+ *   LED_STARTED         NuttX has been started   OFF
+ *   LED_HEAPALLOCATE    Heap has been allocated  OFF
+ *   LED_IRQSENABLED     Interrupts enabled       OFF
+ *   LED_STACKCREATED    Idle stack created       ON
+ *   LED_INIRQ           In an interrupt          No change
+ *   LED_SIGNAL          In a signal handler      No change
+ *   LED_ASSERTION       An assertion failed      No change
+ *   LED_PANIC           The system has crashed   Blinking
+ *   LED_IDLE            STM32 is in sleep mode   Not used
+ */
+
+#define LED_STARTED      0
+#define LED_HEAPALLOCATE 0
+#define LED_IRQSENABLED  0
+#define LED_STACKCREATED 1
+#define LED_INIRQ        2
+#define LED_SIGNAL       2
+#define LED_ASSERTION    2
+#define LED_PANIC        1
+
+/* Button definitions *******************************************************/
+
+/* Nucleo G070RB board supports two buttons; only one button is controllable
+ * by software:
+ *
+ *   B1 USER:  user button connected to STM32 I/O PC13.
+ *   B2 RESET: push button connected to NRST; used to RESET the MCU.
+ */
+
+#define BUTTON_USER      0  /* User B1 */
+#define NUM_BUTTONS      1
+
+#define BUTTON_USER_BIT  (1 << BUTTON_USER)
+
+/* Alternate function pin selections ****************************************/
+
+/* I2C */
+
+#define GPIO_I2C1_SCL       (GPIO_I2C1_SCL_3|GPIO_SPEED_LOW)      /* PB8  */
+#define GPIO_I2C1_SDA       (GPIO_I2C1_SDA_3|GPIO_SPEED_LOW)      /* PB9  */
+
+/* TIM */
+
+#define GPIO_TIM1_CH1OUT    (GPIO_TIM1_CH1OUT_1|GPIO_SPEED_LOW)   /* PA8  */
+#define GPIO_TIM1_CH2OUT    (GPIO_TIM1_CH2OUT_2|GPIO_SPEED_LOW)   /* PB3  */
+#define GPIO_TIM1_CH3OUT    (GPIO_TIM1_CH3OUT_2|GPIO_SPEED_LOW)   /* PB6  */
+#define GPIO_TIM1_CH4OUT    (GPIO_TIM1_CH4OUT_1|GPIO_SPEED_LOW)   /* PA11 */
+#define GPIO_TIM1_CH1NOUT   (GPIO_TIM1_CH1NOUT_2|GPIO_SPEED_LOW)  /* PB13 */
+#define GPIO_TIM1_CH2NOUT   (GPIO_TIM1_CH2NOUT_2|GPIO_SPEED_LOW)  /* PB14 */
+#define GPIO_TIM1_CH3NOUT   (GPIO_TIM1_CH3NOUT_2|GPIO_SPEED_LOW)  /* PB15 */
+
+#define GPIO_TIM3_CH1OUT    (GPIO_TIM3_CH1OUT_2|GPIO_SPEED_LOW)   /* PB4  */
+#define GPIO_TIM3_CH2OUT    (GPIO_TIM3_CH2OUT_2|GPIO_SPEED_LOW)   /* PB5  */
+#define GPIO_TIM3_CH3OUT    (GPIO_TIM3_CH3OUT_1|GPIO_SPEED_LOW)   /* PB0  */
+#define GPIO_TIM3_CH4OUT    (GPIO_TIM3_CH4OUT_1|GPIO_SPEED_LOW)   /* PB1  */
+
+#define GPIO_TIM14_CH1OUT   (GPIO_TIM14_CH1OUT_2|GPIO_SPEED_LOW)  /* PA7  */
+
+#define GPIO_TIM15_CH1OUT   (GPIO_TIM15_CH1OUT_3|GPIO_SPEED_LOW)  /* PC1  */
+#define GPIO_TIM15_CH2OUT   (GPIO_TIM15_CH2OUT_3|GPIO_SPEED_LOW)  /* PC2  */
+#define GPIO_TIM15_CH1NOUT  (GPIO_TIM15_CH1NOUT_1|GPIO_SPEED_LOW) /* PA1  */
+
+#define GPIO_TIM16_CH1OUT   (GPIO_TIM16_CH1OUT_3|GPIO_SPEED_LOW)  /* PD0  */
+
+#define GPIO_TIM17_CH1OUT   (GPIO_TIM17_CH1OUT_2|GPIO_SPEED_LOW)  /* PD1  */
+
+/* USART */
+
+/* By default the USART2 is connected to STLINK Virtual COM Port:
+ * USART2_RX - PA3
+ * USART2_TX - PA2
+ */
+
+#define GPIO_USART2_RX      (GPIO_USART2_RX_1|GPIO_SPEED_HIGH)    /* PA3 */
+#define GPIO_USART2_TX      (GPIO_USART2_TX_1|GPIO_SPEED_HIGH)    /* PA2 */
+
+/* DMA channels *************************************************************/
+
+/* ADC */
+
+/* TODO ADC */
+
+#endif /* __BOARDS_ARM_STM32F0L0G0_NUCLEO_G070RB_INCLUDE_BOARD_H */
