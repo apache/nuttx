7e61c08aad2c58e82eb98b356ab6bab62f04fc4e
raiden00pl <raiden00@railab.me>
2026-05-28 12:12:39 +0200

    !boards/arm/stm32: move common board sources
    BREAKING CHANGE: Move the existing STM32 board common sources to
    boards/arm/common/stm32 and fold in the common STM32F0/L0/G0/C0
    board helpers so split STM32 board families can share one source
    tree.
    
    Signed-off-by: raiden00pl <raiden00@railab.me>

---
+++ boards/arm/common/stm32/src/stm32_l3gd20.c
@@ -0,0 +1,129 @@
+/****************************************************************************
+ * boards/arm/common/stm32/src/stm32_l3gd20.c
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+#include <errno.h>
+#include <nuttx/debug.h>
+#include <stdio.h>
+
+#include <nuttx/spi/spi.h>
+#include <nuttx/sensors/l3gd20.h>
+#include <arch/board/board.h>
+
+#include "stm32.h"
+#include "stm32_spi.h"
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Private Function Prototypes
+ ****************************************************************************/
+
+static int l3gd20_attach(struct l3gd20_config_s * cfg, xcpt_t irq);
+
+/****************************************************************************
+ * Private Data
+ ****************************************************************************/
+
+/* Only one L3GD20 device on board */
+
+static struct l3gd20_config_s g_l3gd20_config =
+{
+  .attach = l3gd20_attach,
+  .irq = BOARD_L3GD20_IRQ,
+  .spi_devid = SPIDEV_ACCELEROMETER(0)
+};
+
+/****************************************************************************
+ * Private Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: l3gd20_attach()
+ *
+ * Description: Attach the l3gd20 interrupt handler to the GPIO interrupt
+ *
+ ****************************************************************************/
+
+static int l3gd20_attach(struct l3gd20_config_s *cfg, xcpt_t irq)
+{
+  return stm32_gpiosetevent(BOARD_L3GD20_GPIO_DREADY, true, false,
+                            true, irq, NULL);
+}
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: board_l3gd20_initialize()
+ *
+ * Description:
+ *   Initialize and register the L3GD20 3 axis gyroscope sensor driver.
+ *
+ * Input Parameters:
+ *   devno - The device number, used to build the device path as
+ *           /dev/sensor/gyro_uncalN
+ *   busno - The SPI bus number
+ *
+ * Returned Value:
+ *   Zero (OK) on success; a negated errno value on failure.
+ *
+ ****************************************************************************/
+
+int board_l3gd20_initialize(int devno, int busno)
+{
+  int ret = 0;
+  struct spi_dev_s *spi;
+
+  /* Configure DREADY IRQ input */
+
+  stm32_configgpio(BOARD_L3GD20_GPIO_DREADY);
+
+  /* Initialize SPI */
+
+  spi = stm32_spibus_initialize(busno);
+
+  if (!spi)
+    {
+      ret = -ENODEV;
+      goto errout;
+    }
+
+  /* Then register the gyro */
+
+  ret = l3gd20_register(devno, spi, &g_l3gd20_config);
+  if (ret != OK)
+    {
+      goto errout;
+    }
+
+errout:
+  return ret;
+}
---
+++ boards/arm/common/stm32/src/stm32_lis3dsh.c
@@ -0,0 +1,117 @@
+/****************************************************************************
+ * boards/arm/common/stm32/src/stm32_lis3dsh.c
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+#include <errno.h>
+#include <nuttx/debug.h>
+#include <stdio.h>
+
+#include <nuttx/spi/spi.h>
+#include <nuttx/sensors/lis3dsh.h>
+#include <arch/board/board.h>
+
+#include "stm32.h"
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Private Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: attach_disc_lis3dsh
+ *
+ * Description:
+ *   Attach the lis3dsh interrupt handler to PE0/EXT0 on the STM32F4 as wired
+ *   on STM32F4Discovery
+ *
+ * Input Parameters:
+ *   config - The lis3dsh instance configuration data containing
+ *            the IRQ number, device ID and interrupt handler
+ *   interrupt_handler - The interrupt handler to attach
+ *   arg -
+ *
+ * Returned Value:
+ *   Zero (OK) on success; a negated errno value on failure.
+ *
+ ****************************************************************************/
+
+int attach_disc_lis3dsh(struct lis3dsh_config_s *config,
+                        xcpt_t interrupt_handler)
+{
+  return stm32_gpiosetevent(BOARD_LIS3DSH_GPIO_EXT0, true, false, false,
+                            interrupt_handler, NULL);
+}
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: board_lis3dsh_initialize
+ *
+ * Description:
+ *   Initialize and register the LIS3DSH 3-axis accelerometer.
+ *
+ * Input Parameters:
+ *   devno - The device number, used to build the device path as /dev/accN
+ *   busno - The SPI bus number
+ *
+ * Returned Value:
+ *   Zero (OK) on success; a negated errno value on failure.
+ *
+ ****************************************************************************/
+
+int board_lis3dsh_initialize(int devno, int busno)
+{
+  static struct lis3dsh_config_s acc0_config;
+  char devpath[12];
+  struct spi_dev_s *spi;
+  int ret;
+
+  sninfo("Initializing LIS3DSH\n");
+
+  acc0_config.irq = 22;
+  acc0_config.spi_devid = 0;
+  acc0_config.attach = &attach_disc_lis3dsh;
+
+  spi = stm32_spibus_initialize(1);
+  if (!spi)
+    {
+      spiinfo("Failed to initialize SPI port\n");
+      ret = -ENODEV;
+    }
+  else
+    {
+      snprintf(devpath, sizeof(devpath), "/dev/acc%d", devno);
+      ret = lis3dsh_register(devpath, spi, &acc0_config);
+    }
+
+  return ret;
+}
