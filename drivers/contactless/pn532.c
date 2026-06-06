9e9926317d15c6398d295a7b20b3059b7ac0574c
Matteo Golin <matteo.golin@gmail.com>
2026-06-06 16:54:27 -0400

    drivers/contactless/pn532: Fix potential overflow
    Fixes a potential overflow where more than 16 bytes are written to the
    cmd_buffer.
    
    Signed-off-by: Matteo Golin <matteo.golin@gmail.com>

--- drivers/contactless/pn532.c
+++ drivers/contactless/pn532.c
@@ -794,6 +794,7 @@ bool pn532_set_rf_config(struct pn532_dev_s * dev,
 
   pn532_frame_init(f, PN532_COMMAND_RFCONFIGURATION);
   f->data[1] = conf->cfg_item;
+  DEBUGASSERT(conf->data_size <= 16);
   memcpy(&f->data[2], conf->config, conf->data_size);
   f->len += conf->data_size + 1;
   pn532_frame_finish(f);
