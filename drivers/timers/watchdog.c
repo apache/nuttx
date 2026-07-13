apache/nuttx

commit 43797ea6cc431f98d9ea440824f9a84bd91ef3ec

drivers/timers/watchdog: add watchdog timer notifier chain
Add support for watchdog timer notifer chain so that users
can customize the callback function when the watchdog timer
times out which enabled by Auto-monitor

Signed-off-by: yaojiaqi <yaojiaqi@lixiang.com>

drivers/timers/watchdog.c
@@ -71,6 +71,20 @@
 #  endif
 #endif
 
+#if defined(CONFIG_WATCHDOG_AUTOMONITOR_BY_ONESHOT)
+#  define WATCHDOG_NOTIFIER_ACTION WATCHDOG_KEEPALIVE_BY_ONESHOT
+#elif defined(CONFIG_WATCHDOG_AUTOMONITOR_BY_TIMER)
+#  define WATCHDOG_NOTIFIER_ACTION WATCHDOG_KEEPALIVE_BY_TIMER
+#elif defined(CONFIG_WATCHDOG_AUTOMONITOR_BY_WDOG)
+#  define WATCHDOG_NOTIFIER_ACTION WATCHDOG_KEEPALIVE_BY_WDOG
+#elif defined(CONFIG_WATCHDOG_AUTOMONITOR_BY_WORKER)
+#  define WATCHDOG_NOTIFIER_ACTION WATCHDOG_KEEPALIVE_BY_WORKER
+#elif defined(CONFIG_WATCHDOG_AUTOMONITOR_BY_CAPTURE)
+#  define WATCHDOG_NOTIFIER_ACTION WATCHDOG_KEEPALIVE_BY_CAPTURE
+#elif defined(CONFIG_WATCHDOG_AUTOMONITOR_BY_IDLE)
+#  define WATCHDOG_NOTIFIER_ACTION WATCHDOG_KEEPALIVE_BY_IDLE
+#endif
+
 /****************************************************************************
  * Private Type Definitions
  ****************************************************************************/
@@ -134,6 +148,10 @@ static const struct file_operations g_wdogops =
   NULL,       /* seek */
   wdog_ioctl, /* ioctl */
 };
+
+#ifdef CONFIG_WATCHDOG_TIMEOUT_NOTIFIER
+static ATOMIC_NOTIFIER_HEAD(g_watchdog_notifier_list);
+#endif
 
 /****************************************************************************
  * Private Functions
@@ -698,6 +716,55 @@ static int wdog_ioctl(FAR struct file *filep, int cmd, unsigned long arg)
 /****************************************************************************
  * Public Functions
  ****************************************************************************/
+
+#ifdef CONFIG_WATCHDOG_TIMEOUT_NOTIFIER
+/****************************************************************************
+ * Name:  watchdog_notifier_chain_register
+ *
+ * Description:
+ *   Add notifier to the watchdog notifier chain
+ *
+ * Input Parameters:
+ *    nb - New entry in notifier chain
+ *
+ ****************************************************************************/
+
+void watchdog_notifier_chain_register(FAR struct notifier_block *nb)
+{
+  atomic_notifier_chain_register(&g_watchdog_notifier_list, nb);
+}
+
+/****************************************************************************
+ * Name:  watchdog_notifier_chain_unregister
+ *
+ * Description:
+ *   Remove notifier from the watchdog notifier chain
+ *
+ * Input Parameters:
+ *    nb - Entry to remove from notifier chain
+ *
+ ****************************************************************************/
+
+void watchdog_notifier_chain_unregister(FAR struct notifier_block *nb)
+{
+  atomic_notifier_chain_unregister(&g_watchdog_notifier_list, nb);
+}
+
+/****************************************************************************
+ * Name: watchdog_automonitor_timeout
+ *
+ * Description:
+ *   This function can be called in the watchdog timeout interrupt handler.
+ *   If so, callbacks on the watchdog timer notify chain are called when the
+ *   watchdog timer times out.
+ *
+ ****************************************************************************/
+
+void watchdog_automonitor_timeout(void)
+{
+  atomic_notifier_call_chain(&g_watchdog_notifier_list, action, data);
+}
+#endif /* CONFIG_WATCHDOG_TIMEOUT_NOTIFIER */
 
 /****************************************************************************
  * Name: watchdog_register

include/nuttx/timers/watchdog.h
@@ -31,6 +31,7 @@
 #include <nuttx/compiler.h>
 #include <nuttx/irq.h>
 #include <nuttx/fs/ioctl.h>
+#include <nuttx/notifier.h>
 
 #ifdef CONFIG_WATCHDOG
 
@@ -87,6 +88,35 @@
 #define WDFLAGS_RESET    (1 << 1) /* 1=Reset when the watchdog timer expires */
 #define WDFLAGS_CAPTURE  (1 << 2) /* 1=Call the user function when the
                                    *   watchdog timer expires */
+
+/* Keepalive Actions ********************************************************/
+
+/* According to the keepalive action specified by the Auto-monitor, callback
+ * functions registered on the watchdog notifier chain may take corresponding
+ * actions.
+ *
+ * These are detected and handled by the "upper half" watchdog timer driver.
+ *
+ * WATCHDOG_KEEPALIVE_BY_ONESHOT      - The watchdog timer is keepalive by
+ *                                      oneshot timer.
+ * WATCHDOG_KEEPALIVE_BY_TIMER        - The watchdog timer is keepalive by
+ *                                      timer.
+ * WATCHDOG_KEEPALIVE_BY_WDOG         - The watchdog timer is keepalive by
+ *                                      wdog.
+ * WATCHDOG_KEEPALIVE_BY_WORKER       - The watchdog timer is keepalive by
+ *                                      worker queue.
+ * WATCHDOG_KEEPALIVE_BY_CAPTURE      - The watchdog timer is keepalive by
+ *                                      capture.
+ * WATCHDOG_KEEPALIVE_BY_IDLE         - The watchdog timer is keepalive by
+ *                                      idle task.
+ */
+
+#define WATCHDOG_KEEPALIVE_BY_ONESHOT 0
+#define WATCHDOG_KEEPALIVE_BY_TIMER   1
+#define WATCHDOG_KEEPALIVE_BY_WDOG    2
+#define WATCHDOG_KEEPALIVE_BY_WORKER  3
+#define WATCHDOG_KEEPALIVE_BY_CAPTURE 4
+#define WATCHDOG_KEEPALIVE_BY_IDLE    5
 
 /****************************************************************************
  * Public Types
@@ -196,6 +226,47 @@ extern "C"
 #else
 #define EXTERN extern
 #endif
+
+#ifdef CONFIG_WATCHDOG_TIMEOUT_NOTIFIER
+/****************************************************************************
+ * Name:  watchdog_notifier_chain_register
+ *
+ * Description:
+ *   Add notifier to the watchdog notifier chain
+ *
+ * Input Parameters:
+ *    nb - New entry in notifier chain
+ *
+ ****************************************************************************/
+
+void watchdog_notifier_chain_register(FAR struct notifier_block *nb);
+
+/****************************************************************************
+ * Name:  watchdog_notifier_chain_unregister
+ *
+ * Description:
+ *   Remove notifier from the watchdog notifier chain
+ *
+ * Input Parameters:
+ *    nb - Entry to remove from notifier chain
+ *
+ ****************************************************************************/
+
+void watchdog_notifier_chain_unregister(FAR struct notifier_block *nb);
+
+/****************************************************************************
+ * Name: watchdog_automonitor_timeout
+ *
+ * Description:
+ *   This function can be called in the watchdog timeout interrupt handler.
+ *   If so, callbacks on the watchdog timer notify chain are called when the
+ *   watchdog timer times out.
+ *
+ ****************************************************************************/
+
+void watchdog_automonitor_timeout(void);
+
+#endif /* CONFIG_WATCHDOG_TIMEOUT_NOTIFIER */
 
 /****************************************************************************
  * Name: watchdog_register

drivers/timers/Kconfig
@@ -439,6 +439,14 @@ menuconfig WATCHDOG_AUTOMONITOR
 
 if WATCHDOG_AUTOMONITOR
 
+config WATCHDOG_TIMEOUT_NOTIFIER
+	bool "Enable watchdog timeout notifier"
+	default n
+	---help---
+		The watchdog timeout notifier chain mechanism supports users registering
+		custom callback functions, which will be called when the watchdog timer
+		managed by Auto-monitor times out.
+
 choice
 	prompt "Auto-monitor keepalive by"
 	default WATCHDOG_AUTOMONITOR_BY_WDOG

commit ebbeab10be92a64a1608f1dc3454a7ef581f3db2

drivers/timers/watchdog: add missing parameters to watchdog_automonitor_timeout
Fix incomplete implementation of watchdog timeout notifier chain.
The function watchdog_automonitor_timeout() was declared with void
parameters but used undefined 'action' and 'data' variables in its
body when calling atomic_notifier_call_chain().

Add 'action' and 'data' parameters to the function signature in both
the implementation and header file.

Fixes: 43797ea6cc (drivers/timers/watchdog: add watchdog timer notifier chain)

Signed-off-by: hanzhijian <hanzhijian@zepp.com>
Author: hanzhijian <hanzhijian@zepp.com>

drivers/timers/watchdog.c
@@ -760,7 +760,7 @@ void watchdog_notifier_chain_unregister(FAR struct notifier_block *nb)
  *
  ****************************************************************************/
 
-void watchdog_automonitor_timeout(void)
+void watchdog_automonitor_timeout(unsigned long action, FAR void *data)
 {
   atomic_notifier_call_chain(&g_watchdog_notifier_list, action, data);
 }

include/nuttx/timers/watchdog.h
@@ -264,7 +264,7 @@ void watchdog_notifier_chain_unregister(FAR struct notifier_block *nb);
  *
  ****************************************************************************/
 
-void watchdog_automonitor_timeout(void);
+void watchdog_automonitor_timeout(unsigned long action, FAR void *data);
 
 #endif /* CONFIG_WATCHDOG_TIMEOUT_NOTIFIER */
 