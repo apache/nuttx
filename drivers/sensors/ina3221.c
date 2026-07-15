47c60a6e8c65b8e85fe5b81748a8587a6bb54529
Catalin Visinescu <catalin_visinescu@yahoo.com>
2026-07-15 18:15:18 -0400

    drivers/: Multiple Drivers Are Registered With World Writable Part 3
    Summary
    
    Permissions (Part 3)
    
    Description:
    
    In kernel builds, any unprivileged process running on the NuttX device can
    open /dev/efuse and attempt to read/write fuse content. Reading the fuses
    may provide valuable information to an attacker controlling the user process.
    The write operation, in extreme cases where the fuse blocks are not locked,
    may brick the device.
    
    DISCLAIMER: I tried to be strict with the settings, better to relax them
    later if it's needed.
    
    This is part of https://github.com/apache/nuttx/issues/19410
    
    Impact
    
    See https://github.com/apache/nuttx/issues/19410
    
    Testing
    
    Compiles ok.
    
    Signed-off-by: Catalin Visinescu <catalin_visinescu@yahoo.com>

--- drivers/sensors/ina3221.c
+++ drivers/sensors/ina3221.c
@@ -393,7 +393,7 @@ int ina3221_register(FAR const char *devpath, FAR struct i2c_master_s *i2c,
 
   /* Register the character driver */
 
-  ret = register_driver(devpath, &g_ina3221fops, 0666, priv);
+  ret = register_driver(devpath, &g_ina3221fops, 0600, priv);
   if (ret < 0)
     {
       snerr("ERROR: Failed to register driver: %d\n", ret);
