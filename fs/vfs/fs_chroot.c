abdfc73861295722ed536b292ae9242850036b82
Abhishek Mishra <mishra.abhishek2808@gmail.com>
2026-08-19 08:07:17 +0000

    fs: add chroot() syscall
    Add CONFIG_FS_CHROOT and POSIX chroot(). Store the jail as an
    absolute path on the task group, and require euid 0 when user
    identity is enabled.
    
    Signed-off-by: Abhishek Mishra <mishra.abhishek2808@gmail.com>

---
+++ fs/vfs/fs_chroot.c
@@ -0,0 +1,131 @@
+/****************************************************************************
+ * fs/vfs/fs_chroot.c
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+#include <sys/stat.h>
+#include <assert.h>
+#include <errno.h>
+#include <limits.h>
+#include <string.h>
+
+#include <nuttx/fs/fs.h>
+#include <nuttx/kmalloc.h>
+#include <nuttx/sched.h>
+
+#include "inode/inode.h"
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: chroot
+ *
+ * Description:
+ *   Cause the named directory to become the root directory, that is, the
+ *   starting point for path names beginning with '/'.
+ *
+ * Input Parameters:
+ *   path - Directory to use as the new root
+ *
+ * Returned Value:
+ *   0(OK) on success; -1(ERROR) on failure with errno set appropriately.
+ *
+ ****************************************************************************/
+
+int chroot(FAR const char *path)
+{
+  FAR struct tcb_s *rtcb;
+  FAR struct task_group_s *group;
+  FAR char *newroot;
+  char abspath[PATH_MAX];
+  struct stat buf;
+  int ret;
+
+  if (path == NULL || path[0] == '\0')
+    {
+      set_errno(ENOENT);
+      return ERROR;
+    }
+
+  rtcb = nxsched_self();
+  DEBUGASSERT(rtcb != NULL && rtcb->group != NULL);
+  group = rtcb->group;
+
+#ifdef CONFIG_SCHED_USER_IDENTITY
+  if (group->tg_euid != 0)
+    {
+      set_errno(EPERM);
+      return ERROR;
+    }
+#endif
+
+  ret = nx_stat(path, &buf, 1);
+  if (ret < 0)
+    {
+      set_errno(-ret);
+      return ERROR;
+    }
+
+  if (!S_ISDIR(buf.st_mode))
+    {
+      set_errno(ENOTDIR);
+      return ERROR;
+    }
+
+  /* Resolve to a host absolute path the same way lookups do: make
+   * absolute, prepend the current jail, and normalize.  No second
+   * inode walk.
+   */
+
+  ret = inode_chroot_hostpath(path, abspath, sizeof(abspath));
+  if (ret < 0)
+    {
+      set_errno(-ret);
+      return ERROR;
+    }
+
+  /* Host "/" means no jail.  Clear any previous root. */
+
+  if (strcmp(abspath, "/") == 0)
+    {
+      kmm_free(group->tg_root);
+      group->tg_root = NULL;
+      return OK;
+    }
+
+  newroot = strdup(abspath);
+  if (newroot == NULL)
+    {
+      set_errno(ENOMEM);
+      return ERROR;
+    }
+
+  kmm_free(group->tg_root);
+  group->tg_root = newroot;
+  return OK;
+}
