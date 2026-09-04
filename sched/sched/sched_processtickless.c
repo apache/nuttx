a7ce26924e35782c63dff6b4f15631a3e56df761
ouyangxiangzhen <ouyangxiangzhen@xiaomi.com>
2026-09-04 14:04:21 +0800

    style: add missing blank line after declarations
    Fix checkpatch "Missing blank line after declarations" errors in
    drivers/timers/arch_timer.c and sched/sched/sched_processtickless.c.
    These are pre-existing issues, not introduced by the recent tickless
    RR series.
    
    Signed-off-by: ouyangxiangzhen <ouyangxiangzhen@xiaomi.com>

--- sched/sched/sched_processtickless.c
+++ sched/sched/sched_processtickless.c
@@ -105,6 +105,7 @@ int up_timer_gettick(FAR clock_t *ticks)
 {
   struct timespec ts;
   int ret;
+
   ret = up_timer_gettime(&ts);
   *ticks = clock_time2ticks_floor(&ts);
   return ret;
@@ -115,6 +116,7 @@ int up_timer_gettick(FAR clock_t *ticks)
 int up_alarm_tick_start(clock_t ticks)
 {
   struct timespec ts;
+
   clock_ticks2time(&ts, ticks);
   return up_alarm_start(&ts);
 }
@@ -124,6 +126,7 @@ int up_alarm_tick_start(clock_t ticks)
 int up_timer_tick_start(clock_t ticks)
 {
   struct timespec ts;
+
   clock_ticks2time(&ts, ticks);
   return up_timer_start(&ts);
 }
@@ -213,6 +216,7 @@ static clock_t nxsched_cpu_scheduler(int cpu, clock_t ticks,
   if ((rtcb->flags & TCB_FLAG_POLICY_MASK) == TCB_FLAG_SCHED_SPORADIC)
     {
       FAR struct sporadic_s *sporadic = rtcb->sporadic;
+
       DEBUGASSERT(sporadic);
 
       /* Save the last time that the scheduler ran.  This time was saved
