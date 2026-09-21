421407c5d9a0e1f27f0f513cd8de88413331b023
Jukka Laitinen <jukka.laitinen@tii.ae>
2026-09-21 08:53:09 +0300

    arch/arm/imxrt: Add missing imxrt118x IRQ 238/239 definitions
    On imxrt1180 there are 240 IRQs. Add the missiong ones:
    
      IRQ 238  ECAT   EtherCAT Reset out (ECAT_RESET_OUT pin-mux signal)
      IRQ 239  EdgeLock  EdgeLock interrupt
    
    Signed-off-by: Jukka Laitinen <jukka.laitinen@tii.ae>

--- arch/arm/include/imxrt/imxrt118x_irq.h
+++ arch/arm/include/imxrt/imxrt118x_irq.h
@@ -273,10 +273,12 @@
 #define IMXRT_IRQ_ASRC                                (IMXRT_IRQ_EXTINT + 235)
 #define IMXRT_IRQ_GPIO6                               (IMXRT_IRQ_EXTINT + 236)
 #define IMXRT_IRQ_DBG_TRACE                           (IMXRT_IRQ_EXTINT + 237)
+#define IMXRT_IRQ_ECAT_RESET_OUT                      (IMXRT_IRQ_EXTINT + 238)
+#define IMXRT_IRQ_EDGELOCK                            (IMXRT_IRQ_EXTINT + 239)
 
 /* Total number of external interrupts (matches the SDK IRQn_Type range). */
 
-#define IMXRT_IRQ_NEXTINT        (238)
+#define IMXRT_IRQ_NEXTINT        (240)
 
 /* Interrupt count for the M33 core on imxrt118x. */
 
